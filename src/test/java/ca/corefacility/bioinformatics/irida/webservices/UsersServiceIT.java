c75419a4bcea4fa67a55850f8c3129061c4f0549
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-02-08 14:30:01 -0600

    Do not show passwords in the response.

--- src/test/java/ca/corefacility/bioinformatics/irida/webservices/UsersServiceIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/webservices/UsersServiceIT.java
@@ -1,13 +1,26 @@
 package ca.corefacility.bioinformatics.irida.webservices;
 
+import com.jayway.restassured.RestAssured;
 import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UsersServiceIT {
 
+    @Before
+    public void setUp() {
+        RestAssured.authentication = RestAssured.basic("fbristow", "password1");
+    }
+
     @Test
     public void testGetUsers() {
-        given().auth().basic("fbristow", "password1").expect().body("id", hasItems(0, 1)).when().get("/api/users");
+        expect().body("id", hasItems(0, 1)).when().get("/api/users");
+    }
+
+    @Test
+    public void testNoPasswords() {
+        expect().body("password", hasItems(nullValue(), nullValue())).when().get("/api/users");
+        // asking for a field that doesn't exist returns null.
     }
 }
