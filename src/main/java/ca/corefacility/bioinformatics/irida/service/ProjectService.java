6408f7335c889b46c2d9fba6c652ebd64192f0be
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-03-26 13:50:04 -0500

    Add support for persisting Projects.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/ProjectService.java
@@ -0,0 +1,26 @@
+package ca.corefacility.bioinformatics.irida.service;
+
+import ca.corefacility.bioinformatics.irida.model.Project;
+import ca.corefacility.bioinformatics.irida.model.Role;
+import ca.corefacility.bioinformatics.irida.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * A specialized service layer for projects.
+ *
+ * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
+ */
+@Transactional
+public interface ProjectService extends CRUDService<String, Project> {
+
+    /**
+     * Add the specified user to the project with a role. If the user is a
+     * manager for the project, then the user should be added to the project
+     * with the 'ROLE_MANAGER' role.
+     *
+     * @param project the project to add the user to.
+     * @param user the user to add to the project.
+     * @param role the role that the user plays on the project.
+     */
+    public void addUserToProject(Project project, User user, Role role);
+}
