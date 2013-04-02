21632c29bc16de65ea485edbcc8217faf16395db
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-04-02 10:45:34 -0500

    Working on setting up watches for the scss.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,39 @@
 'use strict';
 
 module.exports = function (grunt) {
+  // Load all grunt tasks
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
+  // Configure paths
+  var iridaConfig = {
+    dev: 'src/main/webapp/resources/dev'
+  };
+
   grunt.initConfig({
+    irida: iridaConfig,
+    watch: {
+      sass: {
+        dev: {
+          files: {
+            '<%= irida.app %>/scss/users.scss': '<%= irida.app %>/css/users.css'
+          }
+        }
+      }
+    },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
       all: [
         'Gruntfile.js',
-        'src/main/webapp/resources/js/{,*/}*.js',
-        '!src/main/webapp/resources/js/vendor/*',
+        'src/main/webapp/resources/dev/js/{,*/}*.js',
+        '!src/main/webapp/resources/dev/js/vendor/*',
         'test/javascript/spec/{,*/}*.js'
       ]
     }
   });
+
+  grunt.renameTask('regarde', 'watch');
 
   grunt.registerTask('default', [
     'jshint'
