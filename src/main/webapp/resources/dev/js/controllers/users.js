9e3663af3bcd9f73ed3326a5f50a881531bd7b07
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-04-11 08:26:56 -0500

    Set up proper angularjs form validation.

--- src/main/webapp/resources/dev/js/controllers/users.js
+++ src/main/webapp/resources/dev/js/controllers/users.js
@@ -1,94 +1,122 @@
-'use strict';
+function usersListCtrl ($scope) {
+  'use strict';
+  
+}
 
-var app = angular.module('sraApp', ['ngSanitize']);
+function newUserModalCtrl ($scope, $http) {
+  'use strict';
 
-app.controller('usersListCtrl', ['$scope', '$window', function ($scope, $window) {
-  $scope.goToUser = function (e) {
-    var url = e.currentTarget.childNodes[1].innerHTML;
-    $window.location = url;
-  };
-}]);
+  $scope.currUser = {};
 
-app.controller('newUserModalCtrl', ['$scope', function ($scope) {
-  var userListPosn = {
-    start: 21,
-    offset: 20
-  };
-
-  $scope.currUser = {
-    username: '',
-    password: '',
-    email: '',
-    phoneNumber: '',
-    firstName: '',
-    lastName: ''
-  };
-
-  $scope.errors = {
-    username: {
-      message: '',
-      doesExist: false
-    },
-    password: {
-      message: '',
-      doesExist: false
-    },
-    email: {
-      message: '',
-      doesExist: false
-    },
-    phoneNumber: {
-      message: '',
-      doesExist: false
-    },
-    firstName: {
-      message: '',
-      doesExist: false
-    },
-    lastName: {
-      message: '',
-      doesExist: false
-    }
+  $scope.error = {
+    username :"required",
+    password: "required",
+    firstName: "required",
+    lastName: "required",
+    email: 'required',
+    phoneNumber: 'required'
   };
 
   $scope.postNewUser = function () {
-    $.ajax({
-      type: 'POST',
-      data: $scope.currUser,
-      success: function () {
-        updateUserList();
-      },
-      error: function (XMLHttpRequest, textStatus, errorThrown) {
-        if(errorThrown){
-          // TODO(josh): handle the error
-        }
-        $scope.$apply($.each($.parseJSON(XMLHttpRequest.responseText), function (key, value) {
-          var message = value.join('<br/>');
-          $scope.errors[key].message = message;
-          $scope.errors[key].doesExist = true;
-        }));
-      }
-    });
-  };
+   $.ajax({
+     type: 'POST',
+     data: $scope.currUser,
+     success: function () {
+       updateUserList();
+     },
+     error: function (XMLHttpRequest, textStatus, errorThrown) {
+       if(errorThrown){
+         // TODO(josh): handle the error
+       }
+       $scope.$apply($.each($.parseJSON(XMLHttpRequest.responseText), function (key, value) {
+         var message = value.join('<br/>');
+         $scope.error[key] = message;
+         $scope.newUserForm.$setValidity(key, false);
+       }));
+     }
+   });
+ }
 
-  $scope.focusedInput = function (field) {
-    $scope.errors[field].doesExist = false;
-  };
 
-  var updateUserList = function () {
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      contentType: 'application/json; charset=utf-8',
-      dataType: 'json',
-      data: userListPosn,
-      success: function (d) {
-        console.log(d);
-        // TODO(josh): Clear the users table and plug d.users into it.  Need to compile the template.
-      },
-      error: function (d) {
-        console.log(d);
-      }
-    });
-  };
-}]);
+ $scope.focusedInput = function (field) {
+   $scope.errors[field].doesExist = false;
+ };
+}
+
+function updateUserList () {
+  'use strict';
+
+  console.log("This needs to be implemented with a link from Franklin.")
+//   $.ajax({
+//     type: 'GET',
+//     url: '/users',
+//     contentType: 'application/json; charset=utf-8',
+//     dataType: 'json',
+//     data: userListPosn,
+//     success: function (d) {
+//       console.log(d);
+//       // TODO(josh): Clear the users table and plug d.users into it.  Need to compile the template.
+//     },
+//     error: function (d) {
+//       console.log(d);
+//     }
+//   });
+ }
+//'use strict';
+//
+//var app = angular.module('sraApp', ['ngSanitize']);
+//
+//app.controller('usersListCtrl', ['$scope', '$window', function ($scope, $window) {
+//  $scope.goToUser = function (e) {
+//    var url = e.currentTarget.childNodes[1].innerHTML;
+//    $window.location = url;
+//  };
+//}]);
+//
+//app.controller('newUserModalCtrl', ['$scope', function ($scope) {
+//  var userListPosn = {
+//    start: 21,
+//    offset: 20
+//  };
+//
+//  $scope.currUser = {
+//    username: '',
+//    password: '',
+//    email: '',
+//    phoneNumber: '',
+//    firstName: '',
+//    lastName: ''
+//  };
+//
+//  $scope.errors = {
+//    username: {
+//      message: '',
+//      doesExist: false
+//    },
+//    password: {
+//      message: '',
+//      doesExist: false
+//    },
+//    email: {
+//      message: '',
+//      doesExist: false
+//    },
+//    phoneNumber: {
+//      message: '',
+//      doesExist: false
+//    },
+//    firstName: {
+//      message: '',
+//      doesExist: false
+//    },
+//    lastName: {
+//      message: '',
+//      doesExist: false
+//    }
+//  };
+//
+//  $scope.postNewUser = function () {
+//  };
+//
+//
+//}]);
