f1350246f6707acb265ecfdcc59734be455d5e30
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-04-15 19:42:49 -0500

    Replaced the functionality of angularjs with knockout.  Fixed the problem I was having with the json call when using the back button.  It was a caching issue.  I think I will stay now with knockout.

--- src/main/webapp/resources/dev/js/controllers/users.js
+++ src/main/webapp/resources/dev/js/controllers/users.js
@@ -1,137 +1,30 @@
-function usersListCtrl($scope, Users) {
-  'use strict';
-
-  $scope.users = Users.get();
-
-  $scope.viewUser = function (user) {
-    window.location = user.links[0].href;
-  }
-
+function User(data) {
+  "use strict";
+  this.href = data.links[0].href;
+  this.username = data.username;
+  this.firstName = data.firstName;
+  this.lastName = data.lastName;
+  this.email = data.email;
+  this.phoneNumber = data.phoneNumber;
 }
 
-function newUserModalCtrl($scope) {
-  'use strict';
+function UsersViewModel() {
+  "use strict";
+  var self = this;
 
-  $scope.currUser = {};
+  self.users = ko.observableArray([]);
 
-  $scope.error = {
-    username   : 'Required',
-    password   : 'Required',
-    firstName  : 'Required',
-    lastName   : 'Required',
-    email      : 'Required',
-    phoneNumber: 'Required'
-  };
-
-  $scope.postNewUser = function () {
-    $.ajax({
-      type   : 'POST',
-      data   : $scope.currUser,
-      success: function () {
-        updateUserList();
-      },
-      error  : function (XMLHttpRequest, textStatus, errorThrown) {
-        if (errorThrown) {
-          // TODO(josh): handle the error
-        }
-        $scope.$apply($.each($.parseJSON(XMLHttpRequest$scop.responseText), function (key, value) {
-          var message = value.join('<br/>');
-          $scope.error[key] = message;
-          $scope.newUserForm.$setValidity(key, false);
-        }));
-      }
+  $.getJSON("/users", function (allData) {
+    var mappedUsers = $.map(allData.users, function (item) {
+      return new User(item)
     });
-  };
+    self.users(mappedUsers);
+  });
 }
 
-function updateUserList($scope) {
-  'use strict';
-
-  console.log("This needs to be implemented with a link from Franklin.");
-//   $.ajax({
-//     type: 'GET',
-//     url: '/users',
-//     contentType: 'application/json; charset=utf-8',
-//     dataType: 'json',
-//     data: userListPosn,
-//     success: function (d) {
-//       console.log(d);
-//       // TODO(josh): Clear the users table and plug d.users into it.  Need to compile the template.
-//     },
-//     error: function (d) {
-//       console.log(d);
-//     }
-//   });
+function viewUser (event, data) {
+  "use strict";
+  window.location = data.href;
 }
-//  factory('usersService', ['$http', function ($http) {
-//    "use strict";
-//    var usersService = {
-//      async: function () {
-//        var promise = $http.get('/users').then(function (response) {
-//          return response.data;
-//        });
-//        return promise;
-//      }
-//    };
-//    return usersService;
-//  }]);
-
-
-//'use strict';
-//
-//var app = angular.module('sraApp', ['ngSanitize']);
-//
-//app.controller('usersListCtrl', ['$scope', '$window', function ($scope, $window) {
-//  $scope.goToUser = function (e) {
-//    var url = e.currentTarget.childNodes[1].innerHTML;
-//    $window.location = url;
-//  };
-//}]);
-//
-//app.controller('newUserModalCtrl', ['$scope', function ($scope) {
-//  var userListPosn = {
-//    start: 21,
-//    offset: 20
-//  };
-//
-//  $scope.currUser = {
-//    username: '',
-//    password: '',
-//    email: '',
-//    phoneNumber: '',
-//    firstName: '',
-//    lastName: ''
-//  };
-//
-//  $scope.errors = {
-//    username: {
-//      message: '',
-//      doesExist: false
-//    },
-//    password: {
-//      message: '',
-//      doesExist: false
-//    },
-//    email: {
-//      message: '',
-//      doesExist: false
-//    },
-//    phoneNumber: {
-//      message: '',
-//      doesExist: false
-//    },
-//    firstName: {
-//      message: '',
-//      doesExist: false
-//    },
-//    lastName: {
-//      message: '',
-//      doesExist: false
-//    }
-//  };
-//
-//  $scope.postNewUser = function () {
-//  };
-//
-//
-//}]);
+$.ajaxSetup({ cache: false });
+ko.applyBindings(new UsersViewModel());
