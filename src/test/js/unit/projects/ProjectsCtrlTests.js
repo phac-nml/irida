28212af20739b76a693493cd5dce01cb0df398d8
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-04-23 08:13:04 -0500

    Created a test for the project tests.

---
+++ src/test/js/unit/projects/ProjectsCtrlTests.js
@@ -0,0 +1,37 @@
+describe("ProjectsCtrl", function () {
+  var newScope, controller, $httpBackend;
+
+  beforeEach(module('irida'));
+  beforeEach(inject(function ($rootScope, $controller, $injector) {
+    newScope = $rootScope.$new();
+    controller = $controller("ProjectsListCtrl", {$scope: newScope});
+
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', '/projects').respond({projectResources: {projects: [
+      {firstName: 'josh'}
+    ]}});
+  }));
+
+  afterEach(function () {
+    "use strict";
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+//  it("Should have an empty users array", function () {
+//    "use strict";
+//    expect(newScope.users.length).toBe(0);
+//  });
+//
+//  it("Should make a server call", function () {
+//    runs(function () {
+//      $httpBackend.expectGET('/users');
+//      newScope.loadProjects('/users');
+//      waits(1000);
+//      runs(function () {
+//        expect(newScope.users.length).toBe(1);
+//      });
+//      $httpBackend.flush();
+//    });
+//  });
+});
