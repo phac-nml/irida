d082298130aafa077a0fb32dc22c7c732924970f
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-04-26 15:11:27 -0500

    Add invalid property exception file.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/exceptions/InvalidPropertyException.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2013 Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.corefacility.bioinformatics.irida.exceptions;
+
+/**
+ * Thrown when a property cannot be set or retrieved by a service class.
+ *
+ * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
+ */
+public class InvalidPropertyException extends RuntimeException {
+
+    public InvalidPropertyException(String message) {
+        super(message);
+    }
+}
