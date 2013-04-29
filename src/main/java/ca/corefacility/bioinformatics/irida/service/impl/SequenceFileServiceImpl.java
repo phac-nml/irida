17f0ed4e2f78f8483896e0de15fd0bfda5920f23
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-04-28 21:27:28 -0500

    Implement custom logic for managing the actual files associated with SequenceFile objects.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequenceFileServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequenceFileServiceImpl.java
@@ -18,6 +18,9 @@ package ca.corefacility.bioinformatics.irida.service.impl;
 import ca.corefacility.bioinformatics.irida.model.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
 import ca.corefacility.bioinformatics.irida.repositories.CRUDRepository;
+import com.google.common.collect.ImmutableMap;
+import java.io.File;
+import java.util.Map;
 import javax.validation.Validator;
 
 /**
@@ -27,14 +30,20 @@ import javax.validation.Validator;
  */
 public class SequenceFileServiceImpl extends CRUDServiceImpl<Identifier, SequenceFile> {
 
+    private CRUDRepository<File, SequenceFile> fileRepository;
+
     /**
      * Constructor.
      *
      * @param sequenceFileRepository the sequence file repository.
      * @param validator validator.
      */
-    public SequenceFileServiceImpl(CRUDRepository<Identifier, SequenceFile> sequenceFileRepository, Validator validator) {
+    public SequenceFileServiceImpl(
+            CRUDRepository<Identifier, SequenceFile> sequenceFileRepository,
+            CRUDRepository<File, SequenceFile> fileRepository,
+            Validator validator) {
         super(sequenceFileRepository, validator, SequenceFile.class);
+        this.fileRepository = fileRepository;
     }
 
     /**
@@ -43,9 +52,29 @@ public class SequenceFileServiceImpl extends CRUDServiceImpl<Identifier, Sequenc
     @Override
     public SequenceFile create(SequenceFile sequenceFile) {
         // Send the file to the database repository to be stored (in super)
-        
+        sequenceFile = super.create(sequenceFile);
         // Then store the file in an appropriate directory
-        
-        throw new UnsupportedOperationException();
+        sequenceFile = fileRepository.create(sequenceFile);
+        // And finally, update the database with the stored file location
+
+        sequenceFile = super.update(sequenceFile.getIdentifier(),
+                ImmutableMap.of("file", (Object) sequenceFile.getFile()));
+        return sequenceFile;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public SequenceFile update(Identifier id, Map<String, Object> updatedFields) {
+        SequenceFile updated = super.update(id, updatedFields);
+
+        if (updatedFields.containsKey("file")) {
+            updated = fileRepository.update(updated);
+            updated = super.update(id, ImmutableMap.of("file",
+                    (Object) updated.getFile()));
+        }
+
+        return updated;
     }
 }
