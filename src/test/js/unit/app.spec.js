5318a7dc5325ac42a970b51560b39d8961cb21ce
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-05-07 11:41:08 -0500

    New testing specs

--- src/test/js/unit/app.spec.js
+++ src/test/js/unit/app.spec.js
@@ -5,3 +5,21 @@
  * Time: 8:28 AM
  * To change this template use File | Settings | File Templates.
  */
+
+describe('AppCtrl', function () {
+  describe('isCurrentUrl', function () {
+    var AppCtrl, $location, $scope;
+
+    beforeEach(module('irida'));
+
+    beforeEach( inject( function ($controller, _$location_, $rootScope) {
+      $location = _$location_;
+      $scope = $rootScope.$new();
+      AppCtrl = $controller( 'AppCtrl', { $location : $location, $scope: $scope});
+    }));
+
+    it( 'Should pass a dummy test', inject( function () {
+      expect(AppCtrl).toBeTruthy();
+    }));
+  });
+});
