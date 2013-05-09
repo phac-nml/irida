9fbcdbefef8990dba06869c9440892583e5ef259
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-05-09 14:43:41 -0500

    Formatting

--- src/main/webapp/resources/dev/js/user.js
+++ src/main/webapp/resources/dev/js/user.js
@@ -9,80 +9,80 @@
  * It is responsible for the user view.
  */
 angular.module('irida.user', [
-  'ajaxService',
-  'messagingService'])
+    'ajaxService',
+    'messagingService'])
 
 /**
  * Configure the route parameters
  * $routeProvider
  */
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when(
-    '/users/:username', {
-    templateUrl: './partials/user.html',
-    controller: function ($scope, initData) {
-      $scope.user = initData.resource;
-      $scope.links = initData.resource.links;
-    },
-    resolve: {
-      initData: function ($q, $route, ajaxService) {
-        var defer = $q.defer();
-        var username = $route.current.params.username;
-        ajaxService.get('/users/' + username).then(function (data) {
-          defer.resolve(data);
-        });
-        return defer.promise;
-      }
-    }
-  })
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when(
+      '/users/:username', {
+        templateUrl: './partials/user.html',
+        controller: function ($scope, initData) {
+          $scope.user = initData.resource;
+          $scope.links = initData.resource.links;
+        },
+        resolve: {
+          initData: function ($q, $route, ajaxService) {
+            var defer = $q.defer();
+            var username = $route.current.params.username;
+            ajaxService.get('/users/' + username).then(function (data) {
+              defer.resolve(data);
+            });
+            return defer.promise;
+          }
+        }
+      })
+  }])
 
 /**
  * User View Contoller
  */
-.controller('UserCtrl', ['$scope', '$location', 'ajaxService', 'messagingService', function ($scope, $location, ajaxService, messagingService) {
-  'use strict';
+  .controller('UserCtrl', ['$scope', '$location', 'ajaxService', 'messagingService', function ($scope, $location, ajaxService, messagingService) {
+    'use strict';
 
-  $scope.deleteUser = function () {
-    ajaxService.delete($scope.links.self).then(
+    $scope.deleteUser = function () {
+      ajaxService.delete($scope.links.self).then(
 
-    function () {
-      $scope.notifier.icon = "trash";
-      $scope.notifier.message = "Deleted " + $scope.user.username;
-      messagingService.broadcast('notify');
-      $location.path('/users');
-    },
+        function () {
+          $scope.notifier.icon = "trash";
+          $scope.notifier.message = "Deleted " + $scope.user.username;
+          messagingService.broadcast('notify');
+          $location.path('/users');
+        },
 
-    function () {
-      $scope.notifier.icon = 'ban-circle';
-      $scope.notifier.message = 'Could not delete ' + $scope.user.username;
-      messagingService.broadcast('notify');
-    });
-  };
+        function () {
+          $scope.notifier.icon = 'ban-circle';
+          $scope.notifier.message = 'Could not delete ' + $scope.user.username;
+          messagingService.broadcast('notify');
+        });
+    };
 
-  $scope.handleEnter = function ($event) {
-    $event.currentTarget.blur();
-  };
+    $scope.handleEnter = function ($event) {
+      $event.currentTarget.blur();
+    };
 
-  $scope.blur = function (name) {
-    var form = $scope.editUserForm;
-    if (form[name].$invalid) {
-      console.log("NOT VALIDE");
-    } else if ($scope.user[name] != $scope.original[name]) {
-      ajaxService.patch($scope.links.self, '{"' + name + '":"' + $scope.user[name] + '"}').then(
+    $scope.blur = function (name) {
+      var form = $scope.editUserForm;
+      if (form[name].$invalid) {
+        console.log("NOT VALIDE");
+      } else if ($scope.user[name] != $scope.original[name]) {
+        ajaxService.patch($scope.links.self, '{"' + name + '":"' + $scope.user[name] + '"}').then(
 
-      function (data) {
-        $scope.notifier.icon = "save";
-        $scope.notifier.message = "Saved " + name + ": " + $scope.user[name];
-        messagingService.broadcast('notify');
+          function (data) {
+            $scope.notifier.icon = "save";
+            $scope.notifier.message = "Saved " + name + ": " + $scope.user[name];
+            messagingService.broadcast('notify');
 
-        // Update the original
-        $scope.original[name] = $scope.user[name];
-      },
+            // Update the original
+            $scope.original[name] = $scope.user[name];
+          },
 
-      function () {
-        console.log("ERROR");
-      });
-    }
-  };
-}]);
+          function () {
+            console.log("ERROR");
+          });
+      }
+    };
+  }]);
--- src/main/webapp/resources/dev/js/usersList.js
+++ src/main/webapp/resources/dev/js/usersList.js
@@ -1,100 +1,100 @@
 /* global angular, console */
 angular.module('irida.users', [
-  'ajaxService'])
+    'ajaxService'])
 
 /**
  * Configure the route parameters
  * $routeProvider
  */
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.when(
-    '/users', {
-    templateUrl: './partials/users.html',
-    controller: function ($scope, initData) {
-      $scope.users = initData.resource.resources;
-      $scope.links = initData.resource.links;
-    },
-    resolve: {
-      initData: function ($q, ajaxService) {
-        var defer = $q.defer();
-        ajaxService.get('/users').then(function (data) {
-          defer.resolve(data);
-        });
-        return defer.promise;
-      }
-    }
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when(
+      '/users', {
+        templateUrl: './partials/users.html',
+        controller: function ($scope, initData) {
+          $scope.users = initData.resource.resources;
+          $scope.links = initData.resource.links;
+        },
+        resolve: {
+          initData: function ($q, ajaxService) {
+            var defer = $q.defer();
+            ajaxService.get('/users').then(function (data) {
+              defer.resolve(data);
+            });
+            return defer.promise;
+          }
+        }
+      });
+  }])
   .controller('UsersListCtrl', ['$scope', '$route', '$location', 'ajaxService', function ($scope, $route, $location, ajaxService) {
-  'use strict';
+    'use strict';
 
-  $scope.loadUsers = function (url) {
-    if (url) {
-      ajaxService.get(url).then(
+    $scope.loadUsers = function (url) {
+      if (url) {
+        ajaxService.get(url).then(
 
-      function (data) {
-        ajaxSuccessCallback(data);
-      },
+          function (data) {
+            ajaxSuccessCallback(data);
+          },
 
-      function (errorMessage) {
-        // TODO: handle error message
-        console.log(errorMessage);
-      });
-    }
-  };
-
-  $scope.gotoUser = function (url) {
-    $location.path(url.match(/\/users\/.*$/)[0]);
-  };
-
-  $scope.clearForm = function () {
-    $scope.newUser = {};
-    $scope.errors = {};
-
-    // Need to reset all the fields in the form.
-    $('form[name=newUserForm] .ng-dirty').removeClass('ng-dirty').addClass('ng-pristine');
-    var form = $scope.newUserForm;
-    for (var field in form) {
-      if (form[field].$pristine === false) {
-        form[field].$pristine = true;
+          function (errorMessage) {
+            // TODO: handle error message
+            console.log(errorMessage);
+          });
       }
-      if (form[field].$dirty === true) {
-        form[field].$dirty = false;
+    };
+
+    $scope.gotoUser = function (url) {
+      $location.path(url.match(/\/users\/.*$/)[0]);
+    };
+
+    $scope.clearForm = function () {
+      $scope.newUser = {};
+      $scope.errors = {};
+
+      // Need to reset all the fields in the form.
+      $('form[name=newUserForm] .ng-dirty').removeClass('ng-dirty').addClass('ng-pristine');
+      var form = $scope.newUserForm;
+      for (var field in form) {
+        if (form[field].$pristine === false) {
+          form[field].$pristine = true;
+        }
+        if (form[field].$dirty === true) {
+          form[field].$dirty = false;
+        }
+        if (form[field].$invalid) {
+          form[field].$invalid = false;
+        }
       }
-      if (form[field].$invalid) {
-        form[field].$invalid = false;
+      $scope.newUserForm.$pristine = true;
+    };
+
+    $scope.submitNewUser = function () {
+      if ($scope.newUserForm.$valid) {
+        ajaxService.create('/users', $scope.newUser).then(
+
+          function () {
+            $scope.loadUsers('/users');
+            $scope.clearForm();
+            $('#newUserModal').foundation('reveal', 'close');
+          },
+
+          function (data) {
+            $scope.errors = {};
+            angular.forEach(data, function (error, key) {
+              $scope.errors[key] = data[key].join('</br>');
+            });
+          });
+      } else {
+        console.log('NOT VALID');
       }
+    };
+
+    function ajaxSuccessCallback(data) {
+      $scope.links = data.resources.links;
+      $scope.users = data.resources.resources;
     }
-    $scope.newUserForm.$pristine = true;
-  };
 
-  $scope.submitNewUser = function () {
-    if ($scope.newUserForm.$valid) {
-      ajaxService.create('/users', $scope.newUser).then(
-
-      function () {
-        $scope.loadUsers('/users');
-        $scope.clearForm();
-        $('#newUserModal').foundation('reveal', 'close');
-      },
-
-      function (data) {
-        $scope.errors = {};
-        angular.forEach(data, function (error, key) {
-          $scope.errors[key] = data[key].join('</br>');
-        });
-      });
-    } else {
-      console.log('NOT VALID');
-    }
-  };
-
-  function ajaxSuccessCallback(data) {
-    $scope.links = data.resources.links;
-    $scope.users = data.resources.resources;
-  }
-
-  $scope.$on('$routeChangeSuccess', function () {
-    $(document).foundation('reveal', {});
-  });
-}]);
+    $scope.$on('$routeChangeSuccess', function () {
+      $(document).foundation('reveal', {});
+    });
+  }]);
