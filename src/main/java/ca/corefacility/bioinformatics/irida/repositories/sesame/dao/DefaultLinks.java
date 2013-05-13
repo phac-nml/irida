cfc9161a65d012df61d1484a335aa5b84a153cc4
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-05-13 16:09:13 -0500

    Making services and implentations for storing and reading Links

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/DefaultLinks.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2013 Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.corefacility.bioinformatics.irida.repositories.sesame.dao;
+
+import ca.corefacility.bioinformatics.irida.model.Project;
+import ca.corefacility.bioinformatics.irida.model.User;
+import ca.corefacility.bioinformatics.irida.model.alibaba.IridaThing;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ */
+public class DefaultLinks {
+    HashMap<Class, HashMap<Class,RdfPredicate> > links = new HashMap<>();
+    
+    public DefaultLinks(){
+        links.put(Project.class, new HashMap<Class, RdfPredicate>());
+        links.get(Project.class).put(User.class, new RdfPredicate("irida", "hasUser"));
+        
+        links.put(User.class, new HashMap<Class, RdfPredicate>());
+        links.get(User.class).put(Project.class, new RdfPredicate("irida", "hasProject"));
+    }
+    
+    public HashMap<Class, HashMap<Class,RdfPredicate> > getLinks(){
+        return links;
+    }
+    
+    public <S extends IridaThing,O extends IridaThing> RdfPredicate getLink(Class subject,Class object){
+        RdfPredicate link = null;
+        if(links.containsKey(subject)){
+            HashMap<Class,RdfPredicate> preds = links.get(subject);
+            if(preds.containsKey(object)){
+                link = preds.get(object);
+            }
+            else{
+                throw new IllegalArgumentException("Link for subject doesn't exist");
+            }
+        }
+        else{
+            throw new IllegalArgumentException("Link for object doesn't exist");
+        }
+        
+        return link;
+    }
+}
