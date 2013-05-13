60383cb0560af4d4bb5a408cd8bdbe16d01b9506
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-05-13 14:32:33 -0500

    Modify the UserService to load users from the database on behalf of Spring Security. Also update the UserService so that it encodes passwords correctly when persisting users.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/UserServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/UserServiceImpl.java
@@ -19,25 +19,103 @@ import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.Project;
 import ca.corefacility.bioinformatics.irida.model.User;
 import ca.corefacility.bioinformatics.irida.model.roles.impl.UserIdentifier;
-import ca.corefacility.bioinformatics.irida.repositories.CRUDRepository;
 import ca.corefacility.bioinformatics.irida.repositories.UserRepository;
 import ca.corefacility.bioinformatics.irida.service.UserService;
-import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
+ * Implementation of the {@link UserService}.
  *
  * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
  */
 public class UserServiceImpl extends CRUDServiceImpl<UserIdentifier, User> implements UserService {
 
-    public UserServiceImpl(CRUDRepository<UserIdentifier, User> userRepository, Validator validator) {
+    /**
+     * A set containing only a reference to a "USER" role.
+     */
+    private static final Set<GrantedAuthority> USER_ONLY_SET = new HashSet<>();
+    /**
+     * The property name to use for passwords on the {@link User} class.
+     */
+    private static final String PASSWORD_PROPERTY = "password";
+    /**
+     * A reference to the user repository used to manage users.
+     */
+    private UserRepository userRepository;
+    /**
+     * A reference to the password encoder used by the system for storing passwords.
+     */
+    private PasswordEncoder passwordEncoder;
+
+    /**
+     * Constructor, requires a handle on a validator and a repository.
+     *
+     * @param userRepository the repository used to store instances of {@link User}.
+     * @param validator      the validator used to validate instances of {@link User}.
+     */
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, Validator validator) {
         super(userRepository, validator, User.class);
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    @PostConstruct
+    public void initializeRoles() {
+        USER_ONLY_SET.add(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User create(User u) {
+        Set<ConstraintViolation<User>> violations = validatePassword(u.getPassword());
+        if (violations.isEmpty()) {
+            String password = u.getPassword();
+            u.setPassword(passwordEncoder.encode(password));
+            return super.create(u);
+        }
+
+        throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public User update(UserIdentifier uid, Map<String, Object> properties) {
+        if (properties.containsKey(PASSWORD_PROPERTY)) {
+            String password = properties.get(PASSWORD_PROPERTY).toString();
+            Set<ConstraintViolation<User>> violations = validatePassword(password);
+            if (violations.isEmpty()) {
+                properties.put(PASSWORD_PROPERTY, passwordEncoder.encode(password));
+            } else {
+                throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
+            }
+        }
+
+        return super.update(uid, properties);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public User getUserByUsername(String username) throws EntityNotFoundException {
-        return userRepository().getUserByUsername(username);
+        return userRepository.getUserByUsername(username);
     }
 
     /**
@@ -45,10 +123,34 @@ public class UserServiceImpl extends CRUDServiceImpl<UserIdentifier, User> imple
      */
     @Override
     public Collection<User> getUsersForProject(Project project) {
-        return userRepository().getUsersForProject(project);
+        return userRepository.getUsersForProject(project);
     }
 
-    private UserRepository userRepository() {
-        return (UserRepository) repository;
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        org.springframework.security.core.userdetails.User userDetails = null;
+        User u;
+        try {
+            u = userRepository.getUserByUsername(username);
+
+            userDetails = new org.springframework.security.core.userdetails.User(u.getUsername(), u.getPassword(),
+                    USER_ONLY_SET);
+        } catch (EntityNotFoundException e) {
+            throw new UsernameNotFoundException(e.getMessage());
+        }
+        return userDetails;
+    }
+
+    /**
+     * Validate the password of a {@link User} *before* encoding the password and passing to super.
+     *
+     * @param password the password to validate.
+     * @return true if valid, false otherwise.
+     */
+    private Set<ConstraintViolation<User>> validatePassword(String password) {
+        return validator.validateValue(User.class, PASSWORD_PROPERTY, password);
     }
 }
