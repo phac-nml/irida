108a7b629fdca7d4f5a75ac4c19112645d40fed9
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-05-13 09:33:56 -0500

    Updated spelling mistake.  Passes JSHint.

--- src/main/webapp/resources/dev/js/directives/Notifier.js
+++ src/main/webapp/resources/dev/js/directives/Notifier.js
@@ -5,43 +5,42 @@
  */
 
 angular.module('irida')
-    .directive('notifier', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                messege: "@attrMessege",
-                icon: "@attrIcon"
-            },
-            replace: true,
-            controller: function ($scope, $attrs, $element, $timeout) {
+  .directive('notifier', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        message: '@attrMessege',
+        icon: '@attrIcon'
+      },
+      replace: true,
+      controller: function ($scope, $attrs, $element, $timeout) {
+        var time, timer;
+        $scope.hidden = true;
+        $scope.$on('notify', function () {
+          'use strict';
+          if (time < 5) {
+            $timeout.cancel(timer);
+          }
+          time = 5;
+          $scope.hidden = false;
+          countDown();
+        });
 
-                var time, timer;
-                $scope.hidden = true;
-                $scope.$on('notify', function () {
-                    "use strict";
-                    if(time < 5) {
-                        $timeout.cancel(timer);
-                    }
-                    time = 5;
-                    $scope.hidden = false;
-                    countDown();
-                });
-
-                function countDown() {
-                    "use strict";
-                    time -= 1;
-                    if (time > 0) {
-                        timer = $timeout(countDown, 1000);
-                    }
-                    else {
-                        $timeout.cancel(timer);
-                        $scope.hidden = true;
-                    }
-                }
-            },
-            template: '<div class="ng-cloak notifier" data-ng-hide="hidden" data-ng-animate="\'notifier\'"><i class="icon-{{icon}}"></i> <span>{{messege}}</span></div>',
-            link: function () {
-                "use strict";
-            }
-        };
-    });
+        function countDown() {
+          'use strict';
+          time -= 1;
+          if (time > 0) {
+            timer = $timeout(countDown, 1000);
+          }
+          else {
+            $timeout.cancel(timer);
+            $scope.hidden = true;
+          }
+        }
+      },
+      template: '<div class="ng-cloak notifier" data-ng-hide="hidden" data-ng-animate="\'notifier\'"><i class="icon-{{icon}}"></i> <span>{{message}}</span></div>',
+      link: function () {
+        'use strict';
+      }
+    };
+  });
