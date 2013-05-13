c1444828064c25a117724f3d58588f6fa5463779
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-05-13 11:05:57 -0500

    Added testing for landing controller.  Currently just tests to make sure controller exists in the irida context.

---
+++ src/test/js/unit/landing.spec.js
@@ -0,0 +1,28 @@
+/**
+ * User: josh
+ * Date: 2013-05-13
+ * Time: 11:02 AM
+ */
+describe('LandingCtrl', function () {
+  var LandingCtrl, $location, $scope, $httpBackend;
+
+  beforeEach(module('irida'));
+
+  beforeEach(inject(function ($controller, _$location_, $rootScope, $injector) {
+    $location = _$location_;
+    $scope = $rootScope.$new();
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', url).respond(response);
+    LandingCtrl = $controller('ProjectsListCtrl', { $scope: $scope });
+  }));
+
+  afterEach(function () {
+    'use strict';
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should have a valid controller', inject(function () {
+    expect(LandingCtrl).toBeTruthy();
+  }));
+});
