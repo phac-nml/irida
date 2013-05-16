51e47004bd064f110f421b805226142f498418f7
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-05-16 13:53:54 -0500

    Added new basic authentication entry point that *does not* set the WWW-Authenticate header.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/spring/authentication/BasicAuthenticationEntryPoint.java
@@ -0,0 +1,8 @@
+package ca.corefacility.bioinformatics.irida.web.spring.authentication;
+
+/**
+ * Created with IntelliJ IDEA. User: fbristow Date: 16/05/13 Time: 1:47 PM To change this template use File | Settings |
+ * File Templates.
+ */
+public class BasicAuthenticationEntryPoint {
+}
