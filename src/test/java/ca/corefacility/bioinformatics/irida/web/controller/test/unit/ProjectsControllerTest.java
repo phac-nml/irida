587ae0d2c92895209307f861f038d03e16fd41ea
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-05-17 09:44:13 -0500

    Remove login page. Add some tests.

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/ProjectsControllerTest.java
@@ -0,0 +1,8 @@
+package ca.corefacility.bioinformatics.irida.web.controller.test.unit;
+
+/**
+ * Created with IntelliJ IDEA. User: fbristow Date: 17/05/13 Time: 9:23 AM To change this template use File | Settings |
+ * File Templates.
+ */
+public class ProjectsControllerTest {
+}
