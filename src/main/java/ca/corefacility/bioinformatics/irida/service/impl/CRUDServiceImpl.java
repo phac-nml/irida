0ed284139083575cd89ab8f6124febe3987a04cf
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-05-21 14:01:31 -0500

    Check that properties to be changed are valid instead of validating the entire object on update.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/CRUDServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/CRUDServiceImpl.java
@@ -9,27 +9,23 @@ import ca.corefacility.bioinformatics.irida.model.roles.impl.Audit;
 import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
 import ca.corefacility.bioinformatics.irida.repositories.CRUDRepository;
 import ca.corefacility.bioinformatics.irida.service.CRUDService;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.Map.Entry;
 
 /**
- * A universal CRUD service for all types. Specialized services should extend
- * this class to get basic CRUD methods for free.
+ * A universal CRUD service for all types. Specialized services should extend this class to get basic CRUD methods for
+ * free.
  *
  * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
  */
-public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Comparable<ValueType> & Auditable<Audit>> implements CRUDService<KeyType, ValueType> {
+public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Comparable<ValueType> & Auditable<Audit>>
+        implements CRUDService<KeyType, ValueType> {
 
     protected CRUDRepository<KeyType, ValueType> repository;
     protected Validator validator;
@@ -47,9 +43,9 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
     @Override
     public ValueType create(ValueType object) throws ConstraintViolationException, EntityExistsException {
         // the audit information must be initialized by the crud service:
-        Audit audit = new Audit();        
+        Audit audit = new Audit();
         object.setAuditInformation(audit);
-        
+
         Set<ConstraintViolation<ValueType>> constraintViolations = validator.validate(object);
         if (constraintViolations.isEmpty()) {
             return repository.create(object);
@@ -99,7 +95,8 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
      * {@inheritDoc}
      */
     @Override
-    public List<ValueType> list(int page, int size, final String sortProperty, final Order order) throws IllegalArgumentException {
+    public List<ValueType> list(int page, int size, final String sortProperty, final Order order)
+            throws IllegalArgumentException {
         if (!methodAvailable(sortProperty, MethodType.GETTER)) {
             throw new IllegalArgumentException("No method available for property [" + sortProperty + "]");
         }
@@ -131,7 +128,8 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
      * {@inheritDoc}
      */
     @Override
-    public ValueType update(KeyType id, Map<String, Object> updatedFields) throws ConstraintViolationException, EntityExistsException, InvalidPropertyException {
+    public ValueType update(KeyType id, Map<String, Object> updatedFields)
+            throws ConstraintViolationException, EntityExistsException, InvalidPropertyException {
         // check if you can actually call all of the methods requested
         for (Entry<String, Object> field : updatedFields.entrySet()) {
             if (!methodAvailable(field.getKey(), MethodType.SETTER, field.getValue())) {
@@ -139,47 +137,59 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
             }
         }
 
+        // now that you know all of the requested methods exist, validate the supplied values
+        Set<ConstraintViolation<ValueType>> constraintViolations = new HashSet<>();
+
+        // try to validate all of the properties that are attempted to be updated.
+        for (String propertyName : updatedFields.keySet()) {
+            Set<ConstraintViolation<ValueType>> propertyViolations = validator.validateValue(valueType, propertyName,
+                    updatedFields.get(propertyName));
+            constraintViolations.addAll(propertyViolations);
+        }
+
+        // if any validations fail, throw a constraint violation exception.
+        if (!constraintViolations.isEmpty()) {
+            throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(constraintViolations));
+        }
+
         // check if the entity exists in the database
         if (!exists(id)) {
             throw new EntityNotFoundException("Entity not found.");
         }
 
-        // load the entity from the database
+        // at this point, everything is A-OK, so go through the act of updating the entity:
+
+        // 1) load the entity from the database
         ValueType entity = repository.read(id);
 
-        // update the appropriate fields
+        // 2) update the appropriate fields
         for (Entry<String, Object> field : updatedFields.entrySet()) {
             setValue(entity, field.getKey(), field.getValue());
         }
 
-        // set the date that the entity was updated on
+        // 3) set the date that the entity was updated on
         entity.getAuditInformation().setUpdated(new Date());
 
-        // check that it doesn't violate any constraints
-        Set<ConstraintViolation<ValueType>> constraintViolations = validator.validate(entity);
-        if (constraintViolations.isEmpty()) {
-            return repository.update(entity);
-        }
-
-        throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(constraintViolations));
+        // 4) respond to the client with the updated entity
+        return repository.update(entity);
     }
 
     /**
-     * Get the value of property from the instance of
-     * <code>ValueType</code>.
+     * Get the value of property from the instance of <code>ValueType</code>.
      *
-     * @param object the instance to get the property value from.
+     * @param object   the instance to get the property value from.
      * @param property the property name to get the value of.
      * @return the value of the property on the instance.
-     * @throws IllegalArgumentException if no public method exists on the object
-     * corresponding to the property.
+     * @throws IllegalArgumentException if no public method exists on the object corresponding to the property.
      */
-    private <PropertyType extends Comparable> PropertyType getValue(ValueType object, String property) throws IllegalArgumentException {
+    private <PropertyType extends Comparable> PropertyType getValue(ValueType object, String property)
+            throws IllegalArgumentException {
         try {
             Method m = getMethod(property, MethodType.GETTER);
             return (PropertyType) m.invoke(object);
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalArgumentException("No method exists on type [" + valueType + "] for property [" + property + "] (is the method public?)");
+            throw new IllegalArgumentException(
+                    "No method exists on type [" + valueType + "] for property [" + property + "] (is the method public?)");
         } catch (ClassCastException e) {
             throw new IllegalArgumentException("Property [" + property + "] is not comparable.");
         }
@@ -188,9 +198,9 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
     /**
      * Set the value of a property on the specific instance.
      *
-     * @param object the instance to change the value of.
+     * @param object   the instance to change the value of.
      * @param property the property name to change.
-     * @param value the new value of the property.
+     * @param value    the new value of the property.
      * @throws IllegalArgumentException if no appropriate setter can be found.
      */
     private void setValue(ValueType object, String property, Object value) throws IllegalArgumentException {
@@ -198,17 +208,18 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
             Method m = getMethod(property, MethodType.SETTER, value);
             m.invoke(object, value);
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalArgumentException("No method exists on type [" + valueType + "] for property [" + property + "] (is the method public?)");
+            throw new IllegalArgumentException(
+                    "No method exists on type [" + valueType + "] for property [" + property + "] (is the method public?)");
         }
     }
 
     /**
-     * Tests whether or not the supplied property actually applies to the type
-     * of object that we're persisting with this class.
+     * Tests whether or not the supplied property actually applies to the type of object that we're persisting with this
+     * class.
      *
      * @param property the name of the property to test
-     * @param type the type of method that you want to test for.
-     * @param params the list of actual values that you want to test for
+     * @param type     the type of method that you want to test for.
+     * @param params   the list of actual values that you want to test for
      * @return true if a getter exists for the property, false otherwise
      */
     private boolean methodAvailable(String property, MethodType type, Object... params) {
@@ -224,8 +235,8 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
      * Gets the method that we can invoke to get or set a property.
      *
      * @param property the name of the property to test
-     * @param type the type of method that you want to test for.
-     * @param params the list of actual values that you want to test for
+     * @param type     the type of method that you want to test for.
+     * @param params   the list of actual values that you want to test for
      * @return the method that we're looking for
      * @throws NoSuchMethodException if no such method exists.
      */
@@ -262,7 +273,9 @@ public class CRUDServiceImpl<KeyType extends Identifier, ValueType extends Compa
      */
     private String getMethodName(String property, MethodType type) {
         StringBuilder builder = new StringBuilder(type.toString());
+        // append the upper-case first character of the property name
         builder.append(property.substring(0, 1).toUpperCase());
+        // then append the remainder of the property name, as passed
         builder.append(property.substring(1));
         return builder.toString();
     }
