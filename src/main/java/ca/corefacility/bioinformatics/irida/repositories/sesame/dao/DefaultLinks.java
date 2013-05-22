8ecd1ac88e35748485d68c55e8172e905f382ad0
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-05-22 10:07:10 -0500

    Setting up some example links between project and sample.

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/DefaultLinks.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/DefaultLinks.java
@@ -20,16 +20,16 @@ import ca.corefacility.bioinformatics.irida.model.Sample;
 import ca.corefacility.bioinformatics.irida.model.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.User;
 import ca.corefacility.bioinformatics.irida.model.alibaba.IridaThing;
+
 import java.util.HashMap;
 
 /**
- *
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
  */
 public class DefaultLinks {
-    HashMap<Class, HashMap<Class,RdfPredicate> > links = new HashMap<>();
-    
-    public DefaultLinks(){
+    HashMap<Class, HashMap<Class, RdfPredicate>> links = new HashMap<>();
+
+    public DefaultLinks() {
         links.put(Project.class, new HashMap<Class, RdfPredicate>());
         links.get(Project.class).put(User.class, new RdfPredicate("irida", "hasUser"));
         links.get(Project.class).put(Sample.class, new RdfPredicate("irida", "hasSample"));
@@ -37,44 +37,46 @@ public class DefaultLinks {
 
         links.put(User.class, new HashMap<Class, RdfPredicate>());
         links.get(User.class).put(Project.class, new RdfPredicate("irida", "hasProject"));
+        links.put(Sample.class, new HashMap<Class, RdfPredicate>());
+        links.get(Sample.class).put(Project.class, new RdfPredicate("irida", "sampleHasProject"));
 
+        links.put(SequenceFile.class, new HashMap<Class, RdfPredicate>());
+        links.get(SequenceFile.class).put(Project.class, new RdfPredicate("irida", "sequenceFileHasProject"));
     }
-    
-    public HashMap<Class, HashMap<Class,RdfPredicate> > getLinks(){
+
+    public HashMap<Class, HashMap<Class, RdfPredicate>> getLinks() {
         return links;
     }
-    
-    public <S extends IridaThing,O extends IridaThing> RdfPredicate getLink(Class subject,Class object){
+
+    public <S extends IridaThing, O extends IridaThing> RdfPredicate getLink(Class subject, Class object) {
         RdfPredicate link = null;
-        if(links.containsKey(subject)){
-            HashMap<Class,RdfPredicate> preds = links.get(subject);
-            if(preds.containsKey(object)){
+        if (links.containsKey(subject)) {
+            HashMap<Class, RdfPredicate> preds = links.get(subject);
+            if (preds.containsKey(object)) {
                 link = preds.get(object);
-            }
-            else{
+            } else {
                 throw new IllegalArgumentException("Link for subject doesn't exist");
             }
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("Link for object doesn't exist");
         }
-        
+
         return link;
     }
-    
-    public <S extends IridaThing,O extends IridaThing> void addLink(Class subject,RdfPredicate pred,Class object){
-        HashMap<Class,RdfPredicate> preds;
-        
-        if(!links.containsKey(subject)){
+
+    public <S extends IridaThing, O extends IridaThing> void addLink(Class subject, RdfPredicate pred, Class object) {
+        HashMap<Class, RdfPredicate> preds;
+
+        if (!links.containsKey(subject)) {
             links.put(subject, new HashMap<Class, RdfPredicate>());
         }
-        
+
         preds = links.get(subject);
-        
-        if(preds.containsKey(object)){
-            throw new IllegalArgumentException("Default link already exists for " + subject.getName() + " and " + object.getName());
-        }
-        else{
+
+        if (preds.containsKey(object)) {
+            throw new IllegalArgumentException(
+                    "Default link already exists for " + subject.getName() + " and " + object.getName());
+        } else {
             preds.put(object, pred);
         }
     }
--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/ExampleDataLoader.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/ExampleDataLoader.java
@@ -16,11 +16,17 @@
 package ca.corefacility.bioinformatics.irida.repositories.sesame.dao;
 
 import ca.corefacility.bioinformatics.irida.model.Project;
+import ca.corefacility.bioinformatics.irida.model.Sample;
 import ca.corefacility.bioinformatics.irida.model.User;
+import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
+import ca.corefacility.bioinformatics.irida.repositories.CRUDRepository;
 import ca.corefacility.bioinformatics.irida.repositories.ProjectRepository;
 import ca.corefacility.bioinformatics.irida.repositories.RelationshipRepository;
 import ca.corefacility.bioinformatics.irida.repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Load example data into the database.
@@ -32,37 +38,64 @@ public class ExampleDataLoader {
     private UserRepository userRepo;
     private ProjectRepository projRepo;
     private RelationshipRepository relationshipRepo;
+    private CRUDRepository<Identifier, Sample> sampleRepository;
     private PasswordEncoder passwordEncoder;
 
     public ExampleDataLoader() {
     }
 
-    public ExampleDataLoader(UserRepository userRepo, ProjectRepository projRepo, RelationshipRepository relationshipRepo,PasswordEncoder passwordEncoder) {
+    public ExampleDataLoader(UserRepository userRepo, ProjectRepository projRepo, RelationshipRepository relationshipRepo, PasswordEncoder passwordEncoder, CRUDRepository<Identifier, Sample> sampleRepository) {
         this.userRepo = userRepo;
         this.projRepo = projRepo;
         this.relationshipRepo = relationshipRepo;
         this.passwordEncoder = passwordEncoder;
+        this.sampleRepository = sampleRepository;
     }
 
     public void addSampleData() {
-        User tom = userRepo.create(new User("tom", "tom@nowhere.com", passwordEncoder.encode("PASSWOD!1"), "Tom", "Matthews", "1234"));
-        User franklin = userRepo.create(new User("franklin", "franklin@nowhere.com", passwordEncoder.encode("PASSWOD!2"), "Franklin", "Bristow", "2345"));
-        User josh = userRepo.create(new User("joshX", "josh@nowhere.com", passwordEncoder.encode("PASSWOD!3"), "Josh", "Adam", "3456"));
-        userRepo.create(new User("matt", "matt@nowhere.com", passwordEncoder.encode("PASSWOD!4"), "Matthew", "Stuart-Edwards", "4567"));
-        userRepo.create(new User("aaron", "aaron@nowhere.com", passwordEncoder.encode("PASSWOD!5"), "Aaron", "Petkau", "5678"));
-        userRepo.create(new User("fbristow", "fbristow@gmail.com", passwordEncoder.encode("password1"), "Franklin", "Bristow", "7029"));
-        userRepo.create(new User("josh", "jsadam@gmail.com", passwordEncoder.encode("password1"), "Josh", "Adam", "5678"));
-        userRepo.create(new User("admin", "admin@admin.com", passwordEncoder.encode("password1"), "Admin", "Admin", "5678"));
+        User tom = userRepo.create(
+                new User("tom", "tom@nowhere.com", passwordEncoder.encode("PASSWOD!1"), "Tom", "Matthews", "1234"));
+        User franklin = userRepo.create(
+                new User("franklin", "franklin@nowhere.com", passwordEncoder.encode("PASSWOD!2"), "Franklin", "Bristow",
+                        "2345"));
+        User josh = userRepo.create(
+                new User("joshX", "josh@nowhere.com", passwordEncoder.encode("PASSWOD!3"), "Josh", "Adam", "3456"));
+        userRepo.create(
+                new User("matt", "matt@nowhere.com", passwordEncoder.encode("PASSWOD!4"), "Matthew", "Stuart-Edwards",
+                        "4567"));
+        userRepo.create(
+                new User("aaron", "aaron@nowhere.com", passwordEncoder.encode("PASSWOD!5"), "Aaron", "Petkau", "5678"));
+        userRepo.create(
+                new User("fbristow", "fbristow@gmail.com", passwordEncoder.encode("password1"), "Franklin", "Bristow",
+                        "7029"));
+        userRepo.create(
+                new User("josh", "jsadam@gmail.com", passwordEncoder.encode("password1"), "Josh", "Adam", "5678"));
+        userRepo.create(
+                new User("admin", "admin@admin.com", passwordEncoder.encode("password1"), "Admin", "Admin", "5678"));
 
+        List<Sample> samples = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Sample s = new Sample();
+            s.setSampleName("Some sample " + i);
+            s = sampleRepository.create(s);
+            samples.add(s);
+        }
 
         for (int i = 1; i <= 100; i++) {
             Project p = projRepo.create(new Project("Project " + i));
-            User u = userRepo.create(new User("user" + i, "user" + i + "@nowhere.com", "PASSWOD!" + i, "User", "Number" + i,
-                    i + "04-123-4567"));
+            User u = userRepo.create(
+                    new User("user" + i, "user" + i + "@nowhere.com", "PASSWOD!" + i, "User", "Number" + i,
+                            i + "04-123-4567"));
+            // add relationships to users
             relationshipRepo.create(tom, p);
             relationshipRepo.create(franklin, p);
             relationshipRepo.create(josh, p);
             relationshipRepo.create(u, p);
+
+            // add relationships to samples
+            for (int j = 0; j < samples.size(); j++) {
+                relationshipRepo.create(samples.get(j), p);
+            }
         }
 
     }
