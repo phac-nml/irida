d654e92fd17914fe891180fb208763354dee0d21
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-05-24 09:43:35 -0500

    Add tests for constructing related resources.

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/GenericControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/GenericControllerTest.java
@@ -18,7 +18,9 @@ package ca.corefacility.bioinformatics.irida.web.controller.test.unit;
 import ca.corefacility.bioinformatics.irida.exceptions.EntityExistsException;
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.exceptions.InvalidPropertyException;
+import ca.corefacility.bioinformatics.irida.model.Relationship;
 import ca.corefacility.bioinformatics.irida.model.enums.Order;
+import ca.corefacility.bioinformatics.irida.model.roles.impl.Audit;
 import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
 import ca.corefacility.bioinformatics.irida.service.CRUDService;
 import ca.corefacility.bioinformatics.irida.service.RelationshipService;
@@ -32,6 +34,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.net.HttpHeaders;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -55,8 +58,11 @@ import static org.mockito.Mockito.*;
 @SuppressWarnings("unused")
 public class GenericControllerTest {
 
+    private static final String RELATED_IDENTIFIABLE_TEST_ENTITY_KEY = "related";
     private GenericController<Identifier, IdentifiableTestEntity, IdentifiableTestResource> controller;
     private CRUDService<Identifier, IdentifiableTestEntity> crudService;
+    private RelationshipService relationshipService;
+    private EntityLinks entityLinks;
     private IdentifiableTestEntity entity;
     private Identifier id;
     private Map<String, Object> updatedFields;
@@ -65,7 +71,8 @@ public class GenericControllerTest {
     @SuppressWarnings("unchecked")
     public void setUp() {
         crudService = mock(CRUDService.class);
-        RelationshipService relationshipService = mock(RelationshipService.class);
+        relationshipService = mock(RelationshipService.class);
+        entityLinks = mock(EntityLinks.class);
         id = new Identifier();
         entity = new IdentifiableTestEntity();
         entity.setIdentifier(id);
@@ -78,10 +85,12 @@ public class GenericControllerTest {
 
             @Override
             protected Map<String, Class<?>> getUniquelyRelatedClasses() {
-                return (Map<String, Class<?>>) ImmutableMap.of("related", (Class<?>) IdentifiableTestEntity.class);
+                return (Map<String, Class<?>>) ImmutableMap.of(RELATED_IDENTIFIABLE_TEST_ENTITY_KEY,
+                        (Class<?>) IdentifiableTestEntity.class);
             }
         };
         controller.setRelationshipService(relationshipService);
+        controller.setEntityLinks(entityLinks);
         updatedFields = new HashMap<>();
 
         // fake out the servlet response so that the URI builder will work.
@@ -247,9 +256,40 @@ public class GenericControllerTest {
 
     @Test
     public void testGetRelatedResources() {
+        int numRelatedResources = 2;
+        String relatedResourceLabel = "related resource";
+        Date createdDate = new Date();
+        // get a handle on 4 identifiers
+        Identifier[] identifiers = new Identifier[]{new Identifier(), new Identifier(), new Identifier(), new Identifier()};
+        Identifier[] relationshipIdentifiers = new Identifier[]{new Identifier(), new Identifier()};
+
+        // prepare the related resources:
+        Set<Relationship> relationships = new HashSet<>();
+        Relationship r = new Relationship();
+        r.setIdentifier(relationshipIdentifiers[0]);
+        r.setSubject(identifiers[0]);
+        r.setObject(identifiers[1]);
+        identifiers[1].setLabel(relatedResourceLabel);
+        Audit audit = new Audit();
+        audit.setCreated(createdDate);
+        r.setAuditInformation(audit);
+        relationships.add(r);
+        r = new Relationship();
+        r.setIdentifier(relationshipIdentifiers[1]);
+        r.setSubject(identifiers[2]);
+        r.setObject(identifiers[3]);
+        identifiers[3].setLabel(relatedResourceLabel);
+        r.setAuditInformation(audit);
+        relationships.add(r);
+
         // basically, the only way to get related resources is to call "getResource" and supply the appropriate related
         // resource calls for the relationshipService.
         when(crudService.read(id)).thenReturn(entity);
+        when(relationshipService.getRelationshipsForEntity(id, IdentifiableTestEntity.class,
+                IdentifiableTestEntity.class)).thenReturn(relationships);
+        when(entityLinks.linkToSingleResource(any(Class.class), any())).thenReturn(
+                new Link("http://localhost/", PageLink.REL_SELF));
+
         ModelMap model = null;
 
         try {
@@ -261,10 +301,31 @@ public class GenericControllerTest {
         // get the collection of related resources
         assertTrue(model.containsKey(GenericController.RELATED_RESOURCES_NAME));
         @SuppressWarnings("unchecked")
-        Map<String, Collection<LabelledRelationshipResource>> relatedResources =
-                (Map<String, Collection<LabelledRelationshipResource>>) model
+        Map<String, ResourceCollection<LabelledRelationshipResource>> relatedResources =
+                (Map<String, ResourceCollection<LabelledRelationshipResource>>) model
                         .get(GenericController.RELATED_RESOURCES_NAME);
+        assertTrue(relatedResources.containsKey(RELATED_IDENTIFIABLE_TEST_ENTITY_KEY));
 
+        // we should have as many related resources as we expected to have
+        ResourceCollection<LabelledRelationshipResource> labels = relatedResources.get(
+                RELATED_IDENTIFIABLE_TEST_ENTITY_KEY);
+        assertEquals(numRelatedResources, labels.size());
+
+        // the related resources collection should also have a link that's used for generating new
+        // resources (in 'self')
+        assertTrue(labels.hasLink(PageLink.REL_SELF));
+
+        // each of the resources within the collection should have a self link and a delete link. The self link is used
+        // for getting the resource that is linked to, the delete link is used for terminating the relationship between
+        // the two resources.
+        for (LabelledRelationshipResource resource : labels) {
+            assertTrue(resource.hasLink(PageLink.REL_SELF));
+            assertTrue(resource.hasLink(GenericController.REL_DELETE_RELATIONSHIP));
+            // each resource should also have a label that's associated with it
+            assertEquals(relatedResourceLabel, resource.getLabel());
+            // and a creation date (i.e., related since)
+            assertEquals(createdDate, resource.getDateCreated());
+        }
     }
 
     @Test
