535fa0e26f7ed585d048f0064d207776d8b59f3c
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-06-03 11:48:49 -0500

    Merging in master and resolving conflicts

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/TripleStore.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/dao/TripleStore.java
@@ -38,10 +38,7 @@ public class TripleStore {
     public String URI;
     String serverUrl;
     String repoName;
-    /*
-     * To change this template, choose Tools | Templates
-     * and open the template in the editor.
-     */
+
     private Repository repo;
     private ObjectRepository objectRepo;
 
