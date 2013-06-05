cd081af4fdb1ed742f760d40ca5f99bfb30f436d
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-06-05 10:32:51 -0500

    Get sequence files related to a sample, test and implementation.

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/samples/SampleSequenceFilesController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/samples/SampleSequenceFilesController.java
@@ -1,10 +1,29 @@
 package ca.corefacility.bioinformatics.irida.web.controller.api.samples;
 
+import ca.corefacility.bioinformatics.irida.model.Relationship;
+import ca.corefacility.bioinformatics.irida.model.Sample;
+import ca.corefacility.bioinformatics.irida.model.SequenceFile;
+import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
+import ca.corefacility.bioinformatics.irida.service.RelationshipService;
+import ca.corefacility.bioinformatics.irida.service.SampleService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResourceCollection;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.sequencefile.SequenceFileResource;
+import ca.corefacility.bioinformatics.irida.web.controller.api.GenericController;
+import ca.corefacility.bioinformatics.irida.web.controller.api.SequenceFileController;
+import ca.corefacility.bioinformatics.irida.web.controller.api.projects.ProjectSamplesController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Controller for managing relationships between {@link Sample} and {@link SequenceFile}.
@@ -13,16 +32,100 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class SampleSequenceFilesController {
+    /**
+     * Rel to get back to the list of samples.
+     */
+    public static final String REL_SAMPLE = "sample";
+    /**
+     * Reference to the {@link SequenceFileService}.
+     */
+    private SequenceFileService sequenceFileService;
+    /**
+     * Reference to the {@link SampleService}.
+     */
+    private SampleService sampleService;
+    /**
+     * Reference to the {@link RelationshipService}.
+     */
+    private RelationshipService relationshipService;
+
+    protected SampleSequenceFilesController() {
+    }
+
+    @Autowired
+    public SampleSequenceFilesController(SequenceFileService sequenceFileService, SampleService sampleService,
+                                         RelationshipService relationshipService) {
+        this.sequenceFileService = sequenceFileService;
+        this.sampleService = sampleService;
+        this.relationshipService = relationshipService;
+    }
 
     /**
      * Get the {@link SequenceFile} entities associated with a specific {@link Sample}.
      *
-     * @param projectId the identifier for the {@link Project}.
-     * @param sampleId  the identifier for the {@link Sample}.
+     * @param sampleId the identifier for the {@link Sample}.
      * @return the {@link SequenceFile} entities associated with the {@link Sample}.
      */
     @RequestMapping(value = "/projects/{projectId}/samples/{sampleId}/sequenceFiles", method = RequestMethod.GET)
     public ModelMap getSampleSequenceFiles(@PathVariable String projectId, @PathVariable String sampleId) {
+        ModelMap modelMap = new ModelMap();
+        Identifier sampleIdentifier = new Identifier(sampleId);
+
+        // Use the RelationshipService to get the set of SequenceFile identifiers associated with a Sample, then
+        // retrieve each of the SequenceFiles and prepare for serialization.
+        Collection<Relationship> relationships = relationshipService.getRelationshipsForEntity(sampleIdentifier,
+                Sample.class, SequenceFile.class);
+        ResourceCollection<SequenceFileResource> resources = new ResourceCollection<>(relationships.size());
+        for (Relationship r : relationships) {
+            SequenceFile sf = sequenceFileService.read(r.getObject());
+            String sequenceFileId = sf.getIdentifier().getIdentifier();
+            SequenceFileResource sfr = new SequenceFileResource();
+            sfr.setResource(sf);
+            sfr.add(linkTo(methodOn(SequenceFileController.class).getResource(sequenceFileId))
+                    .withSelfRel());
+            sfr.add(linkTo(methodOn(SampleSequenceFilesController.class).getSequenceFileForSample(projectId, sampleId, sequenceFileId))
+                    .withRel(GenericController.REL_RELATIONSHIP));
+            resources.add(sfr);
+        }
+
+        // add a link to this collection
+        resources.add(linkTo(methodOn(SampleSequenceFilesController.class).getSampleSequenceFiles(projectId, sampleId))
+                .withSelfRel());
+        // add a link back to the sample
+        resources.add(linkTo(methodOn(ProjectSamplesController.class).getProjectSample(projectId, sampleId))
+                .withRel(SampleSequenceFilesController.REL_SAMPLE));
+
+        modelMap.addAttribute(GenericController.RESOURCE_NAME, resources);
+        return modelMap;
+    }
+
+    /**
+     * Remove a {@link SequenceFile} from a {@link Sample}. The {@link SequenceFile} will be moved to the
+     * {@link ca.corefacility.bioinformatics.irida.model.Project} that is related to this {@link Sample}.
+     *
+     * @param projectId      the destination {@link ca.corefacility.bioinformatics.irida.model.Project} identifier.
+     * @param sampleId       the source {@link Sample} identifier.
+     * @param sequenceFileId the identifier of the {@link SequenceFile} to move.
+     * @return a status indicating the success of the move.
+     */
+    @RequestMapping(value = "/projects/{projectId}/samples/{sampleId}/sequenceFiles/{sequenceFileId}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> removeSequenceFileFromSample(@PathVariable String projectId,
+                                                               @PathVariable String sampleId,
+                                                               @PathVariable String sequenceFileId) {
+        throw new UnsupportedOperationException("not implemented.");
+    }
+
+    /**
+     * Get a specific {@link SequenceFile} associated with a {@link Sample}.
+     *
+     * @param projectId      the identifier of the {@link ca.corefacility.bioinformatics.irida.model.Project}.
+     * @param sampleId       the identifier of the {@link Sample}.
+     * @param sequenceFileId the identifier of the {@link SequenceFile}.
+     * @return a representation of the {@link SequenceFile}.
+     */
+    @RequestMapping(value = "/projects/{projectId}/samples/{sampleId}/sequenceFiles/{sequenceFileId}", method = RequestMethod.GET)
+    public ModelMap getSequenceFileForSample(@PathVariable String projectId, @PathVariable String sampleId,
+                                             @PathVariable String sequenceFileId) {
         throw new UnsupportedOperationException("not implemented.");
     }
 }
--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/samples/SampleSequenceFilesControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/samples/SampleSequenceFilesControllerTest.java
@@ -1,11 +1,131 @@
 package ca.corefacility.bioinformatics.irida.web.controller.test.unit.samples;
 
+import ca.corefacility.bioinformatics.irida.model.Project;
+import ca.corefacility.bioinformatics.irida.model.Relationship;
+import ca.corefacility.bioinformatics.irida.model.Sample;
+import ca.corefacility.bioinformatics.irida.model.SequenceFile;
+import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
+import ca.corefacility.bioinformatics.irida.service.RelationshipService;
+import ca.corefacility.bioinformatics.irida.service.SampleService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResourceCollection;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.sequencefile.SequenceFileResource;
+import ca.corefacility.bioinformatics.irida.web.controller.api.GenericController;
+import ca.corefacility.bioinformatics.irida.web.controller.api.samples.SampleSequenceFilesController;
+import ca.corefacility.bioinformatics.irida.web.controller.links.PageLink;
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.ui.ModelMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 /**
- * Created with IntelliJ IDEA.
- * User: fbristow
- * Date: 05/06/13
- * Time: 8:54 AM
- * To change this template use File | Settings | File Templates.
+ * Unit tests for {@link SampleSequenceFilesController}.
  */
 public class SampleSequenceFilesControllerTest {
+    private SampleSequenceFilesController controller;
+    private SequenceFileService sequenceFileService;
+    private SampleService sampleService;
+    private RelationshipService relationshipService;
+
+    @Before
+    public void setUp() {
+        sampleService = mock(SampleService.class);
+        sequenceFileService = mock(SequenceFileService.class);
+        relationshipService = mock(RelationshipService.class);
+
+        controller = new SampleSequenceFilesController(sequenceFileService, sampleService, relationshipService);
+    }
+
+    @Test
+    public void testGetSampleSequenceFiles() throws IOException {
+        Project p = constructProject();
+        Sample s = constructSample();
+        SequenceFile sf = constructSequenceFile();
+        Relationship r = new Relationship();
+        r.setIdentifier(new Identifier());
+        r.setSubject(s.getIdentifier());
+        r.setObject(sf.getIdentifier());
+        Collection<Relationship> relationships = Sets.newHashSet(r);
+
+        // mock out the service calls
+        when(relationshipService.getRelationshipsForEntity(s.getIdentifier(), Sample.class, SequenceFile.class))
+                .thenReturn(relationships);
+        when(sequenceFileService.read(sf.getIdentifier())).thenReturn(sf);
+
+        ModelMap modelMap = controller.getSampleSequenceFiles(p.getIdentifier().getIdentifier(), s.getIdentifier().getIdentifier());
+
+        // verify that the service calls were used.
+        verify(relationshipService, times(1)).getRelationshipsForEntity(s.getIdentifier(), Sample.class,
+                SequenceFile.class);
+        verify(sequenceFileService, times(1)).read(sf.getIdentifier());
+
+        Object o = modelMap.get(GenericController.RESOURCE_NAME);
+        assertTrue(o instanceof ResourceCollection);
+        @SuppressWarnings("unchecked")
+        ResourceCollection<SequenceFileResource> resources = (ResourceCollection<SequenceFileResource>) o;
+        assertNotNull(resources);
+        assertEquals(1, resources.size());
+        assertNotNull(resources.getLink(PageLink.REL_SELF));
+        assertNotNull(resources.getLink(SampleSequenceFilesController.REL_SAMPLE));
+        SequenceFileResource sfr = resources.iterator().next();
+        assertNotNull(sfr.getLink(PageLink.REL_SELF));
+        assertNotNull(sfr.getLink(GenericController.REL_RELATIONSHIP));
+        assertEquals(sf.getFile().getName(), sfr.getFile().getName());
+    }
+
+    /**
+     * Construct a simple {@link ca.corefacility.bioinformatics.irida.model.Sample}.
+     *
+     * @return a sample with a name and identifier.
+     */
+    private Sample constructSample() {
+        String sampleId = UUID.randomUUID().toString();
+        Identifier sampleIdentifier = new Identifier();
+        sampleIdentifier.setIdentifier(sampleId);
+        String sampleName = "sampleName";
+        Sample s = new Sample();
+        s.setSampleName(sampleName);
+        s.setIdentifier(sampleIdentifier);
+        return s;
+    }
+
+    /**
+     * Construct a simple {@link SequenceFile}.
+     *
+     * @return a {@link SequenceFile} with identifier.
+     */
+    private SequenceFile constructSequenceFile() throws IOException {
+        String sequenceFileId = UUID.randomUUID().toString();
+        Identifier sequenceFileIdentifier = new Identifier();
+        File f = Files.createTempFile(null, null).toFile();
+        f.deleteOnExit();
+        sequenceFileIdentifier.setIdentifier(sequenceFileId);
+        SequenceFile sf = new SequenceFile();
+        sf.setIdentifier(sequenceFileIdentifier);
+        sf.setFile(f);
+        return sf;
+    }
+
+    /**
+     * Construct a simple {@link Project}.
+     *
+     * @return a project with a name and identifier.
+     */
+    private Project constructProject() {
+        String projectId = UUID.randomUUID().toString();
+        Identifier projectIdentifier = new Identifier();
+        projectIdentifier.setIdentifier(projectId);
+        Project p = new Project();
+        p.setIdentifier(projectIdentifier);
+        return p;
+    }
 }
