5d4cb5353a0a38ea52c05ab7a9817502de294001
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-06-05 10:40:09 -0500

    Added a QualifiedRDFPredicate that doesn't use the local name and prefix but just a URI.  This will be used in the case that we've got a predicate out from the database and we couldn't get the string prefix. Also made prefix and name private so that you need to call the getters to get them.  Added a method to construct a sesame URI for the predicate in both the parent & child so that we can safely construct a URI for this predicate.

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/RelationshipSesameRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sesame/RelationshipSesameRepository.java
@@ -25,6 +25,7 @@ import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
 import ca.corefacility.bioinformatics.irida.model.roles.impl.StringIdentifier;
 import ca.corefacility.bioinformatics.irida.repositories.RelationshipRepository;
 import ca.corefacility.bioinformatics.irida.repositories.sesame.dao.DefaultLinks;
+import ca.corefacility.bioinformatics.irida.repositories.sesame.dao.QualifiedRDFPredicate;
 import ca.corefacility.bioinformatics.irida.repositories.sesame.dao.RdfPredicate;
 import ca.corefacility.bioinformatics.irida.repositories.sesame.dao.TripleStore;
 import org.openrdf.model.Statement;
@@ -134,7 +135,7 @@ public class RelationshipSesameRepository extends SesameRepository implements Re
             con.begin();
             URI subURI = fac.createURI(subNetURI.toString());
             URI objURI = fac.createURI(objNetURI.toString());
-            URI pred = fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
+            URI pred = predicate.getPredicateURI(con);//fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
 
             Identifier identifier = generateNewIdentifier();
             link.setIdentifier(identifier);
@@ -226,7 +227,7 @@ public class RelationshipSesameRepository extends SesameRepository implements Re
             TupleQuery query = con.prepareTupleQuery(QueryLanguage.SPARQL, qs);
 
             URI subURI = fac.createURI(subNetURI.toString());
-            URI predURI = fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
+            URI predURI = predicate.getPredicateURI(con);//fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
             query.setBinding("subject", subURI);
             query.setBinding("predicate", predURI);
             TupleQueryResult results = query.evaluate();
@@ -267,7 +268,7 @@ public class RelationshipSesameRepository extends SesameRepository implements Re
             TupleQuery query = con.prepareTupleQuery(QueryLanguage.SPARQL, qs);
 
             URI objURI = fac.createURI(objNetUri.toString());
-            URI predURI = fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
+            URI predURI = predicate.getPredicateURI(con);//fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
             query.setBinding("object", objURI);
             query.setBinding("predicate", predURI);
             TupleQueryResult results = query.evaluate();
@@ -341,7 +342,7 @@ public class RelationshipSesameRepository extends SesameRepository implements Re
             }
 
             if (predicate != null) {
-                URI predURI = fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
+                URI predURI = predicate.getPredicateURI(con);//fac.createURI(con.getNamespace(predicate.prefix), predicate.name);
                 query.setBinding("pred", predURI);
             }
 
@@ -394,7 +395,7 @@ public class RelationshipSesameRepository extends SesameRepository implements Re
         String predstr = bs.getValue("pred").stringValue();
         URI predURI = fac.createURI(predstr);
 
-        RdfPredicate pred = new RdfPredicate(predURI.getNamespace(), predURI.getLocalName());
+        RdfPredicate pred = new QualifiedRDFPredicate(predstr);//new RdfPredicate(predURI.getNamespace(), predURI.getLocalName());
 
         Identifier subId = getIdentiferForURI(subURI);
         Identifier objId = getIdentiferForURI(objURI);
@@ -470,8 +471,21 @@ public class RelationshipSesameRepository extends SesameRepository implements Re
         URI objecturi = vf.createURI(uri);
 
         try {
+            con.begin();
+            //first we'll remove the predicate between the subject and object.
+            Relationship relationship = read(id);
+            java.net.URI subNetURI = getUriFromIdentifier(relationship.getSubject());
+            java.net.URI objNetURI = getUriFromIdentifier(relationship.getObject());
+            RdfPredicate predicate = relationship.getPredicate();
+            URI subURI = vf.createURI(subNetURI.toString());
+            URI objURI = vf.createURI(objNetURI.toString());
+
+            URI pred = predicate.getPredicateURI(con);//vf.createURI(con.getNamespace(predicate.prefix), predicate.name);
+            con.remove(subURI,pred,objURI);
+            
+            //then we'll remove the relationship object
             con.remove(objecturi, null, null);
-            con.close();
+            con.commit();
 
         } catch (RepositoryException ex) {
             logger.error(ex.getMessage());
