4207ac6ce53cd47322c03795509e07716c1f3920
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-06-06 11:39:54 -0500

    Moved tests for links into the unit test package.

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/links/PageableControllerLinkBuilderTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/links/PageableControllerLinkBuilderTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ca.corefacility.bioinformatics.irida.web.controller.link.test;
+package ca.corefacility.bioinformatics.irida.web.controller.test.unit.links;
 
 import ca.corefacility.bioinformatics.irida.model.enums.Order;
 import ca.corefacility.bioinformatics.irida.web.controller.links.PageLink;
