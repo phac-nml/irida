738cdc389764984e3b8eba3c4f7a6299b9158cb3
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-06-06 12:25:47 -0500

    Add rest-assured as a dependency, enable integration testing.

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/ProjectIntegrationTest.java
@@ -0,0 +1,11 @@
+package ca.corefacility.bioinformatics.irida.web.controller.test.integration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fbristow
+ * Date: 06/06/13
+ * Time: 11:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProjectIntegrationTest {
+}
