1ad4d25a5ef893aed35b5a2d580f94300ae84c52
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-06-07 13:16:12 -0500

    When adding a sample to the project, if the sample has not previously been persisted, then you should persist the sample.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/ProjectServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/ProjectServiceImpl.java
@@ -6,12 +6,19 @@ package ca.corefacility.bioinformatics.irida.service.impl;
 
 import ca.corefacility.bioinformatics.irida.model.*;
 import ca.corefacility.bioinformatics.irida.model.roles.impl.Identifier;
+import ca.corefacility.bioinformatics.irida.repositories.CRUDRepository;
 import ca.corefacility.bioinformatics.irida.repositories.ProjectRepository;
 import ca.corefacility.bioinformatics.irida.repositories.RelationshipRepository;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A specialized service layer for projects.
@@ -20,15 +27,18 @@ import java.util.Collection;
  */
 public class ProjectServiceImpl extends CRUDServiceImpl<Identifier, Project> implements ProjectService {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(ProjectServiceImpl.class);
     private RelationshipRepository relationshipRepository;
     private ProjectRepository projectRepository;
-    
+    private CRUDRepository<Identifier, Sample> sampleRepository;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, RelationshipRepository relationshipRepository, Validator validator) {
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, RelationshipRepository relationshipRepository,
+                              CRUDRepository<Identifier, Sample> sampleRepository, Validator validator) {
         super(projectRepository, validator, Project.class);
         this.projectRepository = projectRepository;
         this.relationshipRepository = relationshipRepository;
+        this.sampleRepository = sampleRepository;
     }
 
     /**
@@ -44,6 +54,18 @@ public class ProjectServiceImpl extends CRUDServiceImpl<Identifier, Project> imp
      */
     @Override
     public Relationship addSampleToProject(Project project, Sample sample) {
+        logger.trace("Adding sample to project.");
+        // the sample hasn't been persisted before, persist it before calling the relationshipRepository.
+        if (sample.getIdentifier() == null) {
+            logger.trace("Going to validate and persist sample prior to creating relationship.");
+            // validate the sample, then persist it:
+            Set<ConstraintViolation<Sample>> constraintViolations = validator.validate(sample);
+            if (constraintViolations.isEmpty()) {
+                sample = sampleRepository.create(sample);
+            } else {
+                throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(constraintViolations));
+            }
+        }
         return relationshipRepository.create(project, sample);
     }
 
