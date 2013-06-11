36e13b234eb1c7411ace2460b790df1325c4a71d
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-06-11 12:16:44 -0500

    Test for removing sequence file from a sample, move to project.

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/sample/SampleSequenceFilesIntegrationTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/sample/SampleSequenceFilesIntegrationTest.java
@@ -15,8 +15,7 @@ import java.util.Map;
 import static com.jayway.restassured.RestAssured.expect;
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.path.json.JsonPath.from;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Integration tests for working with sequence files and samples.
@@ -93,5 +92,40 @@ public class SampleSequenceFilesIntegrationTest {
         assertNotNull(link);
         assertTrue(link.matches("<http://localhost:8080/api/projects/[0-9a-f\\-]+/samples/[a-f0-9\\-]+" +
                 "/sequenceFiles/[a-f0-9\\-]+>; rel=relationship"));
+    }
+
+    @Test
+    public void testRemoveSequenceFileFromSample() throws IOException {
+        // for now, add a sequence file to the sample so that we can remove it
+        String projectUri = "http://localhost:8080/api/projects/c4893f30-b054-46e5-8ebe-ed1b295dfa38";
+        String sampleUri = projectUri + "/samples/07ac0624-8f04-43ba-b45f-e6d65a8bd6ba";
+        Response response = expect().statusCode(HttpStatus.OK.value()).when().get(sampleUri);
+        String sampleBody = response.getBody().asString();
+        String sequenceFileUri = from(sampleBody).getString("resource.links.find{it.rel == 'sample/sequenceFiles'}.href");
+        // prepare a file for sending to the server
+        Path sequenceFile = Files.createTempFile(null, null);
+        Files.write(sequenceFile, ">test read\nACGTACTCATG".getBytes());
+
+        Response r = given().contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .multiPart("file", sequenceFile.toFile()).expect().statusCode(HttpStatus.CREATED.value())
+                .when().post(sequenceFileUri);
+
+        String location = r.getHeader(HttpHeaders.LOCATION);
+        String identifier = location.substring(location.lastIndexOf('/') + 1);
+
+        // get the link, this what we want to remove from the sample
+        String link = r.getHeader(HttpHeaders.LINK);
+        String sequenceFileRelationship = link.substring(1, link.indexOf('>'));
+
+        r = expect().statusCode(HttpStatus.OK.value()).when().delete(sequenceFileRelationship);
+        String responseBody = r.getBody().asString();
+        String sampleLocation = from(responseBody).getString("resource.links.find{it.rel == 'sample'}.href");
+        String sequenceFileLocation = from(responseBody).getString("resource.links.find{it.rel == 'project/sequenceFile'}.href");
+
+        assertNotNull(sampleLocation);
+        assertEquals(sampleUri, sampleLocation);
+
+        assertNotNull(sequenceFileLocation);
+        assertEquals(projectUri + "/sequenceFiles/" + identifier, sequenceFileLocation);
     }
 }
