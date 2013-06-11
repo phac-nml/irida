b3b9777696be68fea387f2bc2a74933244d8d140
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-06-11 11:45:08 -0500

    Add existing sequence file to sample.

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/sample/SampleSequenceFilesIntegrationTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/sample/SampleSequenceFilesIntegrationTest.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.jayway.restassured.RestAssured.expect;
 import static com.jayway.restassured.RestAssured.given;
@@ -51,5 +53,45 @@ public class SampleSequenceFilesIntegrationTest {
 
         // clean up
         Files.delete(sequenceFile);
+    }
+
+    @Test
+    public void testAddExistingSequenceFileToSample() throws IOException {
+        // for now, add a sequence file to another sample
+        String projectUri = "http://localhost:8080/api/projects/c4893f30-b054-46e5-8ebe-ed1b295dfa38";
+        String sampleUri = projectUri + "/samples/07ac0624-8f04-43ba-b45f-e6d65a8bd6ba";
+        Response response = expect().statusCode(HttpStatus.OK.value()).when().get(projectUri);
+        String projectBody = response.getBody().asString();
+        String sequenceFileUri = from(projectBody).getString("resource.links.find{it.rel == 'project/sequenceFiles'}.href");
+        // prepare a file for sending to the server
+        Path sequenceFile = Files.createTempFile(null, null);
+        Files.write(sequenceFile, ">test read\nACGTACTCATG".getBytes());
+
+        Response r = given().contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .multiPart("file", sequenceFile.toFile()).expect().statusCode(HttpStatus.CREATED.value())
+                .when().post(sequenceFileUri);
+
+        // figure out what the identifier for that sequence file is
+        String location = r.getHeader(HttpHeaders.LOCATION);
+        String identifier = location.substring(location.lastIndexOf('/') + 1);
+        Map<String, String> existingSequenceFile = new HashMap<>();
+        existingSequenceFile.put("sequenceFileId", identifier);
+
+        // now figure out where to post the sequence file to add it to the sample
+        String sampleBody = expect().statusCode(HttpStatus.OK.value()).when().get(sampleUri).getBody().asString();
+        sequenceFileUri = from(sampleBody).getString("resource.links.find{it.rel == 'sample/sequenceFiles'}.href");
+
+        // add the sequence file to the sample
+        r = given().body(existingSequenceFile).expect().statusCode(HttpStatus.CREATED.value()).when()
+                .post(sequenceFileUri);
+        location = r.getHeader(HttpHeaders.LOCATION);
+        String link = r.getHeader(HttpHeaders.LINK);
+
+        assertNotNull(location);
+        assertTrue(location.matches("http://localhost:8080/api/sequenceFiles/[a-f0-9\\-]+"));
+
+        assertNotNull(link);
+        assertTrue(link.matches("<http://localhost:8080/api/projects/[0-9a-f\\-]+/samples/[a-f0-9\\-]+" +
+                "/sequenceFiles/[a-f0-9\\-]+>; rel=relationship"));
     }
 }
