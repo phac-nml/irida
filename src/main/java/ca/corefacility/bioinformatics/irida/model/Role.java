bdeab6ad79589064efed0f342a09c09925111068
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-06-24 12:06:56 -0500

    Added some commenting for the Role.valueOf

--- src/main/java/ca/corefacility/bioinformatics/irida/model/Role.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/Role.java
@@ -68,6 +68,11 @@ public class Role implements Comparable<Role>, GrantedAuthority {
         return name;
     }
     
+    /**
+     * Return a {@link Role} for the given string value
+     * @param value The string value to create a {@link Role} for
+     * @return A new {@link Role} instance for the given string value
+     */
     public static Role valueOf(String value){
         return new Role(value);
     }
