014e9f93e6195e5dce9743604dc3a670db8812ee
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-07-09 14:44:20 -0500

    Styles on project samples page. Have downloads working without opening a new page.

--- src/main/webapp/resources/js/controllers/project.js
+++ src/main/webapp/resources/js/controllers/project.js
@@ -6,105 +6,136 @@
  * License: MIT
  */
 (function (ng, app) {
-  'use strict';
-  app.controller('ProjectCtrl', [ '$scope', '$rootScope', '$window', 'ajaxService', '$location', 'projectService',
-    function ($scope, $rootScope, $window, ajaxService, $location, projectService) {
-        projectService.project = $scope.project;
+    'use strict';
+    app.controller('ProjectCtrl', [ '$scope', '$rootScope', '$window', 'ajaxService', '$location', 'projectService',
+        function ($scope, $rootScope, $window, ajaxService, $location, projectService) {
+            projectService.project = $scope.project;
 
-      $scope.data.view = 'samples';
-      $scope.data.users = ['Josh Adam', 'Frankilin Bristow', 'Aaron Petkau', 'Heather Adam', 'Nathan Adam'];
+            $scope.data.view = 'samples';
+            $scope.data.users = ['Josh Adam', 'Frankilin Bristow', 'Aaron Petkau', 'Heather Adam', 'Nathan Adam'];
 
 
-      $scope.addFilesToSample = function (s) {
-        var fileIndexes = ng.element('input[name="files"]:checked');
-        // Get sample information
-        if (fileIndexes.length) {
+            $scope.addFilesToSample = function (s) {
+                var fileIndexes = ng.element('input[name="files"]:checked');
+                // Get sample information
+                if (fileIndexes.length) {
 
-          if (typeof s.data === 'undefined') {
-            ajaxService.get(s.links.self).then(function (data) {
-              s.data = data;
-              addSequenceFileToSample(s, fileIndexes);
-            });
-          }
-          else {
-            addSequenceFileToSample(s, fileIndexes);
-          }
-        }
-      };
+                    if (typeof s.data === 'undefined') {
+                        ajaxService.get(s.links.self).then(function (data) {
+                            s.data = data;
+                            addSequenceFileToSample(s, fileIndexes);
+                        });
+                    }
+                    else {
+                        addSequenceFileToSample(s, fileIndexes);
+                    }
+                }
+            };
 
-      function addSequenceFileToSample(sample, fileIndexes) {
-        var link = sample.data.resource.links['sample/sequenceFiles'];
-        ng.forEach(fileIndexes, function (value) {
-          var index = $(value).val();
-          ajaxService.create(link, {
-            'sequenceFileId': $scope.project.sequenceFiles[index].identifier
-          }).then(function () {
-              // Remove from sequenceFile list
-              $scope.project.sequenceFiles.splice(index, 1);
-            });
-        });
-        var f = fileIndexes.length > 1 ? "s" : '';
-        $rootScope.$broadcast('NOTIFY', {
-          'msg': fileIndexes.length + ' file' + f + ' added to ' + sample.label
-        });
-      }
-
-      /**
-       * Delete the currently viewed project
-       */
-      $scope.deleteProject = function () {
-        ajaxService.deleteItem($scope.project.links.self).then(function () {
-          $rootScope.$broadcast('NOTIFY', {
-            'msg': 'Deleted ' + $scope.project.name,
-            'callback': function () {
-              // TODO: Once undo is set up on the server.
+            function addSequenceFileToSample(sample, fileIndexes) {
+                var link = sample.data.resource.links['sample/sequenceFiles'];
+                ng.forEach(fileIndexes, function (value) {
+                    var index = $(value).val();
+                    ajaxService.create(link, {
+                        'sequenceFileId': $scope.project.sequenceFiles[index].identifier
+                    }).then(function () {
+                            // Remove from sequenceFile list
+                            $scope.project.sequenceFiles.splice(index, 1);
+                        });
+                });
+                var f = fileIndexes.length > 1 ? "s" : '';
+                $rootScope.$broadcast('NOTIFY', {
+                    'msg': fileIndexes.length + ' file' + f + ' added to ' + sample.label
+                });
             }
-          });
 
-          $location.path('/landing');
-        });
-      };
+            /**
+             * Delete the currently viewed project
+             */
+            $scope.deleteProject = function () {
+                ajaxService.deleteItem($scope.project.links.self).then(function () {
+                    $rootScope.$broadcast('NOTIFY', {
+                        'msg': 'Deleted ' + $scope.project.name,
+                        'callback': function () {
+                            // TODO: Once undo is set up on the server.
+                        }
+                    });
 
-      $scope.downloadFile = function (e, fileObject, type) {
-        e.preventDefault();
-        $window.open(fileObject.links[type], '_blank');
-      };
+                    $location.path('/landing');
+                });
+            };
 
-      $scope.checkForAllSelected = function (type) {
-        var t = ng.element("input[name='" + type +"']").length;
-        var c = ng.element("input[name='" + type +"']:checked").length;
+            $scope.downloadFile = function (e, fileObject, type) {
+                e.preventDefault();
+//                $window.open(fileObject.links[type]);
+                var sUrl = fileObject.links[type];
 
-        $scope.display[type].checkedCount = c;
-        if(t === c){
-          $scope.display[type].mainCB = true;
-        }
-        else {
-          $scope.display[type].mainCB = false;}
-      };
+                var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+                var isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
 
-      $scope.display = {
-        files: {
-          allCheckboxes: false,
-          mainCB: false,
-          checkedCount: 0
-        },
-        users: {
-          allCheckboxes: false,
-          mainCB: false,
-          checkedCount: 0
-        }
-      };
+                if (isChrome || isSafari) {
+                    //Creating new link node.
+                    var link = document.createElement('a');
+                    link.href = sUrl;
 
-        $scope.checkCount = function (type) {
-            return ng.element("input[name='" + type + "']:checked").length;
-        };
+                    if (link.download !== undefined){
+                        //Set HTML5 download attribute. This will prevent file from opening if supported.
+                        var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
+                        link.download = fileName;
+                    }
 
-        $scope.selectAllCheckboxes = function (type, value) {
-            ng.forEach(ng.element("input[name='" + type + "']"), function(el) {
-                $(el).prop("checked", value);
-            });
-            $scope.display[type].checkedCount = ng.element("input[name='" + type + "']:checked").length;
-        };
+                    //Dispatching click event.
+                    if (document.createEvent) {
+                        var e = document.createEvent('MouseEvents');
+                        e.initEvent('click' ,true ,true);
+                        link.dispatchEvent(e);
+                        return true;
+                    }
+                }
+
+                // Force file download (whether supported by server).
+                var query = '?download';
+
+                window.open(sUrl + query);
+
+            };
+
+            $scope.checkForAllSelected = function (type) {
+                var t = ng.element("input[name='" + type + "']").length;
+                var c = ng.element("input[name='" + type + "']:checked").length;
+
+                $scope.display[type].checkedCount = c;
+                if (t === c) {
+                    $scope.display[type].mainCB = true;
+                }
+                else {
+                    $scope.display[type].mainCB = false;
+                }
+            };
+
+            $scope.display = {
+                files: {
+                    allCheckboxes: false,
+                    mainCB: false,
+                    checkedCount: 0
+                },
+                users: {
+                    allCheckboxes: false,
+                    mainCB: false,
+                    checkedCount: 0
+                }
+            };
+
+            $scope.checkCount = function (type) {
+                return ng.element("input[name='" + type + "']:checked").length;
+            };
+
+            $scope.selectAllCheckboxes = function (type, value) {
+                ng.forEach(ng.element("input[name='" + type + "']"), function (el) {
+                    $(el).prop("checked", value);
+                });
+                $scope.display[type].checkedCount = ng.element("input[name='" + type + "']:checked").length;
+            };
 
 //      $scope.modifyDisplayOptions = function (type) {
 //        $scope.display[type].allCheckboxes = !$scope.display[type].allCheckboxes;
@@ -118,34 +149,34 @@
 //        }
 //      };
 
-        $scope.changeProjectView = function(view) {
-            $location.path('/projects/' + $scope.project.id + '/' + view);
-        };
+            $scope.changeProjectView = function (view) {
+                $location.path('/projects/' + $scope.project.id + '/' + view);
+            };
 
-      $scope.removeItemFromProject = function(type) {
-        var l = ng.element("input[name='" + type +"']:checked");
-        ng.forEach(l, function(item) {
-          var index = $(item).val();
-          ajaxService.deleteItem($scope.project.users[index].links.relationship).then(function () {
-            $scope.project.users.splice(index, 1);
-            $rootScope.$broadcast('NOTIFY', {
-              'msg': 'Deleted ' + type
-            });
-          });
-        });
-      };
+            $scope.removeItemFromProject = function (type) {
+                var l = ng.element("input[name='" + type + "']:checked");
+                ng.forEach(l, function (item) {
+                    var index = $(item).val();
+                    ajaxService.deleteItem($scope.project.users[index].links.relationship).then(function () {
+                        $scope.project.users.splice(index, 1);
+                        $rootScope.$broadcast('NOTIFY', {
+                            'msg': 'Deleted ' + type
+                        });
+                    });
+                });
+            };
 
-      $scope.gotoUser = function (e, user) {
-        e.preventDefault();
-        var u = user.match(/\/users\/.*/);
-        $location.path(u[0]);
-      };
+            $scope.gotoUser = function (e, user) {
+                e.preventDefault();
+                var u = user.match(/\/users\/.*/);
+                $location.path(u[0]);
+            };
 
-      $scope.gotoProject = function (e, url) {
-        e.preventDefault();
-        var l = url.match(/\/projects\/.*/);
-        $location.path(l[0]);
-      };
-    }
-  ]);
+            $scope.gotoProject = function (e, url) {
+                e.preventDefault();
+                var l = url.match(/\/projects\/.*/);
+                $location.path(l[0]);
+            };
+        }
+    ]);
 })(angular, NGS);
--- src/main/webapp/resources/js/services/ajax-service.js
+++ src/main/webapp/resources/js/services/ajax-service.js
@@ -5,164 +5,178 @@
  * License: MIT
  */
 (function (ng, app) {
-  'use strict';
-  app.factory('ajaxService', ['$http', '$q', function ($http, $q) {
+    'use strict';
+    app.factory('ajaxService', ['$http', '$q', function ($http, $q) {
 
-    function formatObjectLinks(obj) {
-      for (var key in obj) {
-        if (key === 'links') {
-          obj[key] = linkFormatter(obj[key]);
-        }
-        else if (typeof obj[key] === 'object') {
-          formatObjectLinks(obj[key]);
-        }
-      }
-    }
-
-    function linkFormatter(links) {
-      var l = {};
-      for (var i = 0; i < links.length; i++) {
-        l[links[i].rel] = links[i].href;
-      }
-      return l;
-    }
-
-    return {
-      /**
-       * create - use to create a new resource on the server
-       * @param url
-       * @param data
-       * @returns callback promise for success and failure.
-       */
-      create: function (url, data) {
-        var deferred = $q.defer();
-        $http({
-          method: 'POST',
-          url: url,
-          data: data,
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-          .success(function (data, status, headers, config) {
-            deferred.resolve(headers('location'));
-          })
-          .error(function (data) {
-            // TODO: (JOSH - 2013-05-10) Handle create errors
-            deferred.reject(data);
-          });
-        return deferred.promise;
-      },
-      /**
-       * get- use to get a resource from the server
-       * @param url
-       * @returns {*}
-       */
-      get: function (url) {
-        if (url) {
-          var deferred = $q.defer();
-          $http({
-            url: url,
-            method: 'GET'
-          })
-            .success(function (data) {
-              formatObjectLinks(data);
-              deferred.resolve(data);
-            })
-            .error(function () {
-              // TODO: (JOSH - 2013-05-10) Handle get errors properly
-              deferred.reject('An error occurred during get @ ' + url);
-            });
-
-          return deferred.promise;
-        }
-        return false;
-      },
-      /**
-       * post
-       * @param url
-       * @param data
-       * @returns {*}
-       */
-      post: function (url, data) {
-        if (url) {
-          var deferred = $q.defer();
-          $http({
-            url: url,
-            method: 'POST',
-            params: data
-          })
-            .success(function (data) {
-              deferred.resolve(data);
-            })
-            .error(function () {
-              // TODO: (JOSH - 2013-05-10) Handle post errors properly
-              deferred.reject('An error occurred while posting @ ' + url);
-            });
-
-          return deferred.promise;
-        }
-        return false;
-      },
-      /**
-       * patch - uses to update a resource (usually one field)
-       * @param url
-       * @param data
-       * @returns {*}
-       */
-      patch: function (url, data) {
-        if (url && data) {
-          var deferred = $q.defer();
-
-          $http({
-            method: 'PATCH',
-            url: url,
-            data: data,
-            headers: {
-              'Content-Type': 'application/json'
+        function formatObjectLinks(obj) {
+            for (var key in obj) {
+                if (key === 'links') {
+                    obj[key] = linkFormatter(obj[key]);
+                }
+                else if (typeof obj[key] === 'object') {
+                    formatObjectLinks(obj[key]);
+                }
             }
-          })
-            .success(function (data) {
-              deferred.resolve(data);
-            })
-            .error(function (data) {
-              deferred.reject(data);
-            });
-          return deferred.promise;
         }
-        return false;
-      },
-      /**
-       * delete - delete resource from server
-       * @param url
-       * @returns {*}
-       */
-      deleteItem: function (url) {
-        if (url) {
-          var deferred = $q.defer();
 
-          $http({
-            method: 'DELETE',
-            url: url,
-            headers: {
-              'Content-Type': 'application/json'
+        function linkFormatter(links) {
+            var l = {};
+            for (var i = 0; i < links.length; i++) {
+                l[links[i].rel] = links[i].href;
             }
-          })
-            .success(function (data) {
-              formatObjectLinks(data);
-              deferred.resolve(data);
-            })
-            .error(function (data, status, headers, config) {
-              console.log(data);
-              console.log(status);
-              console.log(headers);
-              console.log(config);
-              deferred.reject(status);
-            });
-          return deferred.promise;
+            return l;
         }
-        return false;
-      }
-    };
-  }]);
+
+        return {
+            /**
+             * create - use to create a new resource on the server
+             * @param url
+             * @param data
+             * @returns callback promise for success and failure.
+             */
+            create: function (url, data) {
+                var deferred = $q.defer();
+                $http({
+                    method: 'POST',
+                    url: url,
+                    data: data,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .success(function (data, status, headers, config) {
+                        deferred.resolve(headers('location'));
+                    })
+                    .error(function (data) {
+                        // TODO: (JOSH - 2013-05-10) Handle create errors
+                        deferred.reject(data);
+                    });
+                return deferred.promise;
+            },
+            /**
+             * get- use to get a resource from the server
+             * @param url
+             * @returns {*}
+             */
+            get: function (url) {
+                if (url) {
+                    var deferred = $q.defer();
+                    $http({
+                        url: url,
+                        method: 'GET'
+                    })
+                        .success(function (data) {
+                            formatObjectLinks(data);
+                            deferred.resolve(data);
+                        })
+                        .error(function () {
+                            // TODO: (JOSH - 2013-05-10) Handle get errors properly
+                            deferred.reject('An error occurred during get @ ' + url);
+                        });
+
+                    return deferred.promise;
+                }
+                return false;
+            },
+            /**
+             * post
+             * @param url
+             * @param data
+             * @returns {*}
+             */
+            post: function (url, data) {
+                if (url) {
+                    var deferred = $q.defer();
+                    $http({
+                        url: url,
+                        method: 'POST',
+                        params: data
+                    })
+                        .success(function (data) {
+                            deferred.resolve(data);
+                        })
+                        .error(function () {
+                            // TODO: (JOSH - 2013-05-10) Handle post errors properly
+                            deferred.reject('An error occurred while posting @ ' + url);
+                        });
+
+                    return deferred.promise;
+                }
+                return false;
+            },
+            /**
+             * patch - uses to update a resource (usually one field)
+             * @param url
+             * @param data
+             * @returns {*}
+             */
+            patch: function (url, data) {
+                if (url && data) {
+                    var deferred = $q.defer();
+
+                    $http({
+                        method: 'PATCH',
+                        url: url,
+                        data: data,
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .success(function (data) {
+                            deferred.resolve(data);
+                        })
+                        .error(function (data) {
+                            deferred.reject(data);
+                        });
+                    return deferred.promise;
+                }
+                return false;
+            },
+            /**
+             * delete - delete resource from server
+             * @param url
+             * @returns {*}
+             */
+            deleteItem: function (url) {
+                if (url) {
+                    var deferred = $q.defer();
+
+                    $http({
+                        method: 'DELETE',
+                        url: url,
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .success(function (data) {
+                            formatObjectLinks(data);
+                            deferred.resolve(data);
+                        })
+                        .error(function (data, status, headers, config) {
+                            console.log(data);
+                            console.log(status);
+                            console.log(headers);
+                            console.log(config);
+                            deferred.reject(status);
+                        });
+                    return deferred.promise;
+                }
+                return false;
+            },
+
+            downloadItem: function (url) {
+                if(url){
+                    var deferred = $q.defer();
+
+                    $http({
+                        method: 'GET',
+                        url: url,
+                        headers: {
+                            'Content-Disposition': 'attachment'
+                        }
+                    });
+                }
+            }
+        };
+    }]);
 })(angular, NGS);
