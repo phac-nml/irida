0f764f51bc56c767439ab87763fce671595f41e5
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-07-09 10:37:46 -0500

    Removing the ui.router.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,40 @@
-'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-module.exports = function(grunt) {
-  // Load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  // Configure paths
-  var iridaConfig = {
-    dev: 'src/main/webapp/resources/dev',
-    test: 'src/test'
-  };
+    // Configure paths
+    var iridaConfig = {
+        dev: 'src/main/webapp/resources',
+        test: 'src/test'
+    };
 
-  grunt.initConfig({
-    irida: iridaConfig,
+    grunt.initConfig({
+        irida: iridaConfig,
 
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        '<%= irida.dev %>/js/{,*/}*.js',
-        '!<%= irida.dev %>/js/lib/{,*/}*.js',
-      ]
-    },
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
-      }
-    }
-  });
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= irida.dev %>/js/{,*/}*.js',
+                '!<%= irida.dev %>/js/lib/{,*/}*.js',
+            ]
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        },
+        compass: {
+            config: 'src/main/webapp/'
+        }
+    });
 
-  grunt.registerTask('test', ['karma:unit']);
+    grunt.registerTask('test', ['karma:unit']);
 
-  grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint']);
 };
