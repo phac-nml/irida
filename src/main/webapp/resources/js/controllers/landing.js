9c038dfb594ea746704e633b7fdd97bb3a0264af
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-07-11 12:15:34 -0500

    Forget to add the $scope to the controller

--- src/main/webapp/resources/js/controllers/landing.js
+++ src/main/webapp/resources/js/controllers/landing.js
@@ -2,16 +2,13 @@
  * Created by josh on 2013-06-20.
  */
 (function (ng, app) {
-  'use strict';
+    'use strict';
 
-  app
-    .controller('LandingCtrl', ['ajaxService', function(ajaxService) {
-          ajaxService.get('/api/users/current').then(function(data){
-             console.log(data);
-              $scope.user =data.resource;
-          });
-//      if ($state.current.name === 'projects') {
-//        $state.transitionTo('projects.main');
-//      }
-    }]);
+    app
+        .controller('LandingCtrl', ['$scope', 'ajaxService', function ($scope, ajaxService) {
+            ajaxService.get('/api/users/current').then(function (data) {
+                console.log(data);
+                $scope.user = data.resource;
+            });
+        }]);
 })(angular, NGS);
