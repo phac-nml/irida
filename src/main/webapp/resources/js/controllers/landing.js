52c2be5df72de398f5d29896b5a7a92791bf599c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2013-07-11 12:03:42 -0500

    Fixed basic styles on all project pages.  Removed the ugly nag bar and replaced with pills in the PHAC colour scheme.

--- src/main/webapp/resources/js/controllers/landing.js
+++ src/main/webapp/resources/js/controllers/landing.js
@@ -5,7 +5,11 @@
   'use strict';
 
   app
-    .controller('LandingCtrl', [function() {
+    .controller('LandingCtrl', ['ajaxService', function(ajaxService) {
+          ajaxService.get('/api/users/current').then(function(data){
+             console.log(data);
+              $scope.user =data.resource;
+          });
 //      if ($state.current.name === 'projects') {
 //        $state.transitionTo('projects.main');
 //      }
