71b93ac3a3340563adcc918b3152ed42d82eee21
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-07-12 09:56:26 -0500

    Added RevisionEntity that includes the currently logged in user.  We'll use this for auditing in the future

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/relational/auditing/UserRevEntity.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.corefacility.bioinformatics.irida.repositories.relational.auditing;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.envers.DefaultRevisionEntity;
+import org.hibernate.envers.RevisionEntity;
+
+/**
+ *
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ */
+@Entity
+@RevisionEntity(UserRevListener.class)
+//@Table(name="REVINFO")
+public class UserRevEntity extends DefaultRevisionEntity{
+    
+    Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
