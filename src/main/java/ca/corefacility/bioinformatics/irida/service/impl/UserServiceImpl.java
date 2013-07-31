dc9ae921c699c60aa7cc2f5f3b6a2d21f189fc7a
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-07-31 15:06:24 -0500

    If no role is specified, the user should have ROLE_USER.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/UserServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/UserServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.Project;
+import ca.corefacility.bioinformatics.irida.model.Role;
 import ca.corefacility.bioinformatics.irida.model.User;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
 import ca.corefacility.bioinformatics.irida.repositories.UserRepository;
@@ -26,123 +27,131 @@ import ca.corefacility.bioinformatics.irida.service.UserService;
 
 /**
  * Implementation of the {@link UserService}.
- *
+ * 
  * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
  */
 public class UserServiceImpl extends CRUDServiceImpl<Long, User> implements UserService {
-    /**
-     * The property name to use for passwords on the {@link User} class.
-     */
-    private static final String PASSWORD_PROPERTY = "password";
-    /**
-     * logger
-     */
-    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-    /**
-     * A reference to the user repository used to manage users.
-     */
-    private UserRepository userRepository;
-    /**
-     * A reference to the password encoder used by the system for storing passwords.
-     */
-    private PasswordEncoder passwordEncoder;
+	/**
+	 * The property name to use for passwords on the {@link User} class.
+	 */
+	private static final String PASSWORD_PROPERTY = "password";
+	/**
+	 * logger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+	/**
+	 * A reference to the user repository used to manage users.
+	 */
+	private UserRepository userRepository;
+	/**
+	 * A reference to the password encoder used by the system for storing
+	 * passwords.
+	 */
+	private PasswordEncoder passwordEncoder;
 
-    /**
-     * Constructor, requires a handle on a validator and a repository.
-     *
-     * @param userRepository the repository used to store instances of {@link User}.
-     * @param validator      the validator used to validate instances of {@link User}.
-     */
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, Validator validator) {
-        super(userRepository, validator, User.class);
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+	/**
+	 * Constructor, requires a handle on a validator and a repository.
+	 * 
+	 * @param userRepository
+	 *            the repository used to store instances of {@link User}.
+	 * @param validator
+	 *            the validator used to validate instances of {@link User}.
+	 */
+	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, Validator validator) {
+		super(userRepository, validator, User.class);
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public User create(User u) {
-        Set<ConstraintViolation<User>> violations = validatePassword(u.getPassword());
-        if (violations.isEmpty()) {
-            String password = u.getPassword();
-            u.setPassword(passwordEncoder.encode(password));
-            return super.create(u);
-        }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public User create(User u) {
+		Set<ConstraintViolation<User>> violations = validatePassword(u.getPassword());
+		if (violations.isEmpty()) {
+			String password = u.getPassword();
+			u.setPassword(passwordEncoder.encode(password));
+			if (u.getSystemRole() == null) {
+				u.setSystemRole(new Role("ROLE_USER"));
+			}
+			return super.create(u);
+		}
 
-        throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
-    }
+		throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public User update(Long uid, Map<String, Object> properties) {
-        if (properties.containsKey(PASSWORD_PROPERTY)) {
-            String password = properties.get(PASSWORD_PROPERTY).toString();
-            Set<ConstraintViolation<User>> violations = validatePassword(password);
-            if (violations.isEmpty()) {
-                properties.put(PASSWORD_PROPERTY, passwordEncoder.encode(password));
-            } else {
-                throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
-            }
-        }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public User update(Long uid, Map<String, Object> properties) {
+		if (properties.containsKey(PASSWORD_PROPERTY)) {
+			String password = properties.get(PASSWORD_PROPERTY).toString();
+			Set<ConstraintViolation<User>> violations = validatePassword(password);
+			if (violations.isEmpty()) {
+				properties.put(PASSWORD_PROPERTY, passwordEncoder.encode(password));
+			} else {
+				throw new ConstraintViolationException(new HashSet<ConstraintViolation<?>>(violations));
+			}
+		}
 
-        return super.update(uid, properties);
-    }
+		return super.update(uid, properties);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public User getUserByUsername(String username) throws EntityNotFoundException {
-        return userRepository.getUserByUsername(username);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public User getUserByUsername(String username) throws EntityNotFoundException {
+		return userRepository.getUserByUsername(username);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Collection<Join<Project,User>> getUsersForProject(Project project) {
-        return userRepository.getUsersForProject(project);
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Collection<Join<Project, User>> getUsersForProject(Project project) {
+		return userRepository.getUsersForProject(project);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        logger.trace("Loading user with username: [" + username + "].");
-        org.springframework.security.core.userdetails.User userDetails = null;
-        User u;
-        try {
-            u = userRepository.getUserByUsername(username);
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		logger.trace("Loading user with username: [" + username + "].");
+		org.springframework.security.core.userdetails.User userDetails = null;
+		User u;
+		try {
+			u = userRepository.getUserByUsername(username);
 
-            userDetails = new org.springframework.security.core.userdetails.User(u.getUsername(), u.getPassword(),
-                    u.getAuthorities());
-        } catch (EntityNotFoundException e) {
-            throw new UsernameNotFoundException(e.getMessage());
-        }
-        return userDetails;
-    }
+			userDetails = new org.springframework.security.core.userdetails.User(u.getUsername(), u.getPassword(),
+					u.getAuthorities());
+		} catch (EntityNotFoundException e) {
+			throw new UsernameNotFoundException(e.getMessage());
+		}
+		return userDetails;
+	}
 
-    /**
-     * Validate the password of a {@link User} *before* encoding the password and passing to super.
-     *
-     * @param password the password to validate.
-     * @return true if valid, false otherwise.
-     */
-    private Set<ConstraintViolation<User>> validatePassword(String password) {
-        return validator.validateValue(User.class, PASSWORD_PROPERTY, password);
-    }
+	/**
+	 * Validate the password of a {@link User} *before* encoding the password
+	 * and passing to super.
+	 * 
+	 * @param password
+	 *            the password to validate.
+	 * @return true if valid, false otherwise.
+	 */
+	private Set<ConstraintViolation<User>> validatePassword(String password) {
+		return validator.validateValue(User.class, PASSWORD_PROPERTY, password);
+	}
 
-    /**
-     * {@inheritDoc }
-     */
-    @Override
-    public List<User> getUsersAvailableForProject(Project project) {
-        return userRepository.getUsersAvailableForProject(project);
-    }
+	/**
+	 * {@inheritDoc }
+	 */
+	@Override
+	public List<User> getUsersAvailableForProject(Project project) {
+		return userRepository.getUsersAvailableForProject(project);
+	}
 }
