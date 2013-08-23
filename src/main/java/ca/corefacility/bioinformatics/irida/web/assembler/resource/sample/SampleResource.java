2759fee7d2a97c1d45a1d30087caa75db5c4a81f
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-08-23 09:21:42 -0500

    Added more fields to the sample resource

--- src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/sample/SampleResource.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/sample/SampleResource.java
@@ -29,4 +29,75 @@ public class SampleResource extends IdentifiableResource<Sample> {
     public void setSampleName(String sampleName) {
         resource.setSampleName(sampleName);
     }
+    
+    @XmlElement
+    public String getSampleId(){
+        return resource.getSampleId();
+    }
+    public void setSampleId(String sampleId){
+        resource.setSampleId(sampleId);
+    }
+    
+    @XmlElement
+    public String getSamplePlate() {
+        return resource.getSamplePlate();
+    }
+
+    public void setSamplePlate(String samplePlate) {
+        resource.setSamplePlate(samplePlate);
+    }
+
+    @XmlElement
+    public String getSampleWell() {
+        return resource.getSampleWell();
+    }
+
+    public void setSampleWell(String sampleWell) {
+        resource.setSampleWell(sampleWell);
+    }
+
+    @XmlElement
+    public String getI7IndexId() {
+        return resource.getI7IndexId();
+    }
+
+    public void setI7IndexId(String i7IndexId) {
+        resource.setI7IndexId(i7IndexId);
+    }
+
+    @XmlElement
+    public String getI7Index() {
+        return resource.getI7Index();
+    }
+
+    public void setI7Index(String i7Index) {
+        resource.setI7Index(i7Index);
+    }
+
+    @XmlElement
+    public String getI5IndexId() {
+        return resource.getI5IndexId();
+    }
+
+    public void setI5IndexId(String i5IndexId) {
+        resource.setI5IndexId(i5IndexId);
+    }
+
+    @XmlElement
+    public String getI5Index() {
+        return resource.getI5Index();
+    }
+
+    public void setI5Index(String i5Index) {
+        resource.setI5Index(i5Index);
+    }
+
+    @XmlElement
+    public String getDescription() {
+        return resource.getDescription();
+    }
+
+    public void setDescription(String description) {
+        resource.setDescription(description);
+    }
 }
