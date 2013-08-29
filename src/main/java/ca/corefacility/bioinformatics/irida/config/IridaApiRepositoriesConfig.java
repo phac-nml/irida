ea9245ed18f19cfeed6cdea86932c2b2f7ad779b
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-08-29 11:27:23 -0500

    Adding javaconfig for repositories.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/IridaApiRepositoriesConfig.java
@@ -0,0 +1,14 @@
+package ca.corefacility.bioinformatics.irida.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for repository/data storage classes.
+ * 
+ * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
+ * 
+ */
+@Configuration
+public class IridaApiRepositoriesConfig {
+
+}
