87ddb3eaa3bbf0cd20abb28da4463bd25010e057
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-08-30 09:51:56 -0500

    Replace xml api config with javaconfig, hook into web.xml

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiWebConfig.java
@@ -0,0 +1,87 @@
+package ca.corefacility.bioinformatics.irida.web.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MarshallingView;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.web.spring.view.FastaView;
+import ca.corefacility.bioinformatics.irida.web.spring.view.FastqView;
+import ca.corefacility.bioinformatics.irida.web.spring.view.GenbankView;
+
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Configuration for IRIDA REST API.
+ * 
+ * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
+ * 
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ca.corefacility.bioinformatics.irida.web.controller.api")
+@Import({ IridaApiServicesConfig.class, IridaRestApiSecurityConfig.class })
+public class IridaRestApiWebConfig extends WebMvcConfigurerAdapter {
+
+	private static final long TWO_GIGABYTES = 2147483648l;
+
+	@Bean
+	public MultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setMaxUploadSize(TWO_GIGABYTES);
+		return resolver;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(Arrays.asList((ViewResolver) new BeanNameViewResolver()));
+		resolver.setDefaultViews(defaultViews());
+
+		return resolver;
+	}
+
+	private List<View> defaultViews() {
+		List<View> views = new ArrayList<>();
+		MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
+		jsonView.setPrettyPrint(true);
+		views.add(jsonView);
+		Jaxb2Marshaller jaxb2marshaller = new Jaxb2Marshaller();
+		jaxb2marshaller
+				.setPackagesToScan(new String[] { "ca.corefacility.bioinformatics.irida.web.assembler.resource" });
+		MarshallingView marshallingView = new MarshallingView(jaxb2marshaller);
+		views.add(marshallingView);
+		views.add(new FastaView());
+		views.add(new FastqView());
+		views.add(new GenbankView());
+		return views;
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		Map<String, MediaType> mediaTypes = ImmutableMap.of("json", MediaType.APPLICATION_JSON, "xml",
+				MediaType.APPLICATION_XML, "fasta", MediaType.valueOf("application/fasta"), "fastq",
+				MediaType.valueOf("application/fastq"), "gbk", MediaType.valueOf("application/genbank"));
+		configurer.ignoreAcceptHeader(false).defaultContentType(MediaType.APPLICATION_JSON).favorPathExtension(true)
+				.mediaTypes(mediaTypes);
+	}
+}
