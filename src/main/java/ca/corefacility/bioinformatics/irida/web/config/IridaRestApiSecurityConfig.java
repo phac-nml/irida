127658209d4e4625f1264396e62bf6e3a1d8f48d
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-08-30 13:04:52 -0500

    Enable method-level security by annotating the configuration class in *this* project. Works (for now), but not an ideal solution.

--- src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +14,12 @@ import ca.corefacility.bioinformatics.irida.config.IridaApiSecurityConfig;
 
 @Configuration
 @EnableWebSecurity
+// DO NOT WANT. For some reason, even though we import the
+// IridaApiSecurityConfig below (that already has @EnableGlobalMethodSecurity),
+// we must add the annotation again to this configuration class. That doesn't
+// allow us to turn method security on in the API project, then use the API
+// project configuration elsewhere.
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Import(IridaApiSecurityConfig.class)
 public class IridaRestApiSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
@@ -23,10 +29,5 @@ public class IridaRestApiSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeUrls().anyRequest().fullyAuthenticated().and().httpBasic().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.stateless);
-	}
-
-	@Override
-	protected void registerAuthentication(AuthenticationManagerBuilder auth) {
-		auth.parentAuthenticationManager(authenticationManager);
 	}
 }
