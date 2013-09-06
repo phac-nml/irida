ada692ca5c08c8b0b4792f95c8ce76a789ac6b3d
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-09-06 11:21:34 -0500

    Handle constraint violation exceptions within the user relational repository instead of in the exception itself. Test for situations where the exception doesn't contain a constraint violation name.

--- src/main/java/ca/corefacility/bioinformatics/irida/exceptions/EntityExistsException.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/exceptions/EntityExistsException.java
@@ -1,10 +1,10 @@
 package ca.corefacility.bioinformatics.irida.exceptions;
-import java.util.regex.*;
+
 
 /**
  * When an {@link Identifiable} entity to be created in the database shares an
  * identifier with an existing entity.
- *
+ * 
  * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
  */
 public class EntityExistsException extends RuntimeException {
@@ -13,27 +13,31 @@ public class EntityExistsException extends RuntimeException {
 	private String fieldName;
 
 	/**
-     * Construct a new {@link EntityExistsException} with the specified message.
-     *
-     * @param message the message explaining the exception.
-     */
-    public EntityExistsException(String message) {
-        super(message);
-    }
-	
+	 * Construct a new {@link EntityExistsException} with the specified message.
+	 * 
+	 * @param message
+	 *            the message explaining the exception.
+	 */
+	public EntityExistsException(String message) {
+		super(message);
+	}
+
 	/**
-     * Construct a new {@link EntityExistsException} with the specified message.
-     *
-     * @param message the message explaining the exception.
-	 * @param fieldName The name of the field causing the exception
-     */
-    public EntityExistsException(String message, String fieldName) {
-        this(message);
+	 * Construct a new {@link EntityExistsException} with the specified message.
+	 * 
+	 * @param message
+	 *            the message explaining the exception.
+	 * @param fieldName
+	 *            The name of the field causing the exception
+	 */
+	public EntityExistsException(String message, String fieldName) {
+		this(message);
 		this.fieldName = fieldName;
-    }	
+	}
 
 	/**
 	 * Get the field name that caused the exception
+	 * 
 	 * @return The name of the field that already exists in the database
 	 */
 	public String getFieldName() {
@@ -42,24 +46,11 @@ public class EntityExistsException extends RuntimeException {
 
 	/**
 	 * Set the field name that was in duplicate and caused the exception
-	 * @param fieldName The field name causing the exception
+	 * 
+	 * @param fieldName
+	 *            The field name causing the exception
 	 */
 	public void setFieldName(String fieldName) {
 		this.fieldName = fieldName;
-	}
-	
-	public static String parseConstraintName(String name, String tableName){
-		
-		String regex = "^"+tableName + "_(.*)_CONSTRAINT$";
-		Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-		Matcher matcher = pattern.matcher(name);
-		if(matcher.groupCount() != 1){
-			throw new IllegalArgumentException("Couldn't parse field name from constraint violation: " + name);
-		}
-		
-		matcher.find();
-		String fieldName = matcher.group(1);
-		
-		return fieldName;
 	}
 }
