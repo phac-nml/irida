1a974e3349b88d35230702c43895fe7cc19e4e25
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-09-06 11:25:31 -0500

    Don't autowire the authentication manager, the class we're extending should handle that.

--- src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
@@ -1,33 +1,17 @@
 package ca.corefacility.bioinformatics.irida.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.SessionCreationPolicy;
-
-import ca.corefacility.bioinformatics.irida.config.IridaApiSecurityConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
-// DO NOT WANT. For some reason, even though we import the
-// IridaApiSecurityConfig below (that already has @EnableGlobalMethodSecurity),
-// we must add the annotation again to this configuration class. That doesn't
-// allow us to turn method security on in the API project, then use the API
-// project configuration elsewhere.
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Import(IridaApiSecurityConfig.class)
 public class IridaRestApiSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeUrls().anyRequest().fullyAuthenticated().and().httpBasic().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.stateless);
+		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
 	}
 }
