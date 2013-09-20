e4821fc13bd36bd6d0a52d92d76a12d3d3535c7f
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-09-20 15:33:45 -0500

    Added fields to project in api, added to rest api.  work work.

--- src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/project/ProjectResource.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/project/ProjectResource.java
@@ -5,6 +5,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import ca.corefacility.bioinformatics.irida.model.Project;
 import ca.corefacility.bioinformatics.irida.web.assembler.resource.IdentifiableResource;
+import java.util.Date;
 
 /**
  * A resource for {@link Project}s.
@@ -30,4 +31,67 @@ public class ProjectResource extends IdentifiableResource<Project> {
     public void setName(String name) {
         this.resource.setName(name);
     }
+	
+	@XmlElement
+	public Date getAnticipatedStartDate() {
+		return resource.getAnticipatedStartDate();
+	}
+
+	public void setAnticipatedStartDate(Date anticipatedStartDate) {
+		resource.setAnticipatedStartDate(anticipatedStartDate);
+	}
+
+	@XmlElement
+	public String getProjectDescription() {
+		return resource.getProjectDescription();
+	}
+
+	public void setProjectDescription(String projectDescription) {
+		resource.setProjectDescription(projectDescription);
+	}
+
+	@XmlElement
+	public String getProjectDesign() {
+		return resource.getProjectDesign();
+	}
+
+	public void setProjectDesign(String projectDesign) {
+		resource.setProjectDesign(projectDesign);
+	}
+
+	@XmlElement
+	public String getDataAnalysisRequirements() {
+		return resource.getDataAnalysisRequirements();
+	}
+
+	public void setDataAnalysisRequirements(String dataAnalysisRequirements) {
+		resource.setDataAnalysisRequirements(dataAnalysisRequirements);
+	}
+
+	@XmlElement
+	public String getAdditionalCoreWork() {
+		return resource.getAdditionalCoreWork();
+	}
+
+	public void setAdditionalCoreWork(String additionalCoreWork) {
+		resource.setAdditionalCoreWork(additionalCoreWork);
+	}
+
+	@XmlElement
+	public String getSupplyRequirements() {
+		return resource.getSupplyRequirements();
+	}
+
+	public void setSupplyRequirements(String supplyRequirements) {
+		resource.setSupplyRequirements(supplyRequirements);
+	}
+
+	@XmlElement
+	public String getAdditionalCosts() {
+		return resource.getAdditionalCosts();
+	}
+
+	public void setAdditionalCosts(String additionalCosts) {
+		resource.setAdditionalCosts(additionalCosts);
+	}
 }
