daf9a7abf7cef889fdf5371619ea8502b77346dd
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-09-23 11:45:54 -0500

    Removed all those additional fields we added before for the project management as per the discussion with Shaun.

--- src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/project/ProjectResource.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/project/ProjectResource.java
@@ -33,65 +33,11 @@ public class ProjectResource extends IdentifiableResource<Project> {
     }
 	
 	@XmlElement
-	public Date getAnticipatedStartDate() {
-		return resource.getAnticipatedStartDate();
-	}
-
-	public void setAnticipatedStartDate(Date anticipatedStartDate) {
-		resource.setAnticipatedStartDate(anticipatedStartDate);
-	}
-
-	@XmlElement
 	public String getProjectDescription() {
 		return resource.getProjectDescription();
 	}
 
 	public void setProjectDescription(String projectDescription) {
 		resource.setProjectDescription(projectDescription);
-	}
-
-	@XmlElement
-	public String getProjectDesign() {
-		return resource.getProjectDesign();
-	}
-
-	public void setProjectDesign(String projectDesign) {
-		resource.setProjectDesign(projectDesign);
-	}
-
-	@XmlElement
-	public String getDataAnalysisRequirements() {
-		return resource.getDataAnalysisRequirements();
-	}
-
-	public void setDataAnalysisRequirements(String dataAnalysisRequirements) {
-		resource.setDataAnalysisRequirements(dataAnalysisRequirements);
-	}
-
-	@XmlElement
-	public String getAdditionalCoreWork() {
-		return resource.getAdditionalCoreWork();
-	}
-
-	public void setAdditionalCoreWork(String additionalCoreWork) {
-		resource.setAdditionalCoreWork(additionalCoreWork);
-	}
-
-	@XmlElement
-	public String getSupplyRequirements() {
-		return resource.getSupplyRequirements();
-	}
-
-	public void setSupplyRequirements(String supplyRequirements) {
-		resource.setSupplyRequirements(supplyRequirements);
-	}
-
-	@XmlElement
-	public String getAdditionalCosts() {
-		return resource.getAdditionalCosts();
-	}
-
-	public void setAdditionalCosts(String additionalCosts) {
-		resource.setAdditionalCosts(additionalCosts);
 	}
 }
