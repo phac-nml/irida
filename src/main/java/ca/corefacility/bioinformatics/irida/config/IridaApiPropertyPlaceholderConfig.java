260faf65f72377ff2c697dfdea3cbe87998aab6e
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-10-09 11:37:55 -0500

    Allow configuration of databases and other settings by external file.

--- src/main/java/ca/corefacility/bioinformatics/irida/config/IridaApiPropertyPlaceholderConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/IridaApiPropertyPlaceholderConfig.java
@@ -6,16 +6,23 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
- * Configuration class for loading properties files.
+ * Configuration class for loading properties files. This configuration source
+ * looks in three places for properties:
+ * 
+ * <ol>
+ * <li>within the package at jdbc.dev.properties,</li>
+ * <li>within the package at filesystem.properties, and</li>
+ * <li>on the filesystem at /etc/irida/irida.conf</li>
+ * </ol>
  * 
  * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
  * 
  */
 @Configuration
-@PropertySource({
-		"classpath:/ca/corefacility/bioinformatics/irida/config/jdbc.${spring.profiles.active:dev}.properties",
-		"classpath:/ca/corefacility/bioinformatics/irida/config/filesystem.properties" })
+@PropertySource({ "classpath:/ca/corefacility/bioinformatics/irida/config/jdbc.dev.properties",
+		"classpath:/ca/corefacility/bioinformatics/irida/config/filesystem.properties", "file:/etc/irida/irida.conf" })
 public class IridaApiPropertyPlaceholderConfig {
+
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
--- src/main/java/ca/corefacility/bioinformatics/irida/config/data/IridaApiJdbcDataSourceConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/data/IridaApiJdbcDataSourceConfig.java
@@ -6,69 +6,50 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 
 @Configuration
-@Profile({"dev","prod"})
+@Profile({ "dev", "prod" })
 public class IridaApiJdbcDataSourceConfig implements DataConfig {
-	private @Value("${jdbc.driver}")
-	String driverClassName;
-	private @Value("${jdbc.url}")
-	String url;
-	private @Value("${jdbc.username}")
-	String username;
-	private @Value("${jdbc.password}")
-	String password;
-	private @Value("${jdbc.pool.initialSize}")
-	int initialSize;
-	private @Value("${jdbc.pool.maxActive}")
-	int maxActive;
-	private @Value("${jdbc.pool.maxWait}")
-	int maxWait;
-	
 
-	private @Value("${hibernate.hbm2ddl.auto}")
-	String hbm2ddlAuto;
-	private @Value("${hibernate.hbm2ddl.import_files}")
-	String hbm2ddlImport;
-	private @Value("${hibernate.dialect}")
-	String hibernateDialect;
+	@Autowired
+	Environment environment;
 
 	@Bean
 	public DataSource dataSource() {
 		BasicDataSource basicDataSource = new BasicDataSource();
 
-		basicDataSource.setDriverClassName(driverClassName);
-		basicDataSource.setUrl(url);
-		basicDataSource.setUsername(username);
-		basicDataSource.setPassword(password);
-		basicDataSource.setInitialSize(initialSize);
-		basicDataSource.setMaxActive(maxActive);
-		basicDataSource.setMaxWait(maxWait);
-		basicDataSource.setTestOnBorrow(true);
-		basicDataSource.setTestOnReturn(true);
-		basicDataSource.setTestWhileIdle(true);
-		basicDataSource.setValidationQuery("select 1");
+		basicDataSource.setDriverClassName(environment.getProperty("jdbc.driver"));
+		basicDataSource.setUrl(environment.getProperty("jdbc.url"));
+		basicDataSource.setUsername(environment.getProperty("jdbc.username"));
+		basicDataSource.setPassword(environment.getProperty("jdbc.password"));
+		basicDataSource.setInitialSize(Integer.valueOf(environment.getProperty("jdbc.pool.initialSize")));
+		basicDataSource.setMaxActive(Integer.valueOf(environment.getProperty("jdbc.pool.maxActive")));
+		basicDataSource.setMaxWait(Integer.valueOf(environment.getProperty("jdbc.pool.maxWait")));
+		basicDataSource.setTestOnBorrow(Boolean.valueOf(environment.getProperty("jdbc.pool.testOnBorrow")));
+		basicDataSource.setTestOnReturn(Boolean.valueOf(environment.getProperty("jdbc.pool.testOnReturn")));
+		basicDataSource.setTestWhileIdle(Boolean.valueOf(environment.getProperty("jdbc.pool.testWhileIdle")));
+		basicDataSource.setValidationQuery(environment.getProperty("jdbc.pool.validationQuery"));
 
 		return basicDataSource;
 	}
 
 	@Bean
 	public SessionFactory sessionFactory() {
-		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(
-				dataSource());
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource());
 
 		builder.scanPackages("ca.corefacility.bioinformatics.irida.model",
 				"ca.corefacility.bioinformatics.irida.repositories.relational.auditing");
 		Properties properties = new Properties();
-		properties.put("hibernate.show_sql", false);
-		properties.put("hibernate.hbm2ddl.auto", hbm2ddlAuto);
-		properties.put("hibernate.hbm2ddl.import_files", hbm2ddlImport);
-		properties.put("hibernate.dialect", hibernateDialect);
+		properties.put("hibernate.show_sql", Boolean.valueOf(environment.getProperty("hibernate.show_sql")));
+		properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+		properties.put("hibernate.hbm2ddl.import_files", environment.getProperty("hibernate.hbm2ddl.import_files"));
+		properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
 		builder.addProperties(properties);
 
 		return builder.buildSessionFactory();
