58c6d4663d4d5c3f0437263b0b781c1b8a894872
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-10-23 10:48:33 -0500

    Configuration for JPA with Spring (do not need persistence.xml when using localentitymanagerfactorybean).

--- src/main/java/ca/corefacility/bioinformatics/irida/config/IridaApiRepositoriesConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/IridaApiRepositoriesConfig.java
@@ -4,6 +4,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.sql.DataSource;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.envers.RevisionListener;
 import org.slf4j.Logger;
@@ -14,7 +16,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -47,7 +53,32 @@ public class IridaApiRepositoriesConfig {
 
 	private @Value("${sequence.file.base.directory}")
 	String sequenceFileBaseDirectory;
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+			JpaVendorAdapter jpaVendorAdapter) {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setJpaVendorAdapter(jpaVendorAdapter);
+		factory.setPackagesToScan("ca.corefacility.bioinformatics.irida.model");
+
+		return factory;
+	}
 	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabase(Database.MYSQL);
+		return adapter;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new JpaTransactionManager();
+	}
+
 	@Bean
 	public CRUDRepository<Long, SequenceFile> sequenceFileFilesystemRepository() {
 		Path baseDirectory = Paths.get(sequenceFileBaseDirectory);
@@ -66,10 +97,5 @@ public class IridaApiRepositoriesConfig {
 	@Bean(initMethod = "initialize")
 	public RevisionListener revisionListener() {
 		return new UserRevListener();
-	}
-
-	@Bean
-	public PlatformTransactionManager transactionManager() {
-		return new HibernateTransactionManager(sessionFactory);
 	}
 }
