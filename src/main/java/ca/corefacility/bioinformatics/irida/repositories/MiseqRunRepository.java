0394813bcbe002b9f870715279edc2271254af76
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-10-23 14:57:32 -0500

    Replace addSequenceFileToMiseqRun with MiseqRunSequenceFileJoinRepository.

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/MiseqRunRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/MiseqRunRepository.java
@@ -3,7 +3,6 @@ package ca.corefacility.bioinformatics.irida.repositories;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import ca.corefacility.bioinformatics.irida.exceptions.EntityExistsException;
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.exceptions.StorageException;
 import ca.corefacility.bioinformatics.irida.model.MiseqRun;
@@ -13,17 +12,10 @@ import ca.corefacility.bioinformatics.irida.model.joins.impl.MiseqRunSequenceFil
 /**
  *
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
  */
 public interface MiseqRunRepository extends PagingAndSortingRepository<MiseqRun, Long>{
     
-    /**
-     * Associate a {@link SequenceFile} with a {@link MiseqRun}
-     * @param run The {@link MiseqRun} to add to
-     * @param file The {@link SequenceFile} to add to the run
-     * @return A {@link MiseqRunSequenceFileJoin} describing the relationship between the run and file
-     * @throws EntityExistsException If the sequencefile is already associated with a miseq run.
-     */
-    public MiseqRunSequenceFileJoin addSequenceFileToMiseqRun(MiseqRun run, SequenceFile file) throws EntityExistsException;
     
     /**
      * Get the {@link MiseqRun} associated with a {@link SequenceFile}
--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/MiseqRunServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/MiseqRunServiceImpl.java
@@ -1,4 +1,3 @@
-
 package ca.corefacility.bioinformatics.irida.service.impl;
 
 import javax.validation.Validator;
@@ -9,43 +8,49 @@ import org.springframework.transaction.annotation.Transactional;
 import ca.corefacility.bioinformatics.irida.model.MiseqRun;
 import ca.corefacility.bioinformatics.irida.model.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
+import ca.corefacility.bioinformatics.irida.model.joins.impl.MiseqRunSequenceFileJoin;
 import ca.corefacility.bioinformatics.irida.repositories.MiseqRunRepository;
+import ca.corefacility.bioinformatics.irida.repositories.joins.sequencefile.MiseqRunSequenceFileJoinRepository;
 import ca.corefacility.bioinformatics.irida.service.MiseqRunService;
 
 /**
- *
+ * 
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
  */
 @Service
 public class MiseqRunServiceImpl extends CRUDServiceImpl<Long, MiseqRun> implements MiseqRunService {
 
-    private MiseqRunRepository miseqRepo;
-    
-    protected MiseqRunServiceImpl() {
-    	super(null, null, MiseqRun.class);
-    }
-    
-    public MiseqRunServiceImpl(MiseqRunRepository repository, Validator validator){
-        super(repository, validator, MiseqRun.class);
-        this.miseqRepo = repository;
-    }
+	private MiseqRunRepository miseqRepo;
+	private MiseqRunSequenceFileJoinRepository mrsfRepository;
 
-    /**
-     * {@inheritDoc }
-     */
-    @Override
-    @Transactional
-    public Join<MiseqRun, SequenceFile> addSequenceFileToMiseqRun(MiseqRun run, SequenceFile file) {
-        return miseqRepo.addSequenceFileToMiseqRun(run, file);
-    }
-    
-    /**
-     * {@inheritDoc }
-     */
-    @Override
-    @Transactional(readOnly = true)
-    public Join<MiseqRun, SequenceFile> getMiseqRunForSequenceFile(SequenceFile file) {
-        return miseqRepo.getMiseqRunForSequenceFile(file);
-    }
-    
+	protected MiseqRunServiceImpl() {
+		super(null, null, MiseqRun.class);
+	}
+
+	public MiseqRunServiceImpl(MiseqRunRepository repository, MiseqRunSequenceFileJoinRepository mrsfRepository,
+			Validator validator) {
+		super(repository, validator, MiseqRun.class);
+		this.miseqRepo = repository;
+		this.mrsfRepository = mrsfRepository;
+	}
+
+	/**
+	 * {@inheritDoc }
+	 */
+	@Override
+	@Transactional
+	public Join<MiseqRun, SequenceFile> addSequenceFileToMiseqRun(MiseqRun run, SequenceFile file) {
+		MiseqRunSequenceFileJoin join = new MiseqRunSequenceFileJoin(run, file);
+		return mrsfRepository.save(join);
+	}
+
+	/**
+	 * {@inheritDoc }
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public Join<MiseqRun, SequenceFile> getMiseqRunForSequenceFile(SequenceFile file) {
+		return miseqRepo.getMiseqRunForSequenceFile(file);
+	}
+
 }
