b07e9f8b73bb13af64a7a7e4ea06244039309335
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-10-24 11:49:32 -0500

    Removing old test data files.

--- src/main/resources/ca/corefacility/bioinformatics/irida/sql/test-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO identifiable (nonNull , integerValue , label) VALUES ('not null entity 1',1,'entity 1');
-INSERT INTO identifiable (nonNull , integerValue , label) VALUES ('not null entity 2',2,'entity 2');
-INSERT INTO identifiable (nonNull , integerValue , label) VALUES ('not null entity 3',3,'entity 3');
-INSERT INTO system_role (name,description) VALUES ('ROLE_USER','A basic user');
