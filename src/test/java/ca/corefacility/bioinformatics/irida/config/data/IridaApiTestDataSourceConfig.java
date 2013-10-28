e1683440fa7358c70321f05667ba22941d6e74c2
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2013-10-28 13:28:56 -0500

    Changed test hbm2dll database settings

--- src/test/java/ca/corefacility/bioinformatics/irida/config/data/IridaApiTestDataSourceConfig.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/config/data/IridaApiTestDataSourceConfig.java
@@ -57,7 +57,7 @@ public class IridaApiTestDataSourceConfig implements DataConfig {
 				"ca.corefacility.bioinformatics.irida.utils.model");
 		Properties properties = new Properties();
 		properties.put("hibernate.show_sql", false);
-		properties.put("hibernate.hbm2ddl.auto", "update");
+		properties.put("hibernate.hbm2ddl.auto", "create-drop");
 		properties
 				.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
 
