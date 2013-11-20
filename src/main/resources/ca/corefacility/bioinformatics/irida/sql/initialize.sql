d97a19cbd858d4ff883bdb0e1d59b0e48d1c0fed
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2013-11-20 12:08:54 -0600

    Finished with updates and constraint renaming. Update the initialization data to reflect changes to the schema.

--- src/main/resources/ca/corefacility/bioinformatics/irida/sql/initialize.sql
+++ src/main/resources/ca/corefacility/bioinformatics/irida/sql/initialize.sql
@@ -4,4 +4,4 @@ INSERT INTO system_role (`name`,`description`) VALUES ('ROLE_CLIENT','A client t
 INSERT INTO system_role (`name`,`description`) VALUES ('ROLE_MANAGER','A manager role in the system.');
 
 -- user account required for integration tests
-INSERT INTO user (`createdDate`, `modifiedDate`, `email`, `firstName`, `lastName`, `locale`, `password`, `phoneNumber`, `username`, `enabled`, `system_role`, `credentialsNonExpired`) VALUES (now(), now() , 'you@e-mail.ca', 'Administrator', 'Administrator', 'en', '$2a$10$yvzFLxWA9m2wNQmHpJtWT.MRZv8qV8Mo3EMB6HTkDnUbi9aBrbWWW', '867-5309', 'admin', 1, 2, 0);
+INSERT INTO user (`createdDate`, `modifiedDate`, `email`, `firstName`, `lastName`, `locale`, `password`, `phoneNumber`, `username`, `enabled`, `system_role`, `credentialsNonExpired`) VALUES (now(), now() , 'you@e-mail.ca', 'Administrator', 'Administrator', 'en', '$2a$10$yvzFLxWA9m2wNQmHpJtWT.MRZv8qV8Mo3EMB6HTkDnUbi9aBrbWWW', '867-5309', 'admin', 1, 'ROLE_ADMIN', 0);
