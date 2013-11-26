ad87c7d3041744aae03441ce6cb1286fb7f4e77f
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2013-11-26 16:01:01 -0600

    Added integration tests with Galaxy using galaxybootstrap.  Need to use custom galaxybootstrap library for now.  Integration tests not run automatically with maven because they take a long time to run.

--- lib/install-libs.sh
+++ lib/install-libs.sh
@@ -3,6 +3,8 @@
 mvn install:install-file -Dfile=jbzip2-0.9.jar -DgroupId=org.itadaki.bzip2 -DartifactId=jbzip2 -Dversion=0.9 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn install:install-file -Dfile=sam-1.32.jar -DgroupId=net.sf.samtools -DartifactId=samtools -Dversion=1.32 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn install:install-file -Dfile=fastqc-0.10.1.jar -Dsources=fastqc-0.10.1-src.jar -DpomFile=fastqc-0.10.1.pom -DcreateChecksum=true
+mvn install:install-file -Dfile=galaxybootstrap-0.2.0-rc2-1-SNAPSHOT.jar -DpomFile=galaxybootstrap-0.2.0-rc2-1-SNAPSHOT.pom -DcreateChecksum=true
+
 
 TEMP=$(mktemp --directory)
 pushd $TEMP
