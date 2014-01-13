c88a7bb10c1f2f8f9af35623656c858e96018b0c
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-01-13 09:11:42 -0600

    Added config of Galaxy connection using Spring

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/pipeline/data/galaxy/GalaxyAPIConfig.java
@@ -0,0 +1,28 @@
+package ca.corefacility.bioinformatics.irida.config.pipeline.data.galaxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+
+import ca.corefacility.bioinformatics.irida.pipeline.data.galaxy.impl.GalaxyAPI;
+
+@Configuration
+@Profile({ "dev", "prod" })
+public class GalaxyAPIConfig
+{
+	@Autowired
+	private Environment environment;
+	
+	@Bean
+	public GalaxyAPI galaxyAPI()
+	{
+		String galaxyURL = environment.getProperty("galaxy.url");
+		String galaxyAdminAPIKey = environment.getProperty("galaxy.admin.apiKey");
+		String galaxyAdminEmail = environment.getProperty("galaxy.admin.email");
+		boolean linkFiles = Boolean.valueOf(environment.getProperty("galaxy.linkFiles"));
+		
+		return new GalaxyAPI(galaxyURL, galaxyAdminEmail, galaxyAdminAPIKey, linkFiles);
+	}
+}
