bb15623c8a64b92058daa03670591d38488988c9
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2014-01-29 15:47:12 -0600

    Upgrade to the most recent version of spring-test-dbunit that's not a snapshot.

--- lib/install-libs.sh
+++ lib/install-libs.sh
@@ -4,11 +4,3 @@ mvn install:install-file -Dfile=jbzip2-0.9.jar -DgroupId=org.itadaki.bzip2 -Dart
 mvn install:install-file -Dfile=sam-1.32.jar -DgroupId=net.sf.samtools -DartifactId=samtools -Dversion=1.32 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn install:install-file -Dfile=fastqc-0.10.1.jar -Dsources=fastqc-0.10.1-src.jar -DpomFile=fastqc-0.10.1.pom -DcreateChecksum=true
 
-TEMP=$(mktemp --directory)
-pushd $TEMP
-git clone https://github.com/springtestdbunit/spring-test-dbunit.git
-pushd spring-test-dbunit
-git checkout a2b28bd4e65830231c1c6f5cfe42d2f08d63aded
-mvn clean install
-popd
-popd
