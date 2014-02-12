8980722dfecfc176228486693a2d619c54be2dd1
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-02-12 13:04:48 -0600

    Fixed up checking for invalid GalaxyFolderPath

--- src/test/java/ca/corefacility/bioinformatics/irida/model/upload/galaxy/GalaxyFolderPathTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/model/upload/galaxy/GalaxyFolderPathTest.java
@@ -1,20 +1,7 @@
 package ca.corefacility.bioinformatics.irida.model.upload.galaxy;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Set;
-
-import javax.validation.Configuration;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
-
-import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
-import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
-import org.junit.Before;
+import static org.junit.Assert.*;
 import org.junit.Test;
-import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * Tests for {@link GalaxyFolderPath}.
@@ -23,57 +10,40 @@ import org.springframework.context.support.ResourceBundleMessageSource;
  */
 public class GalaxyFolderPathTest {
 	
-	private static final String MESSAGES_BASENAME = "ca.corefacility.bioinformatics.irida.validation.ValidationMessages";
-	private Validator validator;
-
-	@Before
-	public void setUp() {
-		Configuration<?> configuration = Validation.byDefaultProvider().configure();
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename(MESSAGES_BASENAME);
-		configuration.messageInterpolator(new ResourceBundleMessageInterpolator(new PlatformResourceBundleLocator(
-				MESSAGES_BASENAME)));
-		ValidatorFactory factory = configuration.buildValidatorFactory();
-		validator = factory.getValidator();
-	}
-	
-	@Test
+	/**
+	 * Tests for case of null path name.
+	 */
+	@Test(expected=NullPointerException.class)
 	public void testNullName() {
-		GalaxyFolderPath name = new GalaxyFolderPath(null);
-		
-		Set<ConstraintViolation<GalaxyFolderPath>> constraintViolations
-			= validator.validate(name);
-
-		assertEquals(1, constraintViolations.size());
+		new GalaxyFolderPath(null);
 	}
 	
-	@Test
+	/**
+	 * Tests for case of too short path name.
+	 */
+	@Test(expected=IllegalArgumentException.class)
 	public void testShortName() {
-		GalaxyFolderPath name = new GalaxyFolderPath("a");
-		
-		Set<ConstraintViolation<GalaxyFolderPath>> constraintViolations
-			= validator.validate(name);
-
-		assertEquals(1, constraintViolations.size());
+		new GalaxyFolderPath("a");
 	}
 	
+	/**
+	 * Tests for a valid name.
+	 */
 	@Test
 	public void testValidName() {
-		GalaxyFolderPath name = new GalaxyFolderPath("Abc123_-/");
-		
-		Set<ConstraintViolation<GalaxyFolderPath>> constraintViolations
-			= validator.validate(name);
-
-		assertEquals(0, constraintViolations.size());
+		new GalaxyFolderPath("Abc123_-/");
 	}
 	
+	/**
+	 * Tests for a blacklist of invalid characters.
+	 */
 	@Test
-	public void testInvalidName() {
-		GalaxyFolderPath name = new GalaxyFolderPath("Abc123_-/<");
-		
-		Set<ConstraintViolation<GalaxyFolderPath>> constraintViolations
-			= validator.validate(name);
-
-		assertEquals(1, constraintViolations.size());
+	public void testBlackList() {
+		for (char c : GalaxyFolderPath.BLACKLIST) {
+			try {
+				new GalaxyFolderPath("ATLEAST3" + c);
+				fail("failed to throw exception with invalid character \"" + c + "\"");
+			} catch (IllegalArgumentException e){}
+		}
 	}
 }
