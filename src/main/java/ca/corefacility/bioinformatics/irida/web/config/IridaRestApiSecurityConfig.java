63020f0221353285070af8be41b23a89856e90a6
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-03-28 14:07:29 -0500

    Preparing the web layer for oauth2 authentication

--- src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
@@ -1,6 +1,7 @@
 package ca.corefacility.bioinformatics.irida.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,10 +9,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
+@Import(IridaOAuth2WebConfig.class)
 public class IridaRestApiSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
+	/*@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
-	}
+	}*/
 }
