5d8d32807811c35a132a1ea8d773428c0358a239
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-03-31 17:15:30 -0500

    Removed some useless imports

--- src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiSecurityConfig.java
@@ -2,10 +2,8 @@ package ca.corefacility.bioinformatics.irida.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
