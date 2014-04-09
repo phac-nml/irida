f7d7b00518d78944c109e7b2830ddeed4c7328c6
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-04-09 14:01:20 -0500

    Removed some useless resources and cleaned up some config and such

--- src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiWebConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/config/IridaRestApiWebConfig.java
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.web.accept.ContentNegotiationManager;
-import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.View;
--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/LoginController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/LoginController.java
@@ -3,11 +3,8 @@ package ca.corefacility.bioinformatics.irida.web.controller.api;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class LoginController {
