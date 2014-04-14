00173a662202de023044b864f6ed747b76d95d2c
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-04-14 11:46:31 -0500

    Updated GalaxyHistory to add dataset from library

--- src/main/java/ca/corefacility/bioinformatics/irida/pipeline/upload/galaxy/GalaxyHistory.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/pipeline/upload/galaxy/GalaxyHistory.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 import com.github.jmchilton.blend4j.galaxy.GalaxyInstance;
 import com.github.jmchilton.blend4j.galaxy.HistoriesClient;
 import com.github.jmchilton.blend4j.galaxy.beans.History;
+import com.github.jmchilton.blend4j.galaxy.beans.HistoryDataset;
+import com.github.jmchilton.blend4j.galaxy.beans.HistoryDataset.Source;
+import com.github.jmchilton.blend4j.galaxy.beans.HistoryDetails;
 
 import static com.google.common.base.Preconditions.*;
 
@@ -27,11 +30,35 @@ public class GalaxyHistory {
 		this.galaxyInstance = galaxyInstance;
 	}
 	
+	/**
+	 * Creates a new History for running a workflow.
+	 * @return  A new History for running a workflow.
+	 */
 	public History newHistoryForWorkflow() {
 		HistoriesClient historiesClient = galaxyInstance.getHistoriesClient();
 
 		History history = new History();
 		history.setName(UUID.randomUUID().toString());
 		return historiesClient.create(history);
+	}
+	
+	/**
+	 * Transfers a dataset from a Galaxy library into a history for a workflow.
+	 * @param libraryFileId
+	 * @param history
+	 * @return
+	 */
+	public HistoryDetails libraryDatasetToHistory(String libraryFileId, History history) {
+		checkNotNull(libraryFileId, "libraryFileId is null");
+		checkNotNull(history, "history is null");
+		
+		HistoriesClient historiesClient = galaxyInstance.getHistoriesClient();
+		
+		HistoryDataset historyDataset = new HistoryDataset();
+		historyDataset.setSource(Source.LIBRARY);
+		historyDataset.setContent(libraryFileId);
+		
+		return historiesClient.createHistoryDataset(
+				history.getId(), historyDataset);
 	}
 }
--- src/test/java/ca/corefacility/bioinformatics/irida/pipeline/upload/galaxy/unit/GalaxyHistoryTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/pipeline/upload/galaxy/unit/GalaxyHistoryTest.java
@@ -11,6 +11,8 @@ import org.mockito.MockitoAnnotations;
 import com.github.jmchilton.blend4j.galaxy.GalaxyInstance;
 import com.github.jmchilton.blend4j.galaxy.HistoriesClient;
 import com.github.jmchilton.blend4j.galaxy.beans.History;
+import com.github.jmchilton.blend4j.galaxy.beans.HistoryDataset;
+import com.github.jmchilton.blend4j.galaxy.beans.HistoryDetails;
 
 import ca.corefacility.bioinformatics.irida.pipeline.upload.galaxy.GalaxyHistory;
 
@@ -23,6 +25,9 @@ public class GalaxyHistoryTest {
 
 	@Mock private GalaxyInstance galaxyInstance;
 	@Mock HistoriesClient historiesClient;
+	
+	private final String libraryFileId = "1";
+	private final String historyId = "2";
 	
 	@Before
 	public void setup() {
@@ -39,5 +44,18 @@ public class GalaxyHistoryTest {
 		
 		GalaxyHistory galaxyHistory = new GalaxyHistory(galaxyInstance);
 		assertEquals(newHistory, galaxyHistory.newHistoryForWorkflow());
+	}
+	
+	@Test
+	public void testLibraryDatasetToHistory() {
+		HistoryDetails historyDetails = new HistoryDetails();
+		History createdHistory = new History();
+		createdHistory.setId(historyId);
+		
+		when(historiesClient.createHistoryDataset(any(String.class),
+				any(HistoryDataset.class))).thenReturn(historyDetails);
+		
+		GalaxyHistory galaxyHistory = new GalaxyHistory(galaxyInstance);
+		assertNotNull(galaxyHistory.libraryDatasetToHistory(libraryFileId, createdHistory));
 	}
 }
