41aca9cde35b8b499be697c2b0c292c636b4615f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-15 09:12:30 -0500

    Add hashCode and equals methods.

--- src/main/java/ca/corefacility/bioinformatics/irida/model/PasswordReset.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/PasswordReset.java
@@ -1,6 +1,7 @@
 package ca.corefacility.bioinformatics.irida.model;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 import javax.persistence.*;
@@ -27,7 +28,8 @@ public class PasswordReset implements Comparable<PasswordReset> {
 	@NotNull
 	private String id;
 
-	protected PasswordReset() {}
+	protected PasswordReset() {
+	}
 
 	public PasswordReset(User user) {
 		this.createdDate = new Date();
@@ -50,5 +52,21 @@ public class PasswordReset implements Comparable<PasswordReset> {
 	@Override
 	public int compareTo(PasswordReset passwordReset) {
 		return createdDate.compareTo(passwordReset.createdDate);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(createdDate, user, id);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Project) {
+			PasswordReset p = (PasswordReset) other;
+			return Objects.equals(createdDate, p.createdDate) && Objects.equals(user, p.user)
+					&&					Objects.equals(id, p.id);
+		}
+
+		return false;
 	}
 }
