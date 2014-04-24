8253f45d9cda2ee6015baf830937c82eda8690fa
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-04-24 11:09:53 -0500

    added a generic repository type

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/oltu/OltuGenericRepository.java
@@ -0,0 +1,64 @@
+package ca.corefacility.bioinformatics.irida.repositories.remote.oltu;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import ca.corefacility.bioinformatics.irida.repositories.remote.GenericRemoteRepository;
+import ca.corefacility.bioinformatics.irida.repositories.remote.model.ListResourceWrapper;
+import ca.corefacility.bioinformatics.irida.repositories.remote.model.RemoteResource;
+import ca.corefacility.bioinformatics.irida.repositories.remote.model.ResourceWrapper;
+
+public abstract class OltuGenericRepository<Type extends RemoteResource> implements GenericRemoteRepository<Type> {
+
+	private OAuthTokenRestTemplate restTemplate;
+	private String relativeURI;
+	private URI resourcesURI;
+
+	ParameterizedTypeReference<ListResourceWrapper<Type>> listTypeReference;
+	ParameterizedTypeReference<ResourceWrapper<Type>> objectTypeReference;
+
+	public OltuGenericRepository(OAuthTokenRestTemplate restTemplate,
+			String relativeURI,
+			ParameterizedTypeReference<ListResourceWrapper<Type>> listTypeReference,
+			ParameterizedTypeReference<ResourceWrapper<Type>> objectTypeReference) {
+		this.restTemplate = restTemplate;
+		this.relativeURI = relativeURI;
+		this.listTypeReference = listTypeReference;
+		this.objectTypeReference = objectTypeReference;
+	}
+
+	@Override
+	public Type read(Long id) {
+		URI resourceIdURI = UriBuilder.fromUri(resourcesURI).path(id.toString()).build();
+		ResponseEntity<ResourceWrapper<Type>> exchange = restTemplate.exchange(resourceIdURI, HttpMethod.GET,
+				HttpEntity.EMPTY, objectTypeReference);
+
+		return exchange.getBody().getResource();
+	}
+
+	@Override
+	public List<Type> list() {
+		ResponseEntity<ListResourceWrapper<Type>> exchange = restTemplate.exchange(resourcesURI, HttpMethod.GET,
+				HttpEntity.EMPTY, listTypeReference);
+
+		return exchange.getBody().getResource().getResources();
+	}
+
+	@Override
+	public void setBaseURI(URI baseURI) {
+		restTemplate.setServiceURI(baseURI);
+		resourcesURI = UriBuilder.fromUri(baseURI).path(relativeURI).build();
+	}
+
+	public OAuthTokenRestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+
+}
--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/oltu/OltuProjectRemoteRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/oltu/OltuProjectRemoteRepository.java
@@ -1,59 +1,21 @@
 package ca.corefacility.bioinformatics.irida.repositories.remote.oltu;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.List;
-
-import javax.ws.rs.core.UriBuilder;
-
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 
 import ca.corefacility.bioinformatics.irida.repositories.remote.ProjectRemoteRepository;
 import ca.corefacility.bioinformatics.irida.repositories.remote.model.ListResourceWrapper;
 import ca.corefacility.bioinformatics.irida.repositories.remote.model.RemoteProject;
 import ca.corefacility.bioinformatics.irida.repositories.remote.model.ResourceWrapper;
 
-public class OltuProjectRemoteRepository{// implements ProjectRemoteRepository{
+public class OltuProjectRemoteRepository extends OltuGenericRepository<RemoteProject> implements ProjectRemoteRepository{
 
-	private OAuthTokenRestTemplate restTemplate;
-	private URI resourcesURI;
-	private URI baseURI;
-	private String relativeURI = "projects";
+	public final static String relativeURI = "projects";
 	
-	ParameterizedTypeReference<ListResourceWrapper<RemoteProject>> listTypeReference;
-	ParameterizedTypeReference<ResourceWrapper<RemoteProject>> objectTypeReference;
+	private static ParameterizedTypeReference<ListResourceWrapper<RemoteProject>> listTypeReference = new ParameterizedTypeReference<ListResourceWrapper<RemoteProject>>() {};
+	private static ParameterizedTypeReference<ResourceWrapper<RemoteProject>> objectTypeReference = new ParameterizedTypeReference<ResourceWrapper<RemoteProject>>() {};
 	
 	
 	public OltuProjectRemoteRepository(OAuthTokenRestTemplate restTemplate){
-		this.restTemplate = restTemplate;
-		
-		listTypeReference = new ParameterizedTypeReference<ListResourceWrapper<RemoteProject>>() {};
-		objectTypeReference = new ParameterizedTypeReference<ResourceWrapper<RemoteProject>>() {};
-	}
-	
-	//@Override
-	public RemoteProject read(Long id) {
-		URI resourceIdURI = UriBuilder.fromUri(resourcesURI).path(id.toString()).build();
-		ResponseEntity<ResourceWrapper<RemoteProject>> exchange = restTemplate.exchange(resourceIdURI, HttpMethod.GET, HttpEntity.EMPTY, objectTypeReference);
-
-		return exchange.getBody().getResource();
-	}
-
-	//@Override
-	public List<RemoteProject> list() throws IOException{
-		ResponseEntity<ListResourceWrapper<RemoteProject>> exchange = restTemplate.exchange(resourcesURI, HttpMethod.GET, HttpEntity.EMPTY, listTypeReference);
-		ListResourceWrapper<RemoteProject> body = exchange.getBody();
-		
-		return body.getResource().getResources();
-	}
-
-	//@Override
-	public void setBaseURI(URI baseURI) {
-		this.baseURI = baseURI;
-		restTemplate.setServiceURI(baseURI);
-		resourcesURI = UriBuilder.fromUri(baseURI).path(relativeURI).build();
+		super(restTemplate,relativeURI,listTypeReference,objectTypeReference);
 	}
 }
