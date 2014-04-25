5c2f6c173869b2252a761735a24c9db7b98e118d
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-04-25 15:06:24 -0500

    Clearing some warnings

--- src/main/java/ca/corefacility/bioinformatics/irida/config/IridaRemoteRepositoriesConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/IridaRemoteRepositoriesConfig.java
@@ -1,16 +1,7 @@
 package ca.corefacility.bioinformatics.irida.config;
 
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 
 import ca.corefacility.bioinformatics.irida.repositories.remote.oltu.OAuthTokenRestTemplate;
 import ca.corefacility.bioinformatics.irida.repositories.remote.oltu.OltuProjectRemoteRepository;
