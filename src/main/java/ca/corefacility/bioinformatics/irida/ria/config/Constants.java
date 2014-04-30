399da67c1f2808c1d093103ebe14ce5c86f4caae
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 08:48:35 -0500

    Constants used within the configuration of this appication.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/Constants.java
@@ -0,0 +1,10 @@
+package ca.corefacility.bioinformatics.irida.ria.config;
+
+/**
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+public class Constants {
+	public static final String BASE_URL = "ca.corefacility.bioinformatics.irida.ria";
+	public static final String CONFIG_LOCATION = BASE_URL + ".config";
+	public static final String DEFAULT_MAPPING_URL = "/*";
+}
