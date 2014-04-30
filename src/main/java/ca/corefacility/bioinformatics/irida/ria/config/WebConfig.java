3ead35f3d35ee0dddefa6a50afbc55cfc5123250
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 09:17:26 -0500

    Removed  unnecessary method and add a constant for the base url.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfig.java
@@ -2,25 +2,15 @@ package ca.corefacility.bioinformatics.irida.ria.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.inject.Inject;
 
 /**
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"ca.corefacility.bioinformatics.irida.ria"})
+@ComponentScan(basePackages = { Constants.BASE_URL })
 public class WebConfig extends WebMvcConfigurerAdapter {
-	@Inject
-	Environment env;
 
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
 }
