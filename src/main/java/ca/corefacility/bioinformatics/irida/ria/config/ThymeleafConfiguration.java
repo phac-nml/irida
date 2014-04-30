f3dff68729d39fac78ad6e12272ccd18d4fce385
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 12:24:37 -0500

    Thymeleaf view resolver configuration.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/ThymeleafConfiguration.java
@@ -0,0 +1,77 @@
+package ca.corefacility.bioinformatics.irida.ria.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+/**
+ * Replace JSP's with Thymeleaf templating.
+ * 
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+@Configuration
+public class ThymeleafConfiguration {
+	private static final String TEMPLATE_MODE = "HTML5";
+	private static final String TEMPLATE_PREFIX = "/pages/";
+	private static final String TEMPLATE_SUFFIX = ".html";
+	private static final int TEMPLATE_ORDER = 1;
+	private static final boolean TEMPLATE_DEV_CACHEABLE = false;
+
+	/**
+	 * Add Thymeleaf as the view resolver.
+	 * 
+	 * @return {@link ViewResolver}
+	 */
+	@Bean
+	public ViewResolver viewResolver() {
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(Arrays.asList((ViewResolver) thymeleafViewResolver()));
+		return resolver;
+	}
+
+	/**
+	 * Create a new {@link ServletContextTemplateResolver} and set defaults.
+	 * 
+	 * @return {@link ServletContextTemplateResolver}
+	 */
+	private ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix(TEMPLATE_PREFIX);
+		resolver.setSuffix(TEMPLATE_SUFFIX);
+		resolver.setTemplateMode(TEMPLATE_MODE);
+		resolver.setOrder(TEMPLATE_ORDER);
+
+		// TODO: In production this needs to be set to true. Use environment
+		// variable like jhipster?
+		resolver.setCacheable(TEMPLATE_DEV_CACHEABLE);
+		return resolver;
+	}
+
+	/**
+	 * Thymeleaf needs a template engine, get a {@link SpringTemplateEngine}
+	 * 
+	 * @return {@link SpringTemplateEngine}
+	 */
+	private SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	/**
+	 * Create the actual {@link ThymeleafViewResolver}
+	 * 
+	 * @return {@link ThymeleafViewResolver}
+	 */
+	private ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		return resolver;
+	}
+}
