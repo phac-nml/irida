4bd56c59432722f2b438aba1e5ba503a14e58c5b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 09:16:51 -0500

    Added documentation and removed magic numbers.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebAppInitializer.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebAppInitializer.java
@@ -1,17 +1,20 @@
 package ca.corefacility.bioinformatics.irida.ria.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 /**
+ * This class allows us to configure the ServletContext pragmatically.
+ * 
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ * 
  */
 public class WebAppInitializer implements WebApplicationInitializer {
 	@Override
@@ -20,12 +23,25 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		servletContext.addListener(new ContextLoaderListener(context));
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(
 				context));
-		dispatcher.setLoadOnStartup(1);
+
+		// Give this servlet a high priority for startup
+		dispatcher.setLoadOnStartup(Constants.STARTUP_LOAD_PRIORITY_HIGH);
+
+		// Add a servlet mapping
 		dispatcher.addMapping(Constants.DEFAULT_MAPPING_URL);
 	}
 
+	/**
+	 * Get a new {@link AnnotationConfigWebApplicationContext} and tell it where
+	 * to look for configuration files.
+	 * 
+	 * @return {@link AnnotationConfigWebApplicationContext}
+	 */
 	private AnnotationConfigWebApplicationContext getContext() {
 		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+		// Give a hint to the context where classes marked with @Configuration
+		// are located.
 		context.setConfigLocation(Constants.CONFIG_LOCATION);
 		return context;
 	}
