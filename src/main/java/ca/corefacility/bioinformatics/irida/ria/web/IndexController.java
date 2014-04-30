d36176bd3e36586cfe24f11590cfeb37b1a6f594
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 09:23:07 -0500

    Added hello world controller.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/IndexController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/IndexController.java
@@ -1,7 +1,20 @@
 package ca.corefacility.bioinformatics.irida.ria.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
- * Created by josh on 2014-04-30.
+ * Stand in class to ensure that the web app is running properly.
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
  */
+@Controller
 public class IndexController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String showIndex() {
+		return "Hello World!";
+	}
 }
