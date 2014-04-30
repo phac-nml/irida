10a1e72c50eae425267610d62852da24f9042235
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 12:36:26 -0500

    Moved thymeleaf to a seperate class.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
@@ -1,12 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 /**
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
@@ -15,14 +12,4 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 @EnableWebMvc
 @ComponentScan(basePackages = { "ca.corefacility.bioinformatics.irida.ria" })
 public class WebConfigurer extends WebMvcConfigurerAdapter {
-	private static final String TEMPLATE_MODE = "HTML5";
-	private static final String TEMPLATE_SUFFIX = ".html";
-
-	@Bean
-	public ViewResolver viewResolver() {
-		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-		templateResolver.setTemplateMode(TEMPLATE_MODE);
-		templateResolver.setSuffix(TEMPLATE_SUFFIX);
-		return templateResolver;
-	}
 }
