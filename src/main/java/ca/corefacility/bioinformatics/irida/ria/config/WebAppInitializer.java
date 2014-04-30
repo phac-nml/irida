4b51839a51be43e8b5e4c7fa8c0f737d42db9446
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-04-30 08:49:25 -0500

    Configuration file for the initialization of the application.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebAppInitializer.java
@@ -0,0 +1,32 @@
+package ca.corefacility.bioinformatics.irida.ria.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+public class WebAppInitializer implements WebApplicationInitializer {
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		WebApplicationContext context = getContext();
+		servletContext.addListener(new ContextLoaderListener(context));
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(
+				context));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping(Constants.DEFAULT_MAPPING_URL);
+	}
+
+	private AnnotationConfigWebApplicationContext getContext() {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.setConfigLocation(Constants.CONFIG_LOCATION);
+		return context;
+	}
+}
