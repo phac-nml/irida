5466ff8e7fdab4842513fc5570d48c27f2ba7cbd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-01 11:46:59 -0500

    Added a check for non-production use.  Since bower injects with its relative url.  In production these fies will be minified, concatenated, renamed, have a cache buster add, and moved into the /static/js/ directory.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
@@ -1,7 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,9 +15,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan(basePackages = { "ca.corefacility.bioinformatics.irida.ria" })
 public class WebConfigurer extends WebMvcConfigurerAdapter {
+	public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+	@Autowired
+	private Environment env;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
+
+		// Bower injects need components into the index.html. For development,
+		// these files need to be served as is. In production these files will
+		// be minified, concatenated, and moved into the /static/js/ folder.
+		if (!env.acceptsProfiles(SPRING_PROFILE_PRODUCTION)) {
+			registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
+		}
 	}
 }
