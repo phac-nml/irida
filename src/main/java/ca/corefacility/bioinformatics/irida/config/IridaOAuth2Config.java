ddd792f894b587fe51489682dfacbc6082db7c28
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-05-02 15:34:09 -0500

    Removed the old oauth xml config file

--- src/main/java/ca/corefacility/bioinformatics/irida/config/IridaOAuth2Config.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/IridaOAuth2Config.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.ClientDetailsUserDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
