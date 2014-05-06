42ce002e6e47dc298e39dd32778bcd99ac5a64fa
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-06 13:57:45 -0500

    Config file for karma unit testing.

--- karma-unit.conf.js
+++ karma-unit.conf.js
@@ -1,3 +1,20 @@
-/**
- * Created by josh on 2014-05-06.
- */
+module.exports = function(config) {
+    var app_path = 'src/main/webapp',
+        test_path = 'src/test/javascript';
+    config.set({
+        files : [
+            app_path + '/bower_components/angular/angular.js',
+            app_path + '/bower_components/angular-mocks/angular-mocks.js',
+            app_path + '/scripts/app.js',
+            app_path + '/scripts/projectsPages.js',
+            test_path + '/unit/**/*.js'
+        ],
+        basePath: '',
+        frameworks: ['jasmine'],
+        reporters: ['progress'],
+        browsers: ['Chrome', 'Firefox'],
+        autoWatch: false,
+        singleRun: true,
+        colors: true
+    });
+};
