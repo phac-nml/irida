275679d21122b43435e2a00bb97b8d5ff66de9c3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-07 13:14:33 -0500

    Trying to get node installed locally

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-sudo apt-get install nodejs-legacy npm ruby-sass ruby-compass;
+# Download NodeJS
+wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-x64.tar.gz
+
+# Make a directory to put the executatble into
+mkdir ~/Applications/
+
+# Set up NodeJS
+tar -zxf node-v0.10.28-linux-x64.tar.gz
+cd node-v0.10.28-linux-x64/
+./configure --prefix=~/Applications && make && make install;
+
+# Add NodeJS to the path
+echo 'export PATH=~/Applications/bin:${PATH}' >> ~/.bashrc;
+source ~/.bashrc;
+
+# Install Sass and Compass
+sudo apt-get ruby-sass ruby-compass;
+
+# Install Grunt CLI, Protractor, Karma and Bower
 sudo npm install -g grunt-cli protractor bower;
-sudo chown -R $(whoami) ~/.npm
+
+# Install local dependencies
 npm install;
 bower install;
+
+# Update webdriver
 sudo webdriver-manager update;
