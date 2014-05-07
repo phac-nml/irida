aa6432471dc13c3943c8f4eedf27e10f895e02a4
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-07 08:39:12 -0500

    Updated install script and packages.

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install nodejs-legacy npm ruby-sass ruby-compass;
-sudo npm install -g grunt-cli protractor;
+sudo npm install -g grunt-cli protractor bower;
+sudo chown -R $(whoami) ~/.npm
 sudo npm install;
 bower install;
 sudo webdriver-manager update;
