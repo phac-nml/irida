3af20dfa861ab48a47e1e8e98f7a32a1b6cd31ed
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-12 08:28:23 -0500

    Decided bower_components does not need to be hidden in production.  It is fine to search minimized components from here, there are no secrets there!

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
@@ -1,12 +1,13 @@
 package ca.corefacility.bioinformatics.irida.ria.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
 
 /**
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
@@ -14,21 +15,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "ca.corefacility.bioinformatics.irida.ria" })
+@Import({ IridaApiServicesConfig.class, WebSecurityConfig.class, ThymeleafConfiguration.class })
 public class WebConfigurer extends WebMvcConfigurerAdapter {
 	public static final String SPRING_PROFILE_PRODUCTION = "prod";
 
-	@Autowired
-	private Environment env;
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// CSS: default location "/static/styles" during development and production.
+		// CSS: default location "/static/styles" during development and
+		// production.
 		registry.addResourceHandler("/styles/**").addResourceLocations("/static/styles/");
 		registry.addResourceHandler("/scripts/**").addResourceLocations("/static/scripts/");
-
-		// Allow bower components to be loaded during development.
-		if (!env.acceptsProfiles(SPRING_PROFILE_PRODUCTION)) {
-			registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
-		}
+		registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
 	}
 }
