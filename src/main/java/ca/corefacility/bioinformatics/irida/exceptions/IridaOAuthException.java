08c7873248e16775c7d127b9c0756823df1bbcd5
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-05-14 14:50:36 -0500

    Added comments to the exception class

--- src/main/java/ca/corefacility/bioinformatics/irida/exceptions/IridaOAuthException.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/exceptions/IridaOAuthException.java
@@ -1,24 +1,38 @@
 package ca.corefacility.bioinformatics.irida.exceptions;
 
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
+import ca.corefacility.bioinformatics.irida.model.RemoteAPIToken;
 
-public class IridaOAuthException extends RuntimeException{
+/**
+ * Exception thrown when attempting to read from a remote API via OAuth2 fails.
+ * This exception will often be thrown when a {@link RemoteAPIToken} is invalid
+ * or expired.
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
+public class IridaOAuthException extends RuntimeException {
 
 	private static final long serialVersionUID = 5281201199554307578L;
 	private RemoteAPI remoteAPI;
-	
+
 	/**
 	 * Create a new IridaOAuthException with the given message and service
-	 * @param message The message for this exception
-	 * @param service The service trying to be accessed when this exception was thrown
+	 * 
+	 * @param message
+	 *            The message for this exception
+	 * @param service
+	 *            The service trying to be accessed when this exception was
+	 *            thrown
 	 */
-	public IridaOAuthException(String message, RemoteAPI remoteAPI){
+	public IridaOAuthException(String message, RemoteAPI remoteAPI) {
 		super(message);
 		this.remoteAPI = remoteAPI;
 	}
 
 	/**
 	 * Get the service trying to be accessed when this exception was thrown
+	 * 
 	 * @return The URI of the service
 	 */
 	public RemoteAPI getRemoteAPI() {
@@ -27,7 +41,9 @@ public class IridaOAuthException extends RuntimeException{
 
 	/**
 	 * Set the service for this exception
-	 * @param service the URI of the service
+	 * 
+	 * @param service
+	 *            the URI of the service
 	 */
 	public void setRemoteAPI(RemoteAPI remoteAPI) {
 		this.remoteAPI = remoteAPI;
