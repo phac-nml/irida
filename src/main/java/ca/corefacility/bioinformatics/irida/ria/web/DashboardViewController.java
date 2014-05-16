81b699d4a9f6f9205922af3d19ef001794174a5f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-16 09:45:39 -0500

    Updated to work with new naming system.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/DashboardViewController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/DashboardViewController.java
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
  */
 @Controller
-@RequestMapping("/view/")
-public class ViewController {
-	private static final Logger logger = LoggerFactory.getLogger(ViewController.class);
+@RequestMapping("/dashboard/view/")
+public class DashboardViewController {
+	private static final Logger logger = LoggerFactory.getLogger(DashboardViewController.class);
 
-	@RequestMapping(value = "dashboard", method = RequestMethod.GET)
+	@RequestMapping(value = "main", method = RequestMethod.GET)
 	public String getDashboardView() {
 		logger.debug("Creating partial for dashboard view.");
 		return "views/dashboard";
