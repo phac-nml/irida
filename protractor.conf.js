65e671b645ef532fc50496dd261f5890f4e1254e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-16 12:40:23 -0500

    Updated to test on ubuntu

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,23 +1,11 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumArgs: ['-browserTimeout=60'],
 
     chromeOnly: false,
-
+    browserName: 'phantomjs',
     capabilities: {
-        'browserName': 'phantomjs',
-
-        /*
-         * Can be used to specify the phantomjs binary path.
-         * This can generally be ommitted if you installed phantomjs globally.
-         */
-        'phantomjs.binary.path':'./node_modules/phantomjs/bin/phantomjs',
-
-        /*
-         * Command line arugments to pass to phantomjs.
-         * Can be ommitted if no arguments need to be passed.
-         * Acceptable cli arugments: https://github.com/ariya/phantomjs/wiki/API-Reference#wiki-command-line-options
-         */
-        'phantomjs.cli.args': ['--debug=true', '--webdriver-logfile=webdriver.log', '--webdriver-loglevel=DEBUG']
+        'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs'
     },
 
     specs: ['src/test/javascript/e2e/**/*.spec.js'],
