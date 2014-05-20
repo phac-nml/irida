293048bcdf23a07b326bfe2700a722261cfc14e8
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-05-20 13:03:58 -0500

    Made the OAUthTokenRestTemplateIT more IT like

--- src/test/java/ca/corefacility/bioinformatics/irida/repositories/remote/resttemplate/OAuthTokenRestTemplateIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/repositories/remote/resttemplate/OAuthTokenRestTemplateIT.java
@@ -2,8 +2,6 @@ package ca.corefacility.bioinformatics.irida.repositories.remote.resttemplate;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
@@ -11,45 +9,79 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.web.client.MockRestServiceServer;
 
-import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
+import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiTestDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.config.processing.IridaApiTestMultithreadingConfig;
 import ca.corefacility.bioinformatics.irida.exceptions.IridaOAuthException;
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
-import ca.corefacility.bioinformatics.irida.model.RemoteAPIToken;
+import ca.corefacility.bioinformatics.irida.model.Role;
+import ca.corefacility.bioinformatics.irida.model.User;
+import ca.corefacility.bioinformatics.irida.service.RemoteAPIService;
 import ca.corefacility.bioinformatics.irida.service.RemoteAPITokenService;
 
-public class OAuthTokenRestTemplateIT {
-	private RemoteAPITokenService tokenService;
-	private OAuthTokenRestTemplate restTemplate;
-	private RemoteAPI remoteAPI;
-	private URI serviceURI;
-	private MockRestServiceServer mockServer;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.google.common.collect.ImmutableList;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiServicesConfig.class,
+		IridaApiTestDataSourceConfig.class, IridaApiTestMultithreadingConfig.class })
+@ActiveProfiles("test")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/repositories/remote/resttemplate/OAuthTokenRestTemplateIT.xml")
+@DatabaseTearDown("/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class OAuthTokenRestTemplateIT {
+	@Autowired
+	private RemoteAPITokenService tokenService;
+	@Autowired
+	private RemoteAPIService apiService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@Before
 	public void setUp() throws URISyntaxException {
-		tokenService = mock(RemoteAPITokenService.class);
-		serviceURI = new URI("http://irida.ca/service");
-		remoteAPI = new RemoteAPI(serviceURI.toString(), "a service", "clientId", "clientSecret");
-		restTemplate = new OAuthTokenRestTemplate(tokenService, remoteAPI);
+		//put the user in the security context
+		User u = new User();
+		u.setUsername("tom");
+		u.setPassword(passwordEncoder.encode("Password1"));
+		u.setSystemRole(Role.ROLE_USER);
 
-		mockServer = MockRestServiceServer.createServer(restTemplate);
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(u, "Password1",
+				ImmutableList.of(Role.ROLE_USER));
+		auth.setDetails(u);
+		SecurityContextHolder.getContext().setAuthentication(auth);
 	}
 
 	@Test
-	public void testRequest() {
-		String tokenString = "token111111";
+	public void testOAuthRequest() throws URISyntaxException {
+		RemoteAPI remoteAPI = apiService.read(1l);
+		URI serviceURI = new URI(remoteAPI.getServiceURI());
+		OAuthTokenRestTemplate restTemplate = new OAuthTokenRestTemplate(tokenService, remoteAPI);
+
+		MockRestServiceServer mockServer = MockRestServiceServer.createServer(restTemplate);
+
 		String responseMessage = "{ \"message\" : \"all good\" }";
-		RemoteAPIToken token = new RemoteAPIToken(tokenString, remoteAPI, new Date(System.currentTimeMillis() + 10000));
-		when(tokenService.getToken(remoteAPI)).thenReturn(token);
 
 		mockServer.expect(requestTo(serviceURI)).andExpect(method(HttpMethod.GET))
 				.andRespond(withSuccess(responseMessage, MediaType.APPLICATION_JSON));
@@ -61,27 +93,31 @@ public class OAuthTokenRestTemplateIT {
 	}
 
 	@Test(expected = IridaOAuthException.class)
-	public void testRequestExpiredToken() {
-		String tokenString = "token111111";
-		RemoteAPIToken token = new RemoteAPIToken(tokenString, remoteAPI, new Date(System.currentTimeMillis() - 10000));
-		when(tokenService.getToken(remoteAPI)).thenReturn(token);
+	public void testRequestWithExpiredToken() throws URISyntaxException {
+		RemoteAPI remoteAPI = apiService.read(2l);
+		URI serviceURI = new URI(remoteAPI.getServiceURI());
+		OAuthTokenRestTemplate restTemplate = new OAuthTokenRestTemplate(tokenService, remoteAPI);
 
 		restTemplate.getForEntity(serviceURI, String.class);
 	}
 
 	@Test(expected = IridaOAuthException.class)
-	public void testRequestNoToken() {
-		when(tokenService.getToken(remoteAPI)).thenThrow(new EntityNotFoundException("no token for this service"));
+	public void testRequestWithNoToken() throws URISyntaxException {
+		RemoteAPI remoteAPI = apiService.read(3l);
+		OAuthTokenRestTemplate restTemplate = new OAuthTokenRestTemplate(tokenService, remoteAPI);
+		URI serviceURI = new URI(remoteAPI.getServiceURI());
 
 		restTemplate.getForEntity(serviceURI, String.class);
 	}
 
 	@Test(expected = IridaOAuthException.class)
-	public void testBadToken() {
-		String tokenString = "token111111";
-		RemoteAPIToken token = new RemoteAPIToken(tokenString, remoteAPI, new Date(System.currentTimeMillis() + 10000));
-		when(tokenService.getToken(remoteAPI)).thenReturn(token);
+	public void testRequestWithBadToken() throws URISyntaxException {
+		RemoteAPI remoteAPI = apiService.read(1l);
+		URI serviceURI = new URI(remoteAPI.getServiceURI());
+		OAuthTokenRestTemplate restTemplate = new OAuthTokenRestTemplate(tokenService, remoteAPI);
 
+		MockRestServiceServer mockServer = MockRestServiceServer.createServer(restTemplate);
+		
 		mockServer.expect(requestTo(serviceURI)).andExpect(method(HttpMethod.GET))
 				.andRespond(withStatus(HttpStatus.UNAUTHORIZED));
 
