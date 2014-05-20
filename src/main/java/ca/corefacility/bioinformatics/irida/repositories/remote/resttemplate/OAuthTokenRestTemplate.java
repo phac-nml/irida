ab5bf3e6f5ace150b2a6feadfbd7d73842afbce2
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-05-20 12:00:22 -0500

    Removed a constructor we won't need

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/resttemplate/OAuthTokenRestTemplate.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/resttemplate/OAuthTokenRestTemplate.java
@@ -5,7 +5,7 @@ import java.net.URI;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequest;
-import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,12 +22,22 @@ import ca.corefacility.bioinformatics.irida.service.RemoteAPITokenService;
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
  *
  */
-public class OAuthTokenRestTemplate extends RestTemplate{
+public class OAuthTokenRestTemplate extends RestTemplate {
 	private RemoteAPITokenService tokenService;
-	
+
 	private RemoteAPI remoteAPI;
 	private IridaOAuthErrorHandler errorHandler = new IridaOAuthErrorHandler();
 
+	/**
+	 * Create a new OAuthTokenRestTemplate with the given
+	 * {@link RemoteAPITokenService} and connecting to the given
+	 * {@link RemoteAPI}
+	 * 
+	 * @param tokenService
+	 *            the {@link TokenService} to get OAuth2 tokens from
+	 * @param remoteAPI
+	 *            the {@link RemoteAPI} this rest template will communicate with
+	 */
 	public OAuthTokenRestTemplate(RemoteAPITokenService tokenService, RemoteAPI remoteAPI) {
 		super();
 		this.tokenService = tokenService;
@@ -36,52 +46,41 @@ public class OAuthTokenRestTemplate extends RestTemplate{
 	}
 
 	/**
-	 * Create a new OAuthTokenRestTemplate with the given {@link RemoteAPITokenService} and {@link ClientHttpRequestFactory}
-	 * @param tokenService
-	 * @param requestFactory
-	 */
-	public OAuthTokenRestTemplate(RemoteAPITokenService tokenService,ClientHttpRequestFactory requestFactory) {
-		super(requestFactory);
-		this.tokenService = tokenService;
-		this.setErrorHandler(errorHandler);
-	}
-	
-	/**
 	 * Add an OAuth token from the tokenRepository to the request
 	 */
 	@Override
 	protected ClientHttpRequest createRequest(URI uri, HttpMethod method) throws IOException {
 		RemoteAPIToken token;
-		
-		try{
+
+		try {
 			token = tokenService.getToken(remoteAPI);
-		}
-		catch(EntityNotFoundException ex){
+		} catch (EntityNotFoundException ex) {
 			logger.debug("No token found for service " + remoteAPI);
-			throw new IridaOAuthException("No token fround for service",remoteAPI,ex);
+			throw new IridaOAuthException("No token fround for service", remoteAPI, ex);
 		}
-		
-		if(token.isExpired()){
+
+		if (token.isExpired()) {
 			logger.debug("Token for service is expired " + remoteAPI);
-			throw new IridaOAuthException("Token is expired for service",remoteAPI);
+			throw new IridaOAuthException("Token is expired for service", remoteAPI);
 		}
-		
+
 		ClientHttpRequest createRequest = super.createRequest(uri, method);
 		createRequest.getHeaders().add("Authorization", "Bearer " + token.getTokenString());
-		
+
 		return createRequest;
 	}
-	
+
 	@Override
 	public ResponseErrorHandler getErrorHandler() {
 		return errorHandler;
 	}
-	
+
 	/**
 	 * Set the API this rest template will communicate with
+	 * 
 	 * @param remoteAPI
 	 */
-	public void setRemoteAPI(RemoteAPI remoteAPI){
+	public void setRemoteAPI(RemoteAPI remoteAPI) {
 		this.remoteAPI = remoteAPI;
 		errorHandler.setRemoteAPI(remoteAPI);
 	}
