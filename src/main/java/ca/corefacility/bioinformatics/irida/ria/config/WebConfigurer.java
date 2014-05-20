a67b7e25df4a75e9b82afea9990a6597aa61783f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-20 07:56:05 -0500

    Working mobile site.
    Shared login page with selection on entering site.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
@@ -1,17 +1,24 @@
 package ca.corefacility.bioinformatics.irida.ria.config;
 
 import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
-import com.google.common.collect.ImmutableMap;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerMethodArgumentResolver;
+import org.springframework.mobile.device.view.LiteDeviceDelegatingViewResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import java.util.Map;
+import java.util.List;
 
 /**
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
@@ -19,7 +26,7 @@ import java.util.Map;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "ca.corefacility.bioinformatics.irida.ria" })
-@Import({ IridaApiServicesConfig.class, WebSecurityConfig.class, ThymeleafConfiguration.class })
+@Import({ IridaApiServicesConfig.class, WebSecurityConfig.class })
 public class WebConfigurer extends WebMvcConfigurerAdapter {
 	public static final String SPRING_PROFILE_PRODUCTION = "prod";
 
@@ -32,12 +39,56 @@ public class WebConfigurer extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
 	}
 
+	@Bean
+	public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
+		return new DeviceResolverHandlerInterceptor();
+	}
+
+	@Bean
+	public SitePreferenceHandlerInterceptor sitePreferenceHandlerInterceptor() {
+		return new SitePreferenceHandlerInterceptor();
+	}
+
+	@Bean
+	public SitePreferenceHandlerMethodArgumentResolver sitePreferenceHandlerMethodArgumentResolver() {
+		return new SitePreferenceHandlerMethodArgumentResolver();
+	}
+
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/static/pages/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		return resolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	@Bean
+	public LiteDeviceDelegatingViewResolver liteDeviceDelegatingViewResolver() {
+		ThymeleafViewResolver delegate = new ThymeleafViewResolver();
+		delegate.setTemplateEngine(templateEngine());
+		delegate.setOrder(1);
+		LiteDeviceDelegatingViewResolver resolver = new LiteDeviceDelegatingViewResolver(delegate);
+		resolver.setMobilePrefix("mobile/");
+		resolver.setNormalPrefix("normal/");
+		return resolver;
+	}
+
 	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		Map<String, MediaType> mediaTypes = ImmutableMap.of(
-				"json", MediaType.APPLICATION_JSON,
-				"xml", MediaType.APPLICATION_XML);
-		configurer.ignoreAcceptHeader(false).defaultContentType(MediaType.APPLICATION_JSON).favorPathExtension(true)
-				.mediaTypes(mediaTypes);
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(deviceResolverHandlerInterceptor());
+		registry.addInterceptor(sitePreferenceHandlerInterceptor());
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(sitePreferenceHandlerMethodArgumentResolver());
 	}
 }
