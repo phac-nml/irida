4e8a5f5ddfe503fee362c41824728f19f2a7559e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-05-21 11:31:31 -0500

    Updated configuration path for protractor.

--- Gruntfile.js
+++ Gruntfile.js
@@ -263,7 +263,7 @@ module.exports = function (grunt) {
         },
         protractor: {
             options: {
-                configFile: 'protractor.conf.js', // Default config file
+                configFile: '<%= path.test %>/config/protractor.conf.js', // Default config file
                 keepAlive: true, // If false, the grunt process stops when the test fails.
                 noColor: false // If true, protractor will not use colors in its output.
             },
