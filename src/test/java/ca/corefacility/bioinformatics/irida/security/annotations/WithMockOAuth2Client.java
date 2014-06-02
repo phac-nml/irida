99886b45930bb64c27cdc58c355fadf039168236
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-06-02 13:02:00 -0500

    Comments and formatting

--- src/test/java/ca/corefacility/bioinformatics/irida/security/annotations/WithMockOAuth2Client.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/security/annotations/WithMockOAuth2Client.java
@@ -7,9 +7,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
 /**
+ * Annotation for use with {@link WithMockOAuth2SecurityContextFactory} to add a
+ * {@link OAuth2Authentication} object into the security context for junit
+ * tests.
  * 
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
  *
@@ -19,31 +23,36 @@ import org.springframework.security.test.context.support.WithSecurityContext;
 @Inherited
 @WithSecurityContext(factory = WithMockOAuth2SecurityContextFactory.class)
 public @interface WithMockOAuth2Client {
-    /**
-     * The username to be used. Note that {@link #value()} is a synonym for {@link #username()}, but if {@link #username()} is specified it will take precedence.
-     * @return
-     */
-    String username() default "";
+	/**
+	 * The username to be used. Note that {@link #value()} is a synonym for
+	 * {@link #username()}, but if {@link #username()} is specified it will take
+	 * precedence.
+	 * 
+	 * @return
+	 */
+	String username() default "";
 
-    /**
-     * The roles to use. The default is "USER". A {@link GrantedAuthority} will
-     * be created for each value within roles. Each value in roles will
-     * automatically be prefixed with "ROLE_". For example, the default will
-     * result in "ROLE_USER" being used.
-     *
-     * @return
-     */
-    String[] roles() default { "USER" };
+	/**
+	 * The roles to use. The default is "USER". A {@link GrantedAuthority} will
+	 * be created for each value within roles. Each value in roles will
+	 * automatically be prefixed with "ROLE_". For example, the default will
+	 * result in "ROLE_USER" being used.
+	 *
+	 * @return
+	 */
+	String[] roles() default { "USER" };
 
-    /**
-     * The password to be used. The default is "password".
-     * @return
-     */
-    String password() default "password";
-    
-    /**
-     * The clientId of the client being mocked
-     * @return
-     */
-    String clientId() default "client";
+	/**
+	 * The password to be used. The default is "password".
+	 * 
+	 * @return
+	 */
+	String password() default "password";
+
+	/**
+	 * The clientId of the client being mocked
+	 * 
+	 * @return
+	 */
+	String clientId() default "client";
 }
