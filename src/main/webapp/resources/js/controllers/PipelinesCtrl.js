ca081feee8a21ac44beac5620a9989f0b3de5f22
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-06-03 10:42:45 -0500

    Placeholder controller and template for pipelines.

---
+++ src/main/webapp/resources/js/controllers/PipelinesCtrl.js
@@ -0,0 +1,8 @@
+(function (define) {
+    'use strict';
+    define(['app'], function (app) {
+        app.controller('PipelinesCtrl', [function () {
+            this.message = 'Message from PipelinesCtrl';
+        }]);
+    });
+})(define);
