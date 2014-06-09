c7e790c83e44af8f32ee1ad8a3c3b9f3470d0c50
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-06-09 09:11:59 -0500

    Added the thymeleaf layout dialect to dialects.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/WebConfigurer.java
@@ -1,8 +1,12 @@
 package ca.corefacility.bioinformatics.irida.ria.config;
 
-import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
-import ca.corefacility.bioinformatics.irida.ria.thymeleaf.dialect.onsen.OnsenDialect;
-import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,10 +32,10 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
+import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.ria.thymeleaf.dialect.onsen.OnsenDialect;
+
+import com.github.mxab.thymeleaf.extras.dataattribute.dialect.DataAttributeDialect;
 
 /**
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
@@ -172,6 +176,7 @@ public class WebConfigurer extends WebMvcConfigurerAdapter {
 	private Set<IDialect> additionalDialects() {
 		Set<IDialect> dialects = new HashSet<>();
 		dialects.add(new SpringSecurityDialect());
+		dialects.add(new LayoutDialect());
 		dialects.add(new OnsenDialect());
 		dialects.add(new DataAttributeDialect());
 		return dialects;
