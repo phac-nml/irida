830326b2229ded247efeee44820079530f64af7f
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-06-09 14:20:35 -0500

    Added tests for running workflows

--- src/test/java/ca/corefacility/bioinformatics/irida/pipeline/upload/galaxy/unit/GalaxyWorkflowManagerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/pipeline/upload/galaxy/unit/GalaxyWorkflowManagerTest.java
@@ -3,6 +3,10 @@ package ca.corefacility.bioinformatics.irida.pipeline.upload.galaxy.unit;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -13,7 +17,9 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import ca.corefacility.bioinformatics.irida.exceptions.UploadException;
 import ca.corefacility.bioinformatics.irida.exceptions.WorkflowException;
+import ca.corefacility.bioinformatics.irida.exceptions.galaxy.GalaxyDatasetNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.workflow.WorkflowState;
 import ca.corefacility.bioinformatics.irida.model.workflow.WorkflowStatus;
 import ca.corefacility.bioinformatics.irida.pipeline.upload.galaxy.GalaxyHistory;
@@ -22,7 +28,13 @@ import ca.corefacility.bioinformatics.irida.pipeline.upload.galaxy.GalaxyWorkflo
 import com.github.jmchilton.blend4j.galaxy.GalaxyInstance;
 import com.github.jmchilton.blend4j.galaxy.HistoriesClient;
 import com.github.jmchilton.blend4j.galaxy.WorkflowsClient;
+import com.github.jmchilton.blend4j.galaxy.beans.Dataset;
+import com.github.jmchilton.blend4j.galaxy.beans.History;
 import com.github.jmchilton.blend4j.galaxy.beans.HistoryDetails;
+import com.github.jmchilton.blend4j.galaxy.beans.WorkflowDetails;
+import com.github.jmchilton.blend4j.galaxy.beans.WorkflowInputDefinition;
+import com.github.jmchilton.blend4j.galaxy.beans.WorkflowInputs;
+import com.github.jmchilton.blend4j.galaxy.beans.WorkflowOutputs;
 import com.sun.jersey.api.client.UniformInterfaceException;
 
 /**
@@ -37,23 +49,50 @@ public class GalaxyWorkflowManagerTest {
 	@Mock private HistoriesClient historiesClient;
 	@Mock private HistoryDetails historyDetails;
 	@Mock private WorkflowsClient workflowsClient;
+	@Mock private WorkflowDetails workflowDetails;
+	@Mock private History workflowHistory;
+	@Mock private Dataset inputDataset;
 	
 	@Mock private UniformInterfaceException uniformInterfaceException;
 	
 	private GalaxyWorkflowManager galaxyWorkflowManager;
-	
+		
 	private static final String VALID_HISTORY_ID = "1";
 	private static final String INVALID_HISTORY_ID = "2";
 	
+	private static final String VALID_WORKFLOW_ID = "1";
+	private static final String INVALID_WORKFLOW_ID = "invalid";
+	
+	private static final String VALID_INPUT_LABEL = "fastq";
+	private static final String INVALID_INPUT_LABEL = "invalid";
+	
 	private static final float delta = 0.00001f;
 	
+	private Path dataFile;
+	
+	private Map<String, WorkflowInputDefinition> workflowInputs;
+	
 	@Before
-	public void setup() {
+	public void setup() throws URISyntaxException {
 		MockitoAnnotations.initMocks(this);
 		
 		when(galaxyInstance.getHistoriesClient()).thenReturn(historiesClient);
+		when(galaxyInstance.getWorkflowsClient()).thenReturn(workflowsClient);
 		
 		galaxyWorkflowManager = new GalaxyWorkflowManager(galaxyInstance, galaxyHistory);
+		
+		dataFile = Paths.get(this.getClass().getResource("testData1.fastq")
+				.toURI());
+		
+		String workflowInputId = "1";
+		WorkflowInputDefinition worklowInput = new WorkflowInputDefinition();
+		worklowInput.setLabel(VALID_INPUT_LABEL);
+		
+		workflowInputs = new HashMap<String, WorkflowInputDefinition>();
+		workflowInputs.put(workflowInputId, worklowInput);
+		
+		when(workflowsClient.showWorkflow(VALID_WORKFLOW_ID)).thenReturn(workflowDetails);
+		when(workflowDetails.getInputs()).thenReturn(workflowInputs);
 	}
 	
 	/**
@@ -127,5 +166,64 @@ public class GalaxyWorkflowManagerTest {
 	public void testGetStatusInvalidHistory() throws WorkflowException {
 		when(historiesClient.showHistory(INVALID_HISTORY_ID)).thenThrow(uniformInterfaceException);
 		galaxyWorkflowManager.getStatusFor(INVALID_HISTORY_ID);
+	}
+	
+	/**
+	 * Tests running a single file workflow.
+	 * @throws UploadException
+	 * @throws GalaxyDatasetNotFoundException
+	 * @throws IOException
+	 * @throws WorkflowException
+	 */
+	@Test
+	public void testRunSingleFileWorkflowValid() throws UploadException, GalaxyDatasetNotFoundException, IOException, WorkflowException {
+		WorkflowOutputs workflowOutputs = new WorkflowOutputs();
+		
+		when(galaxyHistory.newHistoryForWorkflow()).thenReturn(workflowHistory);
+		when(workflowHistory.getId()).thenReturn(VALID_HISTORY_ID);
+		when(galaxyHistory.fileToHistory(dataFile, workflowHistory)).thenReturn(inputDataset);
+		when(workflowsClient.runWorkflow(any(WorkflowInputs.class))).thenReturn(workflowOutputs);
+		
+		WorkflowOutputs expectedOutputs =
+				galaxyWorkflowManager.runSingleFileWorkflow(dataFile, VALID_WORKFLOW_ID, VALID_INPUT_LABEL);
+		assertNotNull(expectedOutputs);
+	}
+	
+	/**
+	 * Tests running a workflow with an invalid input label.
+	 * @throws UploadException
+	 * @throws GalaxyDatasetNotFoundException
+	 * @throws IOException
+	 * @throws WorkflowException
+	 */
+	@Test(expected=WorkflowException.class)
+	public void testRunSingleFileWorkflowInvalidInput() throws UploadException, GalaxyDatasetNotFoundException, IOException, WorkflowException {
+		WorkflowOutputs workflowOutputs = new WorkflowOutputs();
+		
+		when(galaxyHistory.newHistoryForWorkflow()).thenReturn(workflowHistory);
+		when(workflowHistory.getId()).thenReturn(VALID_HISTORY_ID);
+		when(galaxyHistory.fileToHistory(dataFile, workflowHistory)).thenReturn(inputDataset);
+		when(workflowsClient.runWorkflow(any(WorkflowInputs.class))).thenReturn(workflowOutputs);
+		
+		galaxyWorkflowManager.runSingleFileWorkflow(dataFile, VALID_WORKFLOW_ID, INVALID_INPUT_LABEL);
+	}
+	
+	/**
+	 * Tests running a workflow with an invalid workflow id.
+	 * @throws UploadException
+	 * @throws GalaxyDatasetNotFoundException
+	 * @throws IOException
+	 * @throws WorkflowException
+	 */
+	@Test(expected=WorkflowException.class)
+	public void testRunSingleFileWorkflowInvalidWorkflowId() throws UploadException, GalaxyDatasetNotFoundException, IOException, WorkflowException {
+		WorkflowOutputs workflowOutputs = new WorkflowOutputs();
+		
+		when(galaxyHistory.newHistoryForWorkflow()).thenReturn(workflowHistory);
+		when(workflowHistory.getId()).thenReturn(VALID_HISTORY_ID);
+		when(galaxyHistory.fileToHistory(dataFile, workflowHistory)).thenReturn(inputDataset);
+		when(workflowsClient.runWorkflow(any(WorkflowInputs.class))).thenReturn(workflowOutputs);
+		
+		galaxyWorkflowManager.runSingleFileWorkflow(dataFile, INVALID_WORKFLOW_ID, VALID_INPUT_LABEL);
 	}
 }
