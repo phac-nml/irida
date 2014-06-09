c773242d0797070a49de870d22bcbf26325ce230
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-06-09 07:53:08 -0500

    Updating to use a default dashboard page for login.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/DashboardController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/DashboardController.java
@@ -2,7 +2,6 @@ package ca.corefacility.bioinformatics.irida.ria.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mobile.device.site.SitePreference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +14,12 @@ import java.security.Principal;
  * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
  */
 @Controller
-public class PageController {
-	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+public class DashboardController {
+	private static final Logger logger = LoggerFactory.getLogger(DashboardController.class);
 
-	@RequestMapping(value = "/app")
-	public String showIndex(SitePreference sitePreference, Model model, Principal principal) {
-		logger.debug("Displaying dashboard page. With site pref: {}", sitePreference);
+	@RequestMapping(value = "/dashboard")
+	public String showIndex(Model model, Principal principal) {
+		logger.debug("Displaying dashboard page");
 		String name = principal.getName();
 		model.addAttribute("name", name);
 		return "index";
