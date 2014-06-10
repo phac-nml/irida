7ecef69c101c46d9fe871a54033c1931f99bb019
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-06-09 20:37:00 -0500

    Removed adding the principal to the dashboard template.  This will be added to one of the menus.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/DashboardController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/DashboardController.java
@@ -3,10 +3,7 @@ package ca.corefacility.bioinformatics.irida.ria.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
 
 /**
  * User Login Page Controller
@@ -18,10 +15,8 @@ public class DashboardController {
 	private static final Logger logger = LoggerFactory.getLogger(DashboardController.class);
 
 	@RequestMapping(value = "/dashboard")
-	public String showIndex(Model model, Principal principal) {
+	public String showIndex() {
 		logger.debug("Displaying dashboard page");
-		String name = principal.getName();
-		model.addAttribute("name", name);
 		return "index";
 	}
 }
