c29cc142554965d80c0b4e89774eef03753f276e
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-06-10 09:13:38 -0500

    Commenting GalaxyDatasetNotFoundException

--- src/main/java/ca/corefacility/bioinformatics/irida/exceptions/galaxy/GalaxyDatasetNotFoundException.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/exceptions/galaxy/GalaxyDatasetNotFoundException.java
@@ -4,18 +4,35 @@ public class GalaxyDatasetNotFoundException extends Exception {
 
 	private static final long serialVersionUID = -9021987573289725500L;
 
+	/**
+	 * Constructs a new GalaxyDatasetNotFoundException with no information.
+	 */
 	public GalaxyDatasetNotFoundException() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Constructs a new GalaxyDatasetNotFoundException with the given message and cause.
+	 * @param message  The message explaining the error.
+	 * @param cause  The cause of this message.
+	 */
 	public GalaxyDatasetNotFoundException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Constructs a new GalaxyDatasetNotFoundException with the given message.
+	 * @param message  The message explaining the error.
+	 */
 	public GalaxyDatasetNotFoundException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructs a new GalaxyDatasetNotFoundException with the given cause.
+	 * @param cause  The cause of this error.
+	 */
+	public GalaxyDatasetNotFoundException(Throwable cause) {
+		super(cause);
 	}
 }
