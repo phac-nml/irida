a04523b51b7370b451b18ce7640521826ca2a975
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-06-09 20:41:53 -0500

    Unit tests for ajax fragment controller

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/web/AjaxFragmentControllerTest.java
@@ -0,0 +1,27 @@
+package ca.corefacility.bioinformatics.irida.ria.web;
+
+import org.junit.Test;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+import java.security.Principal;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit tests for {@link AjaxFragmentController}
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+public class AjaxFragmentControllerTest {
+
+	private AjaxFragmentController controller = new AjaxFragmentController();
+
+
+	@Test
+	public void indexPageNormal() {
+		Model model = new ExtendedModelMap();
+		Principal principal = () -> "tester";
+		assertEquals("fragments/sitemenu", controller.getSiteMenu(model, principal));
+	}
+}
