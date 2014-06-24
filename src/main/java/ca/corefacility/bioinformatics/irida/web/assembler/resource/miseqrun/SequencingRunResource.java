4a73c27fb4849b1b8ca50afd3dccd64917e26c5a
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-06-24 14:06:50 -0500

    removed the default constructor

--- src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/miseqrun/SequencingRunResource.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/assembler/resource/miseqrun/SequencingRunResource.java
@@ -5,23 +5,26 @@ import javax.xml.bind.annotation.XmlElement;
 import ca.corefacility.bioinformatics.irida.model.run.SequencingRun;
 import ca.corefacility.bioinformatics.irida.web.assembler.resource.IdentifiableResource;
 
-public class SequencingRunResource extends IdentifiableResource<SequencingRun> {
+/**
+ * Resource class for a {@link SequencingRun}. This class will be extended by
+ * individual sequencing run resource types
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
+public abstract class SequencingRunResource extends IdentifiableResource<SequencingRun> {
 
-	public SequencingRunResource(){
-		super(null);
-	}
-	
 	public SequencingRunResource(SequencingRun resource) {
 		super(resource);
 	}
 
-    @XmlElement
-    public String getDescription() {
-        return resource.getDescription();
-    }
+	@XmlElement
+	public String getDescription() {
+		return resource.getDescription();
+	}
 
-    public void setDescription(String description) {
-        resource.setDescription(description);
-    }
+	public void setDescription(String description) {
+		resource.setDescription(description);
+	}
 
 }
