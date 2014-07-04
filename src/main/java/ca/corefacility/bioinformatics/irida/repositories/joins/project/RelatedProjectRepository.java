c63e63a8e51ba15716646d7e8fd108c4ee9d03b4
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-04 09:55:29 -0500

    added method to find the child side of relationships

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/joins/project/RelatedProjectRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/joins/project/RelatedProjectRepository.java
@@ -10,11 +10,37 @@ import ca.corefacility.bioinformatics.irida.model.joins.impl.RelatedProjectJoin;
 
 /**
  * Repository for managing {@link RelatedProjectJoin}s
+ * 
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
  *
  */
-public interface RelatedProjectRepository extends CrudRepository<RelatedProjectJoin, Long>{
-	
+public interface RelatedProjectRepository extends CrudRepository<RelatedProjectJoin, Long> {
+
+	/**
+	 * Get the List of {@link Project}s that are related to this project as a
+	 * list of {@link RelatedProjectJoin}s
+	 * 
+	 * This method will return {@link RelatedProjectJoin}s where the given
+	 * project is the {@code subject} property.
+	 * 
+	 * @param project
+	 *            The Project to search from
+	 * @return A List of {@link RelatedProjectJoin}s
+	 */
 	@Query("FROM RelatedProject r WHERE r.subject=?")
-	public List<RelatedProjectJoin> getRelatedProjectsForProject(Project project); 
+	public List<RelatedProjectJoin> getRelatedProjectsForProject(Project project);
+
+	/**
+	 * Get the list of {@link Project}s that this project is related to as a
+	 * list of {@link RelatedProjectJoin}s.
+	 * 
+	 * This method will return {@link RelatedProjectJoin}s where the given
+	 * project is the {@code relatedProject} property.
+	 * 
+	 * @param project
+	 *            The project that is related to other projects
+	 * @return A List of {@link RelatedProjectJoin}s
+	 */
+	@Query("FROM RelatedProject r WHERE r.relatedProject=?")
+	public List<RelatedProjectJoin> getProjectsRelatedToProject(Project project);
 }
