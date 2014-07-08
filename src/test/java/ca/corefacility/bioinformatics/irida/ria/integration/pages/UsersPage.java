f48a4c0910832756009cbc70714af15022d968ec
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-08 12:10:16 -0500

    added basic testing for user list page

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/UsersPage.java
@@ -0,0 +1,42 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import ca.corefacility.bioinformatics.irida.ria.integration.utilities.Ajax;
+
+public class UsersPage {
+	private WebDriver driver;
+
+	public UsersPage(WebDriver driver) {
+		this.driver = driver;
+		driver.get("http://localhost:8080/users");
+		waitForAjax();
+	}
+	
+	public int usersTableSize() {
+		WebElement element = driver.findElement(By.xpath("//table[@id='usersTable']/tbody"));
+		return element.findElements(By.tagName("tr")).size();
+	}
+	
+	public void clickUsernameHeader(){
+		WebElement header = driver.findElement(By.xpath("//table[@id='usersTable']/thead/tr/th[2]"));
+		header.click();
+		waitForAjax();
+	}
+	
+	public List<WebElement> getUsernameColumn() {
+		waitForAjax();
+		return driver.findElements(By.xpath("//table[@id='projectsTable']/tbody//td[2]"));
+	}
+	
+	private void waitForAjax() {
+		Wait<WebDriver> wait = new WebDriverWait(driver, 60);
+		wait.until(Ajax.waitForAjax(60000));
+	}
+}
