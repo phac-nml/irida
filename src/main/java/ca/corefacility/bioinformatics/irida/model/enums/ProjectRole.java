511c99c05431def68e1bb0f60b5cf3a0cdaa93fe
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-08 14:29:31 -0500

    added displayName for ProjectRole

--- src/main/java/ca/corefacility/bioinformatics/irida/model/enums/ProjectRole.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/enums/ProjectRole.java
@@ -1,6 +1,8 @@
 
 package ca.corefacility.bioinformatics.irida.model.enums;
 
+import org.springframework.util.StringUtils;
+
 /**
  *
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
@@ -20,6 +22,18 @@ public enum ProjectRole {
     public String toString() {
         return code;
     }
+    
+	/**
+	 * Get the display name for this role. It will generally return a
+	 * capatalized version of "name" without the "PROJECT_" prefix.
+	 * 
+	 * @return Display name for this Role
+	 */
+	public String getDisplayName() {
+		String ROLE_PREFIX = "PROJECT_";
+		int lastIndexOf = code.lastIndexOf(ROLE_PREFIX);
+		return StringUtils.capitalize(code.substring(lastIndexOf + ROLE_PREFIX.length()).toLowerCase());
+	}
 
     public static ProjectRole fromString(String code) {
         switch (code.toUpperCase()) {
