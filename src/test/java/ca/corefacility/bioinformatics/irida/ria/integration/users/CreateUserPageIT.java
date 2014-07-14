1e202ffe351047c5a38e2b2769feb0a75948cc80
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-14 14:21:56 -0500

    added it test for create user

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/users/CreateUserPageIT.java
@@ -0,0 +1,80 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.users;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiPropertyPlaceholderConfig;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.user.CreateUserPage;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiJdbcDataSourceConfig.class,
+		IridaApiPropertyPlaceholderConfig.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@ActiveProfiles("it")
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/users/CreateUserPageIT.xml")
+@DatabaseTearDown("classpath:/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class CreateUserPageIT {
+	private WebDriver driver;
+	private CreateUserPage createPage;
+	LoginPage loginPage;
+
+	@Before
+	public void setup() {
+		driver = new PhantomJSDriver();
+		loginPage = LoginPage.to(driver);
+		loginPage.doLogin();
+
+		createPage = new CreateUserPage(driver);
+	}
+
+	@After
+	public void destroy() {
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
+	@Test
+	public void createGoodUser() {
+		createPage.createUser("tom", "tom@somwehre.com", "Password1", "Password1");
+		assertTrue(createPage.createSuccess());
+	}
+
+	@Test
+	public void createExistingUsername() {
+		createPage.createUser("mrtest", "tom@somwehre.com", "Password1", "Password1");
+		assertFalse(createPage.createSuccess());
+	}
+
+	@Test
+	public void createExistingEmail() {
+		createPage.createUser("tom", "manager@nowhere.com", "Password1", "Password1");
+		assertFalse(createPage.createSuccess());
+	}
+
+	@Test
+	public void createNoPasswordMatch() {
+		createPage.createUser("tom", "manager@nowhere.com", "Password1", "Different1");
+		assertFalse(createPage.createSuccess());
+	}
+
+}
