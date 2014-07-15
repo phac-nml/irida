b85b95d44720b74765005f0c06734ce2e69bbb74
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-07-15 11:22:13 -0500

    Controller Advice for exceptions

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ExceptionHandlerController.java
@@ -0,0 +1,25 @@
+package ca.corefacility.bioinformatics.irida.ria.web;
+
+import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Created by josh on 2014-07-15.
+ */
+@ControllerAdvice
+public class ExceptionHandlerController {
+    private static final String ERROR_PAGE = "error";
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handleResourceNotFoundException() {
+        return ERROR_PAGE;
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String handleAccessDeniedException() {
+        return ERROR_PAGE;
+    }
+}
