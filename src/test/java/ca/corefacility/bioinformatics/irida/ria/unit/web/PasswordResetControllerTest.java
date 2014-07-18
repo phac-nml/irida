b1539938d299445d69e5ebf8dc4a5fe6002d0ca9
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-18 13:09:33 -0500

    can't test constraint violation exception.  dealt with in IT

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/PasswordResetControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/PasswordResetControllerTest.java
@@ -2,17 +2,15 @@ package ca.corefacility.bioinformatics.irida.ria.unit.web;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 
 import java.util.Base64;
-
-import javax.validation.ConstraintViolationException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -114,29 +112,6 @@ public class PasswordResetControllerTest {
 	}
 
 	@Test
-	public void testSubmitPasswordViolation() {
-		User user = new User(1l, "tom", null, null, null, null, null);
-		PasswordReset passwordReset = new PasswordReset(user);
-		String resetId = passwordReset.getId();
-		String password = "Password1";
-		ExtendedModelMap model = new ExtendedModelMap();
-
-		ConstraintViolationException constraintViolationException = new ConstraintViolationException("bad password",
-				null);
-
-		when(passwordResetService.read(resetId)).thenReturn(passwordReset);
-		when(userService.changePassword(user.getId(), password)).thenThrow(constraintViolationException);
-
-		String sendNewPassword = controller.sendNewPassword(resetId, password, password, model,
-				LocaleContextHolder.getLocale());
-
-		assertEquals(PasswordResetController.PASSWORD_RESET_PAGE, sendNewPassword);
-		assertTrue(model.containsKey("errors"));
-
-		verify(passwordResetService, times(2)).read(resetId);
-	}
-
-	@Test
 	public void testGetNoLoginEmailPage() {
 		assertEquals(PasswordResetController.CREATE_RESET_PAGE, controller.noLoginResetPassword(null));
 	}
@@ -156,7 +131,6 @@ public class PasswordResetControllerTest {
 
 		verify(userService).loadUserByEmail(email);
 		verify(passwordResetService).create(any(PasswordReset.class));
-		
 		verify(emailController).sendPasswordResetLinkEmail(eq(user), any(PasswordReset.class));
 	}
 
