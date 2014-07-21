c5e451369f898d64d4a893a07fc81173c534bc93
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-07-21 14:21:34 -0500

    Project samples datatable.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/utilities/components/ProjectSamplesDataTable.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/utilities/components/ProjectSamplesDataTable.java
@@ -1,7 +1,33 @@
 package ca.corefacility.bioinformatics.irida.ria.utilities.components;
 
+import com.google.common.collect.ImmutableMap;
+
 /**
- * Created by josh on 2014-07-18.
+ * Specific {@link DataTable} for Project > Samples
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
  */
 public class ProjectSamplesDataTable extends DataTable {
+	public static final String ID = "id";
+	public static final String CREATED_DATE = "createdDate";
+	public static final String NUM_FILES = "numFiles";
+	public static final String NAME = "name";
+	// Table default sort information
+	public static final String SORT_DEFAULT_COLUMN = CREATED_DATE;
+	public static final String SORT_DEFAULT_DIRECTION = "desc";
+	private static final int NAME_COL = 2;
+	private static final int CREATED_DATE_COL = 4;
+	private static final ImmutableMap<Integer, String> COLUMN_MAP_SORT = ImmutableMap.<Integer, String> builder()
+			.put(NAME_COL, "sample.sampleName").put(CREATED_DATE_COL, CREATED_DATE).build();
+
+	/**
+	 * The the name of the table column based on the column id.
+	 * 
+	 * @param id
+	 *            The column in the table
+	 * @return String name of the column
+	 */
+	public static String getSortStringFromColumnID(int id) {
+		return COLUMN_MAP_SORT.containsKey(id) ? COLUMN_MAP_SORT.get(id) : CREATED_DATE;
+	}
 }
