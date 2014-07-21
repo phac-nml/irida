45aa8d6ebc3473d61974b84245d9642906f2023f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-07-21 14:22:26 -0500

    Updated ajax call to get project samples.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ProjectsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ProjectsController.java
@@ -6,10 +6,6 @@ import java.util.*;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
-import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
-import ca.corefacility.bioinformatics.irida.ria.utilities.components.DataTable;
-import ca.corefacility.bioinformatics.irida.ria.utilities.components.ProjectSamplesDataTable;
-import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +21,17 @@ import org.springframework.web.bind.annotation.*;
 import ca.corefacility.bioinformatics.irida.model.Project;
 import ca.corefacility.bioinformatics.irida.model.enums.ProjectRole;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
+import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectUserJoin;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.RelatedProjectJoin;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.model.user.Role;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.ria.utilities.Formats;
+import ca.corefacility.bioinformatics.irida.ria.utilities.components.ProjectSamplesDataTable;
 import ca.corefacility.bioinformatics.irida.ria.utilities.components.ProjectsDataTable;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
 
@@ -69,14 +68,15 @@ public class ProjectsController {
 	private final ProjectService projectService;
 	private final SampleService sampleService;
 	private final UserService userService;
-    private final SequenceFileService sequenceFileService;
+	private final SequenceFileService sequenceFileService;
 
 	@Autowired
-	public ProjectsController(ProjectService projectService, SampleService sampleService, UserService userService, SequenceFileService sequenceFileService) {
+	public ProjectsController(ProjectService projectService, SampleService sampleService, UserService userService,
+			SequenceFileService sequenceFileService) {
 		this.projectService = projectService;
 		this.sampleService = sampleService;
 		this.userService = userService;
-        this.sequenceFileService = sequenceFileService;
+		this.sequenceFileService = sequenceFileService;
 	}
 
 	/**
@@ -274,49 +274,40 @@ public class ProjectsController {
 	@RequestMapping(value = "/ajax/{projectId}/samples", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Map<String, Object> getAjaxProjectSamplesMap(
 			@PathVariable Long projectId,
-            final Principal principal,
-            @RequestParam(ProjectsDataTable.REQUEST_PARAM_START) Integer start,
-            @RequestParam(ProjectsDataTable.REQUEST_PARAM_LENGTH) Integer length,
-            @RequestParam(ProjectsDataTable.REQUEST_PARAM_DRAW) Integer draw,
-            @RequestParam(value = ProjectsDataTable.REQUEST_PARAM_SORT_COLUMN, defaultValue = ProjectsDataTable.SORT_DEFAULT_COLUMN) Integer sortColumn,
-            @RequestParam(value = ProjectsDataTable.REQUEST_PARAM_SORT_DIRECTION, defaultValue = ProjectsDataTable.SORT_DEFAULT_DIRECTION) String direction,
-            @RequestParam(ProjectsDataTable.REQUEST_PARAM_SEARCH_VALUE) String searchValue) {
+			@RequestParam(ProjectSamplesDataTable.REQUEST_PARAM_START) Integer start,
+			@RequestParam(ProjectSamplesDataTable.REQUEST_PARAM_LENGTH) Integer length,
+			@RequestParam(ProjectSamplesDataTable.REQUEST_PARAM_DRAW) Integer draw,
+			@RequestParam(value = ProjectSamplesDataTable.REQUEST_PARAM_SORT_COLUMN, defaultValue = ProjectSamplesDataTable.SORT_DEFAULT_COLUMN) Integer sortColumn,
+			@RequestParam(value = ProjectSamplesDataTable.REQUEST_PARAM_SORT_DIRECTION, defaultValue = ProjectSamplesDataTable.SORT_DEFAULT_DIRECTION) String direction,
+			@RequestParam(ProjectSamplesDataTable.REQUEST_PARAM_SEARCH_VALUE) String searchValue) {
         Map<String, Object> response = new HashMap<>();
-
-
         Sort.Direction sortDirection = ProjectSamplesDataTable.getSortDirection(direction);
+		String sortString = ProjectSamplesDataTable.getSortStringFromColumnID(sortColumn);
 
-        // TODO: (Josh - 2014-07-19) Figure out sorting columns
-        String sortString = "name";
-
-        // TODO: (Josh - 2014-07-19) Need to have method for ProjectSampleJoin
-//        Page<ProjectSampleJoin> sampleJoins;
-
-
-        try {
-            Project project = projectService.read(projectId);
-            Collection<Join<Project, Sample>> samples = sampleService.getSamplesForProject(project);
-            List<Join<Project, Sample>> samplesJoin = sampleService.getSamplesForProject(project);
-
-            List<Map<String, String>> samplesList = new ArrayList<>();
-            for (Join<Project, Sample> join : samplesJoin) {
-                Map<String, String> sMap = new HashMap<>();
-                Sample s = join.getObject();
-                sMap.put("id", s.getId().toString());
-                sMap.put("name", s.getLabel());
-                sMap.put("numFiles", String.valueOf(sequenceFileService.getSequenceFilesForSample(s).size()));
-                sMap.put("dateCreated", Formats.DATE.format(s.getCreatedDate()));
-                sMap.put("dateModified", s.getModifiedDate().toString());
-                samplesList.add(sMap);
-            }
-            response.put(ProjectSamplesDataTable.RESPONSE_PARAM_DATA, samplesList);
+		int pageNum = ProjectSamplesDataTable.getPageNumber(start, length);
+		try {
+			Project project = projectService.read(projectId);
+			Page<ProjectSampleJoin> page = sampleService.getSamplesForProjectWithName(project, searchValue, pageNum,
+					length, sortDirection, sortString);
+			List<Map<String, String>> samplesList = new ArrayList<>();
+   for (Join<Project, Sample> join : page) {
+				Map<String, String> sMap = new HashMap<>();
+				Sample s = join.getObject();
+                sMap.put(ProjectSamplesDataTable.ID, s.getId().toString());
+				sMap.put(ProjectSamplesDataTable.NAME, s.getLabel());
+				sMap.put(ProjectSamplesDataTable.NUM_FILES,
+						String.valueOf(sequenceFileService.getSequenceFilesForSample(s).size()));
+				sMap.put(ProjectSamplesDataTable.CREATED_DATE, Formats.DATE.format(join.getTimestamp()));
+				samplesList.add(sMap);
+			}
+			response.put(ProjectSamplesDataTable.RESPONSE_PARAM_DATA, samplesList);
             response.put(ProjectSamplesDataTable.RESPONSE_PARAM_DRAW, draw);
-            response.put(ProjectSamplesDataTable.RESPONSE_PARAM_RECORDS_FILTERED, 0); // Update when paging
-            response.put(ProjectSamplesDataTable.RESPONSE_PARAM_RECORDS_TOTAL, 0); // Update when paging
-            response.put(ProjectSamplesDataTable.RESPONSE_PARAM_SORT_COLUMN, sortColumn);
-            response.put(ProjectSamplesDataTable.RESPONSE_PARAM_SORT_DIRECTION, sortDirection);
+            response.put(ProjectSamplesDataTable.RESPONSE_PARAM_RECORDS_FILTERED, page.getTotalElements());
+			response.put(ProjectSamplesDataTable.RESPONSE_PARAM_RECORDS_TOTAL, page.getTotalElements());
+			response.put(ProjectSamplesDataTable.RESPONSE_PARAM_SORT_COLUMN, sortColumn);
+			response.put(ProjectSamplesDataTable.RESPONSE_PARAM_SORT_DIRECTION, sortDirection);
         } catch (Exception e) {
-            logger.error("Trying to get ajax samples for Project: " + projectId);
+            logger.error("Error retrieving project sample information :" + e.getLocalizedMessage() );
         }
         return response;
     }
