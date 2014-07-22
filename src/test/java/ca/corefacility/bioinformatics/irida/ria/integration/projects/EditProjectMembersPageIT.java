4c7cb8d9564c74c163e14ce0819db0e6f51b3cda
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-22 09:18:11 -0500

    added it

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/EditProjectMembersPageIT.java
@@ -0,0 +1,72 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.projects;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiPropertyPlaceholderConfig;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.EditProjectMembersPage;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+/**
+ * <p>
+ * Integration test to ensure that the Project Collaborators Page.
+ * </p>
+ * 
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiJdbcDataSourceConfig.class,
+		IridaApiPropertyPlaceholderConfig.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@ActiveProfiles("it")
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/ProjectsPageIT.xml")
+@DatabaseTearDown("classpath:/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class EditProjectMembersPageIT {
+	public static final Long PROJECT_ID = 1L;
+	private WebDriver driver;
+	private EditProjectMembersPage editMembersPage;
+
+	@Before
+	public void setUp() {
+		this.driver = new PhantomJSDriver();
+		LoginPage loginPage = LoginPage.to(driver);
+		loginPage.doLogin();
+
+		editMembersPage = new EditProjectMembersPage(driver, PROJECT_ID);
+	}
+
+	@After
+	public void destroy() {
+		if (driver != null) {
+			driver.close();
+			driver.quit();
+		}
+	}
+
+	@Test
+	public void testRemoveUser() {
+		editMembersPage.clickRemoveUserButton(2l);
+		editMembersPage.clickModialPopupButton();
+		List<String> userNames = editMembersPage.getUserNames();
+		assertEquals(1, userNames.size());
+	}
+}
