04eac3c90df31b0f109de8c73f3d267104d7abaa
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-07-23 09:51:18 -0500

    cleaning up controller

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ProjectsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ProjectsController.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
