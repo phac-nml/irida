a98abbdcdba21bcad38e1264f22d50490c978603
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-07-30 14:45:16 -0500

    Created SamplesController
    Hold everything to do with samples

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/samples/SamplesController.java
@@ -0,0 +1,62 @@
+package ca.corefacility.bioinformatics.irida.ria.web.samples;
+
+import ca.corefacility.bioinformatics.irida.model.SequenceFile;
+import ca.corefacility.bioinformatics.irida.model.joins.Join;
+import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.ria.utilities.Formats;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
+import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
+import ca.corefacility.bioinformatics.irida.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by josh on 14-07-30.
+ */
+@Controller
+@RequestMapping("/samples")
+public class SamplesController {
+	// Services
+	private final SampleService sampleService;
+	private final SequenceFileService sequenceFileService;
+
+	@Autowired
+	public SamplesController(SampleService sampleService, SequenceFileService sequenceFileService){
+		this.sampleService = sampleService;
+		this.sequenceFileService = sequenceFileService;
+	}
+
+	/**
+	 * Get a list of details about files associated with a specific sample
+	 * @param sampleId The id of the sample to find the files for.
+	 * @return A list file details.
+	 */
+	@RequestMapping(value = "/ajax/{sampleId}/files", produces = MediaType.APPLICATION_JSON_VALUE)
+	public
+	@ResponseBody
+	List<Map<String, Object>> getFilesForSample(@PathVariable Long sampleId) {
+		Sample sample = sampleService.read(sampleId);
+		List<Join<Sample, SequenceFile>> joinList = sequenceFileService.getSequenceFilesForSample(sample);
+
+		List<Map<String, Object>> response = new ArrayList<>();
+		for (Join<Sample, SequenceFile> join : joinList) {
+			SequenceFile file = join.getObject();
+			Map<String, Object> map = new HashMap<>();
+			map.put("id", file.getId().toString());
+			map.put("name", file.getLabel());
+			map.put("created", Formats.DATE.format(file.getTimestamp()));
+			response.add(map);
+		}
+		return response;
+	}
+}
---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/samples/SamplesControllerTest.java
@@ -0,0 +1,65 @@
+package ca.corefacility.bioinformatics.irida.ria.unit.web.samples;
+
+import ca.corefacility.bioinformatics.irida.model.SequenceFile;
+import ca.corefacility.bioinformatics.irida.model.joins.Join;
+import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.sample.SampleSequenceFileJoin;
+import ca.corefacility.bioinformatics.irida.ria.web.ProjectsController;
+import ca.corefacility.bioinformatics.irida.ria.web.samples.SamplesController;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
+import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
+import ca.corefacility.bioinformatics.irida.service.user.UserService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by josh on 14-07-30.
+ */
+public class SamplesControllerTest {
+
+	// Services
+	private SamplesController controller;
+	private SampleService sampleService;
+	private SequenceFileService sequenceFileService;
+
+	@Before
+	public void setUp() {
+		sampleService = mock(SampleService.class);
+		sequenceFileService = mock(SequenceFileService.class);
+		controller = new SamplesController(sampleService, sequenceFileService);
+	}
+
+	@Test
+	public void testGetFilesForSample() {
+		Sample sample = new Sample("fred");
+		sample.setId(1L);
+		List<Join<Sample, SequenceFile>> joinList = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			Path path = Paths.get("/tmp/sequence-files/fake-file" + i + ".fast");
+			SequenceFile file = new SequenceFile(path);
+			file.setId(1L + i);
+			joinList.add(new SampleSequenceFileJoin(sample, file));
+		}
+		when(sampleService.read(1L)).thenReturn(sample);
+		when(sequenceFileService.getSequenceFilesForSample(sample)).thenReturn(joinList);
+		List<Map<String, Object>> result = controller.getFilesForSample(1L);
+		assertEquals("Should have the correct number of sequence file records.", joinList.size(), result.size());
+
+		Map<String, Object> file1 = result.get(0);
+		assertTrue("File has an id", file1.containsKey("id"));
+		assertTrue("File has an name", file1.containsKey("name"));
+		assertTrue("File has an created", file1.containsKey("created"));
+	}
+}
