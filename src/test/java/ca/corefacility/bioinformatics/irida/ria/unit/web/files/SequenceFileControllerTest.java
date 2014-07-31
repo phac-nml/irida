5744c0caed25ff514790137eba459b6d3dfa5a8d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-07-31 09:15:27 -0500

    Adding unit tests for Sequence files

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/files/SequenceFileControllerTest.java
@@ -0,0 +1,11 @@
+package ca.corefacility.bioinformatics.irida.ria.unit.web.files;
+
+import org.junit.Test;
+
+/**
+ * Created by josh on 14-07-31.
+ */
+public class SequenceFileControllerTest {
+
+	@Test
+}
