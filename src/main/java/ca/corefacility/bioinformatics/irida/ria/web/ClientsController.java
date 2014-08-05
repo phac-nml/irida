5a6b5a71e0ff6d5d38ca0f7ed51a463af400ce58
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-08-05 12:16:08 -0500

    adding pages for viewing clients

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ClientsController.java
@@ -0,0 +1,124 @@
+package ca.corefacility.bioinformatics.irida.ria.web;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ca.corefacility.bioinformatics.irida.model.IridaClientDetails;
+import ca.corefacility.bioinformatics.irida.repositories.specification.IridaClientDetailsSpecification;
+import ca.corefacility.bioinformatics.irida.ria.utilities.Formats;
+import ca.corefacility.bioinformatics.irida.ria.utilities.components.DataTable;
+import ca.corefacility.bioinformatics.irida.service.IridaClientDetailsService;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Controller for all {@link IridaClientDetails} related views
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ */
+@Controller
+@RequestMapping(value = "/clients")
+public class ClientsController {
+
+	public static final String CLIENTS_PAGE = "clients/list";
+
+	private final IridaClientDetailsService clientDetailsService;
+
+	private final String SORT_BY_ID = "id";
+	private final List<String> SORT_COLUMNS = Lists.newArrayList(SORT_BY_ID, "clientId", "createdDate");
+	private static final String SORT_ASCENDING = "asc";
+
+	@Autowired
+	public ClientsController(IridaClientDetailsService clientDetailsService) {
+		this.clientDetailsService = clientDetailsService;
+	}
+
+	/**
+	 * Request for the page to display a list of all clients available.
+	 * 
+	 * @return The name of the page.
+	 */
+	@RequestMapping
+	public String getClientsPage() {
+		return CLIENTS_PAGE;
+	}
+
+	/**
+	 * Ajax request page for getting a list of all clients
+	 * 
+	 * @param start
+	 *            The start element of the page
+	 * @param length
+	 *            The page length
+	 * @param draw
+	 *            Whether to draw the table
+	 * @param sortColumn
+	 *            The column to sort on
+	 * @param direction
+	 *            The direction of the sort
+	 * @param searchValue
+	 *            The string search value for the table
+	 * @return a Map<String,Object> for the table
+	 */
+	@RequestMapping(value = "/ajax/list", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Map<String, Object> getAjaxUserList(
+			@RequestParam(DataTable.REQUEST_PARAM_START) Integer start,
+			@RequestParam(DataTable.REQUEST_PARAM_LENGTH) Integer length,
+			@RequestParam(DataTable.REQUEST_PARAM_DRAW) Integer draw,
+			@RequestParam(value = DataTable.REQUEST_PARAM_SORT_COLUMN, defaultValue = "0") Integer sortColumn,
+			@RequestParam(value = DataTable.REQUEST_PARAM_SORT_DIRECTION, defaultValue = "asc") String direction,
+			@RequestParam(DataTable.REQUEST_PARAM_SEARCH_VALUE) String searchValue) {
+
+		String sortString;
+
+		try {
+			sortString = SORT_COLUMNS.get(sortColumn);
+		} catch (IndexOutOfBoundsException ex) {
+			sortString = SORT_BY_ID;
+		}
+
+		Sort.Direction sortDirection = direction.equals(SORT_ASCENDING) ? Sort.Direction.ASC : Sort.Direction.DESC;
+
+		int pageNum = start / length;
+
+		Page<IridaClientDetails> search = clientDetailsService.search(
+				IridaClientDetailsSpecification.searchUser(searchValue), pageNum, length, sortDirection, sortString);
+
+		List<List<String>> clientsData = new ArrayList<>();
+		for (IridaClientDetails client : search) {
+
+			Set<String> authorizedGrantTypes = client.getAuthorizedGrantTypes();
+
+			String grants = StringUtils.collectionToDelimitedString(authorizedGrantTypes, ", ");
+
+			List<String> row = new ArrayList<>();
+			row.add(client.getId().toString());
+			row.add(client.getClientId());
+			row.add(grants);
+			row.add(Formats.DATE.format(client.getTimestamp()));
+
+			clientsData.add(row);
+		}
+
+		Map<String, Object> map = new HashMap<>();
+		map.put(DataTable.RESPONSE_PARAM_DRAW, draw);
+		map.put(DataTable.RESPONSE_PARAM_RECORDS_TOTAL, search.getTotalElements());
+		map.put(DataTable.RESPONSE_PARAM_RECORDS_FILTERED, search.getTotalElements());
+
+		map.put(DataTable.RESPONSE_PARAM_DATA, clientsData);
+		return map;
+	}
+}
