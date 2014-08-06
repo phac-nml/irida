49b38b6aaf82d904b4c8d42c08f800eed3402dbf
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-08-06 13:33:43 -0500

    Holding the base url
    This needs to be set for all pages so that only one url needs to be changed if we want to test on a remote instance.

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/BasePage.java
@@ -0,0 +1,8 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.pages;
+
+/**
+ * Created by josh on 14-08-06.
+ */
+public class BasePage {
+	public static final String URL = "http://localhost:8080/";
+}
