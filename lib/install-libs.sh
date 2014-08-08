ef35c407150f2d8e266c2f8d5f30e245f526546f
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2014-08-08 13:33:28 -0500

    No need to create and modify existing pom files for dependencies if we can just specify a settings file for maven.

--- lib/install-libs.sh
+++ lib/install-libs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=jbzip2-0.9.jar -DpomFile=jbzip2-0.9.pom -DcreateChecksum=true
-mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=sam-1.32.jar -DpomFile=sam-1.32.pom -DcreateChecksum=true
+mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=jbzip2-0.9.jar -DgroupId=org.itadaki.bzip2 -DartifactId=jbzip2 -Dversion=0.9 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
+mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=sam-1.32.jar -DgroupId=net.sf.samtools -DartifactId=samtools -Dversion=1.32 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=fastqc-0.10.1.jar -Dsources=fastqc-0.10.1-src.jar -DpomFile=fastqc-0.10.1.pom -DcreateChecksum=true
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=galaxybootstrap-0.3.0-rc4-SNAPSHOT.jar -DpomFile=galaxybootstrap-0.3.0-rc4-SNAPSHOT.pom -DcreateChecksum=true
