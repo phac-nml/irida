0640e96edbcdc0a8941261701afc82cda44ae6e8
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-08-13 14:23:58 -0500

    Gettng ready to test.

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/files/ReferenceFileControllerTest.java
@@ -0,0 +1,19 @@
+package ca.corefacility.bioinformatics.irida.ria.unit.web.files;
+
+import ca.corefacility.bioinformatics.irida.ria.web.files.ReferenceFileController;
+
+/**
+ * Unit Tests for @{link ReferenceFileController}
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+public class ReferenceFileControllerTest {
+	// Controller
+	private ReferenceFileController controller;
+
+	// Services
+
+	/***********************************************************************************************
+	 * AJAX TESTS
+	 ***********************************************************************************************/
+}
