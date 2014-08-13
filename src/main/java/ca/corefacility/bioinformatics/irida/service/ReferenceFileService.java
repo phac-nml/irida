353d5f617047167514ee68181502ddeb8a9cbefa
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-08-13 15:07:33 -0500

    added permission for reading reference file

--- src/main/java/ca/corefacility/bioinformatics/irida/service/ReferenceFileService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/ReferenceFileService.java
@@ -1,14 +1,24 @@
 package ca.corefacility.bioinformatics.irida.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 
+import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
+import ca.corefacility.bioinformatics.irida.exceptions.InvalidPropertyException;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.project.ReferenceFile;
 
 public interface ReferenceFileService extends CRUDService<Long, ReferenceFile> {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN') or hasPermission(#id, 'canReadReferenceFile')")
+	public ReferenceFile read(Long id) throws EntityNotFoundException;
+
 	/**
 	 * Get the collection of {@link ReferenceFile} attached to the specified
 	 * {@link Project}.
@@ -20,4 +30,10 @@ public interface ReferenceFileService extends CRUDService<Long, ReferenceFile> {
 	 */
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasPermission(#project, 'canReadProject')")
 	public List<Join<Project, ReferenceFile>> getReferenceFilesForProject(Project project);
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@PreAuthorize("hasRole('ROLE_ADMIN') or hasPermission(#id, 'canReadReferenceFile')")
+	public ReferenceFile update(Long id, Map<String, Object> updatedFields) throws InvalidPropertyException;
 }
--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/ReferenceFileServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/ReferenceFileServiceImpl.java
@@ -1,12 +1,17 @@
 package ca.corefacility.bioinformatics.irida.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ca.corefacility.bioinformatics.irida.exceptions.EntityExistsException;
+import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
+import ca.corefacility.bioinformatics.irida.exceptions.InvalidPropertyException;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.project.ReferenceFile;
@@ -35,7 +40,40 @@ public class ReferenceFileServiceImpl extends CRUDServiceImpl<Long, ReferenceFil
 	 * {@inheritDoc}
 	 */
 	@Override
+	public ReferenceFile read(Long id) throws EntityNotFoundException {
+		return super.read(id);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ReferenceFile update(Long id, Map<String, Object> updatedFields) throws ConstraintViolationException,
+			EntityExistsException, InvalidPropertyException {
+		return super.update(id, updatedFields);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public List<Join<Project, ReferenceFile>> getReferenceFilesForProject(Project project) {
 		return prfjRepository.findReferenceFilesForProject(project);
 	}
+
+	/**
+	 * @throws UnsupportedOperationException
+	 *             Reference files cannot be created by themselves. They must be
+	 *             created in a project.
+	 */
+	@Override
+	public ReferenceFile create(ReferenceFile object) throws ConstraintViolationException, EntityExistsException {
+		throw new UnsupportedOperationException("Reference file must be created in a project");
+	}
+
+	@Override
+	public void delete(Long id) throws EntityNotFoundException {
+		throw new UnsupportedOperationException("Reference file must be deleted from a project");
+	}
+
 }
