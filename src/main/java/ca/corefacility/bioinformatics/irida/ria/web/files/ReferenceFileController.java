a948c8f052c3532f1bcce26b1b867b9e5bdb2800
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-08-14 07:58:17 -0500

    Remove throwing EntityNotFoundException

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/files/ReferenceFileController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/files/ReferenceFileController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.project.ReferenceFile;
 import ca.corefacility.bioinformatics.irida.service.ReferenceFileService;
 
@@ -34,15 +33,10 @@ public class ReferenceFileController {
 	public void downloadReferenceFile(@PathVariable Long fileId,
 			HttpServletResponse response) throws IOException {
 		ReferenceFile file = referenceFileService.read(fileId);
-		if (file != null) {
-			Path path = file.getFile();
-			response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getLabel() + "\"");
-			Files.copy(path, response.getOutputStream());
-			response.flushBuffer();
-		} else {
-			throw new EntityNotFoundException(
-					"Cannot find reference file with id [" + fileId + "]");
-		}
+		Path path = file.getFile();
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getLabel() + "\"");
+		Files.copy(path, response.getOutputStream());
+		response.flushBuffer();
 	}
 }
 
