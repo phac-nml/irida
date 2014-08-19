1ce2b9234247fca343b1dbc45abe460d9cb4d6dd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-08-19 10:24:40 -0500

    Basic sample information

--- src/main/webapp/resources/config.rb
+++ src/main/webapp/resources/config.rb
@@ -4,9 +4,9 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+sass_dir = "../styles/"
+images_dir = "assets"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
