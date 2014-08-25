0e72b2b35aa64c8b480fc981ffaa148bb792c98c
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-08-25 16:32:39 -0500

    unit tests

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/oauth/RemoteAPIControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/oauth/RemoteAPIControllerTest.java
@@ -3,6 +3,7 @@ package ca.corefacility.bioinformatics.irida.ria.unit.web.oauth;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
@@ -10,18 +11,24 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.ui.ExtendedModelMap;
 
+import ca.corefacility.bioinformatics.irida.model.IridaClientDetails;
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
 import ca.corefacility.bioinformatics.irida.ria.utilities.components.DataTable;
+import ca.corefacility.bioinformatics.irida.ria.web.oauth.ClientsController;
 import ca.corefacility.bioinformatics.irida.ria.web.oauth.RemoteAPIController;
 import ca.corefacility.bioinformatics.irida.service.RemoteAPIService;
 
@@ -32,11 +39,15 @@ public class RemoteAPIControllerTest {
 	private RemoteAPIService remoteAPIService;
 	private MessageSource messageSource;
 
+	private Locale locale;
+
 	@Before
 	public void setUp() {
 		remoteAPIService = mock(RemoteAPIService.class);
 		messageSource = mock(MessageSource.class);
-		remoteAPIController = new RemoteAPIController(remoteAPIService,messageSource);
+		remoteAPIController = new RemoteAPIController(remoteAPIService, messageSource);
+		locale = LocaleContextHolder.getLocale();
+
 	}
 
 	@Test
@@ -74,9 +85,56 @@ public class RemoteAPIControllerTest {
 
 		assertNotNull(ajaxAPIList);
 		assertFalse(ajaxAPIList.isEmpty());
-		
+
 		List<List<String>> apiList = (List<List<String>>) ajaxAPIList.get(DataTable.RESPONSE_PARAM_DATA);
 		assertEquals(2, apiList.size());
 
+	}
+
+	@Test
+	public void testGetAddClientPage() {
+		ExtendedModelMap model = new ExtendedModelMap();
+
+		String addClientPage = remoteAPIController.getAddRemoteAPIPage(model);
+
+		assertEquals(RemoteAPIController.ADD_API_PAGE, addClientPage);
+		assertTrue(model.containsAttribute("errors"));
+	}
+
+	@Test
+	public void testPostCreateClient() {
+		RemoteAPI client = new RemoteAPI("name", "http://uri", "a description", "id", "secret");
+		client.setId(1l);
+		ExtendedModelMap model = new ExtendedModelMap();
+
+		when(remoteAPIService.create(client)).thenReturn(client);
+
+		String postCreateClient = remoteAPIController.postCreateRemoteAPI(client, model, locale);
+
+		assertEquals("redirect:/remote_api/1", postCreateClient);
+		verify(remoteAPIService).create(client);
+	}
+
+	@Test
+	public void testPostCreateClientError() {
+		RemoteAPI client = new RemoteAPI("name", "http://uri", "a description", "id", "secret");
+		client.setId(1l);
+		ExtendedModelMap model = new ExtendedModelMap();
+		Locale locale = LocaleContextHolder.getLocale();
+
+		DataIntegrityViolationException ex = new DataIntegrityViolationException("Error: "
+				+ RemoteAPI.SERVICE_URI_CONSTRAINT_NAME);
+
+		when(remoteAPIService.create(client)).thenThrow(ex);
+
+		String postCreateClient = remoteAPIController.postCreateRemoteAPI(client, model, locale);
+
+		assertEquals(RemoteAPIController.ADD_API_PAGE, postCreateClient);
+		assertTrue(model.containsAttribute("errors"));
+		@SuppressWarnings("unchecked")
+		Map<String, String> errors = (Map<String, String>) model.get("errors");
+		assertTrue(errors.containsKey("serviceURI"));
+
+		verify(remoteAPIService).create(client);
 	}
 }
