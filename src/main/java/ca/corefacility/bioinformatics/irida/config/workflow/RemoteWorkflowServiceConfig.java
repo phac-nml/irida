37ee31166d12c8e6a6d1311b04be64bb5b31a434
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-08-26 15:39:08 -0500

    Adding code to configure a workflow as the current workflow

--- src/main/java/ca/corefacility/bioinformatics/irida/config/workflow/RemoteWorkflowServiceConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/workflow/RemoteWorkflowServiceConfig.java
@@ -5,16 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import ca.corefacility.bioinformatics.irida.exceptions.ExecutionManagerConfigurationException;
 import ca.corefacility.bioinformatics.irida.exceptions.WorkflowException;
-import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.model.workflow.galaxy.phylogenomics.RemoteWorkflowPhylogenomics;
 import ca.corefacility.bioinformatics.irida.pipeline.upload.galaxy.GalaxyWorkflowService;
 import ca.corefacility.bioinformatics.irida.repositories.workflow.RemoteWorkflowRepository;
@@ -32,13 +28,7 @@ public class RemoteWorkflowServiceConfig {
 	private static final Logger logger = LoggerFactory
 			.getLogger(RemoteWorkflowServiceConfig.class);
 	
-	private static final String WORKFLOW_ID = "workflow.id";
-	private static final String WORKFLOW_CHECKSUM = "workflow.checksum";
-	private static final String SEQUENCE_INPUT_LABEL = "workflow.input.sequence.label";
-	private static final String REFERENCE_INPUT_LABEL = "workflow.input.reference.label";
-	private static final String TREE_OUTPUT_NAME = "workflow.output.tree.name";
-	private static final String MATRIX_OUTPUT_NAME = "workflow.output.matrix.name";
-	private static final String SNP_TABLE_OUTPUT_NAME = "workflow.output.snp.table.name";
+	private static final String WORKFLOW_ID = "galaxy.workflow.id";
 	
 	@Autowired
 	private Environment environment;
@@ -53,49 +43,22 @@ public class RemoteWorkflowServiceConfig {
 	 * @return A RemoteWorkflowServicePhylogenomics with a correctly implemented workflow.
 	 * @throws WorkflowException 
 	 */
-	@Bean
+	@Lazy @Bean
 	public RemoteWorkflowServicePhylogenomics remoteWorkflowServicePhylogenomics() throws WorkflowException {
+		RemoteWorkflowPhylogenomics remoteWorkflow = null;
+		
 		try {
 			String workflowId = getProperty("workflowId", WORKFLOW_ID);
-			
-			String workflowChecksum = getProperty("workflowChecksum", WORKFLOW_CHECKSUM);
-			String sequenceFileInputLabel = getProperty("sequenceFileInputLabel",SEQUENCE_INPUT_LABEL);
-			String referenceFileInputLabel = getProperty("referenceFileInputLabel",REFERENCE_INPUT_LABEL);
-			String treeName = getProperty("treeName",TREE_OUTPUT_NAME);
-			String matrixName = getProperty("matrixName",MATRIX_OUTPUT_NAME);
-			String tableName = getProperty("tableName",SNP_TABLE_OUTPUT_NAME);
-			
-			RemoteWorkflowPhylogenomics remoteWorkflow;
-			
-			User user = new User();
-			user.setUsername("admin");
-			Authentication authentication = new AnonymousAuthenticationToken("remoteWorkflowServicePhylogenomics",
-					user, AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-			
+	
 			if (remoteWorkflowRepository.exists(workflowId)) {
 				remoteWorkflow = remoteWorkflowRepository.getByType(workflowId,
 						RemoteWorkflowPhylogenomics.class);
-			} else {
-				String realWorkflowChecksum = galaxyWorkflowService.getWorkflowChecksum(workflowId);
-				logger.info("Checksum for workflow " + workflowId + " is " + realWorkflowChecksum);
-				
-				remoteWorkflow = new RemoteWorkflowPhylogenomics(workflowId,
-					workflowChecksum, sequenceFileInputLabel, referenceFileInputLabel,
-					treeName, matrixName, tableName);
-				
-				remoteWorkflow = remoteWorkflowRepository.save(remoteWorkflow);
-			}
-			
-			return new RemoteWorkflowServicePhylogenomics(remoteWorkflow);
+			}			
 		} catch (ExecutionManagerConfigurationException e) {
-			logger.error("Could not build ExecutionManagerGalaxy: " + e.getMessage());
-		} finally {
-			logger.info("Clearing context");
-			SecurityContextHolder.clearContext();
+			logger.error(e.getMessage());
 		}
 		
-		return null;
+		return new RemoteWorkflowServicePhylogenomics(remoteWorkflow);
 	}
 	
 	/**
