0ec64fd52ce589f3005835394a853d0ec444a46a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-08-27 11:02:35 -0500

    Testing for displaying sequence files.

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/samples/SampleFilesPageIT.java
@@ -0,0 +1,66 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.samples;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiPropertyPlaceholderConfig;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.BasePage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.samples.SampleFilesPage;
+
+/**
+ * <p>
+ * Integration test to ensure that the Sample Details Page.
+ * </p>
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiJdbcDataSourceConfig.class,
+		IridaApiPropertyPlaceholderConfig.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@ActiveProfiles("it")
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/samples/SamplePagesIT.xml")
+@DatabaseTearDown("classpath:/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class SampleFilesPageIT {
+	private final String SAMPLE_LABEL = "sample1";
+	private final Long SAMPLE_ID = 1L;
+	private final String FILE_NAME = "test_file.fastq";
+	private WebDriver driver;
+	private SampleFilesPage page;
+
+	@Before
+	public void setUp() {
+		driver = BasePage.initializeDriver();
+		page = new SampleFilesPage(driver);
+	}
+
+	@Test
+	public void testPageSetup() {
+		page.gotoPage(SAMPLE_ID);
+		assertTrue("Page Title contains the sample label", page.getPageTitle().contains(SAMPLE_LABEL));
+		assertEquals("Displays the correct number of sequence files", 3, page.getSequenceFileCount());
+		assertEquals("Displays the correct file name.", FILE_NAME, page.getSequenceFileName());
+	}
+
+	@After
+	public void destroy() {
+		BasePage.destroyDriver(driver);
+	}
+}
