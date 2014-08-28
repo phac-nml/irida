3d5e8086342da1cf66be032c3fd8fe0024508dad
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-08-28 14:55:39 -0500

    Modified analysis state

--- src/main/java/ca/corefacility/bioinformatics/irida/model/enums/AnalysisState.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/enums/AnalysisState.java
@@ -1,5 +1,10 @@
 package ca.corefacility.bioinformatics.irida.model.enums;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Defins a set of states that an analysis submission can be in.
  * 
@@ -11,25 +16,58 @@ public enum AnalysisState {
 	/**
 	 * Occurs when an analysis is first submitted.
 	 */
-	SUBMITTED,
+	SUBMITTED("SUBMITTED"),
 
 	/**
 	 * An analysis that is executing in the execution manager.
 	 */
-	RUNNING,
+	RUNNING("RUNNING"),
 
 	/**
 	 * An analysis that has finished running in the execution manager.
 	 */
-	FINISHED_RUNNING,
+	FINISHED_RUNNING("FINISHED_RUNNING"),
 
 	/**
 	 * An analysis that has completed and been loaded into IRIDA.
 	 */
-	COMPLETED,
+	COMPLETED("COMPLETED"),
 
 	/**
 	 * An analysis that was not successfully able to run.
 	 */
-	ERROR;
+	ERROR("ERROR");
+	
+	private static Map<String, AnalysisState> stateMap = new HashMap<>();
+	private String stateString;
+	
+	/**
+	 * Sets of a Map used to convert a string to a WorkflowState
+	 */
+	static {
+		for (AnalysisState state : AnalysisState.values()) {
+			stateMap.put(state.toString(), state);
+		}
+	}
+	
+	private AnalysisState(String stateString) {
+		this.stateString = stateString;
+	}
+	
+	/**
+	 * Given a string defining a state, converts this to a AnalysisState.
+	 * @param stateString  The string defining the state.
+	 * @return  A AnalysisState for the corresponding state.
+	 */
+	public static AnalysisState fromString(String stateString) {
+		AnalysisState state = stateMap.get(stateString);
+		checkNotNull(state, "state for string \"" + stateString + "\" does not exist");
+		
+		return state;
+	}
+	
+    @Override
+    public String toString() {
+        return stateString;
+    }
 }
