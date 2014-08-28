dd93fa587919a81d7e279748181976e7eff5aafb
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-08-28 16:46:45 -0500

    Added tests for scheduler

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/AnalysisExecutionScheduledTaskImplIT.java
@@ -0,0 +1,162 @@
+package ca.corefacility.bioinformatics.irida.service.impl.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContextTestExcecutionListener;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.config.analysis.AnalysisExecutionServiceTestConfig;
+import ca.corefacility.bioinformatics.irida.config.conditions.WindowsPlatformCondition;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiTestDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.config.pipeline.data.galaxy.NonWindowsLocalGalaxyConfig;
+import ca.corefacility.bioinformatics.irida.config.pipeline.data.galaxy.WindowsLocalGalaxyConfig;
+import ca.corefacility.bioinformatics.irida.config.processing.IridaApiTestMultithreadingConfig;
+import ca.corefacility.bioinformatics.irida.config.workflow.RemoteWorkflowServiceTestConfig;
+import ca.corefacility.bioinformatics.irida.model.enums.AnalysisState;
+import ca.corefacility.bioinformatics.irida.model.workflow.WorkflowStatus;
+import ca.corefacility.bioinformatics.irida.model.workflow.galaxy.phylogenomics.RemoteWorkflowPhylogenomics;
+import ca.corefacility.bioinformatics.irida.model.workflow.submission.galaxy.phylogenomics.AnalysisSubmissionPhylogenomics;
+import ca.corefacility.bioinformatics.irida.repositories.analysis.submission.AnalysisSubmissionRepository;
+import ca.corefacility.bioinformatics.irida.service.AnalysisExecutionGalaxyITService;
+import ca.corefacility.bioinformatics.irida.service.AnalysisExecutionScheduledTask;
+import ca.corefacility.bioinformatics.irida.service.AnalysisSubmissionService;
+import ca.corefacility.bioinformatics.irida.service.analysis.execution.galaxy.phylogenomics.impl.AnalysisExecutionServicePhylogenomics;
+import ca.corefacility.bioinformatics.irida.service.impl.AnalysisExecutionScheduledTaskImpl;
+import ca.corefacility.bioinformatics.irida.service.workflow.galaxy.phylogenomics.impl.RemoteWorkflowServicePhylogenomics;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+/**
+ * Integration tests for analysis schedulers.
+ * 
+ * @author Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {
+		IridaApiServicesConfig.class, IridaApiTestDataSourceConfig.class,
+		IridaApiTestMultithreadingConfig.class,
+		NonWindowsLocalGalaxyConfig.class, WindowsLocalGalaxyConfig.class,
+		AnalysisExecutionServiceTestConfig.class,
+		RemoteWorkflowServiceTestConfig.class })
+@ActiveProfiles("test")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DbUnitTestExecutionListener.class,
+		WithSecurityContextTestExcecutionListener.class })
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/repositories/analysis/AnalysisRepositoryIT.xml")
+@DatabaseTearDown("/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class AnalysisExecutionScheduledTaskImplIT extends
+		AnalysisExecutionGalaxyITService {
+
+	@Autowired
+	private AnalysisExecutionGalaxyITService analysisExecutionGalaxyITService;
+
+	@Autowired
+	private AnalysisSubmissionRepository analysisSubmissionRepository;
+
+	@Autowired
+	private AnalysisSubmissionService analysisSubmissionService;
+
+	@Autowired
+	private RemoteWorkflowServicePhylogenomics remoteWorkflowServicePhylogenomics;
+
+	@Autowired
+	private AnalysisExecutionServicePhylogenomics analysisExecutionServicePhylogenomics;
+
+	private AnalysisExecutionScheduledTask analysisExecutionScheduledTask;
+
+	private Path sequenceFilePath;
+	private Path referenceFilePath;
+
+	/**
+	 * Sets up variables for testing.
+	 * 
+	 * @throws URISyntaxException
+	 * @throws IOException
+	 */
+	@Before
+	public void setup() throws URISyntaxException, IOException {
+		Assume.assumeFalse(WindowsPlatformCondition.isWindows());
+
+		analysisExecutionScheduledTask = new AnalysisExecutionScheduledTaskImpl(
+				analysisSubmissionService, analysisSubmissionRepository,
+				analysisExecutionServicePhylogenomics);
+
+		Path sequenceFilePathReal = Paths
+				.get(AnalysisExecutionGalaxyITService.class.getResource(
+						"testData1.fastq").toURI());
+		Path referenceFilePathReal = Paths
+				.get(AnalysisExecutionGalaxyITService.class.getResource(
+						"testReference.fasta").toURI());
+
+		sequenceFilePath = Files.createTempFile("testData1", ".fastq");
+		Files.delete(sequenceFilePath);
+		Files.copy(sequenceFilePathReal, sequenceFilePath);
+
+		referenceFilePath = Files.createTempFile("testReference", ".fasta");
+		Files.delete(referenceFilePath);
+		Files.copy(referenceFilePathReal, referenceFilePath);
+	}
+
+	/**
+	 * Tests out successfully changing an analysis from submitted to running to
+	 * getting results
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	@WithMockUser(username = "aaron", roles = "ADMIN")
+	public void testSubmitToExecuteToResults() throws Exception {
+		RemoteWorkflowPhylogenomics remoteWorkflowUnsaved = remoteWorkflowServicePhylogenomics
+				.getCurrentWorkflow();
+
+		AnalysisSubmissionPhylogenomics analysisSubmission = analysisExecutionGalaxyITService
+				.setupSubmissionInDatabase(sequenceFilePath, referenceFilePath,
+						remoteWorkflowUnsaved);
+
+		AnalysisSubmissionPhylogenomics submission = analysisExecutionServicePhylogenomics
+				.prepareSubmission(analysisSubmission);
+		AnalysisState state = analysisSubmissionService
+				.getStateForAnalysisSubmission(submission.getRemoteAnalysisId());
+		assertEquals(AnalysisState.SUBMITTED, state);
+
+		analysisExecutionScheduledTask.executeAnalyses();
+
+		state = analysisSubmissionService
+				.getStateForAnalysisSubmission(submission.getRemoteAnalysisId());
+		assertEquals(AnalysisState.RUNNING, state);
+
+		WorkflowStatus status = analysisExecutionServicePhylogenomics
+				.getWorkflowStatus(submission);
+
+		assertValidStatus(status);
+
+		waitUntilSubmissionComplete(submission);
+
+		analysisExecutionScheduledTask.transferAnalysesResults();
+
+		state = analysisSubmissionService
+				.getStateForAnalysisSubmission(submission.getRemoteAnalysisId());
+		assertEquals(AnalysisState.COMPLETED, state);
+	}
+}
