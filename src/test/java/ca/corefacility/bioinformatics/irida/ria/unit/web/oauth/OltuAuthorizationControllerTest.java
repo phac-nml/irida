8be31313e109d854698543ac8b951c098edf2f4b
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-09-02 14:46:53 -0500

    cleaned up imports

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/oauth/OltuAuthorizationControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/oauth/OltuAuthorizationControllerTest.java
@@ -2,6 +2,10 @@ package ca.corefacility.bioinformatics.irida.ria.unit.web.oauth;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -21,13 +25,7 @@ import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
 import ca.corefacility.bioinformatics.irida.model.RemoteAPIToken;
 import ca.corefacility.bioinformatics.irida.ria.web.oauth.OltuAuthorizationController;
