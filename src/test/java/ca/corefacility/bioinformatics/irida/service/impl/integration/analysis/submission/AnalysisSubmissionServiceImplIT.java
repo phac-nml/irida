9c2b857fac2fb102677b1f85175570c7f35c729b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-09-03 13:01:38 -0500

    Integration tests

--- src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/analysis/submission/AnalysisSubmissionServiceImplIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/analysis/submission/AnalysisSubmissionServiceImplIT.java
@@ -1,10 +1,15 @@
 package ca.corefacility.bioinformatics.irida.service.impl.integration.analysis.submission;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithSecurityContextTestExcecutionListener;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,17 +19,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
 import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
 import ca.corefacility.bioinformatics.irida.config.data.IridaApiTestDataSourceConfig;
 import ca.corefacility.bioinformatics.irida.config.processing.IridaApiTestMultithreadingConfig;
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.enums.AnalysisState;
 import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmission;
+import ca.corefacility.bioinformatics.irida.repositories.specification.AnalysisSubmissionSpecification;
 import ca.corefacility.bioinformatics.irida.service.impl.analysis.submission.AnalysisSubmissionServiceImpl;
-
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DatabaseTearDown;
 
 /**
  * Tests for an analysis service.
@@ -90,5 +96,41 @@ public class AnalysisSubmissionServiceImplIT {
 	public void testSetStateForAnalysisSubmissionFailing() {
 		analysisSubmissionService.setStateForAnalysisSubmission(2l,
 				AnalysisState.RUNNING);
+	}
+
+	@Test
+	@WithMockUser(username = "aaron", roles = "ADMIN")
+	public void searchAnalyses() {
+
+		Specification<AnalysisSubmission> specification = AnalysisSubmissionSpecification.searchAnalysis(null, null,
+				null, null);
+		Page<AnalysisSubmission> paged = analysisSubmissionService.search(specification, 0, 10, Sort.Direction.ASC, "createdDate");
+		assertEquals(8, paged.getContent().size());
+
+		// Try filtering a by names
+		String name = "My";
+		specification = AnalysisSubmissionSpecification.searchAnalysis(name, null,
+				null, null);
+		paged = analysisSubmissionService.search(specification, 0, 10, Sort.Direction.ASC, "createdDate");
+		assertEquals(7, paged.getContent().size());
+
+		// Add a minDate filter
+		Date minDate = new Date(1378479662000L);
+		specification = AnalysisSubmissionSpecification.searchAnalysis(name, null, minDate, null);
+		paged = analysisSubmissionService.search(specification, 0, 10, Sort.Direction.ASC, "createdDate");
+		assertEquals(6, paged.getContent().size());
+
+		// Add a maxDate filter
+		Date maxDate = new Date(1389024062000L);
+		specification = AnalysisSubmissionSpecification.searchAnalysis(name, null, minDate, maxDate);
+		paged = analysisSubmissionService.search(specification, 0, 10, Sort.Direction.ASC, "createdDate");
+		assertEquals(5, paged.getContent().size());
+
+		// Add a state filter
+		AnalysisState state = AnalysisState.COMPLETED;
+		specification = AnalysisSubmissionSpecification.searchAnalysis(name, state, minDate, maxDate);
+		paged = analysisSubmissionService.search(specification, 0, 10, Sort.Direction.ASC, "createdDate");
+		assertEquals(1, paged.getContent().size());
+
 	}
 }
