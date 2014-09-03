331fbc5d9305b4dbeadcfc76bd49b3231c83d7c3
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-09-03 13:12:36 -0500

    added file size calculation

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/referencefile/ReferenceFileRepositoryImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/referencefile/ReferenceFileRepositoryImpl.java
@@ -1,9 +1,15 @@
 package ca.corefacility.bioinformatics.irida.repositories.referencefile;
 
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
 import javax.persistence.EntityManager;
 
+import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.io.FastaReaderHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -20,6 +26,7 @@ import ca.corefacility.bioinformatics.irida.repositories.filesystem.FilesystemSu
  */
 @Repository
 public class ReferenceFileRepositoryImpl extends FilesystemSupplementedRepositoryImpl<ReferenceFile> {
+	private static final Logger logger = LoggerFactory.getLogger(ReferenceFileRepositoryImpl.class);
 
 	@Autowired
 	public ReferenceFileRepositoryImpl(EntityManager entityManager,
@@ -32,7 +39,35 @@ public class ReferenceFileRepositoryImpl extends FilesystemSupplementedRepositor
 	 */
 	@Override
 	public ReferenceFile save(ReferenceFile entity) {
+		Long referenceFileLength = getReferenceFileLength(entity);
+		entity.setFileLength(referenceFileLength);
 		return super.saveInternal(entity);
 	}
 
+	/**
+	 * Calculate the length of a reference file in bases
+	 * 
+	 * @param entity
+	 *            The reference file object to calculate
+	 * @return The number of bases in the file
+	 */
+	private Long getReferenceFileLength(ReferenceFile entity) {
+		Long totalLength = 0l;
+
+		Path file = entity.getFile();
+		logger.trace("Calculating reference length for file: " + file);
+		try {
+			LinkedHashMap<String, DNASequence> readFastaDNASequence = FastaReaderHelper.readFastaDNASequence(file
+					.toFile());
+			for (Entry<String, DNASequence> entry : readFastaDNASequence.entrySet()) {
+				logger.trace("Calculating for sequence " + entry.getValue().getAccession());
+				int length = entry.getValue().getLength();
+				totalLength += length;
+			}
+		} catch (Exception e) {
+			logger.error("Cannot calculate reference file length", e);
+			totalLength = null;
+		}
+		return totalLength;
+	}
 }
