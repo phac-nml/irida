b88193dffd6b379c8d174e67cdfe3583b81dc494
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-09-03 15:17:34 -0500

    added tests

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/unit/util/SequenceFileUtilitiesTest.java
@@ -0,0 +1,39 @@
+package ca.corefacility.bioinformatics.irida.service.impl.unit.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.corefacility.bioinformatics.irida.service.util.SequenceFileUtilities;
+
+public class SequenceFileUtilitiesTest {
+	private SequenceFileUtilities sequenceFileUtilities;
+
+	@Before
+	public void setUp() {
+		sequenceFileUtilities = new SequenceFileUtilities();
+	}
+
+	@Test
+	public void testGetSequenceFileLength() throws URISyntaxException {
+		Path file = Paths.get(getClass().getResource(
+				"/ca/corefacility/bioinformatics/irida/service/testReference.fasta").toURI());
+		Long sequenceFileLength = sequenceFileUtilities.getSequenceFileLength(file);
+		assertEquals(new Long(4), sequenceFileLength);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetSequenceFileLengthBadFile() throws URISyntaxException, IOException {
+		Path file = Files.createTempFile("seqfile", ".fasta");
+		file.toFile().deleteOnExit();
+		Long sequenceFileLength = sequenceFileUtilities.getSequenceFileLength(file);
+		assertEquals(new Long(4), sequenceFileLength);
+	}
+}
