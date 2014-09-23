a9c35710e101f46dacb40be7d388dc0df3accae9
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-09-23 15:11:20 -0500

    comments

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/RemoteRelatedProjectRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/RemoteRelatedProjectRepository.java
@@ -7,8 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteRelatedProject;
 
+/**
+ * Repository for storing/retrieving {@link RemoteRelatedProject} objects
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
 public interface RemoteRelatedProjectRepository extends IridaJpaRepository<RemoteRelatedProject, Long> {
-	
+
 	@Query("from RemoteRelatedProject p where p.localProject = ?1")
 	public List<RemoteRelatedProject> getRelatedProjectsForProject(Project project);
 }
