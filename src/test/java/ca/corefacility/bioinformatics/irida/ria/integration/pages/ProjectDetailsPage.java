9869d476dc4c1860415af043aced6b8c6d130d8c
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-09-23 14:30:30 -0500

    moving tests out for associated projects

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/ProjectDetailsPage.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/ProjectDetailsPage.java
@@ -1,8 +1,5 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.pages;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -46,19 +43,4 @@ public class ProjectDetailsPage {
 		WebElement date = driver.findElement(By.id("project-modified"));
 		return date.getText();
 	}
-
-	public List<String> getAssociatedProjects() {
-        List<WebElement> divs = driver.findElements(By.cssSelector("#associated-projects a"));
-        return divs.stream().map(WebElement::getText).collect(Collectors.toList());
-    }
-
-	public String getProjectWithNoRights() {
-		WebElement unauthorized = driver.findElement(By.cssSelector(".unauthorized > .project-name"));
-		return unauthorized.getText();
-	}
-
-    public List<String> getProjectsWithRights() {
-        List<WebElement> authorized = driver.findElements(By.cssSelector(".authorized > .project-name"));
-        return authorized.stream().map(WebElement::getText).collect(Collectors.toList());
-    }
 }
