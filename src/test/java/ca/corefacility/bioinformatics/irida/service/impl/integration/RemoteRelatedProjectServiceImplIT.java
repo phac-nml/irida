316a4fd9d06aaedad98e2d4c022ca095b84adeb5
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-09-26 15:00:29 -0500

    adding ITs

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/RemoteRelatedProjectServiceImplIT.java
@@ -0,0 +1,55 @@
+package ca.corefacility.bioinformatics.irida.service.impl.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContextTestExcecutionListener;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiTestDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.config.processing.IridaApiTestMultithreadingConfig;
+import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
+import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.remote.RemoteRelatedProject;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.RemoteAPIService;
+import ca.corefacility.bioinformatics.irida.service.RemoteRelatedProjectService;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiServicesConfig.class,
+		IridaApiTestDataSourceConfig.class, IridaApiTestMultithreadingConfig.class })
+@ActiveProfiles("test")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class,
+		WithSecurityContextTestExcecutionListener.class })
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/service/impl/RemoteRelatedProjectServiceImplIT.xml")
+@DatabaseTearDown("/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class RemoteRelatedProjectServiceImplIT {
+	@Autowired
+	RemoteRelatedProjectService relatedProjectService;
+
+	@Autowired
+	ProjectService projectService;
+
+	@Autowired
+	RemoteAPIService apiService;
+
+	@WithMockUser(username = "tom", roles = "USER")
+	@Test
+	public void testSave() {
+		Project read = projectService.read(1l);
+		RemoteAPI read2 = apiService.read(1l);
+		RemoteRelatedProject remoteRelatedProject = new RemoteRelatedProject(read, read2, "http://nowhere/projects/2");
+		relatedProjectService.create(remoteRelatedProject);
+	}
+}
