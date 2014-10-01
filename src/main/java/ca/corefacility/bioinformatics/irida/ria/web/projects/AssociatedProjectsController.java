56b2444214de81b585ff1ba54e3d015befcb6a3e
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-01 16:41:59 -0500

    getting intial page listing

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/AssociatedProjectsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/AssociatedProjectsController.java
@@ -2,23 +2,38 @@ package ca.corefacility.bioinformatics.irida.ria.web.projects;
 
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.format.Formatter;
+import org.springframework.format.datetime.DateFormatter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.ImmutableMap;
 
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
+import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectUserJoin;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.RelatedProjectJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteRelatedProject;
 import ca.corefacility.bioinformatics.irida.model.user.Role;
 import ca.corefacility.bioinformatics.irida.model.user.User;
+import ca.corefacility.bioinformatics.irida.repositories.specification.ProjectSpecification;
+import ca.corefacility.bioinformatics.irida.repositories.specification.ProjectUserJoinSpecification;
+import ca.corefacility.bioinformatics.irida.ria.utilities.components.ProjectsAdminDataTable;
+import ca.corefacility.bioinformatics.irida.ria.utilities.components.ProjectsDataTable;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.RemoteRelatedProjectService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
@@ -37,6 +52,11 @@ public class AssociatedProjectsController {
 	private final ProjectControllerUtils projectControllerUtils;
 	private final UserService userService;
 
+	private final Formatter<Date> dateFormatter;
+
+	private Map<Integer, String> SORT_COLUMNS = ImmutableMap
+			.of(1, "id", 2, "name", 3, "organism", 4, "createdDate");
+
 	@Autowired
 	public AssociatedProjectsController(RemoteRelatedProjectService remoteRelatedProjectService,
 			ProjectService projectService, ProjectControllerUtils projectControllerUtils, UserService userService) {
@@ -45,6 +65,7 @@ public class AssociatedProjectsController {
 		this.projectService = projectService;
 		this.projectControllerUtils = projectControllerUtils;
 		this.userService = userService;
+		dateFormatter = new DateFormatter();
 	}
 
 	/**
@@ -85,10 +106,51 @@ public class AssociatedProjectsController {
 	public String editAssociatedProjectsForProject(@PathVariable Long projectId, Model model, Principal principal) {
 		Project project = projectService.read(projectId);
 		model.addAttribute("project", project);
-		
 
 		projectControllerUtils.getProjectTemplateDetails(model, principal, project);
 		return EDIT_ASSOCIATED_PROJECTS_PAGE;
+	}
+
+	@RequestMapping("/{projectId}/associated/ajax/available")
+	@ResponseBody
+	public Map<String, Object> getPotentialAssociatedProjects(
+			@PathVariable Long projectId,
+			final Principal principal,
+			@RequestParam Integer page,
+			@RequestParam Integer count,
+			@RequestParam String sortedBy,
+			@RequestParam String sortDir,
+			@RequestParam(value="search", required=false, defaultValue="") String projectName) {
+		//Project project = projectService.read(projectId);
+		
+		User loggedInUser = userService.getUserByUsername(principal.getName());
+		Sort.Direction sortDirection = sortDir.equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+		
+
+		boolean isAdmin = loggedInUser.getSystemRole().equals(Role.ROLE_ADMIN);
+		List<Project> projects;
+		long totalElements;
+		int totalPages;
+		if (isAdmin) {
+			String sortString = sortedBy;
+			Page<Project> search = projectService.search(ProjectSpecification.searchProjectName(projectName), page, count, sortDirection, sortString);
+			totalElements = search.getTotalElements();
+			totalPages = search.getTotalPages();
+			projects = search.getContent();
+		} else {
+			String sortString = "project." + sortedBy;
+			Page<ProjectUserJoin> searchProjectUsers = projectService.searchProjectUsers(ProjectUserJoinSpecification.searchProjectNameWithUser(projectName, loggedInUser), page, count, sortDirection, sortString);
+			totalElements = searchProjectUsers.getTotalElements();
+			totalPages = searchProjectUsers.getTotalPages();
+			projects = new ArrayList<>();
+			searchProjectUsers.forEach((puj) -> projects.add(puj.getSubject()));
+		}
+		
+		Map<String, Object> map = getProjectsDataMap(projects);
+		map.put("totalAssociated", totalElements);
+		map.put("totalPages", totalPages);
+
+		return map;
 	}
 
 	/**
@@ -152,5 +214,46 @@ public class AssociatedProjectsController {
 		}
 
 		return projectsByApi;
+	}
+
+	/**
+	 * Generates a map of project information for the {@link ProjectsDataTable}
+	 *
+	 * @param projectList
+	 *            a List of {@link ProjectUserJoin} for the current user.
+	 * @param draw
+	 *            property sent from {@link ProjectsDataTable} as the table to
+	 *            render information to.
+	 * @param totalElements
+	 *            Total number of elements that could go into the table.
+	 * @param sortColumn
+	 *            Column to sort by.
+	 * @param sortDirection
+	 *            Direction to sort the column
+	 * @return Map containing the information to put into the
+	 *         {@link ProjectsDataTable}
+	 */
+	private Map<String, Object> getProjectsDataMap(Iterable<Project> projectList) {
+		Map<String, Object> map = new HashMap<>();
+
+		// Create the format required by DataTable
+		List<Map<String, String>> projectsData = new ArrayList<>();
+		for (Project p : projectList) {
+			Map<String, String> convertProjectToMap = convertProjectToMap(p);
+			projectsData.add(convertProjectToMap);
+		}
+		map.put("associated", projectsData);
+		return map;
+	}
+
+	private Map<String, String> convertProjectToMap(Project project) {
+		Map<String, String> projectMap = new HashMap<>();
+
+		projectMap.put("id", project.getId().toString());
+		projectMap.put("name", project.getName());
+		projectMap.put("organism", project.getOrganism());
+		projectMap.put("createdDate", dateFormatter.print(project.getCreatedDate(), LocaleContextHolder.getLocale()));
+
+		return projectMap;
 	}
 }
