1a7ad1e61c3e3dd9ab2ffc38c0835a03a139758e
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-10-07 08:06:26 -0500

    Partial implementation
    Need to add date filters.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/specification/ProjectSampleFilterSpecification.java
@@ -0,0 +1,41 @@
+package ca.corefacility.bioinformatics.irida.repositories.specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
+
+import com.google.common.base.Strings;
+
+/**
+ * Specification for searching {@link ProjectSampleJoin} properties
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
+ */
+public class ProjectSampleFilterSpecification {
+	public static Specification<ProjectSampleJoin> searchProjectSamples(String name, String organism) {
+		return new Specification<ProjectSampleJoin>() {
+			@Override public Predicate toPredicate(Root<ProjectSampleJoin> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+				List<Predicate> predicateList = new ArrayList<>();
+				if (!Strings.isNullOrEmpty(name)) {
+					predicateList.add(criteriaBuilder.like(root.get("sample").get("sampleName"), "%" + name + "%"));
+				}
+				if (!Strings.isNullOrEmpty(organism)) {
+					predicateList.add(criteriaBuilder.like(root.get("sample").get("organism"), "%" + organism + "%"));
+				}
+				if (predicateList.size() > 0) {
+					return criteriaBuilder.and(predicateList.toArray(new Predicate[predicateList.size()]));
+				} else {
+					return null;
+				}
+			}
+		};
+	}
+}
