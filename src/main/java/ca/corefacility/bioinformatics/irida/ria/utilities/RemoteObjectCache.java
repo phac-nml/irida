7054a1500bfdd15bd46e5022c0377ca73d685bf8
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-10 15:23:09 -0500

    added simple class for caching remote objects

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/utilities/RemoteObjectCache.java
@@ -0,0 +1,37 @@
+package ca.corefacility.bioinformatics.irida.ria.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.corefacility.bioinformatics.irida.model.remote.resource.RemoteResource;
+
+public class RemoteObjectCache<Type extends RemoteResource> {
+	private static final Logger logger = LoggerFactory.getLogger(RemoteObjectCache.class);
+	Map<Integer, Type> cache;
+
+	public RemoteObjectCache() {
+		cache = new HashMap<>();
+		logger.debug("RemoteCache initialized");
+	}
+
+	public Type readResource(Integer id) {
+		logger.debug("Reading id " + id);
+		return cache.get(id);
+	}
+
+	public Integer getIdForResource(Type resource) {
+		logger.trace("Cache size is " + cache.size());
+		int hashCode = resource.hashCode();
+		if (!cache.containsKey(hashCode)) {
+			logger.trace(resource + " added to cache");
+			cache.put(hashCode, resource);
+		} else {
+			logger.trace(resource + " already in cache");
+		}
+
+		return hashCode;
+	}
+}
