b2610d7ceede9c9347ba515d1c95e106240a7ff6
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2014-10-14 11:13:21 -0500

    Added installation for blend4j

--- lib/install-libs.sh
+++ lib/install-libs.sh
@@ -3,4 +3,6 @@
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=jbzip2-0.9.jar -DgroupId=org.itadaki.bzip2 -DartifactId=jbzip2 -Dversion=0.9 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=sam-1.32.jar -DgroupId=net.sf.samtools -DartifactId=samtools -Dversion=1.32 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=fastqc-0.10.1.jar -Dsources=fastqc-0.10.1-src.jar -DpomFile=fastqc-0.10.1.pom -DcreateChecksum=true
-mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=galaxybootstrap-0.3.0-rc4-SNAPSHOT.jar -DpomFile=galaxybootstrap-0.3.0-rc4-SNAPSHOT.pom -DcreateChecksum=true
+
+# Git commit 1167ef08bbf7359d781e910ce8253fc36295f291
+mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=blend4j-0.2.0-SNAPSHOT.jar -DpomFile=blend4j-0.2.0-SNAPSHOT.pom -DcreateChecksum=true
