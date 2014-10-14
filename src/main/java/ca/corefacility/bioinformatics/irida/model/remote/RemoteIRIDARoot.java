ab068d3387b353d6c64ad3cadc37b4768f4e2e8b
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-14 10:36:29 -0500

    comments

--- src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteIRIDARoot.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteIRIDARoot.java
@@ -7,18 +7,27 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import ca.corefacility.bioinformatics.irida.model.remote.resource.RESTLink;
 import ca.corefacility.bioinformatics.irida.model.remote.resource.RemoteResource;
 
+/**
+ * The root object for an IRIDA instance. Generally used to get the rels for
+ * projects, or users for an irida instance
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
 public class RemoteIRIDARoot implements RemoteResource {
 	private List<RESTLink> links;
 
 	@Override
 	@JsonIgnore
 	public String getIdentifier() {
+		// no id exists for the root
 		throw new UnsupportedOperationException("ID not available for IRIDA root");
 	}
 
 	@Override
 	@JsonIgnore
 	public void setIdentifier(String identifier) {
+		// no id exists for the root
 		throw new UnsupportedOperationException("ID not available for IRIDA root");
 	}
 
