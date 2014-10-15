e0c6bf43ef807485e5e0ee8c1441d1db796d23a2
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-15 14:56:18 -0500

    comments

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/utilities/RemoteObjectCache.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/utilities/RemoteObjectCache.java
@@ -8,9 +8,20 @@ import org.slf4j.LoggerFactory;
 
 import ca.corefacility.bioinformatics.irida.model.remote.resource.RemoteResource;
 
+/**
+ * In memory cache for storing {@link RemoteResource}s with a retrievable
+ * identifier. This will specifically be used when a {@link RemoteResource}
+ * object needs to be displayed in a form on a page and must be able to be
+ * selected by a user.
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ * @param <Type>
+ *            The type of {@link RemoteResource} to store
+ */
 public class RemoteObjectCache<Type extends RemoteResource> {
 	private static final Logger logger = LoggerFactory.getLogger(RemoteObjectCache.class);
-	Map<Integer, Type> cache;
+	private Map<Integer, Type> cache;
 
 	public RemoteObjectCache() {
 		cache = new HashMap<>();
