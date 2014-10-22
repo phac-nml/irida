8190544254fba56abaa4d8f0f604f055f74bc621
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-22 16:05:51 -0500

    added method to search for remote samples

--- src/main/java/ca/corefacility/bioinformatics/irida/service/remote/SampleRemoteService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/remote/SampleRemoteService.java
@@ -2,10 +2,15 @@ package ca.corefacility.bioinformatics.irida.service.remote;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteProject;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteSample;
 
 public interface SampleRemoteService extends RemoteService<RemoteSample> {
-	public List<RemoteSample> getSamplesForProject(RemoteProject project, RemoteAPI  api);
+	public List<RemoteSample> getSamplesForProject(RemoteProject project, RemoteAPI api);
+
+	public Page<RemoteSample> searchSamplesForProject(RemoteProject project, RemoteAPI api, String search, int page,
+			int size);
 }
--- src/main/java/ca/corefacility/bioinformatics/irida/service/remote/impl/SampleRemoteServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/remote/impl/SampleRemoteServiceImpl.java
@@ -1,8 +1,11 @@
 package ca.corefacility.bioinformatics.irida.service.remote.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
@@ -15,6 +18,7 @@ import ca.corefacility.bioinformatics.irida.service.remote.SampleRemoteService;
 @Service
 public class SampleRemoteServiceImpl extends RemoteServiceImpl<RemoteSample> implements SampleRemoteService {
 	public static final String PROJECT_SAMPLES_REL = "project/samples";
+	public static final String SAMPLES_CACHE_NAME = "samplesForProject";
 
 	@Autowired
 	public SampleRemoteServiceImpl(SampleRemoteRepository sampleRemoteRepository,
@@ -25,8 +29,23 @@ public class SampleRemoteServiceImpl extends RemoteServiceImpl<RemoteSample> imp
 	@Override
 	public List<RemoteSample> getSamplesForProject(RemoteProject project, RemoteAPI api) {
 		String samplesHref = project.getHrefForRel(PROJECT_SAMPLES_REL);
-
 		return list(samplesHref, api);
+	}
+
+	@Override
+	public Page<RemoteSample> searchSamplesForProject(RemoteProject project, RemoteAPI api, String search, int page,
+			int size) {
+		List<RemoteSample> samplesForProject = getSamplesForProject(project, api);
+		List<RemoteSample> filtered = samplesForProject.stream()
+				.filter(s -> s.getSampleName().toLowerCase().contains(search.toLowerCase()))
+				.collect(Collectors.toList());
+
+		int from = Math.max(0, page * size);
+		int to = Math.min(filtered.size(), (page + 1) * size);
+
+		List<RemoteSample> paged = filtered.subList(from, to);
+
+		return new PageImpl<>(paged, null, filtered.size());
 	}
 
 }
