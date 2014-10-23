d6afad2b0662a615159aa60db37ffd6f6e922c40
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-10-22 21:25:43 -0500

    Clean up specification.

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/specification/ProjectSampleFilterSpecification.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/specification/ProjectSampleFilterSpecification.java
@@ -23,29 +23,27 @@ import com.google.common.base.Strings;
  */
 public class ProjectSampleFilterSpecification {
 	public static Specification<ProjectSampleJoin> searchProjectSamples(Project project, String name, String organism, Date minDate, Date maxDate) {
-		return new Specification<ProjectSampleJoin>() {
-			@Override public Predicate toPredicate(Root<ProjectSampleJoin> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-				List<Predicate> predicateList = new ArrayList<>();
-				predicateList.add(criteriaBuilder.equal(root.get("project"), project));
-				if (!Strings.isNullOrEmpty(name)) {
-					predicateList.add(criteriaBuilder.like(root.get("sample").get("sampleName"), "%" + name + "%"));
-				}
-				if (!Strings.isNullOrEmpty(organism)) {
-					predicateList.add(criteriaBuilder.like(root.get("sample").get("organism"), "%" + organism + "%"));
-				}
-				if (minDate != null) {
-					predicateList.add(criteriaBuilder.greaterThanOrEqualTo(
-							root.get("createdDate"), minDate));
-				}
-				if (maxDate != null) {
-					predicateList.add(criteriaBuilder
-							.lessThanOrEqualTo(root.get("createdDate"), maxDate));
-				}
-				if (predicateList.size() > 0) {
-					return criteriaBuilder.and(predicateList.toArray(new Predicate[predicateList.size()]));
-				} else {
-					return null;
-				}
+		return (root, criteriaQuery, criteriaBuilder) -> {
+			List<Predicate> predicateList = new ArrayList<>();
+			predicateList.add(criteriaBuilder.equal(root.get("project"), project));
+			if (!Strings.isNullOrEmpty(name)) {
+				predicateList.add(criteriaBuilder.like(root.get("sample").get("sampleName"), "%" + name + "%"));
+			}
+			if (!Strings.isNullOrEmpty(organism)) {
+				predicateList.add(criteriaBuilder.like(root.get("sample").get("organism"), "%" + organism + "%"));
+			}
+			if (minDate != null) {
+				predicateList.add(criteriaBuilder.greaterThanOrEqualTo(
+						root.get("createdDate"), minDate));
+			}
+			if (maxDate != null) {
+				predicateList.add(criteriaBuilder
+						.lessThanOrEqualTo(root.get("createdDate"), maxDate));
+			}
+			if (predicateList.size() > 0) {
+				return criteriaBuilder.and(predicateList.toArray(new Predicate[predicateList.size()]));
+			} else {
+				return null;
 			}
 		};
 	}
