a92075260e40a16a723b6f8ebbd6ca66b6d730a7
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-27 13:54:23 -0500

    set as spring repository

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/impl/SequenceFileRemoteRepositoryImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/impl/SequenceFileRemoteRepositoryImpl.java
@@ -1,6 +1,8 @@
 package ca.corefacility.bioinformatics.irida.repositories.remote.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Repository;
 
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteSequenceFile;
 import ca.corefacility.bioinformatics.irida.model.remote.resource.ListResourceWrapper;
@@ -14,6 +16,7 @@ import ca.corefacility.bioinformatics.irida.service.RemoteAPITokenService;
  * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
  *
  */
+@Repository
 public class SequenceFileRemoteRepositoryImpl extends RemoteRepositoryImpl<RemoteSequenceFile> implements
 		SequenceFileRemoteRepository {
 	private static final ParameterizedTypeReference<ListResourceWrapper<RemoteSequenceFile>> listTypeReference = new ParameterizedTypeReference<ListResourceWrapper<RemoteSequenceFile>>() {
@@ -22,6 +25,7 @@ public class SequenceFileRemoteRepositoryImpl extends RemoteRepositoryImpl<Remot
 	private static final ParameterizedTypeReference<ResourceWrapper<RemoteSequenceFile>> objectTypeReference = new ParameterizedTypeReference<ResourceWrapper<RemoteSequenceFile>>() {
 	};
 
+	@Autowired
 	public SequenceFileRemoteRepositoryImpl(RemoteAPITokenService tokenService) {
 		super(tokenService, listTypeReference, objectTypeReference);
 	}
