30badc197321b2d52e4cbafc66c1c454b66eda29
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-10-27 11:41:54 -0500

    comments

--- src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteSequenceFile.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteSequenceFile.java
@@ -6,13 +6,25 @@ import ca.corefacility.bioinformatics.irida.model.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.remote.resource.RESTLinks;
 import ca.corefacility.bioinformatics.irida.model.remote.resource.RemoteResource;
 
+/**
+ * {@link SequenceFile} object pulled from a remote IRIDA installation
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
 public class RemoteSequenceFile extends SequenceFile implements RemoteResource {
 	private RESTLinks links;
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public RESTLinks getLinks() {
 		return links;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setLinks(RESTLinks links) {
 		this.links = links;
 	}
