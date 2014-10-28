8636ecff5b66ad627d95a5c54e0aa312ee33f8a7
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-10-28 12:13:12 -0500

    Fixed imports

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/users/UserDetailsPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/users/UserDetailsPageIT.java
@@ -1,5 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.users;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 
 import org.junit.After;
@@ -22,10 +26,6 @@ import ca.corefacility.bioinformatics.irida.ria.integration.pages.user.UserDetai
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiJdbcDataSourceConfig.class,
