3185d5c090be389c3dfd4acaa498a734f9dabddc
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-10 12:11:49 -0600

    added remotesequencefilesnapshot

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/snapshot/remote/RemoteSequenceFileSnapshot.java
@@ -0,0 +1,27 @@
+package ca.corefacility.bioinformatics.irida.model.snapshot.remote;
+
+import java.nio.file.Path;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import ca.corefacility.bioinformatics.irida.model.remote.RemoteSequenceFile;
+import ca.corefacility.bioinformatics.irida.model.remote.resource.RESTLinks;
+import ca.corefacility.bioinformatics.irida.model.snapshot.SequenceFileSnapshot;
+
+@Entity
+public class RemoteSequenceFileSnapshot extends SequenceFileSnapshot implements RemoteSnapshot {
+	@OneToOne
+	private RESTLinks links;
+
+	public RemoteSequenceFileSnapshot(RemoteSequenceFile sequenceFile, Path file) {
+		super(file, sequenceFile.getOptionalProperties());
+		this.links = sequenceFile.getLinks();
+	}
+
+	@Override
+	public RESTLinks getLinks() {
+		return links;
+	}
+
+}
