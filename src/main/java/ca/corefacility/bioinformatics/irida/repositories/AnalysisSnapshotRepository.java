968363b042a0d9043fadf9ded803c82cde7fbeaa
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-10 14:36:40 -0600

    removed old...

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/AnalysisSnapshotRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.corefacility.bioinformatics.irida.repositories;
-
-import ca.corefacility.bioinformatics.irida.model.snapshot.AnalysisSnapshot;
-
-public interface AnalysisSnapshotRepository extends IridaJpaRepository<AnalysisSnapshot, Long> {
-
-}
