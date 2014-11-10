e81f19be4f2c8faa41a36087370e70610c4333fa
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-10 14:36:20 -0600

    renamed analysissnapshot to snapshot

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/SnapshotRepository.java
@@ -0,0 +1,7 @@
+package ca.corefacility.bioinformatics.irida.repositories;
+
+import ca.corefacility.bioinformatics.irida.model.snapshot.Snapshot;
+
+public interface SnapshotRepository extends IridaJpaRepository<Snapshot, Long> {
+
+}
