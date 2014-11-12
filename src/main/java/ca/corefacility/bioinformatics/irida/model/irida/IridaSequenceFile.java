7560916e965173f41ac4b0ed789f912ac2df9dce
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-12 10:31:35 -0600

    added comments

--- src/main/java/ca/corefacility/bioinformatics/irida/model/irida/IridaSequenceFile.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/irida/IridaSequenceFile.java
@@ -3,10 +3,26 @@ package ca.corefacility.bioinformatics.irida.model.irida;
 import java.nio.file.Path;
 import java.util.Map;
 
+/**
+ * Describes fields that must be made available for a Sequence File in IRIDA
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
 public interface IridaSequenceFile {
 
+	/**
+	 * Get the location on the file system where the sequence file is stored
+	 * 
+	 * @return
+	 */
 	public Path getFile();
 
+	/**
+	 * Get a map of optional key/value pair properties
+	 * 
+	 * @return
+	 */
 	public Map<String, String> getOptionalProperties();
 
 }
