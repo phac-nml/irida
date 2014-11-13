6c7bee9c545cf1e2d24492115a8466c930e0eb5a
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-13 10:00:01 -0600

    using default table names for remote snapshot classes

--- src/main/java/ca/corefacility/bioinformatics/irida/model/snapshot/remote/RemoteSequenceFileSnapshot.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/snapshot/remote/RemoteSequenceFileSnapshot.java
@@ -4,14 +4,12 @@ import java.nio.file.Path;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteSequenceFile;
 import ca.corefacility.bioinformatics.irida.model.remote.resource.RESTLinks;
 import ca.corefacility.bioinformatics.irida.model.snapshot.SequenceFileSnapshot;
 
 @Entity
-@Table(name = "remote_sequence_file_snapshot")
 public class RemoteSequenceFileSnapshot extends SequenceFileSnapshot implements RemoteSnapshot {
 	@Embedded
 	private RESTLinks links;
