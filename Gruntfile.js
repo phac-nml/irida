72849ed1f2930fe8deef9a95b199efdbbfa67a49
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-11-14 13:39:57 -0600

    Added watch for changes in html for reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
   var app_path = require('./bower.json').appPath;
   grunt.initConfig({
     paths  : {
+      html: app_path + '/pages',
       scss: app_path + '/styles',
       css : app_path + '/resources/css',
       js  : app_path + '/resources/js'
@@ -18,23 +19,30 @@ module.exports = function (grunt) {
     },
     jshint : {
       options: {
-        reporter: require('jshint-stylish')
+        reporter  : require('jshint-stylish'),
+        'jshintrc': true
       },
-      target : ['<%= paths.js %>/**/*.js', '!<%= paths.js %>/**/*-min.js','!<%= paths.js %>/vendor/**/*.js']
+      target : ['<%= paths.js %>/**/*.js', '!<%= paths.js %>/**/*-min.js', '!<%= paths.js %>/vendor/**/*.js']
     },
     watch  : {
       compass: {
         files  : ['<%= paths.scss %>/**/*.scss'],
         tasks  : ['compass:dev'],
         options: {
-          livereload: true,
+          livereload: true
         }
       },
-      jshint: {
-        files : ['<%= paths.js %>/**/*.js'],
-        tasks: ['newer:jshint'],
+      html   : {
+        files :['<%= paths.html %>/**/*.html'],
         options: {
-          livereload: true,
+          livereload: true
+        }
+      },
+      jshint : {
+        files  : ['<%= paths.js %>/**/*.js'],
+        tasks  : ['newer:jshint'],
+        options: {
+          livereload: true
         }
       }
     }
