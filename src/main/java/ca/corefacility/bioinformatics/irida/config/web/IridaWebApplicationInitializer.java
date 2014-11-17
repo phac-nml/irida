936cf1e1719df7feda5ddcc21b7ccdce75021e60
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2014-11-17 11:55:10 -0600

    Clean up the servlet configuration.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/web/IridaWebApplicationInitializer.java
@@ -0,0 +1,58 @@
+package ca.corefacility.bioinformatics.irida.config.web;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+import ca.corefacility.bioinformatics.irida.web.filter.HttpHeadFilter;
+
+/**
+ * REST API initializer with security.
+ * 
+ * @author Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
+ *
+ */
+public class IridaWebApplicationInitializer extends AbstractDispatcherServletInitializer {
+
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+		// make sure that we load up the database in hibernate by default. This
+		// behaviour can be overridden by external configuration files.
+		servletContext.setInitParameter("spring.profiles.default", "dev");
+
+		// do the default setup
+		super.onStartup(servletContext);
+
+		// install the spring security filter chain.
+		final DelegatingFilterProxy filter = new DelegatingFilterProxy("springSecurityFilterChain");
+		filter.setContextAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.dispatcher");
+		servletContext.addFilter("springSecurityFilterChain", filter).addMappingForUrlPatterns(null, false, "/*");
+	}
+
+	@Override
+	public WebApplicationContext createServletApplicationContext() {
+		final AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(IridaRestApiWebConfig.class);
+		return context;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected WebApplicationContext createRootApplicationContext() {
+		return null;
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] { new HttpHeadFilter() };
+	}
+}
