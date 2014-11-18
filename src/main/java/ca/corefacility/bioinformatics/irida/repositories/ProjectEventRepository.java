0543e0d465a2566554ae0619fed112fd5d8498dc
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-18 13:26:12 -0600

    updated query method

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/ProjectEventRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/ProjectEventRepository.java
@@ -1,7 +1,38 @@
 package ca.corefacility.bioinformatics.irida.repositories;
 
-import ca.corefacility.bioinformatics.irida.model.event.ProjectEvent;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+
+import ca.corefacility.bioinformatics.irida.model.event.ProjectEvent;
+import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.user.User;
+
+/**
+ * Repository for storing events that occurred on a project
+ * 
+ * @author Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
+ *
+ */
 public interface ProjectEventRepository extends IridaJpaRepository<ProjectEvent, Long> {
 
+	/**
+	 * Get the events for a given project
+	 * 
+	 * @param project
+	 *            The project to get events for
+	 * @return A List of {@link ProjectEvent}s
+	 */
+	@Query("FROM ProjectEvent e WHERE e.project=?1")
+	public List<ProjectEvent> getEventsForProject(Project project);
+
+	/**
+	 * Get the events on all projects for a given user
+	 * 
+	 * @param user
+	 *            The {@link User} to get events for
+	 * @return A List of {@link ProjectEvent}s
+	 */
+	@Query("SELECT e FROM ProjectEvent e INNER JOIN e.project as p INNER JOIN p.users as u WHERE u.user=?1")
+	public List<ProjectEvent> getEventsForUser(User user);
 }
