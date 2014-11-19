e791b353a8ed47876b7f4da9211c6840c0bbdb85
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-19 16:19:42 -0600

    moved

--- src/main/java/ca/corefacility/bioinformatics/irida/events/annotations/AddsSampleToProject.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/events/annotations/AddsSampleToProject.java
@@ -1,4 +1,4 @@
-package ca.corefacility.bioinformatics.irida.events;
+package ca.corefacility.bioinformatics.irida.events.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
