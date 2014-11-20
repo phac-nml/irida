be3f99d439c730d2c221631083da6efba551cefe
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-20 10:19:25 -0600

    added hibernate stuff

--- src/main/java/ca/corefacility/bioinformatics/irida/model/event/UserRemovedProjectEvent.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/event/UserRemovedProjectEvent.java
@@ -1,7 +1,10 @@
 package ca.corefacility.bioinformatics.irida.model.event;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.user.User;
@@ -15,6 +18,8 @@ import ca.corefacility.bioinformatics.irida.model.user.User;
 @Entity
 @Table(name = "project_event_user_removed")
 public class UserRemovedProjectEvent extends ProjectEvent {
+	@NotNull
+	@ManyToOne(cascade = CascadeType.DETACH)
 	private User user;
 
 	public UserRemovedProjectEvent(Project project, User user) {
