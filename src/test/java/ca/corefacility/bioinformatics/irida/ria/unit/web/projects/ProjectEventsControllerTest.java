1aed2a5242af6efaa3213bb6397dfd2a95c3df20
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-11-25 15:36:31 -0600

    added unit test

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/projects/ProjectEventsControllerTest.java
@@ -0,0 +1,97 @@
+package ca.corefacility.bioinformatics.irida.ria.unit.web.projects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.ui.ExtendedModelMap;
+
+import ca.corefacility.bioinformatics.irida.model.event.ProjectEvent;
+import ca.corefacility.bioinformatics.irida.model.event.UserRoleSetProjectEvent;
+import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.user.User;
+import ca.corefacility.bioinformatics.irida.ria.web.projects.ProjectEventsController;
+import ca.corefacility.bioinformatics.irida.service.ProjectEventService;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.user.UserService;
+
+import com.google.common.collect.Lists;
+
+public class ProjectEventsControllerTest {
+	private ProjectEventService eventService;
+	private ProjectService projectService;
+	private UserService userService;
+
+	private ProjectEventsController controller;
+
+	@Before
+	public void setup() {
+		eventService = mock(ProjectEventService.class);
+		projectService = mock(ProjectService.class);
+		userService = mock(UserService.class);
+
+		controller = new ProjectEventsController(eventService, projectService, userService);
+	}
+
+	@Test
+	public void testGetRecentEventsForProject() {
+		Long projectId = 1l;
+		Project project = new Project();
+		ExtendedModelMap model = new ExtendedModelMap();
+		ProjectEvent event = new UserRoleSetProjectEvent();
+		Page<ProjectEvent> page = new PageImpl<>(Lists.newArrayList(event));
+
+		when(projectService.read(projectId)).thenReturn(project);
+		when(eventService.getEventsForProject(eq(project), any(Pageable.class))).thenReturn(page);
+
+		String recentEventsForProject = controller.getRecentEventsForProject(projectId, model);
+
+		assertEquals(ProjectEventsController.EVENTS_VIEW, recentEventsForProject);
+		assertTrue(model.containsAttribute("events"));
+		@SuppressWarnings("unchecked")
+		List<Map<String, Object>> events = (List<Map<String, Object>>) model.get("events");
+		assertEquals(1, events.size());
+		Map<String, Object> next = events.iterator().next();
+		assertTrue(next.containsKey("name"));
+		assertTrue(next.containsKey("event"));
+		assertEquals(ProjectEventsController.FRAGMENT_NAMES.get(event.getClass()), next.get("name"));
+		assertEquals(event, next.get("event"));
+	}
+
+	@Test
+	public void testGetRecentEventsForUser() {
+		ExtendedModelMap model = new ExtendedModelMap();
+		ProjectEvent event = new UserRoleSetProjectEvent();
+		Page<ProjectEvent> page = new PageImpl<>(Lists.newArrayList(event));
+		User user = new User();
+		Principal principal = () -> "username";
+
+		when(userService.getUserByUsername(principal.getName())).thenReturn(user);
+		when(eventService.getEventsForUser(eq(user), any(Pageable.class))).thenReturn(page);
+
+		String recentEventsForProject = controller.getRecentEventsForUser(model, principal);
+
+		assertEquals(ProjectEventsController.EVENTS_VIEW, recentEventsForProject);
+		assertTrue(model.containsAttribute("events"));
+		@SuppressWarnings("unchecked")
+		List<Map<String, Object>> events = (List<Map<String, Object>>) model.get("events");
+		assertEquals(1, events.size());
+		Map<String, Object> next = events.iterator().next();
+		assertTrue(next.containsKey("name"));
+		assertTrue(next.containsKey("event"));
+		assertEquals(ProjectEventsController.FRAGMENT_NAMES.get(event.getClass()), next.get("name"));
+		assertEquals(event, next.get("event"));
+	}
+}
