cc02a45e7bf3eab2a0ba58701b37b84768442da6
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-11-27 13:50:34 -0600

    Added message if file size cannot be read

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectReferenceFileController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectReferenceFileController.java
@@ -6,11 +6,13 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +38,7 @@ public class ProjectReferenceFileController {
 	private static final Logger logger = LoggerFactory.getLogger(ProjectReferenceFileController.class);
 	private final ProjectService projectService;
 	private final ReferenceFileService referenceFileService;
+	private final MessageSource messageSource;
 
 	/*
 	 * Converters
@@ -43,17 +46,19 @@ public class ProjectReferenceFileController {
 	private FileSizeConverter fileSizeConverter;
 
 	@Autowired
-	public ProjectReferenceFileController(ProjectService projectService, ReferenceFileService referenceFileService) {
+	public ProjectReferenceFileController(ProjectService projectService, ReferenceFileService referenceFileService,
+			MessageSource messageSource) {
 		this.projectService = projectService;
 		this.referenceFileService = referenceFileService;
+		this.messageSource = messageSource;
 		this.fileSizeConverter = new FileSizeConverter();
 	}
 
 	@RequestMapping("/all")
-	public @ResponseBody Map<String, Object> getReferenceFilesForProject(@PathVariable Long projectId) {
+	public @ResponseBody Map<String, Object> getReferenceFilesForProject(@PathVariable Long projectId, Locale locale) {
 		Project project = projectService.read(projectId);
 		// Let's add the reference files
-		List<Map<String, Object>> files = getReferenceFileData(project);
+		List<Map<String, Object>> files = getReferenceFileData(project, locale);
 		return ImmutableMap.of("files", files);
 	}
 
@@ -64,9 +69,8 @@ public class ProjectReferenceFileController {
 	 * 		{@link Project} Currently viewed project.
 	 *
 	 * @return List of reference file info.
-	 * @throws IOException
 	 */
-	private List<Map<String, Object>> getReferenceFileData(Project project) {
+	private List<Map<String, Object>> getReferenceFileData(Project project, Locale locale) {
 		List<Join<Project, ReferenceFile>> joinList = referenceFileService.getReferenceFilesForProject(project);
 		List<Map<String, Object>> mapList = new ArrayList<>();
 		for (Join<Project, ReferenceFile> join : joinList) {
@@ -81,11 +85,13 @@ public class ProjectReferenceFileController {
 				try {
 					size = Files.size(path);
 					map.put("size", fileSizeConverter.convert(size));
-					mapList.add(map);
 				} catch (IOException e) {
 					logger.error("Cannot find the size of file " + file.getLabel());
+					map.put("size",
+							messageSource.getMessage("projects.reference-file.not-found", new Object[] { }, locale));
 				}
 			}
+			mapList.add(map);
 		}
 		return mapList;
 	}
