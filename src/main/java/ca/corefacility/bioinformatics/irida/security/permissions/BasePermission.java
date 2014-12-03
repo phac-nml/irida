4593dc4a2bdb05abbd392083c8465068318a63ca
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-12-03 12:44:38 -0600

    added identifier type to permissions

--- src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/BasePermission.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/BasePermission.java
@@ -1,5 +1,6 @@
 package ca.corefacility.bioinformatics.irida.security.permissions;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 import org.slf4j.Logger;
@@ -18,8 +19,8 @@ import ca.corefacility.bioinformatics.irida.model.user.Role;
  * @param <DomainObjectType>
  *            the type of domain object that this permission is evaluating.
  */
-public abstract class BasePermission<DomainObjectType> {
-	
+public abstract class BasePermission<DomainObjectType, IdentifierType extends Serializable> {
+
 	private static final Logger logger = LoggerFactory.getLogger(BasePermission.class);
 
 	/**
@@ -46,11 +47,16 @@ public abstract class BasePermission<DomainObjectType> {
 	 * The type of object to be loaded from the database.
 	 */
 	private Class<DomainObjectType> domainObjectType;
-	
+
+	/**
+	 * The type of identifier used to load this object
+	 */
+	private Class<IdentifierType> identifierType;
+
 	/**
 	 * The repository to load objects with.
 	 */
-	private CrudRepository<DomainObjectType, Long> repository;
+	private CrudRepository<DomainObjectType, IdentifierType> repository;
 
 	/**
 	 * Constructor with handles on the type of repository and type of domain
@@ -62,26 +68,33 @@ public abstract class BasePermission<DomainObjectType> {
 	 *            the identifier of the repository to load from the spring
 	 *            application context.
 	 */
-	protected BasePermission(Class<DomainObjectType> domainObjectType, CrudRepository<DomainObjectType, Long> repository) {
+	protected BasePermission(Class<DomainObjectType> domainObjectType, Class<IdentifierType> identifierType,
+			CrudRepository<DomainObjectType, IdentifierType> repository) {
 		this.repository = repository;
 		this.domainObjectType = domainObjectType;
+		this.identifierType = identifierType;
 	}
-	
+
 	/**
 	 * Evaluates the permission of a single object.
-	 * @param authentication  The Authentication object.
-	 * @param targetDomainObject  The target domain object to evaluate permission (assumes this is not a collection).
-	 * @return  True if permission is allowed on this object, false otherwise.
-	 * @throws EntityNotFoundException  If the object does not exist.
+	 * 
+	 * @param authentication
+	 *            The Authentication object.
+	 * @param targetDomainObject
+	 *            The target domain object to evaluate permission (assumes this
+	 *            is not a collection).
+	 * @return True if permission is allowed on this object, false otherwise.
+	 * @throws EntityNotFoundException
+	 *             If the object does not exist.
 	 */
 	@SuppressWarnings("unchecked")
 	private boolean customPermissionAllowedSingleObject(Authentication authentication, Object targetDomainObject) {
 		DomainObjectType domainObject;
 
-		if (targetDomainObject instanceof Long) {
-			Long id = (Long)targetDomainObject;
+		if (identifierType.isAssignableFrom(targetDomainObject.getClass())) {
+			Long id = (Long) targetDomainObject;
 			logger.trace("Trying to find domain object by id [" + id + "]");
-			domainObject = repository.findOne((Long) id);
+			domainObject = repository.findOne((IdentifierType) id);
 			if (domainObject == null) {
 				throw new EntityNotFoundException("Could not find entity with id [" + id + "]");
 			}
@@ -92,23 +105,28 @@ public abstract class BasePermission<DomainObjectType> {
 			throw new IllegalArgumentException("Parameter to " + getClass().getName() + " must be of type Long or "
 					+ domainObjectType.getName() + ".");
 		}
-		
+
 		return customPermissionAllowed(authentication, domainObject);
 	}
-	
+
 	/**
 	 * Tests permission for a collection of objects.
-	 * @param authentication  The Authentication object.
-	 * @param targetDomainObjects  The collection of domain objects to check for permission.
-	 * @return  True if permission is allowed for every object in the collection, false otherwise.
-	 * @throws EntityNotFoundException  If one of the objects in the collection does not exist.
+	 * 
+	 * @param authentication
+	 *            The Authentication object.
+	 * @param targetDomainObjects
+	 *            The collection of domain objects to check for permission.
+	 * @return True if permission is allowed for every object in the collection,
+	 *         false otherwise.
+	 * @throws EntityNotFoundException
+	 *             If one of the objects in the collection does not exist.
 	 */
 	private boolean customPermissionAllowedCollection(Authentication authentication, Collection<?> targetDomainObjects) {
 		boolean permitted = true;
 		for (Object domainObjectInCollection : targetDomainObjects) {
 			permitted &= customPermissionAllowedSingleObject(authentication, domainObjectInCollection);
 		}
-		
+
 		return permitted;
 	}
 
@@ -130,7 +148,7 @@ public abstract class BasePermission<DomainObjectType> {
 		}
 
 		if (targetDomainObject instanceof Collection<?>) {
-			return customPermissionAllowedCollection(authentication, (Collection<?>)targetDomainObject);
+			return customPermissionAllowedCollection(authentication, (Collection<?>) targetDomainObject);
 		} else {
 			return customPermissionAllowedSingleObject(authentication, targetDomainObject);
 		}
