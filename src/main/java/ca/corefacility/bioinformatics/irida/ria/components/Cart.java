d0c861cd21fc6bab75748c9b31a0c95aac41f696
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-12-05 09:14:53 -0600

    changed the Lists to Sets and removed addProject

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/components/Cart.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/components/Cart.java
@@ -1,6 +1,7 @@
 package ca.corefacility.bioinformatics.irida.ria.components;
 
 import java.util.List;
+import java.util.Set;
 
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
@@ -9,51 +10,44 @@ import ca.corefacility.bioinformatics.irida.model.sample.Sample;
  * @author Josh Adam<josh.adam@phac-aspc.gc.ca>
  */
 public interface Cart {
-	/**
-	 * Add an entire project to the cart.
-	 *
-	 * @param project
-	 * 		{@link Project} to add to the cart.
-	 */
-	public void addProject(Project project);
 
 	/**
 	 * Remove an entire project from the cart.
 	 *
 	 * @param project
-	 * 		{@link Project} to remove from the cart.
+	 *            {@link Project} to remove from the cart.
 	 */
 	public void removeProject(Project project);
 
 	/**
-	 * Add a list of samples for a project to the cart.  If the project is not in the cart it should be added to the
-	 * cart.
+	 * Add a list of samples for a project to the cart. If the project is not in
+	 * the cart it should be added to the cart.
 	 *
 	 * @param project
-	 * 		The {@link Project} the samples belong to.
+	 *            The {@link Project} the samples belong to.
 	 * @param samples
-	 * 		A list of {@link Sample} to add to the cart.
+	 *            A list of {@link Sample} to add to the cart.
 	 */
-	public void addProjectSample(Project project, List<Sample> samples);
+	public void addProjectSample(Project project, Set<Sample> samples);
 
 	/**
-	 * Remove a list of samples for a project from the cart. If this removes all the samples the project should be
-	 * removed from the cart.
+	 * Remove a list of samples for a project from the cart. If this removes all
+	 * the samples the project should be removed from the cart.
 	 *
 	 * @param project
-	 * 		The {@link Project} the samples belong to.
+	 *            The {@link Project} the samples belong to.
 	 * @param samples
-	 * 		A list of {@link Sample} to remove from the cart.
+	 *            A list of {@link Sample} to remove from the cart.
 	 */
-	public void removeProjectSample(Project project, List<Sample> samples);
+	public void removeProjectSample(Project project, Set<Sample> samples);
 
 	/**
 	 * Returns a list of samples for a project that are in the cart.
 	 *
 	 * @param project
-	 * 		The {@link Project} to get the samples for.
+	 *            The {@link Project} to get the samples for.
 	 *
 	 * @return A {@link List} of {@link Sample} belonging to the {@link Project}
 	 */
-	public List<Sample> getSelectedSamplesForProject(Project project);
+	public Set<Sample> getSelectedSamplesForProject(Project project);
 }
