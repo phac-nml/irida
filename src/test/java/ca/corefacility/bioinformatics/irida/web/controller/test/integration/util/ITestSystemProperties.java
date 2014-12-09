cfe89bdfd2f074ef342608b85003cc789df97b2b
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2014-12-09 09:26:50 -0600

    Don't use String.format...

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/util/ITestSystemProperties.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/util/ITestSystemProperties.java
@@ -1,7 +1,6 @@
 package ca.corefacility.bioinformatics.irida.web.controller.test.integration.util;
 
 public class ITestSystemProperties {
-	
 	public static final String APP_PORT = System.getProperty("jetty.port");
-	public static final String BASE_URL = String.format("http://localhost:%d/", APP_PORT);
+	protected static final String BASE_URL = "http://localhost:" + APP_PORT + "/";
 }
