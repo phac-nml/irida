12fb5d8369202695d68471dc336871cabbdb619b
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2014-12-09 10:04:49 -0600

    Remove the trailing slash on the base url.

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/util/ITestSystemProperties.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/util/ITestSystemProperties.java
@@ -2,5 +2,5 @@ package ca.corefacility.bioinformatics.irida.web.controller.test.integration.uti
 
 public class ITestSystemProperties {
 	public static final String APP_PORT = System.getProperty("jetty.port");
-	public static final String BASE_URL = "http://localhost:" + APP_PORT + "/";
+	public static final String BASE_URL = "http://localhost:" + APP_PORT;
 }
