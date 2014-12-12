5b28a2def67ba9fb6eab9973a950df1aaaf72e4c
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-12-12 09:54:15 -0600

    back to phantomjsdriver

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/sequencingRuns/SequencingRunsListPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/sequencingRuns/SequencingRunsListPageIT.java
@@ -10,7 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -42,7 +42,7 @@ public class SequencingRunsListPageIT {
 
 	@Before
 	public void setUp() {
-		driver = TestUtilities.setDriverDefaults(new ChromeDriver());
+		driver = TestUtilities.setDriverDefaults(new PhantomJSDriver());
 		LoginPage.loginAsAdmin(driver);
 		page = new SequencingRunsListPage(driver);
 		page.goTo();
