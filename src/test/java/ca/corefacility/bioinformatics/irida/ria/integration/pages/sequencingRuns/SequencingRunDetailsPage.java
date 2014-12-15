b1e4717a21e614ba7adb82b0763ffa0c03836eff
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2014-12-15 09:34:26 -0600

    added IT

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/sequencingRuns/SequencingRunDetailsPage.java
@@ -0,0 +1,46 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.pages.sequencingRuns;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.AbstractPage;
+
+public class SequencingRunDetailsPage extends AbstractPage {
+	public static String PAGEURL = "sequencingRuns/";
+	private static final Logger logger = LoggerFactory.getLogger(SequencingRunDetailsPage.class);
+
+	public SequencingRunDetailsPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public void getDetailsPage(Long id) {
+		String url = PAGEURL + id;
+		logger.debug("Loading sequencing run " + id + " at " + url);
+		get(driver, PAGEURL + id);
+	}
+
+	public Map<String, String> getRunDetails() {
+		logger.debug("Getting run details");
+		Map<String, String> details = new HashMap<>();
+		List<WebElement> detailElement = driver.findElements(By.className("run-detail"));
+		for (WebElement e : detailElement) {
+			String key = e.findElement(By.className("run-detail-key")).getText();
+			String value = e.findElement(By.className("run-detail-value")).getText();
+			logger.debug("Found " + key + " : " + value);
+			details.put(key, value);
+		}
+
+		return details;
+	}
+
+	public String getSequencerType() {
+		return driver.findElement(By.className("sequencer-type")).getText();
+	}
+}
