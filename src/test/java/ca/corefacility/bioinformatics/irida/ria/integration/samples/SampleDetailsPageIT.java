c90aa2881e797b1c5a36195a811e7daac2334e4c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-12-18 11:18:12 -0600

    Restored phantomsjs driver

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/samples/SampleDetailsPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/samples/SampleDetailsPageIT.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -54,7 +54,7 @@ public class SampleDetailsPageIT {
 
 	@Before
 	public void setUp() {
-		driver = TestUtilities.setDriverDefaults(new ChromeDriver());
+		driver = TestUtilities.setDriverDefaults(new PhantomJSDriver());
 		LoginPage.loginAsAdmin(driver);
 		page = new SampleDetailsPage(driver);
 	}
