d8e431cf71b71cf42f734b764b9e5b1a689cebe3
Josh Adam <josh.adam@phac-aspc.gc.ca>
2014-12-29 14:31:42 -0600

    Removed unused import

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/LoginController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/LoginController.java
@@ -2,7 +2,6 @@ package ca.corefacility.bioinformatics.irida.ria.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
