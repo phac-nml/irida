827a6d5dbcc47aa4f60a73e9a3b44f43ac5fdf69
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-01-09 10:46:01 -0600

    Integration Testing.

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pipelines/PipelinesPhylogenomicsPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pipelines/PipelinesPhylogenomicsPageIT.java
@@ -1,7 +1,74 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.pipelines;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.config.services.IridaApiPropertyPlaceholderConfig;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.pipelines.PipelinesPhylogenomicsPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectSamplesPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.utilities.TestUtilities;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
 /**
- * Created by josh on 15-01-09.
+ * <p> Testing for launching a phylogenomics pipeline. </p>
+ *
+ * @author Josh Adam <josh.adam@phac-aspc.gc.ca>
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiJdbcDataSourceConfig.class,
+		IridaApiPropertyPlaceholderConfig.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@ActiveProfiles("it")
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/pipelines/PipelinePhylogenomicsView.xml")
+@DatabaseTearDown("classpath:/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
 public class PipelinesPhylogenomicsPageIT {
+	private static final Logger logger = LoggerFactory.getLogger(PipelinesPhylogenomicsPageIT.class);
+	private WebDriver driver;
+	private PipelinesPhylogenomicsPage page;
+
+	@Before
+	public void setUp() {
+		driver = TestUtilities.setDriverDefaults(new ChromeDriver());
+		page = new PipelinesPhylogenomicsPage(driver);
+	}
+
+	@After
+	public void destroy() {
+		driver.quit();
+	}
+
+	@Test
+	public void testPageSetup() {
+		LoginPage.loginAsUser(driver);
+		ProjectSamplesPage samplesPage = new ProjectSamplesPage(driver);
+		samplesPage.goToPage();
+		samplesPage.selectSampleByRow(1);
+		samplesPage.selectSampleByRow(2);
+		samplesPage.addSamplesToGlobalCart();
+		page.goToPage();
+		logger.info("Checking Phylogenomics Page Setup.");
+		assertTrue(driver.getCurrentUrl().contains(PipelinesPhylogenomicsPage.RELATIVE_URL));
+		assertEquals(2, page.getReferenceFileCount());
+		assertEquals(2, page.getNumberofSamplesDisplayed());
+	}
 }
