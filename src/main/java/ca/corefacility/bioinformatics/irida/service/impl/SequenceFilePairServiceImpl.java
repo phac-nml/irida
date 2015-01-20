877519ef0553b4974f55889fba47d335b7d12a69
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-01-20 13:56:44 -0600

    Removed unused imports.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequenceFilePairServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequenceFilePairServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import javax.validation.Validator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
