cc181c377f6b93d73b89b824abe9acd288602627
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-01-23 08:14:35 -0600

    Added class level documentation.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/SubmissionIds.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/SubmissionIds.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * This class is used to store workflow submission ids in the session.  This should be temporary until
+ * they are stored in the database.  Storing a list in a class seems to be handles better by Spring.
+ *
  * @author Josh Adam<josh.adam@phac-aspc.gc.ca>
  */
 public class SubmissionIds {
