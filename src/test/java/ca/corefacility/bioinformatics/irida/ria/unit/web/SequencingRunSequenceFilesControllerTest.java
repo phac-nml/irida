41669df22fb885565bcd0c2002c87fb959d3d88f
Joel Thiessen <joel.thiessen@icloud.com>
2015-01-26 08:36:40 -0600

    added tests for RESTSequencingRunSequenceFilesController#addSequenceFileToMiseqRun

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/SequencingRunSequenceFilesControllerTest.java
@@ -0,0 +1,121 @@
+package ca.corefacility.bioinformatics.irida.ria.unit.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import ca.corefacility.bioinformatics.irida.model.SequencingRunEntity;
+import ca.corefacility.bioinformatics.irida.model.run.MiseqRun;
+import ca.corefacility.bioinformatics.irida.model.run.SequencingRun;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFile;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFilePairService;
+import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
+import ca.corefacility.bioinformatics.irida.service.SequencingRunService;
+import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.sequencingrun.MiseqRunResource;
+import ca.corefacility.bioinformatics.irida.web.controller.api.RESTGenericController;
+import ca.corefacility.bioinformatics.irida.web.controller.api.samples.RESTSampleSequenceFilesController;
+import ca.corefacility.bioinformatics.irida.web.controller.api.sequencingrun.RESTSequencingRunController;
+import ca.corefacility.bioinformatics.irida.web.controller.api.sequencingrun.RESTSequencingRunSequenceFilesController;
+import ca.corefacility.bioinformatics.irida.web.controller.test.unit.TestDataFactory;
+
+import com.google.common.net.HttpHeaders;
+/**
+ * Tests for {@link RESTSequencingRunSequenceFilesController}.
+ */
+public class SequencingRunSequenceFilesControllerTest {
+	private RESTSampleSequenceFilesController controllerOld;
+	private RESTSequencingRunSequenceFilesController controller;
+	private SequenceFileService sequenceFileService;
+	private SequenceFilePairService sequenceFilePairService;
+	private SampleService sampleService;
+	private ProjectService projectService;
+	private SequencingRunService miseqRunService;
+
+	@Before
+	public void setUp() {
+		sampleService = mock(SampleService.class);
+		sequenceFileService = mock(SequenceFileService.class);
+		sequenceFilePairService = mock(SequenceFilePairService.class);
+		projectService = mock(ProjectService.class);
+		miseqRunService= mock(SequencingRunService.class);
+
+		controllerOld = new RESTSampleSequenceFilesController(sequenceFileService, sequenceFilePairService, sampleService, projectService,miseqRunService);
+		controller = new RESTSequencingRunSequenceFilesController(miseqRunService, sequenceFileService);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+        RequestAttributes ra = new ServletRequestAttributes(new MockHttpServletRequest());
+        RequestContextHolder.setRequestAttributes(ra);
+	}
+	
+	@Test
+	public void addSequenceFileToMiseqRunTest() throws IOException {
+		Long seqId =1l;
+		Long sequencingrunId =2l;
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		SequenceFile file = TestDataFactory.constructSequenceFile();
+		MiseqRun run = new MiseqRun();
+		Map<String, String> representation = new HashMap<String, String>();
+		representation.put(RESTSequencingRunSequenceFilesController.SEQUENCEFILE_ID_KEY, ""+seqId);
+		
+		when(sequenceFileService.read(seqId)).thenReturn(file);
+		when(miseqRunService.read(sequencingrunId)).thenReturn(run);
+		
+		ModelMap modelMap = controller.addSequenceFileToMiseqRun(sequencingrunId,representation, response);
+		
+		verify(sequenceFileService).read(seqId);
+		verify(miseqRunService).read(sequencingrunId);
+		
+		Object o = modelMap.get(RESTGenericController.RESOURCE_NAME);
+		assertNotNull("Object should not be null",o);
+		assertTrue("Object should be an instance of MiseqRunResource",o instanceof MiseqRunResource);
+		MiseqRunResource res = (MiseqRunResource)o;
+		String seqFileLocation = linkTo(RESTSequencingRunController.class).slash(sequencingrunId).slash("sequenceFiles").slash(seqId).withSelfRel().getHref();
+		assertEquals("Sequence file location should be correct",seqFileLocation,res.getLink(Link.REL_SELF).getHref());
+		assertEquals("Sequence file location should be correct",seqFileLocation,response.getHeader(HttpHeaders.LOCATION));
+		assertEquals("HTTP status must be CREATED",HttpStatus.CREATED.value(), response.getStatus());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void addSequenceFileToMiseqRunTestNotMiseqInstance() throws IOException {
+		Long seqId =1l;
+		Long sequencingrunId =2l;
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		SequenceFile file = TestDataFactory.constructSequenceFile();
+		SequencingRun run = new SequencingRunEntity();
+		Map<String, String> representation = new HashMap<String, String>();
+		representation.put(RESTSequencingRunSequenceFilesController.SEQUENCEFILE_ID_KEY, ""+seqId);
+		when(sequenceFileService.read(seqId)).thenReturn(file);
+		when(miseqRunService.read(sequencingrunId)).thenReturn(run);
+		ModelMap modelMap = controller.addSequenceFileToMiseqRun(sequencingrunId,representation, response);
+		modelMap.clear();
+	}
+}
