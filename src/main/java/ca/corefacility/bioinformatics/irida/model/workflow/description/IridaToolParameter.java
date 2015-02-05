cebfa51ff5ce92ebbc5388a44d7b674f2a25758b
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-02-05 15:53:13 -0600

    Added initial classes for adjust tool parameters

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/workflow/description/IridaToolParameter.java
@@ -0,0 +1,90 @@
+package ca.corefacility.bioinformatics.irida.model.workflow.description;
+
+import java.util.Objects;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+/**
+ * A parameter for a particular tool in a workflow.
+ * 
+ * @author Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
+ *
+ */
+
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class IridaToolParameter {
+	@XmlAttribute(name = "toolId")
+	private String toolId;
+	@XmlAttribute(name = "parameterName")
+	private String parameterName;
+	@XmlAttribute(name = "defaultValue")
+	private String defaultValue;
+
+	public IridaToolParameter() {
+	}
+
+	/**
+	 * Builds a new {@link IridaToolParameter} object with the given
+	 * information.
+	 * 
+	 * @param toolId
+	 *            The id of the tool to adjust the parameter.
+	 * @param parameterName
+	 *            The name of the parameter.
+	 * @param defaultValue
+	 *            The default value of this parameter.
+	 */
+	public IridaToolParameter(String toolId, String parameterName, String defaultValue) {
+		this.toolId = toolId;
+		this.parameterName = parameterName;
+		this.defaultValue = defaultValue;
+	}
+
+	/**
+	 * The id of the tool to adjust.
+	 * 
+	 * @return The id of the tool with the parameter to set.
+	 */
+	public String getToolId() {
+		return toolId;
+	}
+
+	/**
+	 * The name of the parameter to adjust.
+	 * 
+	 * @return The name of the parameter to adjust.
+	 */
+	public String getParameterName() {
+		return parameterName;
+	}
+
+	/**
+	 * A default value for the parameter.
+	 * 
+	 * @return A default value for the parameter.
+	 */
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(toolId, parameterName, defaultValue);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		else if (obj instanceof IridaToolParameter) {
+			IridaToolParameter other = (IridaToolParameter) obj;
+
+			return Objects.equals(toolId, other.toolId) && Objects.equals(parameterName, other.parameterName)
+					&& Objects.equals(defaultValue, other.defaultValue);
+		}
+
+		return false;
+	}
+}
---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/workflow/description/IridaWorkflowParameter.java
@@ -0,0 +1,75 @@
+package ca.corefacility.bioinformatics.irida.model.workflow.description;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+
+/**
+ * Defines input parameters for a workflow which can be adjusted.
+ * 
+ * @author Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+public class IridaWorkflowParameter {
+	private String name;
+
+	@XmlElementWrapper(name = "toolParameters")
+	@XmlElement(name = "toolParameter")
+	private List<IridaToolParameter> toolParameters;
+
+	/**
+	 * Creates a new {@link IridaWorkflowParameter} which maps to the given tool
+	 * parameters.
+	 * 
+	 * @param name
+	 *            The name of the parameter.
+	 * @param toolParameters
+	 *            The tool parameters corresponding to this named parameter.
+	 */
+	public IridaWorkflowParameter(String name, List<IridaToolParameter> toolParameters) {
+		this.name = name;
+		this.toolParameters = toolParameters;
+	}
+
+	/**
+	 * Gets the name of this parameter.
+	 * 
+	 * @return The name of this parameter.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Gets a list of tools whose values are affected by this parameter.
+	 * 
+	 * @return A {@link List} of tools whose values are affected by this
+	 *         parameter.
+	 */
+	public List<IridaToolParameter> getToolParameters() {
+		return toolParameters;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, toolParameters);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		else if (obj instanceof IridaWorkflowParameter) {
+			IridaWorkflowParameter other = (IridaWorkflowParameter) obj;
+
+			return Objects.equals(name, other.name) && Objects.equals(toolParameters, other.toolParameters);
+		}
+
+		return false;
+	}
+}
