69056f1f5113e49e092ce5442a5e49b8badf29aa
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-02-10 11:53:02 -0600

    added layout to sql

--- src/main/resources/ca/corefacility/bioinformatics/irida/sql/required-data.sql
+++ src/main/resources/ca/corefacility/bioinformatics/irida/sql/required-data.sql
@@ -281,7 +281,7 @@ insert into remote_api (name,clientId,clientSecret,description,serviceURI,create
 
 -- sequence run
 insert into sequencing_run (createdDate, description, modifiedDate, upload_status) values ('2014-07-30 08:24:33','Superbug','2014-07-30 08:24:33','COMPLETE');
-insert into miseq_run(id,workflow) values (1,"test workflow");
+insert into miseq_run(id,workflow,layout_type,read_lengths) values (1,"test workflow","SINGLE_END",250);
 
 -- sequence files
 insert into sequence_file (id, createdDate, fileRevisionNumber, modifiedDate, filePath, sequencingRun_id) values (1, '2014-07-30 08:24:34',2,'2014-07-30 08:24:34','/tmp/sequence-files/1/2/02-2222_S1_L001_R1_001.fastq',1);
