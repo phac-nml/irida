d70a92d1d0c44ede723632f60a7ab04970e04ef3
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-02-17 10:42:04 -0600

    Modified tests for SNVPhyl to test out overriding parameters

--- src/test/java/ca/corefacility/bioinformatics/irida/service/DatabaseSetupGalaxyITService.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/DatabaseSetupGalaxyITService.java
@@ -253,6 +253,33 @@ public class DatabaseSetupGalaxyITService {
 	}
 	
 	/**
+	 * Sets up an {@link AnalysisSubmission} with a set of
+	 * {@link SequenceFilePair}s that have already been setup with samples.
+	 * 
+	 * @param sequenceFilePairs
+	 *            The set of {@link SequenceFilePair}s to submit.
+	 * @param referenceFilePath
+	 *            The path to an input reference file for this test.
+	 * @param iridaWorkflowID
+	 *            The id of an irida workflow.
+	 * @param parameters
+	 *            The parameters to use.
+	 * @return An {@link AnalysisSubmission} which has been saved to the
+	 *         database.
+	 */
+	public AnalysisSubmission setupPairSubmissionInDatabase(Set<SequenceFilePair> sequenceFilePairs,
+			Path referenceFilePath, Map<String, String> parameters, UUID iridaWorkflowId) {
+
+		ReferenceFile referenceFile = referenceFileRepository.save(new ReferenceFile(referenceFilePath));
+
+		AnalysisSubmission submission = AnalysisSubmission.builder(iridaWorkflowId).name("paired analysis")
+				.inputFilesPaired(sequenceFilePairs).referenceFile(referenceFile).inputParameters(parameters).build();
+		analysisSubmissionService.create(submission);
+
+		return analysisSubmissionRepository.findOne(submission.getId());
+	}
+	
+	/**
 	 * Sets up an {@link AnalysisSubmission} with a list of paired sequence
 	 * files and a single sequence file under the same sample and saves all
 	 * dependencies in database.
