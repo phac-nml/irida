ff53ce054f89927e65ec6880a9913decc44b55ee
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2015-02-20 11:50:15 -0600

    Testing for searching for named workflow parameters by workflow ID.

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/analysis/submission/WorkflowNamedParametersRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/analysis/submission/WorkflowNamedParametersRepository.java
@@ -3,6 +3,8 @@ package ca.corefacility.bioinformatics.irida.repositories.analysis.submission;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
+
 import ca.corefacility.bioinformatics.irida.model.workflow.submission.IridaWorkflowNamedParameters;
 import ca.corefacility.bioinformatics.irida.repositories.IridaJpaRepository;
 
@@ -21,5 +23,6 @@ public interface WorkflowNamedParametersRepository extends IridaJpaRepository<Ir
 	 *            the workflow ID to search for.
 	 * @return the named parameters saved for the workflow.
 	 */
+	@Query("select p from IridaWorkflowNamedParameters p where p.workflowId = ?1")
 	public List<IridaWorkflowNamedParameters> findByWorkflowId(final UUID workflowId);
 }
---
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/WorkflowNamedParametersServiceImplIT.java
@@ -0,0 +1,71 @@
+package ca.corefacility.bioinformatics.irida.service.impl.integration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContextTestExcecutionListener;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import ca.corefacility.bioinformatics.irida.config.IridaApiNoGalaxyTestConfig;
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiTestDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.config.processing.IridaApiTestMultithreadingConfig;
+import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.model.workflow.submission.IridaWorkflowNamedParameters;
+import ca.corefacility.bioinformatics.irida.service.workflow.WorkflowNamedParametersService;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+/**
+ * Tests for named parameter service.
+ * 
+ * @author Franklin Bristow franklin.bristow@phac-aspc.gc.ca
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiNoGalaxyTestConfig.class,
+		IridaApiServicesConfig.class, IridaApiTestDataSourceConfig.class, IridaApiTestMultithreadingConfig.class })
+@ActiveProfiles("test")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class,
+		WithSecurityContextTestExcecutionListener.class })
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/service/impl/WorkflowNamedParametersServiceImplIT.xml")
+@DatabaseTearDown("/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class WorkflowNamedParametersServiceImplIT {
+
+	@Autowired
+	private WorkflowNamedParametersService namedParametersService;
+	@Test
+	@WithMockUser(username = "admin", roles = "ADMIN")
+	public void testGetNamedParametersForWorkflow() {
+		final List<IridaWorkflowNamedParameters> namedParameters = namedParametersService
+				.findNamedParametersForWorkflow(UUID.fromString("e47c1a8b-4ccd-4e56-971b-24c384933f44"));
+		assertNotNull("Should have loaded *some* parameters for the workflow.", namedParameters);
+		assertEquals("Should have 2 named parameters for the worklflow.", 2, namedParameters.size());
+	}
+
+	@Test
+	@WithMockUser(username = "admin", roles = "ADMIN")
+	public void testGetAllNamedParameters() {
+		final Iterable<IridaWorkflowNamedParameters> namedParameters = namedParametersService.findAll();
+		final Set<UUID> uuids = new HashSet<>();
+		for (final IridaWorkflowNamedParameters param : namedParameters) {
+			uuids.add(param.getWorkflowId());
+		}
+		assertEquals("Should be 2 unique IDs.", 2, uuids.size());
+	}
+}
