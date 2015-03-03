230b73c367fb4c356ddda7019cda87b1b485d6c5
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-03-03 15:00:53 -0600

    Made cleanup service only run for prod

--- src/main/java/ca/corefacility/bioinformatics/irida/config/services/IridaApiServicesConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/services/IridaApiServicesConfig.java
@@ -152,7 +152,7 @@ public class IridaApiServicesConfig {
 	
 	@Bean
 	@DependsOn("springLiquibase")
-	@Profile({ "prod", "dev" })
+	@Profile({ "prod" })
 	public AnalysisSubmissionCleanupService analysisSubmissionCleanupService(
 			AnalysisSubmissionRepository analysisSubmissionRepository, UserService userService) {
 		AnalysisSubmissionCleanupService analysisSubmissionCleanupService = new AnalysisSubmissionCleanupServiceImpl(
