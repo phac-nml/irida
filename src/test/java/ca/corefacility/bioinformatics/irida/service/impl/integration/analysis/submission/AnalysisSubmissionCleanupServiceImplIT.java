19568c3170bef024a9d73076d7688228a5752703
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-03-03 14:55:22 -0600

    Added a few additional tests

--- src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/analysis/submission/AnalysisSubmissionCleanupServiceImplIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/analysis/submission/AnalysisSubmissionCleanupServiceImplIT.java
@@ -3,6 +3,7 @@ package ca.corefacility.bioinformatics.irida.service.impl.integration.analysis.s
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,7 @@ import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConf
 import ca.corefacility.bioinformatics.irida.model.enums.AnalysisState;
 import ca.corefacility.bioinformatics.irida.repositories.analysis.submission.AnalysisSubmissionRepository;
 import ca.corefacility.bioinformatics.irida.service.AnalysisSubmissionCleanupService;
+import ca.corefacility.bioinformatics.irida.service.impl.AnalysisSubmissionCleanupServiceImpl;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
@@ -50,6 +52,20 @@ public class AnalysisSubmissionCleanupServiceImplIT {
 	@Autowired
 	private AnalysisSubmissionCleanupService analysisSubmissionCleanupService;
 	
+	@Autowired
+	private AnalysisSubmissionCleanupService analysisSubmissionCleanupServiceLocal;
+	
+	/**
+	 * Setup for tests.
+	 */
+	@Before
+	public void setup() {
+		analysisSubmissionCleanupServiceLocal = new AnalysisSubmissionCleanupServiceImpl(analysisSubmissionRepository);
+	}
+	
+	/**
+	 * Tests failing to run service due to invalid user.
+	 */
 	@Test(expected=AccessDeniedException.class)
 	@WithMockUser(username = "aaron", roles = "")
 	public void testSwitchInconsistentSubmissionsToErrorInvalidUser() {
@@ -57,11 +73,11 @@ public class AnalysisSubmissionCleanupServiceImplIT {
 	}
 	
 	/**
-	 * Tests successfully switching submissions to error.
+	 * Tests successfully switching submissions to error from autowired service.
 	 */
 	@Test
 	@WithMockUser(username = "aaron", roles = "ADMIN")
-	public void testSwitchInconsistentSubmissionsToErrorSuccess() {
+	public void testSwitchInconsistentSubmissionsToErrorAutwiredSuccess() {
 		int analysisSubmissionsChanged = analysisSubmissionCleanupService.switchInconsistentSubmissionsToError();
 
 		assertEquals("Switched invalid number of submissions", 3, analysisSubmissionsChanged);
@@ -90,5 +106,45 @@ public class AnalysisSubmissionCleanupServiceImplIT {
 			fail("Did not throw RuntimeException on second run");
 		} catch (RuntimeException e) {
 		}
+	}
+	
+	/**
+	 * Tests successfully switching submissions to error from service we build ourselves.
+	 */
+	@Test
+	@WithMockUser(username = "aaron", roles = "ADMIN")
+	public void testSwitchInconsistentSubmissionsToErrorLocalSuccess() {
+		int analysisSubmissionsChanged = analysisSubmissionCleanupServiceLocal.switchInconsistentSubmissionsToError();
+
+		assertEquals("Switched invalid number of submissions", 3, analysisSubmissionsChanged);
+		assertEquals("Did not switch SUBMITTING to ERROR", AnalysisState.ERROR, analysisSubmissionRepository.findOne(1L)
+				.getAnalysisState());
+		assertEquals("Did not switch PREPARING to ERROR", AnalysisState.ERROR, analysisSubmissionRepository.findOne(2L)
+				.getAnalysisState());
+		assertEquals("Did not switch COMPLETING to ERROR", AnalysisState.ERROR, analysisSubmissionRepository.findOne(3L)
+				.getAnalysisState());
+
+		// make sure no other submissions have changed
+		assertEquals("Analysis submission state has changed", AnalysisState.NEW,
+				analysisSubmissionRepository.findOne(4L).getAnalysisState());
+		assertEquals("Analysis submission state has changed", AnalysisState.PREPARED, analysisSubmissionRepository.findOne(5L).getAnalysisState());
+		assertEquals("Analysis submission state has changed", AnalysisState.RUNNING,
+				analysisSubmissionRepository.findOne(6L).getAnalysisState());
+		assertEquals("Analysis submission state has changed", AnalysisState.FINISHED_RUNNING, analysisSubmissionRepository.findOne(7L)
+				.getAnalysisState());
+		assertEquals("Analysis submission state has changed", AnalysisState.COMPLETED, analysisSubmissionRepository.findOne(8L)
+				.getAnalysisState());
+		assertEquals("Analysis submission state has changed", AnalysisState.ERROR, analysisSubmissionRepository.findOne(9L)
+				.getAnalysisState());
+	}
+	
+	/**
+	 * Tests successfully failing on attempt to switch submissions to error twice.
+	 */
+	@Test(expected=RuntimeException.class)
+	@WithMockUser(username = "aaron", roles = "ADMIN")
+	public void testSwitchInconsistentSubmissionsToErrorTwiceFail() {
+		analysisSubmissionCleanupServiceLocal.switchInconsistentSubmissionsToError();
+		analysisSubmissionCleanupServiceLocal.switchInconsistentSubmissionsToError();
 	}
 }
