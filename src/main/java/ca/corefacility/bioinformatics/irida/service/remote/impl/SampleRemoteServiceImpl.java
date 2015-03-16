c030d12c3159fae1ba0920df60099640601cd508
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-03-16 14:41:06 -0500

    updated samples service

--- src/main/java/ca/corefacility/bioinformatics/irida/service/remote/impl/SampleRemoteServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/remote/impl/SampleRemoteServiceImpl.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Service;
 
-import ca.corefacility.bioinformatics.irida.model.remote.RemoteProject;
-import ca.corefacility.bioinformatics.irida.model.remote.RemoteSample;
+import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.repositories.remote.SampleRemoteRepository;
 import ca.corefacility.bioinformatics.irida.service.remote.SampleRemoteService;
 
@@ -23,7 +24,7 @@ import com.google.common.base.Strings;
  *
  */
 @Service
-public class SampleRemoteServiceImpl extends RemoteServiceImpl<RemoteSample> implements SampleRemoteService {
+public class SampleRemoteServiceImpl extends RemoteServiceImpl<Sample> implements SampleRemoteService {
 	public static final String PROJECT_SAMPLES_REL = "project/samples";
 	public static final String SAMPLES_CACHE_NAME = "samplesForProject";
 
@@ -33,14 +34,15 @@ public class SampleRemoteServiceImpl extends RemoteServiceImpl<RemoteSample> imp
 	}
 
 	@Override
-	public List<RemoteSample> getSamplesForProject(RemoteProject project) {
-		String samplesHref = project.getHrefForRel(PROJECT_SAMPLES_REL);
+	public List<Sample> getSamplesForProject(Project project) {
+		Link link = project.getLink(PROJECT_SAMPLES_REL);
+		String samplesHref = link.getHref();
 		return list(samplesHref, project.getRemoteAPI());
 	}
 
 	@Override
-	public Page<RemoteSample> searchSamplesForProject(RemoteProject project, String search, int page, int size) {
-		List<RemoteSample> samplesForProject = getSamplesForProject(project);
+	public Page<Sample> searchSamplesForProject(Project project, String search, int page, int size) {
+		List<Sample> samplesForProject = getSamplesForProject(project);
 		if (!Strings.isNullOrEmpty(search)) {
 			samplesForProject = samplesForProject.stream()
 					.filter(s -> s.getSampleName().toLowerCase().contains(search.toLowerCase()))
@@ -50,7 +52,7 @@ public class SampleRemoteServiceImpl extends RemoteServiceImpl<RemoteSample> imp
 		int from = Math.max(0, page * size);
 		int to = Math.min(samplesForProject.size(), (page + 1) * size);
 
-		List<RemoteSample> paged = samplesForProject.subList(from, to);
+		List<Sample> paged = samplesForProject.subList(from, to);
 		return new PageImpl<>(paged, new PageRequest(page, size), samplesForProject.size());
 	}
 
