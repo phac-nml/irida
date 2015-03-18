de4d03828dcea64b1e55cd58c592a5e67d306bc0
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-03-18 13:54:07 -0500

    reverted iridathing to data-federation

--- src/main/java/ca/corefacility/bioinformatics/irida/model/IridaThing.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/IridaThing.java
@@ -2,9 +2,6 @@ package ca.corefacility.bioinformatics.irida.model;
 
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /**
  * An interface for all model classes in the IRIDA system
  * 
@@ -13,26 +10,10 @@ public interface IridaThing extends Timestamped {
 	/**
 	 * Get a human readable label for this object.
 	 * 
-	 * @return human readable label
+	 * @return
 	 */
 	@NotNull(message = "{irida.label.notnull}")
-	@JsonProperty
 	public String getLabel();
-
-	/**
-	 * Method supporting JSON deserialzation. This method should not be used and
-	 * will throw UnsupportedOperationException when called.
-	 * 
-	 * @param label
-	 *            label param
-	 * @throws UnsupportedOperationException
-	 *             A label cannot be set for an object. It should be computed
-	 *             from the object's properties.
-	 */
-	@JsonIgnore
-	public default void setLabel(String label) throws UnsupportedOperationException {
-		throw new UnsupportedOperationException("Label cannot be set on an object.");
-	}
 
 	/**
 	 * Get the numerical identifier for this object
