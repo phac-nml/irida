087aebe816b48aa1a65975e1d8452abc335ae247
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-03-18 09:02:57 -0500

    Added concept of a cleaned/not cleaned state to an analysis

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/enums/AnalysisCleanedState.java
@@ -0,0 +1,44 @@
+package ca.corefacility.bioinformatics.irida.model.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Defines the state for cleaning up intermediate files of an analysis.
+ */
+public enum AnalysisCleanedState {
+	
+	/**
+	 * A state where intermediate files exists for this analysis.
+	 */
+	NOT_CLEANED("NOT_CLEANED"),
+	
+	/**
+	 * Set when an analysis is finished being cleaned.
+	 */
+	CLEANED("CLEANED");
+	
+	private static Map<String, AnalysisCleanedState> stateMap = new HashMap<>();
+	private String stateString;
+	
+	/**
+	 * Sets of a Map used to convert a string to a analysis cleaned state.
+	 */
+	static {
+		for (AnalysisCleanedState state : AnalysisCleanedState.values()) {
+			stateMap.put(state.toString(), state);
+		}
+	}
+	
+	private AnalysisCleanedState(String stateString) {
+		this.stateString = stateString;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return stateString;
+	}
+}
