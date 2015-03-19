8b206e065e3383a10b3b3eea1412c4e5c0f4941a
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-03-19 13:07:01 -0500

    comments

--- src/main/java/ca/corefacility/bioinformatics/irida/service/RemoteAPIService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/RemoteAPIService.java
@@ -11,14 +11,32 @@ import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
  */
 public interface RemoteAPIService extends CRUDService<Long, RemoteAPI> {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	@PreAuthorize("permitAll")
 	public RemoteAPI read(Long id);
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	@PreAuthorize("permitAll")
 	public Iterable<RemoteAPI> findAll();
 
+	/**
+	 * Find the {@link RemoteAPI} where the serviceURI is a substring of the
+	 * given URL.
+	 * 
+	 * For example if you passed the URL
+	 * 'http://irida.ca/api/projects/5/samples/3' the method would find a
+	 * {@link RemoteAPI} with serviceURI of 'http://irida.ca/api'
+	 * 
+	 * @param url
+	 *            The URL to test
+	 * @return The {@link RemoteAPI} matching the given URL
+	 */
 	@PreAuthorize("permitAll")
 	public RemoteAPI getRemoteAPIForUrl(String url);
 
