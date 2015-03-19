d76c1bdc9fdfbc2b0931d45898a9fb30907d7079
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-03-19 12:10:15 -0500

    added method to get an api for a given resource url

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/RemoteAPIRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/RemoteAPIRepository.java
@@ -1,11 +1,24 @@
 package ca.corefacility.bioinformatics.irida.repositories;
 
+import org.springframework.data.jpa.repository.Query;
+
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
 
 /**
- * A repository to store RemoteAPI instances that this IRIDA instance can connect to
+ * A repository to store RemoteAPI instances that this IRIDA instance can
+ * connect to
  *
  */
-public interface RemoteAPIRepository extends IridaJpaRepository<RemoteAPI, Long>{
+public interface RemoteAPIRepository extends IridaJpaRepository<RemoteAPI, Long> {
 
+	/**
+	 * Find the {@link RemoteAPI} where the serviceURI is a substring of the
+	 * given URL
+	 * 
+	 * @param url
+	 *            The URL to test
+	 * @return The {@link RemoteAPI} matching the given URL
+	 */
+	@Query("FROM RemoteAPI api WHERE locate(api.serviceURI, ?1) = 1")
+	public RemoteAPI getRemoteAPIForUrl(String url);
 }
