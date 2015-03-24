90e439c1ecb1a90c3dc2a4f8062a669117917b7c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-03-24 09:03:46 -0500

    Don't need to get the status info initially now.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/AnalysisController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/AnalysisController.java
@@ -138,26 +138,18 @@ public class AnalysisController {
 		model.addAttribute("workflowName", workflowName);
 
 		/*
-		 * If the analysis is completed, add preview information
+		 * Preview information
 		 */
 		try {
 			if (submission.getAnalysisState().equals(AnalysisState.COMPLETED)) {
 				if (analysisType.equals(AnalysisType.PHYLOGENOMICS)) {
 					tree(submission, model);
 				}
-			}if (!submission.getAnalysisState().equals(AnalysisState.ERROR)) {
-				float percentComplete = analysisSubmissionService.getPercentCompleteForAnalysisSubmission(
-						submission.getId());
-				model.addAttribute("percentComplete", Float.toString(percentComplete));
 			}
 
 		} catch (IOException e) {
 			logger.error("Couldn't get preview for analysis", e);
-		} catch (NoPercentageCompleteException e) {
-			logger.error("Couldn't get the percent complete", e);
-		} catch (ExecutionManagerException e) {
-			logger.error("Error", e);
-		}
+		} 
 
 		return viewName;
 	}
