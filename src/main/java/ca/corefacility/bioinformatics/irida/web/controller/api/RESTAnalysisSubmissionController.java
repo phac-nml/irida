060dda4389b76618a8c6857c1f50945fce157d2f
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-04-14 09:33:28 -0500

    added method to list individual analysis types

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/RESTAnalysisSubmissionController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/RESTAnalysisSubmissionController.java
@@ -1,8 +1,14 @@
 package ca.corefacility.bioinformatics.irida.web.controller.api;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Map.Entry;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -12,13 +18,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
+import ca.corefacility.bioinformatics.irida.model.IridaResourceSupport;
 import ca.corefacility.bioinformatics.irida.model.enums.AnalysisState;
 import ca.corefacility.bioinformatics.irida.model.workflow.analysis.Analysis;
+import ca.corefacility.bioinformatics.irida.model.workflow.analysis.AnalysisAssemblyAnnotation;
 import ca.corefacility.bioinformatics.irida.model.workflow.analysis.AnalysisOutputFile;
+import ca.corefacility.bioinformatics.irida.model.workflow.analysis.AnalysisPhylogenomicsPipeline;
 import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmission;
 import ca.corefacility.bioinformatics.irida.service.AnalysisSubmissionService;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResourceCollection;
+
+import com.google.common.collect.ImmutableMap;
 
 @Controller
 @RequestMapping(value = "/api/analysisSubmission")
@@ -26,10 +36,63 @@ public class RESTAnalysisSubmissionController extends RESTGenericController<Anal
 	private AnalysisSubmissionService analysisSubmissionService;
 	public static final String ANALYSIS_LINK = "analysis";
 
+	public static Map<String, Class<? extends Analysis>> ANALYSIS_TYPES = ImmutableMap.of("phylogenomics",
+			AnalysisPhylogenomicsPipeline.class, "assembly", AnalysisAssemblyAnnotation.class);
+
 	@Autowired
 	public RESTAnalysisSubmissionController(AnalysisSubmissionService analysisSubmissionService) {
 		super(analysisSubmissionService, AnalysisSubmission.class);
 		this.analysisSubmissionService = analysisSubmissionService;
+	}
+
+	/**
+	 * Get all analyses of a given type
+	 * 
+	 * @param type
+	 *            The type to request
+	 * @return ModelMap containing the requested type of resource
+	 */
+	@RequestMapping("/analysisType/{type}")
+	public ModelMap listOfType(@PathVariable String type) {
+		ModelMap model = new ModelMap();
+
+		if (!ANALYSIS_TYPES.containsKey(type)) {
+			throw new EntityNotFoundException("Analysis type not found");
+		}
+		Class<? extends Analysis> analysisClass = ANALYSIS_TYPES.get(type);
+
+		Iterable<AnalysisSubmission> submissions = analysisSubmissionService.findAll();
+
+		List<AnalysisSubmission> analysesOfType = StreamSupport.stream(submissions.spliterator(), false)
+				.filter((s) -> s.getAnalysis() != null && s.getAnalysis().getClass().equals(analysisClass))
+				.collect(Collectors.toList());
+
+		ResourceCollection<AnalysisSubmission> resourceCollection = new ResourceCollection<>(analysesOfType.size());
+		for (AnalysisSubmission s : analysesOfType) {
+			s.add(constructCustomResourceLinks(s));
+			s.add(linkTo(methodOn(RESTAnalysisSubmissionController.class).getResource(s.getId())).withSelfRel());
+			resourceCollection.add(s);
+		}
+
+		resourceCollection.add(linkTo(methodOn(RESTAnalysisSubmissionController.class).listOfType(type)).withSelfRel());
+		model.addAttribute(RESOURCE_NAME, resourceCollection);
+
+		return model;
+
+	}
+
+	/**
+	 * {@inheritDoc} Adding links to analysis types in resource collection
+	 */
+	@Override
+	public ModelMap listAllResources() {
+		ModelMap listAllResources = super.listAllResources();
+		IridaResourceSupport object = (IridaResourceSupport) listAllResources.get(RESOURCE_NAME);
+		for (String type : ANALYSIS_TYPES.keySet()) {
+			object.add(linkTo(methodOn(RESTAnalysisSubmissionController.class).listOfType(type)).withRel(type));
+		}
+
+		return listAllResources;
 	}
 
 	@RequestMapping("/{identifier}/analysis")
@@ -42,11 +105,16 @@ public class RESTAnalysisSubmissionController extends RESTGenericController<Anal
 		}
 
 		Analysis analysis = read.getAnalysis();
+
+		/*
+		 * Add links to the available files
+		 */
 		for (String name : analysis.getAnalysisOutputFileNames()) {
 			analysis.add(linkTo(
 					methodOn(RESTAnalysisSubmissionController.class).getAnalysisOutputFile(identifier, name)).withRel(
 					name));
 		}
+
 		model.addAttribute(RESOURCE_NAME, analysis);
 
 		return model;
