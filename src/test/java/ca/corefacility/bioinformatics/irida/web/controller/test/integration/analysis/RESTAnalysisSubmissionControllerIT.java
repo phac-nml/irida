d5cdff56c5d1cdb4478a5d2678ea90d09515c03b
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-04-15 15:37:11 -0500

    added tests for analysis submission functions

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/analysis/RESTAnalysisSubmissionControllerIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/integration/analysis/RESTAnalysisSubmissionControllerIT.java
@@ -1,5 +1,11 @@
 package ca.corefacility.bioinformatics.irida.web.controller.test.integration.analysis;
 
+import static ca.corefacility.bioinformatics.irida.web.controller.test.integration.util.ITestAuthUtils.asAdmin;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,17 +17,12 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 
 import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
 import ca.corefacility.bioinformatics.irida.config.services.IridaApiPropertyPlaceholderConfig;
+import ca.corefacility.bioinformatics.irida.model.enums.AnalysisState;
+import ca.corefacility.bioinformatics.irida.web.spring.view.NewickFileView;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
-
-import static ca.corefacility.bioinformatics.irida.web.controller.test.integration.util.ITestAuthUtils.asAdmin;
-import static ca.corefacility.bioinformatics.irida.web.controller.test.integration.util.ITestAuthUtils.asManager;
-import static ca.corefacility.bioinformatics.irida.web.controller.test.integration.util.ITestAuthUtils.asRole;
-import static ca.corefacility.bioinformatics.irida.web.controller.test.integration.util.ITestAuthUtils.asUser;
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiJdbcDataSourceConfig.class,
@@ -30,19 +31,40 @@ import static org.hamcrest.CoreMatchers.equalTo;
 @ActiveProfiles("it")
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/web/controller/test/integration/analysis/RESTAnalysisSubmissionControllerIT.xml")
 @DatabaseTearDown("classpath:/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+/**
+ * Test for functions of {@link RESTAnalysisSubmissionController}
+ */
 public class RESTAnalysisSubmissionControllerIT {
 
 	public static final String ANALYSIS_BASE = "/api/analysisSubmissions";
 
 	@Test
-	public void testRead() {
+	public void testReadSubmission() {
 		asAdmin().expect().body("resource.name", equalTo("another analysis")).and()
+				.body("resource.analysisState", equalTo(AnalysisState.COMPLETED.toString()))
 				.body("resource.links.rel", hasItems("self", "analysis")).when().get(ANALYSIS_BASE + "/1");
+	}
+
+	@Test
+	public void testReadIncompleteSubmission() {
+		asAdmin().expect().body("resource.analysisState", equalTo(AnalysisState.PREPARING.toString()))
+				.body("resource.links.rel", not(hasItems("analysis"))).when().get(ANALYSIS_BASE + "/2");
 	}
 
 	@Test
 	public void testGetAnalysis() {
 		asAdmin().expect().body("resource.executionManagerAnalysisId", equalTo("XYZABC")).and()
 				.body("resource.links.rel", hasItems("self", "tree")).when().get(ANALYSIS_BASE + "/1/analysis");
+	}
+
+	@Test
+	public void testGetOutputFile() {
+		asAdmin().expect().body("resource.executionManagerFileId", equalTo("123-456-789")).and()
+				.body("resource.label", equalTo("snp_tree.tree")).body("resource.links.rel", hasItems("self")).when()
+				.get(ANALYSIS_BASE + "/1/analysis/file/tree");
+
+		// get the tree file
+		asAdmin().given().header("Accept", NewickFileView.DEFAULT_CONTENT_TYPE).expect().body(containsString("c6706"))
+				.when().get(ANALYSIS_BASE + "/1/analysis/file/tree");
 	}
 }
