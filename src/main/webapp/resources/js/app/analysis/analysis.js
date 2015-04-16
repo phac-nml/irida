09b9ec89443dd3aff539d3ed72e0fa1de2836b22
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-04-16 08:59:06 -0500

    Initial state

--- src/main/webapp/resources/js/app/analysis/analysis.js
+++ src/main/webapp/resources/js/app/analysis/analysis.js
@@ -1,6 +1,6 @@
-/* global angular, ANALYSIS_PAGE */
+/* global ANALYSIS_PAGE */
 
-(function () {
+(function (angular, PhyloCanvas) {
 
   /**
    * Controller to download the analysis.
@@ -112,9 +112,94 @@
     initialize();
   }
 
+
   angular.module('irida.analysis', [])
     .service('AnalysisService', ['$http', '$interval', AnalysisService])
+    .directive('phylocanvas', function phylocanvas() {
+      return {
+        restrict  : 'E',
+        transclude: true,
+        replace   : true,
+        template  : '<div><div ng-transclude></div>{{shape}}</div>',
+        scope     : {
+          shape: "@"
+        },
+        controller: ['$scope', function($scope) {
+          $scope.shape = shape;          
+        }]
+      };
+    })
+    .directive('phylocanvasControls', function () {
+      return {
+        require   : '^phylocanvas',
+        replace   : true,
+        transclude: true,
+        template  : '<div><div class="btn-group" role="group"><button ng-repeat="control in controls" ng-click="select(control)" class="btn btn-default btn-sm" ng-class="{active: control.selected}">{{control.text}}</button></div><div ng-transclude></div></div>',
+        restrict  : "E",
+        controller: ['$scope', function ($scope) {
+          $scope.controls = [];
+          $scope.shape = 'circular';
+          
+          $scope.select = function(control) {
+            angular.forEach($scope.controls, function (eachControl) {
+              eachControl.selected = angular.equals(control, eachControl);
+            });
+            
+          };
+          
+          this.addControl = function (control) {
+            if($scope.controls.length === 0) {
+              control.selected = true;
+            }
+            $scope.controls.push(control);
+          };
+        }]
+      };
+    })
+    .directive('phylocanvasControl', function () {
+      return {
+        require : '^phylocanvasControls',
+        restrict: 'E',
+        replace: true,
+        template: '',
+        scope   : {
+          shape: "@",
+          text : "@"
+        },
+        link    : function (scope, element, attrs, phylocanvasControlsCtrl) {
+          phylocanvasControlsCtrl.addControl(scope);
+        }
+      };
+    })
+    .directive('phylocanvasBody', function () {
+      return {
+        require: "^phylocanvas",
+        scope  : {
+          newick: "@",
+          id    : '@'
+        },
+        link   : function (scope, elem) {
+          angular
+            .element(elem)
+            .css({
+              'height': '500px',
+              'width' : '100%'
+            });
+          var phylo = new PhyloCanvas.Tree(scope.id, {});
+          phylo.load(scope.newick);
+
+          scope.$watch(function () {
+            return scope.$parent.shape;
+          }, function (n, o) {
+            console.log(n, o);
+            if (o !== n) {
+              phylo.setTreeType(n);
+            }
+          });
+        }
+      };
+    })
     .controller('FileDownloadController', [FileDownloadController])
     .controller('StateController', ['AnalysisService', StateController])
   ;
-})();
+})(window.angular, window.PhyloCanvas);
