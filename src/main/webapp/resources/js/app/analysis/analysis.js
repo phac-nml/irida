c5000e5258d3d1a49d3925651da9c6ea891c4716
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-04-16 14:35:12 -0500

    Updated docs

--- src/main/webapp/resources/js/app/analysis/analysis.js
+++ src/main/webapp/resources/js/app/analysis/analysis.js
@@ -113,7 +113,7 @@
   }
 
 
-  angular.module('irida.analysis', ['ui.router', 'phylocanvas'])
+  angular.module('irida.analysis', ['ui.router', 'subnav', 'phylocanvas'])
     .config(['$stateProvider', function ($stateProvider) {
 
       $stateProvider
@@ -131,51 +131,6 @@
         })
       ;
     }])
-    .directive('subNav', function () {
-      return {
-        restrict  : 'E',
-        replace   : true,
-        transclude: true,
-        priority  : -1,
-        template  : '<div style="padding-bottom: 10px;"><ul class="nav nav-pills"><li ng-click="select(link)" ng-class="{active: link.selected}" ng-repeat="link in links"><a id="{{link.state}}" ui-sref="{{link.state}}">{{link.text}}</a></li></ul><div ng-transclude></div></div>',
-        controller: ['$scope', '$location', function ($scope, $location) {
-          $scope.links = [];
-
-          $scope.select = function (link) {
-            angular.forEach($scope.links, function (eachLink) {
-              eachLink.selected = angular.equals(link, eachLink);
-            });
-          };
-
-          var path = $location.path();
-          this.addLink = function (link) {
-            if (path === "" && $scope.links.length === 0) {
-              link.selected = true;
-              $location.path("/" + link.state);
-            }
-            else if (path.indexOf(link.state) > -1) {
-              link.selected = true;
-            }
-            $scope.links.push(link);
-          };
-        }]
-      };
-    })
-    .directive('subNavItem', function () {
-      return {
-        require : '^subNav',
-        restrict: 'E',
-        replace : true,
-        template: '',
-        scope   : {
-          state: "@",
-          text : "@"
-        },
-        link    : function (scope, element, attrs, SubNavCtrl) {
-          SubNavCtrl.addLink(scope);
-        }
-      };
-    })
     .service('AnalysisService', ['$http', '$interval', AnalysisService])
     .controller('FileDownloadController', [FileDownloadController])
     .controller('StateController', ['AnalysisService', StateController])
