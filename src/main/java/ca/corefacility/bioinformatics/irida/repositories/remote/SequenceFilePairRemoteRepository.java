795f2716d081db5b2d1fbdfb9d818bfc250793f8
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-05-07 09:37:42 -0500

    added repos for remote file pairs

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/SequenceFilePairRemoteRepository.java
@@ -0,0 +1,7 @@
+package ca.corefacility.bioinformatics.irida.repositories.remote;
+
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFilePair;
+
+public interface SequenceFilePairRemoteRepository extends RemoteRepository<SequenceFilePair>{
+
+}
---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/impl/SequenceFilePairRemoteRepositoryImpl.java
@@ -0,0 +1,24 @@
+package ca.corefacility.bioinformatics.irida.repositories.remote.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Repository;
+
+import ca.corefacility.bioinformatics.irida.model.remote.resource.ListResourceWrapper;
+import ca.corefacility.bioinformatics.irida.model.remote.resource.ResourceWrapper;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFilePair;
+import ca.corefacility.bioinformatics.irida.repositories.remote.SequenceFilePairRemoteRepository;
+import ca.corefacility.bioinformatics.irida.service.RemoteAPITokenService;
+
+@Repository
+public class SequenceFilePairRemoteRepositoryImpl extends RemoteRepositoryImpl<SequenceFilePair> implements SequenceFilePairRemoteRepository {
+	private static final ParameterizedTypeReference<ListResourceWrapper<SequenceFilePair>> listTypeReference = new ParameterizedTypeReference<ListResourceWrapper<SequenceFilePair>>() {
+	};
+	private static final ParameterizedTypeReference<ResourceWrapper<SequenceFilePair>> objectTypeReference = new ParameterizedTypeReference<ResourceWrapper<SequenceFilePair>>() {
+	};
+
+	@Autowired
+	public SequenceFilePairRemoteRepositoryImpl(RemoteAPITokenService tokenService) {
+		super(tokenService, listTypeReference, objectTypeReference);
+	}
+}
