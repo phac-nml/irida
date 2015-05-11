210f338430ffbd22786f4e8f007c1b4e8d8d0fa5
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-05-11 10:54:48 -0500

    Working creating sample

--- src/main/webapp/resources/js/pages/projects/add-samples.js
+++ src/main/webapp/resources/js/pages/projects/add-samples.js
@@ -1,24 +1,34 @@
 (function(angular, $, TL, PAGE) {
   'use strict';
 
-  function SampleService($http, $log) {
+  function SampleService($http) {
     var url = TL.BASE_URL + 'projects/' + PAGE.project.id + '/samples';
     return {
       createSample: createSample
     };
 
-    function createSample(sample) {
-      $log.info(sample);
-      return $http.post(url, {
-          sample: sample
-        })
+    function createSample(sample, successFn, errorFn) {
+      return $http.post(url, sample)
         .success(function(response) {
-          console.log(response);
+          successFn(response);
         })
         .error(function(response) {
-          console.log(response);
+          errorFn(response);
         });
     }
+  }
+
+  function nameValidator() {
+    var re = /[^A-Za-z0-9\-_]/;
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, elem, attrs, ctrl) {
+        ctrl.$validators.nameValidator = function(value) {
+          return !re.test(value);
+        };
+      }
+    };
   }
 
   function select2() {
@@ -56,12 +66,31 @@
 
     vm.createSample = function createSample() {
       vm.sample.sequencerSampleId = vm.sample.sampleName;
-      sampleService.createSample(vm.sample);
+      console.log(vm.sample);
+      sampleService.createSample(vm.sample, sampleCreatedSuccess, sampleCreatedError);
     };
+
+    function sampleCreatedSuccess(response) {
+      var sample = response.sample;
+      wizardHandler.wizard().next();
+    }
+
+    function sampleCreatedError(response) {
+      console.log(response);
+      var errors = response.errors;
+      for (var key in errors) {
+        if (errors.hasOwnProperty(key) && key !== 'label' && key !== 'sequencerSampleId') {
+          vm.sampleDetailForm[key].$dirty = true;
+          vm.sampleDetailForm[key].$setValidity(errors[key], false);
+          vm.sampleNameError = errors[key];
+        }
+      }
+    }
   }
 
-  angular.module('samples.new', ['mgo-angular-wizard'])
-    .factory('SampleService', ['$http', '$log', SampleService])
+  angular.module('samples.new', ['mgo-angular-wizard', 'ng-file-upload'])
+    .factory('SampleService', ['$http', SampleService])
     .directive('select2', [select2])
+    .directive('nameValidator', [nameValidator])
     .controller('SampleController', ['SampleService', 'WizardHandler', SampleController]);
 })(window.angular, window.$, window.TL, window.PAGE);
