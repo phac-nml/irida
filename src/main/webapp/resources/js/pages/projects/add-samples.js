fa7a10ad0a7bd02232795787c02b2467b2fa2416
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-05-12 08:37:35 -0500

    Removed file upload function

--- src/main/webapp/resources/js/pages/projects/add-samples.js
+++ src/main/webapp/resources/js/pages/projects/add-samples.js
@@ -19,23 +19,6 @@
     }
   }
 
-  function FileService(upload) {
-    return {
-      upload: uploadFiles
-    };
-
-    function uploadFiles(files, id) {
-      console.log(files);
-      if (files && files.length > 0) {
-        upload.upload({
-          // e.g. projects/4/samples/4/files
-          url: URL_BASE + '/' + id + '/files',
-          file: files
-        });
-      }
-    }
-  }
-
   function nameValidator() {
     var re = /[^A-Za-z0-9\-_]/;
     return {
@@ -46,17 +29,6 @@
           return !re.test(value);
         };
       }
-    };
-  }
-
-  function sizeConverter() {
-    return function(bytes) {
-      var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-      if (bytes === 0) {
-        return '0 Byte';
-      }
-      var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-      return (Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]);
     };
   }
 
@@ -86,9 +58,8 @@
     };
   }
 
-  function SampleController(sampleService, fileService, $modal) {
+  function SampleController(sampleService) {
     var vm = this;
-    vm.files = [];
     vm.sample = {};
     vm.uploader = {
       inProgress: false
@@ -100,27 +71,6 @@
     vm.createSample = function createSample() {
       vm.sample.sequencerSampleId = vm.sample.sampleName;
       sampleService.createSample(vm.sample, sampleCreatedSuccess, sampleCreatedError);
-    };
-
-    //    vm.addFiles = function addFiles(f) {
-    //      f.forEach(function(item) {
-    //        vm.files.push(item);
-    //      });
-    //    };
-
-    vm.uploadSingle = function(files) {
-      fileService.upload(files, vm.sample.id);
-    };
-
-    vm.showPairedModal = function() {
-      $modal.open({
-        animation: true,
-        templateUrl: '/paired-template.html',
-        controllerAs: 'pairCtrl',
-        controller: 'PairedUploadController'
-      }).result.then(function(files) {
-        console.log(files);
-      });
     };
 
     function sampleCreatedSuccess(response) {
@@ -140,27 +90,9 @@
     }
   }
 
-  function PairedUploadController($modalInstance) {
-    var vm = this;
-    vm.files = {};
-    vm.selectForward = function selectForward(file) {
-      vm.files.forward = file.pop();
-    };
-    vm.selectReverse = function selectReverse(file) {
-      vm.files.reverse = file.pop();
-    };
-    vm.upload = function upload() {
-      var files = [vm.files.forward, vm.files.reverse];
-      $modalInstance.close(files);
-    };
-  }
-
-  angular.module('samples.new', ['ngFileUpload'])
+  angular.module('samples.new', [])
     .factory('SampleService', ['$http', SampleService])
-    .factory('FileService', ['Upload', FileService])
     .directive('select2', [select2])
     .directive('nameValidator', [nameValidator])
-    .filter('sizeConverter', [sizeConverter])
-    .controller('SampleController', ['SampleService', 'FileService', '$modal', SampleController])
-    .controller('PairedUploadController', ['$modalInstance', PairedUploadController]);
+    .controller('SampleController', ['SampleService', '$modal', SampleController]);
 })(window.angular, window.$, window.TL, window.PAGE);
