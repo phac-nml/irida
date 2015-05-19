10c38c59b05c8f7b6fb9aafe7b764eed7e09ea3b
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-05-19 13:51:17 -0500

    added getter for remoteuri

--- src/main/java/ca/corefacility/bioinformatics/irida/model/sequenceFile/RemoteSequenceFile.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/sequenceFile/RemoteSequenceFile.java
@@ -154,5 +154,9 @@ public class RemoteSequenceFile implements IridaSequenceFile, IridaThing, Versio
 	public void incrementFileRevisionNumber() {
 		fileRevisionNumber++;
 	}
+	
+	public String getRemoteURI() {
+		return remoteURI;
+	}
 
 }
