9b6433da600cf0364b894a9334b82a636be4900b
Thomas Matthews <thomas.matthews@phac-aspc.gc.ca>
2015-05-20 13:15:05 -0500

    removed unused import

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/impl/SequenceFileRemoteRepositoryImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/remote/impl/SequenceFileRemoteRepositoryImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
