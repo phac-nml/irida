7d1d3e6e6681d80fdba03365a5cbc31f150fd01f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-05-26 10:17:42 -0500

    Updated doc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/dialects/processors/icons/IconNotFoundException.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/dialects/processors/icons/IconNotFoundException.java
@@ -4,6 +4,12 @@ package ca.corefacility.bioinformatics.irida.ria.dialects.processors.icons;
  * {@link Exception} thrown when the icon is not in the predefined list.
  */
 public class IconNotFoundException extends Exception {
+	/**
+	 * Construct a new {@link IconNotFoundException} with the specified message.
+	 *
+	 * @param message
+	 *            the message explaining what icon was not found.
+	 */
 	public IconNotFoundException(String message) {
 		super(message);
 	}
