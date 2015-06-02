9febf9a035ff832336eac65c63224261014e9e2c
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-06-02 13:44:42 -0500

    Added liquibase updates for assembled genome and re-name

--- src/main/java/ca/corefacility/bioinformatics/irida/model/genomeFile/AssembledGenomeAnalysis.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/genomeFile/AssembledGenomeAnalysis.java
@@ -2,35 +2,35 @@ package ca.corefacility.bioinformatics.irida.model.genomeFile;
 
 import java.nio.file.Path;
 import java.util.Date;
+import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import ca.corefacility.bioinformatics.irida.model.workflow.analysis.AnalysisOutputFile;
 import ca.corefacility.bioinformatics.irida.service.util.SequenceFileUtilities;
 import ca.corefacility.bioinformatics.irida.service.util.impl.BioJavaSequenceFileUtilitiesImpl;
 
 /**
- * An {@link AssembledGenome} that was assembled through IRIDA.
+ * An {@link AssembledGenome} that was assembled through an IRIDA
+ * {@link Analysis}.
  */
 @Entity
-@Table(name = "assembled_genome")
-@Audited
-@EntityListeners(AuditingEntityListener.class)
-public class AssembledGenomeIrida implements AssembledGenome {
+@Table(name = "assembled_genome_analysis")
+public class AssembledGenomeAnalysis implements AssembledGenome {
 
 	private static final SequenceFileUtilities sequenceFileUtilities = new BioJavaSequenceFileUtilitiesImpl();
 
@@ -38,66 +38,111 @@ public class AssembledGenomeIrida implements AssembledGenome {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(name = "assembled_genome_file", unique = true)
-	@NotNull(message = "{reference.file.file.notnull}")
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "assembled_genome_file", unique = true, nullable = false)
 	private AnalysisOutputFile assembledGenomeFile;
 
 	@CreatedDate
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column(nullable = false)
+	@Column(name = "created_date", nullable = false)
 	private final Date createdDate;
 
-	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date modifiedDate;
-
-	private AssembledGenomeIrida() {
+	private AssembledGenomeAnalysis() {
 		this.createdDate = new Date();
 	}
 
-	public AssembledGenomeIrida(AnalysisOutputFile assembledGenomeFile) {
+	public AssembledGenomeAnalysis(AnalysisOutputFile assembledGenomeFile) {
 		this();
 		this.assembledGenomeFile = assembledGenomeFile;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getLabel() {
 		return assembledGenomeFile.getLabel();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setId(Long id) {
-		this.id = id;
+		throw new UnsupportedOperationException("AssembledGenomeIrida types cannot be modified.");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Date getCreatedDate() {
 		return createdDate;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Date getModifiedDate() {
-		return modifiedDate;
+		return createdDate;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
+		throw new UnsupportedOperationException("AssembledGenomeIrida types cannot be modified.");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Path getFile() {
 		return assembledGenomeFile.getFile();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Long getFileLength() {
 		return sequenceFileUtilities.countSequenceFileLengthInBases(getFile());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(assembledGenomeFile);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+
+		if (o instanceof AssembledGenomeAnalysis) {
+			AssembledGenomeAnalysis a = (AssembledGenomeAnalysis) o;
+			return Objects.equals(assembledGenomeFile, a.assembledGenomeFile);
+		}
+
+		return false;
 	}
 }
