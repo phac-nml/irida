ca215f859db69d8647a0cd598960d17b454362a5
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2015-06-08 15:14:39 -0500

    Add first cut of building irida with a packer script.

---
+++ src/main/resources/packer/scripts/cleanup.sh
@@ -0,0 +1,4 @@
+yum -y clean all
+rm -rf VBoxGuestAdditions_*.iso
+rm -rf /tmp/rubygems-*
+
---
+++ src/main/resources/packer/scripts/irida.sh
@@ -0,0 +1,42 @@
+# install java and apr
+yum -y install apr tomcat java-1.8.0-openjdk-headless mariadb-server mariadb-client
+
+mkdir -p /opt/irida/data/{sequencing,reference,analysis,remote}
+mkdir -p /etc/irida/analytics
+
+cd /opt/irida
+curl -O https://irida.corefacility.ca/distributables/irida-latest.war
+ln -s /opt/irida/irida-latest.war /var/lib/tomcat/webapps/irida.war
+
+curl -O https://irida.corefacility.ca/documentation/administrator/web/config/irida.conf
+ln -s /opt/irida/irida.conf /etc/irida/irida.conf
+
+curl -O https://irida.corefacility.ca/documentation/administrator/web/config/web.conf
+ln -s /opt/irida/web.conf /etc/irida/web.conf
+
+## Set up the directories in /etc/irida/irida.conf
+
+sed -i 's_sequence.file.base.directory=.*_sequence.file.base.directory=/opt/irida/data/sequencing_' /etc/irida/irida.conf
+sed -i 's_reference.file.base.directory=.*_reference.file.base.directory=/opt/irida/data/reference_' /etc/irida/irida.conf
+sed -i 's_output.file.base.directory=.*_output.file.base.directory=/opt/irida/data/analysis_' /etc/irida/irida.conf
+sed -i 's_remote.file.temporary.directory=.*_remote.file.temporary.directory=/opt/irida/data/remote_' /etc/irida/irida.conf
+
+## Set up a local database and point /etc/irida/irida.conf at that database
+systemctl enable mariadb
+systemctl start mariadb
+echo "grant all privileges on irida.* to 'irida'@'localhost' identified by 'irida';" | mysql -u root
+echo "create database irida;" | mysql -u root
+
+sed -i 's_jdbc.url=.*_jdbc.url=jdbc:mysql://localhost:3306/irida_' /etc/irida/irida.conf
+sed -i 's_jdbc.username=.*_jdbc.username=irida_' /etc/irida/irida.conf
+sed -i 's_jdbc.password=.*_jdbc.password=irida_' /etc/irida/irida.conf
+
+systemctl enable tomcat
+systemctl start tomcat
+systemctl stop tomcat
+systemctl stop mariadb
+
+## Set up pass-thru for port 8080 (tomcat default) to port 80, and allow traffic through port 80:
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-forward-port=port=80:toport=8080:proto=tcp
