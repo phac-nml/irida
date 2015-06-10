a4414a6ac00ced93d63df2cbfb4af42b2a15195f
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-06-10 10:19:24 -0500

    Added a catch so the UI still works if the breadcrumb fails.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/BreadCrumbInterceptor.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/BreadCrumbInterceptor.java
@@ -8,7 +8,10 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -19,6 +22,7 @@ import com.google.common.collect.ImmutableMap;
  * Interceptor for handling UI BreadCrumbs
  */
 public class BreadCrumbInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(BreadCrumbInterceptor.class);
 	private final MessageSource messageSource;
 	private Map<String, Boolean> BASE = ImmutableMap.of(
 			"projects", true,
@@ -54,34 +58,38 @@ public class BreadCrumbInterceptor extends HandlerInterceptorAdapter {
 
 			StringBuilder url = new StringBuilder(contextPath);
 
-			for (; counter < parts.length; counter++) {
-				// Should be a noun
-				String noun = parts[counter];
-				url.append("/");
-				url.append(noun);
-
-				crumbs.add(
-						ImmutableMap.of(
-								"text", messageSource.getMessage("bc." + noun, null, locale),
-								"url", url.toString())
-				);
-
-				// Check to see if there is a next part, if there is it is expected to be an id.
-				if (parts.length > ++counter) {
-					String id = parts[counter];
+			try {
+				for (; counter < parts.length; counter++) {
+					// Should be a noun
+					String noun = parts[counter];
 					url.append("/");
-					url.append(id);
+					url.append(noun);
+
 					crumbs.add(
 							ImmutableMap.of(
-									"text", id,
-									"url", url.toString()
-							)
+									"text", messageSource.getMessage("bc." + noun, null, locale),
+									"url", url.toString())
 					);
-				}
-			}
 
-			// Add the breadcrumbs to the model
-			modelAndView.getModelMap().put("crumbs", crumbs);
+					// Check to see if there is a next part, if there is it is expected to be an id.
+					if (parts.length > ++counter) {
+						String id = parts[counter];
+						url.append("/");
+						url.append(id);
+						crumbs.add(
+								ImmutableMap.of(
+										"text", id,
+										"url", url.toString()
+								)
+						);
+					}
+				}
+
+				// Add the breadcrumbs to the model
+				modelAndView.getModelMap().put("crumbs", crumbs);
+			} catch (NoSuchMessageException e) {
+				logger.debug("Missing internationalization for breadcrumb", e.getMessage());
+			}
 		}
 	}
 }
