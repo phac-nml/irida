9583f38ad825e20a21287ae9ebc10f02fece7fe1
tom <thomas.matthews@phac-aspc.gc.ca>
2015-06-22 11:25:06 -0500

    remove auditing on modifiedDate of project.

--- src/main/java/ca/corefacility/bioinformatics/irida/model/project/Project.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/project/Project.java
@@ -20,6 +20,7 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.NotAudited;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -56,6 +57,7 @@ public class Project extends IridaResourceSupport implements IridaThing, IridaPr
 
 	@LastModifiedDate
 	@Temporal(TemporalType.TIMESTAMP)
+	@NotAudited
 	private Date modifiedDate;
 
 	@Lob
