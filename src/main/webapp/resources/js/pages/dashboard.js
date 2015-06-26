3e8f247f99f2690703fc2280e9c2ea47b2f474c2
tom <thomas.matthews@phac-aspc.gc.ca>
2015-06-26 11:56:23 -0500

    moved all event stuff into its own js file

--- src/main/webapp/resources/js/pages/dashboard.js
+++ src/main/webapp/resources/js/pages/dashboard.js
@@ -1,27 +1,3 @@
 (function($, tl) {
-  $(function() {
-    getEvents($("#size").val());
-
-    $("#size").change(function() {
-      getEvents($(this).val());
-    });
-  });
-
-  function getEvents(size) {
-    var eventUrl = tl.BASE_URL + 'events/current_user';
-    $.ajax({
-      method: 'GET',
-      url: eventUrl,
-      data: {
-        size: size
-      },
-      headers: {
-        Accept: 'text/html'
-      },
-      success: function(data, textStatus, jqXHR) {
-        $("#events").html(data);
-      }
-    });
-  }
 
 })(window.jQuery, window.TL);
