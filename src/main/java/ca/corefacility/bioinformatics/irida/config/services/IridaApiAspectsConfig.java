aa3c47c3defb59aa86ba93efd9164265e2329247
tom <thomas.matthews@phac-aspc.gc.ca>
2015-07-10 11:18:14 -0500

    adding aspect

--- src/main/java/ca/corefacility/bioinformatics/irida/config/services/IridaApiAspectsConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/services/IridaApiAspectsConfig.java
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import ca.corefacility.bioinformatics.irida.events.ProjectEventAspect;
 import ca.corefacility.bioinformatics.irida.events.ProjectEventHandler;
+import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmission;
 import ca.corefacility.bioinformatics.irida.repositories.ProjectEventRepository;
 import ca.corefacility.bioinformatics.irida.repositories.ProjectRepository;
 import ca.corefacility.bioinformatics.irida.repositories.analysis.submission.AnalysisSubmissionRepository;
 import ca.corefacility.bioinformatics.irida.repositories.joins.project.ProjectSampleJoinRepository;
+import ca.corefacility.bioinformatics.irida.service.analysis.RunAsSubmissionUserAspect;
 import ca.corefacility.bioinformatics.irida.service.analysis.execution.AnalysisExecutionServiceAspect;
 import ca.corefacility.bioinformatics.irida.validators.ValidMethodParametersAspect;
 
@@ -38,5 +40,16 @@ public class IridaApiAspectsConfig {
 	public AnalysisExecutionServiceAspect analysisExecutionServiceAspect(
 			AnalysisSubmissionRepository analysisSubmissionRepository) {
 		return new AnalysisExecutionServiceAspect(analysisSubmissionRepository);
+	}
+
+	/**
+	 * Aspect for setting the user in the security context to be the user in the
+	 * {@link AnalysisSubmission}
+	 * 
+	 * @return new {@link RunAsSubmissionUserAspect} bean
+	 */
+	@Bean
+	public RunAsSubmissionUserAspect runAsSubmissionUserAspect() {
+		return new RunAsSubmissionUserAspect();
 	}
 }
