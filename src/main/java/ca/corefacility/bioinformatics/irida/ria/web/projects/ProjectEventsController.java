a31e813450d187aa5195ea1cbb4ce164eb5ff8e5
tom <thomas.matthews@phac-aspc.gc.ca>
2015-07-23 13:33:47 -0500

    added method to update subscription

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectEventsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectEventsController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import ca.corefacility.bioinformatics.irida.model.event.DataAddedToSampleProjectEvent;
@@ -144,6 +145,28 @@ public class ProjectEventsController {
 	@RequestMapping("/admin")
 	public String getAdminEventsPage() {
 		return "events/admin";
+	}
+	
+	/**
+	 * Update the subscription status on a {@link Project} for a {@link User}
+	 * 
+	 * @param userId
+	 *            The {@link User} id to update
+	 * @param projectId
+	 *            the {@link Project} to subscribe to
+	 * @param subscribe
+	 *            boolean whether to be subscribed to the project or not
+	 * @return Map success if the subscription status was updated
+	 */
+	@RequestMapping(value = "/projects/{projectId}/subscribe/{userId}", method = RequestMethod.POST)
+	public Map<String, String> addSubscription(@PathVariable Long userId, @PathVariable Long projectId,
+			@RequestParam boolean subscribe) {
+		User user = userService.read(userId);
+		Project project = projectService.read(projectId);
+
+		userService.updateEmailSubscription(user, project, subscribe);
+
+		return ImmutableMap.of("success", "true");
 	}
 
 	/**
