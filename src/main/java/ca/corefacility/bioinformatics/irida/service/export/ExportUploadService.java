6f7ad138fb8f2a48b3be2a7e8fece5e6e0846519
tom <thomas.matthews@phac-aspc.gc.ca>
2015-08-19 13:52:16 -0500

    added upload method

--- src/main/java/ca/corefacility/bioinformatics/irida/service/export/ExportUploadService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/export/ExportUploadService.java
@@ -1,7 +1,12 @@
 package ca.corefacility.bioinformatics.irida.service.export;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.List;
 
+import org.apache.commons.net.ftp.FTPClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,9 @@ import com.google.common.collect.ImmutableMap;
 
 import ca.corefacility.bioinformatics.irida.model.NcbiExportSubmission;
 import ca.corefacility.bioinformatics.irida.model.enums.ExportUploadState;
+import ca.corefacility.bioinformatics.irida.model.export.NcbiBioSampleFiles;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFile;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFilePair;
 
 /**
  * Class which handles uploading a {@link NcbiExportSubmission} to NCBI
@@ -26,6 +34,10 @@ public class ExportUploadService {
 
 	private NcbiExportSubmissionService exportSubmissionService;
 	private TemplateEngine templateEngine;
+	
+	private String ftpHost = "localhost";
+	private String ftpUser = "tom";
+	private String ftpPassword = "xxx";
 
 	@Autowired
 	public ExportUploadService(NcbiExportSubmissionService exportSubmissionService,
@@ -71,5 +83,57 @@ public class ExportUploadService {
 		final String htmlContent = templateEngine.process(NCBI_TEMPLATE, ctx);
 
 		logger.debug(htmlContent);
+	}
+	
+	public void uploadXml(NcbiExportSubmission submission, String xml) {
+		FTPClient client = new FTPClient();
+		try {
+
+			String directoryName = submission.getId().toString();
+
+			client.connect(ftpHost);
+			client.login(ftpUser, ftpPassword);
+			logger.debug(client.getStatus());
+
+			client.changeWorkingDirectory("tmp");
+			client.makeDirectory(directoryName);
+			client.changeWorkingDirectory(directoryName);
+
+			ByteArrayInputStream stream = new ByteArrayInputStream(xml.getBytes());
+
+			boolean storeFile = client.storeFile("submission.xml", stream);
+
+			for (NcbiBioSampleFiles bsFile : submission.getBioSampleFiles()) {
+				for (SequenceFile file : bsFile.getFiles()) {
+					InputStream fileStream = Files.newInputStream(file.getFile());
+
+					client.storeFile(file.getFileName(), fileStream);
+
+					fileStream.close();
+				}
+
+				for (SequenceFilePair pair : bsFile.getPairs()) {
+
+					SequenceFile file = pair.getForwardSequenceFile();
+					InputStream fileStream = Files.newInputStream(file.getFile());
+					client.storeFile(file.getFileName(), fileStream);
+					fileStream.close();
+
+					file = pair.getReverseSequenceFile();
+					fileStream = Files.newInputStream(file.getFile());
+					client.storeFile(file.getFileName(), fileStream);
+					fileStream.close();
+				}
+
+			}
+
+			stream.close();
+
+			client.disconnect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
