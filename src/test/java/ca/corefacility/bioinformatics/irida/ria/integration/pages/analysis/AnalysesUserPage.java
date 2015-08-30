c18374a784aaf4b08b41e7d44ab0feeeecf19f85
Josh Adam <josh.s.adam@gmail.com>
2015-08-30 13:15:16 -0500

    Fixed broken class in test.

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/analysis/AnalysesUserPage.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/analysis/AnalysesUserPage.java
@@ -44,7 +44,7 @@ public class AnalysesUserPage extends AbstractPage {
 	@FindBy(className = "download-analysis-btn")
 	private List<WebElement> downloadAnalysisBtn;
 
-	@FindBy(tagName = "md-progress-linear")
+	@FindBy(tagName = "progress")
 	private List<WebElement> progressBars;
 
 	public AnalysesUserPage(WebDriver driver) {
