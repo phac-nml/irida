12f70089b3d0d03b76b4388f8b31db3d4164132a
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-10-27 13:13:46 -0500

    Updated documentation

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/datatables/ProjectsDatatableUtils.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/datatables/ProjectsDatatableUtils.java
@@ -2,16 +2,24 @@ package ca.corefacility.bioinformatics.irida.ria.web.components.datatables;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.github.dandelion.datatables.core.ajax.ColumnDef;
+import com.github.dandelion.datatables.extras.spring3.ajax.DatatablesParams;
 
 /**
- * Created by josh on 2015-10-22.
+ * Static methods to help create Searching criteria for the projects datatable.
  */
 public class ProjectsDatatableUtils extends DatatablesUtils  {
 	public static final int NAME_COLUMN = 1;
 	public static final int ORGANISM_COLUMN = 2;
 
+	/**
+	 * Generate a {@link Map<String, String>} of search criteria.
+	 *
+	 * @param columnDefs {@link List<ColumnDef>} {@link DatatablesParams} list of column definitions
+	 * @return {@link Map<String, String>} of search criteria
+	 */
 	public static HashMap<String, String> generateSearchMap(List<ColumnDef> columnDefs) {
 		HashMap<String, String> searchMap = new HashMap<>();
 		// 1. Name
