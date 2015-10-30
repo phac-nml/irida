75ab737141e58ce19a000819d6bb07a2976182bd
Josh Adam <josh.adam@phac-aspc.gc.ca>
2015-10-30 15:05:35 -0500

    Updated to use a set

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/datatables/ProjectsDatatableUtils.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/datatables/ProjectsDatatableUtils.java
@@ -3,18 +3,19 @@ package ca.corefacility.bioinformatics.irida.ria.web.components.datatables;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.github.dandelion.datatables.core.ajax.ColumnDef;
 import com.github.dandelion.datatables.extras.spring3.ajax.DatatablesParams;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
 /**
  * Static methods to help create Searching criteria for the projects datatable.
  */
 public class ProjectsDatatableUtils extends DatatablesUtils {
-	private static final Map<String, Boolean> filterableProjectAttributes = ImmutableMap.of(
-			"name", true,
-			"organism", true
+	private static final Set<String> filterableProjectAttributes = ImmutableSet.of(
+			"name",
+			"organism"
 	);
 
 
@@ -29,7 +30,7 @@ public class ProjectsDatatableUtils extends DatatablesUtils {
 	public static HashMap<String, String> generateSearchMap(List<ColumnDef> columnDefs) {
 		HashMap<String, String> searchMap = new HashMap<>();
 
-		columnDefs.stream().filter(def -> def.isFiltered() && filterableProjectAttributes.get(def.getName()))
+		columnDefs.stream().filter(def -> def.isFiltered() && filterableProjectAttributes.contains(def.getName()))
 				.forEach(def -> searchMap.put(def.getName(), def.getSearch()));
 		return searchMap;
 	}
