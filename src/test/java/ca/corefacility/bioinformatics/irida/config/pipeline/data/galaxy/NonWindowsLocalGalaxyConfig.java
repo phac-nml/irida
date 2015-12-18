97da7bae28c0908bd2067b392529ef7c5840a2ac
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2015-12-18 09:11:35 -0600

    Added lines to remove cloudlaunch.py file from older Galaxy so it runs in newer Ubuntu

--- src/test/java/ca/corefacility/bioinformatics/irida/config/pipeline/data/galaxy/NonWindowsLocalGalaxyConfig.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/config/pipeline/data/galaxy/NonWindowsLocalGalaxyConfig.java
@@ -141,6 +141,14 @@ public class NonWindowsLocalGalaxyConfig implements LocalGalaxyConfig {
 		
 				BootStrapper bootStrapper = downloadGalaxy(localGalaxy, repositoryURL, branchName, revisionHash);
 				localGalaxy.setBootStrapper(bootStrapper);
+				
+				// A horrible hack to make older Galaxy work on newer Ubuntu machines.
+				// Need to stop loading of "cloudlaunch.py" which loads up older bioblend.
+				// Easiest way is to delete this file.  This must be fixed after we upgrade Galaxy
+				Path cloudLaunchPyPath = bootStrapper.getRoot().toPath().resolve(Paths.get("lib", "galaxy", "webapps", "galaxy", "controllers", "cloudlaunch.py"));
+				logger.info("Deleting file " + cloudLaunchPyPath + " to fix launching old Galaxy in new Ubuntu");
+				Files.delete(cloudLaunchPyPath);
+				// end horrible hack
 		
 				GalaxyProperties galaxyProperties = setupGalaxyProperties(localGalaxy,revisionHash,databaseURL, databaseConnectionString);
 				localGalaxy.setGalaxyProperties(galaxyProperties);
