f25bef3f62b92208c6a62873cb5bd89c7351116b
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2015-12-18 14:43:01 -0600

    First cut at deleting analysis submissions.

--- src/main/webapp/resources/js/pages/analyses/analyses-list.js
+++ src/main/webapp/resources/js/pages/analyses/analyses-list.js
@@ -1,7 +1,7 @@
 /*
  * Used by the Analyses Listing Datatable (analyses.html)
  */
-var analysesTable = (function(tl, moment){
+var analysesTable = (function(tl, moment, $http){
     /**
      * Fomat the content of the Analysis Name column to link to the analysis.
      * @param data - data for the name cell.
@@ -46,18 +46,41 @@ var analysesTable = (function(tl, moment){
     }
 
     function downloadLink(data,type,full){
+    	var buttons = "<div class='btn-group pull-right'>"
         if(full.submission.analysisState === 'COMPLETED'){
-          return "<a class='btn btn-default btn-xs download-analysis-btn' href='" + PAGE.URLS.download + full.id + "'><i class='fa fa-download' /></a>";
+          buttons += "<a class='btn btn-default btn-xs download-analysis-btn' href='" + PAGE.URLS.download + full.id + "'><span class='fa fa-download' /></a>";
         }
-        else{
-          return "";
-        }
+    	
+    	buttons += "<button type='button' data-toggle='modal' data-target='#deleteConfirmModal' class='btn btn-default btn-xs delete-analysis-btn' data-analysis-id='" + full.id + "'><span class='fa fa-trash'></button></div>";
+    	return buttons;
+    }
+        
+    function deleteLinkCallback(row, data, index) {
+    	$(row).find(".delete-analysis-btn").click(function () {
+    		$("#deleteConfirmModal").on("show.bs.modal", function (event) {
+    			var modal = $(this);
+    			modal.find("#delete-analysis-button").click(function() {
+		    		$.ajax({
+		    			url: PAGE.URLS.deleteUrl + data.id,
+		    			type: 'DELETE',
+		    			success: function (result) {
+		    				oTable_analysisTable.ajax.reload();
+		    				notifications.show({
+		    					       'msg' : result.result
+		    					     });
+		    				modal.modal('hide');
+		    			}
+		    		});
+    			});
+    		});
+    	});
     }
 
     return {
         createLinkButton: createLinkButton,
         getHumanTime: getHumanTime,
         displayProgress: displayProgress,
-        downloadLink: downloadLink
+        downloadLink: downloadLink,
+        deleteLinkCallback: deleteLinkCallback
     };
 })(window.TL, window.moment);
