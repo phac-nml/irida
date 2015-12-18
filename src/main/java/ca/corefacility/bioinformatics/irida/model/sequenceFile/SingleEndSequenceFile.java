51f901711b0bbf98da74b1b2358172137322ec05
tom <thomas.matthews@phac-aspc.gc.ca>
2015-12-18 10:57:09 -0600

    added singleendsequencefile to represent a file without a pair

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/sequenceFile/SingleEndSequenceFile.java
@@ -0,0 +1,60 @@
+package ca.corefacility.bioinformatics.irida.model.sequenceFile;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.collect.Lists;
+
+/**
+ * {@link SequencingObject} from a single ended sequence run. This class will
+ * contain only one SequenceFile.
+ */
+@Entity
+@Table(name = "sequence_file_single_end")
+@EntityListeners(AuditingEntityListener.class)
+@Audited
+public class SingleEndSequenceFile extends SequencingObject {
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@NotNull
+	private SequenceFile file;
+
+	/**
+	 * Throws {@link UnsupportedOperationException} because you should not be
+	 * able to update a file.
+	 */
+	@JsonIgnore
+	@Override
+	public void setModifiedDate(Date modifiedDate) {
+		throw new UnsupportedOperationException("Cannot update a sequence file");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getLabel() {
+		return file.getLabel();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Collection<SequenceFile> getPaths() {
+		return Lists.newArrayList(file);
+	}
+
+}
