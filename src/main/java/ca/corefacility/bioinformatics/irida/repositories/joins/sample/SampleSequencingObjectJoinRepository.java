dd24c5c270cbde08b7fcc66c6d6095657ea16c7d
tom <thomas.matthews@phac-aspc.gc.ca>
2015-12-21 13:55:55 -0600

    added repo for managing sampleseqobjectjoinst

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/joins/sample/SampleSequencingObjectJoinRepository.java
@@ -0,0 +1,9 @@
+package ca.corefacility.bioinformatics.irida.repositories.joins.sample;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.corefacility.bioinformatics.irida.model.sample.SampleSequencingObjectJoin;
+
+public interface SampleSequencingObjectJoinRepository extends CrudRepository<SampleSequencingObjectJoin, Long> {
+
+}
