492f9aae095f410e482721f701c8ea790f34d954
tom <thomas.matthews@phac-aspc.gc.ca>
2015-12-21 13:56:11 -0600

    added method for adding seqobject to sample

--- src/main/java/ca/corefacility/bioinformatics/irida/service/SequencingObjectService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/SequencingObjectService.java
@@ -1,7 +1,10 @@
 package ca.corefacility.bioinformatics.irida.service;
 
+import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.sample.SampleSequencingObjectJoin;
 import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequencingObject;
 
 public interface SequencingObjectService extends CRUDService<Long, SequencingObject> {
 
+	public SampleSequencingObjectJoin createSequencingObjectInSample(SequencingObject seqObject, Sample sample);
 }
--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequencingObjectServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequencingObjectServiceImpl.java
@@ -1,18 +1,66 @@
 package ca.corefacility.bioinformatics.irida.service.impl;
 
+import java.util.Set;
+
+import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import ca.corefacility.bioinformatics.irida.exceptions.EntityExistsException;
+import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.sample.SampleSequencingObjectJoin;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequencingObject;
+import ca.corefacility.bioinformatics.irida.repositories.joins.sample.SampleSequencingObjectJoinRepository;
+import ca.corefacility.bioinformatics.irida.repositories.sequencefile.SequenceFileRepository;
 import ca.corefacility.bioinformatics.irida.repositories.sequencefile.SequencingObjectRepository;
+import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 
 @Service
-public class SequencingObjectServiceImpl extends CRUDServiceImpl<Long, SequencingObject> {
+public class SequencingObjectServiceImpl extends CRUDServiceImpl<Long, SequencingObject> implements
+		SequencingObjectService {
 
-	public SequencingObjectServiceImpl(SequencingObjectRepository repository, Validator validator,
-			Class<SequencingObject> valueType) {
-		super(repository, validator, valueType);
+	private final SampleSequencingObjectJoinRepository ssoRepository;
+	private final SequenceFileRepository sfFileRepository;
+
+	@Autowired
+	public SequencingObjectServiceImpl(SequencingObjectRepository repository,
+			SampleSequencingObjectJoinRepository ssoRepository, SequenceFileRepository sfFileRepository,
+			Validator validator) {
+		super(repository, validator, SequencingObject.class);
+		this.ssoRepository = ssoRepository;
+		this.sfFileRepository = sfFileRepository;
+	}
+
+	@Override
+	@Transactional
+	@PreAuthorize("hasAnyRole('ROLE_SEQUENCER', 'ROLE_USER')")
+	public SequencingObject create(SequencingObject object) throws ConstraintViolationException, EntityExistsException {
+		return super.create(object);
+	}
+
+	@Override
+	@Transactional
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SEQUENCER') or hasPermission(#sample, 'canReadSample')")
+	public SampleSequencingObjectJoin createSequencingObjectInSample(SequencingObject seqObject, Sample sample) {
+
+		// ensure the files are persisted
+		Set<SequenceFile> files = seqObject.getFiles();
+		for (SequenceFile file : files) {
+			file = sfFileRepository.save(file);
+		}
+		seqObject.setFiles(files);
+
+		// create the sequencing object
+		seqObject = create(seqObject);
+
+		// save the new join
+		SampleSequencingObjectJoin sampleSequencingObjectJoin = new SampleSequencingObjectJoin(sample, seqObject);
+		return ssoRepository.save(sampleSequencingObjectJoin);
 	}
 
 }
