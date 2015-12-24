eb3aee9e6b28ed4a994c42631c9192b8cce1bd99
tom <thomas.matthews@phac-aspc.gc.ca>
2015-12-24 12:46:36 -0600

    changed sequencefilepair to cascade persist its files.  Not manually saving the files anymore on create

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequencingObjectServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/SequencingObjectServiceImpl.java
@@ -1,7 +1,5 @@
 package ca.corefacility.bioinformatics.irida.service.impl;
 
-import java.util.Set;
-
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 
@@ -13,10 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 import ca.corefacility.bioinformatics.irida.exceptions.EntityExistsException;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.model.sample.SampleSequencingObjectJoin;
-import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequencingObject;
 import ca.corefacility.bioinformatics.irida.repositories.joins.sample.SampleSequencingObjectJoinRepository;
-import ca.corefacility.bioinformatics.irida.repositories.sequencefile.SequenceFileRepository;
 import ca.corefacility.bioinformatics.irida.repositories.sequencefile.SequencingObjectRepository;
 import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 
@@ -25,15 +21,12 @@ public class SequencingObjectServiceImpl extends CRUDServiceImpl<Long, Sequencin
 		SequencingObjectService {
 
 	private final SampleSequencingObjectJoinRepository ssoRepository;
-	private final SequenceFileRepository sfFileRepository;
 
 	@Autowired
 	public SequencingObjectServiceImpl(SequencingObjectRepository repository,
-			SampleSequencingObjectJoinRepository ssoRepository, SequenceFileRepository sfFileRepository,
-			Validator validator) {
+			SampleSequencingObjectJoinRepository ssoRepository, Validator validator) {
 		super(repository, validator, SequencingObject.class);
 		this.ssoRepository = ssoRepository;
-		this.sfFileRepository = sfFileRepository;
 	}
 
 	@Override
@@ -47,14 +40,6 @@ public class SequencingObjectServiceImpl extends CRUDServiceImpl<Long, Sequencin
 	@Transactional
 	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_SEQUENCER') or hasPermission(#sample, 'canReadSample')")
 	public SampleSequencingObjectJoin createSequencingObjectInSample(SequencingObject seqObject, Sample sample) {
-
-		// ensure the files are persisted
-		Set<SequenceFile> files = seqObject.getFiles();
-		for (SequenceFile file : files) {
-			file = sfFileRepository.save(file);
-		}
-		seqObject.setFiles(files);
-
 		// create the sequencing object
 		seqObject = create(seqObject);
 
