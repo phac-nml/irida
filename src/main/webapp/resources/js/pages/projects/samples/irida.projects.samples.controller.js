09564716ec404d246546891a1a8720b3c757adeb
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-01-12 09:27:57 -0600

    Trying to get custom dom working

--- src/main/webapp/resources/js/pages/projects/samples/irida.projects.samples.controller.js
+++ src/main/webapp/resources/js/pages/projects/samples/irida.projects.samples.controller.js
@@ -1,12 +1,13 @@
 (function (ng) {
 	"use strict";
 
-	function SamplesController(service, DTOptionsBuilder) {
+	function SamplesController($scope, $templateCache, service, DTOptionsBuilder) {
 		var vm = this;
 		vm.selected = {};
+		vm.dtInstance = {};
 
 		vm.dtOptions = DTOptionsBuilder.newOptions()
-			.withDOM("<'row filter-row'<'col-sm-6'2><'col-sm-6'0f>><'row datatables-active-filters'1><'panel panel-default''<'row'<'col-sm-12'tr>>><'row'<'col-sm-3'l><'col-sm-6'p><'col-sm-3 text-right'i>>");
+			.withDOM("<'row filter-row'<'col-sm-9 buttons'><'col-sm-3'0f>><'row datatables-active-filters'1><'panel panel-default''<'row'<'col-sm-12'tr>>><'row'<'col-sm-3'l><'col-sm-6'p><'col-sm-3 text-right'i>>");
 
 		vm.updateAllSelected = function () {
 			vm.samples.forEach(function (sample, index) {
@@ -25,9 +26,16 @@
 		service.fetchSamples().then(function (samples) {
 			vm.samples = samples;
 		});
+
+
+		var buttons = $templateCache.get("buttons.html");
+
+		$scope.$on('event:dataTableLoaded', function () {
+			console.log("You got it!");
+		});
 	}
 
 	ng.module("irida.projects.samples.controller", ["irida.projects.samples.service", "datatables"])
-		.controller("SamplesController", ["samplesService", "DTOptionsBuilder", SamplesController]);
+		.controller("SamplesController", ["$scope", "$templateCache", "samplesService", "DTOptionsBuilder", SamplesController]);
 	;
 })(window.angular);
