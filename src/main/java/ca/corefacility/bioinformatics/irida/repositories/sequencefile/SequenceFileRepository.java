0c1a85ba17c54e069414539ea6c354d75e702b84
tom <thomas.matthews@phac-aspc.gc.ca>
2016-01-19 14:32:56 -0600

    moving sequencing_run_id to sequencing_object

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sequencefile/SequenceFileRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sequencefile/SequenceFileRepository.java
@@ -24,6 +24,6 @@ public interface SequenceFileRepository extends FilesystemSupplementedRepository
 	 *            the run to load the files for.
 	 * @return the files created as part of a run.
 	 */
-	@Query("select f from SequenceFile f where f.sequencingRun = ?1")
+	@Query("select f from SequenceFile f")
 	public Set<SequenceFile> findSequenceFilesForSequencingRun(SequencingRun sequencingRun);
 }
