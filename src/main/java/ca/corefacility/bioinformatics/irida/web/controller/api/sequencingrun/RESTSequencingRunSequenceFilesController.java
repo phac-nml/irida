0118ff4bae909493f8afffaea290e487c8effae0
tom <thomas.matthews@phac-aspc.gc.ca>
2016-01-19 11:30:01 -0600

    changed method on controller to use seqobject rather than file when adding ot run

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/sequencingrun/RESTSequencingRunSequenceFilesController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/sequencingrun/RESTSequencingRunSequenceFilesController.java
@@ -1,4 +1,3 @@
-
 package ca.corefacility.bioinformatics.irida.web.controller.api.sequencingrun;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -20,7 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import ca.corefacility.bioinformatics.irida.model.run.MiseqRun;
 import ca.corefacility.bioinformatics.irida.model.run.SequencingRun;
 import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFile;
-import ca.corefacility.bioinformatics.irida.service.SequenceFileService;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequencingObject;
+import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 import ca.corefacility.bioinformatics.irida.service.SequencingRunService;
 import ca.corefacility.bioinformatics.irida.web.controller.api.RESTGenericController;
 
@@ -32,20 +32,21 @@ import com.google.common.net.HttpHeaders;
 @Controller
 public class RESTSequencingRunSequenceFilesController {
 
-    private SequencingRunService miseqRunService;
-    private SequenceFileService sequencefileService;
-    /**
-     * key used in map when adding sequencefile to miseqrun.
-     */
-    public static final String SEQUENCEFILE_ID_KEY = "sequenceFileId";
-    
-    @Autowired
-    public RESTSequencingRunSequenceFilesController(SequencingRunService service, SequenceFileService sequencefileService) {
-        this.miseqRunService = service;
-        this.sequencefileService = sequencefileService;
-    }
-    
-    /**
+	private SequencingRunService miseqRunService;
+	private SequencingObjectService sequencingObjectService;
+	/**
+	 * key used in map when adding sequencefile to miseqrun.
+	 */
+	public static final String SEQUENCEFILE_ID_KEY = "sequenceFileId";
+
+	@Autowired
+	public RESTSequencingRunSequenceFilesController(SequencingRunService service,
+			SequencingObjectService sequencingObjectService) {
+		this.miseqRunService = service;
+		this.sequencingObjectService = sequencingObjectService;
+	}
+
+	/**
 	 * Add a relationship between a {@link MiseqRun} and a {@link SequenceFile}.
 	 *
 	 * @param sequencingrunId
@@ -57,36 +58,33 @@ public class RESTSequencingRunSequenceFilesController {
 	 *            a reference to the response.
 	 * @return a response indicating that the collection was modified.
 	 */
-    @RequestMapping(value = "/api/sequencingrun/{sequencingrunId}/sequenceFiles", method = RequestMethod.POST)
-    public ModelMap addSequenceFileToMiseqRun(@PathVariable Long sequencingrunId,
-    		@RequestBody Map<String, String> representation, HttpServletResponse response) {
-    	ModelMap modelMap = new ModelMap();
-    	String stringId = representation.get(SEQUENCEFILE_ID_KEY);
-    	long seqId = Long.parseLong(stringId);
-    	// first, get the SequenceFile
-    	SequenceFile file = sequencefileService.read(seqId);
-    	// then, get the miseq run
-    	SequencingRun run = miseqRunService.read(sequencingrunId);
-    	// then add the user to the project with the specified role.
-    	miseqRunService.addSequenceFileToSequencingRun(run, file);
-    	
-    	MiseqRun miseqRun;
-    	if(run instanceof MiseqRun)
-    	{
-    		miseqRun = (MiseqRun)run;
-    	}
-    	else
-    	{
-    		throw new IllegalArgumentException("The sequencing run ID must correspond to a a valid MiSeq sequence");
-    	}
-    	Link seqFileLocation = linkTo(RESTSequencingRunController.class).slash(sequencingrunId).slash("sequenceFiles").slash(seqId).withSelfRel();
-    	miseqRun.add(seqFileLocation);
-    	modelMap.addAttribute(RESTGenericController.RESOURCE_NAME, miseqRun);
-    	response.addHeader(HttpHeaders.LOCATION, seqFileLocation.getHref());
-    	response.setStatus(HttpStatus.CREATED.value());
+	@RequestMapping(value = "/api/sequencingrun/{sequencingrunId}/sequenceFiles", method = RequestMethod.POST)
+	public ModelMap addSequenceFileToMiseqRun(@PathVariable Long sequencingrunId,
+			@RequestBody Map<String, String> representation, HttpServletResponse response) {
+		ModelMap modelMap = new ModelMap();
+		String stringId = representation.get(SEQUENCEFILE_ID_KEY);
+		long seqId = Long.parseLong(stringId);
+		// first, get the SequenceFile
+		SequencingObject sequencingObject = sequencingObjectService.read(seqId);
+		// then, get the miseq run
+		SequencingRun run = miseqRunService.read(sequencingrunId);
+		// then add the user to the project with the specified role.
+		miseqRunService.addSequenceFileToSequencingRun(run, sequencingObject);
 
-    	return modelMap;
-    }
-    
-    
+		MiseqRun miseqRun;
+		if (run instanceof MiseqRun) {
+			miseqRun = (MiseqRun) run;
+		} else {
+			throw new IllegalArgumentException("The sequencing run ID must correspond to a a valid MiSeq sequence");
+		}
+		Link seqFileLocation = linkTo(RESTSequencingRunController.class).slash(sequencingrunId).slash("sequenceFiles")
+				.slash(seqId).withSelfRel();
+		miseqRun.add(seqFileLocation);
+		modelMap.addAttribute(RESTGenericController.RESOURCE_NAME, miseqRun);
+		response.addHeader(HttpHeaders.LOCATION, seqFileLocation.getHref());
+		response.setStatus(HttpStatus.CREATED.value());
+
+		return modelMap;
+	}
+
 }
