c2c935909b24875d1374bf004f88788e24adc0ba
johnc <jcuratcha_@live.com>
2016-01-29 14:38:16 -0600

    Run tests in separate jobs (reverted other changes)

--- scripts/integration-tests-rest.sh
+++ scripts/integration-tests-rest.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
-echo 'drop database if exists irida_rest_test; drop database if exists irida_rest_galaxy_test; create database irida_rest_test;create database irida_rest_galaxy_test;' | mysql -u test -ptest
-
-xvfb-run -a mvn verify -Prest_testing -Djdbc.url="jdbc:mysql://localhost:3306/irida_rest_test" -Djetty.port=8080 -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_rest_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock" #> rest_out.txt
+rm -rf /tmp/gxbootstrap*
+rm -rf /tmp/shed_tools/
+pkill -u gitlab-runner -f "python ./scripts/paster.py" || true
+echo 'drop database if exists irida_test; drop database if exists irida_galaxy_test; create database irida_test;create database irida_galaxy_test;' | mysql -u test -ptest
+pushd lib
+./install-libs.sh
+popd
+xvfb-run mvn clean verify -Prest_testing -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock"
--- scripts/integration-tests-ui.sh
+++ scripts/integration-tests-ui.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
-echo 'drop database if exists irida_ui_test; drop database if exists irida_ui_galaxy_test; create database irida_ui_test;create database irida_ui_galaxy_test;' | mysql -u test -ptest
-
-xvfb-run -a mvn verify -Pui_testing -Djdbc.url="jdbc:mysql://localhost:3306/irida_ui_test" -Djetty.port=8082 -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_ui_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock" #> ui_out.txt
+rm -rf /tmp/gxbootstrap*
+rm -rf /tmp/shed_tools/
+pkill -u gitlab-runner -f "python ./scripts/paster.py" || true
+echo 'drop database if exists irida_test; drop database if exists irida_galaxy_test; create database irida_test;create database irida_galaxy_test;' | mysql -u test -ptest
+pushd lib
+./install-libs.sh
+popd
+xvfb-run mvn clean verify -Pui_testing -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock"
