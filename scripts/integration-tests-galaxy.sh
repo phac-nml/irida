25773d5018459eda9bfea831c72f68a6cfe457a1
johnc <jcuratcha_@live.com>
2016-01-29 11:50:12 -0600

    Fixed jdbc port for galaxy profile

--- scripts/integration-tests-galaxy.sh
+++ scripts/integration-tests-galaxy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#rm -rf /tmp/gxbootstrap*
-#rm -rf /tmp/shed_tools/
-#pkill -u gitlab-runner -f "python ./scripts/paster.py" || true
+
 echo 'drop database if exists irida_galaxygroup_test; drop database if exists irida_galaxygroup_galaxy_test; create database irida_galaxygroup_test;create database irida_galaxygroup_galaxy_test;' | mysql -u test -ptest
-#pushd lib
-#./install-libs.sh
-#popd
+
 xvfb-run -a mvn verify -Pgalaxy_testing -Djdbc.url="jdbc:mysql://localhost:3306/irida_galaxygroup_test" -Djetty.port=8083 -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxygroup_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock" #> galaxy_out.txt
--- scripts/integration-tests-service.sh
+++ scripts/integration-tests-service.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-#rm -rf /tmp/gxbootstrap*
-#rm -rf /tmp/shed_tools/
-#pkill -u gitlab-runner -f "python ./scripts/paster.py" || true
+
 echo 'drop database if exists irida_service_test; drop database if exists irida_service_galaxy_test; create database irida_service_test;create database irida_service_galaxy_test;' | mysql -u test -ptest
-#pushd lib
-#./install-libs.sh
-#popd
+
 xvfb-run -a mvn verify -Pservice_testing -Djdbc.url="jdbc:mysql://localhost:3306/irida_service_test" -Djetty.port=8081 -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_service_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock" #> service_out.txt
