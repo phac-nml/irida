b860d9053df7717d3f2d57121721603d1e124866
johnc <jcuratcha_@live.com>
2016-01-29 11:49:24 -0600

    Fixed jdbc port for galaxy profile

--- scripts/integration-tests-galaxy.sh
+++ scripts/integration-tests-galaxy.sh
@@ -6,4 +6,4 @@ echo 'drop database if exists irida_galaxygroup_test; drop database if exists ir
 #pushd lib
 #./install-libs.sh
 #popd
-xvfb-run -a mvn verify -Pgalaxy_testing -Djdbc.url="jdbc:mysql://localhost:3310/irida_galaxygroup_test" -Djetty.port=8083 -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxygroup_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock" #> galaxy_out.txt
+xvfb-run -a mvn verify -Pgalaxy_testing -Djdbc.url="jdbc:mysql://localhost:3306/irida_galaxygroup_test" -Djetty.port=8083 -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxygroup_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock" #> galaxy_out.txt
