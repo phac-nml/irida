d4c1f0d4c3bcf8b02cb30b737839cb3b7dc77f85
johnc <jcuratcha_@live.com>
2016-02-01 09:47:39 -0600

    Moved install-libs.sh step into before_script, changed before-script to before_script

--- scripts/integration-tests-galaxy.sh
+++ scripts/integration-tests-galaxy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-pushd lib
-./install-libs.sh
-popd
 xvfb-run mvn clean verify -Pgalaxy_testing -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock"
