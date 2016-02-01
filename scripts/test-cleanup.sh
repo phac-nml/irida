44c519e541761036bb7e6aa4e4bd0ef0e2d23d80
johnc <jcuratcha_@live.com>
2016-02-01 08:44:57 -0600

    Added cleanup task before tests are run, moved cleanup steps into separate script

---
+++ scripts/test-cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf /tmp/gxbootstrap*
+rm -rf /tmp/shed_tools/
+pkill -u gitlab-runner -f "python ./scripts/paster.py" || true
+echo 'drop database if exists irida_test; drop database if exists irida_galaxy_test; create database irida_test;create database irida_galaxy_test;' | mysql -u test -ptest
