6c0399dac27df7cc2a962767e5b253309062a5ab
johnc <jcuratcha_@live.com>
2016-02-22 13:35:08 -0600

    Removed unused imports, added script for testing only one test class or test profile

--- src/main/java/ca/corefacility/bioinformatics/irida/config/data/IridaApiJdbcDataSourceConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/data/IridaApiJdbcDataSourceConfig.java
@@ -1,7 +1,6 @@
 package ca.corefacility.bioinformatics.irida.config.data;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
