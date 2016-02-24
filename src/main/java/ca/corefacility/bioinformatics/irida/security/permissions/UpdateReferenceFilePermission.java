a3c217da40a4e5fc37e8642870dd34233ebbf226
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2016-02-24 14:59:45 -0600

    Rewrite update reference file to use project owner permission.

--- src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/UpdateReferenceFilePermission.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/UpdateReferenceFilePermission.java
@@ -6,16 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import ca.corefacility.bioinformatics.irida.model.enums.ProjectRole;
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
-import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectUserJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.project.ReferenceFile;
-import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.repositories.joins.project.ProjectReferenceFileJoinRepository;
-import ca.corefacility.bioinformatics.irida.repositories.joins.project.ProjectUserJoinRepository;
 import ca.corefacility.bioinformatics.irida.repositories.referencefile.ReferenceFileRepository;
-import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
 
 /**
  * Confirms that the authenticated user is allowed to modify a reference file.
@@ -27,18 +22,16 @@ public class UpdateReferenceFilePermission extends BasePermission<ReferenceFile,
 
 	public static final String PERMISSION_PROVIDED = "canUpdateReferenceFile";
 
-	private final UserRepository userRepository;
-	private final ProjectUserJoinRepository pujRepository;
 	private final ProjectReferenceFileJoinRepository prfRepository;
+	private final ProjectOwnerPermission projectOwnerPermission;
 
 	@Autowired
-	public UpdateReferenceFilePermission(ReferenceFileRepository referenceFileRepository,
-			UserRepository userRepository, ProjectUserJoinRepository pujRepository,
-			ProjectReferenceFileJoinRepository prfRepository) {
+	public UpdateReferenceFilePermission(final ReferenceFileRepository referenceFileRepository,
+			final ProjectReferenceFileJoinRepository prfRepository,
+			final ProjectOwnerPermission projectOwnerPermission) {
 		super(ReferenceFile.class, Long.class, referenceFileRepository);
-		this.userRepository = userRepository;
-		this.pujRepository = pujRepository;
 		this.prfRepository = prfRepository;
+		this.projectOwnerPermission = projectOwnerPermission;
 	}
 
 	@Override
@@ -51,26 +44,12 @@ public class UpdateReferenceFilePermission extends BasePermission<ReferenceFile,
 	 */
 	@Override
 	protected boolean customPermissionAllowed(Authentication authentication, ReferenceFile targetDomainObject) {
-		// get the logged in user
-		User user = userRepository.loadUserByUsername(authentication.getName());
 		// get the projects for the file
 		List<Join<Project, ReferenceFile>> findProjectsForReferenceFile = prfRepository
 				.findProjectsForReferenceFile(targetDomainObject);
 
-		// for each project
-		for (Join<Project, ReferenceFile> prJoin : findProjectsForReferenceFile) {
-			// get the users on the project
-			List<Join<Project, User>> usersForProject = pujRepository.getUsersForProject(prJoin.getSubject());
-			for (Join<Project, User> pj : usersForProject) {
-				ProjectUserJoin puJoin = (ProjectUserJoin) pj;
-				// if the user is on the project, they can read the file
-				if (puJoin.getObject().equals(user) && puJoin.getProjectRole().equals(ProjectRole.PROJECT_OWNER)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
+		return findProjectsForReferenceFile.stream()
+				.anyMatch(j -> projectOwnerPermission.isAllowed(authentication, j.getSubject()));
 	}
 
 }
