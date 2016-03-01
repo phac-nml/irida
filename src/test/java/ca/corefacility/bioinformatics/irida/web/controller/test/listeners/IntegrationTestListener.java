216403ecf2230722ed9e7e4f6f2fbc4ba0ee9102
johnc <jcuratcha_@live.com>
2016-03-01 14:42:50 -0600

    removed `Scanner` when checking for profile, instead using `Streams` to read the `active-profile.txt` file

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/listeners/IntegrationTestListener.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/listeners/IntegrationTestListener.java
@@ -12,11 +12,14 @@ import org.slf4j.LoggerFactory;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 /**
  * Global settings for all integration tests.
@@ -48,6 +51,9 @@ public class IntegrationTestListener extends RunListener {
 	 * For starting chrome only when running individual tests,
 	 * running the ui_testing profile, or running all_testing
 	 *
+	 * Does this by reading the `active-profile.txt` file, which is output
+	 * by the `help:active-profiles` maven plugin/goal
+	 *
 	 * Essentially preventing ChromeDriver from running when it
 	 * isn't required for the tests.
 	 *
@@ -56,23 +62,14 @@ public class IntegrationTestListener extends RunListener {
 	 */
 	public boolean isRunningUITests() {
 		try {
-			Path path = Paths.get("src/test/resources/active-profile.txt");
-			final Scanner scanner = new Scanner(path.toFile());
-
 			ArrayList<String> whitelist = new ArrayList<>();
-			//if adding any new profiles to black list, add them here!
 			whitelist.add("ui_testing");
 			whitelist.add("all_testing");
 
-			while (scanner.hasNext()) {
-				String[] line = scanner.nextLine().split("\\s+");
-				for (String str : line) {
-					if (whitelist.contains(str)) {
-						logger.debug("Current profile is whitelisted: running ChromeDriver.");
-						return true;
-					}
-				}
-			}
+			return Files.lines(Paths.get("src/test/resources/active-profile.txt"))
+					.map(line -> line.split("\\s+"))
+					.flatMap(Arrays::stream)
+					.anyMatch((str) -> whitelist.contains(str));
 		}
 		catch (Exception e) {
 			//If the file doesn't exist or we have problems reading the file, we don't want to stop
@@ -81,8 +78,7 @@ public class IntegrationTestListener extends RunListener {
 			logger.error("ERROR: " + e.toString() + " -> running ChromeDriver by default");
 		}
 
-		logger.debug("Current profile not in whitelist: disabling ChromeDriver");
-		return false;
+		return true;
 	}
 
 	/**
