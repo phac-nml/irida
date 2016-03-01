a37f630c1aaaa2c2add1cad1ac50c9849d2201af
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-03-01 14:45:15 -0600

    Updated to use server sided templating on remove modal.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectTemplatesController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectTemplatesController.java
@@ -3,6 +3,7 @@ package ca.corefacility.bioinformatics.irida.ria.web.projects;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ProjectTemplatesController {
 
 	@RequestMapping("/remove-modal")
-	public String getRemoveSamplesFromProjectModal(@RequestParam List<String> names) {
-
-		return "projects/templates/remove";
+	public String getRemoveSamplesFromProjectModal(@RequestParam(name = "names[]") List<String> names, Model model) {
+		model.addAttribute("names", names);
+		return "projects/templates/remove-modal.tmpl";
 	}
 }
