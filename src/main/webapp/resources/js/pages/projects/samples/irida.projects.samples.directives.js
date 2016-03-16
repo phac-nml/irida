2ec9261db26d4bccc45d921a2a6b6c6c248d1e69
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-03-16 09:02:43 -0500

    Working on the select2 for searching projects

--- src/main/webapp/resources/js/pages/projects/samples/irida.projects.samples.directives.js
+++ src/main/webapp/resources/js/pages/projects/samples/irida.projects.samples.directives.js
@@ -1,4 +1,4 @@
-(function(ng, $) {
+(function(ng, $, page) {
   "use strict";
 
   function projectsSelect2 ($timeout) {
@@ -7,7 +7,35 @@
       require: "ngModel",
       link: function(scope, elem, attrs, ctrl) {
         $(elem).select2({
+          minimumLength: 2,
+          ajax         : {
+            url: page.urls.projects.available,
+            dataType   : 'json',
+            quietMillis: 250,
+            data       : function (search, page) {
+              return {
+                term    : search, // search term
+                page    : page - 1, //zero based method
+                pageSize: 10
+              };
+            },
+            results    : function (data, page) {
+              var results = [];
 
+              var more = (page * 10) < data.total;
+
+              _.forEach(data.projects, function (p) {
+                if ($rootScope.projectId !== parseInt(p.identifier)) {
+                  results.push({
+                    id  : p.identifier,
+                    text: p.text || p.name
+                  });
+                }
+              });
+
+              return {results: results, more: more};
+            }
+          }
         });
       }
     };
@@ -15,4 +43,4 @@
 
   ng.module("irida.projects.samples.directives", ["irida.projects.samples.service"])
     .directive("projectsSelect2", ["$timeout", projectsSelect2]);
-}(window.angular, window.jQuery));
+}(window.angular, window.jQuery, window.PAGE));
