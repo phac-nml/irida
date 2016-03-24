35e880604ee04b7601c2d4959883a1ee8cceb94f
johnc <jcuratcha_@live.com>
2016-03-24 14:09:13 -0500

    Fixed permissions, wrote script to build then start docker container, then running Galaxy test suite.

--- scripts/galaxy-docker.sh
+++ scripts/galaxy-docker.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+#
+# Script for building and then starting a Docker-Galaxy instance
+#   specifically built for IRIDA's integration testing, then
+#   starting IRIDA's Galaxy integration tests
+#
 
+#pull build the docker image, or download it if it doesn't yet exist
+docker build jcuratcha/irida-galaxy-integration:0.1.1
 
-xvfb-run mvn clean verify -Pgalaxy_testing -Dtest.galaxy.database.connection="mysql://test:test@localhost/irida_galaxy_test?unix_socket=/var/run/mysqld/mysqld.sock"
+#run docker container and save the outputted container ID
+OUTPUT="$(docker run -d -p 48888:80 -v /Warehouse/Temporary/irida-john:/Warehouse/Temporary/irida-john jcuratcha/irida-galaxy-integration:0.1.1)"
+
+#run the test suite
+mvn clean verify -Pgalaxy_testing
+
+#kill the container with the long container id
+docker kill ${OUTPUT}
