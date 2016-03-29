0c9d77c6baf8064309820b8e0239995bf688439e
johnc <jcuratcha_@live.com>
2016-03-29 10:57:49 -0500

    Removed unneeded `sudo` when running galaxy test suite.

--- scripts/galaxy-docker.sh
+++ scripts/galaxy-docker.sh
@@ -10,7 +10,7 @@
 OUTPUT="$(docker run -d -p 48888:80 -v /Warehouse/Temporary/irida-john:/Warehouse/Temporary/irida-john jcuratcha/irida-galaxy-integration:0.1.1)"
 
 #run the test suite
-sudo -u curatchaj mvn clean verify -Pgalaxy_testing
+mvn clean verify -Pgalaxy_testing
 
 #kill the container with the long container id
 docker kill ${OUTPUT}
