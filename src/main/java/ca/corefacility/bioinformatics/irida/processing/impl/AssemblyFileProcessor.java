93c15ccae0f6126de54efe6cda91c0c7f9325e21
tom <thomas.matthews@phac-aspc.gc.ca>
2016-04-15 09:49:24 -0500

    changed some logging

--- src/main/java/ca/corefacility/bioinformatics/irida/processing/impl/AssemblyFileProcessor.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/processing/impl/AssemblyFileProcessor.java
@@ -31,7 +31,8 @@ public class AssemblyFileProcessor implements FileProcessor {
 	IridaWorkflowsService workflowsService;
 
 	public AssemblyFileProcessor(SequencingObjectRepository objectRepository,
-			AnalysisSubmissionRepository submissionRepository, IridaWorkflowsService workflowsService, UserRepository userRepository) {
+			AnalysisSubmissionRepository submissionRepository, IridaWorkflowsService workflowsService,
+			UserRepository userRepository) {
 		this.objectRepository = objectRepository;
 		this.submissionRepository = submissionRepository;
 		this.workflowsService = workflowsService;
@@ -40,35 +41,32 @@ public class AssemblyFileProcessor implements FileProcessor {
 
 	@Override
 	public void process(Long sequenceFileId) throws FileProcessorException {
-		logger.debug("Getting file object");
+		logger.trace("Setting up automated assembly");
 		SequencingObject sequencingObject = objectRepository.findOne(sequenceFileId);
 
 		User admin = userRepository.loadUserByUsername("admin");
-		
-		logger.debug("Getting workflow");
-		
+
 		IridaWorkflow defaultWorkflowByType;
 		try {
 			defaultWorkflowByType = workflowsService.getDefaultWorkflowByType(AnalysisType.ASSEMBLY_ANNOTATION);
 		} catch (IridaWorkflowNotFoundException e) {
-			throw new FileProcessorException("Cannot find assembly workflow",e);
+			throw new FileProcessorException("Cannot find assembly workflow", e);
 		}
 
 		UUID pipelineUUID = defaultWorkflowByType.getWorkflowIdentifier();
-		
+
 		Builder builder = new AnalysisSubmission.Builder(pipelineUUID);
 		AnalysisSubmission submission = builder.inputFilesPaired(Sets.newHashSet((SequenceFilePair) sequencingObject))
-				.name("testSubmission").build();
+				.name("Automated Assembly").build();
 		submission.setSubmitter(admin);
 
-		logger.debug("Creating submission");
 		submission = submissionRepository.save(submission);
-		
+
 		sequencingObject.setAutomatedAssembly(submission);
-		
+
 		objectRepository.save(sequencingObject);
 
-		logger.debug("all done");
+		logger.trace("Submission created: " + submission.getId());
 	}
 
 	@Override
