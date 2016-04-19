7700d5b4e29089203b652ccef440d713e88a249b
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-04-19 11:48:35 -0500

    Toolbar appears on the same line as the search box

--- src/main/webapp/resources/js/pages/projects/project-samples-table.js
+++ src/main/webapp/resources/js/pages/projects/project-samples-table.js
@@ -2,3 +2,13 @@
 /*global oTable_samplesTable */
 
 var table = oTable_samplesTable;
+
+// Add the toolbar to the table.
+var $target = $("[ng-app]");
+angular.element($target).injector().invoke(["$rootScope", "$compile", function ($rootScope, $compile) {
+  var toolbar = $(".filter-row > div")[0],
+    tools = $("#toolbar");
+  $(toolbar).html(function () {
+    return $compile(tools)($rootScope);
+  });
+}]);
