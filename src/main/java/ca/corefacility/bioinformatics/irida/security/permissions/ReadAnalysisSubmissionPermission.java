42f8d3e6238191a7fcf93f19b165f1f6036295ce
tom <thomas.matthews@phac-aspc.gc.ca>
2016-04-21 11:39:09 -0500

    updated analysis submission permission to allow reading for automated assemblies

--- src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/ReadAnalysisSubmissionPermission.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/ReadAnalysisSubmissionPermission.java
@@ -1,11 +1,14 @@
 package ca.corefacility.bioinformatics.irida.security.permissions;
 
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFilePair;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmission;
 import ca.corefacility.bioinformatics.irida.repositories.analysis.submission.AnalysisSubmissionRepository;
@@ -23,6 +26,7 @@ public class ReadAnalysisSubmissionPermission extends BasePermission<AnalysisSub
 	private static final String PERMISSION_PROVIDED = "canReadAnalysisSubmission";
 
 	private UserRepository userRepository;
+	private final ReadSequencingObjectPermission seqObjectPermission;
 
 	/**
 	 * Constructs a new {@link ReadAnalysisSubmissionPermission} with the given
@@ -32,12 +36,16 @@ public class ReadAnalysisSubmissionPermission extends BasePermission<AnalysisSub
 	 *            A {@link AnalysisSubmissionRepository}.
 	 * @param userRepository
 	 *            A {@link UserRepository}.
+	 * @param seqObjectPermission
+	 *            {@link ReadSequencingObjectPermission} to test if the
+	 *            {@link AnalysisSubmission} is part of an automated assembly
 	 */
 	@Autowired
 	public ReadAnalysisSubmissionPermission(AnalysisSubmissionRepository analysisSubmissionRepository,
-			UserRepository userRepository) {
+			UserRepository userRepository, ReadSequencingObjectPermission seqObjectPermission) {
 		super(AnalysisSubmission.class, Long.class, analysisSubmissionRepository);
 		this.userRepository = userRepository;
+		this.seqObjectPermission = seqObjectPermission;
 	}
 
 	/**
@@ -58,6 +66,22 @@ public class ReadAnalysisSubmissionPermission extends BasePermission<AnalysisSub
 		User u = userRepository.loadUserByUsername(authentication.getName());
 
 		if (analysisSubmission.getSubmitter().equals(u)) {
+			logger.trace("Permission GRANTED for [" + authentication + "] on analysis submission ["
+					+ analysisSubmission + "]");
+			return true;
+		}
+
+		/*
+		 * If the user isn't set it might be an automated submission. Check if
+		 * this analysis is the auto assembly for a file and if they can read
+		 * the file
+		 */
+		Set<SequenceFilePair> pairedInputFiles = analysisSubmission.getPairedInputFiles();
+
+		boolean anyMatch = pairedInputFiles.stream().filter(o -> o.getAutomatedAssembly().equals(analysisSubmission))
+				.anyMatch(p -> seqObjectPermission.customPermissionAllowed(authentication, p));
+
+		if (anyMatch) {
 			logger.trace("Permission GRANTED for [" + authentication + "] on analysis submission ["
 					+ analysisSubmission + "]");
 			return true;
