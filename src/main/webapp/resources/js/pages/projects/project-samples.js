501946dea39fc1f23a0936361f35f471183bc126
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-05-02 10:57:02 -0500

    Updated docs

--- src/main/webapp/resources/js/pages/projects/project-samples.js
+++ src/main/webapp/resources/js/pages/projects/project-samples.js
@@ -12,16 +12,24 @@
       });
       oTable_samplesTable.ajax.url(page.urls.samples.project + '/?' + params).load();
     };
+
+    /**
+     * This method is used to display / hide an associated project in the table.
+     * @param $event - click event
+     */
     AssociatedProjectsController.prototype.toggleAssociatedProject = function ($event) {
+      // If the input is click we need to prevent the default since we will select it later and need
+      // to know the state of it before the click event.
       if(!$($event.target).is('input')) {
         $event.preventDefault();
       }
+      // Need to stop all propagation since this is an anchor tag.
       $event.stopPropagation();
       $event.stopImmediatePropagation();
-      var target = $($event.currentTarget),
-          id = target.data("id"),
-          index = this.visible.indexOf(id),
-          checkbox = $(target.find("input:checkbox"));
+      var target   = $($event.currentTarget),
+          id       = target.data("id"), // This is the id for the project.
+          index    = this.visible.indexOf(id), // Checking to see if the selected project is currently visible
+          checkbox = $(target.find("input:checkbox")); // Find the checkbox so we can select it later.
       if (index > -1) {
         this.visible.splice(index, 1);
         checkbox.prop('checked', false);
