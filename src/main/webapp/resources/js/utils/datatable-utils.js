899c0156a47d35e4fee80e1b2d7a487069614a48
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-05-03 10:51:21 -0500

    Custom colours to indicate different projects.

--- src/main/webapp/resources/js/utils/datatable-utils.js
+++ src/main/webapp/resources/js/utils/datatable-utils.js
@@ -1,8 +1,51 @@
 /*eslint no-undef: 2*/
 /*global Option:true*/
 /*exported datatable*/
-var datatable = (function(moment, tl, page) {
+
+/**
+ * Custom function to add colours to project labels.
+ */
+var ProjectColourer = (function () {
+  "use strict";
+  
+  var index    = 0, // Index of the current colour
+      // These colours are intense colours pick from
+      // https://www.google.com/design/spec/style/color.html#color-color-palette A better solution might be to allow
+      // project managers to set custom colours for projects and store in project settings.
+      colours  = ["#D50000", "#C51162", "#AA00FF", "#6200EA", "#304FFE", "#2962FF", "#0091EA", "#00B8D4", "#00BFA5", "#00C853", "#64DD17", "#AEEA00", "#FFD600", "#FFAB00", "#FF6D00"],
+      // JSON to store already chosen project colours.
+      projects = {};
+
+  function ProjectColours() {
+  }
+
+  /**
+   * Get a colour for the project name provided
+   * @param name Name of the project
+   * @returns {string} colour
+   */
+  ProjectColours.prototype.getColour = function (name) {
+    // Check to see if the project already has a colour.
+    if (typeof projects[name] === "undefined") {
+      // Since currently there is a limited number of colours, make sure you wrap around to the start if
+      // all the colours are currently in use.
+      if (colours.length === index) {
+        index = 0;
+      }
+      // Select the colour for the project.
+      projects[name] = colours[index++];
+    }
+    return projects[name];
+  };
+  return ProjectColours;
+}());
+
+var datatable = (function(moment, $, tl, page) {
   'use strict';
+  // This is used to get a colour for the project label in a table cell
+  var projectColourer = new ProjectColourer();
+
+
   /**
    * Format the date in a table.
    * Requires the page to have the date format object.
@@ -56,6 +99,16 @@ var datatable = (function(moment, tl, page) {
     }
   }
 
+
+  function highlightAssociatedProjectRows(data, type, full) {
+    var $div = $("<div><div class='project-label'>&nbsp;" + data + "</div></div>");
+    if (full.sampleType === 'ASSOCIATED') {
+      var colour = projectColourer.getColour(full.project.label);
+      $div.find("div.project-label").css("border-color", colour);
+    }
+    return $div.html();
+  }
+
   /**
    * Called when the datatable is drawn.
    *    Resizes the table
@@ -90,11 +143,12 @@ var datatable = (function(moment, tl, page) {
   }
 
   return {
-    formatDate: formatDate,
-    i18n: i18n,
-    forceContentSize: forceContentSize,
-    tableDrawn: tableDrawn,
-    displayListSize: displayListSize,
-    formatSampleLink: formatSampleLink
+    formatDate                    : formatDate,
+    i18n                          : i18n,
+    forceContentSize              : forceContentSize,
+    tableDrawn                    : tableDrawn,
+    displayListSize               : displayListSize,
+    formatSampleLink              : formatSampleLink,
+    highlightAssociatedProjectRows: highlightAssociatedProjectRows
   };
-})(window.moment, window.TL, window.PAGE);
+})(window.moment, window.jQuery, window.TL, window.PAGE);
