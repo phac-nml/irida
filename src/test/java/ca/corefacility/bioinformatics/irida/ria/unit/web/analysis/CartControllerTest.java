c94ede0588db03bb9c5365fd5e7ad3501bf5b21b
tom <thomas.matthews@phac-aspc.gc.ca>
2016-05-04 16:19:05 -0500

    updated test to have request in context

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/analysis/CartControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/analysis/CartControllerTest.java
@@ -16,6 +16,12 @@ import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.google.common.collect.Sets;
 
 import ca.corefacility.bioinformatics.irida.model.joins.Join;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
@@ -27,8 +33,6 @@ import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 import ca.corefacility.bioinformatics.irida.service.remote.SampleRemoteService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
-
-import com.google.common.collect.Sets;
 
 public class CartControllerTest {
 	SampleService sampleService;
@@ -184,6 +188,9 @@ public class CartControllerTest {
 
 	@Test
 	public void testGetCartMap() {
+		RequestAttributes ra = new ServletRequestAttributes(new MockHttpServletRequest());
+        RequestContextHolder.setRequestAttributes(ra);
+		
 		Map<Project, Set<Sample>> selected = new HashMap<>();
 		selected.put(project, samples);
 		controller.setSelected(selected);
@@ -200,6 +207,7 @@ public class CartControllerTest {
 		for (Map<String, Object> map : sList) {
 			assertTrue(map.containsKey("id"));
 			assertTrue(map.containsKey("label"));
+			assertTrue(map.containsKey("href"));
 		}
 	}
 
