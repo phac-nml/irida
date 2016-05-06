66b3050cb9fef456dd71627a9d189b3d720fde0c
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2016-05-06 10:58:20 -0500

    Build galaxy using a docker image instead of building it from scratch.

--- packer/scripts/irida-galaxy.sh
+++ packer/scripts/irida-galaxy.sh
@@ -1,3 +1,13 @@
+wait_for_galaxy() {
+	echo "Waiting patiently for Galaxy to start..."
+	# sleep 10
+	while ! bash -c "curl http://localhost:9090 2> /dev/null > /dev/null" ; do
+		echo -n '.'
+		sleep 1
+	done
+	echo "Galaxy has (hopefully) started by now..."
+}
+
 cat > /etc/yum.repos.d/docker.repo <<EOF
 [dockerrepo]
 name=Docker Repository
@@ -13,7 +23,31 @@ systemctl enable docker
 systemctl start docker
 
 # run the galaxy container, --restart=always makes sure it starts up on boot
-docker run --restart=always -d -p 9090:80 -v /home/irida/data/sequencing:/home/irida/data/sequencing fbristow/irida-galaxy-docker
+mkdir -p /home/irida/data/galaxy-export
+curl http://gorgon.corefacility.ca/~fbristow/irida-galaxy-docker.tar.gz | gunzip -c | docker load
+docker run --name galaxy -d -p 9090:80 -v /home/irida/data/galaxy-export/:/export/ -v /home/irida/data/sequencing:/home/irida/data/sequencing fbristow/irida-galaxy-docker
+
+# wait for galaxy to succeed starting up for the first time, so we don't have to wait for postgres to start up next time
+wait_for_galaxy
+docker kill galaxy
+docker rm galaxy
+
+cat > /etc/systemd/system/galaxy.service <<EOF
+[Unit]
+Description=Galaxy
+Requires=docker.service
+After=docker.service
+
+[Service]
+ExecStartPre=-/usr/bin/docker kill galaxy
+ExecStartPre=-/usr/bin/docker rm galaxy
+ExecStart=/usr/bin/docker run --name galaxy -d -p 9090:80 -v /home/irida/data/galaxy-export/:/export/ -v /home/irida/data/sequencing:/home/irida/data/sequencing fbristow/irida-galaxy-docker
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable galaxy
 
 # configure IRIDA to connect to the Galaxy Docker container:
 sed -i 's_galaxy.execution.url=.*_galaxy.execution.url=http://localhost:9090/_' /etc/irida/irida.conf
