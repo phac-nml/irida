e65e1b143607fc11da7a1e25f9669fe2e9ac703c
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-05-13 08:46:56 -0500

    Don't need this complex code anymore

--- src/main/webapp/resources/js/pages/projects/project-samples-table.js
+++ src/main/webapp/resources/js/pages/projects/project-samples-table.js
@@ -1,21 +1,6 @@
 // The contents of this file gets load via Dandelion Datatables to the end of IIFE.
 
 // Add the toolbar to the table since this does not exist until datatables loads the table.
-/*
- *  Add the toolbar to the table.
- *
- *  Since Datatables is dynamically adding the toolbar buttons beside the search input, we need a way for
- *  AngularJS to compile the template for these buttons.  Here we are getting a reference to AngularJS's
- *  $compile function outside of Angular by using `injector().invoke()`
- */
-// var $target = $("[ng-app]");
-// angular.element($target).injector().invoke(["$rootScope", "$compile", function ($rootScope, $compile) {
-//   var toolbar = $(".filter-row > div")[0],
-//       tools = $("#toolbar");
-//   $(toolbar).html(function () {
-//     return $compile(tools)($rootScope);
-//   });
-// }]);
 var toolbar = $(".filter-row > div")[0],
     tools   = $("#toolbar");
 $(toolbar).html(tools);
