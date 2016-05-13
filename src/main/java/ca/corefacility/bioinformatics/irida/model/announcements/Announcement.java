d6701f2d3f3f8de72e59e78bb86fb5d006baa26b
johnc <jcuratcha_@live.com>
2016-05-13 07:00:46 -0500

    Added Announcement and AnnouncementsUser to data model.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/announcements/Announcement.java
@@ -0,0 +1,10 @@
+package ca.corefacility.bioinformatics.irida.model.announcements;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "announcement")
+public class Announcement {
+}
---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/announcements/AnnouncementsUser.java
@@ -0,0 +1,9 @@
+package ca.corefacility.bioinformatics.irida.model.announcements;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "announcements_user")
+public class AnnouncementsUser {
+}
