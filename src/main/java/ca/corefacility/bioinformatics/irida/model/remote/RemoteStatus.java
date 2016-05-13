64d4399c2c5231ce06b7266a8b3c683ab19fe8ca
tom <thomas.matthews@phac-aspc.gc.ca>
2016-05-13 13:35:36 -0500

    added remote status object

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteStatus.java
@@ -0,0 +1,58 @@
+package ca.corefacility.bioinformatics.irida.model.remote;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class RemoteStatus {
+
+	@Id
+	private String remoteURL;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private SyncStatus syncStatus;
+
+	public RemoteStatus(String remoteURL) {
+		syncStatus = SyncStatus.UNSYNCHRONIZED;
+		this.remoteURL = remoteURL;
+	}
+
+	public String getRemoteURL() {
+		return remoteURL;
+	}
+
+	public void setRemoteURL(String remoteURL) {
+		this.remoteURL = remoteURL;
+	}
+
+	public void setSyncStatus(SyncStatus syncStatus) {
+		this.syncStatus = syncStatus;
+	}
+
+	public SyncStatus getSyncStatus() {
+		return syncStatus;
+	}
+
+	public enum SyncStatus {
+		/**
+		 * Entity should not be synchronized
+		 */
+		UNSYNCHRONIZED,
+		/**
+		 * Marked to be synchronized
+		 */
+		MARKED,
+		/**
+		 * Currently being copied
+		 */
+		UPDATING,
+		/**
+		 * Up to date
+		 */
+		SYNCHRONIZED
+	}
+}
