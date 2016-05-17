3479fee1b7d4dc48ab864c933c817d3b22c3532a
tom <thomas.matthews@phac-aspc.gc.ca>
2016-05-17 13:01:45 -0500

    added service to run the synchronizatino of a remoteproject locally

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/remote/ProjectSynchronizationService.java
@@ -0,0 +1,96 @@
+package ca.corefacility.bioinformatics.irida.service.remote;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.remote.RemoteStatus.SyncStatus;
+import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFilePair;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SingleEndSequenceFile;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
+import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
+import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
+
+@Service
+public class ProjectSynchronizationService {
+	private ProjectService projectService;
+	private SampleService sampleService;
+	private SequencingObjectService objectService;
+
+	private ProjectRemoteService projectRemoteService;
+	private SampleRemoteService sampleRemoteService;
+	private SingleEndSequenceFileRemoteService singleEndRemoteService;
+	private SequenceFilePairRemoteService pairRemoteService;
+
+	@Autowired
+	public ProjectSynchronizationService(ProjectService projectService, SampleService sampleService,
+			SequencingObjectService objectService, ProjectRemoteService projectRemoteService,
+			SampleRemoteService sampleRemoteService, SingleEndSequenceFileRemoteService singleEndRemoteService,
+			SequenceFilePairRemoteService pairRemoteService) {
+
+		this.projectService = projectService;
+		this.sampleService = sampleService;
+		this.objectService = objectService;
+		this.projectRemoteService = projectRemoteService;
+		this.sampleRemoteService = sampleRemoteService;
+		this.singleEndRemoteService = singleEndRemoteService;
+		this.pairRemoteService = pairRemoteService;
+	}
+
+	public void findMarkedProjectsToSync() {
+		List<Project> markedProjects = projectService.getProjectsWithRemoteSyncStatus(SyncStatus.MARKED);
+
+		for (Project project : markedProjects) {
+			syncProject(project);
+		}
+	}
+
+	public void syncProject(Project project) {
+		String projectURL = project.getRemoteStatus().getURL();
+
+		Project readProject = projectRemoteService.read(projectURL);
+
+		List<Sample> readSamplesForProject = sampleRemoteService.getSamplesForProject(readProject);
+
+		for (Sample s : readSamplesForProject) {
+			s.setId(null);
+			syncSample(s, project);
+		}
+
+	}
+
+	public void syncSample(Sample sample, Project project) {
+		sampleService.create(sample);
+		
+		projectService.addSampleToProject(project, sample);
+
+		List<SequenceFilePair> sequenceFilePairsForSample = pairRemoteService.getSequenceFilePairsForSample(sample);
+
+		for (SequenceFilePair pair : sequenceFilePairsForSample) {
+			pair.setId(null);
+			syncSequenceFilePair(pair, sample);
+		}
+
+		List<SingleEndSequenceFile> unpairedFilesForSample = singleEndRemoteService.getUnpairedFilesForSample(sample);
+
+		for (SingleEndSequenceFile file : unpairedFilesForSample) {
+			file.setId(null);
+			syncSingleEndSequenceFile(file);
+		}
+	}
+
+	public void syncSingleEndSequenceFile(SingleEndSequenceFile file) {
+		// objectService.create(file);
+	}
+
+	public void syncSequenceFilePair(SequenceFilePair pair, Sample sample) {
+		pair = pairRemoteService.mirrorPair(pair);
+
+		pair.getFiles().forEach(s -> s.setId(null));
+
+		objectService.createSequencingObjectInSample(pair, sample);
+	}
+}
