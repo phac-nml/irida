66b1831e66898b7eb49d312ed47c72bccb5401f1
tom <thomas.matthews@phac-aspc.gc.ca>
2016-05-18 15:11:00 -0500

    modified rendering for IT tests

--- src/main/webapp/resources/js/pages/sequencing-runs/sequencing-runs-list.js
+++ src/main/webapp/resources/js/pages/sequencing-runs/sequencing-runs-list.js
@@ -1,6 +1,6 @@
 var runsTable = (function(page) {
 	function runLinkRow(data, type, full) {
-		return '<a class="item-link" title="' + data + '" href="' + page.urls.link
+		return '<a class="item-link run-id" title="' + data + '" href="' + page.urls.link
 				+ full.id + '"><span>' + data + '</span></a>';		
 	};
 	
