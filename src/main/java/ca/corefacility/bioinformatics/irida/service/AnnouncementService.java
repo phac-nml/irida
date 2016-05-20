b0c66a9287702033232a8f05fce2b16f315cdc8f
johnc <jcuratcha_@live.com>
2016-05-20 10:12:04 -0500

    Added service classes for announcements.

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/AnnouncementService.java
@@ -0,0 +1,7 @@
+package ca.corefacility.bioinformatics.irida.service;
+
+/**
+ * Created by curatchaj on 20/05/16.
+ */
+public interface AnnouncementService {
+}
---
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/AnnouncementServiceImpl.java
@@ -0,0 +1,9 @@
+package ca.corefacility.bioinformatics.irida.service.impl;
+
+import ca.corefacility.bioinformatics.irida.service.AnnouncementService;
+
+/**
+ * Created by curatchaj on 20/05/16.
+ */
+public class AnnouncementServiceImpl implements AnnouncementService {
+}
