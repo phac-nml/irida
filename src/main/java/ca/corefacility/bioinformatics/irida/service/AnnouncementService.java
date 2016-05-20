7dbf61b0f7dde2b012b0a6bc1677dc497403fd85
johnc <jcuratcha_@live.com>
2016-05-20 10:23:26 -0500

    Started building up service layer for announcements.

--- src/main/java/ca/corefacility/bioinformatics/irida/service/AnnouncementService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/AnnouncementService.java
@@ -3,5 +3,6 @@ package ca.corefacility.bioinformatics.irida.service;
 /**
  * Created by curatchaj on 20/05/16.
  */
-public interface AnnouncementService {
+public interface AnnouncementService extends CRUDService {
+
 }
--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/AnnouncementServiceImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/AnnouncementServiceImpl.java
@@ -1,9 +1,36 @@
 package ca.corefacility.bioinformatics.irida.service.impl;
 
+import ca.corefacility.bioinformatics.irida.model.announcements.Announcement;
+import ca.corefacility.bioinformatics.irida.repositories.AnnouncementRepository;
+import ca.corefacility.bioinformatics.irida.repositories.joins.announcement.AnnouncementUserJoinRepository;
+import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
 import ca.corefacility.bioinformatics.irida.service.AnnouncementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.Validator;
 
 /**
- * Created by curatchaj on 20/05/16.
+ *  Service for managing @{link Announcements}
+ *
  */
-public class AnnouncementServiceImpl implements AnnouncementService {
+public class AnnouncementServiceImpl extends CRUDServiceImpl<Long, Announcement> implements AnnouncementService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AnnouncementServiceImpl.class);
+
+    private AnnouncementRepository announcementRepository;
+    private AnnouncementUserJoinRepository announcementUserJoinRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public AnnouncementServiceImpl(AnnouncementRepository announcementRepository,
+                                   AnnouncementUserJoinRepository announcementUserJoinRepository,
+                                   UserRepository userRepository,
+                                   Validator validator) {
+        super(announcementRepository, validator, Announcement.class);
+        this.announcementRepository = announcementRepository;
+        this.announcementUserJoinRepository = announcementUserJoinRepository;
+        this.userRepository = userRepository;
+    }
 }
