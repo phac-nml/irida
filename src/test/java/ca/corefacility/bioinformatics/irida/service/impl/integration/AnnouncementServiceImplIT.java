021913c66a952dd34d0065e9b2889ebb11cbeef8
johnc <jcuratcha_@live.com>
2016-05-20 11:50:28 -0500

    Added testing data for announcements, created integration test class for announcements.

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/AnnouncementServiceImplIT.java
@@ -0,0 +1,29 @@
+package ca.corefacility.bioinformatics.irida.service.impl.integration;
+
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConfig;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import org.junit.runner.RunWith;
+import org.springframework.security.test.context.support.WithSecurityContextTestExcecutionListener;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+/**
+ * Integration tests for testing out Announcements
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiServicesConfig.class,
+        IridaApiJdbcDataSourceConfig.class })
+@ActiveProfiles("it")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class,
+        WithSecurityContextTestExcecutionListener.class })
+@DatabaseSetup("/ca/corefacility/bioinformatics/irida/service/impl/AnnouncementServiceImplIT.xml")
+@DatabaseTearDown("/ca/corefacility/bioinformatics/irida/test/integration/TableReset.xml")
+public class AnnouncementServiceImplIT {
+}
