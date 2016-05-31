c2b7a4ff65dea21e1d01cfe53af2f4172fa7cec5
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-05-31 13:31:04 -0500

    Redirect user if already logged in

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/LoginController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/LoginController.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +22,10 @@ import ca.corefacility.bioinformatics.irida.service.EmailController;
 public class LoginController extends BaseController {
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	private static final String LOGIN_PAGE = "login";
-	
-	
+
+
 	private final EmailController emailController;
-	
+
 	@Autowired
 	public LoginController(final EmailController emailController) {
 		this.emailController = emailController;
@@ -39,16 +42,23 @@ public class LoginController extends BaseController {
 			@RequestParam(value="galaxyCallbackUrl",required=false) String galaxyCallbackURL,
 			@RequestParam(value="galaxyClientID",required=false) String galaxyClientID,
 			HttpSession httpSession) {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+			logger.debug("User is already logged in.");
+			return "forward:/dashboard";
+		}
+
 		logger.debug("Displaying login page.");
-		
+
 		model.addAttribute("emailConfigured", emailController.isMailConfigured());
-		
+
 		//External exporting functionality
 		if(galaxyCallbackURL != null && galaxyClientID !=null) {
 			httpSession.setAttribute(ProjectsController.GALAXY_CALLBACK_VARIABLE_NAME, galaxyCallbackURL);
 			httpSession.setAttribute(ProjectsController.GALAXY_CLIENT_ID_NAME, galaxyClientID);
 		}
-	
+
 		model.addAttribute("error", hasError);
 		return LOGIN_PAGE;
 	}
