69b8c2cbac0de98de956c0cadb773c9e590c8984
tom <thomas.matthews@phac-aspc.gc.ca>
2016-06-08 11:48:30 -0500

    removed unnecessary imports

--- src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteStatus.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/remote/RemoteStatus.java
@@ -1,7 +1,5 @@
 package ca.corefacility.bioinformatics.irida.model.remote;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,8 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.envers.Audited;
