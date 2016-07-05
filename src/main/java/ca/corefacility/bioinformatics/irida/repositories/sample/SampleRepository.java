de4f435fc65d5bac1d7d7819201f6bfd41057c44
Josh Adam <josh.adam@phac-aspc.gc.ca>
2016-07-05 11:54:34 -0500

    Updated docs

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/SampleRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/SampleRepository.java
@@ -32,6 +32,18 @@ public interface SampleRepository extends IridaJpaRepository<Sample, Long> {
 	@Query("select j.sample from ProjectSampleJoin j where j.project = ?1 and j.sample.sampleName = ?2")
 	public Sample getSampleBySampleName(Project p, String sampleName) throws EntityNotFoundException;
 
+	/**
+	 * Get a {@link Page} of {@link Sample}s based on a list of {@link Sample} names
+	 *
+	 * @param project
+	 * 		The {@link Project} that the {@link Sample} belongs to.
+	 * @param sampleNames
+	 * 		A {@link List} of {@link String} sample names
+	 * @param pageable
+	 * 		{@link Pageable} information about which {@link Sample}s to retrun
+	 *
+	 * @return The {@link Page} of {@link ProjectSampleJoin}
+	 */
 	@Query("select j from ProjectSampleJoin j where j.project = ?1 and j.sample.sampleName in ?2")
 	public Page<ProjectSampleJoin> findSampleByNameInProject(Project project, List<String> sampleNames, Pageable pageable);
 }
