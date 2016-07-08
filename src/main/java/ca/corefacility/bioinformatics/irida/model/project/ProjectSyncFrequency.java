05b40d6d704097581242a89b050754843683d5f9
tom <thomas.matthews@phac-aspc.gc.ca>
2016-07-08 14:33:49 -0500

    added never option for syncing

--- src/main/java/ca/corefacility/bioinformatics/irida/model/project/ProjectSyncFrequency.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/project/ProjectSyncFrequency.java
@@ -1,7 +1,7 @@
 package ca.corefacility.bioinformatics.irida.model.project;
 
 public enum ProjectSyncFrequency {
-	DAILY(1), WEEKLY(7), MONTHLY(30), BIMONTHLY(60), QUARTERLY(90);
+	NEVER(Integer.MAX_VALUE), DAILY(1), WEEKLY(7), MONTHLY(30), SEMIMONTHLY(60), QUARTERLY(90);
 
 	private int days;
 
