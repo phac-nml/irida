11a6c83de67701610a38b3c2edc6fc6428fbca12
tom <thomas.matthews@phac-aspc.gc.ca>
2016-07-11 13:54:20 -0500

    added comments to sync frequency

--- src/main/java/ca/corefacility/bioinformatics/irida/model/project/ProjectSyncFrequency.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/project/ProjectSyncFrequency.java
@@ -1,5 +1,8 @@
 package ca.corefacility.bioinformatics.irida.model.project;
 
+/**
+ * Describes how often a project should be synchronized by a remote api
+ */
 public enum ProjectSyncFrequency {
 	NEVER(Integer.MAX_VALUE), DAILY(1), WEEKLY(7), MONTHLY(30), SEMIMONTHLY(60), QUARTERLY(90);
 
