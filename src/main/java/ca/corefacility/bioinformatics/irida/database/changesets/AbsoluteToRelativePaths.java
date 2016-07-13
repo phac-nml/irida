43d1ced4094a9b7e6e0636bdcc682e0fc094d24d
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2016-07-13 15:39:53 -0500

    Liquibase can now be invoked on the command line to update paths to relative paths.

--- src/main/java/ca/corefacility/bioinformatics/irida/database/changesets/AbsoluteToRelativePaths.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/database/changesets/AbsoluteToRelativePaths.java
@@ -1,11 +1,19 @@
 package ca.corefacility.bioinformatics.irida.database.changesets;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig.ApplicationContextAwareSpringLiquibase;
 import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig.ApplicationContextAwareSpringLiquibase.ApplicationContextSpringResourceOpener;
 import liquibase.change.custom.CustomSqlChange;
 import liquibase.database.Database;
@@ -19,7 +27,10 @@ import liquibase.statement.core.RawSqlStatement;
 public class AbsoluteToRelativePaths implements CustomSqlChange {
 
 	private static final Logger logger = LoggerFactory.getLogger(AbsoluteToRelativePaths.class);
-	private ApplicationContext applicationContext;
+	private Path sequenceFileDirectory;
+	private Path referenceFileDirectory;
+	private Path outputFileDirectory;
+	private Path snapshotFileDirectory;
 
 	@Override
 	public String getConfirmationMessage() {
@@ -33,9 +44,33 @@ public class AbsoluteToRelativePaths implements CustomSqlChange {
 
 	@Override
 	public void setFileOpener(ResourceAccessor resourceAccessor) {
-		final ApplicationContext applicationContext = ((ApplicationContextSpringResourceOpener) resourceAccessor)
-				.getApplicationContext();
-		this.applicationContext = applicationContext;
+		logger.info("Setting the file opener.");
+		if (resourceAccessor instanceof ApplicationContextAwareSpringLiquibase) {
+			logger.info("We're running inside of a spring instance, getting the existing application context.");
+			final ApplicationContext applicationContext = ((ApplicationContextSpringResourceOpener) resourceAccessor)
+					.getApplicationContext();
+			this.sequenceFileDirectory = applicationContext.getBean("sequenceFileBaseDirectory", Path.class);
+			this.referenceFileDirectory = applicationContext.getBean("referenceFileBaseDirectory", Path.class);
+			this.outputFileDirectory = applicationContext.getBean("outputFileBaseDirectory", Path.class);
+			this.snapshotFileDirectory = applicationContext.getBean("snapshotFileBaseDirectory", Path.class);
+		} else {
+			logger.info("Need to manually load the keys from the config file, we're not running in a spring context.");
+			try {
+				final Set<InputStream> resources = resourceAccessor.getResourcesAsStream("/etc/irida/irida.conf");
+				// should only be one file in the set since we specified exactly
+				// one file
+				final InputStream config = resources.iterator().next();
+				final Properties properties = new Properties();
+				properties.load(config);
+
+				this.sequenceFileDirectory = Paths.get(properties.getProperty("sequence.file.base.directory"));
+				this.referenceFileDirectory = Paths.get(properties.getProperty("reference.file.base.directory"));
+				this.outputFileDirectory = Paths.get(properties.getProperty("output.file.base.directory"));
+				this.snapshotFileDirectory = Paths.get(properties.getProperty("snapshot.file.base.directory"));
+			} catch (final IOException e) {
+				logger.error("Failed to load config file.", e);
+			}
+		}
 	}
 
 	@Override
@@ -47,22 +82,35 @@ public class AbsoluteToRelativePaths implements CustomSqlChange {
 	public SqlStatement[] generateStatements(Database database) throws CustomChangeException {
 		// for each type of directory and file-class, go through and strip out
 		// the prefix in the database.
-		final Path sequenceFileDirectory = applicationContext.getBean("sequenceFileBaseDirectory", Path.class);
-		final Path referenceFileDirectory = applicationContext.getBean("referenceFileBaseDirectory", Path.class);
-		final Path outputFileDirectory = applicationContext.getBean("outputFileBaseDirectory", Path.class);
-		final Path snapshotFileDirectory = applicationContext.getBean("snapshotFileBaseDirectory", Path.class);
+
+		final String sequenceFileDirectoryPath = appendPathSeparator(this.sequenceFileDirectory.toString());
+		final String referenceFileDirectoryPath = appendPathSeparator(this.referenceFileDirectory.toString());
+		final String outputFileDirectoryPath = appendPathSeparator(this.outputFileDirectory.toString());
+		final String snapshotFileDirectoryPath = appendPathSeparator(this.snapshotFileDirectory.toString());
 
 		return new SqlStatement[] {
 				new RawSqlStatement(String.format("update sequence_file set file_path = replace(file_path, '%s', '')",
-						sequenceFileDirectory.toString())),
+						sequenceFileDirectoryPath)),
 				new RawSqlStatement(String.format("update reference_file set filePath = replace(filePath, '%s', '')",
-						referenceFileDirectory.toString())),
+						referenceFileDirectoryPath)),
 				new RawSqlStatement(
 						String.format("update analysis_output_file set file_path = replace(file_path, '%s', '')",
-								outputFileDirectory.toString())),
+								outputFileDirectoryPath)),
 				new RawSqlStatement(
 						String.format("update remote_sequence_file set file_path = replace(file_path, '%s', '')",
-								snapshotFileDirectory.toString())) };
+								snapshotFileDirectoryPath)) };
 	}
 
+	// make sure that we've got trailing path separators so that the paths in
+	// the database are actually
+	// relative, i.e., we're translating /sequencefiles/1/2/file.fastq to
+	// 1/2/file.fastq, not /1/2/file.fastq
+	private static String appendPathSeparator(final String path) {
+		final String pathSeparator = FileSystems.getDefault().getSeparator();
+		if (!path.endsWith(pathSeparator)) {
+			return path + pathSeparator;
+		} else {
+			return path;
+		}
+	}
 }
