376085dcf83a847cc844bc586e2357a6c41ee730
tom <thomas.matthews@phac-aspc.gc.ca>
2016-07-14 08:55:49 -0500

    rolled back remoteapicontroller because nothing actually changed

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/oauth/RemoteAPIController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/oauth/RemoteAPIController.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
 
-import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -294,7 +293,7 @@ public class RemoteAPIController extends BaseController {
 	@ResponseBody
 	public String checkApiStatus(@PathVariable Long apiId) {
 		RemoteAPI api = remoteAPIService.read(apiId);
-		
+
 		try {
 			projectRemoteService.getServiceStatus(api);
 			return VALID_OAUTH_CONNECTION;
