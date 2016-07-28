c8f544cb0e26eaa04b4a4ee09dfb189f1da22be0
Josh Adam <josh.adam@canada.ca>
2016-07-28 11:40:42 -0500

    Added csv export

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/datatables/export/TableExport.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/components/datatables/export/TableExport.java
@@ -1,5 +1,6 @@
 package ca.corefacility.bioinformatics.irida.ria.web.components.datatables.export;
 
+import com.github.dandelion.datatables.core.export.CsvExport;
 import com.github.dandelion.datatables.core.export.ExportConf;
 import com.github.dandelion.datatables.core.export.ReservedFormat;
 import com.github.dandelion.datatables.extras.export.poi.XlsxExport;
@@ -15,6 +16,12 @@ abstract class TableExport {
 			this.exportConf = new ExportConf.Builder(ReservedFormat.XLSX)
 					.header(true)
 					.exportClass(new XlsxExport())
+					.fileName(fileName)
+					.build();
+		} else if (exportFormat.equals(ReservedFormat.CSV)) {
+			this.exportConf = new ExportConf.Builder(ReservedFormat.CSV)
+					.header(true)
+					.exportClass(new CsvExport())
 					.fileName(fileName)
 					.build();
 		} else {
