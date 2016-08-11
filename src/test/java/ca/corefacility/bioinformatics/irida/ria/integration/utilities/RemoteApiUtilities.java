4e02ab1042d2ce1277164888878f3c1dc7754eb1
Josh Adam <josh.s.adam@gmail.com>
2016-08-11 08:22:57 -0500

    Revert "Even more logging"
    This reverts commit e4765263f5fbd4421cf82b61a9629aba6ec4847c

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/utilities/RemoteApiUtilities.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/utilities/RemoteApiUtilities.java
@@ -4,16 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.remoteapi.CreateRemoteAPIPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.remoteapi.RemoteAPIDetailsPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.remoteapi.RemoteAPIDetailsPage.ApiStatus;
 
 public class RemoteApiUtilities {
-	private static final Logger logger = LoggerFactory.getLogger(RemoteApiUtilities.class);
-
 	public static void addRemoteApi(WebDriver driver) {
 		CreateRemoteAPIPage page = new CreateRemoteAPIPage(driver);
 
@@ -26,12 +22,8 @@ public class RemoteApiUtilities {
 		RemoteAPIDetailsPage remoteAPIDetailsPage = new RemoteAPIDetailsPage(driver);
 
 		ApiStatus remoteApiStatus = remoteAPIDetailsPage.getRemoteApiStatus();
-
-		logger.debug("REMOTE API UTILTIES: CLICKING CONNECT");
 		remoteAPIDetailsPage.clickConnect();
-		logger.debug("REMOTE API UTILITIES: CENNECT CLICKED");
 		remoteAPIDetailsPage.clickAuthorize();
-		logger.debug("AUTHORIZE CLICKED");
 
 		remoteApiStatus = remoteAPIDetailsPage.getRemoteApiStatus();
 		assertEquals("api status should be connected", ApiStatus.CONNECTED, remoteApiStatus);
