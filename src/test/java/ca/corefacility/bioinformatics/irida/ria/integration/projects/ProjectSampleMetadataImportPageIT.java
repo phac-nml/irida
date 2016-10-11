3a6a0f4a837e8c3d0b627d907eed3565347c418f
Josh Adam <josh.adam@canada.ca>
2016-10-11 14:18:24 -0500

    Moved it to within the method

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectSampleMetadataImportPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectSampleMetadataImportPageIT.java
@@ -13,18 +13,17 @@ import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.Proje
 
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSampleMetadataView.xml")
 public class ProjectSampleMetadataImportPageIT extends AbstractIridaUIITChromeDriver {
-	private static final String GOOD_PATH = "src/test/resources/files/metadata-upload/good.xlsx";
-	private ProjectSampleMetadataImportPage page;
+	private static final String GOOD_FILE_PATH = "src/test/resources/files/metadata-upload/good.xlsx";
 
 	@Before
 	public void init() {
 		LoginPage.loginAsManager(driver());
-		page = ProjectSampleMetadataImportPage.goToPage(driver());
 	}
 
 	@Test
 	public void testGoodFileAndHeaders() {
-		page.uploadMetadataFile(GOOD_PATH);
+		ProjectSampleMetadataImportPage page = ProjectSampleMetadataImportPage.goToPage(driver());
+		page.uploadMetadataFile(GOOD_FILE_PATH);
 		page.selectSampleNameColumn();
 		assertEquals("Has incorrect amount of rows matching sample names", 5, page.getFoundCount());
 	}
