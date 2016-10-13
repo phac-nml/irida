8a29f7fb2188ba446cc0d6c013c02c5f7e2b8ae9
Josh Adam <josh.adam@canada.ca>
2016-10-13 09:30:53 -0500

    Spot to add buttons to the datatable

--- src/main/webapp/resources/js/utilities/datatables.utilities.js
+++ src/main/webapp/resources/js/utilities/datatables.utilities.js
@@ -1,6 +1,7 @@
 // Default table html DOM layout
 export const dom = `
 <'row'<'col-sm-6'l><'col-sm-6'f>>
+<'row'<'col-sm-12'B>>
 <'row'<'col-sm-12'tr>>
 <'row'<'col-sm-12'i>>
 <'row'<'col-sm-12'p>>
