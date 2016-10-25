ac21077202cd08fd3d6ae36ce0ee49aa45990adb
tom <thomas.matthews@phac-aspc.gc.ca>
2016-10-25 15:38:50 -0500

    marking the uploading user when creating a sequencing run

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/sequencingrun/RESTSequencingRunController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/sequencingrun/RESTSequencingRunController.java
@@ -3,6 +3,7 @@ package ca.corefacility.bioinformatics.irida.web.controller.api.sequencingrun;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.security.Principal;
 import java.util.Collection;
 
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +22,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import ca.corefacility.bioinformatics.irida.model.run.MiseqRun;
 import ca.corefacility.bioinformatics.irida.model.run.SequencingRun;
+import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.SequencingRunService;
+import ca.corefacility.bioinformatics.irida.service.user.UserService;
 import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResourceCollection;
 import ca.corefacility.bioinformatics.irida.web.controller.api.RESTGenericController;
 import ca.corefacility.bioinformatics.irida.web.controller.api.projects.RESTProjectsController;
@@ -35,6 +39,8 @@ public class RESTSequencingRunController extends RESTGenericController<Sequencin
 	private static final Logger logger = LoggerFactory.getLogger(RESTSequencingRunController.class);
 
 	public static final String MISEQ_REL = "sequencingRun/miseq";
+
+	private UserService userService;
 
 	/**
 	 * Default constructor. Should not be used.
@@ -51,14 +57,18 @@ public class RESTSequencingRunController extends RESTGenericController<Sequencin
 	 *            controller.
 	 */
 	@Autowired
-	public RESTSequencingRunController(SequencingRunService service) {
+	public RESTSequencingRunController(SequencingRunService service, UserService userService) {
 		super(service, SequencingRun.class);
+		this.userService = userService;
 
 	}
 
 	@RequestMapping(value = "/miseqrun", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.APPLICATION_XML_VALUE })
-	public ModelMap createMiseqRun(@RequestBody MiseqRun representation, HttpServletResponse response) {
+	public ModelMap createMiseqRun(@RequestBody MiseqRun representation, HttpServletResponse response,
+			Principal principal) {
+		User user = userService.getUserByUsername(principal.getName());
+		representation.setUser(user);
 		logger.trace("creating miseq run");
 		return create(representation, response);
 	}
@@ -69,7 +79,8 @@ public class RESTSequencingRunController extends RESTGenericController<Sequencin
 	@Override
 	protected Collection<Link> constructCollectionResourceLinks(ResourceCollection<SequencingRun> list) {
 		Collection<Link> links = super.constructCollectionResourceLinks(list);
-		links.add(linkTo(methodOn(RESTSequencingRunController.class).createMiseqRun(null, null)).withRel(MISEQ_REL));
+		links.add(linkTo(methodOn(RESTSequencingRunController.class).createMiseqRun(null, null, null))
+				.withRel(MISEQ_REL));
 		return links;
 	}
 
