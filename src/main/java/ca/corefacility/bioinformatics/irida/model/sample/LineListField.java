cebc004b52f2bbc0716a1a3fae358cdcb3d9c23e
tom <thomas.matthews@phac-aspc.gc.ca>
2016-11-03 11:53:43 -0500

    added entity annotations to linelistfield

--- src/main/java/ca/corefacility/bioinformatics/irida/model/sample/LineListField.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/sample/LineListField.java
@@ -1,15 +1,39 @@
 package ca.corefacility.bioinformatics.irida.model.sample;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 /**
  * Describes an individual field in a linelist.
  */
+@Entity
+@Table(name = "line_list_field")
+@Audited
+@EntityListeners(AuditingEntityListener.class)
 public class LineListField {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
 	private String label;
+
 	private String type;
 
 	public LineListField(String label, String type) {
 		this.label = label;
 		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
 	}
 
 	public String getLabel() {
