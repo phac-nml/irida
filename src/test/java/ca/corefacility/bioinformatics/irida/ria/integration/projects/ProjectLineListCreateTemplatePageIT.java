0c770443081fd7a2a9787e1a14032a9cd0d240ea
tom <thomas.matthews@phac-aspc.gc.ca>
2016-11-14 10:00:15 -0600

    missing an assert import and using the wrong junit imports

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectLineListCreateTemplatePageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectLineListCreateTemplatePageIT.java
@@ -1,7 +1,8 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.projects;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
