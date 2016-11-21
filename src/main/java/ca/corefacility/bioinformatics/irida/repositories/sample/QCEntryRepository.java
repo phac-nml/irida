eb56e66895757e0556670182c33714073aa35cda
tom <thomas.matthews@phac-aspc.gc.ca>
2016-11-21 16:22:03 -0600

    commenting

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/QCEntryRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/QCEntryRepository.java
@@ -3,6 +3,9 @@ package ca.corefacility.bioinformatics.irida.repositories.sample;
 import ca.corefacility.bioinformatics.irida.model.sample.QCEntry;
 import ca.corefacility.bioinformatics.irida.repositories.IridaJpaRepository;
 
+/**
+ * Repository for saving and retrieving {@link QCEntry} objects
+ */
 public interface QCEntryRepository extends IridaJpaRepository<QCEntry, Long> {
 
 }
