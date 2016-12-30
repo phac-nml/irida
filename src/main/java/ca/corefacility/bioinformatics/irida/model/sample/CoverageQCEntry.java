d354e448561cf8fafbcf0e83ff64af7e49223ccf
tom <thomas.matthews@phac-aspc.gc.ca>
2016-12-30 09:19:41 -0600

    added entity tracking coverage

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/sample/CoverageQCEntry.java
@@ -0,0 +1,41 @@
+package ca.corefacility.bioinformatics.irida.model.sample;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequencingObject;
+
+@Entity
+public class CoverageQCEntry extends QCEntry {
+
+	@NotNull
+	Long coverage;
+
+	public CoverageQCEntry() {
+		super();
+	}
+
+	public CoverageQCEntry(SequencingObject sequencingObject, Long coverage) {
+		super(sequencingObject);
+		this.coverage = coverage;
+	}
+
+	@Override
+	public String getMessage() {
+		return coverage + "x";
+	}
+
+	public void setCoverage(Long coverage) {
+		this.coverage = coverage;
+	}
+
+	public Long getCoverage() {
+		return coverage;
+	}
+
+	@Override
+	public QCEntryType getType() {
+		return QCEntryType.COVERAGE;
+	}
+
+}
