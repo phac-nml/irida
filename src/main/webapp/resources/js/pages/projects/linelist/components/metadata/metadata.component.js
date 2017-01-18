2a0c6945cbc84d82c7c261da7b3232366a2bc2ef
Josh Adam <josh.adam@canada.ca>
2017-01-18 14:27:45 -0600

    Code to handle column sorting

--- src/main/webapp/resources/js/pages/projects/linelist/components/metadata/metadata.component.js
+++ src/main/webapp/resources/js/pages/projects/linelist/components/metadata/metadata.component.js
@@ -1,10 +1,20 @@
 import {EVENTS} from './../../constants';
-
 const templateUrl = 'metadata.button.tmpl';
 const asideTemplateUrl = 'metadata.aside.tmpl';
 
 const FIELDS = window.headersList.map((header, index) => {
   return ({text: header, index, selected: true});
+});
+
+// Need to modify the fields if the table reorders the headers
+// Listen for moving columns
+document.body.addEventListener(EVENTS.TABLE.colReorder, e => {
+  const {to, from} = e.detail;
+  const temp = FIELDS[from];
+  FIELDS[from] = FIELDS[to];
+  FIELDS[from].index = from;
+  FIELDS[to] = temp;
+  FIELDS[to].index = to;
 });
 
 export const MetadataComponent = {
@@ -27,7 +37,7 @@ export const MetadataComponent = {
               EVENTS.TABLE.columnVisibility,
               {
                 detail: {
-                  column: field.index
+                  column: Number(field.index)
                 },
                 bubbles: true
               });
--- src/main/webapp/resources/js/pages/projects/linelist/linelist.datatables.js
+++ src/main/webapp/resources/js/pages/projects/linelist/linelist.datatables.js
@@ -3,21 +3,29 @@ import {EVENTS} from './constants';
 import {formatBasicHeaders} from '../../../utilities/datatables.utilities';
 const $ = require('jquery');
 require('datatables.net');
-
 require('datatables-bootstrap3-plugin');
 require('datatables.net-scroller');
+require('datatables.net-colreorder');
 require('style!datatables.net-scroller-bs/css/scroller.bootstrap.css');
 require('style!datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
+require('style!datatables.net-colreorder-bs/css/colReorder.bootstrap.css');
 
-const headers = formatBasicHeaders(window.headersList);
+const headers = formatBasicHeaders(Object.assign(window.headersList));
 
 const $table = $(`#linelist`).DataTable({
-  data: window.metadataList,
+  data: Object.assign(window.metadataList),
   columns: headers,
   scrollX: true,
   scrollY: 600,
   deferRender: true,
-  scroller: true
+  scroller: true,
+  colReorder: true
+});
+
+$table.on('column-reorder', (e, settings, detail) => {
+  const event = new CustomEvent(EVENTS.TABLE.colReorder, {detail,
+    bubbles: true});
+  e.currentTarget.dispatchEvent(event);
 });
 
 document.body.addEventListener(EVENTS.TABLE.columnVisibility, e => {
