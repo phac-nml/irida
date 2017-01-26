7355bb81fb90533e7537cdb079ca55235236dd0f
tom <thomas.matthews@phac-aspc.gc.ca>
2017-01-26 09:14:17 -0600

    wrote test for coverage

--- src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/SequencingObjectServiceImplIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/impl/integration/SequencingObjectServiceImplIT.java
@@ -46,7 +46,9 @@ import com.google.common.collect.Sets;
 
 import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
 import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConfig;
+import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.run.SequencingRun;
+import ca.corefacility.bioinformatics.irida.model.sample.CoverageQCEntry;
 import ca.corefacility.bioinformatics.irida.model.sample.FileProcessorErrorQCEntry;
 import ca.corefacility.bioinformatics.irida.model.sample.QCEntry;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
@@ -60,6 +62,7 @@ import ca.corefacility.bioinformatics.irida.model.user.Role;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.model.workflow.analysis.AnalysisFastQC;
 import ca.corefacility.bioinformatics.irida.service.AnalysisService;
+import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 import ca.corefacility.bioinformatics.irida.service.SequencingRunService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
@@ -80,7 +83,7 @@ public class SequencingObjectServiceImplIT {
 			+ SEQUENCE + "\n+\n?????????").getBytes();
 
 	private static final String CHECKSUM = "85e440ab2f17636ab24b12e8e4b4d445b6131e7df785cbd02d56c2688eef55fb";
-	
+
 	private static final String ZIPPED_CHECKSUM = "a2ff0d0790029822b4a2457a2912414ffc532136729ad2fd06efc70fe428ab32";
 
 	@Autowired
@@ -97,6 +100,9 @@ public class SequencingObjectServiceImplIT {
 
 	@Autowired
 	private SequencingRunService sequencingRunService;
+
+	@Autowired
+	private ProjectService projectService;
 
 	@Autowired
 	@Qualifier("sequenceFileBaseDirectory")
@@ -176,7 +182,7 @@ public class SequencingObjectServiceImplIT {
 
 		SequencingRun mr = sequencingRunService.read(1L);
 		sequencingRunService.addSequencingObjectToSequencingRun(mr, so);
-		
+
 		// Wait 5 seconds. file processing should have failed by then.
 		Thread.sleep(5000);
 
@@ -346,7 +352,7 @@ public class SequencingObjectServiceImplIT {
 		assertFalse("File name is still gzipped.", sf.getFile().getFileName().toString().endsWith(".gz"));
 		AnalysisFastQC analysis = asRole(Role.ROLE_ADMIN, "admin").analysisService
 				.getFastQCAnalysisForSequenceFile(readObject, sf.getId());
-		
+
 		// verify the file checksum was taken properly
 		assertEquals("checksum should be equal", ZIPPED_CHECKSUM, sf.getUploadSha256());
 
@@ -393,5 +399,57 @@ public class SequencingObjectServiceImplIT {
 		SequenceFile read = sequencingObject.getFileWithId(1L);
 		assertEquals("5", read.getOptionalProperty("samplePlate"));
 		assertEquals("10", read.getOptionalProperty("sampleWell"));
+	}
+
+	@Test
+	@WithMockUser(username = "admin", roles = "ADMIN")
+	public void testCoverage() throws IOException, InterruptedException {
+		Project project = projectService.read(1L);
+		project.setGenomeSize(3L);
+		project.setRequiredCoverage(2);
+
+		project = projectService.update(project);
+
+		Sample s = sampleService.read(1L);
+		SequenceFile sf = new SequenceFile();
+		Path sequenceFile = Files.createTempFile("TEMPORARY-SEQUENCE-FILE", ".gz");
+		OutputStream gzOut = new GZIPOutputStream(Files.newOutputStream(sequenceFile));
+		gzOut.write(FASTQ_FILE_CONTENTS);
+		gzOut.close();
+
+		sf.setFile(sequenceFile);
+		SingleEndSequenceFile so = new SingleEndSequenceFile(sf);
+
+		objectService.createSequencingObjectInSample(so, s);
+
+		SequencingRun mr = sequencingRunService.read(1L);
+		sequencingRunService.addSequencingObjectToSequencingRun(mr, so);
+
+		// Wait 5 seconds. file processing should have run by then.
+		Thread.sleep(5000);
+
+		Sample readSample = sampleService.read(s.getId());
+
+		List<QCEntry> qcEntries = sampleService.getQCEntriesForSample(readSample);
+
+		assertEquals("should be one qc entry", 1, qcEntries.size());
+		QCEntry qcEntry = qcEntries.iterator().next();
+
+		assertTrue("should be coverage entry", qcEntry instanceof CoverageQCEntry);
+		assertTrue("qc should have passed", qcEntry.isPositive());
+		assertEquals("should be 3x coverage", "3x", qcEntry.getMessage());
+
+		project.setRequiredCoverage(10);
+		project = projectService.update(project);
+		projectService.runCoverageForProject(project);
+
+		// Wait 5 seconds. file processing should have run by then.
+		Thread.sleep(5000);
+
+		qcEntries = sampleService.getQCEntriesForSample(readSample);
+		assertEquals("should be one qc entry", 1, qcEntries.size());
+		qcEntry = qcEntries.iterator().next();
+		assertTrue("should be coverage entry", qcEntry instanceof CoverageQCEntry);
+		assertFalse("qc should have failed", qcEntry.isPositive());
 	}
 }
