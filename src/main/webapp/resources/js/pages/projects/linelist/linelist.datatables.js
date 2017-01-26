ce7458e0fcc7fb1ff06490d13bf14b7bb67c74b4
Josh Adam <josh.adam@canada.ca>
2017-01-25 18:32:41 -0600

    Code cleanup

--- src/main/webapp/resources/js/pages/projects/linelist/linelist.datatables.js
+++ src/main/webapp/resources/js/pages/projects/linelist/linelist.datatables.js
@@ -9,7 +9,15 @@ require('datatables.net-scroller');
 require('style!datatables.net-scroller-bs/css/scroller.bootstrap.css');
 require('style!datatables-bootstrap3-plugin/media/css/datatables-bootstrap3.css');
 
-const $table = $(`#linelist`).DataTable({
+// window.metadataList is not in a format that Datatables
+// can use.  This transforms it into an array of values.
+const data = window.metadataList.map(row => {
+  return Object.keys(row).map(key => row[key].value);
+});
+
+// Initialize the Datatable on the page.
+const table = $(`#linelist`).DataTable({
+  data,
   dom,
   scrollX: true,
   scrollY: '50vh',
@@ -19,7 +27,7 @@ const $table = $(`#linelist`).DataTable({
 document.body.addEventListener(EVENTS.TABLE.columnVisibility, e => {
   const columnName = e.detail.column;
   if (columnName) {
-    const column = $table.column(columnName);
+    const column = table.column(columnName);
     column.visible(!column.visible());
   }
 });
