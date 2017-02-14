8a0ec1c2337453c1089275a86274e1e81cd2c05a
Josh Adam <josh.adam@canada.ca>
2017-02-14 09:39:43 -0600

    Breaking settings down into page partials.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSettingsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSettingsController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.common.collect.ImmutableMap;
-
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.project.ProjectSyncFrequency;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteStatus;
@@ -26,6 +24,8 @@ import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.remote.ProjectRemoteService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
+
+import com.google.common.collect.ImmutableMap;
 
 @Controller
 @RequestMapping("/projects/{projectId}/settings")
@@ -47,24 +47,51 @@ public class ProjectSettingsController {
 	}
 
 	/**
-	 * Request for a {@link Project} settings page
+	 * Request for a {@link Project} basic settings page
 	 *
 	 * @param projectId
-	 *            the ID of the {@link Project} to read
+	 * 		the ID of the {@link Project} to read
 	 * @param model
-	 *            Model for the view
+	 * 		Model for the view
 	 * @param principal
-	 *            Logged in user
+	 * 		Logged in user
+	 *
 	 * @return name of the project settings page
 	 */
 	@RequestMapping("")
 	@PreAuthorize("hasPermission(#projectId, 'canManageLocalProjectSettings')")
-	public String getProjectSettingsPage(@PathVariable Long projectId, final Model model, final Principal principal) {
+	public String getProjectSettingsBasicPage(@PathVariable Long projectId, final Model model,
+			final Principal principal) {
+		Project project = projectService.read(projectId);
+		model.addAttribute("project", project);
+		projectControllerUtils.getProjectTemplateDetails(model, principal, project);
+		model.addAttribute("activeNave", "settings");
+		model.addAttribute("page", "basic");
+		return "projects/project_settings";
+	}
+
+	/**
+	 * Request for a {@link Project} remote settings page
+	 *
+	 * @param projectId
+	 * 		the ID of the {@link Project} to read
+	 * @param model
+	 * 		Model for the view
+	 * @param principal
+	 * 		Logged in user
+	 *
+	 * @return name of the project remote settings page
+	 */
+	@RequestMapping("/remote")
+	@PreAuthorize("hasPermission(#projectId, 'canManageLocalProjectSettings')")
+	public String getProjectSettingsRemotePage(@PathVariable Long projectId, final Model model,
+			final Principal principal) {
 		Project project = projectService.read(projectId);
 		model.addAttribute("project", project);
 		model.addAttribute("frequencies", ProjectSyncFrequency.values());
 		projectControllerUtils.getProjectTemplateDetails(model, principal, project);
 		model.addAttribute("activeNave", "settings");
+		model.addAttribute("page", "remote");
 		return "projects/project_settings";
 	}
 
@@ -72,18 +99,18 @@ public class ProjectSettingsController {
 	 * Update the project sync settings
 	 *
 	 * @param projectId
-	 *            the project id to update
+	 * 		the project id to update
 	 * @param frequency
-	 *            the sync frequency to set
+	 * 		the sync frequency to set
 	 * @param forceSync
-	 *            Set the project's sync status to MARKED
+	 * 		Set the project's sync status to MARKED
 	 * @param changeUser
-	 *            update the user on a remote project to the current logged in
-	 *            user
+	 * 		update the user on a remote project to the current logged in user
 	 * @param principal
-	 *            The current logged in user
+	 * 		The current logged in user
 	 * @param locale
-	 *            user's locale
+	 * 		user's locale
+	 *
 	 * @return result message if successful
 	 */
 	@RequestMapping(value = "/sync", method = RequestMethod.POST)
@@ -114,25 +141,26 @@ public class ProjectSettingsController {
 
 		if (changeUser) {
 			// ensure the user can read the project
-			try{
+			try {
 				projectRemoteService.read(remoteStatus.getURL());
 
 				User user = userService.getUserByUsername(principal.getName());
 				remoteStatus.setReadBy(user);
 				updates.put("remoteStatus", remoteStatus);
-				message = messageSource.getMessage("project.settings.notifications.sync.userchange", new Object[] {}, locale);
-			}catch(Exception ex){
-				error = messageSource.getMessage("project.settings.notifications.sync.userchange.error", new Object[] {}, locale);
+				message = messageSource
+						.getMessage("project.settings.notifications.sync.userchange", new Object[] {}, locale);
+			} catch (Exception ex) {
+				error = messageSource
+						.getMessage("project.settings.notifications.sync.userchange.error", new Object[] {}, locale);
 			}
 		}
 
 		projectService.updateProjectSettings(read, updates);
 
-		Map<String,String> response;
-		if(error == null){
+		Map<String, String> response;
+		if (error == null) {
 			response = ImmutableMap.of("result", message);
-		}
-		else{
+		} else {
 			response = ImmutableMap.of("error", error);
 		}
 
@@ -143,11 +171,12 @@ public class ProjectSettingsController {
 	 * Update the project assembly setting for the {@link Project}
 	 *
 	 * @param projectId
-	 *            the ID of a {@link Project}
+	 * 		the ID of a {@link Project}
 	 * @param assemble
-	 *            Whether or not to do automated assemblies
+	 * 		Whether or not to do automated assemblies
 	 * @param model
-	 *            Model for the view
+	 * 		Model for the view
+	 *
 	 * @return success message if successful
 	 */
 	@RequestMapping(value = "/assemble", method = RequestMethod.POST)
@@ -156,19 +185,20 @@ public class ProjectSettingsController {
 			final Model model, Locale locale) {
 		Project read = projectService.read(projectId);
 
-		Map<String,Object> updates = new HashMap<>();
+		Map<String, Object> updates = new HashMap<>();
 		updates.put("assembleUploads", assemble);
 
 		projectService.updateProjectSettings(read, updates);
 
-
 		String message = null;
 		if (assemble) {
-			message = messageSource.getMessage("project.settings.notifications.assemble.enabled",
-					new Object[] { read.getLabel() }, locale);
+			message = messageSource
+					.getMessage("project.settings.notifications.assemble.enabled", new Object[] { read.getLabel() },
+							locale);
 		} else {
-			message = messageSource.getMessage("project.settings.notifications.assemble.disabled",
-					new Object[] { read.getLabel() }, locale);
+			message = messageSource
+					.getMessage("project.settings.notifications.assemble.disabled", new Object[] { read.getLabel() },
+							locale);
 		}
 
 		return ImmutableMap.of("result", message);
