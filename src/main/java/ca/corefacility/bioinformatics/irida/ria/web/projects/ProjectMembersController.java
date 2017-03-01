1120ddcf4d3ff1f6cf4ea6dd68832ecaf1c76ff5
tom <thomas.matthews@phac-aspc.gc.ca>
2017-03-01 11:01:47 -0600

    moved groups under settings panel

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectMembersController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectMembersController.java
@@ -115,7 +115,7 @@ public class ProjectMembersController {
 	 *            Id for the project to show the users for
 	 * @return The name of the project members page.
 	 */
-	@RequestMapping(value = "/{projectId}/groups", method = RequestMethod.GET)
+	@RequestMapping(value = "/{projectId}/settings/groups", method = RequestMethod.GET)
 	public String getProjectGroupsPage(final Model model, final Principal principal, @PathVariable Long projectId) {
 		logger.trace("Getting project members for project " + projectId);
 		Project project = projectService.read(projectId);
@@ -123,7 +123,8 @@ public class ProjectMembersController {
 		projectUtils.getProjectTemplateDetails(model, principal, project);
 		model.addAttribute(ACTIVE_NAV, ACTIVE_NAV_MEMBERS);
 		model.addAttribute("projectRoles", projectRoles);
-		return PROJECT_GROUPS_PAGE;
+		model.addAttribute("page", "groups");
+		return "projects/project_settings";
 	}
 
 	/**
@@ -166,7 +167,7 @@ public class ProjectMembersController {
 	 *            the reported locale of the browser
 	 * @return map for showing success message.
 	 */
-	@RequestMapping(value = "/{projectId}/groups", method = RequestMethod.POST)
+	@RequestMapping(value = "/{projectId}/settings/groups", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, String> addProjectGroupMember(@PathVariable Long projectId, @RequestParam Long memberId,
 			@RequestParam String projectRole, Locale locale) {
@@ -207,7 +208,7 @@ public class ProjectMembersController {
 	 *            A search term
 	 * @return A {@code Map<Long,String>} of the userID and user label
 	 */
-	@RequestMapping("/{projectId}/ajax/availablegroupmembers")
+	@RequestMapping("/{projectId}/settings/ajax/availablegroupmembers")
 	@ResponseBody
 	public Collection<UserGroup> getGroupsAvailableForProject(@PathVariable Long projectId, @RequestParam String term) {
 		final Project project = projectService.read(projectId);
@@ -248,7 +249,7 @@ public class ProjectMembersController {
 	 * @param userId
 	 *            The user to remove
 	 */
-	@RequestMapping(path = "{projectId}/groups/{userId}", method = RequestMethod.DELETE)
+	@RequestMapping(path = "{projectId}/settings/groups/{userId}", method = RequestMethod.DELETE)
 	@ResponseBody
 	public Map<String, String> removeUserGroup(final @PathVariable Long projectId, final @PathVariable Long userId, final Locale locale) {
 		final Project project = projectService.read(projectId);
@@ -303,7 +304,7 @@ public class ProjectMembersController {
 	 * @param projectRole
 	 *            The role to set
 	 */
-	@RequestMapping(path = "{projectId}/groups/editrole/{userId}", method = RequestMethod.POST)
+	@RequestMapping(path = "{projectId}/settings/groups/editrole/{userId}", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, String> updateUserGroupRole(final @PathVariable Long projectId, final @PathVariable Long userId,
 			final @RequestParam String projectRole, final Locale locale) {
@@ -362,7 +363,7 @@ public class ProjectMembersController {
 	 *            the id of the project we're looking at
 	 * @return a page of groups on the project.
 	 */
-	@RequestMapping(value = "/ajax/{projectId}/groups")
+	@RequestMapping(value = "/{projectId}/settings/ajax/groups")
 	public @ResponseBody DatatablesResponse<UserGroupProjectJoin> getProjectGroupMembers(
 			final @DatatablesParams DatatablesCriterias criteria, final @PathVariable Long projectId) {
 		final Project p = projectService.read(projectId);
@@ -408,7 +409,7 @@ public class ProjectMembersController {
 	 *            Model for rendering the view
 	 * @return Name of the user group removal modal
 	 */
-	@RequestMapping(path = "/removeUserGroupModal", method = RequestMethod.POST)
+	@RequestMapping(path = "/settings/removeUserGroupModal", method = RequestMethod.POST)
 	public String getRemoveUserGroupModal(final @RequestParam Long memberId, final Model model) {
 		final UserGroup userGroup = userGroupService.read(memberId);
 		model.addAttribute("member", userGroup);
