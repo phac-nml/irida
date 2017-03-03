0e0fe98779689be7f74ce18fcd7d168c0331cb8e
Josh Adam <josh.adam@canada.ca>
2017-03-03 08:49:47 -0600

    List templates

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSettingsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSettingsController.java
@@ -1,7 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.web.projects;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -16,13 +18,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectMetadataTemplateJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.project.ProjectSyncFrequency;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteStatus;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteStatus.SyncStatus;
+import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplate;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.remote.ProjectRemoteService;
+import ca.corefacility.bioinformatics.irida.service.sample.MetadataTemplateService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
 
 import com.google.common.collect.ImmutableMap;
@@ -31,15 +36,18 @@ import com.google.common.collect.ImmutableMap;
 @RequestMapping("/projects/{projectId}/settings")
 public class ProjectSettingsController {
 	private final MessageSource messageSource;
+	private final MetadataTemplateService metadataTemplateService;
 	private final ProjectControllerUtils projectControllerUtils;
 	private final ProjectService projectService;
 	private final ProjectRemoteService projectRemoteService;
 	private final UserService userService;
 
 	@Autowired
-	public ProjectSettingsController(MessageSource messageSource, ProjectControllerUtils projectControllerUtils,
-			ProjectService projectService, ProjectRemoteService projectRemoteService, UserService userService) {
+	public ProjectSettingsController(MessageSource messageSource, MetadataTemplateService metadataTemplateService,
+			ProjectControllerUtils projectControllerUtils, ProjectService projectService,
+			ProjectRemoteService projectRemoteService, UserService userService) {
 		this.messageSource = messageSource;
+		this.metadataTemplateService = metadataTemplateService;
 		this.projectControllerUtils = projectControllerUtils;
 		this.projectService = projectService;
 		this.projectRemoteService = projectRemoteService;
@@ -92,6 +100,39 @@ public class ProjectSettingsController {
 		projectControllerUtils.getProjectTemplateDetails(model, principal, project);
 		model.addAttribute("activeNave", "settings");
 		model.addAttribute("page", "remote");
+		return "projects/project_settings";
+	}
+
+	/**
+	 * Request for a {@link Project} remote settings page
+	 *
+	 * @param projectId
+	 * 		the ID of the {@link Project} to read
+	 * @param model
+	 * 		Model for the view
+	 * @param principal
+	 * 		Logged in user
+	 *
+	 * @return name of the project remote settings page
+	 */
+	@RequestMapping("/metadata-templates")
+	@PreAuthorize("hasPermission(#projectId, 'canManageLocalProjectSettings')")
+	public String getSampleMetadataTemplatesPage(@PathVariable Long projectId, final Model model,
+			final Principal principal) {
+		Project project = projectService.read(projectId);
+		model.addAttribute("project", project);
+		projectControllerUtils.getProjectTemplateDetails(model, principal, project);
+
+		List<ProjectMetadataTemplateJoin> templateJoins = metadataTemplateService
+				.getMetadataTemplatesForProject(project);
+		List<MetadataTemplate> templates = new ArrayList<>();
+		for (ProjectMetadataTemplateJoin join : templateJoins) {
+			templates.add(join.getObject());
+		}
+		model.addAttribute("templates", templates);
+
+		model.addAttribute("activeNave", "settings");
+		model.addAttribute("page", "metadata_templates");
 		return "projects/project_settings";
 	}
 
