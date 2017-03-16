37b7d79a41cc0052f8a8691218f3d6991541bed6
Josh Adam <josh.adam@canada.ca>
2017-03-16 11:18:08 -0500

    Updated documentation on column reordering based on templates.

--- src/main/webapp/resources/js/pages/projects/linelist/components/linelist-table/linelist-table.component.js
+++ src/main/webapp/resources/js/pages/projects/linelist/components/linelist-table/linelist-table.component.js
@@ -68,26 +68,42 @@ function controller($scope,
    * Listen for changes to which template is to be displayed in the table.
    */
   $scope.$on(EVENTS.TABLE.template, (e, args) => {
-    const {fields} = args;
+    const {templateFields} = args.fields;
+
+    // The `order` will be and array of the current ordering (e.g.: [1, 2, 4, 5, 3])
+    // in relation to its initial order.
     const order = $ctrl.table.DataTable.colReorder.order();
 
-    let openColumn = fields.length; // Start of the non-template fields
+    // This will be the position in the order for the next non-template field.
+    let openColumn = templateFields.length;
+
+    // We need to update the order of the table to reflect the current template.
+    // This goes through each column int the table
     $ctrl.dtColumns.forEach((column, index) => {
-      // See if the column should be displayed based on the template
-      // and find out where.
-      const fieldIndex = fields.findIndex(field => {
+      // See if the column is in the template fields, and capture its index
+      // to add to the new order.
+      const templateIndex = templateFields.findIndex(field => {
         return field.label === column.sTitle;
       });
-      if (fieldIndex > -1) {
-        order[fieldIndex] = index;
+
+      if (templateIndex > -1) {
+        // Column has been found in the template.
+        // Add the column's index (this is the columns original index when the table was created)
+        // to the order array and the position it is required in the template.
+        // E.g. If it is originally the 2nd column, but needs to be in the first position [2, ...]
+        order[templateIndex] = index;
+        // Only template columns should be visible
         column.visible = true;
       } else {
+        // Column not found in the template.
+        // Add it to the next open column position - these are after the template slots.
         order[openColumn] = index;
         openColumn += 1;
+        // Only template columns should be visible
         column.visible = false;
       }
     });
-    // Complete the actual reorder in the Datatables.
+    // Ask Datatables to display the updated order.
     $ctrl.dtOptions.withColReorderOrder(order);
   });
 }
