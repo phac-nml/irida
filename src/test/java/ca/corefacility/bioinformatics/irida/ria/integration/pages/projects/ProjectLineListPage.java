c4fa875c525650cbeca2663bc4d4609ce463701b
Josh Adam <josh.adam@canada.ca>
2017-04-28 08:17:26 -0500

    Ensure the modal is closed before trying to proceed.

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/projects/ProjectLineListPage.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/projects/ProjectLineListPage.java
@@ -2,11 +2,11 @@ package ca.corefacility.bioinformatics.irida.ria.integration.pages.projects;
 
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -72,8 +72,9 @@ public class ProjectLineListPage extends ProjectPageBase {
 	}
 
 	public void closeColumnVisibilityPanel() {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 		closeAsideBtn.click();
-		waitForTime(300); // Wait for animation to end.
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("metadata-open")));
 	}
 
 	public void toggleColumn(String buttonLabel) {
