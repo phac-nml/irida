fd0bae149c772c5e954cd4c66ed12eb958fb7242
tom <thomas.matthews@phac-aspc.gc.ca>
2017-05-03 14:31:35 -0500

    added method to add metadata

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/samples/RESTSampleMetadataController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/samples/RESTSampleMetadataController.java
@@ -3,6 +3,7 @@ package ca.corefacility.bioinformatics.irida.web.controller.api.samples;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,6 +22,7 @@ import ca.corefacility.bioinformatics.irida.model.IridaResourceSupport;
 import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplateField;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.model.sample.metadata.MetadataEntry;
+import ca.corefacility.bioinformatics.irida.service.sample.MetadataTemplateService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.web.controller.api.RESTGenericController;
 import ca.corefacility.bioinformatics.irida.web.controller.api.projects.RESTProjectSamplesController;
@@ -27,18 +30,21 @@ import ca.corefacility.bioinformatics.irida.web.controller.api.projects.RESTProj
 @Controller
 public class RESTSampleMetadataController {
 	private static final Logger logger = LoggerFactory.getLogger(RESTSampleMetadataController.class);
-	
+
 	public static final String SAMPLE_REL = "sample";
 
 	private SampleService sampleService;
+	private MetadataTemplateService metadataTemplateService;
 
 	@Autowired
-	public RESTSampleMetadataController(SampleService sampleService) {
+	public RESTSampleMetadataController(SampleService sampleService, MetadataTemplateService metadataTemplateService) {
 		this.sampleService = sampleService;
+		this.metadataTemplateService = metadataTemplateService;
 	}
-	
+
 	@RequestMapping(value = "/api/samples/{sampleId}/metadata", method = RequestMethod.GET)
-	public ModelMap getSampleMetadata(@PathVariable Long sampleId){
+	public ModelMap getSampleMetadata(@PathVariable Long sampleId) {
+		logger.trace("Getting sample metadata for " + sampleId);
 		ModelMap modelMap = new ModelMap();
 		Sample s = sampleService.read(sampleId);
 
@@ -47,23 +53,54 @@ public class RESTSampleMetadataController {
 		modelMap.addAttribute(RESTGenericController.RESOURCE_NAME, response);
 		return modelMap;
 	}
-	
+
+	@RequestMapping(value = "/api/samples/{sampleId}/metadata", method = RequestMethod.POST)
+	public ModelMap saveSampleMetadata(@PathVariable Long sampleId,
+			@RequestBody Map<String, MetadataEntry> metadataMap) {
+		Sample s = sampleService.read(sampleId);
+
+		Map<MetadataTemplateField, MetadataEntry> metadata = new HashMap<>();
+		metadataMap.entrySet().forEach(e -> {
+
+			// get the metadatatemplatefield if it exists
+			MetadataTemplateField field = metadataTemplateService.readMetadataFieldByLabel(e.getKey());
+
+			// if not, create a new one
+			if (field == null) {
+				field = new MetadataTemplateField(e.getKey(), "text");
+				field = metadataTemplateService.saveMetadataField(field);
+				logger.trace("creating new template field: " + e.getKey());
+			}
+
+			metadata.put(field, e.getValue());
+		});
+
+		s.setMetadata(metadata);
+
+		sampleService.update(s);
+
+		return getSampleMetadata(sampleId);
+	}
+
 	private SampleMetadataResponse buildSampleMetadataResponse(final Sample s) {
 		SampleMetadataResponse response = new SampleMetadataResponse(s);
 		response.add(linkTo(methodOn(RESTProjectSamplesController.class).getSample(s.getId())).withRel(SAMPLE_REL));
-		
+
 		return response;
 	}
-	
-	private class SampleMetadataResponse extends IridaResourceSupport{
+
+	/**
+	 * Response class so we can add links to sample metadata
+	 */
+	private class SampleMetadataResponse extends IridaResourceSupport {
 		Sample sample;
-		
-		public SampleMetadataResponse(Sample sample){
+
+		public SampleMetadataResponse(Sample sample) {
 			this.sample = sample;
 		}
-		
+
 		@JsonAnyGetter
-		public Map<MetadataTemplateField,MetadataEntry> getMetadata(){
+		public Map<MetadataTemplateField, MetadataEntry> getMetadata() {
 			return sample.getMetadata();
 		}
 	}
