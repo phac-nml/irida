9d641cb00ee0acba54547efac11730b93c4ca4e4
Josh Adam <josh.adam@canada.ca>
2017-05-19 11:36:14 -0500

    Fixed initial test and added test for manager.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectMetadataTemplateIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectMetadataTemplateIT.java
@@ -7,17 +7,24 @@ import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectSettingsMetadataTemplatesPage;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectMetadataTemplateView.xml")
 public class ProjectMetadataTemplateIT extends AbstractIridaUIITChromeDriver {
 	private final int PROJECT_ID = 1;
 
 	// Test creating a template as user;
-	@Test
+	@Test(expected = AssertionError.class)
 	public void testCreateTemplateAsUser() {
 		LoginPage.loginAsUser(driver());
+		ProjectSettingsMetadataTemplatesPage.goToPage(driver(), PROJECT_ID);
+	}
+
+	// Test creating a template as user;
+	@Test(expected = AssertionError.class)
+	public void testCreateTemplateAsManager() {
+		LoginPage.loginAsManager(driver());
 		ProjectSettingsMetadataTemplatesPage page = ProjectSettingsMetadataTemplatesPage.goToPage(driver(), PROJECT_ID);
-		assertEquals("Shoud be able to see metadata templates", 1, page.getNumberOfTemplatesInProject());
+		assertEquals("Should be one template on the page", 1, page.getNumberOfTemplatesInProject());
 	}
 }
