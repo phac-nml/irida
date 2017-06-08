8b9db43a8b01abdcf0536bacd43931dd10667b3a
Josh Adam <josh.adam@canada.ca>
2017-06-07 23:16:20 -0500

    Updated documentation.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/joins/project/ProjectMetadataTemplateJoinRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/joins/project/ProjectMetadataTemplateJoinRepository.java
@@ -1,18 +1,23 @@
 package ca.corefacility.bioinformatics.irida.repositories.joins.project;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
-
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectMetadataTemplateJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.repositories.IridaJpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Repository for storing and reading {@link ProjectMetadataTemplateJoin}s
  */
 public interface ProjectMetadataTemplateJoinRepository extends IridaJpaRepository<ProjectMetadataTemplateJoin, Long> {
 
-	@Query("FROM ProjectMetadataTemplateJoin j WHERE j.project=?1")
-	public List<ProjectMetadataTemplateJoin> getMetadataTemplatesForProject(Project project);
+    /**
+     * Get a {@link List} of {@link ProjectMetadataTemplateJoin} related to a {@link Project}
+     *
+     * @param project The {@link Project} to find templates for.
+     * @return A {@link List} of {@link ProjectMetadataTemplateJoin} belonging to a specific {@link Project}
+     */
+    @Query("FROM ProjectMetadataTemplateJoin j WHERE j.project=?1")
+    public List<ProjectMetadataTemplateJoin> getMetadataTemplatesForProject(Project project);
 }
