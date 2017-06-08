a1ab0e2ef9442c03deb84fe4e57b09d690ce3120
Josh Adam <josh.adam@canada.ca>
2017-06-07 23:18:31 -0500

    Updated comments.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/MetadataFieldRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/MetadataFieldRepository.java
@@ -1,29 +1,32 @@
 package ca.corefacility.bioinformatics.irida.repositories.sample;
 
-import java.util.List;
-
+import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplateField;
+import ca.corefacility.bioinformatics.irida.repositories.IridaJpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplateField;
-import ca.corefacility.bioinformatics.irida.repositories.IridaJpaRepository;
+import java.util.List;
 
 /**
  * A repository for storing and reading {@link MetadataTemplateField}s
  */
 public interface MetadataFieldRepository extends IridaJpaRepository<MetadataTemplateField, Long> {
 
-	/**
-	 * Get a {@link MetadataTemplateField} based on its {@link String} label.
-	 *
-	 * @param label
-	 * 		the {@link String} field label
-	 *
-	 * @return {@link MetadataTemplateField}
-	 */
-	@Query("from MetadataTemplateField m where m.label = ?1")
-	public MetadataTemplateField findMetadataFieldByLabel(String label);
+    /**
+     * Get a {@link MetadataTemplateField} based on its {@link String} label.
+     *
+     * @param label the {@link String} field label
+     * @return {@link MetadataTemplateField}
+     */
+    @Query("from MetadataTemplateField m where m.label = ?1")
+    public MetadataTemplateField findMetadataFieldByLabel(String label);
 
-	@Query("FROM MetadataTemplateField m where m.label LIKE %:query%")
-	public List<MetadataTemplateField> findAllMetadataFieldsByLabelQuery(@Param("query") String query);
+    /**
+     * Get a {@link List} of {@link MetadataTemplateField} with a label that partially matches the query
+     *
+     * @param query The {@link String} to test the {@link MetadataTemplateField}'s label against.
+     * @return {@link List} of {@link MetadataTemplateField} with labels that match the query.
+     */
+    @Query("FROM MetadataTemplateField m where m.label LIKE %:query%")
+    public List<MetadataTemplateField> findAllMetadataFieldsByLabelQuery(@Param("query") String query);
 }
