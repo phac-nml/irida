5aae003ad61018fbb63f81a09adbc5b69c2ccfaf
Josh Adam <josh.adam@canada.ca>
2017-06-24 23:15:01 -0500

    incorrect import statement.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/projects/common/sample-metadata-templates/sample-metadata-template.module.js
+++ src/main/webapp/resources/js/pages/projects/common/sample-metadata-templates/sample-metadata-template.module.js
@@ -1,4 +1,4 @@
-import 'angular';
+import angular from 'angular';
 import 'angular-resource';
 import {metadataTemplateName} from './directives/metadataTemplateName';
 import {SampleMetadataTemplateService} from './services/sample-metadata-template.service';
