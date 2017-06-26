2dea58773a34daff14a63119daf4e37bc399757e
Josh Adam <josh.adam@canada.ca>
2017-06-26 08:02:57 -0500

    Working table

---
+++ src/main/webapp/resources/js/pages/analysis/analysis-service.js
@@ -0,0 +1,13 @@
+const $ = require('jquery');
+
+/**
+ * Delete an analysis.
+ * @param {object} analysis to delete
+ * @return {*} promise that the delete will occur.
+ */
+export function deleteAnalysis({id}) {
+  return $.ajax({
+    url: `${window.PAGE.URLS.deleteUrl}${id}`,
+    type: 'DELETE'
+  });
+}
