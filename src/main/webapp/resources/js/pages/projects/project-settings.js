ba91d2903b9f55de446f6957ffa48f3dd25366e6
tom <thomas.matthews@phac-aspc.gc.ca>
2017-07-05 12:10:41 -0500

    added "not set" to coverage settings when nothing's entered

--- src/main/webapp/resources/js/pages/projects/project-settings.js
+++ src/main/webapp/resources/js/pages/projects/project-settings.js
@@ -58,9 +58,24 @@ var projectSettings = (function(page, notifications) {
                 200 : function(response){
                     notifications.show({'msg': response.result});
                     
-                    $("#minimum-coverage-display").html(minimumCoverage + "x");
-                    $("#maximum-coverage-display").html(maximumCoverage + "x");
-                    $("#genome-size-display").html(genomeSize + "bp");
+                    
+                    if(minimumCoverage) {
+                        $("#minimum-coverage-display").html(minimumCoverage + "x");
+                    } else {
+                        $("#minimum-coverage-display").html(page.i18n.not_set);
+                    }
+
+                    if(maximumCoverage) {
+                        $("#maximum-coverage-display").html(maximumCoverage + "x");
+                    } else {
+                        $("#maximum-coverage-display").html(page.i18n.not_set);
+                    }
+
+                    if(genomeSize) {
+                        $("#genome-size-display").html(genomeSize + "bp");
+                    } else {
+                        $("#genome-size-display").html(page.i18n.not_set);
+                    }
 
                     $(".edit-coverage").toggle();
                 }
