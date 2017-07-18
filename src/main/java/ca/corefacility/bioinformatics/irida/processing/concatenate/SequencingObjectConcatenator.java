8c0c3647ad0f20d66e25b4db1516c908669f1a62
tom <thomas.matthews@phac-aspc.gc.ca>
2017-07-18 16:09:34 -0500

    changed concatenator interface to abstract class as it can use the same appending method

--- src/main/java/ca/corefacility/bioinformatics/irida/processing/concatenate/SequencingObjectConcatenator.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/processing/concatenate/SequencingObjectConcatenator.java
@@ -1,10 +1,32 @@
 package ca.corefacility.bioinformatics.irida.processing.concatenate;
 
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
+
 import ca.corefacility.bioinformatics.irida.exceptions.ConcatenateException;
+import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequenceFile;
 import ca.corefacility.bioinformatics.irida.model.sequenceFile.SequencingObject;
 
-public interface SequencingObjectConcatenator<Type extends SequencingObject> {
-	public Type concatenateFiles(Set<? extends SequencingObject> toConcatenate) throws ConcatenateException;
+public abstract class SequencingObjectConcatenator<Type extends SequencingObject> {
+	public abstract Type concatenateFiles(Set<? extends SequencingObject> toConcatenate) throws ConcatenateException;
+
+	protected void appendToFile(Path target, SequenceFile file) throws ConcatenateException {
+
+		try (FileWriter fw = new FileWriter(target.toFile(), true);
+				BufferedWriter writer = new BufferedWriter(fw);
+				FileReader reader = new FileReader(file.getFile().toFile())) {
+
+			IOUtils.copy(reader, writer);
+
+		} catch (IOException e) {
+			throw new ConcatenateException("Could not open target file for writing", e);
+		}
+
+	}
 }
