f2e9c5f4117c18ffdfc4221c48260ba1bd85eb4e
Josh Adam <josh.adam@canada.ca>
2017-08-30 09:37:38 -0500

    Removed all dandelion files from events

--- src/main/webapp/configs/webpack/entries.js
+++ src/main/webapp/configs/webpack/entries.js
@@ -5,6 +5,7 @@
  */
 module.exports = {
   app: "./resources/js/app.js",
+  events: "./resources/js/modules/events/events.js",
   projects: "./resources/js/pages/projects/projects.js",
   "samples-metadata-import":
     "./resources/js/pages/projects/samples-metadata-import/index.js",
