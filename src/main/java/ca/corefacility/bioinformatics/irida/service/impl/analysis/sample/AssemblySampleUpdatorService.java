8e400ed90931bf42eb6fbb8f5529667c1f9db0fa
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2017-09-06 15:39:04 -0500

    Got production assemblies to save properly

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/analysis/sample/AssemblySampleUpdatorService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/analysis/sample/AssemblySampleUpdatorService.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import ca.corefacility.bioinformatics.irida.model.assembly.GenomeAssembly;
 import ca.corefacility.bioinformatics.irida.model.assembly.GenomeAssemblyFromAnalysis;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.SampleGenomeAssemblyJoin;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmission;
+import ca.corefacility.bioinformatics.irida.repositories.joins.sample.GenomeAssemblyRepository;
 import ca.corefacility.bioinformatics.irida.repositories.joins.sample.SampleGenomeAssemblyJoinRepository;
 import ca.corefacility.bioinformatics.irida.service.analysis.sample.AnalysisSampleUpdatorService;
 
@@ -23,11 +25,14 @@ public class AssemblySampleUpdatorService implements AnalysisSampleUpdatorServic
 
 	private static final Logger logger = LoggerFactory.getLogger(AssemblySampleUpdatorService.class);
 
+	private final GenomeAssemblyRepository genomeAssemblyRepository;
 	private final SampleGenomeAssemblyJoinRepository sampleGenomeAssemblyJoinRepository;
 
 	@Autowired
-	public AssemblySampleUpdatorService(SampleGenomeAssemblyJoinRepository sampleGenomeAssemblyJoinRepository) {
+	public AssemblySampleUpdatorService(GenomeAssemblyRepository genomeAssemblyRepository,
+			SampleGenomeAssemblyJoinRepository sampleGenomeAssemblyJoinRepository) {
 		this.sampleGenomeAssemblyJoinRepository = sampleGenomeAssemblyJoinRepository;
+		this.genomeAssemblyRepository = genomeAssemblyRepository;
 	}
 
 	@Override
@@ -38,10 +43,12 @@ public class AssemblySampleUpdatorService implements AnalysisSampleUpdatorServic
 		}
 
 		Sample sample = samples.iterator().next();
-		SampleGenomeAssemblyJoin sampleGenomeAssemblyJoin = new SampleGenomeAssemblyJoin(sample,
-				new GenomeAssemblyFromAnalysis(analysis));
-		
-		logger.trace("Saving join for sample [" + sample.getId() + "] to analysis submission [" + analysis.getId() + "]");
+
+		GenomeAssembly genomeAssembly = genomeAssemblyRepository.save(new GenomeAssemblyFromAnalysis(analysis));
+		SampleGenomeAssemblyJoin sampleGenomeAssemblyJoin = new SampleGenomeAssemblyJoin(sample, genomeAssembly);
+
+		logger.trace(
+				"Saving join for sample [" + sample.getId() + "] to analysis submission [" + analysis.getId() + "]");
 		sampleGenomeAssemblyJoinRepository.save(sampleGenomeAssemblyJoin);
 	}
 }
