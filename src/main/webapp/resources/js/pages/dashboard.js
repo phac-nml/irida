3d3dfd1f9dec19fff1c5ae7728f5f88cd7eb3363
Josh Adam <josh.s.adam@gmail.com>
2017-09-20 15:05:46 -0500

    Fixed issue with dashboard announcements

--- src/main/webapp/resources/js/pages/dashboard.js
+++ src/main/webapp/resources/js/pages/dashboard.js
@@ -3,6 +3,5 @@ import { EventsModule } from "../modules/events/events";
 import { DashboardAnnouncementsModule } from "./announcement/announcementDashboard";
 
 const app = angular.module("irida");
-app
-  .requires.push(EventsModule)
-  .requires.push(DashboardAnnouncementsModule);
+app.requires.push(EventsModule);
+app.requires.push(DashboardAnnouncementsModule);
