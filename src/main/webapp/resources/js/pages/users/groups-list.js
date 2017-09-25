2b261bca53564e269ed85c0f3c769d13278ca009
Josh Adam <josh.s.adam@gmail.com>
2017-09-25 10:05:32 -0500

    Removed commented out text.

--- src/main/webapp/resources/js/pages/users/groups-list.js
+++ src/main/webapp/resources/js/pages/users/groups-list.js
@@ -1,13 +1,13 @@
 import $ from "jquery";
-import "../../vendor/datatables/datatables";
 import {
-  tableConfig,
-  generateColumnOrderInfo,
-  createItemLink,
   createButtonCell,
-  createDeleteBtn
+  createDeleteBtn,
+  createItemLink,
+  generateColumnOrderInfo,
+  tableConfig
 } from "../../utilities/datatables-utilities";
-import { formatDate } from "../../utilities/date-utilities";
+import {formatDate} from "../../utilities/date-utilities";
+import "../../vendor/datatables/datatables";
 
 const table = $("#groupsTable");
 const url = table.data("url");
@@ -91,69 +91,3 @@ $dt.on("click", ".remove-btn", function(e) {
     }
   );
 });
-
-// var groupsTable = (function(page) {
-// 	function groupLinkRow(data, type, full) {
-// 		return '<a class="item-link" title="' + data + '" href="' + page.urls.link
-// 				+ full.group.identifier + '"><span>' + data + '</span></a>';
-// 	};
-//
-// 	function descriptionRow(data, type, full) {
-// 		return '<p class="crop">' + full.group.description + '</p>';
-// 	};
-//
-// 	function removeGroupButton(data, type, full) {
-// 		if (full.groupOwner || full.admin) {
-// 			return "<div class='btn-group pull-right' data-toggle='tooltip' data-placement='left' title='" + page.i18n.remove + "'><button type='button' class='btn btn-default btn-xs remove-group-btn'><span class='fa fa-remove'></span></div>";
-// 		} else {
-// 			return "";
-// 		}
-// 	};
-//
-// 	function deleteLinkCallback(row, data) {
-// 		var row = $(row);
-// 		row.find(".remove-group-btn").click(function () {
-// $("#removeGroupModal").load(
-//   page.urls.deleteModal + "#removeGroupModalGen",
-//   { userGroupId: data.group.identifier },
-//   function() {
-//     var modal = $(this);
-//     modal.on("show.bs.modal", function() {
-//       $(this)
-//         .find("#remove-group-button")
-//         .off("click")
-//         .click(function() {
-//           $.ajax({
-//             url: page.urls.deleteGroup + data.group.identifier,
-//             type: "DELETE",
-//             success: function(result) {
-//               oTable_groupsTable.ajax.reload();
-//               notifications.show({
-//                 msg: result.result
-//               });
-//               modal.modal("hide");
-//             },
-//             error: function() {
-//               notifications.show({
-//                 msg: page.i18n.unexpectedRemoveError,
-//                 type: "error"
-//               });
-//               modal.modal("hide");
-//             }
-//           });
-//         });
-//     });
-//     modal.modal("show");
-//   }
-// );
-// });
-// 		row.find('[data-toggle="tooltip"]').tooltip();
-// 	};
-//
-// 	return {
-// 		groupLinkRow : groupLinkRow,
-// 		removeGroupButton : removeGroupButton,
-// 		deleteLinkCallback : deleteLinkCallback,
-// 		descriptionRow : descriptionRow
-// 	};
-// })(window.PAGE);
