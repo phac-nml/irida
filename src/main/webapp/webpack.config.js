374b900c3477f28567bab59614bedfdbc39db5f7
Josh Adam <josh.adam@canada.ca>
2017-10-06 08:08:48 -0500

    Seperating out development and production

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,17 +1,26 @@
 const path = require("path");
 let entries = require("./configs/webpack/entries.js");
 
-module.exports = {
+const PATHS = {
+  build: path.join(__dirname, "resources/js/build")
+};
+
+const commonConfig = () => ({
   entry: entries,
   devtool: "source-maps",
   module: {
-    loaders: [
+    rules: [
       {
-        test: /.js?$/,
+        test: /\.js$/,
         loader: "babel-loader",
         exclude: /node_modules/,
-        query: {
-          presets: ["es2015", "stage-0"]
+        options: {
+          // Enable caching for improved performance during
+          // development.
+          // It uses default OS directory by default. If you need
+          // something more custom, pass a path to it.
+          // I.e., { cacheDirectory: '<path>' }
+          cacheDirectory: true
         }
       },
       { test: /\.css$/, loader: "style-loader!css-loader" }
@@ -25,6 +34,51 @@ module.exports = {
     moment: "moment"
   },
   output: {
+    path: PATHS.build,
     filename: "[name].bundle.js"
   }
+});
+
+const productionConfig = () => commonConfig();
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. Good for complex setups.
+      historyApiFallback: true,
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+      proxy: {
+        "/": "http://localhost:8080"
+      },
+      hot: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: "errors-only",
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT // Defaults to 8080
+    }
+  };
+
+  return Object.assign({}, commonConfig(), config);
+};
+
+module.exports = env => {
+  console.log(env);
+  if (env.target === "production") {
+    return productionConfig();
+  }
+
+  return developmentConfig();
 };
