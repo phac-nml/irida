622d63bb71c5fc21cb4417db3e3962cb2a22ded9
Josh Adam <josh.adam@canada.ca>
2017-11-15 13:20:13 -0600

    Updated documentation.

--- src/main/webapp/resources/js/pages/projects/samples/SampleButtons.js
+++ src/main/webapp/resources/js/pages/projects/samples/SampleButtons.js
@@ -1,5 +1,16 @@
+/**
+ * This file is used to handle the buttons in the project samples page toolbar.
+ * Each button type is responsible for setting its enabled state and displaying
+ * tooltips explaining to the user what the state means.
+ */
+
 import $ from "jquery";
 
+/**
+ * Check the state of the calling button to see if it should be enabled or not.
+ * @param count the number if samples currently selected.
+ * @param hasAssociated if there are associated project currently displayed.
+ */
 function checkState(count, hasAssociated) {
   // Remove the tooltip. A new one will be created based on the
   // information provided.
@@ -27,13 +38,25 @@ function checkState(count, hasAssociated) {
   }
 }
 
+/**
+ * Class representing the add sample/s to cart button.
+ */
 export class SampleCartButton {
+  /**
+   * Create the cart button
+   * @param {object} node actual DOM node for the button.
+   * @param clickHandler button click handler
+   */
   constructor(node, clickHandler) {
     this.$node = $(node);
     this.$node.on("click", clickHandler);
     this.checkState();
   }
 
+  /**
+   * Check the state the button should be in.
+   * @param  {number} count current number of selected samples (defaults to 0)
+   */
   checkState(count = 0) {
     if (count < this.$node.data("enabledAt")) {
       this.$node.prop("disabled", true);
@@ -43,7 +66,16 @@ export class SampleCartButton {
   }
 }
 
+
+/**
+ * Class representing sample export buttons
+ */
 export class SampleExportButton {
+  /**
+   * Create a sample export button
+   * @param {object} node actual DOM node for the button.
+   * @param clickHandler button click handler
+   */
   constructor(node, clickHandler) {
     const $node = $(node);
     this.$node = $node;
@@ -55,6 +87,11 @@ export class SampleExportButton {
     this.checkState();
   }
 
+  /**
+   * Check the state the button should be in.
+   * @param  {number} count current number of selected samples (defaults to 0)
+   * @param {boolean} hasAssociated if associated projects are displayed in the table.
+   */
   checkState(count = 0, hasAssociated = false) {
     checkState.call(this, count, hasAssociated);
   }
