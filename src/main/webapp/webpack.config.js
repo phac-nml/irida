089bae8bed9ce007868e18bd71b661f05416ef02
Josh Adam <josh.s.adam@gmail.com>
2017-12-06 11:09:52 -0600

    Initial files

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -28,6 +28,18 @@ const commonConfig = merge([
     output: {
       path: PATHS.build,
       filename: "js/[name].bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: "url-loader?limit=10000"
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+          use: "file-loader"
+        }
+      ]
     }
   },
   parts.loadJavaScript(),
