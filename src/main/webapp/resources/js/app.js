e5eb3648f527b3dd8f138154dfaa81b7dd944331
Josh Adam <josh.s.adam@gmail.com>
2017-12-19 10:16:27 -0600

    cleaning up sass files

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -3,10 +3,8 @@ import angular from "angular";
 import "./modules/cart/irida.cart";
 import { IridaSession } from "./services/IridaSession";
 import "./pages/search/irida.search";
-
 // Import css
-import "../../styles/app.scss";
-
+import "../sass/app.scss";
 /*
 This will load notifications into the global namespace.  Remove this once all
 files have been converted over to wekbpack builds.
