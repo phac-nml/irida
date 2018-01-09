de28f5b228795408697b16bccf8fcc75df23d281
Peter Kruczkiewicz <peter.kruczkiewicz@canada.ca>
2018-01-09 16:09:52 -0600

    Hide job error popover on click outside of popover Added i18n for analyses table job error popover Truncating stdout/stderr to N lines rather than N characters Added icon beside Error state with tooltip "Click to preview job error info"
    Signed-off-by: pkruczkiewicz <peter.kruczkiewicz@canada.ca>

--- src/main/webapp/resources/js/pages/analyses/analyses-table.js
+++ src/main/webapp/resources/js/pages/analyses/analyses-table.js
@@ -18,6 +18,12 @@ import {
 import { deleteAnalysis } from "../analysis/analysis-service";
 import { showNotification } from "../../modules/notifications";
 
+/**
+ * Internationalized text from div#messages.hidden
+ * @type {Object} map of data attribute key name to i18n text
+ */
+const MESSAGES = $("#messages").data();
+
 /*
 Get the table headers and create a look up table for them.
 This give the row name in snake case and its index.
@@ -36,8 +42,12 @@ const POPOVER_OPTIONS = {
   template: $("#popover-template").clone()
 };
 
-const SHOW_POPOVER = `[preview]`;
-const HIDE_POPOVER = `[hide]`;
+const jobErrorIcon = `
+<i class="fa fa-fw fa-question-circle js-job-error-tooltip" 
+   data-toggle="tooltip"
+   data-placement="auto right"
+   title="${MESSAGES.tooltip}">
+</i>`;
 
 const CHAR_TO_ESCAPED = {
   "&": "&amp;",
@@ -49,6 +59,13 @@ const CHAR_TO_ESCAPED = {
   "`": "&#x60;",
   "=": "&#x3D;"
 };
+
+/**
+ * Get a handle on the table
+ * @type {*|jQuery|HTMLElement}
+ */
+const $table = $("#analyses");
+
 /**
  * Replace special characters in a string with escaped characters
  * @param string String to escape
@@ -56,17 +73,26 @@ const CHAR_TO_ESCAPED = {
  */
 function escapeHtml(string) {
   return String(string).replace(/[&<>"'`=\/]/g, s => CHAR_TO_ESCAPED[s]);
-
 }
 
 /**
- * Truncate a string to a specified length prepending "..." if truncated
- * @param s String to truncate
- * @param length Truncate string to an integer `length`
- * @returns {string} Truncated string `s` if string length longer than `length`
+ * Truncate a multiline string `s` to only the last `n` lines.
+ * @param s Multiline string to truncate if necessary
+ * @param n Number of lines to take from end of `s`
+ * @param delimiter New-line delimiter
+ * @returns {String} Truncated multiline string
  */
-const truncString = (s, length) => {
-  return s.length > length ? "..." + s.substring(s.length - length) : s;
+const truncateOutput = (s, n = 5, delimiter = "\n") => {
+  const split = s.split(delimiter);
+  if (split.length <= n) {
+    return s;
+  }
+  const out = split
+    .slice(split.length - n - 1, split.length)
+    .join(delimiter)
+    .trim();
+  return `[...${MESSAGES.truncatedOutput}...]
+${out}`;
 };
 
 /**
@@ -77,35 +103,26 @@ const truncString = (s, length) => {
  * @param {object} full data for row object.
  * @return {string} of DOM representing cell.
  */
-function createState(full) {
+function createState({ state, percentComplete, jobError }) {
   const stateClasses = {
     COMPLETED: "progress-bar-success",
     ERROR: "progress-bar-danger"
   };
-
   let stateClass = "";
-  if (stateClasses[full.state] !== null) {
-    stateClass = stateClasses[full.state];
+  if (stateClasses[state] !== null) {
+    stateClass = stateClasses[state];
   }
-
   let errorStateClass = "";
-  let percent = full.percentComplete;
-  if (/^Error.*/.test(full.state)) {
+  let percent = percentComplete;
+  if (/^Error.*/.test(state)) {
     stateClass = stateClasses.ERROR;
     percent = 100;
     errorStateClass = "class='js-analysis-error'";
   }
-  let showingPopover = "";
-  if (typeof full.jobError !== "undefined" && full.jobError !== null) {
-    showingPopover = `<br>
-        <span class="js-showing-popover small text-muted">
-            ${SHOW_POPOVER}
-        </span>`;
-  }
   return `
 <div ${errorStateClass}>
-  ${full.state}
-  ${showingPopover}
+  ${state}
+  ${jobError !== null ? jobErrorIcon : ""}
   <div class='progress analysis__state'>
     <div class='progress-bar ${stateClass}' 
          role='progressbar' aria-valuenow='${percent}' 
@@ -118,10 +135,114 @@ function createState(full) {
 }
 
 /**
- * Get a handle on the table
- * @type {*|jQuery|HTMLElement}
+ * Check for clicking outside of a popover. Close any popovers if user clicks
+ * outside the popover. Remove this event handler from $("body")
+ * @param e
  */
-const $table = $("#analyses");
+const clickAwayPopoverHandler = e => {
+  const $target = $(e.target);
+  if (
+    $target.data("toggle") !== "popover" &&
+    $target.parents(".popover.in").length === 0
+  ) {
+    $("[data-original-title]").popover("hide");
+    // remove this event handler
+    unregisterPopoverClickAway();
+  }
+};
+
+/**
+ * Document body HTML element
+ * @type {jQuery|HTMLElement}
+ */
+const $body = $("body");
+/**
+ * Unregister `clickAwayPopoverHandler` event from `$body`
+ * @returns {*}
+ */
+const unregisterPopoverClickAway = () =>
+  $body.off("click", clickAwayPopoverHandler);
+
+/**
+ * Register `clickAwayPopoverHandler` event to `$body`
+ * @returns {*}
+ */
+const registerPopoverClickAway = () =>
+  $body.on("click", clickAwayPopoverHandler);
+
+/**
+ * Setup the BS3 Popover UI for showing job error info
+ * @param jobError Destructured job error object
+ * @param row DataTables row
+ */
+const setupJobErrorPopoverUI = ({ jobError }, row) => {
+  const $row = $(row);
+  const $errorTd = $row.find(".js-analysis-error");
+  if ($errorTd.length > 0 && jobError !== null) {
+    const $link = $row.find("a.btn-link");
+    let { standardError = "", standardOutput = "" } = jobError;
+    // only show the last N lines of stderr/stdout
+    standardError = truncateOutput(standardError);
+    standardOutput = truncateOutput(standardOutput);
+    // construct <a> link to analysis submission details page
+    const linkToSubmission = `<a href="${$link.attr(
+      "href"
+    )}">${$link.html()}</a>`;
+    // text and link for "Go to {analysis submission page} for more info"
+    const goto = MESSAGES.goToSubmission.replace("{0}", linkToSubmission);
+    // popover main content
+    const content = `
+<div>
+  <h5>${MESSAGES.standardError}</h5>
+  <pre>${escapeHtml(standardError)}</pre>
+  <h5>${MESSAGES.standardOutput}</h5>
+  <pre>${escapeHtml(standardOutput)}</pre>
+  <p>${goto}</p>
+</div>`;
+    const title = `
+<span>
+  Job Error - ${jobError.toolName} (v${jobError.toolVersion}) 
+</span>
+<i class="pull-right fa fa-fw fa-times text-danger js-close-popover" />
+`;
+    // set popover data attrs
+    $errorTd.data({ title: title, content: content });
+    $errorTd.css({ position: "relative", cursor: "pointer" });
+
+    // initialize popover for table cell with job error info
+    $errorTd
+      .popover(POPOVER_OPTIONS)
+      .on("shown.bs.popover", () => {
+        // adjust width of popover to X% of client window width
+        $(".popover").css(
+          "max-width",
+          Math.floor($(window).width() * 0.33) + "px"
+        );
+        // popover close button click event
+        $(".js-close-popover").on("click", () => {
+          $errorTd.popover("hide");
+          $errorTd.popover(POPOVER_OPTIONS);
+        });
+        // register event for when user clicks outside of popover to dismiss it
+        registerPopoverClickAway();
+      })
+      .on("hidden.bs.popover", e => {
+        // need following to ensure that user does not need to click twice
+        // to open popover (see https://stackoverflow.com/a/34320956)
+        $(e.target).data("bs.popover").inState.click = false;
+      });
+  }
+};
+
+/**
+ * Initialize tooltip for icon that says "Click to preview job error info"
+ * @param row DataTables row
+ */
+function initClickToShowJobErrorTooltip(row) {
+  $(row)
+    .find(".js-job-error-tooltip")
+    .tooltip();
+}
 
 const config = Object.assign(tableConfig, {
   ajax: window.PAGE.URLS.analyses,
@@ -204,56 +325,8 @@ const config = Object.assign(tableConfig, {
     ) {
       return;
     }
-    const joberror = data.jobError;
-    const $row = $(row);
-    const $errorTd = $row.find(".js-analysis-error");
-    if ($errorTd.length > 0) {
-      const $link = $row.find("a.btn-link");
-      const MAX_LENGTH = 400;
-      let stderr = joberror.standardError;
-      let stdout = joberror.standardOutput;
-
-      stderr = truncString(stderr, MAX_LENGTH);
-      stdout = truncString(stdout, MAX_LENGTH);
-      const content = `
-<div>
-  <h5>Standard Error</h5>
-  <pre>${escapeHtml(stderr)}</pre>
-  <h5>Standard Output</h5>
-  <pre>${escapeHtml(stdout)}</pre>
-  <p>Go to <a href="${$link.attr("href")}">${$link.html()}</a> for more info</p>
-</div>`;
-      const title = `
-<span>
-  Job Error - ${joberror.toolName} (v${joberror.toolVersion}) 
-</span>
-<i class="pull-right fa fa-fw fa-times text-danger js-close-popover" />
-`;
-      $errorTd.data("title", title);
-      $errorTd.data("content", content);
-      $errorTd.css("position", "relative");
-      $errorTd.css("cursor", "pointer");
-
-      $errorTd
-        .popover(POPOVER_OPTIONS)
-        .on("shown.bs.popover", () => {
-          $errorTd.find(".js-showing-popover").html(HIDE_POPOVER);
-          $(".popover").css(
-            "max-width",
-            Math.floor($(window).width() * 0.5) + "px"
-          );
-          $(".js-close-popover").on("click", () => {
-            $errorTd.popover("hide");
-            $errorTd.popover(POPOVER_OPTIONS);
-          });
-        })
-        .on("hidden.bs.popover", e => {
-          $errorTd.find(".js-showing-popover").html(SHOW_POPOVER);
-          // need following to ensure that user does not need to click twice
-          // to open popover (see https://stackoverflow.com/a/34320956)
-          $(e.target).data("bs.popover").inState.click = false;
-        });
-    }
+    setupJobErrorPopoverUI(data, row);
+    initClickToShowJobErrorTooltip(row);
   }
 });
 /*
