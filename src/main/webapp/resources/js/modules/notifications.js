6feefdb9b216a1d044f1f024cc0355e5bd407b9b
Josh Adam <josh.s.adam@gmail.com>
2018-01-26 14:57:58 -0600

    Moved to bottom right

--- src/main/webapp/resources/js/modules/notifications.js
+++ src/main/webapp/resources/js/modules/notifications.js
@@ -4,11 +4,8 @@ export function showNotification({ text, type = "success" }) {
   return noty({
     theme: "metroui",
     timeout: 3500, // [integer|boolean] delay for closing event in milliseconds. Set false for sticky notifications
+    layout: "bottomRight",
     progressBar: true,
-    animation: {
-      open: "animated bounceInRight",
-      close: "animated bounceOutRight"
-    },
     type,
     text
   });
