706065645cfd4cf273c4d454f783f6b5903a034f
Josh Adam <josh.s.adam@gmail.com>
2018-01-30 13:27:36 -0600

    Updated notifications to bottom right.

--- src/main/webapp/resources/js/modules/notifications.js
+++ src/main/webapp/resources/js/modules/notifications.js
@@ -1,14 +1,20 @@
-import "noty";
+import Noty from "noty";
+import "noty/src/noty.scss";
+import "noty/src/themes/sunset.scss";
 
-export function showNotification({ text, type = "success" }) {
-  return noty({
-    theme: "metroui",
+export function showNotification({ text, type = "alert" }) {
+  return new Noty({
+    theme: "sunset",
     timeout: 3500, // [integer|boolean] delay for closing event in milliseconds. Set false for sticky notifications
     layout: "bottomRight",
     progressBar: true,
     type,
-    text
-  });
+    text,
+    animation: {
+      open: "animated fadeInUp",
+      close: "animated fadeOutDown"
+    }
+  }).show();
 }
 
 // TODO: Remove this after all notification usages are through a webpack bundle.
