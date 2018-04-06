d3c279124ec3a91c57de4d0c1e5e00ad59135b7a
Josh Adam <josh.adam@canada.ca>
2018-04-06 08:01:13 -0500

    Added redux and basic pages

---
+++ src/main/webapp/resources/js/pages/projects/linelist/containers/MetadataTable/reducer.js
@@ -0,0 +1,3 @@
+const reducer = (state = {}, action) => {};
+
+export default reducer;
--- src/main/webapp/resources/js/pages/projects/linelist/index.js
+++ src/main/webapp/resources/js/pages/projects/linelist/index.js
@@ -1,8 +1,13 @@
 /**
  * Root file for the linelist page.
  */
+import { createStore } from "redux";
+import tableReducer from "./containers/MetadataTable/reducer";
+
 import MetadataEntryApi from "./api/metadataEntryApi";
 import MetadataFieldApi from "./api/metadataFieldApi";
+
+const store = createStore(tableReducer);
 
 /**
  * These are only here now to test the API.
