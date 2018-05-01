6650451507e6c19a1b38f1c8b3a9c594c238ee3c
Josh Adam <josh.adam@canada.ca>
2018-05-01 09:41:15 -0500

    Removed code that should be in the next merge request.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/projects/linelist/sagas/fields.js
+++ src/main/webapp/resources/js/pages/projects/linelist/sagas/fields.js
@@ -1,29 +1,7 @@
 import { call, put, take } from "redux-saga/effects";
 import { fetchMetadataFields } from "../../../../apis/metadata/field";
-import { types } from "../../../../reducers/app";
+import { types } from "../../../../redux/reducers/app";
 import { actions } from "../reducers/fields";
-
-/*
-Special handler for formatting the sample Name Column;
- */
-const sampleNameColumn = {
-  sort: "asc",
-  pinned: "left",
-  lockPosition: true,
-  cellRenderer: "SampleNameRenderer"
-};
-
-/**
- * Format the column definitions.
- * @param {array} cols
- * @returns {*}
- */
-const formatColumns = cols =>
-  cols.map((f, i) => ({
-    field: f.label,
-    headerName: f.label.toUpperCase(),
-    ...(i === 0 ? sampleNameColumn : {})
-  }));
 
 /**
  * Fetch all the metadata fields required to initialize the table.
@@ -35,8 +13,7 @@ export function* fieldsLoadingSaga() {
     const { id } = yield take(types.INIT_APP);
     // Let the page know that we are loading the fields
     yield put(actions.load());
-    const { data } = yield call(fetchMetadataFields, id);
-    const fields = formatColumns(data);
+    const { data: fields } = yield call(fetchMetadataFields, id);
     yield put(actions.success(fields));
   } catch (error) {
     yield put(actions.error(error));
