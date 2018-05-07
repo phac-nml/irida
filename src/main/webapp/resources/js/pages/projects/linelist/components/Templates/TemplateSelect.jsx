1a269ca36744ed465e275345598855071b4c9b64
Josh Adam <josh.adam@canada.ca>
2018-05-07 13:19:09 -0500

    Updated internationalization

--- src/main/webapp/resources/js/pages/projects/linelist/components/Templates/TemplateSelect.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Templates/TemplateSelect.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Select } from "antd";
 const { Option } = Select;
 
+const { i18n } = window.PAGE;
 export function TemplateSelect(props) {
   const templates = props.templates.toJS();
   if (templates.length) {
@@ -11,7 +12,7 @@ export function TemplateSelect(props) {
         style={{ width: 250 }}
         onSelect={id => props.fetchTemplate(id)}
       >
-        <Option value="-1">_None_</Option>
+        <Option value="-1">{i18n.linelist.Select.none}</Option>
         {templates.map(t => (
           <Option key={t.id} value={t.id}>
             {t.label}
