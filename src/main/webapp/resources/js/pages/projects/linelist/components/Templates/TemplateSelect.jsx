603843f37a8ad69baaf729b1b41f8b05a95a77fb
Josh Adam <josh.adam@canada.ca>
2018-05-08 13:41:37 -0500

    Disable template selection if none are available.

--- src/main/webapp/resources/js/pages/projects/linelist/components/Templates/TemplateSelect.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Templates/TemplateSelect.jsx
@@ -5,22 +5,19 @@ const { Option } = Select;
 const { i18n } = window.PAGE;
 export function TemplateSelect(props) {
   const templates = props.templates.toJS();
-  if (templates.length) {
-    return (
-      <Select
-        defaultValue={"" + props.current}
-        style={{ width: 250 }}
-        onSelect={id => props.fetchTemplate(id)}
-      >
-        <Option value="-1">{i18n.linelist.Select.none}</Option>
-        {templates.map(t => (
-          <Option key={t.id} value={t.id}>
-            {t.label}
-          </Option>
-        ))}
-      </Select>
-    );
-  } else {
-    return <span />;
-  }
+  return (
+    <Select
+      disabled={templates.length === 0}
+      defaultValue={"" + props.current}
+      style={{ width: 250 }}
+      onSelect={id => props.fetchTemplate(id)}
+    >
+      <Option value="-1">{i18n.linelist.Select.none}</Option>
+      {templates.map(t => (
+        <Option key={t.id} value={t.id}>
+          {t.label}
+        </Option>
+      ))}
+    </Select>
+  );
 }
