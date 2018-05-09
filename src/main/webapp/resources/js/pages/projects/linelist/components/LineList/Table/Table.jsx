8018c8a775a46481cede2219706d2bf5482fe7bf
Josh Adam <josh.adam@canada.ca>
2018-05-09 10:54:50 -0500

    Updated column rendering

--- src/main/webapp/resources/js/pages/projects/linelist/components/LineList/Table/Table.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/LineList/Table/Table.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { List } from "immutable";
 import ImmutablePropTypes from "react-immutable-proptypes";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid/dist/styles/ag-grid.css";
@@ -8,30 +9,6 @@ import { LoadingOverlay } from "./LoadingOverlay";
 import { SampleNameRenderer } from "./renderers/SampleNameRenderer";
 
 const { i18n } = window.PAGE;
-
-// Format the fields based on a template
-const applyTemplate = (template, fields) => {
-  if (template.length === 0) {
-    return fields.map(f => {
-      f.hide = false;
-      return f;
-    });
-  }
-
-  // Need to keep sample name first
-  const sampleName = fields.shift();
-
-  const final = [];
-  template.forEach(t => {
-    const index = fields.findIndex(f => t.label === f.field);
-    if (index > -1) {
-      const field = fields.splice(index, 1)[0];
-      field.hide = false;
-      final.push(field);
-    }
-  });
-  return [sampleName, ...final];
-};
 
 export class Table extends React.Component {
   containerStyle = {
@@ -45,12 +22,64 @@ export class Table extends React.Component {
     super(props);
   }
 
+  shouldComponentUpdate(nextProps) {
+    if (!nextProps.fields.equals(this.props.fields)) {
+      return true;
+    }
+    if (
+      List.isList(nextProps.entries) &&
+      !nextProps.entries.equals(this.props.entries)
+    ) {
+      return true;
+    }
+    if (!nextProps.template.equals(this.props.template)) {
+      // Update the table nothing else
+      this.applyTemplate(nextProps.template);
+    }
+    // If these have not been changed then don't update the entries;
+    return false;
+  }
+
+  applyTemplate = templateList => {
+    const template = templateList.toJS();
+    const columnState = this.columnApi.getColumnState();
+
+    // If there are no fields, then there is no template :)
+    // Therefore just show all the fields.
+    if (template.length === 0) {
+      const state = columnState.map(c => {
+        c.hide = false;
+        return c;
+      });
+      this.columnApi.setColumnState(state);
+      return;
+    }
+
+    // Need to keep sample name first
+    let final = [columnState.shift()];
+    template.forEach(t => {
+      const index = columnState.findIndex(f => t.label === f.colId);
+      if (index > -1) {
+        const field = columnState.splice(index, 1)[0];
+        field.hide = false;
+        final.push(field);
+      }
+    });
+    const remainder = columnState.map(c => {
+      c.hide = true;
+      return c;
+    });
+    this.columnApi.setColumnState([...final, ...remainder]);
+  };
+
   /*
   Allow access to the grids API
    */
   onGridReady = params => {
     this.api = params.api;
     this.columnApi = params.columnApi;
+
+    this.api.sortChanged;
   };
 
   render() {
@@ -60,10 +89,7 @@ export class Table extends React.Component {
           enableSorting={true}
           enableColResize={true}
           localeText={i18n.linelist.agGrid}
-          columnDefs={applyTemplate(
-            this.props.template.toJS(),
-            this.props.fields.toJS()
-          )}
+          columnDefs={this.props.fields.toJS()}
           rowData={
             this.props.entries === null
               ? this.props.entries
