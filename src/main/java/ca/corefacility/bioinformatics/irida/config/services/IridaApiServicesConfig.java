a073a57b0024f5d458c6a5ed6b7813437fd79e5b
pkruczkiewicz <peter.kruczkiewicz@canada.ca>
2018-05-24 12:55:04 -0500

    Sort workflow messages.properties paths by descending order Hopefully the latest messages files will be preferentially used for populating i18n messages

--- src/main/java/ca/corefacility/bioinformatics/irida/config/services/IridaApiServicesConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/services/IridaApiServicesConfig.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -175,7 +172,7 @@ public class IridaApiServicesConfig {
 		return Arrays.stream(resources)
 				.map(x -> getClasspathResourceBasename(pattern, x))
 				.filter(Objects::nonNull)
-				.sorted()
+				.sorted(Comparator.reverseOrder())
 				.collect(Collectors.toList());
 	}
 
