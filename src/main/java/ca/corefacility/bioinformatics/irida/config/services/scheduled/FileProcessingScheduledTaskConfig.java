b121949b17255b5f6fc1d0239018037da5c41055
tom <thomas.matthews@phac-aspc.gc.ca>
2018-06-01 11:06:41 -0500

    added a config option to disable file processing on a machine

--- src/main/java/ca/corefacility/bioinformatics/irida/config/services/scheduled/FileProcessingScheduledTaskConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/services/scheduled/FileProcessingScheduledTaskConfig.java
@@ -2,6 +2,7 @@ package ca.corefacility.bioinformatics.irida.config.services.scheduled;
 
 import ca.corefacility.bioinformatics.irida.service.SequencingObjectProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -14,11 +15,16 @@ public class FileProcessingScheduledTaskConfig {
 	@Autowired
 	private SequencingObjectProcessingService fileProcessingService;
 
+	@Value("${file.processing.process}")
+	private boolean processFiles;
+
 	/**
 	 * Check for newly uploaded files to process
 	 */
 	@Scheduled(fixedDelay = 5000)
 	public void processFiles() {
-		fileProcessingService.runProcessingJob();
+		if (processFiles) {
+			fileProcessingService.runProcessingJob();
+		}
 	}
 }
