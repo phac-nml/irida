b7c604868937835cec970cd1f59255b5b913ad8a
Josh Adam <josh.adam@canada.ca>
2018-07-23 15:27:31 -0500

    Simplified component

--- src/main/webapp/resources/js/pages/projects/linelist/components/Table/renderers/SampleNameRenderer.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Table/renderers/SampleNameRenderer.jsx
@@ -3,23 +3,16 @@ import PropTypes from "prop-types";
 
 const { BASE_URL } = window.TL;
 
-export class SampleNameRenderer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.href = `${BASE_URL}samples/${Number(props.data.sampleId)}`;
-    this.name = props.value;
-  }
-  render() {
+export function SampleNameRenderer (props) {
     return (
       <a
         target="_blank"
         className="t-sample-name"
-        href={this.href}
+        href={`${BASE_URL}samples/${Number(props.data.sampleId)}`}
       >
-        {this.name}
+        {props.value}
       </a>
     );
-  }
 }
 
 SampleNameRenderer.propTypes = {
