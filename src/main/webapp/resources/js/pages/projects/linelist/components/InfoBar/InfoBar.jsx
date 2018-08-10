5c320e6b66caaf05425dab43d3a857f2f58d0f9f
Josh Adam <josh.adam@canada.ca>
2018-08-10 14:20:10 -0500

    Display the number of samples filtered on the bottom of the table

--- src/main/webapp/resources/js/pages/projects/linelist/components/InfoBar/InfoBar.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/InfoBar/InfoBar.jsx
@@ -5,14 +5,21 @@ const { i18n } = window.PAGE;
 export function InfoBar(props) {
   return (
     <div className="ag-grid-info-panel">
-      {props.selectedCount === 0
-        ? i18n.linelist.selected.none
-        : props.selectedCount === 1
-          ? i18n.linelist.selected.one
-          : i18n.linelist.selected.multiple.replace(
-              "_COUNT_",
-              props.selectedCount
-            )}
+      <span>
+        {props.selectedCount === 0
+          ? i18n.linelist.selected.none
+          : props.selectedCount === 1
+            ? i18n.linelist.selected.one
+            : i18n.linelist.selected.multiple.replace(
+                "_COUNT_",
+                props.selectedCount
+              )}
+      </span>
+      <span>
+        {i18n.linelist.infobar.filterCounts
+          .replace("[FILTER_COUNT]", props.filterCount)
+          .replace("[TOTAL]", props.totalSamples)}
+      </span>
     </div>
   );
 }
--- src/main/webapp/resources/js/pages/projects/linelist/components/Table/Table.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Table/Table.jsx
@@ -19,7 +19,8 @@ const { i18n } = window.PAGE;
  */
 export class Table extends React.Component {
   state = {
-    entries: null
+    entries: null,
+    filterCount: 0
   };
 
   /*
@@ -404,7 +405,14 @@ export class Table extends React.Component {
    * Search the entire table for a value.
    * @param {string} value
    */
-  quickSearch = value => this.api.setQuickFilter(value);
+  quickSearch = value => {
+    this.api.setQuickFilter(value);
+  };
+
+  setFilterCount = () =>
+    this.props.onFilter(
+      this.api.getModel().rootNode.childrenAfterFilter.length
+    );
 
   render() {
     return (
@@ -416,6 +424,7 @@ export class Table extends React.Component {
           id="linelist-grid"
           rowSelection="multiple"
           enableFilter={true}
+          onFilterChanged={this.setFilterCount}
           enableSorting={true}
           enableColResize={true}
           localeText={i18n.linelist.agGrid}
@@ -447,5 +456,6 @@ Table.propTypes = {
   fields: ImmutablePropTypes.list.isRequired,
   entries: ImmutablePropTypes.list,
   templates: ImmutablePropTypes.list,
-  current: PropTypes.number.isRequired
+  current: PropTypes.number.isRequired,
+  onFilter: PropTypes.func.isRequired
 };
