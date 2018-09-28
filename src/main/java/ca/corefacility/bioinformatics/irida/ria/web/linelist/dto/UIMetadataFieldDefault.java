fc02cf8a80f6365b14436549cd2e3ad0351c6530
Josh Adam <josh.adam@canada.ca>
2018-09-28 10:59:40 -0500

    AgGridColumn now handles this properly.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/linelist/dto/UIMetadataFieldDefault.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/linelist/dto/UIMetadataFieldDefault.java
@@ -7,17 +7,7 @@ import ca.corefacility.bioinformatics.irida.ria.web.components.agGrid.AgGridColu
  * on the line list page.
  */
 public class UIMetadataFieldDefault extends AgGridColumn {
-	private String field;
-
-	public UIMetadataFieldDefault(String field, String headerName, String type) {
+	public UIMetadataFieldDefault(String headerName, String type) {
 		super(headerName, type, false, false);
-		this.setLockPinned(true);
-		this.setLockPosition(true);
-		this.field = field;
-	}
-
-	@Override
-	public String getField() {
-		return field;
 	}
 }
