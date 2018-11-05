e5666b58e922487b542df123233e51df165b643a
Josh Adam <josh.adam@canada.ca>
2018-11-05 08:25:10 -0600

    Removed unused code
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/apis/cart/cart.js
+++ src/main/webapp/resources/js/apis/cart/cart.js
@@ -5,17 +5,9 @@ const url = `${window.TL.BASE_URL}cart`;
 /**
  * Add samples for a project to the cart.
  * @param {number} projectId Identifier for the project the samples are from.
- * @param {array} sampleIds array of sample ids to add to cart.
+ * @param {array} samples array of sample {ids } to add to cart.
  * @returns {AxiosPromise<any>}
  */
-// export function putSampleInCart(projectId, sampleIds) {
-//   const params = new URLSearchParams();
-//   params.append("projectId", projectId);
-//   params.append("sampleIds[]", sampleIds);
-//
-//   return axios.post(`${window.TL.BASE_URL}cart/add/samples`, params);
-// }
-
 export const putSampleInCart = async (projectId, samples) =>
   axios.put(url, {
     projectId,
