40296fe83b98519638204e414eae15b434e0f534
Josh Adam <josh.adam@canada.ca>
2018-11-08 10:05:45 -0600

    Made the modal a warning with primary button

--- src/main/webapp/resources/js/components/session/SessionModal.jsx
+++ src/main/webapp/resources/js/components/session/SessionModal.jsx
@@ -62,8 +62,7 @@ export default class SessionModal extends React.Component {
    * Display the modal to the user.
    */
   showModal = () => {
-    Modal.confirm({
-      okType: "warning",
+    Modal.warning({
       title: i18n.title.replace("[TIME]", this.props.time / 60000), // Convert time to minutes.
       onOk: this.keepSession,
       onCancel: this.logout,
