5a43874af174838a2d3fc3094b2f5e9f7e55e2d5
Josh Adam <josh.adam@canada.ca>
2018-11-22 14:39:59 -0600

    Cart empty state

--- src/main/webapp/resources/js/pages/cart/index.js
+++ src/main/webapp/resources/js/pages/cart/index.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { Row, Col, Icon } from "antd";
 import { getProjectsInCart } from "../../apis/cart/cart";
 
+const { i18n } = window.PAGE;
+const { BASE_URL } = window.TL;
+
 function EmptyCartState() {
-  return <h1>EMPTY</h1>;
+  return (
+    <Row
+      type="flex"
+      justify="center"
+      align="middle"
+      style={{ height: "100%", flexDirection: "column" }}
+    >
+      <img
+        height="300px"
+        src={`${BASE_URL}resources/img/empty-cart.svg`}
+        alt="The cart is empty"
+      />
+      <p
+        style={{
+          fontSize: "1.4em",
+          fontWeight: 600,
+          color: "#1890ff",
+          marginBottom: ".5em"
+        }}
+      >
+        {i18n.cart.empty}
+      </p>
+      <p style={{ color: "#222222" }}>{i18n.cart.emptySub}</p>
+    </Row>
+  );
 }
 
 class Cart extends Component {
