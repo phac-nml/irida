9a47f78331723ca3cd08f928617a6c1c346e6b9b
Josh Adam <josh.adam@canada.ca>
2018-11-22 08:46:46 -0600

    Working on empty state.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/cart/index.js
+++ src/main/webapp/resources/js/pages/cart/index.js
@@ -1,39 +1,14 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { getCart, getProjectsInCart } from "../../apis/cart/cart";
-import { List, Skeleton } from "antd";
-
-// class CartProject extends React.Component {
-//   state = { loading: true };
-//
-//   componentDidMount() {
-//     const { id } = this.props;
-//     console.log(id);
-//   }
-//
-//   render() {
-//     const { loading } = this.state;
-//     const { id } = this.props;
-//
-//     return (
-//       <List.Item key={id} actions={[<a>Remove</a>]}>
-//         <Skeleton loading={loading} active>
-//           <List.Item.Meta
-//             title={this.state.title}
-//             description={<Skeleton active={true} title={false} />}
-//           />
-//         </Skeleton>
-//       </List.Item>
-//     );
-//   }
-// }
+import { getProjectsInCart } from "../../apis/cart/cart";
 
 function EmptyCartState() {
-  return;
+  return <h1>EMPTY</h1>;
 }
 
 class Cart extends Component {
-  state = { projects: [] };
+  state = {};
+
   componentDidMount() {
     getProjectsInCart().then(data => {
       this.setState({ projects: data });
@@ -42,13 +17,12 @@ class Cart extends Component {
 
   render() {
     const { projects } = this.state;
-    return (
-      <List>
-        {projects.map(id => (
-          <CartProject key={id} id={id} />
-        ))}
-      </List>
-    );
+    if (typeof projects === "undefined") {
+      return <div>Loading...</div>;
+    } else if (projects.length === 0) {
+      return <EmptyCartState />;
+    }
+    return null;
   }
 }
 
