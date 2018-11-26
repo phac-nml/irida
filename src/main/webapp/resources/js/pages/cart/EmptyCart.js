400834e731a6898a230ef0dcbe08f9e5a5cd40d9
Josh Adam <josh.adam@canada.ca>
2018-11-26 10:05:30 -0600

    Made compoent from empty cart state.

---
+++ src/main/webapp/resources/js/pages/cart/EmptyCart.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Row } from "antd";
+
+const { i18n } = window.PAGE;
+const { BASE_URL } = window.TL;
+
+export default function EmptyCart() {
+  return (
+    <Row
+      type="flex"
+      justify="center"
+      align="middle"
+      style={{ height: "100%", flexDirection: "column" }}
+    >
+      <img
+        height="300px"
+        src={`${BASE_URL}resources/img/empty-cart.svg`}
+        alt={i18n.cart.imageAlt}
+      />
+      <p
+        style={{
+          fontSize: "1.4em",
+          fontWeight: 600,
+          color: "#1890ff",
+          marginBottom: ".5em"
+        }}
+      >
+        {i18n.cart.empty}
+      </p>
+      <a
+        href={`${BASE_URL}projects`}
+        style={{ borderBottom: "2px solid #1890ff" }}
+      >
+        {i18n.cart.emptySub}
+      </a>
+    </Row>
+  );
+}
--- src/main/webapp/resources/js/pages/cart/index.js
+++ src/main/webapp/resources/js/pages/cart/index.js
@@ -1,43 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { Row } from "antd";
+import EmptyCart from "./EmptyCart";
 import { getProjectsInCart } from "../../apis/cart/cart";
-
-const { i18n } = window.PAGE;
-const { BASE_URL } = window.TL;
-
-function EmptyCartState() {
-  return (
-    <Row
-      type="flex"
-      justify="center"
-      align="middle"
-      style={{ height: "100%", flexDirection: "column" }}
-    >
-      <img
-        height="300px"
-        src={`${BASE_URL}resources/img/empty-cart.svg`}
-        alt={i18n.cart.imageAlt}
-      />
-      <p
-        style={{
-          fontSize: "1.4em",
-          fontWeight: 600,
-          color: "#1890ff",
-          marginBottom: ".5em"
-        }}
-      >
-        {i18n.cart.empty}
-      </p>
-      <a
-        href={`${BASE_URL}projects`}
-        style={{ borderBottom: "2px solid #1890ff" }}
-      >
-        {i18n.cart.emptySub}
-      </a>
-    </Row>
-  );
-}
 
 class Cart extends Component {
   state = {};
@@ -53,7 +17,7 @@ class Cart extends Component {
     if (typeof projects === "undefined") {
       return <div>Loading...</div>;
     } else if (projects.length === 0) {
-      return <EmptyCartState />;
+      return <EmptyCart />;
     }
     return null;
   }
