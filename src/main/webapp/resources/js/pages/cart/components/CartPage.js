0a31e272ef594d9b6251cc204252329d7900702b
Josh Adam <josh.adam@canada.ca>
2019-02-05 13:56:26 -0600

    Added prop types
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/cart/components/CartPage.js
+++ src/main/webapp/resources/js/pages/cart/components/CartPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import CartEmpty from "./CartEmpty";
 import Cart from "./Cart";
@@ -11,6 +12,11 @@ function CartPageComponent({ initialized = true, count = 0 }) {
   }
   return <Cart count={count} />;
 }
+
+CartPageComponent.propTypes = {
+  initialized: PropTypes.bool.isRequired,
+  count: PropTypes.number
+};
 
 const mapStateToProps = state => ({
   count: state.cart.count,
