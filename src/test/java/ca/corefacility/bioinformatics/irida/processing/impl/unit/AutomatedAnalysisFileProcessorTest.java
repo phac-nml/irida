8a1f87593b61e47fec6f117f750b2cfac346fb70
Tom Matthews <thomas.matthews@canada.ca>
2019-03-11 13:36:12 -0500

    updated assembly test to autoanalysistest.  it fails right now but it's renamed

--- src/test/java/ca/corefacility/bioinformatics/irida/processing/impl/unit/AutomatedAnalysisFileProcessorTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/processing/impl/unit/AutomatedAnalysisFileProcessorTest.java
@@ -1,22 +1,5 @@
 package ca.corefacility.bioinformatics.irida.processing.impl.unit;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
-
-import java.nio.file.Paths;
-import java.util.UUID;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import com.google.common.collect.ImmutableList;
-
 import ca.corefacility.bioinformatics.irida.exceptions.IridaWorkflowNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
@@ -29,15 +12,29 @@ import ca.corefacility.bioinformatics.irida.model.workflow.IridaWorkflow;
 import ca.corefacility.bioinformatics.irida.model.workflow.analysis.type.BuiltInAnalysisTypes;
 import ca.corefacility.bioinformatics.irida.model.workflow.description.IridaWorkflowDescription;
 import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmission;
-import ca.corefacility.bioinformatics.irida.processing.impl.AssemblyFileProcessor;
+import ca.corefacility.bioinformatics.irida.processing.impl.AutomatedAnalysisFileProcessor;
 import ca.corefacility.bioinformatics.irida.repositories.analysis.submission.AnalysisSubmissionRepository;
+import ca.corefacility.bioinformatics.irida.repositories.analysis.submission.AnalysisSubmissionTemplateRepository;
 import ca.corefacility.bioinformatics.irida.repositories.joins.project.ProjectSampleJoinRepository;
 import ca.corefacility.bioinformatics.irida.repositories.joins.sample.SampleSequencingObjectJoinRepository;
 import ca.corefacility.bioinformatics.irida.repositories.sequencefile.SequencingObjectRepository;
 import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
 import ca.corefacility.bioinformatics.irida.service.workflow.IridaWorkflowsService;
+import com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-public class AssemblyFileProcessorTest {
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class AutomatedAnalysisFileProcessorTest {
 	@Mock
 	private SequencingObjectRepository objectRepository;
 	@Mock
@@ -50,15 +47,17 @@ public class AssemblyFileProcessorTest {
 	private UserRepository userRepository;
 	@Mock
 	private IridaWorkflowsService workflowsService;
+	@Mock
+	private AnalysisSubmissionTemplateRepository templateRepository;
 
-	private AssemblyFileProcessor processor;
+	private AutomatedAnalysisFileProcessor processor;
 
 	@Before
 	public void setUp() throws IridaWorkflowNotFoundException {
 		MockitoAnnotations.initMocks(this);
 
-		processor = new AssemblyFileProcessor(objectRepository, submissionRepository, workflowsService, userRepository,
-				ssoRepository, psjRepository);
+		processor = new AutomatedAnalysisFileProcessor(ssoRepository, psjRepository, submissionRepository,
+				templateRepository, workflowsService, objectRepository);
 
 		UUID workflowUUID = UUID.randomUUID();
 		IridaWorkflowDescription workflowDescription = new IridaWorkflowDescription(workflowUUID, null, null, null,
@@ -121,8 +120,8 @@ public class AssemblyFileProcessorTest {
 
 		when(ssoRepository.getSampleForSequencingObject(pair)).thenReturn(new SampleSequencingObjectJoin(sample, pair));
 		when(psjRepository.getProjectForSample(sample)).thenReturn(
-				ImmutableList
-						.of(new ProjectSampleJoin(disabledProject, sample, true), new ProjectSampleJoin(project, sample, true)));
+				ImmutableList.of(new ProjectSampleJoin(disabledProject, sample, true),
+						new ProjectSampleJoin(project, sample, true)));
 
 		processor.process(pair);
 
