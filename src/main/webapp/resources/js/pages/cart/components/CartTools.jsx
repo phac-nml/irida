030b7d5441ec3777ab3ab6f4d69471c0a8c96874
Josh Adam <josh.adam@canada.ca>
2019-03-14 14:49:03 -0500

    Update the cart page is galaxy removed from session.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/cart/components/CartTools.jsx
+++ src/main/webapp/resources/js/pages/cart/components/CartTools.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Location, Router } from "@reach/router";
+import React, { Component } from "react";
+import { Location, navigate, Router } from "@reach/router";
 import { Row } from "antd";
 import styled from "styled-components";
 import { CartToolsMenu } from "./CartToolsMenu";
@@ -31,53 +31,99 @@ const ToolsInner = styled.div`
 /**
  * Wrapper component for functionality available in the cart.
  */
-export function CartTools() {
-  const fromGalaxy = typeof window.GALAXY !== "undefined";
+export class CartTools extends Component {
+  constructor(props) {
+    super(props);
 
-  /*
-   * Update here to add new tab items to the page.
-   */
-  const paths = [
-    {
-      key: "/cart/pipelines",
-      link: "cart/pipelines",
-      text: getI18N("CartTools.menu.pipelines"),
-      component: (
-        <Pipelines
-          key="/cart/pipelines"
-          path="cart/pipelines"
-          default={!fromGalaxy}
-        />
-      )
-    }
-  ];
-
-  if (fromGalaxy) {
-    paths.unshift({
-      key: "/cart/galaxy",
-      link: "cart/galaxy",
-      text: getI18N("CartTools.menu.galaxy"),
-      component: <GalaxyExport key="cart/galaxy" path="cart/galaxy" default />
-    });
+    const fromGalaxy = typeof window.GALAXY !== "undefined";
+    this.state = {
+      fromGalaxy,
+      paths: [
+        fromGalaxy
+          ? {
+              key: "/cart/galaxy",
+              link: "cart/galaxy",
+              text: getI18N("CartTools.menu.galaxy"),
+              component: (
+                <GalaxyExport key="cart/galaxy" path="cart/galaxy" default />
+              )
+            }
+          : null,
+        {
+          key: "/cart/pipelines",
+          link: "cart/pipelines",
+          text: getI18N("CartTools.menu.pipelines"),
+          component: (
+            <Pipelines
+              key="/cart/pipelines"
+              path="cart/pipelines"
+              default={!fromGalaxy}
+            />
+          )
+        }
+      ].filter(Boolean)
+    };
   }
 
-  return (
-    <ToolsWrapper>
-      <Location>
-        {({ location }) => (
-          <>
-            <CartToolsMenu
-              pathname={
-                location.pathname
-              }
-              paths={paths}
-            />
-            <ToolsInner>
-              <Router>{paths.map(path => path.component)}</Router>
-            </ToolsInner>
-          </>
-        )}
-      </Location>
-    </ToolsWrapper>
-  );
+  componentDidMount() {
+    if (this.state.fromGalaxy) {
+      /*
+      If this is within a galaxy session, the user has the opportunity to remove the session
+      from IRIDA.  When this happens this listener will ensure that the galaxy tab is removed
+      from the UI, and the user is redirected to the pipelines page.
+       */
+      this.galaxySesssionListener = document.body.addEventListener(
+        "galaxy:removal",
+        this.removeGalaxy
+      );
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.fromGalaxy) {
+      /*
+      Remove the galaxy listener to prevent leakage.
+       */
+      this.removeGalaxyListener();
+    }
+  }
+
+  removeGalaxy = () => {
+    // Remove the galaxy tab and redirect to the pipelines page.
+    if (this.state.fromGalaxy) {
+      this.setState(
+        prevState => ({
+          fromGalaxy: false,
+          paths: prevState.paths.splice(1)
+        }),
+        () => {
+          navigate("/cart/pipelines");
+          this.removeGalaxyListener();
+        }
+      );
+    }
+  };
+
+  removeGalaxyListener = () =>
+    document.body.removeEventListener("galaxy:removal", this.removeGalaxy);
+
+  render() {
+    return (
+      <ToolsWrapper>
+        <Location>
+          {({ location }) => (
+            <>
+              <CartToolsMenu
+                pathname={location.pathname}
+                paths={this.state.paths}
+              />
+              <ToolsInner>
+                <Router>{this.state.paths.map(path => path.component)}</Router>
+              </ToolsInner>
+            </>
+          )}
+        </Location>
+      </ToolsWrapper>
+    );
+  }
 }
