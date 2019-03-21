85e82818ac2d2c81d09edcf178d99b586d438b05
Deep Sidhu <sukhdeep.sidhu@phac-aspc.gc.ca>
2019-03-21 10:49:10 -0500

    Added controller and UI tests

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/GroupsControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/GroupsControllerTest.java
@@ -1,18 +1,21 @@
 package ca.corefacility.bioinformatics.irida.ria.unit.web;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 import org.springframework.context.MessageSource;
 
 import ca.corefacility.bioinformatics.irida.model.user.User;
-import ca.corefacility.bioinformatics.irida.model.user.group.UserGroup;
 import ca.corefacility.bioinformatics.irida.ria.web.GroupsController;
 import ca.corefacility.bioinformatics.irida.service.user.UserGroupService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
@@ -37,23 +40,38 @@ public class GroupsControllerTest {
 	}
 
 	@Test
-	public void testAddMemberToGroup() {
-		String term = "TEST";
-		String term2 = "Test";
-		String term3 = "USeR";
-		String term4 = "User";
-
+	public void testGetUsersNotInGroup() {
+		String searchTerm1 = "TEST";
+		String searchTerm2 = "Test";
+		String searchTerm3 = "USeR";
+		String searchTerm4 = "User";
 		Long groupId = 1L;
 		Long userId = 2L;
 		Long anotherUserId = 3L;
-		UserGroup userGroup = new UserGroup("new group");
-		userGroup.setId(groupId);
+
 		List<User> users = Lists.newArrayList(new User(userId, "test01", null, null, "Test", "User", null),
 				new User(anotherUserId, "newUser", null, null, "test02", "user", null));
 
-		when(controller.getUsersNotInGroup(groupId, term)).thenReturn(users);
-		when(controller.getUsersNotInGroup(groupId, term2)).thenReturn(users);
-		when(controller.getUsersNotInGroup(groupId, term3)).thenReturn(users);
-		when(controller.getUsersNotInGroup(groupId, term4)).thenReturn(users);
+		when(controller.getUsersNotInGroup(groupId, searchTerm1)).thenReturn(users);
+		when(controller.getUsersNotInGroup(groupId, searchTerm2)).thenReturn(users);
+		when(controller.getUsersNotInGroup(groupId, searchTerm3)).thenReturn(users);
+		when(controller.getUsersNotInGroup(groupId, searchTerm4)).thenReturn(users);
+		verify(userGroupService, times(4)).read(groupId);
+
+		Collection<User> usersNotInGroup = controller.getUsersNotInGroup(groupId, searchTerm1);
+		assertFalse(usersNotInGroup.isEmpty());
+		assertTrue(usersNotInGroup.size() == users.size());
+
+		usersNotInGroup = controller.getUsersNotInGroup(groupId, searchTerm2);
+		assertFalse(usersNotInGroup.isEmpty());
+		assertTrue(usersNotInGroup.size() == users.size());
+
+		usersNotInGroup = controller.getUsersNotInGroup(groupId, searchTerm3);
+		assertFalse(usersNotInGroup.isEmpty());
+		assertTrue(usersNotInGroup.size() == users.size());
+
+		usersNotInGroup = controller.getUsersNotInGroup(groupId, searchTerm4);
+		assertFalse(usersNotInGroup.isEmpty());
+		assertTrue(usersNotInGroup.size() == users.size());
 	}
 }
