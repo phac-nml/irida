fe7c26fa3db176f0045d669be8327e1dbbee5b2e
Tom Matthews <thomas.matthews@canada.ca>
2019-03-25 15:52:15 -0500

    added javadoc for delete confirm

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSettingsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSettingsController.java
@@ -1,28 +1,6 @@
 package ca.corefacility.bioinformatics.irida.ria.web.projects;
 
-import java.security.Principal;
-import java.util.*;
-import java.util.stream.Collectors;
-
 import ca.corefacility.bioinformatics.irida.exceptions.IridaWorkflowNotFoundException;
-import ca.corefacility.bioinformatics.irida.model.workflow.IridaWorkflow;
-import ca.corefacility.bioinformatics.irida.model.workflow.analysis.type.AnalysisType;
-import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmissionTemplate;
-import ca.corefacility.bioinformatics.irida.service.AnalysisSubmissionService;
-import ca.corefacility.bioinformatics.irida.service.workflow.IridaWorkflowsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.google.common.collect.ImmutableMap;
-
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectMetadataTemplateJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.project.ProjectSyncFrequency;
@@ -30,10 +8,26 @@ import ca.corefacility.bioinformatics.irida.model.remote.RemoteStatus;
 import ca.corefacility.bioinformatics.irida.model.remote.RemoteStatus.SyncStatus;
 import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplate;
 import ca.corefacility.bioinformatics.irida.model.user.User;
+import ca.corefacility.bioinformatics.irida.model.workflow.IridaWorkflow;
+import ca.corefacility.bioinformatics.irida.model.workflow.analysis.type.AnalysisType;
+import ca.corefacility.bioinformatics.irida.model.workflow.submission.AnalysisSubmissionTemplate;
+import ca.corefacility.bioinformatics.irida.service.AnalysisSubmissionService;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.remote.ProjectRemoteService;
 import ca.corefacility.bioinformatics.irida.service.sample.MetadataTemplateService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
+import ca.corefacility.bioinformatics.irida.service.workflow.IridaWorkflowsService;
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Handles basic settings pages for a project
@@ -168,6 +162,13 @@ public class ProjectSettingsController {
 		return "projects/templates/remove-analysis-template-modal";
 	}
 
+	/**
+	 * Delete the given {@link AnalysisSubmissionTemplate} from the given {@link Project}
+	 *
+	 * @param templateId The {@link AnalysisSubmissionTemplate} id
+	 * @param projectId  the {@link Project} id
+	 * @return Redirect to the project settings page after completion
+	 */
 	@RequestMapping(path = "/template/remove", method = RequestMethod.POST)
 	public String removeTemplateConfirm(final @RequestParam Long templateId, final @PathVariable Long projectId) {
 		Project project = projectService.read(projectId);
