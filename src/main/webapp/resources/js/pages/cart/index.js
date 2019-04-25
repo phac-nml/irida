1556436808882e14cc42f1ae7805b1c97d3b5c10
Josh Adam <josh.adam@canada.ca>
2019-04-25 13:25:36 -0500

    Fixed typo
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/cart/index.js
+++ src/main/webapp/resources/js/pages/cart/index.js
@@ -17,7 +17,7 @@ import { Cart } from "./components/Cart";
 
 /*
 WEBPACK PUBLIC PATH:
-Webpack does not now what the servlet context path is.  To fix this, webpack exposed
+Webpack does not know what the servlet context path is.  To fix this, webpack exposed
 the variable `__webpack_public_path__`
 See: https://webpack.js.org/guides/public-path/#on-the-fly
  */
