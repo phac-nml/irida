7dc8b9ff2b6fb7c4acdbecf38bf83a8bde52d0d5
Josh Adam <josh.adam@canada.ca>
2019-05-21 12:58:18 -0500

    Removed unused Typography dependency.

--- src/main/webapp/resources/js/pages/projects/linelist/App/App.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/App/App.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { getStore } from "../../../../redux/getStore";
 import { actions } from "../../../../redux/reducers/app";
 import * as reducers from "../reducers";
 import * as sagas from "../sagas";
 import LineList from "../components/LineList/LineListContainer";
+import { IntlProvider } from "react-intl";
 
 const store = getStore(reducers, sagas);
 
-export const App = () => (
-  <Provider store={store}>
-    <LineList />
-  </Provider>
-);
+export function App() {
+  const [translations, setTranslations] = useState();
+
+  useEffect(() => {
+    console.log("Fetching translations");
+    setTranslations({ foobar: "BAZ" });
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <IntlProvider locale="en" messages={translations}>
+        <LineList />
+      </IntlProvider>
+    </Provider>
+  );
+}
 
 const CURRENT_PROJECT_ID = window.project.id;
 store.dispatch(actions.initialize({ id: CURRENT_PROJECT_ID }));
