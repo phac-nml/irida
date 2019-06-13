fd6de6707ab46e4f05dffbf7980975954b34d19c
Tom Matthews <thomas.matthews@canada.ca>
2019-06-13 15:48:38 -0500

    updated apache jena to much newer version to try to clean up a logging redundancy

--- src/main/java/ca/corefacility/bioinformatics/irida/service/impl/InMemoryTaxonomyService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/service/impl/InMemoryTaxonomyService.java
@@ -1,39 +1,25 @@
 package ca.corefacility.bioinformatics.irida.service.impl;
 
-import java.nio.file.Path;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.apache.jena.atlas.lib.StrUtils;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.lucene.queryparser.classic.QueryParser;
-
 import ca.corefacility.bioinformatics.irida.service.TaxonomyService;
 import ca.corefacility.bioinformatics.irida.util.TreeNode;
-
 import com.google.common.base.Strings;
-import com.hp.hpl.jena.query.Dataset;
-import com.hp.hpl.jena.query.DatasetFactory;
-import com.hp.hpl.jena.query.ParameterizedSparqlString;
-import com.hp.hpl.jena.query.Query;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.vocabulary.RDFS;
+import org.apache.jena.atlas.lib.StrUtils;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.lucene.queryparser.classic.QueryParser;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.Map.Entry;
+
 
 /**
  * A {@link TaxonomyService} leveraging Apache Jena's in memory storage service
  * and Apache Lucene's text searching abilities.
- * 
- *
  */
 public class InMemoryTaxonomyService implements TaxonomyService {
 	private Model model;
@@ -48,7 +34,7 @@ public class InMemoryTaxonomyService implements TaxonomyService {
 	private final String ROOT_IRI = "http://purl.obolibrary.org/obo/NCBITaxon_2";
 
 	public InMemoryTaxonomyService(Path taxonomyFileLocation) {
-		dataset = DatasetFactory.createMem();
+		dataset = DatasetFactory.create();
 
 		model = dataset.getDefaultModel();
 		RDFDataMgr.read(model, taxonomyFileLocation.toString());
@@ -98,7 +84,7 @@ public class InMemoryTaxonomyService implements TaxonomyService {
 	/**
 	 * Build a result tree from a searched resource. This search will look
 	 * upwards in the tree until there are no more parent nodes.
-	 * 
+	 *
 	 * @param resource
 	 *            The resource to start from
 	 * @param searchTerm
@@ -132,7 +118,7 @@ public class InMemoryTaxonomyService implements TaxonomyService {
 
 	/**
 	 * Get a parent node with the matching search term
-	 * 
+	 *
 	 * @param resource
 	 *            The resource to start walking up from
 	 * @param searchTerm
