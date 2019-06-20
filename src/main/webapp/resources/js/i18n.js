952b737ee894076fb0b9420b671168aa6038a1d1
Josh Adam <josh.adam@canada.ca>
2019-06-20 10:47:15 -0500

    Updated JSDoc

--- src/main/webapp/resources/js/i18n.js
+++ src/main/webapp/resources/js/i18n.js
@@ -1,12 +1,29 @@
+/**
+ * Get the translation for the key from the global window.translations
+ * @param {string} key
+ * @returns {*}
+ */
 const getTranslation = key => {
   if (window.translations && window.translations[key])
     return window.translations[key];
   throw new Error(`No internationalization string for key: ${key}`);
 };
 
+/**
+ * Replace placeholders in the translation with provided values.
+ * @param {string} text
+ * @param {array} args - value to inject into the placeholder in the translation
+ * @returns {*}
+ */
 const replaceKeys = (text, args) =>
   text.replace(/{([0-9]+)}/g, (_, index) => args[index]);
 
+/**
+ * Globally exposed function to allow to internationalization.
+ * @param {string} key
+ * @param {array} args - list of strings to inject into the translation.
+ * @returns {string}
+ */
 function i18n(key, ...args) {
   try {
     const text = getTranslation(key);
