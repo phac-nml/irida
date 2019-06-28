cd7f920f96ade29694f475347d5c2449cd56ffdf
Tom Matthews <thomas.matthews@canada.ca>
2019-06-28 15:14:27 -0500

    login success handler setting user's locale

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/security/LoginSuccessHandler.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/security/LoginSuccessHandler.java
@@ -1,22 +1,21 @@
 package ca.corefacility.bioinformatics.irida.ria.security;
 
-import java.io.IOException;
-import java.util.Date;
+import ca.corefacility.bioinformatics.irida.model.user.User;
+import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
+import com.timgroup.jgravatar.Gravatar;
+import com.timgroup.jgravatar.GravatarDefaultImage;
+import com.timgroup.jgravatar.GravatarRating;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.web.servlet.LocaleResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-
-import ca.corefacility.bioinformatics.irida.model.user.User;
-import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
-
-import com.timgroup.jgravatar.Gravatar;
-import com.timgroup.jgravatar.GravatarDefaultImage;
-import com.timgroup.jgravatar.GravatarRating;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Handles actions for when a user is successfully logged in.
@@ -26,18 +25,24 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
 
 	private UserRepository userRepository;
 
-	public LoginSuccessHandler(UserRepository userRepository){
+	private LocaleResolver localeResolver;
+
+	public LoginSuccessHandler(UserRepository userRepository, LocaleResolver localeResolver) {
 		this.userRepository = userRepository;
+		this.localeResolver = localeResolver;
 	}
 
 	@Override
-	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse, Authentication authentication)
-			throws IOException, ServletException {
+	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+			Authentication authentication) throws IOException, ServletException {
 		super.onAuthenticationSuccess(httpServletRequest, httpServletResponse, authentication);
 
 		HttpSession session = httpServletRequest.getSession();
 		User user = (User) authentication.getPrincipal();
+
+		Locale locale = Locale.forLanguageTag(user.getLocale());
+
+		localeResolver.setLocale(httpServletRequest, httpServletResponse, locale);
 
 		// Add gravatar url as to the session for use in thymeleaf templates.
 		Gravatar gravatar = new Gravatar(25, GravatarRating.GENERAL_AUDIENCES, GravatarDefaultImage.IDENTICON);
