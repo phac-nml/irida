5f6c512a7339d638d5de9db4299eeca3e6e362c1
Josh Adam <josh.adam@canada.ca>
2019-08-01 11:37:12 -0500

    Only wrap the progress in flex box.
    Signed-off-by: Josh Adam <josh.adam@canada.ca>

--- src/main/webapp/resources/js/pages/analyses/AnalysisState.jsx
+++ src/main/webapp/resources/js/pages/analyses/AnalysisState.jsx
@@ -10,9 +10,9 @@ import { Badge, Progress } from "antd";
  */
 export function AnalysisState({ state, percentage }) {
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
+    <>
       {percentage < 100 && state !== "Error" ? (
-        <>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <Progress
             type="circle"
             percent={percentage}
@@ -20,12 +20,12 @@ export function AnalysisState({ state, percentage }) {
             style={{ paddingRight: 8 }}
           />
           {state}
-        </>
+        </div>
       ) : state === "Error" ? (
         <Badge status="error" text={state} />
       ) : (
         <Badge status="success" text={state} />
       )}
-    </div>
+    </>
   );
 }
