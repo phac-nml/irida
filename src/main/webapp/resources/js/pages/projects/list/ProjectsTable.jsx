48ed26c085b5123522eca53487eddcc3d9937936
Eric Enns <eric.enns@phac-aspc.gc.ca>
2019-08-09 11:38:47 -0500

    Fix extra / in projects link from projects table

--- src/main/webapp/resources/js/pages/projects/list/ProjectsTable.jsx
+++ src/main/webapp/resources/js/pages/projects/list/ProjectsTable.jsx
@@ -125,7 +125,7 @@ export function ProjectsTable() {
         ) : null
     },
     {
-      ...nameColumnFormat(`${window.TL.BASE_URL}projects/`),
+      ...nameColumnFormat(`${window.TL.BASE_URL}projects`),
       title: getI18N("ProjectsTable_th_name")
     },
     {
