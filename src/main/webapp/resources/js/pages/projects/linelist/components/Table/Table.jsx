90e9455ee9bdb4f013804d0c58b876651e366489
Josh Adam <josh.adam@canada.ca>
2019-08-09 13:16:44 -0500

    Does not need to be forced into a boolean

--- src/main/webapp/resources/js/pages/projects/linelist/components/Table/Table.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Table/Table.jsx
@@ -371,7 +371,7 @@ export class TableComponent extends React.Component {
       /*
       Show a notification that allows the user to reverse the change to the value.
        */
-      const text = Boolean(data[field])
+      const text = data[field]
         ? i18n.linelist.editing.undo.full
         : i18n.linelist.editing.undo.empty;
       showUndoNotification(
