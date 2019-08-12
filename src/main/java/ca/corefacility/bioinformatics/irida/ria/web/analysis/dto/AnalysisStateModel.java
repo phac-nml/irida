7bac805e908392567a3b69fe67d84ad5c61ee936
Josh Adam <josh.adam@canada.ca>
2019-08-12 14:24:51 -0500

    Fixed JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/dto/AnalysisStateModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/dto/AnalysisStateModel.java
@@ -1,7 +1,16 @@
 package ca.corefacility.bioinformatics.irida.ria.web.analysis.dto;
 
+/**
+ * UI Representation of an Analysis Submission state.
+ */
 public class AnalysisStateModel {
+	/*
+	Internationalized label
+	 */
 	private String text;
+	/*
+	Acutal enum value for the state.
+	 */
 	private String value;
 
 	public AnalysisStateModel(String text, String value) {
