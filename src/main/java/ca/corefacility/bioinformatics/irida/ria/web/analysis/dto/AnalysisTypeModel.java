17795ba4cbc715b028afd1e9d710bb30b73d1c4e
Josh Adam <josh.adam@canada.ca>
2019-08-12 14:59:35 -0500

    Fixed JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/dto/AnalysisTypeModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/dto/AnalysisTypeModel.java
@@ -1,7 +1,17 @@
 package ca.corefacility.bioinformatics.irida.ria.web.analysis.dto;
 
+/**
+ * UI Representation of an Analysis Type
+ */
 public class AnalysisTypeModel {
+	/*
+	Internationalized type
+	 */
 	private String text;
+
+	/*
+	Actual value for the type.
+	 */
 	private String value;
 
 	public AnalysisTypeModel(String text, String value) {
