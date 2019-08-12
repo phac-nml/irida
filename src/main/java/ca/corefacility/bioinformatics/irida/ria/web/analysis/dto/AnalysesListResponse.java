51a825ada9bcb09df9a3692c0631457373e0a7d4
Josh Adam <josh.adam@canada.ca>
2019-08-12 14:25:16 -0500

    Fixed JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/dto/AnalysesListResponse.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/dto/AnalysesListResponse.java
@@ -2,6 +2,9 @@ package ca.corefacility.bioinformatics.irida.ria.web.analysis.dto;
 
 import java.util.List;
 
+/**
+ * UI Response for the current page of the Analyses Table.
+ */
 public class AnalysesListResponse {
 	private List<AnalysisModel> analyses;
 	private Long total;
