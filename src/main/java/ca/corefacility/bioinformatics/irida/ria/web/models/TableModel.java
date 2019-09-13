3584e43d6799a6b2284e8bbed2b1265587b5eba7
Josh Adam <josh.adam@canada.ca>
2019-09-13 10:59:48 -0500

    Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/models/TableModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/models/TableModel.java
@@ -1,3 +1,6 @@
 package ca.corefacility.bioinformatics.irida.ria.web.models;
 
-public class TableModel {}
+/**
+ * Parent class for items to be return in a {@link TableResponse}
+ */
+public abstract class TableModel {}
