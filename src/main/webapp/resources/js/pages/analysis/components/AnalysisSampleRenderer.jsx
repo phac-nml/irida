98eeefc90630d6a534f1008c08fd52aec5baa10c
Josh Adam <josh.adam@canada.ca>
2019-09-17 15:16:44 -0500

    Updated rendering of samples

--- src/main/webapp/resources/js/pages/analysis/components/AnalysisSampleRenderer.jsx
+++ src/main/webapp/resources/js/pages/analysis/components/AnalysisSampleRenderer.jsx
@@ -6,7 +6,7 @@
 import React, { useContext } from "react";
 import { getI18N } from "../../../utilities/i18n-utilties";
 import { AnalysisSamplesContext } from "../../../contexts/AnalysisSamplesContext";
-import { Card, Row, Col, Icon, Button } from "antd";
+import { Avatar, Icon, List } from "antd";
 
 export function AnalysisSampleRenderer() {
   /*
@@ -20,79 +20,110 @@ export function AnalysisSampleRenderer() {
     const samplesList = [];
 
     if (analysisSamplesContext.sequenceFilePairList.length > 0) {
-      let pairIndex = 0;
-      for (const [
-        index,
-        sampleObj
-      ] of analysisSamplesContext.samples.entries()) {
-        samplesList.push(
-          <Card
-            type="inner"
-            title={
-              <Button
-                type="link"
-                href={`${window.TL.BASE_URL}samples/${sampleObj.sample.identifier}/details`}
-                target="_blank"
-              >
-                <Icon type="filter" rotate="180" />{" "}
-                {sampleObj.sample.sampleName}
-              </Button>
-            }
-            key={`sampleId-${sampleObj.sample.identifier}`}
-          >
-            <Row
-              key={`fileId-${analysisSamplesContext.sequenceFilePairList[pairIndex].identifier}`}
-            >
-              <span
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center"
-                }}
-              >
-                <Button
-                  type="link"
-                  target="_blank"
-                  href={`${window.TL.BASE_URL}sequenceFiles/${sampleObj.sequenceFilePair.identifier}/file/${analysisSamplesContext.sequenceFilePairList[pairIndex].identifier}/summary`}
-                >
-                  <Icon type="arrow-right" />{" "}
-                  {analysisSamplesContext.sequenceFilePairList[pairIndex].label}
-                </Button>
-                <span>
-                  {analysisSamplesContext.sequenceFileSizeList[pairIndex]}
-                </span>
-              </span>
-            </Row>
-            <Row
-              key={`fileId-${analysisSamplesContext.sequenceFilePairList[pairIndex + 1].identifier}`}
-            >
-              <span
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center"
-                }}
-              >
-                <Button
-                  type="link"
-                  target="_blank"
-                  href={`${window.TL.BASE_URL}sequenceFiles/${sampleObj.sequenceFilePair.identifier}/file/${analysisSamplesContext.sequenceFilePairList[pairIndex + 1].identifier}/summary`}
-                >
-                  <Icon type="arrow-left" />{" "}
-                  {
-                    analysisSamplesContext.sequenceFilePairList[pairIndex + 1]
-                      .label
+      return (
+        <List
+          bordered
+          dataSource={analysisSamplesContext.samples}
+          renderItem={item => {
+            console.log(item);
+            return (
+              <List.Item>
+                <List.Item.Meta
+                  key={item.id}
+                  avatar={
+                    <Avatar>
+                      <Icon type="experiment" />
+                    </Avatar>
                   }
-                </Button>
-                <span>
-                  {analysisSamplesContext.sequenceFileSizeList[pairIndex + 1]}
-                </span>
-              </span>
-            </Row>
-          </Card>
-        );
-        pairIndex = pairIndex + 2;
-      }
+                  title={<a href={"#"}>{item.sample.label}</a>}
+                  description={
+                    <div>
+                      {item.sequenceFilePair.files.map(file => (
+                        <div key={`file-${file.id}`}>
+                          <a href={"#"}>{file.fileName}</a>
+                        </div>
+                      ))}
+                    </div>
+                  }
+                />
+              </List.Item>
+            );
+          }}
+        />
+      );
+      // let pairIndex = 0;
+      // for (const [
+      //   index,
+      //   sampleObj
+      // ] of analysisSamplesContext.samples.entries()) {
+      //   samplesList.push(
+      //     <Card
+      //       type="inner"
+      //       title={
+      //         <Button
+      //           type="link"
+      //           href={`${window.TL.BASE_URL}samples/${sampleObj.sample.identifier}/details`}
+      //           target="_blank"
+      //         >
+      //           <Icon type="filter" rotate="180" />{" "}
+      //           {sampleObj.sample.sampleName}
+      //         </Button>
+      //       }
+      //       key={`sampleId-${sampleObj.sample.identifier}`}
+      //     >
+      //       <Row
+      //         key={`fileId-${analysisSamplesContext.sequenceFilePairList[pairIndex].identifier}`}
+      //       >
+      //         <span
+      //           style={{
+      //             display: "flex",
+      //             justifyContent: "space-between",
+      //             alignItems: "center"
+      //           }}
+      //         >
+      //           <Button
+      //             type="link"
+      //             target="_blank"
+      //             href={`${window.TL.BASE_URL}sequenceFiles/${sampleObj.sequenceFilePair.identifier}/file/${analysisSamplesContext.sequenceFilePairList[pairIndex].identifier}/summary`}
+      //           >
+      //             <Icon type="arrow-right" />{" "}
+      //             {analysisSamplesContext.sequenceFilePairList[pairIndex].label}
+      //           </Button>
+      //           <span>
+      //             {analysisSamplesContext.sequenceFileSizeList[pairIndex]}
+      //           </span>
+      //         </span>
+      //       </Row>
+      //       <Row
+      //         key={`fileId-${analysisSamplesContext.sequenceFilePairList[pairIndex + 1].identifier}`}
+      //       >
+      //         <span
+      //           style={{
+      //             display: "flex",
+      //             justifyContent: "space-between",
+      //             alignItems: "center"
+      //           }}
+      //         >
+      //           <Button
+      //             type="link"
+      //             target="_blank"
+      //             href={`${window.TL.BASE_URL}sequenceFiles/${sampleObj.sequenceFilePair.identifier}/file/${analysisSamplesContext.sequenceFilePairList[pairIndex + 1].identifier}/summary`}
+      //           >
+      //             <Icon type="arrow-left" />{" "}
+      //             {
+      //               analysisSamplesContext.sequenceFilePairList[pairIndex + 1]
+      //                 .label
+      //             }
+      //           </Button>
+      //           <span>
+      //             {analysisSamplesContext.sequenceFileSizeList[pairIndex + 1]}
+      //           </span>
+      //         </span>
+      //       </Row>
+      //     </Card>
+      //   );
+      //   pairIndex = pairIndex + 2;
+      // }
     } else {
       samplesList.push(
         <p key={`no-paired-end-0`}>{getI18N("AnalysisSamples.noPairedEnd")}</p>
