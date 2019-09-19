22f8c4100efdefc4459b73130b0231aabc0f73bf
Deep Sidhu <sukhdeep.sidhu@phac-aspc.gc.ca>
2019-09-19 15:37:34 -0500

    Updated translation text. Message is now displayed when an analysis has 0 samples. Fixed sample search

--- src/main/webapp/resources/js/pages/analysis/components/AnalysisSampleRenderer.jsx
+++ src/main/webapp/resources/js/pages/analysis/components/AnalysisSampleRenderer.jsx
@@ -6,10 +6,11 @@
 import React, { useContext, useState } from "react";
 import { getI18N } from "../../../utilities/i18n-utilties";
 import { AnalysisSamplesContext } from "../../../contexts/AnalysisSamplesContext";
-import { Avatar, Icon, List, Input } from "antd";
+import { Avatar, Icon, List, Input, Typography } from "antd";
 import { SPACE_MD } from "../../../styles/spacing";
 
 const { Search } = Input;
+const { Text } = Typography;
 
 export function AnalysisSampleRenderer() {
   /*
@@ -25,61 +26,55 @@ export function AnalysisSampleRenderer() {
   );
 
   const renderSamples = () => {
-    if (analysisSamplesContext.samples.length > 0) {
-      return (
-        <List
-          bordered
-          dataSource={filteredSamples}
-          style={{ height: sampleDisplayHeight, overflowY: "auto" }}
-          renderItem={item => {
-            return (
-              <List.Item>
-                <List.Item.Meta
-                  key={item.sampleId}
-                  avatar={
-                    <Avatar>
-                      <Icon type="experiment" />
-                    </Avatar>
-                  }
-                  title={
-                    <a
-                      href={`${window.TL.BASE_URL}samples/${item.sampleId}/details`}
-                      target="_blank"
-                    >
-                      {item.sampleName}
-                    </a>
-                  }
-                  description={
-                    <div>
-                      <div key={`file-${item.forward.identifier}`}>
-                        <a
-                          href={`${window.TL.BASE_URL}sequenceFiles/${item.sequenceFilePairId}/file/${item.forward.identifier}/summary`}
-                          target="_blank"
-                        >
-                          {item.forward.fileName}
-                        </a>
-                      </div>
-                      <div key={`file-${item.reverse.identifier}`}>
-                        <a
-                          href={`${window.TL.BASE_URL}sequenceFiles/${item.sequenceFilePairId}/file/${item.reverse.identifier}/summary`}
-                          target="_blank"
-                        >
-                          {item.reverse.fileName}
-                        </a>
-                      </div>
+    return (
+      <List
+        bordered
+        dataSource={filteredSamples}
+        style={{ height: sampleDisplayHeight, overflowY: "auto" }}
+        renderItem={item => {
+          return (
+            <List.Item>
+              <List.Item.Meta
+                key={item.sampleId}
+                avatar={
+                  <Avatar>
+                    <Icon type="experiment" />
+                  </Avatar>
+                }
+                title={
+                  <a
+                    href={`${window.TL.BASE_URL}samples/${item.sampleId}/details`}
+                    target="_blank"
+                  >
+                    {item.sampleName}
+                  </a>
+                }
+                description={
+                  <div>
+                    <div key={`file-${item.forward.identifier}`}>
+                      <a
+                        href={`${window.TL.BASE_URL}sequenceFiles/${item.sequenceFilePairId}/file/${item.forward.identifier}/summary`}
+                        target="_blank"
+                      >
+                        {item.forward.fileName}
+                      </a>
                     </div>
-                  }
-                />
-              </List.Item>
-            );
-          }}
-        />
-      );
-    } else {
-      return (
-        <p key={`no-paired-end-0`}>{getI18N("AnalysisSamples.noPairedEnd")}</p>
-      );
-    }
+                    <div key={`file-${item.reverse.identifier}`}>
+                      <a
+                        href={`${window.TL.BASE_URL}sequenceFiles/${item.sequenceFilePairId}/file/${item.reverse.identifier}/summary`}
+                        target="_blank"
+                      >
+                        {item.reverse.fileName}
+                      </a>
+                    </div>
+                  </div>
+                }
+              />
+            </List.Item>
+          );
+        }}
+      />
+    );
   };
 
   /*
@@ -93,9 +88,9 @@ export function AnalysisSampleRenderer() {
       searchStr = String(searchStr).toLowerCase();
       const samplesContainingSearchValue = analysisSamplesContext.samples.filter(
         sample =>
-          sample.sampleName.includes(searchStr) ||
-          sample.forward.fileName.includes(searchStr) ||
-          sample.reverse.fileName.includes(searchStr)
+          sample.sampleName.toLowerCase().includes(searchStr) ||
+          sample.forward.fileName.toLowerCase().includes(searchStr) ||
+          sample.reverse.fileName.toLowerCase().includes(searchStr)
       );
       setFilteredSamples(samplesContainingSearchValue);
     }
@@ -113,7 +108,11 @@ export function AnalysisSampleRenderer() {
           />
           {renderSamples()}
         </div>
-      ) : null}
+      ) : (
+        <Text strong key={`no-paired-end-0`}>
+          {getI18N("AnalysisSamples.noPairedEnd")}
+        </Text>
+      )}
     </>
   );
 }
