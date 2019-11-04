c0d3b5d68e1cb9eab67980323764735456f7806c
Eric Enns <eric.enns@phac-aspc.gc.ca>
2019-11-04 15:29:31 -0600

    Remove unused import for User model

--- src/main/java/ca/corefacility/bioinformatics/irida/model/user/User.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/user/User.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import org.hibernate.annotations.Immutable;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
 import org.hibernate.validator.constraints.Email;
