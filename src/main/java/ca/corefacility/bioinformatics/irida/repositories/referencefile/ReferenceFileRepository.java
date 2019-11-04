a44d96c67d7c8a63b1877fb64ad8deab83001e2f
Eric Enns <eric.enns@phac-aspc.gc.ca>
2019-11-04 09:37:28 -0600

    Update comments to state why save is overridden

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/referencefile/ReferenceFileRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/referencefile/ReferenceFileRepository.java
@@ -11,9 +11,11 @@ import ca.corefacility.bioinformatics.irida.repositories.filesystem.FilesystemSu
  */
 public interface ReferenceFileRepository extends IridaJpaRepository<ReferenceFile, Long>,
 		FilesystemSupplementedRepository<ReferenceFile> {
-	//TODO: Look into a solution that places this in FilesystemSupplementedRepository
 	/**
 	 * {@inheritDoc}
+	 *
+	 * Save is overridden here instead of in FilesystemSupplementedRepository as it would throw a
+	 * compilation error
 	 */
 	<S extends ReferenceFile> S save(S entity);
 }
