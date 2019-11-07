3ce9d15384edebcbbc46221a282033890d0ec604
Josh Adam <josh.adam@canada.ca>
2019-11-07 14:57:58 -0600

    Updated JsDoc on TextFilter.js

--- src/main/webapp/resources/js/components/Tables/fitlers/TextFilter.js
+++ src/main/webapp/resources/js/components/Tables/fitlers/TextFilter.js
@@ -1,8 +1,28 @@
+/**
+ * @file Component responsible for rendering a text filter for an ant.design table column.
+ */
 import React from "react";
 import { Button, Input } from "antd";
 import { grey4 } from "../../../styles/colors";
 import { SPACE_XS } from "../../../styles/spacing";
 
+/**
+ * This callback type is `setSelectedKeys` and is used by ant.design table to
+ * set the current value.
+ *
+ * @callback setSelectedKeys
+ * @param {string[]} - where the only value is the value of the filter.
+ */
+
+/**
+ * Render a text filter component to an ant.design column
+ * @param {setSelectedKeys} setSelectedKeys - callback to handle the actual table filtering.
+ * @param {string[]} selectedKeys - the current value of the table filter
+ * @param {function} confirm - triggers the table filter to run.
+ * @param {function} clearFilters - clears the filters on the column.
+ * @returns {*}
+ * @constructor
+ */
 export const TextFilter = ({
   setSelectedKeys,
   selectedKeys,
