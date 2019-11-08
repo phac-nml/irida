bbe476aac3c33275094afab6d2be49b9fd570d15
Josh Adam <josh.adam@canada.ca>
2019-11-08 09:03:24 -0600

    Updated JsDoc on ConfirmButton.jsx

--- src/main/webapp/resources/js/components/Buttons/ConfirmButton.jsx
+++ src/main/webapp/resources/js/components/Buttons/ConfirmButton.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Button, Popconfirm } from "antd";
 
+/**
+ * Renders a Button component wrapped in a Popconfirm
+ * @param {string} title - Title on the Popconfirm
+ * @param {function} onConfirm - What to do when the ok is clicked on the Popconfirm
+ * @param {string} label - Text for the button
+ * @param {object} props - any remaining properties to pass in
+ * @returns {*}
+ * @constructor
+ */
 export function ConfirmButton({title, onConfirm, label, ...props}) {
   return (
     <Popconfirm
       title={title}
       onConfirm={onConfirm}
-      {...props}
     >
-      <Button type="link">{label}</Button>
+      <Button type="link" {...props}>{label}</Button>
     </Popconfirm>
   );
 }
