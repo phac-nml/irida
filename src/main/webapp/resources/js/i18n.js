4801bbec820f908fc19c5b39328dfbe2d866159e
Josh Adam <josh.adam@canada.ca>
2019-11-19 13:09:02 -0600

    Don't need to make window.translations null since there could be multiple

--- src/main/webapp/resources/js/i18n.js
+++ src/main/webapp/resources/js/i18n.js
@@ -2,7 +2,6 @@
  * On load, get all the internationalization strings and combine them into one object
  */
 const translations = Object.assign({}, ...window.translations);
-window.translations = null;
 
 /**
  * Get the translation for the key from the global window.translations
