b6ffc13de25214b1177ad950e453e26dba52622d
Deep Sidhu <sukhdeep.sidhu@canada.ca>
2019-12-09 14:31:09 -0600

    Set background for layout with standardized color

--- src/main/webapp/resources/js/pages/analysis/components/AnalysisOutputFiles.jsx
+++ src/main/webapp/resources/js/pages/analysis/components/AnalysisOutputFiles.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Layout } from "antd";
 import { SPACE_MD } from "../../../styles/spacing";
+import { grey1 } from "../../../styles/colors";
 
 const { Content } = Layout;
 
@@ -11,7 +12,7 @@ const OutputFilePreview = React.lazy(() =>
 export default function AnalysisOutputFiles() {
   const BASE_URL = `${window.PAGE.base}`;
   return (
-    <Layout style={{ paddingLeft: SPACE_MD, backgroundColor: "white" }}>
+    <Layout style={{ paddingLeft: SPACE_MD, backgroundColor: grey1 }}>
       <Content>
         <OutputFilePreview path={`${BASE_URL}/file_preview`} />
       </Content>
