f3868756a6df09acb7af3abf6754d83dc700eeef
Deep Sidhu <sukhdeep.sidhu@canada.ca>
2019-12-09 13:47:24 -0600

    Set phylocanvas div height to 100%. Updated button values

--- src/main/webapp/resources/js/pages/analysis/components/tree/Tree.jsx
+++ src/main/webapp/resources/js/pages/analysis/components/tree/Tree.jsx
@@ -12,7 +12,7 @@ import { getNewickTree } from "../../../../apis/analysis/analysis";
 import { ContentLoading } from "../../../../components/loader/ContentLoading";
 import { WarningAlert } from "../../../../components/alerts/WarningAlert";
 import styled from "styled-components";
-import { SPACE_XS, SPACE_MD } from "../../../../styles/spacing";
+import { SPACE_MD } from "../../../../styles/spacing";
 import { BORDER_LIGHT } from "../../../../styles/borders";
 
 const CANVAS_HEIGHT = 600;
@@ -51,7 +51,7 @@ export default function Tree() {
       <Phylocanvas
         data={newickString}
         treeType={currTreeShape}
-        style={{ height: CANVAS_HEIGHT - SPACE_XS }}
+        style={{ height: "100%" }}
       />
     );
   }
@@ -81,10 +81,10 @@ export default function Tree() {
                 <Radio.Button value="circular">
                   {getI18N("AnalysisPhylogeneticTree.circular")}
                 </Radio.Button>
-                <Radio.Button value="diagonal">
+                <Radio.Button value="radial">
                   {getI18N("AnalysisPhylogeneticTree.radial")}
                 </Radio.Button>
-                <Radio.Button value="radial">
+                <Radio.Button value="diagonal">
                   {getI18N("AnalysisPhylogeneticTree.diagonal")}
                 </Radio.Button>
                 <Radio.Button value="hierarchical">
