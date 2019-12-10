97763f1f85041bc150383277901ada7e6860ad44
Deep Sidhu <sukhdeep.sidhu@canada.ca>
2019-12-10 10:14:53 -0600

    Updated urls to use routes

--- src/main/webapp/resources/js/pages/analysis/components/AnalysisOutputFiles.jsx
+++ src/main/webapp/resources/js/pages/analysis/components/AnalysisOutputFiles.jsx
@@ -1,20 +1,24 @@
+/**
+ * @File component renders a preview of output files
+ */
+
 import React from "react";
 import { Layout } from "antd";
 import { SPACE_MD } from "../../../styles/spacing";
 import { grey1 } from "../../../styles/colors";
-
-const { Content } = Layout;
+import { ANALYSIS, OUTPUT } from "../routes";
 
 const OutputFilePreview = React.lazy(() =>
   import("./outputs/OutputFilePreview")
 );
+const { Content } = Layout;
 
 export default function AnalysisOutputFiles() {
-  const BASE_URL = `${window.PAGE.base}`;
+  const BASE_URL = `${window.PAGE.base}/${ANALYSIS.OUTPUT}`;
   return (
     <Layout style={{ paddingLeft: SPACE_MD, backgroundColor: grey1 }}>
       <Content>
-        <OutputFilePreview path={`${BASE_URL}/file_preview`} />
+        <OutputFilePreview path={`${BASE_URL}/${OUTPUT.FILE_PREVIEW}`} />
       </Content>
     </Layout>
   );
