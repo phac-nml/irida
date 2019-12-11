88461150187725e897fdd8afd18d33fafd64c5cf
Eric Enns <eric.enns@phac-aspc.gc.ca>
2019-12-11 11:33:26 -0600

    Fixed optimizeCSSAssetsPlugin config

--- src/main/webapp/wepack.config.prod.js
+++ src/main/webapp/wepack.config.prod.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpack = require("webpack");
-const cssnano = require("cssnano");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
