952283cc454d2b98924d7bf7f6ee2e9fa3faf8c0
Josh Adam <josh.adam@canada.ca>
2019-12-12 08:13:43 -0600

    Formatting

--- src/main/webapp/resources/js/pages/projects/linelist/components/Table/renderers/SampleNameRenderer.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/Table/renderers/SampleNameRenderer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { FIELDS } from "../../../constants";
 
 const { BASE_URL } = window.TL;
--- src/main/webapp/resources/js/pages/search/search.js
+++ src/main/webapp/resources/js/pages/search/search.js
@@ -31,9 +31,7 @@ const projectConfig = Object.assign({}, tableConfig, {
           url: `${window.PAGE.urls.project}${full.id}`,
           label: `${
             full.remote
-              ? `<div aria-hidden="true" data-toggle="tooltip" data-placement="top" title="${
-                  window.PAGE.i18n.remote
-                }">${data}&nbsp;<i style="color: #000;" class="fa fa-exchange pull-right"></i></div>`
+              ? `<div aria-hidden="true" data-toggle="tooltip" data-placement="top" title="${window.PAGE.i18n.remote}">${data}&nbsp;<i style="color: #000;" class="fa fa-exchange pull-right"></i></div>`
               : data
           }`,
           width: "200px"
@@ -70,9 +68,7 @@ const sampleConfig = Object.assign({}, tableConfig, {
       render(data, type, full) {
         // Render the name as a link to the actual project.
         return createItemLink({
-          url: `${window.PAGE.urls.project}${full.projectId}/samples/${
-            full.id
-          }`,
+          url: `${window.PAGE.urls.project}${full.projectId}/samples/${full.id}`,
           label: data
           //width: "200px"
         });
