e839fbaea2a13841bdf15b74e5d8968d17582e40
Deep Sidhu <sukhdeep.sidhu@canada.ca>
2019-12-30 13:28:55 -0600

    Full provenance for a file is now displayed. Added comments

--- src/main/webapp/resources/js/pages/analysis/components/AnalysisProvenance.jsx
+++ src/main/webapp/resources/js/pages/analysis/components/AnalysisProvenance.jsx
@@ -20,6 +20,8 @@ export default function AnalysisProvenance() {
     AnalysisOutputsContext
   );
   const [provenance, setProvenance] = useState(null);
+  const [toolInfo, setToolInfo] = useState([]);
+  const [currFileName, setCurrFileName] = useState(null);
 
   // Gets the analysis outputs on load if they have not
   // already been loaded
@@ -29,7 +31,7 @@ export default function AnalysisProvenance() {
     }
   }, []);
 
-  // Get the provenance for the file
+  // Get the provenance for the file that the user selected
   function getProvenance(filename) {
     if (typeof filename !== "undefined") {
       if (
@@ -37,18 +39,21 @@ export default function AnalysisProvenance() {
           !provenance.data.filename.includes(filename.toString())) ||
         provenance === null
       ) {
+        setCurrFileName(filename.toString());
         getAnalysisProvenanceByFile(
           analysisContext.analysis.identifier,
           filename.toString()
         ).then(data => {
           setProvenance(data);
+          setToolInfo([]);
         });
       }
     }
   }
 
-  // Displays the execution parameters for the tool
-  function displayExecutionParameters(tool) {
+  // Returns a collapse panel for the tool and it's execution parameters.
+  // Parameters are sorted alphabetically by label
+  function displayToolWithExecutionParameters(tool, margin) {
     let parameters = [];
     let sortedParameters = tool.executionParameters.sort((a, b) =>
       a.parameterName > b.parameterName ? 1 : -1
@@ -66,38 +71,81 @@ export default function AnalysisProvenance() {
     }
 
     return (
-      <Collapse style={{ border: "none" }}>
+      <Collapse
+        style={{ border: "none", backgroundColor: grey1 }}
+        key={`collapse-${tool.toolName}-${Math.random()}`}
+      >
         <Panel
           header={tool.toolName}
           key={tool.toolname}
-          style={{ backgroundColor: grey1 }}
+          style={{
+            backgroundColor: grey1,
+            marginLeft: `${3 * margin}px`,
+            borderBottom: "0px"
+          }}
         >
-          <Descriptions bordered column={3} size="small">
-            {parameters}
-          </Descriptions>
+          {parameters.length > 0 ? (
+            <Descriptions bordered column={2} size="small">
+              {parameters}
+            </Descriptions>
+          ) : (
+            ""
+          )}
         </Panel>
       </Collapse>
     );
   }
 
-  // Creates the panel for the first tool and gets the execution parameters for it
+  // Recursive function to get all the previous execution
+  // tools and their respective execution parameters for
+  // the tool that the output was created by
+  function getPreviousExecutionTools(tool, margin) {
+    let prevTools = tool.previousExecutionTools;
+
+    // We either add 5px to the previous
+    // margin or subtract depending on
+    // which "level" the tool is at
+    if (prevTools.length > 0) {
+      margin = margin + 5;
+    } else {
+      margin = margin - 5;
+    }
+
+    for (let currTool of prevTools) {
+      toolInfo.push(displayToolWithExecutionParameters(currTool, margin));
+      getPreviousExecutionTools(currTool, margin);
+    }
+  }
+
+  // Creates the panels for all the output files but
+  // gets the provenance for only the output file that
+  // the user is viewing the provenance for
   function getCreatedByToolPanels() {
-    let c = [];
+    let panels = [];
     if (analysisOutputsContext.outputs !== null) {
-      for (let d of analysisOutputsContext.outputs) {
-        c.push(
+      for (let output of analysisOutputsContext.outputs) {
+        {
+          provenance !== null && currFileName === output.filename
+            ? getPreviousExecutionTools(provenance.data.createdByTool, 0)
+            : "";
+        }
+        panels.push(
           <Panel
-            header={d.filename}
-            key={d.filename}
+            header={output.filename}
+            key={output.filename}
             style={{ backgroundColor: grey1 }}
           >
-            {provenance !== null
-              ? displayExecutionParameters(provenance.data.createdByTool)
-              : ""}
+            {currFileName === output.filename && provenance !== null
+              ? displayToolWithExecutionParameters(
+                  provenance.data.createdByTool,
+                  0
+                )
+              : null}
+            {currFileName === output.filename ? toolInfo : null}
           </Panel>
         );
       }
-      return c;
+      return panels;
     }
   }
 
