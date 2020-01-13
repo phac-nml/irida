5ece8153a71b773ea963bb724717ebe9bf28131e
Dan Fornika <dfornika@gmail.com>
2020-01-13 15:59:11 -0800

    Change how dates are constructed in test

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/utilities/converters/StringToDateConverterTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/utilities/converters/StringToDateConverterTest.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.core.convert.converter.Converter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,15 +21,29 @@ public class StringToDateConverterTest {
 
 	@Before
 	public void setUp() {
+
 		converter = new StringToDateConverter();
 	}
 
 	@Test
 	public void testConversions() {
+		SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
+		isoFormat.setTimeZone(TimeZone.getDefault());
+
 		String unix_epoch_string = "1970-01-01";
-		Date unix_epoch_date = new Date(0L);
+		Date unix_epoch_date = null;
+		try {
+			unix_epoch_date = isoFormat.parse("1970-01-01");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		String y2k_string = "2000-01-01";
-		Date y2k_date = new Date(946713600000L);
+		Date y2k_date = null;
+		try {
+			y2k_date = isoFormat.parse("2000-01-01");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 
 		assertEquals("The beginning of the universe", unix_epoch_date, converter.convert(unix_epoch_string));
 		assertEquals("New millenium", y2k_date, converter.convert(y2k_string));
