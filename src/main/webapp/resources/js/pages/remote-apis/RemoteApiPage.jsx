049723fc808e4c283f15cd97098704b40dc52cbd
Josh Adam <josh.adam@canada.ca>
2020-01-28 11:31:24 -0600

    Starting work on add new button

--- src/main/webapp/resources/js/pages/remote-apis/RemoteApiPage.jsx
+++ src/main/webapp/resources/js/pages/remote-apis/RemoteApiPage.jsx
@@ -3,10 +3,11 @@ import { render } from "react-dom";
 import { PageWrapper } from "../../components/page/PageWrapper";
 import { RemoteApiTable } from "./RemoteApiTable";
 import { PagedTableProvider } from "../../contexts/PagedTableContext";
+import { AddNewButton } from "../../components/Buttons/AddNewButton";
 
 export function RemoteApiPage({}) {
   return (
-    <PageWrapper title={i18n("RemoteApi.title")}>
+    <PageWrapper title={i18n("RemoteApi.title")} headerExtras={<AddNewButton text={"FOOBAR"} />}>
       <PagedTableProvider url="remote_api/ajax/list">
         <RemoteApiTable />
       </PagedTableProvider>
