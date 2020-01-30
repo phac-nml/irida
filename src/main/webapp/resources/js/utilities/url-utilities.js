2b82c65e398a66d3dc23f0fdf366549e5565219f
Josh Adam <josh.adam@canada.ca>
2020-01-30 09:34:30 -0600

    Check to see if the url has the context path first.

--- src/main/webapp/resources/js/utilities/url-utilities.js
+++ src/main/webapp/resources/js/utilities/url-utilities.js
@@ -9,23 +9,22 @@
  * @return {string|*}
  */
 export function setBaseUrl(url) {
-  // Remove any leading slashes
-  url = url.replace(/^\/+/, "");
-
   /*
   Get the base url which is set via the thymeleaf template engine.
    */
   const BASE_URL = window.TL?._BASE_URL || "/";
+
   /*
   Check to make sure that the given url has not already been given the
   base url.
    */
-  if (
-    (BASE_URL !== "/" && url.startsWith(BASE_URL)) ||
-    url.startsWith("http")
-  ) {
+  if (url.startsWith(BASE_URL) || url.startsWith("http")) {
     return url;
   }
+
+  // Remove any leading slashes
+  url = url.replace(/^\/+/, "");
+
   /*
   Create the new url
    */
