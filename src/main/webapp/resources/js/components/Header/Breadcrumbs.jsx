cfabaff606083d09abed2952dd9cc64e63a06f4a
Josh Adam <josh.adam@canada.ca>
2020-02-18 09:18:46 -0600

    removed styled-components as it conflicted with already loaded one

--- src/main/webapp/resources/js/components/Header/Breadcrumbs.jsx
+++ src/main/webapp/resources/js/components/Header/Breadcrumbs.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import styled from "styled-components";
 import { Breadcrumb } from "antd";
 import { grey2, grey4, grey8 } from "../../styles/colors";
 import { SPACE_MD, SPACE_XS } from "../../styles/spacing";
 import { setBaseUrl } from "../../utilities/url-utilities";
 import { HomeTwoTone } from "@ant-design/icons";
 
-const BreadCrumbs = styled(Breadcrumb)`
-  background-color: ${grey2};
-  padding: ${SPACE_XS} 15px;
-  justify-content: flex-start;
-  border-bottom: 1px solid ${grey4};
-  margin-bottom: ${SPACE_MD};
-`;
-
 export function Breadcrumbs({ crumbs = [] }) {
   return crumbs.length ? (
-    <BreadCrumbs>
+    <Breadcrumb
+      style={{
+        backgroundColor: grey2,
+        padding: `${SPACE_XS} 15px`,
+        justifyContent: "flex-start",
+        borderBottom: `1px solid ${grey4}`,
+        marginBottom: SPACE_MD
+      }}
+    >
       <Breadcrumb.Item>
         <a href={setBaseUrl("")}>
           <HomeTwoTone style={{ color: grey8 }} />
@@ -27,6 +26,6 @@ export function Breadcrumbs({ crumbs = [] }) {
           <a href={crumb.url}>{crumb.label}</a>
         </Breadcrumb.Item>
       ))}
-    </BreadCrumbs>
+    </Breadcrumb>
   ) : null;
 }
