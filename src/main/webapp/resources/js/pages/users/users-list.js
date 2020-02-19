b05ddc2e2829025efe249e7e27a9b06ed34540c3
Josh Adam <josh.adam@canada.ca>
2020-02-19 08:33:37 -0600

    Initial page setup.

--- src/main/webapp/resources/js/pages/users/users-list.js
+++ src/main/webapp/resources/js/pages/users/users-list.js
@@ -1,55 +1,77 @@
-import "../../vendor/datatables/datatables";
-import {
-  createItemLink,
-  generateColumnOrderInfo,
-  tableConfig
-} from "../../utilities/datatables-utilities";
-import { formatDate } from "../../utilities/date-utilities";
+import React from "react";
+import { render } from "react-dom";
+import { PageWrapper } from "../../components/page/PageWrapper";
+import { PagedTableProvider } from "../../components/ant.design/PagedTable";
+import { setBaseUrl } from "../../utilities/url-utilities";
 
-/*
-Get the table headers and create a look up table for them.
-This give the row name in snake case and its index.
- */
-let COLUMNS = generateColumnOrderInfo();
+function UsersTable() {
+  return <h2>FUCK ME</h2>;
+}
 
-const config = Object.assign(tableConfig, {
-  ajax: window.PAGE.urls.table,
-  order: [[COLUMNS.USERNAME, "desc"]],
-  columnDefs: [
-    {
-      targets: [COLUMNS.USERNAME],
-      render(data, type, full) {
-        return createItemLink({
-          url: `${window.PAGE.urls.link}${full.id}`,
-          label: data
-        });
-      }
-    },
-    {
-      targets: [COLUMNS.EMAIL],
-      render(data) {
-        return `<a href="mailto:${data}" class="btn btn-link">${data}</a>`;
-      }
-    },
-    {
-      targets: [COLUMNS.CREATED_DATE],
-      render(data) {
-        const date = formatDate({ date: data });
-        return `<time>${date}</time>`;
-      }
-    },
-    {
-      targets: [COLUMNS.LAST_LOGIN],
-      render(data) {
-        if (data != null) {
-          const date = formatDate({ date: data });
-          return `<time class="last-login">${date}</time>`;
-        } else {
-          return "";
-        }
-      }
-    }
-  ]
-});
+function UsersPage() {
+  return (
+    <PageWrapper title={i18n("users.title")}>
+      <PagedTableProvider url={setBaseUrl("users/ajax/list")}>
+        <UsersTable />
+      </PagedTableProvider>
+    </PageWrapper>
+  );
+}
 
-$("#usersTable").DataTable(config);
+render(<UsersPage />, document.querySelector("#react-root"));
+
+// import "../../vendor/datatables/datatables";
+// import {
+//   createItemLink,
+//   generateColumnOrderInfo,
+//   tableConfig
+// } from "../../utilities/datatables-utilities";
+// import { formatDate } from "../../utilities/date-utilities";
+//
+// /*
+// Get the table headers and create a look up table for them.
+// This give the row name in snake case and its index.
+//  */
+// let COLUMNS = generateColumnOrderInfo();
+//
+// const config = Object.assign(tableConfig, {
+//   ajax: window.PAGE.urls.table,
+//   order: [[COLUMNS.USERNAME, "desc"]],
+//   columnDefs: [
+//     {
+//       targets: [COLUMNS.USERNAME],
+//       render(data, type, full) {
+//         return createItemLink({
+//           url: `${window.PAGE.urls.link}${full.id}`,
+//           label: data
+//         });
+//       }
+//     },
+//     {
+//       targets: [COLUMNS.EMAIL],
+//       render(data) {
+//         return `<a href="mailto:${data}" class="btn btn-link">${data}</a>`;
+//       }
+//     },
+//     {
+//       targets: [COLUMNS.CREATED_DATE],
+//       render(data) {
+//         const date = formatDate({ date: data });
+//         return `<time>${date}</time>`;
+//       }
+//     },
+//     {
+//       targets: [COLUMNS.LAST_LOGIN],
+//       render(data) {
+//         if (data != null) {
+//           const date = formatDate({ date: data });
+//           return `<time class="last-login">${date}</time>`;
+//         } else {
+//           return "";
+//         }
+//       }
+//     }
+//   ]
+// });
+//
+// $("#usersTable").DataTable(config);
