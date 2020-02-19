b1c0850383c7d4239018a91e3e3756fea9dfd655
Josh Adam <josh.adam@canada.ca>
2020-02-19 15:07:43 -0600

    Updated comments on MarkdownViewer

--- src/main/webapp/resources/js/components/markdown/MarkdownViewer.jsx
+++ src/main/webapp/resources/js/components/markdown/MarkdownViewer.jsx
@@ -1,3 +1,6 @@
+/**
+ * @fileOverview Renders a component to view markdown.
+ */
 import React, { useState } from "react";
 import * as Showdown from "showdown";
 import styled from "styled-components";
@@ -15,9 +18,17 @@ export const markdownConverter = new Showdown.Converter({
   tasklists: true
 });
 
+/**
+ * Render markdown viewer to a react component.
+ * @type {Converter}
+ */
 export function MarkdownViewer({ markdown }) {
   const [value, setValue] = useState("");
 
+  /*
+  We can use `dangerouslySetInnerHTML` here since we know where the content of
+  the markdown is coming from.
+   */
   Promise.resolve(markdownConverter.makeHtml(markdown)).then(md => setValue(md));
   return <StyledViewer dangerouslySetInnerHTML={{ __html: value }} />;
 }
