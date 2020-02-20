3611183a5b981a594baf068381c38cd40427d260
Josh Adam <josh.adam@canada.ca>
2020-02-20 08:36:57 -0600

    Added comments to AnnouncementsTable.jsx

--- src/main/webapp/resources/js/pages/announcement/AnnouncementsTable.jsx
+++ src/main/webapp/resources/js/pages/announcement/AnnouncementsTable.jsx
@@ -1,3 +1,6 @@
+/**
+ * @fileOverview React component to render the announcements table.
+ */
 import React, { forwardRef, useContext, useImperativeHandle } from "react";
 import { setBaseUrl } from "../../utilities/url-utilities";
 import { dateColumnFormat } from "../../components/ant.design/table-renderers";
@@ -10,6 +13,11 @@ import {
 } from "../../components/ant.design/PagedTable";
 import { MarkdownViewer } from "../../components/markdown/MarkdownViewer";
 
+/**
+ * React component to render the announcements table.
+ * Requires a `ref` to be passed in so parent can update the table.
+ * @type {React.ForwardRefExoticComponent<React.PropsWithoutRef<{}> & React.RefAttributes<unknown>>}
+ */
 export const AnnouncementsTable = forwardRef((props, ref) => {
   const { updateTable } = useContext(PagedTableContext);
 
