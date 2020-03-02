313656a11ccf84b0a77f7b2ec8f9bfc822abf1bf
Josh Adam <josh.adam@canada.ca>
2020-03-02 14:21:08 -0600

    Updated edit icon

--- src/main/webapp/resources/js/pages/announcement/EditAnnouncement.jsx
+++ src/main/webapp/resources/js/pages/announcement/EditAnnouncement.jsx
@@ -3,6 +3,7 @@ import { EditOutlined } from "@ant-design/icons";
 import { Button, Modal } from "antd";
 import { MarkdownEditor } from "../../components/markdown/MarkdownEditor";
 import { FONT_COLOR_PRIMARY } from "../../styles/fonts";
+import { IconEdit } from "../../components/icons/Icons";
 
 /**
  * Render React component to edit an announcement.
@@ -22,7 +23,7 @@ export function EditAnnouncement({ announcement, updateAnnouncement }) {
   function displayModal() {
     Modal.confirm({
       title: i18n("EditAnnouncement.title"),
-      icon: <EditOutlined style={{ color: FONT_COLOR_PRIMARY }} />,
+      icon: <IconEdit style={{ color: FONT_COLOR_PRIMARY }} />,
       width: "80%",
       content: (
         <MarkdownEditor ref={markdownRef} markdown={announcement.message} />
