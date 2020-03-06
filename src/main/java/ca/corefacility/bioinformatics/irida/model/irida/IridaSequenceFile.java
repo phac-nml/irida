e96e98710399e507cae50e956ffbdd01f81e314d
Tom Matthews <thomas.matthews@canada.ca>
2020-03-06 12:34:40 -0600

    reused iridasequencefile for genomeassembly

--- src/main/java/ca/corefacility/bioinformatics/irida/model/irida/IridaSequenceFile.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/irida/IridaSequenceFile.java
@@ -1,24 +1,21 @@
 package ca.corefacility.bioinformatics.irida.model.irida;
 
-import java.nio.file.Path;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.nio.file.Path;
 
 /**
  * Describes fields that must be made available for a Sequence File in IRIDA
- * 
- *
  */
 public interface IridaSequenceFile {
 
 	/**
 	 * Get the location on the file system where the sequence file is stored
-	 * 
+	 *
 	 * @return the physical location of the file.
 	 */
 	public Path getFile();
-	
+
 	/**
 	 * Get the size of the file.
 	 *
@@ -26,17 +23,12 @@ public interface IridaSequenceFile {
 	 */
 	@JsonIgnore
 	public String getFileSize();
-	
+
 	/**
-	 * From
-	 * (http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-
-	 * into-human-readable-format-in-java)
+	 * From (http://stackoverflow.com/questions/3758606/how-to-convert-byte-size- into-human-readable-format-in-java)
 	 *
-	 * @param bytes
-	 *            The {@link Long} size of the file in bytes.
-	 * @param si
-	 *            {@link Boolean} true to use si units
-	 *
+	 * @param bytes The {@link Long} size of the file in bytes.
+	 * @param si    {@link Boolean} true to use si units
 	 * @return A human readable {@link String} representation of the file size.
 	 */
 	public static String humanReadableByteCount(long bytes, boolean si) {
