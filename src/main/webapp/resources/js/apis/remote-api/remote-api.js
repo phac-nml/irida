e12e1a4e25932a5bd9f057beb28cd9e29f63f39f
Josh Adam <josh.adam@canada.ca>
2020-03-06 10:40:39 -0600

    chore: update JSDoc

--- src/main/webapp/resources/js/apis/remote-api/remote-api.js
+++ src/main/webapp/resources/js/apis/remote-api/remote-api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * Check the status of a specific Remote API.
+ * @param {number} id - identifier for the API.
+ * @returns {Promise<boolean>}
+ */
 export function checkConnectionStatus({ id }) {
   return axios
     .get(`ajax/remote_api/status/${id}`)
