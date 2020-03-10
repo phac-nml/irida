2614a896f949c88e8c79837440bbdda64d990717
Tom Matthews <thomas.matthews@canada.ca>
2020-03-10 14:44:10 -0500

    added analysis rel where possible

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/samples/RESTSampleAssemblyController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/samples/RESTSampleAssemblyController.java
@@ -2,20 +2,25 @@ package ca.corefacility.bioinformatics.irida.web.controller.api.samples;
 
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.assembly.GenomeAssembly;
+import ca.corefacility.bioinformatics.irida.model.assembly.GenomeAssemblyFromAnalysis;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.SampleGenomeAssemblyJoin;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResourceCollection;
+import ca.corefacility.bioinformatics.irida.web.controller.api.RESTAnalysisSubmissionController;
 import ca.corefacility.bioinformatics.irida.web.controller.api.RESTGenericController;
 import ca.corefacility.bioinformatics.irida.web.controller.api.projects.RESTProjectSamplesController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -29,6 +34,7 @@ public class RESTSampleAssemblyController {
 
 	public static final String REL_SAMPLE = "sample";
 	public static final String REL_SAMPLE_ASSEMBLIES = "sample/assemblies";
+	public static final String REL_ASSEMBLY_ANALYSIS = "assembly/analysis";
 
 	private SampleService sampleService;
 
@@ -55,8 +61,8 @@ public class RESTSampleAssemblyController {
 		for (SampleGenomeAssemblyJoin join : assembliesForSample) {
 			GenomeAssembly genomeAssembly = join.getObject();
 
-			genomeAssembly.add(linkTo(methodOn(RESTSampleAssemblyController.class).readAssemblyForSample(sampleId,
-					genomeAssembly.getId())).withSelfRel());
+			genomeAssembly.add(getLinksForAssembly(genomeAssembly, sampleId));
+
 			assemblyResources.add(genomeAssembly);
 		}
 
@@ -98,11 +104,26 @@ public class RESTSampleAssemblyController {
 
 		GenomeAssembly genomeAssembly = genomeAssemblyOpt.get();
 
-		genomeAssembly.add(linkTo(methodOn(RESTSampleAssemblyController.class).readAssemblyForSample(sampleId,
-				assemblyId)).withSelfRel());
+		genomeAssembly.add(getLinksForAssembly(genomeAssembly, sampleId));
 
 		modelMap.addAttribute(RESTGenericController.RESOURCE_NAME, genomeAssembly);
 
 		return modelMap;
+	}
+
+	private Collection<Link> getLinksForAssembly(GenomeAssembly assembly, Long sampleId) {
+		List<Link> links = new ArrayList<>();
+		links.add(linkTo(methodOn(RESTSampleAssemblyController.class).readAssemblyForSample(sampleId,
+				assembly.getId())).withSelfRel());
+
+		if (assembly instanceof GenomeAssemblyFromAnalysis) {
+			GenomeAssemblyFromAnalysis analysisAssembly = (GenomeAssemblyFromAnalysis) assembly;
+
+			links.add(linkTo(methodOn(RESTAnalysisSubmissionController.class).getResource(
+					analysisAssembly.getAnalysisSubmission()
+							.getId())).withRel(REL_ASSEMBLY_ANALYSIS));
+		}
+
+		return links;
 	}
 }
