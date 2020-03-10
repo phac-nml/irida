9dacaec7682df471920edb93701f7c127bfe4007
Josh Adam <josh.adam@canada.ca>
2020-03-10 08:39:45 -0500

    style: Use pure links for password reset and activate

--- src/main/webapp/resources/js/pages/LoginPage.jsx
+++ src/main/webapp/resources/js/pages/LoginPage.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import { Alert, Button, Form, Input } from "antd";
-import { blue6, grey1 } from "../styles/colors";
+import { blue6 } from "../styles/colors";
 import { IconLocked, IconUser } from "../components/icons/Icons";
 
-function LoginPage({ form }) {
+function LoginPage() {
+  const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
 
-  const { getFieldDecorator, getFieldsError } = form;
-  const hasErrors = ((fieldsError = getFieldsError()) =>
-    Object.keys(fieldsError).some(field => fieldsError[field]))();
-
-  function handleSubmit() {
-    if (!hasErrors) {
-      setLoading(true);
-      document.forms["loginForm"].submit();
-    }
-  }
+  const onFinish = values => {
+    console.log(values);
+  };
 
   return (
     <>
-      <img src="/resources/img/irida_logo_light.svg" alt="" />
+      <img
+        src="/resources/img/irida_logo_light.svg"
+        alt=""
+        style={{ marginBottom: 10 }}
+      />
       {window.PAGE?.hasErrors ? (
         <Alert
           style={{ margin: `18px 0 28px 0` }}
@@ -37,42 +35,36 @@ function LoginPage({ form }) {
       ) : null}
 
       <Form
+        form={form}
+        // onFinish={onFinish}
         action={`${window.PAGE.BASE_URL}login`}
         name="loginForm"
         method="POST"
       >
-        <Form.Item label={i18n("LoginPage.username")}>
-          {getFieldDecorator("username", {
-            rules: [
-              {
-                required: true,
-                message: i18n("LoginPage.username.required")
-              }
-            ]
-          })(
-            <Input
-              name="username"
-              style={{ backgroundColor: grey1 }}
-              prefix={<IconUser style={{ color: blue6 }} />}
-            />
-          )}
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: i18n("LoginPage.username.required")
+            }
+          ]}
+        >
+          <Input prefix={<IconUser style={{ color: blue6 }} />} />
         </Form.Item>
-        <Form.Item label={i18n("LoginPage.password")}>
-          {getFieldDecorator("password", {
-            rules: [
-              {
-                required: true,
-                message: i18n("LoginPage.password.required")
-              }
-            ]
-          })(
-            <Input
-              name="password"
-              style={{ backgroundColor: grey1 }}
-              prefix={<IconLocked style={{ color: blue6 }} />}
-              type="password"
-            />
-          )}
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: i18n("LoginPage.password.required")
+            }
+          ]}
+        >
+          <Input
+            prefix={<IconLocked style={{ color: blue6 }} />}
+            type="password"
+          />
         </Form.Item>
         <Form.Item>
           <Button
@@ -81,29 +73,29 @@ function LoginPage({ form }) {
             block
             loading={loading}
             htmlType="submit"
-            disabled={hasErrors}
-            onClick={handleSubmit}
           >
             {i18n("LoginPage.submit")}
           </Button>
         </Form.Item>
-        <Form.Item style={{ display: "flex", justifyContent: "space-between" }}>
-          <Button type="link" href={`${window.PAGE.BASE_URL}password_reset`}>
-            {i18n("LoginPage.forgot")}
-          </Button>
-
-          <Button
-            type="link"
-            href={`${window.PAGE.BASE_URL}password_reset/activate`}
+        <Form.Item>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between"
+            }}
           >
-            {i18n("LoginPage.activate")}
-          </Button>
+            <a type="link" href={`${window.PAGE.BASE_URL}password_reset`}>
+              {i18n("LoginPage.forgot")}
+            </a>
+
+            <a href={`${window.PAGE.BASE_URL}password_reset/activate`}>
+              {i18n("LoginPage.activate")}
+            </a>
+          </div>
         </Form.Item>
       </Form>
     </>
   );
 }
 
-const WrappedLoginForm = Form.create({ name: "loginForm" })(LoginPage);
-
-render(<WrappedLoginForm />, document.querySelector("#login-root"));
+render(<LoginPage />, document.querySelector("#login-root"));
