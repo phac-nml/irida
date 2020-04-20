251a35964fc22763f4e3de71918a2022a7b276e6
Josh Adam <josh.adam@canada.ca>
2020-04-20 07:46:27 -0500

    💡️ Update Javadoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/settings/ProjectDetailsAjaxController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/settings/ProjectDetailsAjaxController.java
@@ -1,6 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.web.projects.settings;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,25 +13,44 @@ import ca.corefacility.bioinformatics.irida.ria.web.projects.settings.dto.Projec
 import ca.corefacility.bioinformatics.irida.ria.web.projects.settings.dto.UpdateProjectAttributeRequest;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 
+/**
+ * Handle asynchronous requests for the UI project details page.
+ */
 @RestController
 @RequestMapping("/ajax/projects/{projectId}/details")
 public class ProjectDetailsAjaxController {
 	private final ProjectService projectService;
+	private final MessageSource messageSource;
 
 	@Autowired
-	public ProjectDetailsAjaxController(ProjectService projectService) {
+	public ProjectDetailsAjaxController(ProjectService projectService, MessageSource messageSource) {
 		this.projectService = projectService;
+		this.messageSource = messageSource;
 	}
 
+	/**
+	 * Get general details about the project.
+	 *
+	 * @param projectId {@link Long} identifier for the project
+	 * @return {@link ResponseEntity} containing the project details
+	 */
 	@RequestMapping("")
 	public ResponseEntity<ProjectDetailsResponse> getProjectDetails(@PathVariable Long projectId) {
 		Project project = projectService.read(projectId);
 		return ResponseEntity.ok(new ProjectDetailsResponse(project));
 	}
 
+	/**
+	 * Update a field within the project details.
+	 *
+	 * @param projectId {@link Long} identifier for the project
+	 * @param request   {@link UpdateProjectAttributeRequest} details about which field to update
+	 * @param locale    {@link Locale} for the currently logged in user
+	 * @return {@link ResponseEntity} explaining to the user the results of the update.
+	 */
 	@RequestMapping(value = "/edit", method = RequestMethod.PUT)
 	public ResponseEntity<String> updateProjectDetails(@PathVariable Long projectId,
-			@RequestBody UpdateProjectAttributeRequest request) {
+			@RequestBody UpdateProjectAttributeRequest request, Locale locale) {
 		Project project = projectService.read(projectId);
 		switch (request.getField()) {
 		case "label":
@@ -42,9 +64,11 @@ public class ProjectDetailsAjaxController {
 			break;
 		default:
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-					.body("ERROR");
+					.body(messageSource.getMessage("server.ProjectDetails.error", new Object[] { request.getField() },
+							locale));
 		}
 		projectService.update(project);
-		return ResponseEntity.ok("SUCCESS");
+		return ResponseEntity.ok(messageSource.getMessage("server.ProjectDetails.success",
+				new Object[] { request.getField(), request.getValue() }, locale));
 	}
 }
