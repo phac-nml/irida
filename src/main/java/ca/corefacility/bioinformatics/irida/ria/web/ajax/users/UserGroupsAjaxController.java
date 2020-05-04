9d85173dd52acba8c7c8ffd9f7f38d096a7294d1
Josh Adam <josh.adam@canada.ca>
2020-05-04 09:28:38 -0500

    :bulb: Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/users/UserGroupsAjaxController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/users/UserGroupsAjaxController.java
@@ -10,6 +10,9 @@ import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableRequest;
 import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableResponse;
 import ca.corefacility.bioinformatics.irida.ria.web.services.UIUserGroupsService;
 
+/**
+ * Controller for asynchronous request for User Groups
+ */
 @RestController
 @RequestMapping("/ajax/user-groups")
 public class UserGroupsAjaxController {
@@ -19,11 +22,24 @@ public class UserGroupsAjaxController {
 		this.service = userGroupService;
 	}
 
+	/**
+	 * Gat a paged list of user groups
+	 *
+	 * @param request details about the current table page
+	 * @return {@link TableResponse} for the current page of user groups
+	 */
 	@RequestMapping("/list")
 	public ResponseEntity<TableResponse<UserGroupTableModel>> getUserGroups(@RequestBody TableRequest request) {
 		return ResponseEntity.ok(service.getUserGroups(request));
 	}
 
+	/**
+	 * Delete a specific user group
+	 *
+	 * @param id     Identifier for the user group to delete
+	 * @param locale Current users locale
+	 * @return Message to user about what happened
+	 */
 	@RequestMapping(value = "", method = RequestMethod.DELETE)
 	public ResponseEntity<String> deleteUserGroups(@RequestParam Long id, Locale locale) {
 		return ResponseEntity.ok(service.deleteUserGroup(id, locale));
