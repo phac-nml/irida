31097b5434c3501e80534cf5e989b0a506807a79
Josh Adam <josh.adam@canada.ca>
2020-05-04 09:31:18 -0500

    :bulb: Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIUserGroupsService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIUserGroupsService.java
@@ -22,6 +22,9 @@ import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableRequest;
 import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableResponse;
 import ca.corefacility.bioinformatics.irida.service.user.UserGroupService;
 
+/**
+ * Service class for the UI for handling {@link UserGroup}s
+ */
 @Component
 public class UIUserGroupsService {
 	private UserGroupService userGroupService;
@@ -37,6 +40,12 @@ public class UIUserGroupsService {
 		this.messageSource = messageSource;
 	}
 
+	/**
+	 * Gat a paged list of user groups
+	 *
+	 * @param request details about the current table page
+	 * @return {@link TableResponse} for the current page of user groups
+	 */
 	public TableResponse<UserGroupTableModel> getUserGroups(TableRequest request) {
 		Page<UserGroup> pagedGroups = userGroupService.search(
 				UserGroupSpecification.searchUserGroup(request.getSearch()),
@@ -53,12 +62,26 @@ public class UIUserGroupsService {
 		return new TableResponse<>(groups, pagedGroups.getTotalElements());
 	}
 
+	/**
+	 * Delete a specific user group
+	 *
+	 * @param id     Identifier for the user group to delete
+	 * @param locale Current users locale
+	 * @return Message to user about what happened
+	 */
 	public String deleteUserGroup(Long id, Locale locale) {
 		UserGroup group = userGroupService.read(id);
 		userGroupService.delete(id);
 		return messageSource.getMessage("server.usergroups.delete-success", new Object[] { group.getLabel() }, locale);
 	}
 
+	/**
+	 * Determine if the current user is the owner of the current group.
+	 *
+	 * @param user  currently logged in user
+	 * @param group {@link UserGroup} to see if user is the owner
+	 * @return {@link Boolean} true if the owner of the group
+	 */
 	private boolean isGroupOwner(User user, UserGroup group) {
 		Collection<UserGroupJoin> groupUsers = userGroupService.getUsersForGroup(group);
 		UserGroupJoin currentUserJoin = groupUsers.stream()
