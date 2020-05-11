9be135d4d6f313ecb814a96bad5973a8d0ef48ad
Josh Adam <josh.adam@canada.ca>
2020-05-11 08:38:38 -0500

    Updated JavaDoc for UserGroupProjectTableModel

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/UserGroupProjectTableModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/UserGroupProjectTableModel.java
@@ -3,6 +3,9 @@ package ca.corefacility.bioinformatics.irida.ria.web.ajax.dto;
 import ca.corefacility.bioinformatics.irida.model.user.group.UserGroupProjectJoin;
 import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableModel;
 
+/**
+ * Used to send information about a project in a user group to the UI.
+ */
 public class UserGroupProjectTableModel extends TableModel {
 	private final String role;
 
