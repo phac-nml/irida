8f6b0dd6799c26f4134ad0e27f3ec7b8b57fe07c
Josh Adam <josh.adam@canada.ca>
2020-05-11 08:31:30 -0500

    Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/FieldUpdate.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/FieldUpdate.java
@@ -1,7 +1,17 @@
 package ca.corefacility.bioinformatics.irida.ria.web.ajax.dto;
 
+/**
+ * Used by UI when any field needs to have its value updated.
+ */
 public class FieldUpdate {
+	/*
+	Attribute on the object to update
+	 */
 	private String field;
+
+	/*
+	New value to set
+	 */
 	private String value;
 
 	public String getField() {
