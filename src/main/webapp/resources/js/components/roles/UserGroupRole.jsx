3510322c2098dc93241b530c73e1c9381928fed6
Josh Adam <josh.adam@canada.ca>
2020-05-12 12:40:00 -0500

    Updated JSDoc on UserGroupRole.jsx

--- src/main/webapp/resources/js/components/roles/UserGroupRole.jsx
+++ src/main/webapp/resources/js/components/roles/UserGroupRole.jsx
@@ -3,11 +3,24 @@ import { UserGroupRolesContext } from "../../contexts/UserGroupRolesContext";
 import { notification, Select } from "antd";
 import { updateUserRoleOnUserGroups } from "../../apis/users/groups";
 
+/**
+ * React component to either display the users role, or to provide an input
+ * for updating User Group roles if the current use can manage them.
+ * @param {object} user - the user the roles are for
+ * @param {boolean} canManage - whether the current user can manage this user
+ * @param {number} groupId - identifier for the user group
+ * @returns {*}
+ * @constructor
+ */
 export function UserGroupRole({ user, canManage, groupId }) {
   const [role, setRole] = useState(user.role);
   const [loading, setLoading] = useState(false);
   const { roles, getRoleFromKey } = useContext(UserGroupRolesContext);
 
+  /**
+   * When the role changes update the server to keep the state.
+   * @param value
+   */
   const onChange = (value) => {
     if (value !== role) {
       setLoading(true);
