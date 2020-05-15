6aaade5070ee5e27891d8a960f28af3ae0dc7316
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2020-05-15 14:30:43 -0500

    switched to installing blend4j via jar file instead of maven repo

--- lib/install-libs.sh
+++ lib/install-libs.sh
@@ -3,3 +3,4 @@
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=jbzip2-0.9.jar -DgroupId=org.itadaki.bzip2 -DartifactId=jbzip2 -Dversion=0.9 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=sam-1.103.jar -DgroupId=net.sf.samtools -DartifactId=samtools -Dversion=1.103 -Dpackaging=jar -DgeneratePom=true -DcreateChecksum=true
 mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=fastqc-0.11.7.jar -Dsources=fastqc-0.11.7-src.jar -DpomFile=fastqc-0.11.7.pom -DcreateChecksum=true
+mvn --settings maven-central-secure-settings.xml install:install-file -Dfile=blend4j-0.2.1-2201df9.jar -DpomFile=blend4j-0.2.1-2201df9.pom -DcreateChecksum=true
