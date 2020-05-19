4e505d4682bf1207bfc125dcf129699049dfe1bf
Josh Adam <josh.adam@canada.ca>
2020-05-19 11:07:21 -0500

    Reverted to originial, should not have been modified

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/security/LoginSuccessHandler.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/security/LoginSuccessHandler.java
@@ -1,26 +1,23 @@
 package ca.corefacility.bioinformatics.irida.ria.security;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.Locale;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import ca.corefacility.bioinformatics.irida.model.user.User;
+import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
+import com.timgroup.jgravatar.Gravatar;
+import com.timgroup.jgravatar.GravatarDefaultImage;
+import com.timgroup.jgravatar.GravatarRating;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.web.servlet.LocaleResolver;
 
-import ca.corefacility.bioinformatics.irida.model.user.User;
-import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
-
-import com.timgroup.jgravatar.Gravatar;
-import com.timgroup.jgravatar.GravatarDefaultImage;
-import com.timgroup.jgravatar.GravatarRating;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Handles actions for when a user is successfully logged in.
@@ -38,7 +35,7 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
 		this.userRepository = userRepository;
 		this.localeResolver = localeResolver;
 	}
-	
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
 			Authentication authentication) throws IOException, ServletException {
