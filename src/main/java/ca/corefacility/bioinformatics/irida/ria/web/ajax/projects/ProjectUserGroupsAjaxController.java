905296f47ae4ba143de97afb43f3b228b88d2824
Josh Adam <josh.adam@canada.ca>
2020-05-20 13:51:24 -0500

    Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/projects/ProjectUserGroupsAjaxController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/projects/ProjectUserGroupsAjaxController.java
@@ -26,30 +26,69 @@ public class ProjectUserGroupsAjaxController {
 		this.service = service;
 	}
 
+	/**
+	 * Get a table page of {@link ProjectUserGroupsTableModel}
+	 *
+	 * @param projectId Identifier for a project
+	 * @param request   {@link TableRequest} details about the current page of the table
+	 * @return {@link TableResponse}
+	 */
 	@RequestMapping("")
 	public ResponseEntity<TableResponse<ProjectUserGroupsTableModel>> getProjectUserGroups(@PathVariable Long projectId,
 			@RequestBody TableRequest request) {
 		return ResponseEntity.ok(service.getUserGroupsForProject(projectId, request));
 	}
 
+	/**
+	 * Remove a user group from a project
+	 *
+	 * @param projectId Identifier for a project
+	 * @param groupId   Identifier for an user group
+	 * @param locale    current users locale
+	 * @return message to user about the result of removing the user group
+	 */
 	@RequestMapping(value = "", method = RequestMethod.DELETE)
 	public ResponseEntity<String> removeUserGroupFromProject(@PathVariable long projectId, @RequestParam long groupId,
 			Locale locale) {
 		return ResponseEntity.ok(service.removeUserGroupFromProject(projectId, groupId, locale));
 	}
 
+	/**
+	 * Get a list of user groups that are not on the current project
+	 *
+	 * @param projectId Identifier for the current project
+	 * @param query     Filter string to search the existing user groups by
+	 * @return List of user groups
+	 */
 	@RequestMapping("/available")
 	public ResponseEntity<List<UserGroup>> getAvailableUserGroupsForProject(@PathVariable Long projectId,
 			@RequestParam String query) {
 		return ResponseEntity.ok(service.getAvailableUserGroupsForProject(projectId, query));
 	}
 
+	/**
+	 * Add a user group to the current project
+	 *
+	 * @param projectId Identifier for a project
+	 * @param request   Identifier for an user group
+	 * @param locale    Current users locale
+	 * @return message to user about the outcome of adding the user group to the project
+	 */
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public ResponseEntity<String> addUserGroupToProject(@PathVariable Long projectId,
 			@RequestBody NewMemberRequest request, Locale locale) {
 		return ResponseEntity.ok(service.addUserGroupToProject(projectId, request, locale));
 	}
 
+	/**
+	 * Update the project role of a user group on the current project
+	 *
+	 * @param projectId Identifier for a project
+	 * @param id        Identifier for an user group
+	 * @param role      Role to update the user group to
+	 * @param locale    Current users locale
+	 * @return message to user about the result of the update
+	 */
 	@RequestMapping(value = "/role", method = RequestMethod.PUT)
 	public ResponseEntity<String> updateUserGroupRoleOnProject(@PathVariable Long projectId, @RequestParam Long id,
 			@RequestParam String role, Locale locale) {
--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIProjectUserGroupsService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIProjectUserGroupsService.java
@@ -20,6 +20,9 @@ import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableResponse;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.user.UserGroupService;
 
+/**
+ * UI Service to handle requests for project user groups.
+ */
 @Component
 public class UIProjectUserGroupsService {
 	private final ProjectService projectService;
@@ -33,15 +36,33 @@ public class UIProjectUserGroupsService {
 		this.messageSource = messageSource;
 	}
 
+	/**
+	 * Get a table page of {@link ProjectUserGroupsTableModel}
+	 *
+	 * @param projectId Identifier for a {@link Project}
+	 * @param request   {@link TableRequest} details about the current page of the table
+	 * @return {@link TableResponse}
+	 */
 	public TableResponse<ProjectUserGroupsTableModel> getUserGroupsForProject(Long projectId, TableRequest request) {
 		Project project = projectService.read(projectId);
 		Page<UserGroupProjectJoin> userGroupJoins = userGroupService.getUserGroupsForProject(request.getSearch(),
 				project, request.getCurrent(), request.getPageSize(), request.getSort());
-		List<ProjectUserGroupsTableModel> groups = userGroupJoins.getContent().stream().map(j -> new ProjectUserGroupsTableModel(j.getObject(), j.getProjectRole()
-				.toString(), j.getCreatedDate())).collect(Collectors.toList());
+		List<ProjectUserGroupsTableModel> groups = userGroupJoins.getContent()
+				.stream()
+				.map(j -> new ProjectUserGroupsTableModel(j.getObject(), j.getProjectRole()
+						.toString(), j.getCreatedDate()))
+				.collect(Collectors.toList());
 		return new TableResponse<>(groups, userGroupJoins.getTotalElements());
 	}
 
+	/**
+	 * Remove a {@link UserGroup} from a {@link Project}
+	 *
+	 * @param projectId Identifier for a {@link Project}
+	 * @param groupId   Identifier for an {@link UserGroup}
+	 * @param locale    current users {@link Locale}
+	 * @return message to user about the result of removing the user group
+	 */
 	public String removeUserGroupFromProject(long projectId, long groupId, Locale locale) {
 		Project project = projectService.read(projectId);
 		UserGroup group = userGroupService.read(groupId);
@@ -55,11 +76,26 @@ public class UIProjectUserGroupsService {
 		}
 	}
 
+	/**
+	 * Get a list of {@link UserGroup}s that are not on the current {@link Project}
+	 *
+	 * @param projectId Identifier for the current {@link Project}
+	 * @param query     Filter string to search the existing {@link UserGroup}s by
+	 * @return List of {@link UserGroup}
+	 */
 	public List<UserGroup> getAvailableUserGroupsForProject(Long projectId, String query) {
 		Project project = projectService.read(projectId);
 		return userGroupService.getUserGroupsNotOnProject(project, query);
 	}
 
+	/**
+	 * Add a {@link UserGroup} to the current {@link Project}
+	 *
+	 * @param projectId Identifier for a {@link Project}
+	 * @param request   Identifier for an {@link UserGroup}
+	 * @param locale    Current users {@link Locale}
+	 * @return message to user about the outcome of adding the user group to the project
+	 */
 	public String addUserGroupToProject(Long projectId, NewMemberRequest request, Locale locale) {
 		Project project = projectService.read(projectId);
 		UserGroup group = userGroupService.read(request.getId());
@@ -68,6 +104,16 @@ public class UIProjectUserGroupsService {
 		return messageSource.getMessage("server.usergroups.add-group", new Object[] { group.getLabel() }, locale);
 	}
 
+	/**
+	 * Update the {@link ProjectRole} of a {@link UserGroup} on the current {@link Project}
+	 *
+	 * @param projectId Identifier for a {@link Project}
+	 * @param groupId   Identifier for an {@link UserGroup}
+	 * @param role      Role to update the user group to
+	 * @param locale    Current users {@link Locale}
+	 * @return message to user about the result of the update
+	 * @throws ProjectWithoutOwnerException thrown when updating the role will result in the project to have no owner
+	 */
 	public String updateUserGroupRoleOnProject(Long projectId, Long groupId, String role, Locale locale)
 			throws ProjectWithoutOwnerException {
 		Project project = projectService.read(projectId);
@@ -80,9 +126,8 @@ public class UIProjectUserGroupsService {
 			return messageSource.getMessage("server.usergroups.update-role.success",
 					new Object[] { group.getLabel(), roleString }, locale);
 		} catch (ProjectWithoutOwnerException e) {
-			throw new ProjectWithoutOwnerException(
-					messageSource.getMessage("server.usergroups.update-role.error", new Object[] { group.getLabel() },
-							locale));
+			throw new ProjectWithoutOwnerException(messageSource.getMessage("server.usergroups.update-role.error", new Object[] { group.getLabel() },
+					locale));
 		}
 	}
 }
