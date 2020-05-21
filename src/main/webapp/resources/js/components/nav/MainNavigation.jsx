5b3ce442de4b779393612441741ddbd69717e143
Josh Adam <josh.adam@canada.ca>
2020-05-21 15:21:36 -0500

    Added links for analyses

--- src/main/webapp/resources/js/components/nav/MainNavigation.jsx
+++ src/main/webapp/resources/js/components/nav/MainNavigation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Input, Menu } from "antd";
 import { IconFolder, IconLogout, IconUser } from "../icons/Icons";
 import { setBaseUrl } from "../../utilities/url-utilities";
-import { grey4 } from "../../styles/colors";
+import { grey10, grey4 } from "../../styles/colors";
 import { SPACE_MD, SPACE_XS } from "../../styles/spacing";
 
 const { Item, ItemGroup, SubMenu } = Menu;
@@ -57,6 +57,24 @@ export function MainNavigation({}) {
             </ItemGroup>
           ) : null}
         </SubMenu>
+        <SubMenu title={<><IconFolder /> {i18n("nav.main.analysis")}</>}>
+          <Item key="user:analysis">
+            <a href={setBaseUrl(`/analysis`)}>{i18n("nav.main.analysis-admin-user")}</a>
+          </Item>
+          {window.TL._USER.systemRole === "ROLE_ADMIN" ? (
+            <ItemGroup title="Admin">
+              <Item key={"admin:analyses"}>
+                <a href={setBaseUrl(`/analysis/all`)}>
+                  {i18n("nav.main.analysis-admin-all")}
+                </a>
+              </Item>
+            </ItemGroup>
+          ) : null}
+          <Menu.Divider />
+          <Item key="analysis:outputs">
+            <a href={setBaseUrl(`/analysis/user/analysis-outputs`)}>{i18n("Analysis.outputFiles")}</a>
+          </Item>
+        </SubMenu>
       </Menu>
       <Menu mode="horizontal" theme="dark">
         <SubMenu
@@ -64,7 +82,7 @@ export function MainNavigation({}) {
             <>
               <Avatar
                 size="small"
-                style={{ backgroundColor: grey4, marginRight: SPACE_XS }}
+                style={{ backgroundColor: grey4, color: grey10, marginRight: SPACE_XS }}
                 icon={<IconUser />}
               />
               {`${window.TL._USER.firstName} ${window.TL._USER.lastName}`}
