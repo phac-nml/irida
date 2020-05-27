8f492e261522a583ba1ed564df35ea80ef701448
Josh Adam <josh.adam@canada.ca>
2020-05-27 13:10:17 -0500

    Working on adding new fields dynamically

--- src/main/webapp/resources/js/pages/metadata/templates/details/Template.jsx
+++ src/main/webapp/resources/js/pages/metadata/templates/details/Template.jsx
@@ -1,66 +1,80 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useMetadataTemplate } from "../../../../contexts/metadata-template-context";
 import { Button, Col, Row, Space, Table } from "antd";
+
+import { HotTable } from "@handsontable/react";
+import Handsontable from "handsontable";
+import "handsontable/dist/handsontable.css";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_FIELD":
+      return {
+        ...state,
+        headers: [...state.headers].push("FOOBAR"),
+        columns: [...state.columns].push({ data: "DS" }),
+      };
+  }
+};
 
 export function Template() {
   const { template } = useMetadataTemplate();
   const { fields } = template;
+  const hotRef = useRef();
 
+  const [colHeaders, setColHeaders] = useState([
+    "ID",
+    "First Name",
+    "Last Name",
+    "Address",
+  ]);
   const [columns, setColumns] = useState([
-    {
-      title: "Address",
-      children: [
-        {
-          title: "Street",
-          dataIndex: "street",
-        },
-        {
-          title: "City",
-          dataIndex: "city",
-        },
-      ],
-    },
-    {
-      title: "What Pipeline?",
-      dataIndex: "p0",
-      children: [],
-    },
-    {
-      title: "Another Pipeline",
-      dataIndex: "p1",
-    },
+    { data: "id" },
+    { data: "name.first" },
+    { data: "name.last" },
+    { data: "address" },
   ]);
 
-  const addNewGroup = () => {
-    const c = [...columns];
-    c.push({
-      title: "FOOBAR",
-    });
-    setColumns(c);
+  const settings = {
+    width: "100%",
+    colWidths: 100,
+    rowHeights: 50,
+    licenseKey: "non-commercial-and-evaluation",
+    data: [],
+    dataSchema: { id: null, name: { first: null, last: null }, address: null },
+    colHeaders,
+    columns,
+    minSpareRows: 0,
+    rowHeaders: true,
   };
 
-  const addField = () => {
-    const c = [...columns];
-    c.find((i) => i.dataIndex === "p0").children.push({ title: "ADDED" });
-    setColumns(c);
+  const addNewGroup = () => {
+    const t = [...colHeaders];
+    t.push("FOOBAR");
+    setColHeaders(t);
+    const s = [...columns];
+    s.push({ data: "foobar" });
+    setColumns(s);
   };
 
   return (
-    <>
-      <Space direction="vertical">
-        <Space>
-          <Button onClick={addNewGroup}>New Field Group</Button>
-          <Button onClick={addField}>New Field</Button>
-        </Space>
-        <Table
-          style={{ width: 900 }}
-          scroll={{ x: true }}
-          bordered
-          columns={columns}
-          dataSource={[]}
-          size="middle"
-        />
-      </Space>
-    </>
+    <div
+      className="hot handsontable htRowHeaders htColumnHeaders"
+      style={{ width: "100%", overflow: "hidden", border: `1px solid blue` }}
+    >
+      <Button onClick={addNewGroup}>New Field Group</Button>
+      <HotTable
+        width="100%"
+        id="net-template"
+        ref={hotRef}
+        settings={settings}
+      />
+      {/*<Space direction="vertical">*/}
+      {/*  <Space>*/}
+      {/*  </Space>*/}
+      {/*  <div style={{ width: 800 }}>*/}
+      {/*  </div>*/}
+      {/*</Space>*/}
+    </div>
   );
 }
