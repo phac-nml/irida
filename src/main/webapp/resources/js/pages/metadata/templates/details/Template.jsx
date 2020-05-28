1841b2db2de644d15a4dd0df785da2907ebf4db4
Josh Adam <josh.adam@canada.ca>
2020-05-28 12:54:10 -0500

    :construction: Creating a template using Handsontable

--- src/main/webapp/resources/js/pages/metadata/templates/details/Template.jsx
+++ src/main/webapp/resources/js/pages/metadata/templates/details/Template.jsx
@@ -1,80 +1,67 @@
 import React, { useRef, useState } from "react";
 import { useMetadataTemplate } from "../../../../contexts/metadata-template-context";
-import { Button, Col, Row, Space, Table } from "antd";
+import { Button } from "antd";
 
 import { HotTable } from "@handsontable/react";
-import Handsontable from "handsontable";
 import "handsontable/dist/handsontable.css";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_FIELD":
-      return {
-        ...state,
-        headers: [...state.headers].push("FOOBAR"),
-        columns: [...state.columns].push({ data: "DS" }),
-      };
-  }
-};
+import { SPACE_SM } from "../../../../styles/spacing";
 
 export function Template() {
   const { template } = useMetadataTemplate();
-  const { fields } = template;
+  const [fields, setFields] = useState(template.fields.map((f) => f.label));
   const hotRef = useRef();
 
-  const [colHeaders, setColHeaders] = useState([
-    "ID",
-    "First Name",
-    "Last Name",
-    "Address",
-  ]);
-  const [columns, setColumns] = useState([
-    { data: "id" },
-    { data: "name.first" },
-    { data: "name.last" },
-    { data: "address" },
-  ]);
-
   const settings = {
+    data: [fields],
     width: "100%",
-    colWidths: 100,
-    rowHeights: 50,
+    colWidths: 140,
     licenseKey: "non-commercial-and-evaluation",
-    data: [],
-    dataSchema: { id: null, name: { first: null, last: null }, address: null },
-    colHeaders,
-    columns,
     minSpareRows: 0,
-    rowHeaders: true,
+    colHeaders: true,
+    manualColumnMove: true,
+    allowRemoveColumn: true,
+    allowInsertColumn: true,
+    contextMenu: {
+      items: {
+        col_left: {
+          disabled: false,
+        },
+        col_right: {
+          disabled: false,
+        },
+        remove_col: {
+          name: "Remove Field",
+          disabled: false,
+          callback(key, selections) {
+            const f = [...fields];
+            selections.forEach((s) => {
+              f.splice(s.start.col, 1);
+            });
+            setFields(f);
+          },
+        },
+      },
+    },
+    stretchH: "all",
   };
 
-  const addNewGroup = () => {
-    const t = [...colHeaders];
-    t.push("FOOBAR");
-    setColHeaders(t);
-    const s = [...columns];
-    s.push({ data: "foobar" });
-    setColumns(s);
+  const getFields = () => {
+    console.log(hotRef.current.hotInstance.getDataAtRow(0));
   };
 
   return (
     <div
-      className="hot handsontable htRowHeaders htColumnHeaders"
-      style={{ width: "100%", overflow: "hidden", border: `1px solid blue` }}
+      className="hot handsontable htColumnHeaders"
+      style={{
+        width: "100%",
+        overflow: "hidden",
+        border: `1px solid transparent`,
+      }}
     >
-      <Button onClick={addNewGroup}>New Field Group</Button>
-      <HotTable
-        width="100%"
-        id="net-template"
-        ref={hotRef}
-        settings={settings}
-      />
-      {/*<Space direction="vertical">*/}
-      {/*  <Space>*/}
-      {/*  </Space>*/}
-      {/*  <div style={{ width: 800 }}>*/}
-      {/*  </div>*/}
-      {/*</Space>*/}
+      <div style={{ marginBottom: SPACE_SM }}>
+        <Button onClick={getFields}>New Field Group</Button>
+      </div>
+      <HotTable ref={hotRef} settings={settings} />
     </div>
   );
 }
