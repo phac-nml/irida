25ed4694faffa523c1539d04b64d0b58ad394d41
Josh Adam <josh.adam@canada.ca>
2020-05-29 11:20:04 -0500

    Cleaned up layout.

--- src/main/webapp/resources/js/pages/projects/metadata-templates/index.js
+++ src/main/webapp/resources/js/pages/projects/metadata-templates/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import { getProjectMetadataTemplates } from "../../../apis/metadata/metadata-templates";
-import { Avatar, Button, Col, List, Row, Tag, Typography } from "antd";
+import { Avatar, Button, List, Space, Tag, Typography } from "antd";
 import {
   IconDownloadFile,
   IconMetadataTemplate,
@@ -12,27 +12,37 @@ const { Title } = Typography;
 
 function ProjectMetadataTemplates() {
   const [templates, setTemplates] = useState([]);
+  const BASE_URL = setBaseUrl(
+    `/projects/${window.project.id}/metadata-templates`
+  );
 
   useEffect(() => {
-    getProjectMetadataTemplates(4).then((data) => setTemplates(data));
+    getProjectMetadataTemplates(window.project.id).then((data) =>
+      setTemplates(data)
+    );
   }, []);
 
   return (
-    <Row>
-      <Col xl={18}>
+    <div>
+      <Space direction="vertical" style={{ width: "100%" }}>
         <Title level={2}>{i18n("ProjectMetadataTemplates.title")}</Title>
+        <Space>
+          <Button href={`${BASE_URL}/new`} className="t-create-template-btn">
+            Create New Template
+          </Button>
+        </Space>
         <List
+          bordered
           itemLayout="horizontal"
           dataSource={templates}
           renderItem={(item) => (
             <List.Item
+              className="t-template"
               actions={[
                 <Button
                   shape="circle"
                   icon={<IconDownloadFile />}
-                  href={setBaseUrl(
-                    `/projects/${window.project.id}/metadata-templates/${item.id}/excel`
-                  )}
+                  href={`${BASE_URL}/${item.id}/excel`}
                   key="list-download"
                 />,
               ]}
@@ -41,7 +51,10 @@ function ProjectMetadataTemplates() {
                 avatar={<Avatar icon={<IconMetadataTemplate />} />}
                 title={
                   <div
-                    style={{ display: "flex", justifyContent: "space-between" }}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                    }}
                   >
                     <a
                       href={setBaseUrl(
@@ -60,8 +73,8 @@ function ProjectMetadataTemplates() {
             </List.Item>
           )}
         />
-      </Col>
-    </Row>
+      </Space>
+    </div>
   );
 }
 
