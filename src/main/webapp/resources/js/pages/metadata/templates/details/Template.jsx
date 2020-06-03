8f5cf58e78772e0c6369bf516d350a82be12b2f3
Josh Adam <josh.adam@canada.ca>
2020-06-03 09:13:07 -0500

    :heavy_minus_sign: Removed handsontable

--- src/main/webapp/resources/js/pages/metadata/templates/details/Template.jsx
+++ src/main/webapp/resources/js/pages/metadata/templates/details/Template.jsx
@@ -1,66 +1,17 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useMetadataTemplate } from "../../../../contexts/metadata-template-context";
 import { Button } from "antd";
 
-import { HotTable } from "@handsontable/react";
-import "handsontable/dist/handsontable.css";
 import { SPACE_SM } from "../../../../styles/spacing";
 
 export function Template() {
   const { template } = useMetadataTemplate();
   const [fields, setFields] = useState(template.fields.map((f) => f.label));
-  const hotRef = useRef();
 
-  const settings = {
-    data: [fields],
-    width: "100%",
-    colWidths: 140,
-    licenseKey: "non-commercial-and-evaluation",
-    minSpareRows: 0,
-    colHeaders: true,
-    manualColumnMove: true,
-    allowRemoveColumn: true,
-    allowInsertColumn: true,
-    contextMenu: {
-      items: {
-        col_left: {
-          disabled: false,
-        },
-        col_right: {
-          disabled: false,
-        },
-        remove_col: {
-          name: "Remove Field",
-          disabled: false,
-          callback(key, selections) {
-            const f = [...fields];
-            selections.forEach((s) => {
-              f.splice(s.start.col, 1);
-            });
-            setFields(f);
-          },
-        },
-      },
-    },
-    stretchH: "all",
-  };
-
-  const getFields = () => {
-    const hot = hotRef.current.hotInstance;
-    console.log(hot.getDataAtRow(0));
-
-    const settings = hot.getSettings();
-    const colHeadersArray = hot.getSettings().colHeaders;
-    console.log(settings);
-    // colHeadersArray.push("Price #2");
-    // settings["colHeaders"] = colHeadersArray;
-
-    hot.updateSettings(settings);
-  };
+  const getFields = () => {};
 
   return (
     <div
-      className="hot handsontable htColumnHeaders"
       style={{
         width: "100%",
         overflow: "hidden",
@@ -70,7 +21,6 @@ export function Template() {
       <div style={{ marginBottom: SPACE_SM }}>
         <Button onClick={getFields}>New Field Group</Button>
       </div>
-      <HotTable ref={hotRef} settings={settings} />
     </div>
   );
 }
