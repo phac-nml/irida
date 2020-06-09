9f6d0cd737dc347a5b98613b2c96f70a3c0d3a62
Josh Adam <josh.adam@canada.ca>
2020-06-09 08:42:27 -0500

    Updated JSDoc

--- src/main/webapp/resources/js/apis/metadata/metadata-templates.js
+++ src/main/webapp/resources/js/apis/metadata/metadata-templates.js
@@ -3,6 +3,11 @@ import { setBaseUrl } from "../../utilities/url-utilities";
 
 const BASE_URL = setBaseUrl(`/ajax/metadata-templates`);
 
+/**
+ * Get all metadata templates associated with a project
+ * @param projectId
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 export function getProjectMetadataTemplates(projectId) {
   return axios
     .get(`${BASE_URL}?projectId=${projectId}`)
