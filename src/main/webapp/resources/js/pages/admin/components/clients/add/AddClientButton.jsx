497afa61672143db165c71cb58a97c54bca6a216
Josh Adam <josh.adam@canada.ca>
2020-08-24 14:44:43 -0500

    :bulb: Updated JSDoc

--- src/main/webapp/resources/js/pages/admin/components/clients/add/AddClientButton.jsx
+++ src/main/webapp/resources/js/pages/admin/components/clients/add/AddClientButton.jsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import { AddNewButton } from "../../../../../components/Buttons/AddNewButton";
 import { AddClientModal } from "./AddClientModal";
 
+/**
+ * React component to render a Ant Design button to the screen, when clicked
+ * it opens a modal to create a new client.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function AddClientButton() {
   const [visible, setVisible] = useState(false);
 
