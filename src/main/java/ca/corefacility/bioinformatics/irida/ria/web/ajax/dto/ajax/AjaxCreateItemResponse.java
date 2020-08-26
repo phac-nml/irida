14a6feee6677dcc58e64760d91abb7305b1a9dd7
Josh Adam <josh.adam@canada.ca>
2020-08-26 11:22:16 -0500

    :bulb: Fixed typo in JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/ajax/AjaxCreateItemResponse.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/ajax/AjaxCreateItemResponse.java
@@ -1,7 +1,7 @@
 package ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.ajax;
 
 /**
- * UI Response for successfully creating an item.  Since all items in IRIDA require and id,
+ * UI Response for successfully creating an item.  Since all items in IRIDA require an id,
  * the identifier is returned to the client on successful creation.
  */
 public class AjaxCreateItemResponse extends AjaxResponse {
