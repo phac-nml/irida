f8d964b3d83af89ee69253b91ed5cd9e0fbe242d
Josh Adam <josh.adam@canada.ca>
2020-08-25 21:54:36 -0500

    🚧 Reverted files that should not have been modified.

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/clients/ClientTableModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/clients/ClientTableModel.java
@@ -1,26 +1,25 @@
 package ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.clients;
 
-import java.util.Set;
-
 import ca.corefacility.bioinformatics.irida.model.IridaClientDetails;
 import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableModel;
+
+import java.util.Set;
 
 /**
  * Used to represent an {@link IridaClientDetails} in an ant.design table on the Clients page.
  */
 public class ClientTableModel extends TableModel {
-	private final IridaClientDetails details;
+	private final Set<String> grants;
 	private final int tokens;
-
 
 	public ClientTableModel(IridaClientDetails client, int tokens) {
 		super(client.getId(), client.getClientId(), client.getCreatedDate(), client.getModifiedDate());
-		this.details = client;
+		this.grants = client.getAuthorizedGrantTypes();
 		this.tokens = tokens;
 	}
 
-	public IridaClientDetails getDetails() {
-		return details;
+	public Set<String> getGrants() {
+		return grants;
 	}
 
 	public int getTokens() {
--- src/main/webapp/resources/js/pages/admin/components/clients/listing/ClientsTables.jsx
+++ src/main/webapp/resources/js/pages/admin/components/clients/listing/ClientsTables.jsx
@@ -1,28 +1,10 @@
-import React, { useContext, useState } from "react";
-import {
-  PagedTable,
-  PagedTableContext,
-} from "../../../../../components/ant.design/PagedTable";
-import {
-  Button,
-  Form,
-  Input,
-  List,
-  PageHeader,
-  Popconfirm,
-  Radio,
-  Tag,
-  Typography,
-} from "antd";
-import { setBaseUrl } from "../../../../../utilities/url-utilities";
-import { dateColumnFormat } from "../../../../../components/ant.design/table-renderers";
-import { revokeClientTokens } from "../../../../../apis/clients/clients";
-import { IconStop } from "../../../../../components/icons/Icons";
-import { REFRESH_TOKEN_VALIDITY, TOKEN_VALIDITY } from "../constants";
-import { SPACE_MD } from "../../../../../styles/spacing";
-
-const { Paragraph } = Typography;
-const { Item } = Form;
+import React, {useContext} from "react";
+import {PagedTable, PagedTableContext} from "../../../../../components/ant.design/PagedTable";
+import {Button, Popconfirm, Tag} from "antd";
+import {setBaseUrl} from "../../../../../utilities/url-utilities";
+import {dateColumnFormat} from "../../../../../components/ant.design/table-renderers";
+import {revokeClientTokens} from "../../../../../apis/clients/clients";
+import {IconStop} from "../../../../../components/icons/Icons";
 
 /**
  * Table for displaying a list of clients.
@@ -34,13 +16,13 @@ export function ClientsTable() {
 
   const columns = [
     {
-      title: i18n("ClientsTable.column.id"),
+      title: i18n("iridaThing.id"),
       width: 80,
       dataIndex: "id",
-      sorter: true,
+      sorter: true
     },
     {
-      title: i18n("ClientsTable.column.clientId"),
+      title: i18n("client.clientid"),
       dataIndex: "name",
       ellipsis: true,
       sorter: true,
@@ -50,37 +32,37 @@ export function ClientsTable() {
             {text}
           </a>
         );
-      },
+      }
     },
     {
-      title: i18n("ClientsTable.column.grants"),
-      dataIndex: "details",
-      render(grants, item) {
+      title: i18n("client.grant-types"),
+      dataIndex: "grants",
+      render(grants) {
         const colors = {
           password: "purple",
           authorization_code: "volcano",
-          refresh_token: "magenta",
+          refresh_token: "magenta"
         };
         return (
           <div>
-            {item.details.authorizedGrantTypes.map((g) => (
+            {grants.map(g => (
               <Tag color={colors[g] || ""} key={g}>
                 {g}
               </Tag>
             ))}
           </div>
         );
-      },
+      }
     },
     {
       ...dateColumnFormat(),
-      title: i18n("ClientsTable.column.created"),
-      dataIndex: "createdDate",
+      title: i18n("iridaThing.timestamp"),
+      dataIndex: "createdDate"
     },
     {
-      title: i18n("ClientsTable.column.activeTokens"),
+      title: i18n("client.details.token.active"),
       dataIndex: "tokens",
-      align: "right",
+      align: "right"
     },
     {
       key: "action",
@@ -102,8 +84,8 @@ export function ClientsTable() {
             </Button>
           </Popconfirm>
         );
-      },
-    },
+      }
+    }
   ];
 
   /**
@@ -114,138 +96,5 @@ export function ClientsTable() {
     revokeClientTokens(id).then(updateTable);
   }
 
-  const radioStyle = { display: "block", lineHeight: `35px` };
-  const [grantType, setGrantType] = useState("password");
-
-  return (
-    <PagedTable
-      className={"t-admin-clients-table"}
-      columns={columns}
-      expandable={{
-        expandedRowRender: (record) => (
-          <div>
-            <PageHeader
-              title={record.name}
-              extra={[
-                <Button key={`revoke-${record.id}`}>REVOKE</Button>,
-                <Button key={`remove-${record.id}`}>REMOVED</Button>,
-              ]}
-            />
-            <List>
-              <List.Item>
-                <List.Item.Meta
-                  title={"CLIENT SECRET"}
-                  description={
-                    <Paragraph copyable>
-                      {record.details.clientSecret}
-                    </Paragraph>
-                  }
-                />
-              </List.Item>
-            </List>
-            <Form
-              layout="vertical"
-              initialValues={{
-                tokenValidity: record.details.accessTokenValiditySeconds,
-                grantType: record.details.authorizedGrantTypes[0],
-                refreshToken:
-                  record.details.refreshTokenValiditySeconds === null
-                    ? 0
-                    : record.details.refreshTokenValiditySeconds,
-                read: "read",
-                write: "no",
-              }}
-            >
-              <Item
-                label={i18n("AddClientForm.tokenValidity")}
-                name="tokenValidity"
-              >
-                <Radio.Group>
-                  {TOKEN_VALIDITY.map((token) => (
-                    <Radio.Button key={token.value} value={token.value}>
-                      {token.text}
-                    </Radio.Button>
-                  ))}
-                </Radio.Group>
-              </Item>
-              <Item label={i18n("AddClientForm.grantTypes")} name="grantType">
-                <Radio.Group onChange={(e) => setGrantType(e.target.value)}>
-                  <Radio style={radioStyle} value="password">
-                    {i18n("AddClientForm.grant.password")}
-                  </Radio>
-                  <Radio style={radioStyle} value="authorization_code">
-                    {i18n("AddClientForm.grant.authorizationCode")}
-
-                    {grantType === "authorization_code" ? (
-                      <Item
-                        name="redirectURI"
-                        style={{
-                          display: "inline-block",
-                          marginLeft: SPACE_MD,
-                          marginBottom: 0,
-                          width: 400,
-                        }}
-                        rules={[
-                          {
-                            required: true,
-                            message: i18n(
-                              "AddClientForm.grant.authorizationCode.redirect.warning"
-                            ),
-                          },
-                        ]}
-                      >
-                        <Input
-                          placeholder={i18n(
-                            "AddClientForm.grant.authorizationCode.redirect"
-                          )}
-                        />
-                      </Item>
-                    ) : null}
-                  </Radio>
-                </Radio.Group>
-              </Item>
-              <Item
-                label={i18n("AddClientForm.refreshToken")}
-                name="refreshToken"
-              >
-                <Radio.Group>
-                  {REFRESH_TOKEN_VALIDITY.map((token) => (
-                    <Radio.Button key={token.value} value={token.value}>
-                      {token.text}
-                    </Radio.Button>
-                  ))}
-                </Radio.Group>
-              </Item>
-              <Item label={i18n("AddClientForm.readScope")} name="read">
-                <Radio.Group>
-                  <Radio.Button value="no">
-                    {i18n("AddClientForm.scopeNotAllowed")}
-                  </Radio.Button>
-                  <Radio.Button value="read">
-                    {i18n("AddClientForm.scopeAllowed")}
-                  </Radio.Button>
-                  <Radio.Button value="auto">
-                    {i18n("AddClientForm.scopeAllowedAutoApprove")}
-                  </Radio.Button>
-                </Radio.Group>
-              </Item>
-              <Item label={i18n("AddClientForm.writeScope")} name="write">
-                <Radio.Group>
-                  <Radio.Button value="no">
-                    {i18n("AddClientForm.scopeNotAllowed")}
-                  </Radio.Button>
-                  <Radio.Button value="write">
-                    {i18n("AddClientForm.scopeAllowed")}
-                  </Radio.Button>
-                  <Radio.Button value="auto">
-                    {i18n("AddClientForm.scopeAllowedAutoApprove")}
-                  </Radio.Button>
-                </Radio.Group>
-              </Item>
-            </Form>
-          </div>
-        ),
-      }}
-    />
-  );
+  return <PagedTable className={"t-admin-clients-table"} columns={columns} />;
 }
