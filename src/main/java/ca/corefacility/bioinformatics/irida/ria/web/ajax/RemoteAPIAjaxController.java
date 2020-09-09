a5a804b8bf56f9cad1e10fcfb001f26364780575
Josh Adam <josh.adam@canada.ca>
2020-09-09 09:55:23 -0500

    📗 Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/RemoteAPIAjaxController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/RemoteAPIAjaxController.java
@@ -1,12 +1,14 @@
 package ca.corefacility.bioinformatics.irida.ria.web.ajax;
 
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
+import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
+import ca.corefacility.bioinformatics.irida.repositories.specification.RemoteAPISpecification;
 import ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.RemoteAPIModel;
+import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableRequest;
+import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableResponse;
+import ca.corefacility.bioinformatics.irida.ria.web.rempoteapi.dto.RemoteAPITableModel;
 import ca.corefacility.bioinformatics.irida.ria.web.services.UIRemoteAPIService;
+import ca.corefacility.bioinformatics.irida.service.RemoteAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -14,14 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import ca.corefacility.bioinformatics.irida.exceptions.IridaOAuthException;
-import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
-import ca.corefacility.bioinformatics.irida.repositories.specification.RemoteAPISpecification;
-import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableRequest;
-import ca.corefacility.bioinformatics.irida.ria.web.models.tables.TableResponse;
-import ca.corefacility.bioinformatics.irida.ria.web.rempoteapi.dto.RemoteAPITableModel;
-import ca.corefacility.bioinformatics.irida.service.RemoteAPIService;
-import ca.corefacility.bioinformatics.irida.service.remote.ProjectRemoteService;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Controller for asynchronous requests for remote api functionality.
@@ -29,62 +26,70 @@ import ca.corefacility.bioinformatics.irida.service.remote.ProjectRemoteService;
 @RestController
 @RequestMapping("/ajax/remote_api")
 public class RemoteAPIAjaxController {
-	private final RemoteAPIService remoteAPIService;
-	private final ProjectRemoteService projectRemoteService;
-	private final UIRemoteAPIService service;
+    private final RemoteAPIService remoteAPIService;
+    private final UIRemoteAPIService service;
 
-	public static final String VALID_OAUTH_CONNECTION = "valid_token";
-	public static final String INVALID_OAUTH_TOKEN = "invalid_token";
+    public static final String INVALID_OAUTH_TOKEN = "invalid_token";
 
-	@Autowired
-	public RemoteAPIAjaxController(RemoteAPIService remoteAPIService, ProjectRemoteService projectRemoteService, UIRemoteAPIService service) {
-		this.remoteAPIService = remoteAPIService;
-		this.projectRemoteService = projectRemoteService;
-		this.service = service;
-	}
+    @Autowired
+    public RemoteAPIAjaxController(RemoteAPIService remoteAPIService, UIRemoteAPIService service) {
+        this.remoteAPIService = remoteAPIService;
+        this.service = service;
+    }
 
-	/**
-	 * Get a list of the current page for the Remote API Table
-	 *
-	 * @param tableRequest - the details for the current page of the Table
-	 * @return {@link TableResponse}
-	 */
-	@RequestMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
-	public TableResponse<RemoteAPITableModel> getAjaxAPIList(@RequestBody TableRequest tableRequest) {
-		Page<RemoteAPI> search = remoteAPIService.search(
-				RemoteAPISpecification.searchRemoteAPI(tableRequest.getSearch()), tableRequest.getCurrent(),
-				tableRequest.getPageSize(), tableRequest.getSortDirection(), tableRequest.getSortColumn());
+    /**
+     * Get a list of the current page for the Remote API Table
+     *
+     * @param tableRequest - the details for the current page of the Table
+     * @return {@link TableResponse}
+     */
+    @RequestMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public TableResponse<RemoteAPITableModel> getAjaxAPIList(@RequestBody TableRequest tableRequest) {
+        Page<RemoteAPI> search = remoteAPIService.search(
+                RemoteAPISpecification.searchRemoteAPI(tableRequest.getSearch()), tableRequest.getCurrent(),
+                tableRequest.getPageSize(), tableRequest.getSortDirection(), tableRequest.getSortColumn());
 
-		List<RemoteAPITableModel> apiData = search.getContent()
-				.stream()
-				.map(RemoteAPITableModel::new)
-				.collect(Collectors.toList());
-		return new TableResponse<>(apiData, search.getTotalElements());
-	}
+        List<RemoteAPITableModel> apiData = search.getContent()
+                .stream()
+                .map(RemoteAPITableModel::new)
+                .collect(Collectors.toList());
+        return new TableResponse<>(apiData, search.getTotalElements());
+    }
 
-	/**
-	 * Check the currently logged in user's OAuth2 connection status to a given
-	 * API
-	 *
-	 * @param apiId The ID of the api
-	 * @return "valid" or "invalid_token" message
-	 */
-	@RequestMapping("/status/{apiId}")
-	public ResponseEntity<Date> checkAPIStatus(@PathVariable Long apiId) {
-		try {
-			return ResponseEntity.ok(service.checkAPIStatus(apiId));
-		} catch (EntityNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
-	}
+    /**
+     * Check the currently logged in user's OAuth2 connection status to a given
+     * API
+     *
+     * @param apiId The ID of the api
+     * @return "valid" or "invalid_token" message
+     */
+    @RequestMapping("/status/{apiId}")
+    public ResponseEntity<Date> checkAPIStatus(@PathVariable Long apiId) {
+        try {
+            return ResponseEntity.ok(service.checkAPIStatus(apiId));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 
-	@GetMapping("/{remoteId}")
-	public ResponseEntity<RemoteAPIModel> getRemoteAPIDetails(@PathVariable long remoteId) {
-		return ResponseEntity.ok(service.getRemoteApiDetails(remoteId));
-	}
+    /**
+     * Get details about a Remote API Connection
+     *
+     * @param remoteId Identifier for a Remote API
+     * @return {@link ResponseEntity} with the details of the Remote API
+     */
+    @GetMapping("/{remoteId}")
+    public ResponseEntity<RemoteAPIModel> getRemoteAPIDetails(@PathVariable long remoteId) {
+        return ResponseEntity.ok(service.getRemoteApiDetails(remoteId));
+    }
 
-	@DeleteMapping("/{remoteId}/delete")
-	public void deleteRemoteAPI(@PathVariable long remoteId) {
-		service.deleteRemoteAPI(remoteId);
-	}
+    /**
+     * Delete a Remote API Connection
+     *
+     * @param remoteId Identifier for a Remote API.
+     */
+    @DeleteMapping("/{remoteId}/delete")
+    public void deleteRemoteAPI(@PathVariable long remoteId) {
+        service.deleteRemoteAPI(remoteId);
+    }
 }
--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIRemoteAPIService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIRemoteAPIService.java
@@ -1,6 +1,5 @@
 package ca.corefacility.bioinformatics.irida.ria.web.services;
 
-import ca.corefacility.bioinformatics.irida.exceptions.EntityNotFoundException;
 import ca.corefacility.bioinformatics.irida.model.RemoteAPI;
 import ca.corefacility.bioinformatics.irida.model.RemoteAPIToken;
 import ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.RemoteAPIModel;
@@ -25,17 +24,34 @@ public class UIRemoteAPIService {
         this.tokenService = tokenService;
     }
 
+    /**
+     * Check the status of a {@link RemoteAPI}
+     *
+     * @param remoteId identifier for a {@link RemoteAPI}
+     * @return expiration {@link Date} of the REmote API Token
+     */
     public Date checkAPIStatus(long remoteId) {
         RemoteAPI api = remoteAPIService.read(remoteId);
         RemoteAPIToken token = tokenService.getToken(api);
         return token.getExpiryDate();
     }
 
+    /**
+     * Get details about a specific {@link RemoteAPI}
+     *
+     * @param remoteId identifier for the {@link RemoteAPI}
+     * @return {@link RemoteAPIModel} containing the details of the {@link RemoteAPI}
+     */
     public RemoteAPIModel getRemoteApiDetails(long remoteId) {
         RemoteAPI remoteAPI = remoteAPIService.read(remoteId);
         return new RemoteAPIModel(remoteAPI);
     }
 
+    /**
+     * Delete a specific {@link RemoteAPI}
+     *
+     * @param remoteId Identifier for the {@link RemoteAPI} to delete
+     */
     public void deleteRemoteAPI(long remoteId) {
         remoteAPIService.delete(remoteId);
     }
