6776d1b0c0a53952420effbd5a157263be78b4e6
Josh Adam <josh.adam@canada.ca>
2020-09-14 13:15:25 -0500

    :memo: Fixed typo

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/RemoteProjectModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/RemoteProjectModel.java
@@ -2,6 +2,10 @@ package ca.corefacility.bioinformatics.irida.ria.web.ajax.dto;
 
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 
+/**
+ * Used to represent a remote project for selecting from available remote projects in the
+ * create remote synchronized project form
+ */
 public class RemoteProjectModel {
 	private final String value; // Should be id for the item
 	private final String label; // Should be the name for the item;
