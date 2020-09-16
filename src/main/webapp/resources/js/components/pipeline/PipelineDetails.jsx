7506586579ca3ad805fb6c1355aab25ac5f0b62d
Josh Adam <josh.adam@canada.ca>
2020-09-16 10:35:02 -0500

    :bulb: Updated JSDoc

--- src/main/webapp/resources/js/components/pipeline/PipelineDetails.jsx
+++ src/main/webapp/resources/js/components/pipeline/PipelineDetails.jsx
@@ -3,15 +3,24 @@ import { Form, Input } from "antd";
 import { useLaunchDispatch, useLaunchState } from "./launch-context";
 import * as CONSTANTS from "./lauch-constants";
 
+/**
+ * React component for the editing of launching a pipeline base details, including
+ * name and description.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function PipelineDetails({}) {
   const { name, description } = useLaunchState();
   const dispatch = useLaunchDispatch();
   const nameRef = useRef();
 
   useEffect(() => {
+    // Set focus on the name input at mount time.
     setTimeout(() => nameRef.current.focus(), 100);
   }, []);
 
+  // Select all the text in the name input
+  // Used so that when focus is set it all gets selected.
   const selectNameText = () => nameRef.current.select();
 
   return (
