d7dcbd535466aa0621446d45d102376347034dbf
Tom Matthews <t.c.matthews@gmail.com>
2020-10-06 09:01:50 -0500

    added exception for when a link is not available in the rest api

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/exceptions/LinkNotFoundException.java
@@ -0,0 +1,8 @@
+package ca.corefacility.bioinformatics.irida.exceptions;
+
+public class LinkNotFoundException extends RuntimeException {
+
+	public LinkNotFoundException(String message) {
+		super(message);
+	}
+}
