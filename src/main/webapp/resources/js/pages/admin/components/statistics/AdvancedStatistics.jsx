e55597c169da58c3e48dcdb94c8b5c7e2eff8f08
Josh Adam <josh.adam@canada.ca>
2020-10-15 12:42:18 -0500

    :construction: Cleaned up routing and layout

--- src/main/webapp/resources/js/pages/admin/components/statistics/AdvancedStatistics.jsx
+++ src/main/webapp/resources/js/pages/admin/components/statistics/AdvancedStatistics.jsx
@@ -5,40 +5,40 @@
 
 import React, { useContext, useEffect, useState } from "react";
 import { Bar, Column, Line, Pie } from "@ant-design/charts";
-import { Form, Typography } from "antd";
+import { Card, Form, PageHeader } from "antd";
 import { TimePeriodSelect } from "./TimePeriodSelect";
 import {
   AdminStatisticsContext,
-  defaultTimePeriod,
-  defaultChartType,
   chartTypes,
-  statisticTypes
+  defaultChartType,
+  defaultTimePeriod,
+  statisticTypes,
 } from "../../../../contexts/AdminStatisticsContext";
-import { SPACE_LG } from "../../../../styles/spacing";
+import { SPACE_LG, SPACE_MD } from "../../../../styles/spacing";
 import { ChartTypeButtons } from "./ChartTypeButtons";
-import { getChartConfiguration } from "../../chart-config"
+import { getChartConfiguration } from "../../chart-config";
+import { useNavigate } from "@reach/router";
+import { setBaseUrl } from "../../../../utilities/url-utilities";
 
-const { Title } = Typography;
-
-export default function AdvancedStatistics({statType}) {
+export default function AdvancedStatistics({ statType }) {
   const {
     adminStatisticsContext,
     updateAnalysesStatsTimePeriod,
     updateProjectStatsTimePeriod,
     updateSampleStatsTimePeriod,
-    updateUserStatsTimePeriod
+    updateUserStatsTimePeriod,
   } = useContext(AdminStatisticsContext);
-
 
   const [timePeriod, setTimePeriod] = useState(defaultTimePeriod);
   const [chartType, setChartType] = useState(defaultChartType);
   const [form] = Form.useForm();
+  const navigate = useNavigate();
 
   const components = {
     [chartTypes.BAR]: Bar,
     [chartTypes.COLUMN]: Column,
     [chartTypes.LINE]: Line,
-    [chartTypes.PIE]: Pie
+    [chartTypes.PIE]: Pie,
   };
 
   useEffect(() => {
@@ -50,52 +50,64 @@ export default function AdvancedStatistics({statType}) {
   }, [statType]);
 
   function updateTimePeriod(currTimePeriod) {
-    if(statType === statisticTypes.ANALYSES) {
+    if (statType === statisticTypes.ANALYSES) {
       updateAnalysesStatsTimePeriod(timePeriod);
     } else if (statType === statisticTypes.PROJECTS) {
       updateProjectStatsTimePeriod(timePeriod);
     } else if (statType === statisticTypes.SAMPLES) {
-      updateSampleStatsTimePeriod(timePeriod)
+      updateSampleStatsTimePeriod(timePeriod);
     } else if (statType === statisticTypes.USERS) {
       updateUserStatsTimePeriod(timePeriod);
     }
     setTimePeriod(currTimePeriod);
   }
 
-  const chartTitle = statType === statisticTypes.ANALYSES ?
-      ("Number of Analyses Ran")
-    : statType === statisticTypes.PROJECTS ?
-      ("Number of Projects Created")
-    : statType === statisticTypes.SAMPLES ?
-      ("Number of Samples Created")
-    : statType === statisticTypes.USERS ?
-        ("Number of Users Created")
-    : null
-  ;
+  const chartTitle =
+    statType === statisticTypes.ANALYSES
+      ? "Number of Analyses Ran"
+      : statType === statisticTypes.PROJECTS
+      ? "Number of Projects Created"
+      : statType === statisticTypes.SAMPLES
+      ? "Number of Samples Created"
+      : statType === statisticTypes.USERS
+      ? "Number of Users Created"
+      : null;
 
   function displayChart() {
     const Component = components[chartType];
     return Component ? (
-      <Component {...getChartConfiguration(chartType, statType, adminStatisticsContext.statistics)} />
+      <Component
+        {...getChartConfiguration(
+          chartType,
+          statType,
+          adminStatisticsContext.statistics
+        )}
+      />
     ) : null;
   }
 
   return (
-    <div>
+    <>
+      <PageHeader
+        title={chartTitle}
+        onBack={() => navigate(setBaseUrl(`/admin/statistics`))}
+      />
+      <Card style={{ margin: SPACE_LG }}>
         <Form
           form={form}
           initialValues={{
             "time-period": defaultTimePeriod,
           }}
-          style={{marginBottom: SPACE_LG}}
+          style={{ marginBottom: SPACE_MD }}
         >
           <TimePeriodSelect onChange={(e) => updateTimePeriod(e)} />
-          <ChartTypeButtons onChange={(e) => setChartType(e.target.value)} value={chartType}/>
+          <ChartTypeButtons
+            onChange={(e) => setChartType(e.target.value)}
+            value={chartType}
+          />
         </Form>
-        <div style={{marginBottom: SPACE_LG}}>
-          <Title level={4}>{chartTitle}</Title>
-        </div>
         {displayChart()}
-    </div>
+      </Card>
+    </>
   );
 }
--- src/main/webapp/resources/js/pages/admin/components/statistics/BasicStats.jsx
+++ src/main/webapp/resources/js/pages/admin/components/statistics/BasicStats.jsx
@@ -4,9 +4,9 @@
  * charts
  */
 
-import React, { lazy, Suspense, useContext } from "react";
-import { Col, Card, Row, Statistic } from "antd";
-import { SPACE_LG, SPACE_MD } from "../../../../styles/spacing";
+import React, { useContext } from "react";
+import { Card, Col, Row, Statistic } from "antd";
+import { SPACE_MD } from "../../../../styles/spacing";
 import { TinyColumn } from "@ant-design/charts";
 import { ADMINSTATS } from "../../routes";
 
@@ -14,7 +14,7 @@ import {
   AdminStatisticsContext,
   chartTypes,
   defaultTimePeriod,
-  timePeriodMap
+  timePeriodMap,
 } from "../../../../contexts/AdminStatisticsContext";
 
 import { getChartConfiguration } from "../../chart-config";
@@ -22,13 +22,7 @@ import styled from "styled-components";
 import { blue6 } from "../../../../styles/colors";
 
 import { setBaseUrl } from "../../../../utilities/url-utilities";
-import { Link, Router } from "@reach/router";
-import { ContentLoading } from "../../../../components/loader";
-
-const AnalysesStats = lazy(() => import("./AnalysesStats"));
-const ProjectStats = lazy(() => import("./ProjectStats"));
-const SampleStats = lazy(() => import("./SampleStats"));
-const UserStats = lazy(() => import("./UserStats"));
+import { Link } from "@reach/router";
 
 const LinkCard = styled(Card)`
   &:hover {
@@ -37,11 +31,8 @@ const LinkCard = styled(Card)`
 `;
 
 export default function BasicStats() {
-  const { adminStatisticsContext } = useContext(
-    AdminStatisticsContext
-  );
+  const { adminStatisticsContext } = useContext(AdminStatisticsContext);
 
-  const ADMIN_URL = setBaseUrl("/admin");
   const DEFAULT_URL = setBaseUrl("/admin/statistics");
 
   const cards = [
@@ -49,72 +40,55 @@ export default function BasicStats() {
       key: `analyses`,
       title: `Analyses run in past ${timePeriodMap[defaultTimePeriod]}`,
       value: adminStatisticsContext.basicStats.analysesRun,
-      url: `${DEFAULT_URL}/${ADMINSTATS.ANALYSES}`
+      url: `${DEFAULT_URL}/${ADMINSTATS.ANALYSES}`,
     },
     {
       key: `projects`,
       title: `Projects created in past ${timePeriodMap[defaultTimePeriod]}`,
       value: adminStatisticsContext.basicStats.projectsCreated,
-      url: `${DEFAULT_URL}/${ADMINSTATS.PROJECTS}`
+      url: `${DEFAULT_URL}/${ADMINSTATS.PROJECTS}`,
     },
     {
       key: `samples`,
       title: `Samples created in past ${timePeriodMap[defaultTimePeriod]}`,
       value: adminStatisticsContext.basicStats.samplesCreated,
-      url: `${DEFAULT_URL}/${ADMINSTATS.SAMPLES}`
+      url: `${DEFAULT_URL}/${ADMINSTATS.SAMPLES}`,
     },
     {
       key: `users`,
       title: `Users created in past ${timePeriodMap[defaultTimePeriod]}`,
       value: adminStatisticsContext.basicStats.usersCreated,
-      url: `${DEFAULT_URL}/${ADMINSTATS.USERS}`
+      url: `${DEFAULT_URL}/${ADMINSTATS.USERS}`,
     },
     {
       key: `usersLoggedIn`,
       title: `Users logged on in past ${timePeriodMap[defaultTimePeriod]}`,
       value: adminStatisticsContext.basicStats.usersLoggedIn,
-      url: `${DEFAULT_URL}`
+      url: `${DEFAULT_URL}`,
     },
   ];
   return (
-    <div className="t-statistics">
-      {window.location.pathname === ADMIN_URL || window.location.pathname === DEFAULT_URL ?
-        <div style={{ marginBottom: SPACE_LG }} className="t-stats-basic">
-          <Row gutter={[16, 16]}>
-            {cards.map((card) => (
-              <Col sm={24} md={12} xl={8} xxl={6} key={card.key}>
-                <Link to={card.url}>
-                  <LinkCard>
-                    <Statistic title={card.title} value={card.value} />
-                    <TinyColumn {...getChartConfiguration(chartTypes.TINYCOLUMN, card.key, adminStatisticsContext.statistics)} />
-                  </LinkCard>
-                </Link>
-              </Col>
-            ))}
-          </Row>
-        </div>
-        : null
-      }
-      <Suspense fallback={<ContentLoading />}>
-        <Router style={{ paddingTop: SPACE_MD }}>
-          <AnalysesStats
-            path={`${DEFAULT_URL}/${ADMINSTATS.ANALYSES}`}
-            key="analyses"
-          />
-          <ProjectStats
-            path={`${DEFAULT_URL}/${ADMINSTATS.PROJECTS}`}
-            key="projects"
-          />
-          <SampleStats
-            path={`${DEFAULT_URL}/${ADMINSTATS.SAMPLES}`}
-            key="samples"
-          />
-          <UserStats
-            path={`${DEFAULT_URL}/${ADMINSTATS.USERS}`}
-            key="users"
-          />
-        </Router>
-      </Suspense>
-    </div>
+    <Row
+      gutter={[16, 16]}
+      className="t-statistics t-stats-basic"
+      style={{ padding: SPACE_MD }}
+    >
+      {cards.map((card) => (
+        <Col sm={24} md={12} xl={8} xxl={6} key={card.key}>
+          <Link to={card.url}>
+            <LinkCard>
+              <Statistic title={card.title} value={card.value} />
+              <TinyColumn
+                {...getChartConfiguration(
+                  chartTypes.TINYCOLUMN,
+                  card.key,
+                  adminStatisticsContext.statistics
+                )}
+              />
+            </LinkCard>
+          </Link>
+        </Col>
+      ))}
+    </Row>
   );
 }
