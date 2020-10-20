e0681703b90bc40beb40a7dff720281dd938787f
Josh Adam <josh.adam@canada.ca>
2020-10-20 10:38:54 -0500

    :globe_with_meridians: Cleaned up internationalizations on the LaunchDetails.jsx

--- src/main/webapp/resources/js/pages/launch/LaunchDetails.jsx
+++ src/main/webapp/resources/js/pages/launch/LaunchDetails.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Form, Input } from "antd";
+import {Form, Input} from "antd";
 
+/**
+ * React component for editing the basic information for launching an IRIDA Workflow Pipeline.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function LaunchDetails() {
   return (
     <>
       <Form.Item
         label={i18n("LaunchDetails.name")}
         name="name"
-        rules={[{ required: true, message: "PIPELINES REQUIRE A NAME" }]}
+        rules={[
+          {
+            required: true,
+            message: i18n("LaunchDLaunchDetails.name.required"),
+          },
+        ]}
       >
         <Input />
       </Form.Item>
