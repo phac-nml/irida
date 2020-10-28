09c38933065c2f73c8ada70f797482f68931d95a
Katherine Thiessen <katherine.thiessen@gmail.com>
2020-10-28 10:16:32 -0500

    adding scripts that populate the new title column in the announcements table

---
+++ src/main/resources/scripts/announcements/generate_json.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import argparse
+from bs4 import BeautifulSoup
+import json
+from markdown import markdown
+import mysql.connector
+import re
+
+def create_json_file(json_file, host, user, password, database):
+
+    json_data = []
+    json_file = open(json_file,"w+")
+
+    db = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT id, created_date, created_by_id, title, message, priority FROM announcement")
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    for id, created_date, created_by_id, title, message, priority in result:
+        if not title:
+            soup = BeautifulSoup(markdown(message), 'html.parser')
+            # find any header elements
+            headers = soup.find_all(re.compile('^h[1-6]$'))
+            if headers:
+                title = headers[0].string
+                # remove header from message
+                headers[0].extract()
+                message = soup
+
+            json_data.append({"id":id, "created_date":created_date, "created_by_id":created_by_id, "title":title, "message":message, "priority":priority})
+
+    # convert the list to a JSON string
+    json_string = json.dumps(json_data, indent=2, default=str)
+    json_file.write(json_string)
+    json_file.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument( 'json_file', help="The output json file of announcement titles to be populated.")
+    parser.add_argument( '--host', default='localhost', help="The database host name.", required=False)
+    parser.add_argument( '--database', default='irida_test', help="The database name.", required=False)
+    parser.add_argument( '--user', default='root', help="The database user name.", required=False)
+    parser.add_argument( '--password', help="The database password.", required=False)
+
+    args = parser.parse_args()
+
+    create_json_file(args.json_file, args.host, args.user, args.password, args.database)
+
+if __name__ == '__main__':
+    main()
---
+++ src/main/resources/scripts/announcements/update_announcements.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import argparse
+import json
+import mysql.connector
+
+def update_db(json_file, host, user, password, database):
+    try:
+        db = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        cursor = db.cursor()
+        query = """UPDATE announcement SET title = %s, message = %s WHERE id = %s;"""
+
+        json_file = open(json_file, "r")
+        json_data = json.load(json_file)
+        json_file.close()
+
+        for json_object in json_data:
+            id = json_object['id']
+            title = json_object['title']
+            message = json_object['message']
+            data = (title, message, id)
+            cursor.execute(query, data)
+            db.commit()
+            print("Record ID {} updated successfully!".format(id))
+
+    except mysql.connector.Error as error:
+        print("Failed to update table record: {}".format(error))
+
+    finally:
+        if (db.is_connected()):
+            cursor.close()
+            db.close()
+            print("Database connection is closed.")
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument( 'json_file', help="The input json file of announcement titles to be populated.")
+    parser.add_argument( '--host', default='localhost', help="The database host name.", required=False)
+    parser.add_argument( '--database', default='irida_test', help="The database name.", required=False)
+    parser.add_argument( '--user', default='root', help="The database user name.", required=False)
+    parser.add_argument( '--password', help="The database password.", required=False)
+
+    args = parser.parse_args()
+
+    update_db(args.json_file, args.host, args.user, args.password, args.database)
+
+if __name__ == '__main__':
+    main()
