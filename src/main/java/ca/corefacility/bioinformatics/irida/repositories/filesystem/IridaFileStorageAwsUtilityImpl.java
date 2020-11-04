2954fe8656d6f558e5882255d52a951da3639cec
Deep Sidhu <deepsidhu85@gmail.com>
2020-11-04 15:40:33 -0600

    Removed unused import

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/filesystem/IridaFileStorageAwsUtilityImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/filesystem/IridaFileStorageAwsUtilityImpl.java
@@ -5,7 +5,6 @@ import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.zip.GZIPInputStream;
