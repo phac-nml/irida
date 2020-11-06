da2f590075a4125763860023854e9d25c38ac202
Josh Adam <josh.adam@canada.ca>
2020-11-06 10:36:43 -0600

    :bulb: Updated JSDoc

--- src/main/webapp/resources/js/pages/launch/components/ParametersModal/SaveParametersAsFooter.jsx
+++ src/main/webapp/resources/js/pages/launch/components/ParametersModal/SaveParametersAsFooter.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Button, Form, Input } from "antd";
 import { useLaunchState } from "../../launch-context";
 
+/**
+ * React component to render the footer of the saved parameters modal
+ * Complicated due to the nature of having to either save or just store
+ * modified parameters.
+ * @param {function} onCancel - called when the modal is cancelled
+ * @param {function} onSaveAs - called when the parameters are saved as a new name
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function SaveParametersAsFooter({ onCancel, onSaveAs }) {
   const { parameterSet } = useLaunchState();
   const inputRef = React.useRef();
