a92f907537ac87a27d560544f2c27c270b1cc3d4
Katherine Thiessen <katherine.thiessen@gmail.com>
2020-11-18 10:50:52 -0600

    changing modal.confirm to a modal as we are able to set the visibility

--- src/main/webapp/resources/js/pages/admin/components/announcements/CreateAnnouncementModal.jsx
+++ src/main/webapp/resources/js/pages/admin/components/announcements/CreateAnnouncementModal.jsx
@@ -1,6 +1,64 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Checkbox, Form, Input, Modal } from "antd";
+import { IconEdit } from "../../../../components/icons/Icons";
+import { FONT_COLOR_PRIMARY } from "../../../../styles/fonts";
+import { MarkdownEditor } from "../../../../components/markdown/MarkdownEditor";
 
-export function CreateAnnouncementModal({visible}) {
+export function CreateAnnouncementModal({
+  visible,
+  closeModal,
+  createAnnouncement,
+}) {
+  const markdownRef = useRef();
+  const [form] = Form.useForm();
 
+  function saveMarkdown() {
+    form.validateFields().then((values) => {
+      const markdown = markdownRef.current.getMarkdown();
+      const title = values.title;
+      const priority = values.priority;
 
+      createAnnouncement(title, markdown, priority);
+      closeModal();
+    });
+  }
+  return (
+    <Modal
+      visible={visible}
+      title={
+        <>
+          <IconEdit style={{ color: FONT_COLOR_PRIMARY }} />
+          {i18n("CreateNewAnnouncement.title")}
+        </>
+      }
+      width="80%"
+      okText={i18n("CreateNewAnnouncement.okBtn")}
+      okButtonProps={{
+        className: "t-submit-announcement",
+      }}
+      onOk={saveMarkdown}
+      onCancel={closeModal}
+    >
+      <Form layout="vertical" form={form} preserve={false}>
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[
+            {
+              required: true,
+              message: "Please input a title",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="message" label="Message">
+          <MarkdownEditor ref={markdownRef} />
+        </Form.Item>
+        <Form.Item name="priority" label="Priority" valuePropName="checked">
+          <Checkbox />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 }
--- src/main/webapp/resources/js/pages/admin/components/announcements/CreateNewAnnouncement.jsx
+++ src/main/webapp/resources/js/pages/admin/components/announcements/CreateNewAnnouncement.jsx
@@ -1,9 +1,6 @@
-import React, { useRef } from "react";
-import { Checkbox, Form, Input, Modal } from "antd";
+import React from "react";
 import { AddNewButton } from "../../../../components/Buttons/AddNewButton";
-import { MarkdownEditor } from "../../../../components/markdown/MarkdownEditor";
-import { FONT_COLOR_PRIMARY } from "../../../../styles/fonts";
-import { IconEdit } from "../../../../components/icons/Icons";
+import { CreateAnnouncementModal } from "./CreateAnnouncementModal";
 
 /**
  * Component to add a button which will open a modal to create an announcement.
@@ -13,61 +10,19 @@ import { IconEdit } from "../../../../components/icons/Icons";
  */
 export function CreateNewAnnouncement({ createAnnouncement }) {
   const [visible, setVisible] = React.useState(false);
-  const markdownRef = useRef();
-  const [form] = Form.useForm();
-
-  function saveMarkdown() {
-    form.validateFields().then((values) => {
-      const markdown = markdownRef.current.getMarkdown();
-      const title = values.title;
-      const priority = values.priority;
-
-      createAnnouncement(title, markdown, priority);
-    });
-  }
-
-  function displayModal() {
-    Modal.confirm({
-      title: i18n("CreateNewAnnouncement.title"),
-      icon: <IconEdit style={{ color: FONT_COLOR_PRIMARY }} />,
-      width: "80%",
-      content: (
-        <Form layout="vertical" form={form}>
-          <Form.Item
-            name="title"
-            label="Title"
-            rules={[
-              {
-                required: true,
-                message: "Please input a title",
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item name="message" label="Message">
-            <MarkdownEditor ref={markdownRef} />
-          </Form.Item>
-          <Form.Item name="priority" label="Priority" valuePropName="checked">
-            <Checkbox />
-          </Form.Item>
-        </Form>
-      ),
-      okText: i18n("CreateNewAnnouncement.okBtn"),
-      okButtonProps: {
-        className: "t-submit-announcement",
-      },
-      onOk() {
-        saveMarkdown();
-      },
-    });
-  }
 
   return (
-    <AddNewButton
-      className="t-create-announcement"
-      onClick={() => setVisible(true)}
-      text={i18n("CreateNewAnnouncement.title")}
-    />
+    <>
+      <AddNewButton
+        className="t-create-announcement"
+        onClick={() => setVisible(true)}
+        text={i18n("CreateNewAnnouncement.title")}
+      />
+      <CreateAnnouncementModal
+        visible={visible}
+        closeModal={() => setVisible(false)}
+        createAnnouncement={createAnnouncement}
+      />
+    </>
   );
 }
