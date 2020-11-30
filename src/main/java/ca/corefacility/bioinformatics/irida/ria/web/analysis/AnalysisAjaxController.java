dca4590ac2b04621e4f52f20200fe2bc9c40266a
Deep Sidhu <deepsidhu85@gmail.com>
2020-11-30 14:47:04 -0600

    Added throwable cause to error message

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/AnalysisAjaxController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/analysis/AnalysisAjaxController.java
@@ -1096,7 +1096,7 @@ public class AnalysisAjaxController {
 			} catch (NoSuchFileException e) {
 				logger.error("File was not found: " + e.toString());
 			} catch (IOException e) {
-				logger.error("Unable to read input stream for file");
+				logger.error("Unable to read input stream for file", e);
 			}
 
 		}
