238a09c7f58f1b4e0f602a758c1d05890476d133
Josh Adam <josh.adam@canada.ca>
2020-12-14 13:22:15 -0600

    Add JSDoc to CalendarDate

--- src/main/webapp/resources/js/components/CalendarDate.jsx
+++ src/main/webapp/resources/js/components/CalendarDate.jsx
@@ -3,6 +3,12 @@ import { IconCalendarTwoTone } from "./icons/Icons";
 import { formatInternationalizedDateTime } from "../utilities/date-utilities";
 import { Space, Typography } from "antd";
 
+/**
+ * React component to display a calendar icon with a formatted date
+ * @param date - Date to be displayed.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function CalendarDate({ date }) {
   return (
     <Space size="small">
