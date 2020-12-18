36217e186faf24e3afa542f73527c487c7048c32
Josh Adam <josh.adam@canada.ca>
2020-12-18 14:26:29 -0600

    Updated JSDoc

--- src/main/webapp/resources/js/pages/launch/LaunchFiles.jsx
+++ src/main/webapp/resources/js/pages/launch/LaunchFiles.jsx
@@ -29,10 +29,21 @@ export function LaunchFiles() {
     { acceptsPairedSequenceFiles: paired, acceptsSingleSequenceFiles: singles },
     dispatch,
   ] = useLaunch();
+  /*
+  State to determine whether to hide samples that do not have any usable files
+  in them.  These sample will not be run on the pipeline either way.
+   */
   const [hideUnusable, setHideUnusable] = React.useState(true);
+  /*
+  State to hold the count of the number of samples currently being displayed by the UI.
+   */
   const [visibleSamples, setVisibleSamples] = React.useState();
   const [samples, setSamples] = React.useState();
 
+  /*
+  Called on initialization.  This gets the samples that are currently in the cart,
+  with their associated files.
+   */
   React.useEffect(() => {
     fetchPipelineSamples({
       paired,
@@ -49,10 +60,20 @@ export function LaunchFiles() {
       .catch((message) => notification.error({ message }));
   }, [paired, singles]);
 
+  /*
+  Called when a user selects a different set of files to run on the sample.
+  Needs to be updated through the reducer so that the correct file ids are
+  set when the pipeline is launched.
+   */
   React.useEffect(() => {
     setSelectedSampleFiles(dispatch, selected);
-  }, [selected]);
+  }, [dispatch, selected]);
 
+  /*
+  Called when there are samples or when the toggling the visible samples it triggered
+  Sets the samples to display either all or only ones with good files.
+
+   */
   React.useEffect(() => {
     if (samples) {
       if (hideUnusable) {
@@ -63,8 +84,15 @@ export function LaunchFiles() {
     }
   }, [samples, hideUnusable]);
 
+  /*
+  Toggle whether or not to show samples that do not have files that
+  can be run on the current pipeline.
+   */
   const toggleUsable = () => setHideUnusable(!hideUnusable);
 
+  /*
+  Called independently for each sample when the selected file set is changed.
+   */
   const updateSelectedFiles = (previous, current) => {
     const ids = new Set(selected);
     ids.delete(previous);
@@ -72,6 +100,9 @@ export function LaunchFiles() {
     setSelected(Array.from(ids));
   };
 
+  /*
+  Called to remove a sample from the cart.
+   */
   const removeSampleFromCart = (sample, selectedId) => {
     removeSample(sample.project.id, sample.id).then(() => {
       setSamples(samples.filter((sample) => sample.id !== sample.id));
