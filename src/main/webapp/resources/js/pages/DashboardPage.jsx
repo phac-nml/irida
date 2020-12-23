d96607e541994cafe8cc572fe842a8e4cad02531
Katherine Thiessen <katherine.thiessen@gmail.com>
2020-12-23 10:22:35 -0600

    renaming Dashboard.jsx to DashboardPage.jsx

--- src/main/webapp/resources/js/pages/DashboardPage.jsx
+++ src/main/webapp/resources/js/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+
 import { AnnouncementDashboard } from "./announcement/AnnouncementDashboard";
 
 render(<AnnouncementDashboard />, document.querySelector("#dashboard-root"));
