527182be8960898f6b7868039c5a69fd7ea4d269
Katherine Thiessen <katherine.thiessen@gmail.com>
2021-01-05 10:17:03 -0600

    removing read announcements from dashboard

--- src/main/webapp/resources/js/pages/announcement/AnnouncementDashboard.jsx
+++ src/main/webapp/resources/js/pages/announcement/AnnouncementDashboard.jsx
@@ -2,27 +2,21 @@ import React, { useEffect, useState } from "react";
 import { List, notification, Tabs, Typography } from "antd";
 import { fromNow } from "../../utilities/date-utilities";
 import {
-  getReadAnnouncements,
   getUnreadAnnouncements,
   markAnnouncementRead,
 } from "../../apis/announcements/announcements";
 import { IconFlag } from "../../components/icons/Icons";
 import { blue6, grey2 } from "../../styles/colors";
-import ViewReadAnnouncement from "./ViewReadAnnouncement";
 import ViewUnreadAnnouncement from "./ViewUnreadAnnouncement";
 import Markdown from "react-markdown";
 
 export function AnnouncementDashboard() {
-  const [readAnnouncements, setReadAnnouncements] = useState([]);
   const [unreadAnnouncements, setUnreadAnnouncements] = useState([]);
   const [unreadTotal, setUnreadTotal] = useState(0);
   const { TabPane } = Tabs;
   const { Paragraph } = Typography;
 
   useEffect(() => {
-    getReadAnnouncements().then((data) => {
-      setReadAnnouncements(data.data);
-    });
     getUnreadAnnouncements().then((data) => {
       setUnreadAnnouncements(data.data);
       setUnreadTotal(data.data.length);
@@ -40,51 +34,28 @@ export function AnnouncementDashboard() {
   }
 
   return (
-    <Tabs defaultActiveKey="1">
-      <TabPane tab={"Unread (" + unreadAnnouncements.length + ")"} key="1">
-        <List
-          pagination={unreadTotal > 0 ? { pageSize: 5 } : false}
-          dataSource={unreadAnnouncements}
-          renderItem={(item) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={
-                  <IconFlag style={{ color: item.priority ? blue6 : grey2 }} />
-                }
-                title=<ViewUnreadAnnouncement
-                  announcement={item}
-                  markAnnouncementAsRead={markAnnouncementAsRead}
-                />
-                description={fromNow({ date: item.createdDate })}
-              />
-            </List.Item>
-          )}
-        />
-      </TabPane>
-      <TabPane tab={"Read (" + readAnnouncements.length + ")"} key="2">
-        <List
-          pagination={{ pageSize: 5 }}
-          dataSource={readAnnouncements}
-          renderItem={(item) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={
-                  <IconFlag
-                    style={{ color: item.subject.priority ? blue6 : grey2 }}
-                  />
-                }
-                title=<ViewReadAnnouncement announcement={item} />
-                description={fromNow({ date: item.subject.createdDate })}
-              />
-              {/*<Paragraph*/}
-              {/*  ellipsis={{ rows: 2, expandable: true, symbol: "more" }}*/}
-              {/*>*/}
-              {/*  <Markdown source={item.subject.message} />*/}
-              {/*</Paragraph>*/}
-            </List.Item>
-          )}
-        />
-      </TabPane>
-    </Tabs>
+    <List
+      pagination={unreadTotal > 5 ? { pageSize: 5 } : false}
+      dataSource={unreadAnnouncements}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={
+              <IconFlag style={{ color: item.priority ? blue6 : grey2 }} />
+            }
+            title=<ViewUnreadAnnouncement
+              announcement={item}
+              markAnnouncementAsRead={markAnnouncementAsRead}
+            />
+            description={fromNow({ date: item.createdDate })}
+          />
+          {/*<Paragraph*/}
+          {/*  ellipsis={{ rows: 2, expandable: true, symbol: "more" }}*/}
+          {/*>*/}
+          {/*  <Markdown source={item.message} />*/}
+          {/*</Paragraph>*/}
+        </List.Item>
+      )}
+    />
   );
 }
