fb639f3196300d47ecb44efdb476e478c70c2084
Katherine Thiessen <katherine.thiessen@gmail.com>
2021-01-08 13:13:23 -0600

    revert user link on user name in modal

--- src/main/webapp/resources/js/pages/announcement/ViewUnreadAnnouncement.jsx
+++ src/main/webapp/resources/js/pages/announcement/ViewUnreadAnnouncement.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOMServer from "react-dom/server";
 import { Modal, Typography } from "antd";
 import { VisibilityProvider } from "../../contexts/visibility-context";
 import { formatDate } from "../../utilities/date-utilities";
 import Markdown from "react-markdown";
 import { LinkButton } from "../../components/Buttons/LinkButton";
 import { PriorityFlag } from "./PriorityFlag";
-import { setBaseUrl } from "../../utilities/url-utilities";
+
 /**
  * Component to add a button which will open a modal to view a unread announcement.
  * @param {function} ViewAnnouncement - the function that displays a unread announcement.
@@ -16,20 +15,6 @@ import { setBaseUrl } from "../../utilities/url-utilities";
 const { Text } = Typography;
 const { confirm } = Modal;
 function ViewUnreadAnnouncementModal({ announcement, markAnnouncementAsRead }) {
-  function createUserLinkHTML() {
-    return {
-      __html: i18n(
-        "ViewUnreadAnnouncement.create.details",
-        ReactDOMServer.renderToString(
-          <a href={setBaseUrl(`/users/${announcement.user.identifier}`)}>
-            {announcement.user.username}
-          </a>
-        ),
-        formatDate({ date: announcement.createdDate })
-      ),
-    };
-  }
-
   const displayAnnouncement = () =>
     confirm({
       type: "info",
@@ -44,7 +29,11 @@ function ViewUnreadAnnouncementModal({ announcement, markAnnouncementAsRead }) {
           <Text strong>{announcement.title}</Text>
           <br />
           <Text type="secondary" style={{ fontSize: `.8em` }}>
-            <div dangerouslySetInnerHTML={createUserLinkHTML()} />
+            {i18n(
+              "ViewUnreadAnnouncement.create.details",
+              announcement.user.username,
+              formatDate({ date: announcement.createdDate })
+            )}
           </Text>
         </>
       ),
