18c9868fe28d252d3997411ef096244fcfd81f36
Katherine Thiessen <katherine.thiessen@gmail.com>
2021-01-12 13:03:50 -0600

    adding title to read messages page

--- src/main/webapp/resources/js/pages/announcement/announcements.js
+++ src/main/webapp/resources/js/pages/announcement/announcements.js
@@ -5,12 +5,14 @@ import "../../../css/modules/announcements.css";
 
 const markdownConverter = new showdown.Converter();
 
-// Change all the markdown in each announcment to HTML use showdown.
+// Change all the markdown in each announcement to HTML use showdown.
 $(".announcement-item").each((i, elm) => {
   const $elm = $(elm);
+  const title = $elm.data("title");
   const message = $elm.data("message");
   const createdDate = $elm.data("createddate");
   const readon = $elm.data("readon");
+  $elm.find(".title").html(title);
   $elm.find(".markdown").html(markdownConverter.makeHtml(message));
   $elm.find(".created-date").html(fromNow({ date: createdDate }));
   $elm.find(".readon").html(fromNow({ date: readon }));
