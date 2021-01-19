bef9ddd2579c31ebd29c2d406de838bb52f6af73
Katherine Thiessen <katherine.thiessen@gmail.com>
2021-01-19 13:10:33 -0600

    trying client-side list filtering

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/announcements/AnnouncementsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/announcements/AnnouncementsController.java
@@ -1,18 +1,11 @@
 package ca.corefacility.bioinformatics.irida.ria.web.announcements;
 
-import java.security.Principal;
-import java.util.List;
-
-import ca.corefacility.bioinformatics.irida.ria.web.announcements.dto.AnnouncementUserReadDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import ca.corefacility.bioinformatics.irida.model.announcements.Announcement;
-import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.ria.web.BaseController;
 import ca.corefacility.bioinformatics.irida.service.AnnouncementService;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
@@ -39,24 +32,12 @@ public class AnnouncementsController extends BaseController {
     }
 
     /**
-     * Gets a list of {@link Announcement}s for the current {@link User}
+     * Request for the page to display a list of read and unread announcements for user.
      *
-     * @param model
-     *              Model for the view
-     * @param principal
-     *              The user fetching the announcements (usually the one currently logged in)
-     * @return The announcement page containing announcement information for the user
+     * @return The name of the page.
      */
     @RequestMapping(value = "/user/list", method = RequestMethod.GET)
-    public String getAnnouncementsAsUser(final Model model, Principal principal) {
-
-        User user = userService.getUserByUsername(principal.getName());
-        List<AnnouncementUserReadDetails> announcements = announcementService.getAnnouncementsForUser(user);
-
-        logger.trace("Announcements list size: " + announcements.size());
-
-        model.addAttribute("announcements", announcements);
-
+    public String getAnnouncementsPage() {
         return ANNOUNCEMENT_LIST;
     }
 }
--- src/main/webapp/resources/js/pages/announcement/components/AnnouncementsPage.jsx
+++ src/main/webapp/resources/js/pages/announcement/components/AnnouncementsPage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { render } from "react-dom";
 import { PageWrapper } from "../../../components/page/PageWrapper";
 import { getAnnouncements } from "../../../apis/announcements/announcements";
-import { List } from "antd";
-import { fromNow } from "../../../utilities/date-utilities";
+import { Avatar, Form, List, Radio, Typography } from "antd";
+import { PriorityFlag } from "./PriorityFlag";
+import { formatDate } from "../../../utilities/date-utilities";
+
+const { Text } = Typography;
 
 /**
  * React component to display the page for administration of users.
@@ -12,26 +14,53 @@ import { fromNow } from "../../../utilities/date-utilities";
  */
 export function AnnouncementsPage({}) {
   const [announcements, setAnnouncements] = useState([]);
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
-    console.log(window.PAGE.announcements);
     getAnnouncements().then((data) => {
-      setAnnouncements(data.data);
-      console.log(data.data);
+      switch (filter) {
+        case "read":
+          return setAnnouncements(
+            data.data.filter((item) => item.announcementUserJoin !== null)
+          );
+        case "unread":
+          return setAnnouncements(
+            data.data.filter((item) => item.announcementUserJoin === null)
+          );
+        default:
+          return setAnnouncements(data.data);
+      }
     });
-  }, []);
+  }, [filter]);
 
   return (
     <PageWrapper title="Announcements">
+      <Form>
+        <Form.Item>
+          <Radio.Group
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <Radio.Button value="all">All</Radio.Button>
+            <Radio.Button value="unread">Unread</Radio.Button>
+            <Radio.Button value="read">Read</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+      </Form>
       <List
         dataSource={announcements}
         renderItem={(item) => (
           <List.Item className="t-announcement-item">
             <List.Item.Meta
-              title={item.announcement.title}
-              description={fromNow({ date: item.announcement.createdDate })}
+              avatar=<Avatar
+                style={{ backgroundColor: "#fff" }}
+                icon={<PriorityFlag hasPriority={item.announcement.priority} />}
+              />
+              title=<Text strong={item.announcementUserJoin ? false : true}>
+                {item.announcement.title}
+              </Text>
+              description={formatDate({ date: item.announcement.createdDate })}
             />
-            {item.announcement.message}
           </List.Item>
         )}
       />
