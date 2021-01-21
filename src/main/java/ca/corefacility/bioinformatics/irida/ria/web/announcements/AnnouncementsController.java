ab1aa2f7562e5d5fa1fe18511f1e9ddb9290beaa
Katherine Thiessen <katherine.thiessen@gmail.com>
2021-01-21 10:49:05 -0600

    removing unused code

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/announcements/AnnouncementsController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/announcements/AnnouncementsController.java
@@ -1,14 +1,10 @@
 package ca.corefacility.bioinformatics.irida.ria.web.announcements;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import ca.corefacility.bioinformatics.irida.ria.web.BaseController;
-import ca.corefacility.bioinformatics.irida.service.AnnouncementService;
-import ca.corefacility.bioinformatics.irida.service.user.UserService;
 
 /**
  * Controller for handling {@link ca.corefacility.bioinformatics.irida.model.announcements.Announcement} views
@@ -17,18 +13,10 @@ import ca.corefacility.bioinformatics.irida.service.user.UserService;
 @RequestMapping(value = "/announcements")
 public class AnnouncementsController extends BaseController {
 
-    private static final Logger logger = LoggerFactory.getLogger(AnnouncementsController.class);
-
     private static final String ANNOUNCEMENT_LIST = "announcements/list";
 
-    private final UserService userService;
-    private final AnnouncementService announcementService;
-
     @Autowired
-    public AnnouncementsController(UserService userService,
-                                   AnnouncementService announcementService) {
-        this.userService = userService;
-        this.announcementService = announcementService;
+    public AnnouncementsController() {
     }
 
     /**
--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/announcements/AnnouncementsControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/announcements/AnnouncementsControllerTest.java
@@ -1,49 +1,27 @@
 package ca.corefacility.bioinformatics.irida.web.controller.test.unit.announcements;
 
-import java.security.Principal;
-import java.util.List;
-import java.util.Map;
-
-import ca.corefacility.bioinformatics.irida.ria.web.clients.ClientsController;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.ui.ExtendedModelMap;
 
-import ca.corefacility.bioinformatics.irida.model.announcements.Announcement;
-import ca.corefacility.bioinformatics.irida.model.announcements.AnnouncementUserJoin;
-import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.ria.web.announcements.AnnouncementsController;
-import ca.corefacility.bioinformatics.irida.service.AnnouncementService;
-import ca.corefacility.bioinformatics.irida.service.user.UserService;
 
-import com.google.common.collect.Lists;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+
 
 /**
  * Unit tests for {@link AnnouncementsController}
  */
 public class AnnouncementsControllerTest {
 
-    //HTML Page names
     private static final String ANNOUNCEMENT_LIST = "announcements/list";
 
-    //Services
-    private UserService userService;
-    private AnnouncementService announcementService;
     private AnnouncementsController announcementsController;
 
     @Before
     public void setUp() {
-        userService = mock(UserService.class);
-        announcementService = mock(AnnouncementService.class);
-        announcementsController = new AnnouncementsController(userService, announcementService);
+        announcementsController = new AnnouncementsController();
     }
 
-    @SuppressWarnings("rawtypes")
 	@Test
     public void testGetAnnouncementsAsUser() {
         String page = announcementsController.getAnnouncementsPage();
