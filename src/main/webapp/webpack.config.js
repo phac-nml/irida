756846288cf7b98d326213c2b939fe2ca41baee2
Josh Adam <josh.adam@canada.ca>
2021-01-25 18:35:24 -0600

    Inital work

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,128 +1,34 @@
 const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const i18nThymeleafWebpackPlugin = require("./webpack/i18nThymeleafWebpackPlugin");
-const { formatAntStyles } = require("./styles");
 
-const dev = require("./webpack.config.dev");
-const prod = require("./webpack.config.prod");
-
-const entries = require("./entries.js");
-
-const BUILD_PATH = path.resolve(__dirname, "dist");
-
-const config = {
-  externals: {
-    jquery: "jQuery",
-  },
-  stats: {
-    children: false,
-    cached: false,
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-    alias: {
-      "./dist/cpexcel.js": "",
-    },
-  },
-  entry: entries,
+module.exports = {
+  entry: "./resources/js/app.js",
   output: {
-    path: BUILD_PATH,
-    publicPath: `/dist/`,
-    filename: "js/[name].bundle.js",
+    filename: "main.bundle.js",
+    path: path.resolve(__dirname, "dist/js"),
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: "babel-loader?cacheDirectory",
-      },
-      {
-        test: /\.less$/,
+        test: /\.css$/i,
         use: [
+          "style-loader",
+          "css-loader",
           {
-            loader: "style-loader",
-          },
-          {
-            loader: "css-loader", // translates CSS into CommonJS
-          },
-          {
-            loader: "less-loader", // compiles Less to CSS
+            loader: "postcss-loader",
             options: {
-              lessOptions: {
-                modifyVars: { ...formatAntStyles() },
-                javascriptEnabled: true,
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
               },
             },
           },
         ],
       },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: (loader) => [
-                require("postcss-import")({ root: loader.resourcePath }),
-                require("postcss-preset-env")(),
-                require("cssnano")(),
-                require("autoprefixer")(),
-                require("postcss-nested")(),
-              ],
-            },
-          },
-        ],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: {
-          loader: "url-loader",
-        },
-      },
-      {
-        test: /\.(ttf|eot|svg|otf|gif|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: {
-          loader: "file-loader",
-        },
-      },
-      {
-        test: require.resolve("jquery"),
-        use: [
-          {
-            loader: "expose-loader",
-            options: "$",
-          },
-          {
-            loader: "expose-loader",
-            options: "jQuery",
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/[name].bundle.css",
-    }),
-    new i18nThymeleafWebpackPlugin({
-      functionName: "i18n",
-    }),
-    new webpack.ProvidePlugin({
-      i18n: path.resolve(path.join(__dirname, "resources/js/i18n")),
-    }),
-  ],
-};
-
-module.exports = ({ mode = "development" }) => {
-  return merge(
-    { mode },
-    config,
-    mode === "production" ? prod.config : dev.config
-  );
+    ]}
 };
