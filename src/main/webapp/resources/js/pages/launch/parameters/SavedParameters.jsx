ec650472a24342b7b01286cb84c104368b5de0b1
Josh Adam <josh.adam@canada.ca>
2021-01-29 13:02:15 -0600

    Finished testing assembly pipeline layout

--- src/main/webapp/resources/js/pages/launch/parameters/SavedParameters.jsx
+++ src/main/webapp/resources/js/pages/launch/parameters/SavedParameters.jsx
@@ -60,12 +60,10 @@ export function SavedParameters({ form, sets }) {
     const fieldsValue = form.getFieldsValue(
       sets[0].parameters.map((parameter) => parameter.name)
     );
+    const name = saveParamsForm.getFieldValue("name");
+    // TODO: make sure this is unique
     try {
-      const id = await saveModifiedParametersAs(
-        dispatch,
-        saveParamsForm.getFieldValue("name"),
-        fieldsValue
-      );
+      const id = await saveModifiedParametersAs(dispatch, name, fieldsValue);
       setCurrentSetId(id);
       setModified({});
     } catch (e) {}
@@ -85,48 +83,50 @@ export function SavedParameters({ form, sets }) {
                 style={{ marginBottom: SPACE_XS, marginTop: SPACE_XS }}
                 type={"warning"}
                 message={i18n("SavedParameters.modified")}
-                description={i18n("SavedParameters.modified.description")}
-                action={
-                  <Space>
-                    <Popover
-                      placement="bottomRight"
-                      trigger="click"
-                      content={
-                        <>
-                          <Typography.Text>
-                            {i18n("SavedParameters.modified.name")}
-                          </Typography.Text>
-                          <Form
-                            form={saveParamsForm}
-                            layout="inline"
-                            style={{
-                              width: 305,
-                            }}
-                          >
-                            <Form.Item name="name" required>
-                              <Input className="t-modified-name" />
-                            </Form.Item>
-                            <Form.Item>
-                              <Button
-                                onClick={saveParameters}
-                                className="t-saveas-submit"
-                              >
-                                {i18n("SavedParameters.modified.save")}
-                              </Button>
-                            </Form.Item>
-                          </Form>
-                        </>
-                      }
-                    >
-                      <Button
-                        size="small"
-                        type="ghost"
-                        className="t-modified-saveas"
+                description={
+                  <div>
+                    {i18n("SavedParameters.modified.description")}{" "}
+                    <Space>
+                      <Popover
+                        placement="bottomRight"
+                        trigger="click"
+                        content={
+                          <>
+                            <Typography.Text>
+                              {i18n("SavedParameters.modified.name")}
+                            </Typography.Text>
+                            <Form
+                              form={saveParamsForm}
+                              layout="inline"
+                              style={{
+                                width: 305,
+                              }}
+                            >
+                              <Form.Item name="name" required>
+                                <Input className="t-modified-name" />
+                              </Form.Item>
+                              <Form.Item>
+                                <Button
+                                  onClick={saveParameters}
+                                  className="t-saveas-submit"
+                                >
+                                  {i18n("SavedParameters.modified.save")}
+                                </Button>
+                              </Form.Item>
+                            </Form>
+                          </>
+                        }
                       >
-                        {i18n("SavedParameters.modified.saveAs")}
-                      </Button>
-                    </Popover>
-                  </Space>
+                        <Button
+                          size="small"
+                          type="ghost"
+                          className="t-modified-saveas"
+                        >
+                          {i18n("SavedParameters.modified.saveAs")}
+                        </Button>
+                      </Popover>
+                    </Space>
+                  </div>
                 }
               />
             ) : null
--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pipelines/AssemblyPipelinePageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pipelines/AssemblyPipelinePageIT.java
@@ -1,10 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.pipelines;
 
 import org.junit.Test;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConfig;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.cart.CartPage;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
@@ -14,7 +13,6 @@ import static org.junit.Assert.*;
 /**
  * Testing for launching an assembly pipeline.
  */
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { IridaApiServicesConfig.class })
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/pipelines/AssemblyPipelinePageIT.xml")
 public class AssemblyPipelinePageIT extends BasePipelineLaunchPageIT {
 
@@ -32,127 +30,19 @@ public class AssemblyPipelinePageIT extends BasePipelineLaunchPageIT {
 		assertTrue("Should be able to select sample files", page.isLaunchFilesDisplayed());
 		assertFalse("This pipeline does not need a reference file, so there should be none requested", page.isReferenceFilesRequiredDisplayed());
 
-	}
+		// Test the name input
+		page.clearName();
+		assertTrue("There should be an error displayed that a name is required", page.isNameErrorDisplayed());
+		page.updateName("TEST_NAME");
+		assertFalse("Name required warning should be cleared", page.isNameErrorDisplayed());
 
-//
-//	@Autowired
-//	private AnalysisSubmissionRepository analysisSubmissionRepository;
-//
-//	@Before
-//	public void setUpTest() {
-//		page = new BasicPipelinePage(driver());
-//		cartPage = new CartPage(driver());
-//	}
-//
-//	@Test
-//	public void testPageSetup() {
-//		addSamplesToCartManager();
-//
-//		logger.info("Checking Assembly Page Setup.");
-//		assertEquals("Should display the correct number of samples.", 1, page.getNumberOfSamplesDisplayed());
-//	}
-//
-//	@Test
-//	public void testShareResultsWithSamples() {
-//		addSamplesToCartManager();
-//
-//		String analysisName = AssemblyPipelinePageIT.class.getName() + ".testShareResultsWithSamples";
-//		page.setNameForAnalysisPipeline(analysisName);
-//
-//		assertTrue("Share Results with Samples checkbox should exist", page.existsShareResultsWithSamples());
-//		page.clickShareResultsWithSamples();
-//		page.clickLaunchPipelineBtn();
-//		assertTrue("Message should be displayed once the pipeline finished submitting",
-//				page.isPipelineSubmittedSuccessMessageShown());
-//
-//		AnalysisSubmission submission = findAnalysisSubmissionWithName(analysisName + "_sample1");
-//
-//		assertNotNull("Analysis Submission is null", submission);
-//		assertTrue("updateSamples should be true", submission.getUpdateSamples());
-//	}
-//
-//	@Test
-//	public void testNoShareResultsWithSamples() {
-//		addSamplesToCartManager();
-//
-//		String analysisName = AssemblyPipelinePageIT.class.getName() + ".testNoShareResultsWithSamples";
-//		page.setNameForAnalysisPipeline(analysisName);
-//
-//		assertTrue("Share Results with Samples checkbox should exist", page.existsShareResultsWithSamples());
-//		page.clickLaunchPipelineBtn();
-//		assertTrue("Message should be displayed once the pipeline finished submitting",
-//				page.isPipelineSubmittedSuccessMessageShown());
-//
-//		AnalysisSubmission submission = findAnalysisSubmissionWithName(analysisName + "_sample1");
-//
-//		assertNotNull("Analysis Submission is null", submission);
-//		assertFalse("updateSamples should be false", submission.getUpdateSamples());
-//	}
-//
-//	@Test
-//	public void testUserNoShareResultsWithSamples() {
-//		addSamplesToCartUser();
-//
-//		String analysisName = AssemblyPipelinePageIT.class.getName() + ".testUserNoShareResultsWithSamples";
-//		page.setNameForAnalysisPipeline(analysisName);
-//
-//		assertFalse("Share Results with Samples checkbox should not exist", page.existsShareResultsWithSamples());
-//		page.clickLaunchPipelineBtn();
-//		assertTrue("Message should be displayed once the pipeline finished submitting",
-//				page.isPipelineSubmittedSuccessMessageShown());
-//
-//		AnalysisSubmission submission = findAnalysisSubmissionWithName(analysisName + "_sample1");
-//
-//		assertNotNull("Analysis Submission is null", submission);
-//		assertFalse("updateSamples should be false", submission.getUpdateSamples());
-//	}
-//
-//	private void addSamplesToCartUser() {
-//		LoginPage.loginAsUser(driver());
-//		addSamplesToCart();
-//	}
-//
-//	private void addSamplesToCartManager() {
-//		LoginPage.loginAsManager(driver());
-//		addSamplesToCart();
-//	}
-//
-//	private void addSamplesToCart() {
-//		ProjectSamplesPage samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
-//		samplesPage.selectSample(0);
-//		samplesPage.addSelectedSamplesToCart();
-//		cartPage.selectAssemblyPipeline();
-//	}
-//
-//	private AnalysisSubmission findAnalysisSubmissionWithName(String name) {
-//		AnalysisSubmission submission = null;
-//		for (AnalysisSubmission s : analysisSubmissionRepository.findAll()) {
-//			if (name.equals(s.getName())) {
-//				submission = s;
-//			}
-//		}
-//
-//		return submission;
-//	}
-//
-//	@Test
-//	public void testEmailPipelineResult() {
-//		addSamplesToCartManager();
-//
-//		String analysisName = AssemblyPipelinePageIT.class.getName() + ".testEmailPipelineResult";
-//		page.setNameForAnalysisPipeline(analysisName);
-//
-//		assertTrue("Email Pipeline Result status select should exist", page.existsEmailPipelineResultSelect());
-//		page.selectErrorFromEmailDropdown();
-//
-//		page.clickLaunchPipelineBtn();
-//		assertTrue("Message should be displayed once the pipeline finished submitting",
-//				page.isPipelineSubmittedSuccessMessageShown());
-//
-//		AnalysisSubmission submission = findAnalysisSubmissionWithName(analysisName + "_sample1");
-//
-//		assertNotNull("Analysis Submission is null", submission);
-//		assertFalse("emailPipelineResultCompleted should be true", submission.getEmailPipelineResultCompleted());
-//		assertTrue("emailPipelineResultError should be true", submission.getEmailPipelineResultError());
-//	}
+		// Make sure the saved pipeline parameter inputs are set up correctly
+		assertEquals("Assembly Pipeline should have 20 inputs", 20, page.getNumberOfSavedPipelineParameters());
+		assertFalse("Should not be displaying modified parameter alert", page.isModifiedAlertVisible());
+
+		// Test submitting
+		page.submit();
+		WebDriverWait wait = new WebDriverWait(driver(), 5);
+		wait.until(ExpectedConditions.urlMatches("/analysis/"));
+	}
 }
