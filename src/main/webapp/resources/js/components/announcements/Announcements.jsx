bfd4afbff5f155dc5398076bb28e881b981ca3c7
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-02-02 10:09:06 -0600

    working on displaying confirm modal

--- src/main/webapp/resources/js/components/announcements/Announcements.jsx
+++ src/main/webapp/resources/js/components/announcements/Announcements.jsx
@@ -10,83 +10,54 @@ import Markdown from "react-markdown";
 import { formatDate } from "../../utilities/date-utilities";
 
 const { Text } = Typography;
-
+const { confirm } = Modal;
 export function Announcements() {
-  const [announcements, setAnnouncements] = useState([]);
-  const [announcement, setAnnouncement] = useState({});
+  const [announcements, setAnnouncements] = useState();
   const [index, setIndex] = useState(0);
-  const [visible, setVisibility] = useState(true);
 
   useEffect(() => {
     getUnreadAnnouncements().then((data) => {
-      setAnnouncements(data.data);
-      setAnnouncement(data.data[index]);
+      console.log(data.data);
+      if (data.data) {
+        setAnnouncements(data.data);
+      }
     });
   }, []);
 
   useEffect(() => {
-    setAnnouncement(announcements[index]);
-    console.log(announcement);
-  }, [index]);
-
-  const incrementIndex = () => {
-    if (index == announcements.length - 1) {
-      setIndex(0);
-    } else {
-      setIndex(index + 1);
+    console.log("HEEEEEEELLLLLLLOOOOO?????????????");
+    if (announcements) {
+      console.log("HEEEEEEELLLLLLLOOOOO");
+      confirm({
+        type: "info",
+        width: `60%`,
+        icon: <PriorityFlag hasPriority={announcements[index].priority} />,
+        okText: `Read`,
+        onOk() {
+          setIndex(index + 1);
+          // markAnnouncementRead(announcement.identifier);
+        },
+        title: (
+          <>
+            <Text strong>{announcements[index].title}</Text>
+            <br />
+            <Text type="secondary" style={{ fontSize: `.8em` }}>
+              {i18n(
+                "ViewUnreadAnnouncement.create.details",
+                announcements[index].user.username,
+                formatDate({ date: announcements[index].createdDate })
+              )}
+            </Text>
+          </>
+        ),
+        content: (
+          <div style={{ overflowY: "auto", maxHeight: 600 }}>
+            <Markdown source={announcements[index].message} />
+          </div>
+        ),
+      });
     }
-  };
+  }, [announcements]);
 
-  const decrementIndex = () => {
-    if (index == 0) {
-      setIndex(announcements.length - 1);
-    } else {
-      setIndex(index - 1);
-    }
-  };
-
-  return (
-    <>
-      {announcement && announcement.user && (
-        <Modal
-          title={
-            <>
-              <Space>
-                <PriorityFlag hasPriority={announcement.priority} />
-                <Text strong>{announcement.title}</Text>
-              </Space>
-              <br />
-              <Text type="secondary" style={{ fontSize: `.8em` }}>
-                {i18n(
-                  "ViewUnreadAnnouncement.create.details",
-                  announcement.user.username,
-                  formatDate({ date: announcement.createdDate })
-                )}
-              </Text>
-            </>
-          }
-          onCancel={() => setVisibility(false)}
-          visible={visible}
-          width="60%"
-          footer={null}
-        >
-          <Row justify="space-between" align="middle">
-            <Col span={2} style={{ textAlign: "left" }}>
-              <IconLeft onClick={decrementIndex} />
-            </Col>
-            <Col span={20}>
-              <div key={announcement.identifier}>
-                <div style={{ overflowY: "auto", maxHeight: 600 }}>
-                  <Markdown source={announcement.message} />
-                </div>
-              </div>
-            </Col>
-            <Col span={2} style={{ textAlign: "right" }}>
-              <IconRight onClick={incrementIndex} />
-            </Col>
-          </Row>
-        </Modal>
-      )}
-    </>
-  );
+  return null;
 }
