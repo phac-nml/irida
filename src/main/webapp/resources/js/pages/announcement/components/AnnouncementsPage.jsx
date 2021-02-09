46ec42c1e86c68998024befc8b68a6cd952ca27b
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-02-09 12:20:38 -0600

    adding style so radio button group doesn't look disjointed

--- src/main/webapp/resources/js/pages/announcement/components/AnnouncementsPage.jsx
+++ src/main/webapp/resources/js/pages/announcement/components/AnnouncementsPage.jsx
@@ -4,7 +4,17 @@ import {
   getAnnouncements,
   markAnnouncementRead,
 } from "../../../apis/announcements/announcements";
-import { Avatar, Col, Empty, Form, List, notification, Radio, Row } from "antd";
+import {
+  Avatar,
+  Col,
+  Empty,
+  Form,
+  List,
+  notification,
+  Radio,
+  Row,
+  Space,
+} from "antd";
 import { PriorityFlag } from "./PriorityFlag";
 import { fromNow } from "../../../utilities/date-utilities";
 import ViewReadAnnouncement from "./ViewReadAnnouncement";
@@ -64,80 +74,85 @@ export function AnnouncementsPage({}) {
 
   return (
     <PageWrapper title="Announcements">
-      <Row justify="center">
-        <Col xs={24} sm={20} md={16} lg={12} xl={8}>
-          <Form>
-            <Form.Item>
-              <Radio.Group
-                value={filter}
-                onChange={(e) => setFilter(e.target.value)}
-              >
-                <Radio.Button value="all" className="t-all-announcements">
-                  {i18n("AnnouncementsPage.filter.all")}
-                </Radio.Button>
-                <Radio.Button value="unread" className="t-unread-announcements">
-                  {i18n("AnnouncementsPage.filter.unread")}
-                </Radio.Button>
-                <Radio.Button value="read" className="t-read-announcements">
-                  {i18n("AnnouncementsPage.filter.read")}
-                </Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
-      <Row justify="center">
-        <Col xs={24} sm={20} md={16} lg={12} xl={8}>
-          <List
-            bordered
-            locale={{
-              emptyText: (
-                <Empty
-                  image={Empty.PRESENTED_IMAGE_SIMPLE}
-                  description={i18n("AnnouncementsPage.emptyList")}
-                />
-              ),
-            }}
-            dataSource={filteredAnnouncements}
-            pagination={
-              filteredAnnouncements.length > 10 ? { pageSize: 10 } : false
-            }
-            renderItem={(item) => (
-              <List.Item
-                className="t-announcement-item"
-                style={item.read ? {} : { backgroundColor: grey2 }}
-              >
-                <List.Item.Meta
-                  avatar=<Avatar
-                    size="small"
-                    style={
-                      item.read
-                        ? { backgroundColor: "#fff" }
-                        : { backgroundColor: grey2 }
-                    }
-                    icon={<PriorityFlag hasPriority={item.priority} />}
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <Row justify="center">
+          <Col xs={24} sm={20} md={16} lg={12} xl={8}>
+            <Form>
+              <Form.Item noStyle>
+                <Radio.Group
+                  value={filter}
+                  onChange={(e) => setFilter(e.target.value)}
+                >
+                  <Radio.Button value="all" className="t-all-announcements">
+                    {i18n("AnnouncementsPage.filter.all")}
+                  </Radio.Button>
+                  <Radio.Button
+                    value="unread"
+                    className="t-unread-announcements"
+                  >
+                    {i18n("AnnouncementsPage.filter.unread")}
+                  </Radio.Button>
+                  <Radio.Button value="read" className="t-read-announcements">
+                    {i18n("AnnouncementsPage.filter.read")}
+                  </Radio.Button>
+                </Radio.Group>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+        <Row justify="center">
+          <Col xs={24} sm={20} md={16} lg={12} xl={8}>
+            <List
+              bordered
+              locale={{
+                emptyText: (
+                  <Empty
+                    image={Empty.PRESENTED_IMAGE_SIMPLE}
+                    description={i18n("AnnouncementsPage.emptyList")}
                   />
-                  title={
-                    item.read ? (
-                      <ViewReadAnnouncement
-                        announcementID={item.announcementID}
-                        announcementTitle={item.title}
-                      />
-                    ) : (
-                      <ViewUnreadAnnouncement
-                        announcementID={item.announcementID}
-                        announcementTitle={item.title}
-                        markAnnouncementAsRead={markAnnouncementAsRead}
-                      />
-                    )
-                  }
-                  description={fromNow({ date: item.createdDate })}
-                />
-              </List.Item>
-            )}
-          />
-        </Col>
-      </Row>
+                ),
+              }}
+              dataSource={filteredAnnouncements}
+              pagination={
+                filteredAnnouncements.length > 10 ? { pageSize: 10 } : false
+              }
+              renderItem={(item) => (
+                <List.Item
+                  className="t-announcement-item"
+                  style={item.read ? {} : { backgroundColor: grey2 }}
+                >
+                  <List.Item.Meta
+                    avatar=<Avatar
+                      size="small"
+                      style={
+                        item.read
+                          ? { backgroundColor: "#fff" }
+                          : { backgroundColor: grey2 }
+                      }
+                      icon={<PriorityFlag hasPriority={item.priority} />}
+                    />
+                    title={
+                      item.read ? (
+                        <ViewReadAnnouncement
+                          announcementID={item.announcementID}
+                          announcementTitle={item.title}
+                        />
+                      ) : (
+                        <ViewUnreadAnnouncement
+                          announcementID={item.announcementID}
+                          announcementTitle={item.title}
+                          markAnnouncementAsRead={markAnnouncementAsRead}
+                        />
+                      )
+                    }
+                    description={fromNow({ date: item.createdDate })}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+        </Row>
+      </Space>
     </PageWrapper>
   );
 }
