6a7b85119a81ae39f4b4bbbee82de48da3720f6f
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-02-10 11:43:56 -0600

    refactoring

---
+++ src/main/webapp/resources/js/pages/announcement/components/ViewAnnouncementModal.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Modal, notification, Typography } from "antd";
+import { formatDate } from "../../../utilities/date-utilities";
+import Markdown from "react-markdown";
+import { LinkButton } from "../../../components/Buttons/LinkButton";
+import { PriorityFlag } from "./PriorityFlag";
+import { getAnnouncement } from "../../../apis/announcements/announcements";
+
+const { Text } = Typography;
+const { confirm } = Modal;
+
+/**
+ * Component to add a button which will open a modal to view a unread announcement.
+ * @param {long} announcementID - the announcement identifier.
+ * @param {string} announcementTitle - the announcement title to be displayed in the link.
+ * @param {function} markAnnouncementAsRead - the function that marks an announcement as read.
+ * @returns {*}
+ * @constructor
+ */
+export default function ViewAnnouncementModal({
+  announcementID,
+  announcementTitle,
+  markAnnouncementAsRead,
+}) {
+  function showAnnouncement(aID) {
+    return getAnnouncement({ aID })
+      .then((data) => {
+        displayAnnouncement({ announcement: data });
+      })
+      .catch(({ message }) => {
+        notification.error({ message });
+      });
+  }
+
+  const displayAnnouncement = ({ announcement }) =>
+    confirm({
+      type: "info",
+      width: `60%`,
+      icon: <PriorityFlag hasPriority={announcement.priority} />,
+      okText: `Read`,
+      onOk() {
+        markAnnouncementAsRead(announcement.identifier);
+      },
+      title: (
+        <>
+          <Text strong>{announcement.title}</Text>
+          <br />
+          <Text type="secondary" style={{ fontSize: `.8em` }}>
+            {i18n(
+              "ViewAnnouncementModal.details",
+              announcement.user.username,
+              formatDate({ date: announcement.createdDate })
+            )}
+          </Text>
+        </>
+      ),
+      content: (
+        <div style={{ overflowY: "auto", maxHeight: 600, paddingRight: 10 }}>
+          <Markdown source={announcement.message} />
+        </div>
+      ),
+    });
+  return (
+    <>
+      <LinkButton
+        text={announcementTitle}
+        onClick={() => showAnnouncement(announcementID)}
+      />
+    </>
+  );
+}
--- src/main/webapp/resources/js/pages/announcement/components/ViewReadAnnouncement.jsx
+++ src/main/webapp/resources/js/pages/announcement/components/ViewReadAnnouncement.jsx
@@ -1,69 +1,6 @@
 import React from "react";
-import { Modal, notification, Typography } from "antd";
 import { VisibilityProvider } from "../../../contexts/visibility-context";
-import { formatDate } from "../../../utilities/date-utilities";
-import Markdown from "react-markdown";
-import { LinkButton } from "../../../components/Buttons/LinkButton";
-import { PriorityFlag } from "./PriorityFlag";
-import { getAnnouncement } from "../../../apis/announcements/announcements";
-
-const { Text } = Typography;
-const { info } = Modal;
-
-/**
- * Component to add a button which will open a modal to view a read announcement.
- * @param {long} announcementID - the announcement identifier.
- * @param {string} announcementTitle - the announcement title to be displayed in the link.
- * @returns {*}
- * @constructor
- */
-function ViewReadAnnouncementModal({ announcementID, announcementTitle }) {
-  function showAnnouncement(aID) {
-    return getAnnouncement({ aID })
-      .then((data) => {
-        displayAnnouncement({ announcement: data });
-      })
-      .catch(({ message }) => {
-        notification.error({ message });
-      });
-  }
-
-  const displayAnnouncement = ({ announcement }) =>
-    info({
-      type: "info",
-      width: `60%`,
-      icon: <PriorityFlag hasPriority={announcement.priority} />,
-      title: (
-        <>
-          <Text strong>{announcement.title}</Text>
-          <br />
-          <Text type="secondary" style={{ fontSize: `.8em` }}>
-            {i18n(
-              "ViewReadAnnouncement.details",
-              announcement.user.username,
-              formatDate({
-                date: announcement.createdDate,
-              })
-            )}
-          </Text>
-        </>
-      ),
-      content: (
-        <div style={{ overflowY: "auto", maxHeight: 600, paddingRight: 10 }}>
-          <Markdown source={announcement.message} />
-        </div>
-      ),
-    });
-
-  return (
-    <>
-      <LinkButton
-        text={announcementTitle}
-        onClick={() => showAnnouncement(announcementID)}
-      />
-    </>
-  );
-}
+import ViewAnnouncementModal from "./ViewAnnouncementModal";
 
 export default function ViewReadAnnouncement({
   announcementID,
@@ -71,7 +8,7 @@ export default function ViewReadAnnouncement({
 }) {
   return (
     <VisibilityProvider>
-      <ViewReadAnnouncementModal
+      <ViewAnnouncementModal
         announcementID={announcementID}
         announcementTitle={announcementTitle}
       />
