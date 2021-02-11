d4d2e732d92523da8d2b57ef3a54054a5db5a936
Josh Adam <josh.adam@canada.ca>
2021-02-11 11:12:15 -0600

    Updated settings comments.

--- src/main/webapp/resources/js/apis/projects/settings.js
+++ src/main/webapp/resources/js/apis/projects/settings.js
@@ -1,28 +1,51 @@
 import axios from "axios";
 import { setBaseUrl } from "../../utilities/url-utilities";
 
+/**
+ * Get the processing priorities information for the current project.
+ *
+ * @param {number} projectId - identifier for the current project
+ * @returns {Promise<any>} the current priority and list of available priorities
+ */
 export const fetchProcessingInformation = async (projectId) =>
-  fetch(setBaseUrl(`ajax/projects/${projectId}/settings/priorities`))
-    .then((response) => response.json())
-    .then((data) => data);
+  axios
+    .get(setBaseUrl(`ajax/projects/${projectId}/settings/priorities`))
+    .then(({ data }) => data);
 
+/**
+ * Update the processing priority for the current project
+ *
+ * @param {number} projectId - identifier for the current project
+ * @param {string} priority - priority to update the project to
+ * @returns {Promise<any>}
+ */
 export const updateProcessingPriority = async (projectId, priority) =>
-  fetch(
-    setBaseUrl(
-      `ajax/projects/${projectId}/settings/priority?priority=${priority}`
-    ),
-    {
-      method: `PUT`,
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => data.message);
+  axios
+    .put(
+      setBaseUrl(
+        `ajax/projects/${projectId}/settings/priority?priority=${priority}`
+      )
+    )
+    .then(({ data }) => data.message);
 
+/**
+ * Get the process coverage (minimum, maximum, and genome size) for the project
+ *
+ * @param {number} projectId - identifier for the current project
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 export const fetchProcessingCoverage = async (projectId) =>
-  fetch(setBaseUrl(`ajax/projects/${projectId}/settings/coverage`))
-    .then((response) => response.json())
-    .then((data) => data);
+  axios
+    .get(setBaseUrl(`ajax/projects/${projectId}/settings/coverage`))
+    .then(({ data }) => data);
 
+/**
+ * Update the process coverage (minimum, maximum, and genome size) for the project
+ *
+ * @param {number} projectId - identifier for the current project
+ * @param {object} coverage - updated coverage for the project
+ * @returns {Promise<*>}
+ */
 export const updateProcessingCoverage = async (projectId, coverage) => {
   try {
     const { data } = await axios.put(
@@ -35,6 +58,12 @@ export const updateProcessingCoverage = async (projectId, coverage) => {
   }
 };
 
+/**
+ * Get current analysis templates (automated pipelines) for the current project
+ *
+ * @param {number} projectId - identifier for the current project
+ * @returns {Promise<any>}
+ */
 export async function fetchAnalysisTemplatesForProject(projectId) {
   try {
     const { data } = await axios.get(
@@ -46,6 +75,12 @@ export async function fetchAnalysisTemplatesForProject(projectId) {
   }
 }
 
+/**
+ * Delete an existing analysis template (automated pipeline) from the current project
+ * @param {number} templateId - analysis template identifier
+ * @param {number} projectId - identifier for the current project
+ * @returns {Promise<*>}
+ */
 export async function deleteAnalysisTemplateForProject(templateId, projectId) {
   try {
     const { data } = await axios.delete(
