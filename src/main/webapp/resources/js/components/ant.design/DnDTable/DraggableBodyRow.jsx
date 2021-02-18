a4eaefb7937441d4390ff73c7dbb9523fb0c82f6
Josh Adam <josh.adam@canada.ca>
2021-02-18 12:58:16 -0600

    Made seperate component for a dnd table

---
+++ src/main/webapp/resources/js/components/ant.design/DnDTable/DraggableBodyRow.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
+
+const type = "DraggableBodyRow";
+
+export const DraggableBodyRow = ({
+  index,
+  moveRow,
+  className,
+  style,
+  ...restProps
+}) => {
+  const ref = React.useRef();
+  const [{ isOver, dropClassName }, drop] = useDrop({
+    accept: type,
+    collect: (monitor) => {
+      const { index: dragIndex } = monitor.getItem() || {};
+      if (dragIndex === index) {
+        return {};
+      }
+      return {
+        isOver: monitor.isOver(),
+        dropClassName:
+          dragIndex < index ? " drop-over-downward" : " drop-over-upward",
+      };
+    },
+    drop: (item) => {
+      moveRow(item.index, index);
+    },
+  });
+  const [, drag] = useDrag({
+    item: { type, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  drop(drag(ref));
+  return (
+    <tr
+      ref={ref}
+      className={`${className}${isOver ? dropClassName : ""}`}
+      style={{ cursor: "move", ...style }}
+      {...restProps}
+    />
+  );
+};
