6635d663c80c8681a892cd23d2a2870daba24570
Josh Adam <josh.adam@canada.ca>
2021-03-04 09:57:58 -0600

    Cleaning loaders

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -31,20 +31,8 @@ module.exports = {
       {
         test: /\.less$/i,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: (resourcePath, context) => {
-                // publicPath is the relative path of the resource to the context
-                // e.g. for ./css/admin/main.css the publicPath will be ../../
-                // while for ./css/main.css the publicPath will be ../
-                return path.relative(path.dirname(resourcePath), context) + "/";
-              },
-            },
-          },
-          {
-            loader: "css-loader",
-          },
+          MiniCssExtractPlugin.loader,
+          "css-loader",
           {
             loader: "less-loader",
             options: {
