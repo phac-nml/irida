60a4a851682cc4159cd089393f1830f39810b1d1
Josh Adam <josh.adam@canada.ca>
2021-03-05 12:52:24 -0600

    Cleaned up webpack config

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -9,100 +9,95 @@ const formatAntStyles = require("./styles");
 
 const antColours = formatAntStyles();
 
-const webpackConfig = {
-  cache: {
-    type: "filesystem",
-  },
-  entry: entries,
-  resolve: {
-    extensions: [".js", ".jsx"],
-    symlinks: false,
-  },
-  output: {
-    filename: "js/[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    pathinfo: false,
-  },
-  externals: {
-    jquery: "jQuery",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        include: path.resolve(__dirname, "resources/js"),
-        loader: "babel-loader",
-        options: {
-          cacheCompression: false,
-          cacheDirectory: true,
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    cache: {
+      type: "filesystem",
+    },
+    entry: entries,
+    resolve: {
+      extensions: [".js", ".jsx"],
+      symlinks: false,
+    },
+    output: {
+      filename: "js/[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      pathinfo: false,
+    },
+    externals: {
+      jquery: "jQuery",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          include: path.resolve(__dirname, "resources/js"),
+          loader: "babel-loader",
+          options: {
+            cacheCompression: false,
+            cacheDirectory: true,
+          },
         },
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "less-loader",
-            options: {
-              lessOptions: {
-                modifyVars: antColours,
-                javascriptEnabled: true,
+        {
+          test: /\.less$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "less-loader",
+              options: {
+                lessOptions: {
+                  modifyVars: antColours,
+                  javascriptEnabled: true,
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          { loader: "css-loader", options: { importLoaders: 1 } },
-          "postcss-loader",
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      ignoreOrder: true,
-      filename: "css/[name].bundle.css",
-    }),
-    new i18nThymeleafWebpackPlugin({
-      functionName: "i18n",
-    }),
-    new webpack.ProvidePlugin({
-      i18n: path.resolve(path.join(__dirname, "resources/js/i18n")),
-      process: "process/browser",
-    }),
-  ],
-};
-
-module.exports = (env, argv) => {
-  if (argv.mode === "development") {
-    webpackConfig.devtool = "source-map";
-    webpackConfig.optimization = {
-      minimize: false,
-    };
-  }
-
-  if (argv.mode === "production") {
-    webpackConfig.optimization = {
-      minimize: true,
-      minimizer: [
-        new CssMinimizerPlugin({ parallel: true }),
-        new TerserPlugin({ parallel: true, include: /\/resources/ }),
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
       ],
-    };
-  }
-
-  return webpackConfig;
+    },
+    optimization: {
+      ...(isProduction
+        ? {
+            minimize: true,
+            minimizer: [
+              new CssMinimizerPlugin({ parallel: true }),
+              new TerserPlugin({ parallel: true, include: /\/resources/ }),
+            ],
+          }
+        : { minimize: false }),
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        ignoreOrder: true,
+        filename: "css/[name].bundle.css",
+      }),
+      new i18nThymeleafWebpackPlugin({
+        functionName: "i18n",
+      }),
+      new webpack.ProvidePlugin({
+        i18n: path.resolve(path.join(__dirname, "resources/js/i18n")),
+        process: "process/browser",
+      }),
+    ],
+  };
 };
