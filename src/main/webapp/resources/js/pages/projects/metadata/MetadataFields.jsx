5742becb3f98fc9088942f690d0ecd67a9dd7e1e
Josh Adam <josh.adam@canada.ca>
2021-03-15 14:50:08 -0500

    Updated JSDoc on Reach Router parent components

--- src/main/webapp/resources/js/pages/projects/metadata/MetadataFields.jsx
+++ src/main/webapp/resources/js/pages/projects/metadata/MetadataFields.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-
+/**
+ * Component that wrap all components that handle metadata fields.  Required
+ * for Reach Router nesting.
+ *
+ * @param children
+ * @returns {*}
+ * @constructor
+ */
 export function MetadataFields({ children }) {
   return children;
 }
