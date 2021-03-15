16b1dd8044cdcd499333271a938b6d7ee357b5c1
Josh Adam <josh.adam@canada.ca>
2021-03-15 14:47:54 -0500

    Updated JSDoc

--- src/main/webapp/resources/js/pages/projects/metadata/MetadataFieldCreate.jsx
+++ src/main/webapp/resources/js/pages/projects/metadata/MetadataFieldCreate.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { PageHeader, Space } from "antd";
 
+/**
+ * Component to add a new metadata field to a project
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function MetadataFieldCreate({}) {
   return (
     <PageHeader title={"Create Metadata Field"}>
