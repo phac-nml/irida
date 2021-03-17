abfa20037bdc478283c3cf82b8ed5ad6cc30c030
Josh Adam <josh.adam@canada.ca>
2021-03-17 13:33:36 -0500

    Inital tests setup

---
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/projects/ProjectMetadataPage.java
@@ -0,0 +1,18 @@
+package ca.corefacility.bioinformatics.irida.ria.integration.pages.projects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.AbstractPage;
+
+public class ProjectMetadataPage extends AbstractPage {
+
+	public ProjectMetadataPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public static ProjectMetadataPage goTo(WebDriver driver) {
+		get(driver, "/projects/1/settings/metadata");
+		return PageFactory.initElements(driver, ProjectMetadataPage.class);
+	}
+}
