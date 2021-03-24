5ab3ce2704511781fdab00d2ea897ec1286063df
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-03-24 07:59:02 -0500

    fixing redirect issue for authorization_code oauth2

--- src/main/java/ca/corefacility/bioinformatics/irida/config/web/OpenAPIConfig.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/config/web/OpenAPIConfig.java
@@ -2,8 +2,6 @@ package ca.corefacility.bioinformatics.irida.config.web;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.extensions.Extension;
-import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.*;
@@ -23,14 +21,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SecurityScheme(
         name = "oauth2",
         type = SecuritySchemeType.OAUTH2,
-        flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "/api/oauth/authorize", tokenUrl = "/api/oauth/token", scopes = {@OAuthScope(name = "read", description = "This is the read scope.")}),
-                password = @OAuthFlow(tokenUrl = "/api/oauth/token"))
-)
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "/api/oauth/authorize", tokenUrl = "/api/oauth/token",
+                scopes = {@OAuthScope(name = "read", description = "This is the read scope."), @OAuthScope(name = "write", description = "This is the write scope.")}),
+                password = @OAuthFlow(tokenUrl = "/api/oauth/token")))
 @OpenAPIDefinition(
         info = @Info(title = "IRIDA REST API", version = "v1",
                 description = "The IRIDA REST API follows a standard output format, regardless of the resource being accessed. Resources can be accessed as an individual resource or as part of a resource collection.",
                 contact = @Contact(name = "the Bioinformatics Team", email = "helpdesk@cscscience.ca")),
-        security = @SecurityRequirement(name = "oauth2", scopes = {"read"})
-)
+        security = @SecurityRequirement(name = "oauth2", scopes = {"read", "write"}))
 public class OpenAPIConfig implements WebMvcConfigurer {
 }
--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/RESTAnalysisSubmissionController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/RESTAnalysisSubmissionController.java
@@ -27,7 +27,6 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
