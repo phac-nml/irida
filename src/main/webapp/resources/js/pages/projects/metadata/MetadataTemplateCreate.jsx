f53975c40e096c37ad154fc96c6ad2ee2da6ed31
Josh Adam <josh.adam@canada.ca>
2021-03-24 20:10:35 -0500

    Fixed i18n keys

--- src/main/webapp/resources/js/pages/projects/metadata/MetadataTemplateCreate.jsx
+++ src/main/webapp/resources/js/pages/projects/metadata/MetadataTemplateCreate.jsx
@@ -118,7 +118,9 @@ export function MetadataTemplateCreate({ children, projectId, fields = [] }) {
                   <>
                     <Text strong>{i18n("MetadataTemplate.fields")}</Text>
                     <HelpPopover
-                      content={<div>{i18n("MetadataTemplateManager.drag")}</div>}
+                      content={
+                        <div>{i18n("MetadataTemplateManager.drag")}</div>
+                      }
                     />
                   </>
                 ),
