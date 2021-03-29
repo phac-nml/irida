1671255ad5705813f10e13499a4159e78788e8d8
Josh Adam <josh.adam@canada.ca>
2021-03-29 11:51:18 -0500

    Updated JSDoc

--- src/main/webapp/resources/js/pages/projects/redux/coverageSlice.js
+++ src/main/webapp/resources/js/pages/projects/redux/coverageSlice.js
@@ -4,10 +4,25 @@ import {
   putProcessingCoverage,
 } from "../../../apis/projects/settings";
 
+/**
+ * Internationalized term if the processing coverage is not set.
+ * @constant
+ * @type {string}
+ */
 const NOT_SET = i18n("ProcessingCoverage.not-set");
 
+/**
+ * Redux async thunk for fetching the project coverage details (minimum, maximum, and genome size).
+ * Condition: will only be called once since this does not need to be updated on every view.
+ * @type {AsyncThunk<{genomeSize: *|string, maximum: *|string, minimum: *|string}, void, {}>}
+ */
 export const fetchProjectCoverage = createAsyncThunk(
   `processing/fetchProjectCoverage`,
+  /**
+   * Get the processing coverage for a specific project
+   * @param {number} projectId - identifier for the current project;
+   * @returns {Promise<{genomeSize: (*|string), maximum: (*|string), minimum: (*|string)}>}
+   */
   async (projectId) => {
     const { minimum, maximum, genomeSize } = await fetchProcessingCoverage(
       projectId
@@ -31,8 +46,18 @@ export const fetchProjectCoverage = createAsyncThunk(
   }
 );
 
+/**
+ * Redux async thunk for updating the processing coverage on a project.
+ * @type {AsyncThunk<{[p: string]: *}, {readonly coverage?: *, readonly projectId?: *}, {}>}
+ */
 export const updateProcessingCoverage = createAsyncThunk(
   `processing/updateProcessingCoverage`,
+  /**
+   * Update processing coverage on a project
+   * @param {number} projectId - identifier for the current project
+   * @param {Object} coverage - Details about the new coverage (minimum, maximum, genomeSize)
+   * @returns {Promise<{[p: string]: *}>}
+   */
   async ({ projectId, coverage }) => {
     const message = await putProcessingCoverage(projectId, coverage);
     return {
