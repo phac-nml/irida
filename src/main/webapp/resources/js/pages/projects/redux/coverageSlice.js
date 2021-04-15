2e9c9e812be3d01f4a60035da9251f5d2d440fa6
Josh Adam <josh.adam@canada.ca>
2021-04-15 13:51:36 -0500

    Removed unused coma

--- src/main/webapp/resources/js/pages/projects/redux/coverageSlice.js
+++ src/main/webapp/resources/js/pages/projects/redux/coverageSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchProcessingCoverage, } from "../../../apis/projects/settings";
+import { fetchProcessingCoverage } from "../../../apis/projects/settings";
 
 /**
  * Internationalized term if the processing coverage is not set.
