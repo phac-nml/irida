480f13d1fdbeae45ca3de129acedec844b0247b9
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-05-19 11:03:44 -0500

    starting to use DraggerUpload component

--- src/main/webapp/resources/js/pages/projects/samples-metadata-import2/components/SampleMetadataFileUploader.jsx
+++ src/main/webapp/resources/js/pages/projects/samples-metadata-import2/components/SampleMetadataFileUploader.jsx
@@ -1,26 +1,40 @@
 import React from "react";
 import {
   Alert,
+  Space,
   Typography,
-  Space
 } from "antd";
-
-const { Text, Title, Paragraph } = Typography
+import { DragUpload } from "../../../../components/files/DragUpload";
 
 export function SampleMetadataFileUploader() {
+  const { Text, Title, Paragraph } = Typography
+  const options = {
+    multiple: true,
+    showUploadList: false,
+  }
+
   return (
-    <Typography>
-      <Title level={3}>{i18n("metadata.upload.component.title")}</Title>
-      <Paragraph>{i18n("metadata.upload.component.intro")}</Paragraph>
-      <Alert
-        message={
-          <Space direction="vertical">
-            <Text>{i18n("metadata.upload.component.text")}</Text>
-            <Text strong={true}>{i18n("metadata.upload.component.warning")}</Text>
-          </Space>
-        }
-        type="info"
-      />
-    </Typography>
+    <>
+      <Space direction="vertical">
+        <Typography>
+          <Title level={3}>{i18n("metadata.upload.component.title")}</Title>
+          <Paragraph>{i18n("metadata.upload.component.intro")}</Paragraph>
+          <Alert
+            message={
+              <Space direction="vertical">
+                <Text>{i18n("metadata.upload.component.text")}</Text>
+                <Text strong={true}>{i18n("metadata.upload.component.warning")}</Text>
+              </Space>
+            }
+            type="info"
+          />
+        </Typography>
+        <DragUpload
+          className="t-sample-metadata-file-uploader"
+          uploadText={i18n("metadata.dropzone.message")}
+          options={options}
+        />
+      </Space>
+    </>
   );
 }
