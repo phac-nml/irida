776dbc530e962781bd3501ae5a6b328c73c9a03f
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-05-21 14:09:27 -0500

    Starting to add ant design steps

--- src/main/webapp/resources/js/pages/projects/samples-metadata-import2/components/SampleMetadataFileUploader.jsx
+++ src/main/webapp/resources/js/pages/projects/samples-metadata-import2/components/SampleMetadataFileUploader.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 import {
   Alert,
+  Divider,
+  message,
   Space,
+  Steps,
   Typography,
 } from "antd";
 import { DragUpload } from "../../../../components/files/DragUpload";
+import { setBaseUrl } from "../../../../utilities/url-utilities";
 
 export function SampleMetadataFileUploader() {
+  const { Step } = Steps;
   const { Text, Title, Paragraph } = Typography
   const options = {
-    multiple: true,
-    showUploadList: false,
+    multiple: false,
+    showUploadList: true,
+    accept: [".xls", ".xlsx", ".csv"],
+    // action: setBaseUrl(`/file`),
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
   }
 
   return (
     <>
+      <Steps current={0}>
+        <Step title="Upload File" />
+        <Step title="Verify Headers" />
+        <Step title="Review Data" />
+        <Step title="Complete" />
+      </Steps>
+      <Divider />
       <Space direction="vertical">
         <Typography>
           <Title level={3}>{i18n("metadata.upload.component.title")}</Title>
