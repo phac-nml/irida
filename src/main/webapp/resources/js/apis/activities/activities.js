f472a6e20800c2c4dc24d9dadd0c1ef7685fbea6
Josh Adam <josh.adam@canada.ca>
2021-05-25 12:59:25 -0500

    ated JSDoc

--- src/main/webapp/resources/js/apis/activities/activities.js
+++ src/main/webapp/resources/js/apis/activities/activities.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+/**
+ * @file API for handling activities
+ */
+
 const BASE_URL = `/ajax/activities`;
 
+/**
+ * Get a page of activities for a project
+ *
+ * @param {number} projectId - current project identifier
+ * @param {number} page - page of activities requested
+ * @returns {Promise<AxiosResponse<any>>}
+ */
 export function getProjectActivities({ projectId, page = 0 }) {
   return axios
     .get(`${BASE_URL}/project?projectId=${projectId}&page=${page}`)
