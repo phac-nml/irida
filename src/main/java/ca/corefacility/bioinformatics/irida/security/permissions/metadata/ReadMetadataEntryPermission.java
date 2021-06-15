8bdfea88fedb1bcd281dd0c619663b2ba4b270ac
Tom Matthews <t.c.matthews@gmail.com>
2021-06-15 11:19:57 -0500

    fixed docs issues in basepermission and readentrypermission

--- src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/metadata/ReadMetadataEntryPermission.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/security/permissions/metadata/ReadMetadataEntryPermission.java
@@ -2,7 +2,7 @@ package ca.corefacility.bioinformatics.irida.security.permissions.metadata;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -17,22 +17,16 @@ import ca.corefacility.bioinformatics.irida.model.sample.metadata.MetadataRestri
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.repositories.joins.project.ProjectSampleJoinRepository;
 import ca.corefacility.bioinformatics.irida.repositories.joins.project.ProjectUserJoinRepository;
-import ca.corefacility.bioinformatics.irida.repositories.joins.project.UserGroupProjectJoinRepository;
 import ca.corefacility.bioinformatics.irida.repositories.sample.MetadataEntryRepository;
 import ca.corefacility.bioinformatics.irida.repositories.sample.MetadataRestrictionRepository;
-import ca.corefacility.bioinformatics.irida.repositories.user.UserGroupJoinRepository;
 import ca.corefacility.bioinformatics.irida.repositories.user.UserRepository;
-import ca.corefacility.bioinformatics.irida.security.permissions.BasePermission;
 import ca.corefacility.bioinformatics.irida.security.permissions.RepositoryBackedPermission;
 
+/**
+ * Permission for reading individual {@link MetadataEntry} for a sample
+ */
 @Component
 public class ReadMetadataEntryPermission extends RepositoryBackedPermission<MetadataEntry, Long> {
-	/**
-	 * Constructor with handles on the type of repository and type of domain
-	 * object.
-	 *
-	 * @param repository the repository to load objects of the type for this
-	 */
 
 	public static final String PERMISSION_PROVIDED = "canReadMetadataEntry";
 
@@ -42,6 +36,7 @@ public class ReadMetadataEntryPermission extends RepositoryBackedPermission<Meta
 
 	UserRepository userRepository;
 
+	@Autowired
 	public ReadMetadataEntryPermission(MetadataEntryRepository repository,
 			ProjectSampleJoinRepository projectSampleJoinRepository,
 			MetadataRestrictionRepository restrictionRepository, ProjectUserJoinRepository projectUserJoinRepository,
@@ -77,7 +72,7 @@ public class ReadMetadataEntryPermission extends RepositoryBackedPermission<Meta
 			ProjectUserJoin projectJoinForUser = projectUserJoinRepository.getProjectJoinForUser(project, user);
 
 			ProjectRole userProjectRole = ProjectRole.PROJECT_USER;
-			if(projectJoinForUser != null){
+			if (projectJoinForUser != null) {
 				userProjectRole = projectJoinForUser.getProjectRole();
 			}
 
@@ -86,7 +81,8 @@ public class ReadMetadataEntryPermission extends RepositoryBackedPermission<Meta
 				restrictionForFieldAndProject = new MetadataRestriction(project, field, ProjectRole.PROJECT_USER);
 			}
 
-			if(userProjectRole.getLevel() >= restrictionForFieldAndProject.getLevel().getLevel()){
+			if (userProjectRole.getLevel() >= restrictionForFieldAndProject.getLevel()
+					.getLevel()) {
 				return true;
 			}
 		}
