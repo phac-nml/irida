997f78d5c0a9506ed4574b24c3b0d7bee3ed612f
Deep Sidhu <deepsidhu85@gmail.com>
2021-06-29 11:27:31 -0500

    Added server code to get automated single sample analysis outputs and updated AutomatedSingleSampleAnalysisOutputs ui component to display these outputs in a table

--- src/main/webapp/resources/js/apis/projects/analyses.js
+++ src/main/webapp/resources/js/apis/projects/analyses.js
@@ -5,20 +5,30 @@ import { setBaseUrl } from "../../utilities/url-utilities";
  * API for CRUD operations for Shared Single Sample Analysis Outputs
  * @type {Api<(args: (string | FetchArgs), api: BaseQueryApi, extraOptions: {}) => MaybePromise<QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>>, {getAssociatedProjects: *, addAssociatedProject: *, removeAssociatedProject: *}, string, string, typeof coreModuleName> | Api<(args: (string | FetchArgs), api: BaseQueryApi, extraOptions: {}) => MaybePromise<QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>>, {getAssociatedProjects: *, addAssociatedProject: *, removeAssociatedProject: *}, string, string, typeof coreModuleName | typeof reactHooksModuleName>}
  */
+
 export const singleSampleAnalysisOutputsApi = createApi({
   reducerPath: `singleSampleAnalysisOutputsApi`,
   baseQuery: fetchBaseQuery({
-    baseUrl: setBaseUrl(`/ajax/projects/analyses-outputs`),
+    baseUrl: "/ajax/projects/analyses-outputs",
   }),
   tagTypes: ["ProjectSampleAnalysisOutputInfo"],
   endpoints: (build) => ({
     getSharedSingleSampleAnalysisOutputs: build.query({
-      query: (projectId) => ({ url: "shared", params: { projectId } }),
+      query: (projectId) => ({ url: "/shared", params: { projectId } }),
       providesTags: ["ProjectSampleAnalysisOutputInfo"],
+    }),
+    getAutomatedSingleSampleAnalysisOutputs: build.query({
+      query: (projectId) => ({
+        url: `/automated`,
+        params: { projectId },
+        method: `GET`,
+      }),
+      invalidateTags: ["ProjectSampleAnalysisOutputInfo"],
     }),
   }),
 });
 
 export const {
   useGetSharedSingleSampleAnalysisOutputsQuery,
+  useGetAutomatedSingleSampleAnalysisOutputsQuery,
 } = singleSampleAnalysisOutputsApi;
--- src/main/webapp/resources/js/pages/projects/project-analyses/components/AutomatedSingleSampleAnalysisOutputs.jsx
+++ src/main/webapp/resources/js/pages/projects/project-analyses/components/AutomatedSingleSampleAnalysisOutputs.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Typography } from "antd";
+import SingleSampleAnalysisOutputs from "./SingleSampleAnalysisOutputs";
+import { useGetAutomatedSingleSampleAnalysisOutputsQuery } from "../../../../apis/projects/analyses";
 const { Title } = Typography;
 
-export default function AutomatedSingleSampleAnalysisOutputs() {
+export default function AutomatedSingleSampleAnalysisOutputs({ projectId }) {
+  const {
+    data: automatedSingleSampleAnalysisOutputs = {},
+    isLoading,
+  } = useGetAutomatedSingleSampleAnalysisOutputsQuery(projectId);
+
   return (
-    <Title level={2}>
-      {i18n("AutomatedSingleSampleAnalysisOutputs.title")}
-    </Title>
+    <>
+      <Title level={2}>
+        {i18n("AutomatedSingleSampleAnalysisOutputs.title")}
+      </Title>
+      <SingleSampleAnalysisOutputs
+        outputs={automatedSingleSampleAnalysisOutputs}
+        isLoading={isLoading}
+        projectId={projectId}
+      />
+    </>
   );
 }
