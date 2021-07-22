bbdd7a72b666102dbe658289f1b662fd4168f741
Josh Adam <josh.adam@canada.ca>
2021-07-22 11:05:21 -0500

    Placeholder list of samples

--- src/main/webapp/resources/js/pages/projects/samples-share/index.js
+++ src/main/webapp/resources/js/pages/projects/samples-share/index.js
@@ -1,17 +1,51 @@
-import { Button } from "antd";
+import { Router } from "@reach/router";
 import React from "react";
 import { render } from "react-dom";
-import { IconShare } from "../../../components/icons/Icons";
+import { setBaseUrl } from "../../../utilities/url-utilities";
 
-export function SamplesShareButton() {
+/**
+ * React component for the layout of the share samples page.
+ *
+ * @param {number} projectId - identifier for the current project
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export function ShareSamplesLayout({ projectId }) {
+  const [samples, setSamples] = React.useState([]);
+
+  React.useEffect(() => {
+    /*
+    When this component is rendered we get the original data out of session
+     */
+    const dataString = window.sessionStorage.getItem(`share-${projectId}`);
+    if (!dataString) {
+      window.location.href = setBaseUrl(`/projects/${projectId}`);
+    }
+    const dataJson = JSON.parse(dataString);
+    // TODO: Use redux to store this data
+    setSamples(dataJson);
+  }, [projectId]);
+
   return (
-    <Button type="link" icon={<IconShare />}>
-      Share Samples
-    </Button>
+    <>
+      <p>
+        This is just a placeholder and a check to make sure that the samples
+        have been retrieved from storage
+      </p>
+      <ul>
+        {samples.map((sample) => (
+          <li key={sample.name}>{sample.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
 render(
-  <h1>Share Samples with Another Project</h1>,
+  <Router>
+    <ShareSamplesLayout
+      path={setBaseUrl(`/projects/:projectId/samples-share`)}
+    />
+  </Router>,
   document.querySelector("#root")
 );
--- src/main/webapp/resources/js/pages/projects/samples/samples-share/index.js
+++ src/main/webapp/resources/js/pages/projects/samples/samples-share/index.js
@@ -2,10 +2,20 @@ import { Button } from "antd";
 import React from "react";
 import { render } from "react-dom";
 import { IconShare } from "../../../../components/icons/Icons";
+import { grey9 } from "../../../../styles/colors";
+import { setBaseUrl } from "../../../../utilities/url-utilities";
 
+/**
+ * React component to render a button to link to the share samples page.
+ * To speed this up, it saves the selected samples to session storage for
+ * performance issues.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function SamplesShareButton() {
   const shareSamples = () => {
     const selected = window.$dt.select.selected()[0];
+    const projectId = selected.values().next().value.projectId;
     const samples = [];
     selected.forEach(({ id, sampleName: name, owner }) => {
       samples.push({
@@ -14,11 +24,32 @@ export function SamplesShareButton() {
         owner,
       });
     });
-    console.log(samples);
+
+    /*
+    We are going to store the sample information into session storage since it can
+    become massive request and thn data is already present on the client.
+     */
+    if (samples.length) {
+      window.sessionStorage.setItem(
+        `share-${projectId}`,
+        JSON.stringify(samples)
+      );
+      window.location.href = setBaseUrl(`/projects/${projectId}/samples-share`);
+    }
   };
 
   return (
-    <Button type="link" icon={<IconShare />} onClick={shareSamples}>
+    <Button
+      type="link"
+      icon={<IconShare style={{ marginRight: 3, color: grey9 }} />}
+      style={{
+        margin: `0 inherit`,
+        padding: 0,
+        paddingLeft: 20,
+        color: grey9,
+      }}
+      onClick={shareSamples}
+    >
       {i18n("SamplesShareButton.label")}
     </Button>
   );
