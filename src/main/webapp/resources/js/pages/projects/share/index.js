be66514de68f04f4eebb389dcfa16fd8c7696f56
Josh Adam <josh.adam@canada.ca>
2021-08-18 09:34:16 -0500

    Documented component not look at.

--- src/main/webapp/resources/js/pages/projects/share/index.js
+++ src/main/webapp/resources/js/pages/projects/share/index.js
@@ -1,13 +1,36 @@
+import { Table } from "antd";
 import React from "react";
 import { render } from "react-dom";
 
+/**
+ * Base component for sharing samples between projects.
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function ShareSamples() {
+  const [samples, setSamples] = React.useState();
+
+  /*
+  CURRENTLY THIS IS JUST TO HAVE SOMETHING ON THE PAGE AND SHOW THAT THE SAMPLES
+  GET HERE.  DON'T WORRY TOO MUCH ABOUT LOOKING THROUGH THIS YET.
+   */
+
   React.useEffect(() => {
     const stringData = window.sessionStorage.getItem("share");
     const data = JSON.parse(stringData);
-    console.log(data);
+
+    setSamples(data.samples);
   }, []);
-  return <div>JELLO</div>;
+
+  return (
+    <Table
+      loading={!samples}
+      dataSource={samples}
+      rowKey={(sample) => `sample-${sample.id}`}
+      columns={[{ title: "Name", dataIndex: "name" }]}
+    />
+  );
 }
 
 render(<ShareSamples />, document.querySelector("#root"));
