8dcd1ef02553ac19c1f318757d71066a9dbb9126
Josh Adam <josh.adam@canada.ca>
2021-08-31 14:15:28 -0500

    Removed unused annotatons

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/config/thymeleaf/webpacker/util/WebpackEntry.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/config/thymeleaf/webpacker/util/WebpackEntry.java
@@ -1,30 +1,16 @@
 package ca.corefacility.bioinformatics.irida.ria.config.thymeleaf.webpacker.util;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JacksonAnnotation;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Each webpack entry with have at least one JavaScript file, and possibly css files and html (translatons)
  */
 public class WebpackEntry {
-	@JsonProperty("js")
-	List<String> javascript;
+	private List<String> javascript;
 
-	@JsonProperty("css")
-	List<String> css;
+	private List<String> css;
 
-	@JsonProperty("html")
-	List<String> html;
-
-	@Retention(RetentionPolicy.RUNTIME)
-	@JacksonAnnotation
-	public @interface SkipAssetsWrapper {
-		String value();
-	}
+	private List<String> html;
 
 	public List<String> getJavascript() {
 		return javascript;
