b45f74e80428f3830464660c9e304d046f4d84be
Tom Matthews <t.c.matthews@gmail.com>
2021-09-15 08:53:59 -0500

    added explanation of the 2 queries instead of one

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/MetadataFieldRepositoryImpl.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/sample/MetadataFieldRepositoryImpl.java
@@ -33,15 +33,21 @@ public class MetadataFieldRepositoryImpl implements MetadataFieldRepositoryCusto
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<MetadataTemplateField> getMetadataFieldsForProject(Project p) {
-
 		NamedParameterJdbcTemplate tmpl = new NamedParameterJdbcTemplate(dataSource);
 		MapSqlParameterSource parameters = new MapSqlParameterSource();
 
-		//get the metadata fields available in the project
+		/*
+		 * The below code for loading the metadata fields could be simplified into a single query, but the previous
+		 * 3-join query was taking 10s of seconds to load for large projects.  The 2 query process below for loading the
+		 * fields is providing significantly better performance than loading fields in a single 3-join query.
+		 */
+
+		//First load all the field IDs related to this project
 		String fieldIdQueryString = "SELECT DISTINCT e.field_id FROM metadata_entry e INNER JOIN project_sample p ON e.sample_id=p.sample_id WHERE p.project_id=:project";
 		parameters.addValue("project", p.getId());
 		List<Long> fieldIds = tmpl.queryForList(fieldIdQueryString, parameters, Long.class);
 
+		//next load all the full fields with those ids
 		String queryString = "SELECT * from metadata_field f WHERE f.id IN :fields";
 		Query nativeQuery = entityManager.createNativeQuery(queryString, MetadataTemplateField.class);
 		nativeQuery.setParameter("fields", fieldIds);
