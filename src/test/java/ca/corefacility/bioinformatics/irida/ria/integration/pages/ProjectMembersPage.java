fc0dc36c5f5e15c49131d75cd9d299ff250ed0f1
Josh Adam <josh.adam@canada.ca>
2021-10-18 13:42:23 -0500

    Updated chroedriver and testing

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/ProjectMembersPage.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/ProjectMembersPage.java
@@ -35,7 +35,7 @@ public class ProjectMembersPage extends AbstractPage {
 	private WebElement removeErrorNotification;
 
 	@FindBy(className = "t-project-role-select")
-	private List<WebElement> projectRoleSelect;
+	private List<WebElement> roleSelects;
 
 	public ProjectMembersPage(WebDriver driver) {
 		super(driver);
@@ -102,7 +102,7 @@ public class ProjectMembersPage extends AbstractPage {
 	}
 
 	public void updateUserRole(int row, String role) {
-		WebElement roleSelect = projectRoleSelect.get(row);
+		WebElement roleSelect = roleSelects.get(row);
 		roleSelect.click();
 		driver.findElement(By.className("t-" + role)).click();
 	}
