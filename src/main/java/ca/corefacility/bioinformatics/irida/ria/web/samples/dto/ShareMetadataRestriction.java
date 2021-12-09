1ec91102f4cd3dd8cb28b56d166c828b069d5ff4
Josh Adam <josh.adam@canada.ca>
2021-12-09 05:12:43 -0600

    Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/samples/dto/ShareMetadataRestriction.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/samples/dto/ShareMetadataRestriction.java
@@ -1,5 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.web.samples.dto;
 
+/**
+ * Helper class used when sharing / moving samples to indicate, which restriction level
+ * the metadata attached should have.
+ */
 public class ShareMetadataRestriction {
 	private Long identifier;
 	private String restriction;
