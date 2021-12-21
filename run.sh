a1b8b29ef9b7a120eff35b05d9201c317688fa0e
Eric Enns <eric.enns@phac-aspc.gc.ca>
2021-12-21 14:31:48 -0600

    Fixed run.sh for spring boot

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ export MIN_JS=false
 ADD=""
 
 if [ "$1" = "--create-db" ]; then
-   ADD="-Dhbm.dev.auto=create"
+   ADD="--hbm.dev.auto=create"
    echo "Dropping then Creating/Recreating database schema"
 else
    echo "Updating database schema without dropping"
   fi
 
-mvn clean spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=dev" ${ADD}
+mvn clean spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=dev ${ADD}"
