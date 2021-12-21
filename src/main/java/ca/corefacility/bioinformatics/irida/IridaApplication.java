d405768d85c636c5c8d9da38377c281c70a8b272
Eric Enns <eric.enns@phac-aspc.gc.ca>
2021-12-20 19:30:17 -0600

    Fixed issue with 404 error page in Spring Boot.

--- src/main/java/ca/corefacility/bioinformatics/irida/IridaApplication.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/IridaApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 
 import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConfig;
 
@@ -19,6 +23,11 @@ import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConf
 )
 @Import(IridaApiServicesConfig.class)
 public class IridaApplication {
+
+	@Bean
+	public ErrorPageRegistrar errorPageRegistrar() {
+		return registry -> registry.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IridaApplication.class, args);
