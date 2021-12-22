5200c30b6c76bd8bf906ec362711b245693da389
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-12-22 12:39:03 -0600

    adding content loading component

--- src/main/webapp/resources/js/pages/user/components/UserDetailsPage.jsx
+++ src/main/webapp/resources/js/pages/user/components/UserDetailsPage.jsx
@@ -10,6 +10,7 @@ import {
   Switch,
   Typography,
 } from "antd";
+import { ContentLoading } from "../../../components/loader";
 import { formatDate } from "../../../utilities/date-utilities";
 
 import {
@@ -24,7 +25,7 @@ import {
  */
 export default function UserDetailsPage() {
   const { userId } = useParams();
-  const { data, isSuccess } = useGetUserDetailsQuery(userId);
+  const { data, isLoading } = useGetUserDetailsQuery(userId);
   const [editUser] = useEditUserDetailsMutation();
 
   const [formErrors, setFormErrors] = useState();
@@ -45,153 +46,160 @@ export default function UserDetailsPage() {
       });
   };
 
-  if (isSuccess) {
-    return (
-      <Space direction="vertical">
-        <Typography.Title level={4}>{data.user.username}</Typography.Title>
-        <Form
-          layout="vertical"
-          initialValues={data.user}
-          onFinish={onFormFinish}
-        >
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.firstName")}
-            name="firstName"
-            help={
-              formErrors?.find((error) => error.field === "firstName")?.message
-            }
-            validateStatus={
-              formErrors?.find((error) => error.field === "firstName")
-                ? "error"
-                : undefined
-            }
+  return (
+    <>
+      {isLoading ? (
+        <ContentLoading message={i18n("UserDetailsPage.loading.message")} />
+      ) : (
+        <Space direction="vertical">
+          <Typography.Title level={4}>{data.user.username}</Typography.Title>
+          <Form
+            layout="vertical"
+            initialValues={data.user}
+            onFinish={onFormFinish}
           >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.lastName")}
-            name="lastName"
-            help={
-              formErrors?.find((error) => error.field === "lastName")?.message
-            }
-            validateStatus={
-              formErrors?.find((error) => error.field === "lastName")
-                ? "error"
-                : undefined
-            }
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.email")}
-            name="email"
-            help={formErrors?.find((error) => error.field === "email")?.message}
-            validateStatus={
-              formErrors?.find((error) => error.field === "email")
-                ? "error"
-                : undefined
-            }
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.phoneNumber")}
-            name="phoneNumber"
-            help={
-              formErrors?.find((error) => error.field === "phoneNumber")
-                ?.message
-            }
-            validateStatus={
-              formErrors?.find((error) => error.field === "phoneNumber")
-                ? "error"
-                : undefined
-            }
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.locale")}
-            name="locale"
-          >
-            <Select>
-              {data.locales.map((locale, index) => (
-                <Select.Option
-                  key={`user-account-details-locale-${index}`}
-                  value={locale.language}
-                >
-                  {locale.name}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.role")}
-            name="role"
-            help={formErrors?.find((error) => error.field === "role")?.message}
-            validateStatus={
-              formErrors?.find((error) => error.field === "role")
-                ? "error"
-                : undefined
-            }
-            hidden={!data.admin}
-          >
-            <Select>
-              {data.allowedRoles.map((role, index) => (
-                <Select.Option
-                  key={`user-account-details-role-${index}`}
-                  value={role.code}
-                  disabled={!data.canEditUserStatus}
-                >
-                  {role.name}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label={i18n("UserDetailsPage.form.label.enabled")}
-            name="enabled"
-            valuePropName="checked"
-            hidden={!data.admin}
-          >
-            <Switch disabled={!data.canEditUserStatus} />
-          </Form.Item>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={!data.canEditUserInfo}
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.firstName")}
+              name="firstName"
+              help={
+                formErrors?.find((error) => error.field === "firstName")
+                  ?.message
+              }
+              validateStatus={
+                formErrors?.find((error) => error.field === "firstName")
+                  ? "error"
+                  : undefined
+              }
             >
-              {i18n("UserDetailsPage.form.button.submit")}
-            </Button>
-          </Form.Item>
-        </Form>
-        <Typography.Text type="secondary">
-          {data.user.createdDate
-            ? i18n(
-                "UserDetailsPage.createdDate",
-                formatDate({ date: data.user.createdDate })
-              )
-            : ""}
-        </Typography.Text>
-        <Typography.Text type="secondary">
-          {data.user.modifiedDate
-            ? i18n(
-                "UserDetailsPage.modifiedDate",
-                formatDate({ date: data.user.modifiedDate })
-              )
-            : ""}
-        </Typography.Text>
-        <Typography.Text type="secondary">
-          {data.user.lastLogin
-            ? i18n(
-                "UserDetailsPage.lastLogin",
-                formatDate({ date: data.user.lastLogin })
-              )
-            : ""}
-        </Typography.Text>
-      </Space>
-    );
-  } else {
-    return null;
-  }
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.lastName")}
+              name="lastName"
+              help={
+                formErrors?.find((error) => error.field === "lastName")?.message
+              }
+              validateStatus={
+                formErrors?.find((error) => error.field === "lastName")
+                  ? "error"
+                  : undefined
+              }
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.email")}
+              name="email"
+              help={
+                formErrors?.find((error) => error.field === "email")?.message
+              }
+              validateStatus={
+                formErrors?.find((error) => error.field === "email")
+                  ? "error"
+                  : undefined
+              }
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.phoneNumber")}
+              name="phoneNumber"
+              help={
+                formErrors?.find((error) => error.field === "phoneNumber")
+                  ?.message
+              }
+              validateStatus={
+                formErrors?.find((error) => error.field === "phoneNumber")
+                  ? "error"
+                  : undefined
+              }
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.locale")}
+              name="locale"
+            >
+              <Select>
+                {data.locales.map((locale, index) => (
+                  <Select.Option
+                    key={`user-account-details-locale-${index}`}
+                    value={locale.language}
+                  >
+                    {locale.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.role")}
+              name="role"
+              help={
+                formErrors?.find((error) => error.field === "role")?.message
+              }
+              validateStatus={
+                formErrors?.find((error) => error.field === "role")
+                  ? "error"
+                  : undefined
+              }
+              hidden={!data.admin}
+            >
+              <Select>
+                {data.allowedRoles.map((role, index) => (
+                  <Select.Option
+                    key={`user-account-details-role-${index}`}
+                    value={role.code}
+                    disabled={!data.canEditUserStatus}
+                  >
+                    {role.name}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label={i18n("UserDetailsPage.form.label.enabled")}
+              name="enabled"
+              valuePropName="checked"
+              hidden={!data.admin}
+            >
+              <Switch disabled={!data.canEditUserStatus} />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!data.canEditUserInfo}
+              >
+                {i18n("UserDetailsPage.form.button.submit")}
+              </Button>
+            </Form.Item>
+          </Form>
+          <Typography.Text type="secondary">
+            {data.user.createdDate
+              ? i18n(
+                  "UserDetailsPage.createdDate",
+                  formatDate({ date: data.user.createdDate })
+                )
+              : ""}
+          </Typography.Text>
+          <Typography.Text type="secondary">
+            {data.user.modifiedDate
+              ? i18n(
+                  "UserDetailsPage.modifiedDate",
+                  formatDate({ date: data.user.modifiedDate })
+                )
+              : ""}
+          </Typography.Text>
+          <Typography.Text type="secondary">
+            {data.user.lastLogin
+              ? i18n(
+                  "UserDetailsPage.lastLogin",
+                  formatDate({ date: data.user.lastLogin })
+                )
+              : ""}
+          </Typography.Text>
+        </Space>
+      )}
+    </>
+  );
 }
