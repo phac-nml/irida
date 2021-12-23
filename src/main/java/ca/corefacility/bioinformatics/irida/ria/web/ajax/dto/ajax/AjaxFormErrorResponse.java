53d84e06d0e159bdb8d33f45ccb58c90878a2841
Josh Adam <josh.adam@canada.ca>
2021-12-23 12:35:16 -0600

    Updated JSDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/ajax/AjaxFormErrorResponse.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/ajax/dto/ajax/AjaxFormErrorResponse.java
@@ -2,6 +2,9 @@ package ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.ajax;
 
 import java.util.Map;
 
+/**
+ * AJAX response to return form errors to the client
+ */
 public class AjaxFormErrorResponse extends AjaxResponse {
 	private Map<String, String> errors;
 
