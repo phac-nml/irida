b2ca1ab525e6d6b9c3ecdd9d13c0786b30ff9a8f
Katherine Thiessen <katherine.thiessen@canada.ca>
2021-12-23 15:05:21 -0600

    starting to build the password page

--- src/main/webapp/resources/js/pages/user/components/UserPasswordPage.jsx
+++ src/main/webapp/resources/js/pages/user/components/UserPasswordPage.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  Alert,
+  Button,
+  Form,
+  Input,
+  Space,
+  Typography,
+} from "antd";
 
 /**
  * React component to display the user password page.
@@ -6,5 +14,58 @@ import React from "react";
  * @constructor
  */
 export default function UserPasswordPage() {
-  return <div>User Password Page</div>;
+  const [valid, setValid] = React.useState(true);
+
+  return(
+    <Space direction="vertical">
+      <Typography.Title level={4}>{i18n("UserPasswordPage.page.title")}</Typography.Title>
+      <Alert
+        message={i18n("UserPasswordPage.alert.title")}
+        description={
+          <Typography.Paragraph>
+            {i18n("UserPasswordPage.alert.description")}
+              <ul>
+                <li>{i18n("UserPasswordPage.alert.rule1")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule2")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule3")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule4")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule5")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule6")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule7")}</li>
+                <li>{i18n("UserPasswordPage.alert.rule8")}</li>
+              </ul>
+            </Typography.Paragraph>
+          }
+          type="error"
+          showIcon
+        />
+      <Form
+        layout="vertical"
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Password canot be blank.' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Confirm Password"
+          name="confirmPassword"
+          rules={[{ required: true, message: 'Confirm password canot be blank.' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Space>
+  )
 }
