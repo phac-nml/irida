35572aa5c57349e8690b588d7bb80d91b85cd8ed
Josh Adam <josh.adam@canada.ca>
2022-01-05 13:51:56 -0600

    Added file comments

--- src/main/webapp/resources/js/pages/admin/components/AdminContent.jsx
+++ src/main/webapp/resources/js/pages/admin/components/AdminContent.jsx
@@ -4,6 +4,11 @@ import { getUserGroupRoles } from "../../../apis/users/groups";
 import { RolesProvider } from "../../../contexts/roles-context";
 import { UserGroupsProvider } from "../../../contexts/UserGroupsContext";
 
+/**
+ * React component to wrap providers around the content within the admin panel.
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function AdminContent() {
   return (
     <UserGroupsProvider>
