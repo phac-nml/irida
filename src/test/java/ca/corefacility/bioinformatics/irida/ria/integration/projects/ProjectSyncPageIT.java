9b129863671276106ed31a382a862a988666408d
Josh Adam <josh.adam@canada.ca>
2022-01-05 13:31:12 -0600

    Fixed Project sync page

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectSyncPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectSyncPageIT.java
@@ -3,13 +3,22 @@ package ca.corefacility.bioinformatics.irida.ria.integration.projects;
 import org.junit.Test;
 
 import ca.corefacility.bioinformatics.irida.ria.integration.AbstractIridaUIITChromeDriver;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.clients.ClientDetailsPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.clients.CreateClientPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectDetailsPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectSyncPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.utilities.RemoteApiUtilities;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/ProjectsPageIT.xml")
 public class ProjectSyncPageIT extends AbstractIridaUIITChromeDriver {
 
+	CreateClientPage createClientPage;
 	ProjectSyncPage page;
 
 	String clientId = "myClient";
@@ -17,28 +26,28 @@ public class ProjectSyncPageIT extends AbstractIridaUIITChromeDriver {
 
 	@Test
 	public void testSyncProject() {
-//		LoginPage.loginAsAdmin(driver());
-//
-//		//create the oauth client
-//		String redirectLocation = RemoteApiUtilities.getRedirectLocation();
-//		createClientPage = new CreateClientPage(driver());
-//		createClientPage.goTo();
-//		createClientPage.createClientWithDetails(clientId, "authorization_code", redirectLocation, true, false);
-//		ClientDetailsPage detailsPage = new ClientDetailsPage(driver());
-//		clientSecret = detailsPage.getClientSecret();
-//
-//		RemoteApiUtilities.addRemoteApi(driver(), clientId, clientSecret);
-//		page = ProjectSyncPage.goTo(driver());
-//		page.selectApi(0);
-//		final String name = "project";
-//		page.selectProjectInListing(name);
-//
-//		String url = page.getProjectUrl();
-//		assertFalse("URL should not be empty", url.isEmpty());
-//		page.submitProject();
-//
-//		ProjectDetailsPage projectDetailsPage = ProjectDetailsPage.initElements(driver());
-//		String dataProjectName = projectDetailsPage.getProjectName();
-//		assertEquals("Should be on the remote project page", dataProjectName, name);
+		LoginPage.loginAsAdmin(driver());
+
+		//create the oauth client
+		String redirectLocation = RemoteApiUtilities.getRedirectLocation();
+		createClientPage = new CreateClientPage(driver());
+		createClientPage.goTo();
+		createClientPage.createClientWithDetails(clientId, "authorization_code", redirectLocation, true, false);
+		ClientDetailsPage detailsPage = new ClientDetailsPage(driver());
+		clientSecret = detailsPage.getClientSecret();
+
+		RemoteApiUtilities.addRemoteApi(driver(), clientId, clientSecret);
+		page = ProjectSyncPage.goTo(driver());
+		page.selectApi(0);
+		final String name = "project";
+		page.selectProjectInListing(name);
+
+		String url = page.getProjectUrl();
+		assertFalse("URL should not be empty", url.isEmpty());
+		page.submitProject();
+
+		ProjectDetailsPage projectDetailsPage = ProjectDetailsPage.initElements(driver());
+		String dataProjectName = projectDetailsPage.getProjectName();
+		assertEquals("Should be on the remote project page", dataProjectName, name);
 	}
 }
