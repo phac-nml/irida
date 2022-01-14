83027a9e080bb0b89fa767c1f3637265c2f58f34
Josh Adam <josh.adam@canada.ca>
2022-01-14 08:54:36 -0600

    Fixed test

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/services/UISampleServiceTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/services/UISampleServiceTest.java
@@ -14,6 +14,7 @@ import ca.corefacility.bioinformatics.irida.model.sample.Sample;
 import ca.corefacility.bioinformatics.irida.model.user.Role;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.ria.web.samples.dto.SampleDetails;
+import ca.corefacility.bioinformatics.irida.ria.web.samples.dto.ShareMetadataRestriction;
 import ca.corefacility.bioinformatics.irida.ria.web.samples.dto.ShareSamplesRequest;
 import ca.corefacility.bioinformatics.irida.ria.web.services.UICartService;
 import ca.corefacility.bioinformatics.irida.ria.web.services.UISampleService;
@@ -90,6 +91,7 @@ public class UISampleServiceTest {
 		request.setCurrentId(CURRENT_PROJECT_ID);
 		request.setTargetId(TARGET_PROJECT_ID);
 		request.setSampleIds(ImmutableList.of(SAMPLE_ID));
+		request.setRestrictions(ImmutableList.of(new ShareMetadataRestriction(1L, "LEVEL_1")));
 		request.setRemove(false);
 		request.setLocked(false);
 		service.shareSamplesWithProject(request, Locale.CANADA);
