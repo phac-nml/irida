123c9e84c9a6ccadd9169144cdc7a975f16b69b6
Josh Adam <josh.adam@canada.ca>
2022-01-19 08:58:10 -0600

    Updated regex to get path and removed develeper warning

--- src/main/webapp/resources/js/pages/projects/settings/components/SettingsNav.jsx
+++ src/main/webapp/resources/js/pages/projects/settings/components/SettingsNav.jsx
@@ -8,98 +8,64 @@ import { Link, useLocation } from "react-router-dom";
  * @constructor
  */
 export default function SettingsNav({
-                                      basePath,
-                                      showRemote = false,
-                                      canManage = false
-                                    }) {
-                                      const location = useLocation();
-                                      const [key, setKey] = React.useState();
+  basePath,
+  showRemote = false,
+  canManage = false,
+}) {
+  const location = useLocation();
+  const [key, setKey] = React.useState();
 
-                                      const keyRegex =
-                                        /\/projects\/\d+\/settings\/([\w_-]+)/;
-                                      React.useEffect(() => {
-                                        const path = keyRegex.exec(
-                                          location.pathname
-                                        );
-                                        if (path) {
-                                          setKey(path[1]);
-                                        } else {
-                                          throw new Error(
-                                            "Developer: You ed the schema for the settings URL - fix this"
-                                          );
-                                        }
-                                      }, [location]);
+  const keyRegex = /\/projects\/\d+\/settings\/(?<path>[\w_-]+)/;
+  React.useEffect(() => {
+    const found = location.pathname.match(keyRegex);
+    setKey(found.groups.path);
+  }, [location]);
 
-                                      return (
-                                        <Menu selectedKeys={[key]}>
-                                          <Menu.Item key="details">
-                                            <Link to={`${basePath}details`}>
-                                              {i18n(
-                                                "project.settings.page.details"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          <Menu.Item key="processing">
-                                            <Link to={`${basePath}processing`}>
-                                              {i18n(
-                                                "project.settings.page.processing"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          <Menu.Item key="members">
-                                            <Link to={`${basePath}members`}>
-                                              {i18n(
-                                                "project.settings.page.members"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          <Menu.Item key="groups">
-                                            <Link to={`${basePath}groups`}>
-                                              {i18n(
-                                                "project.settings.page.groups"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          <Menu.Item key="metadata">
-                                            <Link
-                                              className="t-m-field-link"
-                                              to={`${basePath}metadata/fields`}
-                                            >
-                                              {i18n(
-                                                "project.settings.page.metadata"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          <Menu.Item key="associated">
-                                            <Link to="associated">
-                                              {i18n(
-                                                "project.settings.page.associated"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          <Menu.Item key="references">
-                                            <Link to="references">
-                                              {i18n(
-                                                "project.settings.page.referenceFiles"
-                                              )}
-                                            </Link>
-                                          </Menu.Item>
-                                          {showRemote && (
-                                            <Menu.Item key="remote">
-                                              <Link to="remote">
-                                                {i18n(
-                                                  "project.settings.page.remote"
-                                                )}
-                                              </Link>
-                                            </Menu.Item>
-                                          )}
-                                          {canManage && (
-                                            <Menu.Item key="delete">
-                                              <Link to="delete">
-                                                {i18n("DeleteProject.title")}
-                                              </Link>
-                                            </Menu.Item>
-                                          )}
-                                        </Menu>
-                                      );
-                                    }
+  return (
+    <Menu selectedKeys={[key]}>
+      <Menu.Item key="details">
+        <Link to={`${basePath}details`}>
+          {i18n("project.settings.page.details")}
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="processing">
+        <Link to={`${basePath}processing`}>
+          {i18n("project.settings.page.processing")}
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="members">
+        <Link to={`${basePath}members`}>
+          {i18n("project.settings.page.members")}
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="groups">
+        <Link to={`${basePath}groups`}>
+          {i18n("project.settings.page.groups")}
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="metadata">
+        <Link className="t-m-field-link" to={`${basePath}metadata/fields`}>
+          {i18n("project.settings.page.metadata")}
+        </Link>
+      </Menu.Item>
+      <Menu.Item key="associated">
+        <Link to="associated">{i18n("project.settings.page.associated")}</Link>
+      </Menu.Item>
+      <Menu.Item key="references">
+        <Link to="references">
+          {i18n("project.settings.page.referenceFiles")}
+        </Link>
+      </Menu.Item>
+      {showRemote && (
+        <Menu.Item key="remote">
+          <Link to="remote">{i18n("project.settings.page.remote")}</Link>
+        </Menu.Item>
+      )}
+      {canManage && (
+        <Menu.Item key="delete">
+          <Link to="delete">{i18n("DeleteProject.title")}</Link>
+        </Menu.Item>
+      )}
+    </Menu>
+  );
+}
