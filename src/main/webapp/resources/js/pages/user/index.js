942b9d0bbdeb498fc48fde9d496622f166d7fcf0
Katherine Thiessen <katherine.thiessen@canada.ca>
2022-01-20 11:11:48 -0600

    adding lazy loading

--- src/main/webapp/resources/js/pages/user/index.js
+++ src/main/webapp/resources/js/pages/user/index.js
@@ -3,12 +3,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { setBaseUrl } from "../../utilities/url-utilities";
-
+import { ContentLoading } from "../../components/loader";
 import store from "./store";
-import UserAccountLayout from "./components/UserAccountLayout";
-import UserDetailsPage from "./components/UserDetailsPage";
-import UserProjectsPage from "./components/UserProjectsPage";
-import UserPasswordPage from "./components/UserPasswordPage";
+const UserAccountLayout = React.lazy(() => import("./components/UserAccountLayout"));
+const UserDetailsPage = React.lazy(() => import("./components/UserDetailsPage"));
+const UserProjectsPage = React.lazy(() => import("./components/UserProjectsPage"));
+const UserPasswordPage = React.lazy(() => import("./components/UserPasswordPage"));
 
 /**
  * React component that displays the user pages.
@@ -17,16 +17,18 @@ import UserPasswordPage from "./components/UserPasswordPage";
  */
 render(
   <Provider store={store}>
-    <BrowserRouter basename={setBaseUrl("/users")}>
-      <Routes>
-        <Route path="/:userId" element={<UserAccountLayout />}>
-          <Route index element={<UserDetailsPage />} />
-          <Route path="details" element={<UserDetailsPage />} />
-          <Route path="projects" element={<UserProjectsPage />} />
-          <Route path="security" element={<UserPasswordPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <React.Suspense fallback={<ContentLoading />}>
+      <BrowserRouter basename={setBaseUrl("/users")}>
+        <Routes>
+          <Route path="/:userId" element={<UserAccountLayout />}>
+            <Route index element={<UserDetailsPage />} />
+            <Route path="details" element={<UserDetailsPage />} />
+            <Route path="projects" element={<UserProjectsPage />} />
+            <Route path="security" element={<UserPasswordPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Suspense>
   </Provider>,
   document.querySelector("#user-account-root")
 );
