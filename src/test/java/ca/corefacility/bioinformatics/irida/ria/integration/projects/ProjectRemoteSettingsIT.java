7c95fdecf6ce05dd861cf8204dbe2399a229c053
Josh Adam <josh.adam@canada.ca>
2022-01-24 09:39:07 -0600

    Fixed checking project name

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectRemoteSettingsIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectRemoteSettingsIT.java
@@ -7,13 +7,13 @@ import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.clients.ClientDetailsPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.clients.CreateClientPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectRemoteSettingsPage;
+import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectSamplesPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ProjectSyncPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.utilities.RemoteApiUtilities;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * <p>
@@ -52,6 +52,9 @@ public class ProjectRemoteSettingsIT extends AbstractIridaUIITChromeDriver {
 		String url = page.getProjectUrl();
 		assertFalse("URL should not be empty", url.isEmpty());
 		page.submitProject();
+
+		ProjectSamplesPage samplesPage = ProjectSamplesPage.initPage(driver());
+		assertEquals("Should have the correct project name", name, samplesPage.getProjectName());
 
 		ProjectRemoteSettingsPage remoteSettingsPage = ProjectRemoteSettingsPage.initElements(driver());
 		final Long projectId = remoteSettingsPage.getProjectId();
