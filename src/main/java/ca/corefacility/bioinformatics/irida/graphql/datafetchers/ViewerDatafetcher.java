a618afaa977628c11e5297f3a07ea0044c420994
Eric Enns <eric.enns@phac-aspc.gc.ca>
2022-01-31 12:29:37 -0600

    Added in javadoc to ViewerDataFetcher.

--- src/main/java/ca/corefacility/bioinformatics/irida/graphql/datafetchers/ViewerDatafetcher.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/graphql/datafetchers/ViewerDatafetcher.java
@@ -10,12 +10,22 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.service.user.UserService;
 
+/**
+ * Datafetcher to get the currently authenticated {@link User}.
+ */
 @DgsComponent
 public class ViewerDatafetcher {
 
 	@Autowired
 	private UserService userService;
 
+	/**
+	 * Get the currently authenticated {@link User}.
+	 * 
+	 * @param dfe
+	 *            the {@link DgsDataFetchingEnvironment}
+	 * @return the {@link User}
+	 */
 	@DgsData(parentType = "Query", field = "viewer")
 	public User viewer(DgsDataFetchingEnvironment dfe) {
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
