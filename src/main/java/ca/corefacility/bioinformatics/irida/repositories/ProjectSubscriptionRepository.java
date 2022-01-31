caf1b69733d47ca2c56a870c20c2c910d83ca98f
Katherine Thiessen <katherine.thiessen@canada.ca>
2022-01-31 14:06:42 -0600

    missing comments

--- src/main/java/ca/corefacility/bioinformatics/irida/repositories/ProjectSubscriptionRepository.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/repositories/ProjectSubscriptionRepository.java
@@ -16,9 +16,22 @@ import ca.corefacility.bioinformatics.irida.model.user.User;
 public interface ProjectSubscriptionRepository
 		extends CrudRepository<ProjectSubscription, Long>, RevisionRepository<ProjectSubscription, Long, Integer> {
 
+	/**
+	 * Find the {@link ProjectSubscription} for a specific {@link User} and {@link Project}
+	 *
+	 * @param user    the user
+	 * @param project the project
+	 * @return a {@link ProjectSubscription}
+	 */
 	@Query("FROM ProjectSubscription ps WHERE ps.user=?1 AND ps.project=?2")
 	public ProjectSubscription getSubscription(User user, Project project);
 
+	/**
+	 * Find all the {@link ProjectSubscription}s for a specific {@link User}
+	 *
+	 * @param user the user's subscriptions
+	 * @return a list of {@link Project} ids
+	 */
 	@Query("Select ps.project.id FROM ProjectSubscription ps WHERE ps.user=?1")
 	public List<Long> getProjectIdsByUser(User user);
 
