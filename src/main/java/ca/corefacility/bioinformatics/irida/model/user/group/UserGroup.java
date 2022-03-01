1bb0b5a7ffd5d0ccda30accab9ce12bb3bc820e4
Katherine Thiessen <katherine.thiessen@canada.ca>
2022-03-01 09:54:07 -0600

    undo

--- src/main/java/ca/corefacility/bioinformatics/irida/model/user/group/UserGroup.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/user/group/UserGroup.java
@@ -52,7 +52,7 @@ public class UserGroup implements MutableIridaThing {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date modifiedDate;
 
-	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "group")
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, mappedBy = "group")
 	private Set<UserGroupJoin> users;
 
 	@NotAudited
