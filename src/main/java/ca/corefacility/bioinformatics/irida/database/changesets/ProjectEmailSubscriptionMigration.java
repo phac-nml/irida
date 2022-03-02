11bb781cde1f38de9c3db7fff65fc25de7302ff4
Katherine Thiessen <katherine.thiessen@canada.ca>
2022-03-02 12:56:03 -0600

    starting to create a CustomSqlChange to migrate project subscription data

---
+++ src/main/java/ca/corefacility/bioinformatics/irida/database/changesets/ProjectEmailSubscriptionMigration.java
@@ -0,0 +1,105 @@
+package ca.corefacility.bioinformatics.irida.database.changesets;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import ca.corefacility.bioinformatics.irida.config.data.IridaApiJdbcDataSourceConfig;
+import ca.corefacility.bioinformatics.irida.model.subscription.ProjectSubscription;
+
+import liquibase.change.custom.CustomSqlChange;
+import liquibase.database.Database;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+import liquibase.statement.SqlStatement;
+
+/**
+ * Liquibase update to create {@link ProjectSubscription} entries for every project that a user has access to.
+ */
+public class ProjectEmailSubscriptionMigration implements CustomSqlChange {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProjectEmailSubscriptionMigration.class);
+	private DataSource dataSource;
+
+	@Override
+	public SqlStatement[] generateStatements(Database database) throws CustomChangeException {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		// Get all the current user project level access
+		List<ProjectEmailSubscriptionMigration.ProjectSubscriptionResult> projectSubscriptionResults = jdbcTemplate.query(
+				"SELECT * FROM project_user", (rs, rowNum) -> {
+					return new ProjectSubscriptionResult(rs.getLong("project_id"), rs.getLong("user_id"),
+							rs.getBoolean("email_subscription"));
+				});
+
+		// Create a new ProjectSubscription entry for each user project level access
+		for (ProjectEmailSubscriptionMigration.ProjectSubscriptionResult projectSubscriptionResult : projectSubscriptionResults) {
+			jdbcTemplate.update(
+					"INSERT INTO project_subscription (project_id, user_id, email_subscription, created_date) VALUES (?,?,?,?)",
+					projectSubscriptionResult.projectId, projectSubscriptionResult.userId,
+					projectSubscriptionResult.emailSubscription, new Date());
+		}
+
+		// Get all the current group project level access
+
+		return new SqlStatement[0];
+	}
+
+	@Override
+	public String getConfirmationMessage() {
+		return "Successfully created project subscriptions for every project a user has access to";
+	}
+
+	@Override
+	public void setUp() throws SetupException {
+		logger.info("Starting to create project subscriptions for every project a user has access to");
+	}
+
+	@Override
+	public void setFileOpener(ResourceAccessor resourceAccessor) {
+		logger.info("The resource accessor is of type [" + resourceAccessor.getClass() + "]");
+		final ApplicationContext applicationContext;
+		if (resourceAccessor instanceof IridaApiJdbcDataSourceConfig.ApplicationContextAwareSpringLiquibase.ApplicationContextSpringResourceOpener) {
+			applicationContext = ((IridaApiJdbcDataSourceConfig.ApplicationContextAwareSpringLiquibase.ApplicationContextSpringResourceOpener) resourceAccessor).getApplicationContext();
+		} else {
+			applicationContext = null;
+		}
+
+		if (applicationContext != null) {
+			logger.info("We're running inside of a spring instance, getting the existing application context.");
+			this.dataSource = applicationContext.getBean(DataSource.class);
+		} else {
+			logger.error(
+					"This changeset *must* be run from a servlet container as it requires access to Spring's application context.");
+			throw new IllegalStateException(
+					"This changeset *must* be run from a servlet container as it requires access to Spring's application context.");
+		}
+	}
+
+	@Override
+	public ValidationErrors validate(Database database) {
+		return null;
+	}
+
+	/**
+	 * Convenience class to store project subscription results of a query
+	 */
+	private class ProjectSubscriptionResult {
+		Long projectId;
+		Long userId;
+		boolean emailSubscription;
+
+		public ProjectSubscriptionResult(Long projectId, Long userId, boolean emailSubscription) {
+			this.projectId = projectId;
+			this.userId = userId;
+			this.emailSubscription = emailSubscription;
+		}
+	}
+}
