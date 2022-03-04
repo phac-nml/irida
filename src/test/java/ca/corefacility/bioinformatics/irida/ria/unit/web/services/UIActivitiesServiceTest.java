e11b13d2471ef07c03338eb6dfcea8af1c91e339
Deep Sidhu <deepsidhu85@gmail.com>
2022-03-04 06:55:40 -0600

    Updated activities service tests

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/services/UIActivitiesServiceTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/services/UIActivitiesServiceTest.java
@@ -20,6 +20,7 @@ import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectUserJoin;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.user.Role;
 import ca.corefacility.bioinformatics.irida.model.user.User;
 import ca.corefacility.bioinformatics.irida.model.user.group.UserGroup;
 import ca.corefacility.bioinformatics.irida.model.user.group.UserGroupProjectJoin;
@@ -34,6 +35,7 @@ import com.google.common.collect.ImmutableList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 public class UIActivitiesServiceTest {
 	private UIActivitiesService service;
@@ -43,6 +45,8 @@ public class UIActivitiesServiceTest {
 
 	private final Sample sample2 = new Sample("SAMPLE_2");
 	private final Project project2 = new Project("PROJECT_2");
+
+	private final Sample sample3 = new Sample("SAMPLE_3");
 
 	private ProjectEventService eventService;
 	private UserService userService;
@@ -69,26 +73,38 @@ public class UIActivitiesServiceTest {
 				.thenReturn(project);
 		Mockito.when(projectService.read(2l))
 				.thenReturn(project2);
+
+		user2.setSystemRole(Role.ROLE_ADMIN);
 	}
 
 	@Test
 	public void testGetActivitiesForProject() {
-		Mockito.when(eventService.getEventsForProject(any(), any()))
-				.thenReturn(createMockProjectEvents());
+		Mockito.when(eventService.getEventsForProject(eq(project), any()))
+				.thenReturn(createMockProjectEvents(null, project, false));
 		PagedListResponse response = service.getActivitiesForProject(1l, 0, Locale.ENGLISH);
 		assertEquals(4, response.getTotal(), "Should have 4 events");
 		assertEquals("project_sample_added", ((Activity)response.getContent().get(0)).getType(), "First should be a project_sample_added event");
 		assertEquals("project_user_role_updated", ((Activity)response.getContent().get(1)).getType(), "Second should be a project_user_role_updated event");
 		assertEquals("project_user_removed", ((Activity)response.getContent().get(2)).getType(), "Third should be a project_user_removed event");
 		assertEquals("project_user_group_added", ((Activity)response.getContent().get(3)).getType(), "Forth should be a project_user_group_added event");
+
+		Mockito.when(eventService.getEventsForProject(eq(project2), any()))
+				.thenReturn(createMockProjectEvents(null, project2, false));
+		response = service.getActivitiesForProject(2l, 0, Locale.ENGLISH);
+		assertEquals(5, response.getTotal(), "Should have 5 events");
+		assertEquals("project_sample_added", ((Activity)response.getContent().get(0)).getType(), "First should be a project_sample_added event");
+		assertEquals("project_user_role_updated", ((Activity)response.getContent().get(1)).getType(), "Second should be a project_user_role_updated event");
+		assertEquals("project_user_removed", ((Activity)response.getContent().get(2)).getType(), "Third should be a project_user_removed event");
+		assertEquals("project_user_group_added", ((Activity)response.getContent().get(3)).getType(), "Forth should be a project_user_group_added event");
+		assertEquals("project_sample_added", ((Activity)response.getContent().get(4)).getType(), "Fifth should be a project_sample_added event");
 	}
 
 	@Test
 	public void testGetAllProjectsRecentActivity() {
 		Mockito.when(eventService.getAllProjectsEvents(any()))
-				.thenReturn(createMockProjectEvents());
+				.thenReturn(createMockProjectEvents(user2, null, true));
 		PagedListResponse response = service.getAllRecentActivities(0, Locale.ENGLISH);
-		assertEquals(8, response.getContent().size(), "Should have 8 events");
+		assertEquals(9, response.getTotal(), "Should have 9 events");
 		assertEquals("project_sample_added", ((Activity)response.getContent().get(0)).getType(), "First should be a project_sample_added event");
 		assertEquals("project_user_role_updated", ((Activity)response.getContent().get(1)).getType(), "Second should be a project_user_role_updated event");
 		assertEquals("project_user_removed", ((Activity)response.getContent().get(2)).getType(), "Third should be a project_user_removed event");
@@ -97,12 +113,14 @@ public class UIActivitiesServiceTest {
 		assertEquals("project_user_role_updated", ((Activity)response.getContent().get(5)).getType(), "Sixth should be a project_user_role_updated event");
 		assertEquals("project_user_removed", ((Activity)response.getContent().get(6)).getType(), "Seventh should be a project_user_removed event");
 		assertEquals("project_user_group_added", ((Activity)response.getContent().get(7)).getType(), "Eighth should be a project_user_group_added event");
+		assertEquals("project_sample_added", ((Activity)response.getContent().get(8)).getType(), "Nineth should be a project_sample_added event");
 	}
 
 	@Test
 	public void testGetUserProjectsRecentActivity() {
-		Mockito.when(eventService.getEventsForUser(any(), any()))
-				.thenReturn(createMockProjectEvents());
+		Mockito.when(eventService.getEventsForUser(eq(user), any()))
+				.thenReturn(createMockProjectEvents(user, null, false));
+
 		Mockito.when(userService.getUserByUsername("FRED"))
 				.thenReturn(user);
 		Principal principal = () -> "FRED";
@@ -113,17 +131,21 @@ public class UIActivitiesServiceTest {
 		assertEquals("project_user_removed", ((Activity)response.getContent().get(2)).getType(), "Third should be a project_user_removed event");
 		assertEquals("project_user_group_added", ((Activity)response.getContent().get(3)).getType(), "Forth should be a project_user_group_added event");
 
+		Mockito.when(eventService.getEventsForUser(eq(user2), any()))
+				.thenReturn(createMockProjectEvents(user2, null, false));
 		principal = () -> "FRED2";
 		Mockito.when(userService.getUserByUsername("FRED2"))
 				.thenReturn(user2);
 		response = service.getRecentActivitiesForUser(0, Locale.ENGLISH, principal);
+		assertEquals(5, response.getTotal(), "Should have 5 events");
 		assertEquals("project_sample_added", ((Activity)response.getContent().get(0)).getType(), "First should be a project_sample_added event");
 		assertEquals("project_user_role_updated", ((Activity)response.getContent().get(1)).getType(), "Second should be a project_user_role_updated event");
 		assertEquals("project_user_removed", ((Activity)response.getContent().get(2)).getType(), "Third should be a project_user_removed event");
 		assertEquals("project_user_group_added", ((Activity)response.getContent().get(3)).getType(), "Forth should be a project_user_group_added event");
+		assertEquals("project_sample_added", ((Activity)response.getContent().get(4)).getType(), "Fifth should be a project_sample_added event");
 	}
 
-	private Page<ProjectEvent> createMockProjectEvents() {
+	private Page<ProjectEvent> createMockProjectEvents(User currUser, Project specificProject, boolean adminListingPage) {
 		ProjectSampleJoin join = new ProjectSampleJoin(project, sample, true);
 		SampleAddedProjectEvent sampleAddedProjectEvent = new SampleAddedProjectEvent(join);
 
@@ -141,18 +163,33 @@ public class UIActivitiesServiceTest {
 		ProjectSampleJoin join2 = new ProjectSampleJoin(project2, sample2, true);
 		SampleAddedProjectEvent sampleAddedProjectEvent2 = new SampleAddedProjectEvent(join2);
 
+		ProjectSampleJoin join3 = new ProjectSampleJoin(project2, sample3, true);
+		SampleAddedProjectEvent sampleAddedProjectEvent3 = new SampleAddedProjectEvent(join3);
+
 		ProjectUserJoin projectUserJoinUser2 = new ProjectUserJoin(project2, user2, ProjectRole.PROJECT_USER);
 		UserRoleSetProjectEvent userRoleSetProjectEventUser2 = new UserRoleSetProjectEvent(projectUserJoinUser2);
 
 		UserRemovedProjectEvent userRemovedProjectEvent2 = new UserRemovedProjectEvent(project2, user2);
 
-		UserGroup userGroup2 = new UserGroup("MY GROUP");
+		UserGroup userGroup2 = new UserGroup("MY GROUP 2");
 		UserGroupProjectJoin userGroupProjectJoin2 = new UserGroupProjectJoin(project2, userGroup2,
 				ProjectRole.PROJECT_USER);
 		UserGroupRoleSetProjectEvent userGroupRoleSetProjectEvent2 = new UserGroupRoleSetProjectEvent(
 				userGroupProjectJoin2);
 
 		return new Page<ProjectEvent>() {
+			List<ProjectEvent> eventListAll = ImmutableList.of(sampleAddedProjectEvent, userRoleSetProjectEventUser, userRemovedProjectEvent, userGroupRoleSetProjectEvent,
+					sampleAddedProjectEvent2, userRoleSetProjectEventUser2, userRemovedProjectEvent2, userGroupRoleSetProjectEvent2, sampleAddedProjectEvent3);
+
+			List<ProjectEvent> eventListUser = ImmutableList.of(sampleAddedProjectEvent, userRoleSetProjectEventUser, userRemovedProjectEvent, userGroupRoleSetProjectEvent);
+
+			List<ProjectEvent> eventListUser2 = ImmutableList.of(sampleAddedProjectEvent2, userRoleSetProjectEventUser2, userRemovedProjectEvent2, userGroupRoleSetProjectEvent2, sampleAddedProjectEvent3);
+
+			List<ProjectEvent> project1Events = ImmutableList.of(sampleAddedProjectEvent, userRoleSetProjectEventUser, userRemovedProjectEvent, userGroupRoleSetProjectEvent);
+
+			List<ProjectEvent> project2Events = ImmutableList.of(sampleAddedProjectEvent2, userRoleSetProjectEventUser2, userRemovedProjectEvent2, userGroupRoleSetProjectEvent2, sampleAddedProjectEvent3);
+
+
 			@Override
 			public int getTotalPages() {
 				return 1;
@@ -160,7 +197,11 @@ public class UIActivitiesServiceTest {
 
 			@Override
 			public long getTotalElements() {
-				return 4;
+				if(currUser != null && currUser.getSystemRole() == Role.ROLE_ADMIN && adminListingPage) return eventListAll.size();
+				else if(currUser == null && specificProject == project) return project1Events.size();
+				else if(currUser == null && specificProject == project2) return project2Events.size();
+				else if(currUser == user) return eventListUser.size();
+				else return eventListUser2.size();
 			}
 
 			@Override
@@ -180,13 +221,20 @@ public class UIActivitiesServiceTest {
 
 			@Override
 			public int getNumberOfElements() {
-				return 4;
+				if(currUser != null && currUser.getSystemRole() == Role.ROLE_ADMIN && adminListingPage) return eventListAll.size();
+				else if(currUser == null && specificProject == project) return project1Events.size();
+				else if(currUser == null && specificProject == project2) return project2Events.size();
+				else if(currUser == user) return eventListUser.size();
+				else return eventListUser2.size();
 			}
 
 			@Override
 			public List<ProjectEvent> getContent() {
-				return ImmutableList.of(sampleAddedProjectEvent, userRoleSetProjectEventUser, userRemovedProjectEvent, userGroupRoleSetProjectEvent,
-						sampleAddedProjectEvent2, userRoleSetProjectEventUser2, userRemovedProjectEvent2, userGroupRoleSetProjectEvent2);
+				if(currUser != null && currUser.getSystemRole() == Role.ROLE_ADMIN && adminListingPage) return eventListAll;
+				else if(currUser == null && specificProject == project) return project1Events;
+				else if(currUser == null && specificProject == project2) return project2Events;
+				else if(currUser == user) return eventListUser;
+				else return eventListUser2;
 			}
 
 			@Override
