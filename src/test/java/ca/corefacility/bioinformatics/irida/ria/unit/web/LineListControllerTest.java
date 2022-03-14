63cea0d7967d8567d61107e263f71e3890a9903b
Eric Enns <eric.enns@phac-aspc.gc.ca>
2022-03-14 16:36:02 -0500

    Fixed tests for RESTSampleMetadataController and LineListController after batch load refactor.

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/LineListControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/LineListControllerTest.java
@@ -1,17 +1,19 @@
 package ca.corefacility.bioinformatics.irida.ria.unit.web;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
 import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
 
+import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoinMinimal;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.project.ProjectMinimal;
 import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplateField;
-import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.sample.SampleMinimal;
 import ca.corefacility.bioinformatics.irida.model.sample.metadata.MetadataEntry;
 import ca.corefacility.bioinformatics.irida.ria.web.linelist.LineListController;
 import ca.corefacility.bioinformatics.irida.ria.web.linelist.dto.UISampleMetadata;
@@ -21,8 +23,12 @@ import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.sample.MetadataTemplateService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 
-import com.google.common.collect.Lists;
+import com.github.jsonldjava.shaded.com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit test for {@link LineListController}
@@ -62,26 +68,46 @@ public class LineListControllerTest {
 	public void testGetAllProjectMetadataEntries() {
 		long projectId = 1L;
 		Project project = new Project("p1");
-		Sample s1 = new Sample("s1");
-		s1.setId(1L);
-		s1.setModifiedDate(new Date());
-		Sample s2 = new Sample("s2");
-		s2.setId(2L);
-		s2.setModifiedDate(new Date());
+		ProjectMinimal p1 = mock(ProjectMinimal.class);
+		when(p1.getId()).thenReturn(1L);
+		when(p1.getName()).thenReturn("p1");
+		SampleMinimal s1 = mock(SampleMinimal.class);
+		when(s1.getId()).thenReturn(1L);
+		when(s1.getSampleName()).thenReturn("s1");
+		when(s1.getCreatedDate()).thenReturn(new Date());
+		when(s1.getModifiedDate()).thenReturn(new Date());
+		SampleMinimal s2 = mock(SampleMinimal.class);
+		when(s2.getId()).thenReturn(2L);
+		when(s2.getSampleName()).thenReturn("s2");
+		when(s2.getCreatedDate()).thenReturn(new Date());
+		when(s2.getModifiedDate()).thenReturn(new Date());
 		MetadataTemplateField field = new MetadataTemplateField("field", "text");
+
+		ProjectSampleJoinMinimal psjm1 = mock(ProjectSampleJoinMinimal.class);
+		when(psjm1.getSubject()).thenReturn(p1);
+		when(psjm1.getObject()).thenReturn(s1);
+		ProjectSampleJoinMinimal psjm2 = mock(ProjectSampleJoinMinimal.class);
+		when(psjm2.getSubject()).thenReturn(p1);
+		when(psjm2.getObject()).thenReturn(s2);
 
 		Map<Long, Set<MetadataEntry>> metadata = new HashMap<>();
 		metadata.put(s1.getId(), Sets.newHashSet(new MetadataEntry("value", "text", field)));
 		metadata.put(s2.getId(), Sets.newHashSet(new MetadataEntry("value2", "text", field)));
 
+		Page<ProjectSampleJoinMinimal> pageOne = new PageImpl<>(Lists.newArrayList(psjm1, psjm2));
+		Page<ProjectSampleJoinMinimal> pageTwo = new PageImpl<>(Lists.newArrayList());
+
 		when(projectService.read(projectId)).thenReturn(project);
-		when(sampleService.getMetadataForProject(project)).thenReturn(metadata);
-		when(sampleService.getSamplesForProjectShallow(project)).thenReturn(Lists.newArrayList(s1, s2));
-		List<UISampleMetadata> projectSamplesMetadataEntries = lineListController.getProjectSamplesMetadataEntries(
-				projectId);
+		when(sampleService.getFilteredProjectSamples(eq(Arrays.asList(project)), eq(Collections.emptyList()), eq(""),
+				eq(""), eq(""), isNull(), isNull(), eq(0), any(Integer.class), any(Sort.class))).thenReturn(pageOne);
+		when(sampleService.getFilteredProjectSamples(eq(Arrays.asList(project)), eq(Collections.emptyList()), eq(""),
+				eq(""), eq(""), isNull(), isNull(), eq(1), any(Integer.class), any(Sort.class))).thenReturn(pageTwo);
+		when(sampleService.getMetadataForProjectSamples(eq(project), anyList())).thenReturn(metadata);
+		List<UISampleMetadata> projectSamplesMetadataEntries = lineListController
+				.getProjectSamplesMetadataEntries(projectId);
 
 		assertEquals(2, projectSamplesMetadataEntries.size());
 
-		verify(sampleService, times(1)).getMetadataForProject(project);
+		verify(sampleService, times(1)).getMetadataForProjectSamples(project, Lists.newArrayList(1L, 2L));
 	}
 }
--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/samples/RESTSampleMetadataControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/samples/RESTSampleMetadataControllerTest.java
@@ -1,23 +1,25 @@
 package ca.corefacility.bioinformatics.irida.web.controller.test.unit.samples;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResponseResource;
+import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
 
+import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoinMinimal;
 import ca.corefacility.bioinformatics.irida.model.project.Project;
+import ca.corefacility.bioinformatics.irida.model.project.ProjectMinimal;
 import ca.corefacility.bioinformatics.irida.model.sample.MetadataTemplateField;
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
+import ca.corefacility.bioinformatics.irida.model.sample.SampleMinimal;
 import ca.corefacility.bioinformatics.irida.model.sample.metadata.MetadataEntry;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.sample.MetadataTemplateService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResourceCollection;
+import ca.corefacility.bioinformatics.irida.web.assembler.resource.ResponseResource;
 import ca.corefacility.bioinformatics.irida.web.assembler.resource.sample.SampleMetadataResponse;
 import ca.corefacility.bioinformatics.irida.web.controller.api.samples.RESTSampleMetadataController;
 
@@ -27,6 +29,7 @@ import com.google.common.collect.Sets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 public class RESTSampleMetadataControllerTest {
@@ -47,41 +50,58 @@ public class RESTSampleMetadataControllerTest {
 
 	@Test
 	public void testReadProjectSampleMetadata() {
-		Sample s1 = new Sample("s1");
-		s1.setId(1L);
-		Sample s2 = new Sample("s2");
-		s2.setId(2L);
+		SampleMinimal s1 = mock(SampleMinimal.class);
+		when(s1.getId()).thenReturn(1L);
+		when(s1.getSampleName()).thenReturn("s1");
+		when(s1.getCreatedDate()).thenReturn(new Date());
+		when(s1.getModifiedDate()).thenReturn(new Date());
+		SampleMinimal s2 = mock(SampleMinimal.class);
+		when(s2.getId()).thenReturn(2L);
+		when(s2.getSampleName()).thenReturn("s2");
+		when(s2.getCreatedDate()).thenReturn(new Date());
+		when(s2.getModifiedDate()).thenReturn(new Date());
 		Project p1 = new Project("p1");
 		p1.setId(3L);
+		ProjectMinimal pm1 = mock(ProjectMinimal.class);
+		when(pm1.getId()).thenReturn(3L);
+		when(pm1.getName()).thenReturn("p1");
 
 		MetadataTemplateField f1 = new MetadataTemplateField("f1", "text");
+
+		ProjectSampleJoinMinimal psjm1 = mock(ProjectSampleJoinMinimal.class);
+		when(psjm1.getSubject()).thenReturn(pm1);
+		when(psjm1.getObject()).thenReturn(s1);
+		ProjectSampleJoinMinimal psjm2 = mock(ProjectSampleJoinMinimal.class);
+		when(psjm2.getSubject()).thenReturn(pm1);
+		when(psjm2.getObject()).thenReturn(s2);
 
 		Map<Long, Set<MetadataEntry>> metadata = new HashMap<>();
 		metadata.put(s1.getId(), Sets.newHashSet(new MetadataEntry("value", "text", f1)));
 		metadata.put(s2.getId(), Sets.newHashSet(new MetadataEntry("value2", "text", f1)));
 
+		Page<ProjectSampleJoinMinimal> pageOne = new PageImpl<>(Lists.newArrayList(psjm1, psjm2));
+		Page<ProjectSampleJoinMinimal> pageTwo = new PageImpl<>(Lists.newArrayList());
+
 		when(projectService.read(p1.getId())).thenReturn(p1);
-		when(sampleService.getSamplesForProjectShallow(p1)).thenReturn(Lists.newArrayList(s1, s2));
+		when(sampleService.getFilteredProjectSamples(eq(Arrays.asList(p1)), eq(Collections.emptyList()), eq(""), eq(""),
+				eq(""), isNull(), isNull(), eq(0), any(Integer.class), any(Sort.class))).thenReturn(pageOne);
+		when(sampleService.getFilteredProjectSamples(eq(Arrays.asList(p1)), eq(Collections.emptyList()), eq(""), eq(""),
+				eq(""), isNull(), isNull(), eq(1), any(Integer.class), any(Sort.class))).thenReturn(pageTwo);
+		when(sampleService.getMetadataForProjectSamples(eq(p1), anyList())).thenReturn(metadata);
 
-		when(sampleService.getMetadataForProject(p1)).thenReturn(metadata);
-
-		ResponseResource<ResourceCollection<SampleMetadataResponse>> responseResource = metadataController.getProjectSampleMetadata(
-				p1.getId());
+		ResponseResource<ResourceCollection<SampleMetadataResponse>> responseResource = metadataController
+				.getProjectSampleMetadata(p1.getId());
 
 		ResourceCollection<SampleMetadataResponse> responses = responseResource.getResource();
 
 		assertEquals(2, responses.size());
 		for (SampleMetadataResponse response : responses) {
-			assertEquals(1, response.getMetadata()
-					.size());
-			assertTrue(response.getMetadata()
-					.keySet()
-					.contains(f1));
+			assertEquals(1, response.getMetadata().size());
+			assertTrue(response.getMetadata().keySet().contains(f1));
 		}
 
 		verify(projectService).read(p1.getId());
-		verify(sampleService).getSamplesForProjectShallow(p1);
-		verify(sampleService).getMetadataForProject(p1);
+		verify(sampleService, times(1)).getMetadataForProjectSamples(p1, Lists.newArrayList(1L, 2L));
 	}
 
 	@Test
