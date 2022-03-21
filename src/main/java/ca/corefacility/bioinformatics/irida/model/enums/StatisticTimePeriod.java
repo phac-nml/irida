0a6a1ba9005307aade428c493691ee6a9eff3d70
Katherine Thiessen <katherine.thiessen@canada.ca>
2022-03-21 14:38:28 -0500

    fix for sorting columns

--- src/main/java/ca/corefacility/bioinformatics/irida/model/enums/StatisticTimePeriod.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/enums/StatisticTimePeriod.java
@@ -1,9 +1,7 @@
 package ca.corefacility.bioinformatics.irida.model.enums;
 
 /**
- * Defines a arrays of time periods and group by formats used
- * for getting statistics for the Admin Panel.
- *
+ * Defines a arrays of time periods and group by formats used for getting statistics for the Admin Panel.
  */
 
 public enum StatisticTimePeriod {
@@ -18,24 +16,24 @@ public enum StatisticTimePeriod {
 	 * Time Period: Last day
 	 * Grouped by: HOUR
 	 */
-	HOURLY(new int[] {1}, "%H:00"),
+	HOURLY(new int[] { 1 }, "%H:00"),
 	/*
 	 * Time Period: 7, 14, and 30 days
 	 * Grouped by: month/day
 	 */
-	DAILY(new int[] {7,14,30}, "%m/%d"),
+	DAILY(new int[] { 7, 14, 30 }, "%b %e"),
 	/*
 	 * Time Period: 90 and 365 days
 	 * Grouped by: month/year
 	 */
-	MONTHLY(new int[] {90, 365}, "%m/%y"),
+	MONTHLY(new int[] { 90, 365 }, "%b %Y"),
 	/*
 	 * Time Period: 730, 1825, and 3650 days (2 years, 5 years, 10 years)
 	 * Grouped by: year
 	 */
-	YEARLY(new int[] {730, 1825, 3650}, "%Y");
+	YEARLY(new int[] { 730, 1825, 3650 }, "%Y");
 
-	private int [] values;
+	private int[] values;
 	private String groupByFormat;
 
 	private StatisticTimePeriod(int[] values, String groupByFormat) {
