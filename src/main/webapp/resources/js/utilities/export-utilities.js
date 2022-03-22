d139e722f7eb0c76b7b297caf88a2e6da4b11a8f
Eric Enns <eric.enns@phac-aspc.gc.ca>
2022-03-22 11:50:31 -0500

    Removed console.time statements from export-utilities.js and fixed comment in ProjectRepositoryCustom.java

--- src/main/webapp/resources/js/utilities/export-utilities.js
+++ src/main/webapp/resources/js/utilities/export-utilities.js
@@ -6,10 +6,6 @@ import * as XLSX from "xlsx";
  * @param {string} data - csv representation of the table data.
  */
 export default ({ filename, data }) => {
-  console.time("prep");
   const workbook = XLSX.read(data, { type: 'binary', raw: true, dense: true });
-  console.timeEnd("prep");
-  console.time("writeExcel");
   XLSX.writeFile(workbook, filename, { bookType: 'xlsx', type: 'base64' });
-  console.timeEnd("writeExcel");
 };
