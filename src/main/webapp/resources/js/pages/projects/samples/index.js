ae131cb77de973efefeda264371c13d99dfbb2ed
Josh Adam <josh.adam@canada.ca>
2022-03-25 14:51:23 -0500

    Basics of working pagination

--- src/main/webapp/resources/js/pages/projects/samples/index.js
+++ src/main/webapp/resources/js/pages/projects/samples/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import { getProjectIdFromUrl } from "../../../utilities/url-utilities";
 import { Table } from "antd";
+import axios from "axios";
 
 function SamplesTable() {
+  const [samples, setSamples] = React.useState([]);
+  const [pagination, setPagination] = React.useState({
+    current: 1,
+    pageSize: 10,
+  });
+  const [selectedRowKeys, setSelectedRowKeys] = React.useState([]);
   const projectId = getProjectIdFromUrl();
 
   React.useEffect(() => {
@@ -12,48 +19,80 @@ function SamplesTable() {
     params.append("projectIds", 5);
     // Add associated projectIds here.
 
-    fetch(`/ajax/project-samples?${params.toString()}`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        pageSize: 10,
-        pageNumber: 0,
-      }),
-    });
+    axios
+      .post(`/ajax/project-samples?${params.toString()}`, pagination)
+      .then(({ data }) => {
+        setSamples(data.content);
+        setPagination({ ...pagination, total: data.total });
+      });
   }, []);
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: (selectedRowKeys) => {
+      setSelectedRowKeys(selectedRowKeys);
+    },
+    onSelectAll: (selected, selectedRows, changeRows) => {
+      console.log({ selected, selectedRows, changeRows });
+      // TODO: Call to server to get all samples for selected projects.
+    },
+  };
+
+  const handleTableChange = (pagination, filters, sorter) => {
+    setPagination({ ...pagination });
+    // TODO: handle filter
+    // TODO: handle sort
+
+    const params = new URLSearchParams();
+    params.append("projectIds", projectId);
+    params.append("projectIds", 5);
+    // Add associated projectIds here.
+    axios
+      .post(`/ajax/project-samples?${params.toString()}`, pagination)
+      .then(({ data }) => {
+        setSamples(data.content);
+        setPagination({ ...pagination, total: data.total });
+      });
+  };
 
   const columns = [
     {
       title: "Name",
-      dataIndex: "name",
+      dataIndex: ["sample", "sampleName"],
       key: "name",
+      render: (name, row, index) => <a>{name}</a>,
     },
     {
       title: "Organism",
-      dataIndex: "organism",
+      dataIndex: ["sample", "organism"],
       key: "organism",
     },
     {
       title: "Project",
-      dataIndex: "project",
+      dataIndex: ["project", "name"],
       key: "project",
     },
     {
       title: "Created",
-      dataIndex: "created",
+      dataIndex: ["sample", "createdDate"],
       key: "created",
     },
     {
-      modified: "Modified",
-      dataIndex: "modified",
+      title: "Modified",
+      dataIndex: ["sample", "modifiedDate"],
       key: "modified",
     },
   ];
 
-  return <Table columns={columns} />;
+  return (
+    <Table
+      columns={columns}
+      dataSource={samples}
+      rowSelection={rowSelection}
+      pagination={pagination}
+      onChange={handleTableChange}
+    />
+  );
 }
 
 render(
