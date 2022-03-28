c19c6db087af6f118b86c1011ee16173e5685cb5
Josh Adam <josh.adam@canada.ca>
2022-03-28 09:30:34 -0500

    Added back missing method

--- src/main/webapp/resources/js/utilities/table-utilities.js
+++ src/main/webapp/resources/js/utilities/table-utilities.js
@@ -18,3 +18,7 @@ export function formatSort(sorter) {
   }
   return [fromSorter(sorter)];
 }
+
+export const stringSorter = (property) => (a, b) =>
+  a[property].localeCompare(b[property], window.TL.LANGUAGE_TAG, {
+    sensitivity: "base",
