d953dcd0127bdd5ec75d1aa43ec117a5709c15b9
Josh Adam <josh.adam@canada.ca>
2022-03-28 09:34:59 -0500

    Added missing bracket

--- src/main/webapp/resources/js/utilities/table-utilities.js
+++ src/main/webapp/resources/js/utilities/table-utilities.js
@@ -22,3 +22,4 @@ export function formatSort(sorter) {
 export const stringSorter = (property) => (a, b) =>
   a[property].localeCompare(b[property], window.TL.LANGUAGE_TAG, {
     sensitivity: "base",
+  });
