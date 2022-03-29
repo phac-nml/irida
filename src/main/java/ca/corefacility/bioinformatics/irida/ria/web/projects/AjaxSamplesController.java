43fdd9765ee92a3df627f56e783958c5b55d6e65
Josh Adam <josh.adam@canada.ca>
2022-03-29 09:19:21 -0500

    Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/AjaxSamplesController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/projects/AjaxSamplesController.java
@@ -5,9 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import ca.corefacility.bioinformatics.irida.ria.web.models.tables.AntTableResponse;
+import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.ProjectSampleTableItem;
 import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.ProjectSamplesTableRequest;
 import ca.corefacility.bioinformatics.irida.ria.web.services.UISampleService;
 
+/**
+ * UI Ajax Controller for the project samples page.
+ */
 @RestController
 @RequestMapping("/ajax/project-samples/{projectId}")
 public class AjaxSamplesController {
@@ -18,8 +22,15 @@ public class AjaxSamplesController {
 		this.uiSampleService = uiSampleService;
 	}
 
+	/**
+	 * Returns a Page of samples for a project based on the information in the {@link ProjectSamplesTableRequest}
+	 *
+	 * @param projectId Identifier for the current project
+	 * @param request   Information about the current state of the project samples table.
+	 * @return The Page of samples
+	 */
 	@PostMapping("")
-	public ResponseEntity<AntTableResponse> getProjectSamples(@PathVariable Long projectId,
+	public ResponseEntity<AntTableResponse<ProjectSampleTableItem>> getPagedProjectSamples(@PathVariable Long projectId,
 			@RequestBody ProjectSamplesTableRequest request) {
 		return ResponseEntity.ok(uiSampleService.getPagedProjectSamples(projectId, request));
 	}
