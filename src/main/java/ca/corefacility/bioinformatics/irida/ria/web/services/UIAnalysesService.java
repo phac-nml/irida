637725114e448406764bae54a227ff8d76049986
Deep Sidhu <deepsidhu85@gmail.com>
2022-03-29 15:09:38 -0500

    Removed unused import

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIAnalysesService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UIAnalysesService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import ca.corefacility.bioinformatics.irida.model.sample.Sample;
