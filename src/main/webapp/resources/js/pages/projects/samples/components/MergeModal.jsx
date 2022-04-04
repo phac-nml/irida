7f8b8f42eaa6e7b1072ee66a603c65264eb9c077
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-04-04 07:14:52 -0500

    Cleaned up some of the MergeSample modal layout

--- src/main/webapp/resources/js/pages/projects/samples/components/MergeModal.jsx
+++ src/main/webapp/resources/js/pages/projects/samples/components/MergeModal.jsx
@@ -9,170 +9,81 @@ import {
   Radio,
   Row,
   Space,
-  Typography
+  Typography,
 } from "antd";
 
 export default function MergeModal({ samples, visible, onOk }) {
-                                                                 const copy = Object.entries(
-                                                                   samples
-                                                                 ).map(
-                                                                   ([
-                                                                     ,
-                                                                     sample
-                                                                   ]) => sample
-                                                                 );
-                                                                 const [
-                                                                   value,
-                                                                   setValue
-                                                                 ] = React.useState(
-                                                                   copy[0].id
-                                                                 );
-                                                                 const [
-                                                                   renameSample,
-                                                                   setRenameSample
-                                                                 ] = React.useState(
-                                                                   false
-                                                                 );
+  const copy = Object.entries(samples).map(([, sample]) => sample);
+  const [value, setValue] = React.useState(copy[0].id);
+  const [renameSample, setRenameSample] = React.useState(false);
 
-                                                                 return (
-                                                                   <Modal
-                                                                     title="Merge Samples"
-                                                                     visible={
-                                                                       visible
-                                                                     }
-                                                                     onOk={onOk}
-                                                                     onCancel={
-                                                                       onOk
-                                                                     }
-                                                                   >
-                                                                     <Row
-                                                                       gutter={[
-                                                                         16,
-                                                                         16
-                                                                       ]}
-                                                                     >
-                                                                       <Col
-                                                                         span={
-                                                                           24
-                                                                         }
-                                                                       >
-                                                                         <Alert
-                                                                           type="warning"
-                                                                           showIcon
-                                                                           message={
-                                                                             "All samples will be merged into a single sample."
-                                                                           }
-                                                                           description={
-                                                                             "Currently only the metadata from the sample selected will retain its metadata."
-                                                                           }
-                                                                         />
-                                                                       </Col>
-                                                                       <Col
-                                                                         span={
-                                                                           24
-                                                                         }
-                                                                       >
-                                                                         <Typography.Text>
-                                                                           The
-                                                                           following
-                                                                           2
-                                                                           samples
-                                                                           will
-                                                                           be
-                                                                           merged
-                                                                           into
-                                                                           the
-                                                                           selected
-                                                                           sample.
-                                                                         </Typography.Text>
-                                                                       </Col>
-                                                                       <Col
-                                                                         span={
-                                                                           24
-                                                                         }
-                                                                       >
-                                                                         <Form layout="vertical">
-                                                                           <Form.Item
-                                                                             label={
-                                                                               "Select primary sample"
-                                                                             }
-                                                                             validateStatus="warning"
-                                                                             help={
-                                                                               "Other samples details will be overwritten by the primary sample"
-                                                                             }
-                                                                             required
-                                                                           >
-                                                                             <Radio.Group
-                                                                               value={
-                                                                                 value
-                                                                               }
-                                                                             >
-                                                                               <Space
-                                                                                 direction="vertical"
-                                                                                 onChange={e =>
-                                                                                   setValue(
-                                                                                     e
-                                                                                       .target
-                                                                                       .value
-                                                                                   )
-                                                                                 }
-                                                                                 value={
-                                                                                   value
-                                                                                 }
-                                                                               >
-                                                                                 {copy.map(
-                                                                                   sample => {
-                                                                                     return (
-                                                                                       <Radio
-                                                                                         value={
-                                                                                           sample.id
-                                                                                         }
-                                                                                         key={`sample-${sample.id}`}
-                                                                                       >
-                                                                                         {
-                                                                                           sample.sampleName
-                                                                                         }
-                                                                                       </Radio>
-                                                                                     );
-                                                                                   }
-                                                                                 )}
-                                                                               </Space>
-                                                                             </Radio.Group>
-                                                                           </Form.Item>
-                                                                           <Form.Item>
-                                                                             <Checkbox
-                                                                               checked={
-                                                                                 renameSample
-                                                                               }
-                                                                               onChange={e =>
-                                                                                 setRenameSample(
-                                                                                   e
-                                                                                     .target
-                                                                                     .checked
-                                                                                 )
-                                                                               }
-                                                                             >
-                                                                               Rename
-                                                                               Sample
-                                                                             </Checkbox>
-                                                                           </Form.Item>
-                                                                           <Form.Item
-                                                                             label={
-                                                                               "New sample name"
-                                                                             }
-                                                                             help={
-                                                                               "(Only letters, numbers and - ! @ # $ % ~ ', No spaces or tabs)"
-                                                                             }
-                                                                           >
-                                                                             <Input
-                                                                               disabled={
-                                                                                 !renameSample
-                                                                               }
-                                                                             />
-                                                                           </Form.Item>
-                                                                         </Form>
-                                                                       </Col>
-                                                                     </Row>
-                                                                   </Modal>
-                                                                 );
-                                                               }
+  return (
+    <Modal
+      title="Merge Samples"
+      visible={visible}
+      onOk={onOk}
+      onCancel={onOk}
+      width={600}
+    >
+      <Row gutter={[16, 16]}>
+        <Col span={24}>
+          <Alert
+            type="warning"
+            showIcon
+            message={"All samples will be merged into a single sample."}
+            description={
+              "Currently only the metadata from the sample selected will retain its metadata."
+            }
+          />
+        </Col>
+        <Col span={24}>
+          <Typography.Text>
+            The following 2 samples will be merged into the selected sample.
+          </Typography.Text>
+        </Col>
+        <Col span={24}>
+          <Form layout="vertical">
+            <Form.Item
+              label={"Select primary sample"}
+              tooltip={
+                "Other samples details will be overwritten by the primary sample"
+              }
+              required
+            >
+              <Radio.Group value={value}>
+                <Space
+                  direction="vertical"
+                  onChange={(e) => setValue(e.target.value)}
+                  value={value}
+                >
+                  {copy.map((sample) => {
+                    return (
+                      <Radio value={sample.id} key={`sample-${sample.id}`}>
+                        {sample.sampleName}
+                      </Radio>
+                    );
+                  })}
+                </Space>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item
+              help={
+                "(Only letters, numbers and - ! @ # $ % ~ ', No spaces or tabs)"
+              }
+            >
+              <Checkbox
+                checked={renameSample}
+                onChange={(e) => setRenameSample(e.target.checked)}
+              >
+                Rename Sample
+              </Checkbox>
+              <Form.Item noStyle>
+                <Input disabled={!renameSample} />
+              </Form.Item>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </Modal>
+  );
+}
