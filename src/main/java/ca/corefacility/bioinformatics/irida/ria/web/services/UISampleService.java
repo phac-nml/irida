5426d4297e2cb3825fbf6190ee12c824340e7086
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-04-03 20:23:18 -0500

    Merge sample layout
    Initial server code

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UISampleService.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/services/UISampleService.java
@@ -1,15 +1,5 @@
 package ca.corefacility.bioinformatics.irida.ria.web.services;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.data.domain.Page;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-
 import ca.corefacility.bioinformatics.irida.model.assembly.GenomeAssembly;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.ProjectSampleJoin;
 import ca.corefacility.bioinformatics.irida.model.joins.impl.SampleGenomeAssemblyJoin;
@@ -26,6 +16,7 @@ import ca.corefacility.bioinformatics.irida.ria.web.models.tables.AntTableRespon
 import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.ProjectCartSample;
 import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.ProjectSampleTableItem;
 import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.ProjectSamplesTableRequest;
+import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.samples.MergeRequest;
 import ca.corefacility.bioinformatics.irida.ria.web.projects.dto.samples.ProjectSamplesFilter;
 import ca.corefacility.bioinformatics.irida.ria.web.samples.dto.SampleDetails;
 import ca.corefacility.bioinformatics.irida.ria.web.samples.dto.SampleFiles;
@@ -35,8 +26,17 @@ import ca.corefacility.bioinformatics.irida.service.GenomeAssemblyService;
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
-
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * UI Service for samples
@@ -276,5 +276,12 @@ public class UISampleService {
 		}
 
 		return filteredProjectSamples;
+	}
+
+	public void mergeTwoSamples(MergeRequest request) {
+		List<Sample> samples = (List<Sample>) sampleService.readMultiple(request.getIds());
+		if (!Strings.isNullOrEmpty(request.getName())) {
+
+		}
 	}
 }
