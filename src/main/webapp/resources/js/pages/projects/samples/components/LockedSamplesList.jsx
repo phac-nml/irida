8c0abd0926b6bfbc51dde1d2073697f58f586f43
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-04-08 21:13:03 -0500

    chore: Updated JSDoc and i18n

--- src/main/webapp/resources/js/pages/projects/samples/components/LockedSamplesList.jsx
+++ src/main/webapp/resources/js/pages/projects/samples/components/LockedSamplesList.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { LockOutlined } from "@ant-design/icons";
 import { List, Space, Typography } from "antd";
 
+/**
+ * React Element to render a list of locked samples.  Use this when they
+ * cannot be used in the requested action (e.g. remove).
+ * @param {array} locked - list of samples that are locked from modification
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function LockedSamplesList({ locked }) {
   return (
     <List
@@ -10,9 +17,7 @@ export default function LockedSamplesList({ locked }) {
       header={
         <Space>
           <LockOutlined />
-          <Typography.Text>
-            You do not have permission to modify these samples
-          </Typography.Text>
+          <Typography.Text>{i18n("LockedSamplesList.header")}</Typography.Text>
         </Space>
       }
       dataSource={locked}
