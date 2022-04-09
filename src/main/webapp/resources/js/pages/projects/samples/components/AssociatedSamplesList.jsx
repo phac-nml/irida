0d902a3ec1bbd3651e019159e1ce06b78ef75bad
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-04-08 21:11:30 -0500

    chore: Updated JSDoc and i18n

--- src/main/webapp/resources/js/pages/projects/samples/components/AssociatedSamplesList.jsx
+++ src/main/webapp/resources/js/pages/projects/samples/components/AssociatedSamplesList.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { WarningOutlined } from "@ant-design/icons";
 import { List, Space, Typography } from "antd";
 
+/**
+ * React Element to render a list of associated samples.  Use this when they
+ * cannot be used in the requested action (e.g. remove).
+ * @param {array} associated - list of samples belonging to associated projects
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function AssociatedSamplesList({ associated }) {
   return (
     <List
@@ -11,8 +18,7 @@ export default function AssociatedSamplesList({ associated }) {
         <Space>
           <WarningOutlined />
           <Typography.Text>
-            These samples are from an associated project and cannot be removed
-            from there
+            {i18n("AssociatedSamplesList.header")}
           </Typography.Text>
         </Space>
       }
