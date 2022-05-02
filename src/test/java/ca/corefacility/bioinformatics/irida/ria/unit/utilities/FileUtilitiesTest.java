73d5b6c3461a3ea239dbafe8f027b160ef3cb558
Deep Sidhu <deepsidhu85@gmail.com>
2022-05-01 20:04:20 -0500

    Fixed null pointer

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/utilities/FileUtilitiesTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/utilities/FileUtilitiesTest.java
@@ -7,12 +7,22 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ca.corefacility.bioinformatics.irida.repositories.filesystem.IridaFileStorageLocalUtilityImpl;
+import ca.corefacility.bioinformatics.irida.repositories.filesystem.IridaFileStorageUtility;
 import ca.corefacility.bioinformatics.irida.ria.utilities.FileUtilities;
+import ca.corefacility.bioinformatics.irida.util.IridaFiles;
 
 public class FileUtilitiesTest {
-    
+    private IridaFileStorageUtility iridaFileStorageUtility;
+
+    @BeforeEach
+    public void setup () {
+        iridaFileStorageUtility = new IridaFileStorageLocalUtilityImpl();
+        IridaFiles.setIridaFileStorageUtility(iridaFileStorageUtility);
+    }
     @Test
     public void testIsZippedFile() throws IOException {
         Path snpTreePath = Paths.get("src/test/resources/files/snp_tree.tree");
