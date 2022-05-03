2fa2a46c4c556734cc2b2a6b97320f5626a99c18
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-05-03 07:53:29 -0500

    Fixed testing ProjectShareSamplesIT

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectShareSamplesIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/projects/ProjectShareSamplesIT.java
@@ -1,6 +1,5 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.projects;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import ca.corefacility.bioinformatics.irida.ria.integration.AbstractIridaUIITChromeDriver;
@@ -9,6 +8,8 @@ import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.Proje
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.projects.ShareSamplesPage;
 
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/projects/ProjectSamplesView.xml")
 public class ProjectShareSamplesIT extends AbstractIridaUIITChromeDriver {
@@ -19,8 +20,10 @@ public class ProjectShareSamplesIT extends AbstractIridaUIITChromeDriver {
 		LoginPage.loginAsManager(driver());
 
 		// SHARING SINGLE SAMPLE
+		ProjectSamplesPage samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
+		samplesPage.selectSampleByName("sample5fg44");
+		samplesPage.shareSamples();
 
-		addOneSample();
 		assertFalse(shareSamplesPage.isShareButtonDisabled(),
 				"Share button should be disabled without a project selected");
 		shareSamplesPage.searchForProject("project2");
@@ -31,8 +34,13 @@ public class ProjectShareSamplesIT extends AbstractIridaUIITChromeDriver {
 		assertEquals(shareSamplesPage.getSuccessTitle(), "Successfully Shared 1 Sample");
 
 		// MOVING MULTIPLE SAMPLES
+		samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
+		samplesPage.selectSampleByName("sample5fg44");
+		samplesPage.selectSampleByName("sample554sg5");
+		samplesPage.selectSampleByName("sample5ddfg4");
+		samplesPage.selectSampleByName("sample57567");
+		samplesPage.shareSamples();
 
-		addMultipleSamples();
 		assertFalse(shareSamplesPage.isShareButtonDisabled(),
 				"Share button should be disabled without a project selected");
 		shareSamplesPage.searchForProject("project2");
@@ -45,8 +53,10 @@ public class ProjectShareSamplesIT extends AbstractIridaUIITChromeDriver {
 		assertEquals(shareSamplesPage.getSuccessTitle(), "Successfully Moved Samples");
 
 		// MOVE SINGLE SAMPLE
+		samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
+		samplesPage.selectSampleByName("sample5fg44");
+		samplesPage.shareSamples();
 
-		addOneSample();
 		assertFalse(shareSamplesPage.isShareButtonDisabled(),
 				"Share button should be disabled without a project selected");
 		shareSamplesPage.searchForProject("project4");
@@ -58,8 +68,12 @@ public class ProjectShareSamplesIT extends AbstractIridaUIITChromeDriver {
 		assertEquals(shareSamplesPage.getSuccessTitle(), "Successfully Moved 1 Sample");
 
 		// SHARING MULTIPLE SAMPLES
-
-		addMultipleSamples();
+		samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
+		samplesPage.selectSampleByName("sample_5_fg_22");
+		samplesPage.selectSampleByName("sample-5-fg-22");
+		samplesPage.selectSampleByName("sample5dt5");
+		samplesPage.selectSampleByName("sample55422r");
+		samplesPage.shareSamples();
 		assertFalse(shareSamplesPage.isShareButtonDisabled(),
 				"Share button should be disabled without a project selected");
 		shareSamplesPage.searchForProject("project2");
@@ -72,20 +86,5 @@ public class ProjectShareSamplesIT extends AbstractIridaUIITChromeDriver {
 		assertTrue(shareSamplesPage.isSuccessResultDisplayed(), "Success result should be displayed");
 		assertEquals(shareSamplesPage.getSuccessTitle(), "Successfully Shared Samples");
 
-	}
-
-	private void addOneSample() {
-		ProjectSamplesPage samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
-		samplesPage.selectSample(0);
-		samplesPage.shareSamples();
-	}
-
-	private void addMultipleSamples() {
-		ProjectSamplesPage samplesPage = ProjectSamplesPage.gotToPage(driver(), 1);
-		samplesPage.selectSample(0);
-		samplesPage.selectSample(1);
-		samplesPage.selectSample(2);
-		samplesPage.selectSample(3);
-		samplesPage.shareSamples();
 	}
 }
