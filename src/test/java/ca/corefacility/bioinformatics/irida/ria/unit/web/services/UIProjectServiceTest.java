8540215c5858d35f115e441b1c139b5df2d2b704
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-05-04 10:13:21 -0500

    Testing create with samples

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/services/UIProjectServiceTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/unit/web/services/UIProjectServiceTest.java
@@ -12,10 +12,13 @@ import ca.corefacility.bioinformatics.irida.security.permissions.project.Project
 import ca.corefacility.bioinformatics.irida.service.ProjectService;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 
+import com.google.common.collect.ImmutableList;
+
 import static org.mockito.Mockito.*;
 
 public class UIProjectServiceTest {
 	private final Project PROJECT_01 = new Project();
+	private final Long PROJECT_01_ID = 1L;
 	private UIProjectsService service;
 	private ProjectService projectService;
 	private SampleService sampleService;
@@ -32,17 +35,24 @@ public class UIProjectServiceTest {
 				manageLocalProjectSettingsPermission);
 
 		// Set up the project
-		PROJECT_01.setId(1L);
+		PROJECT_01.setId(PROJECT_01_ID);
 		PROJECT_01.setName("Test Project");
 		when(projectService.create(any(Project.class))).thenReturn(PROJECT_01);
+		when(projectService.createProjectWithSamples(any(Project.class), anyList(), anyBoolean())).thenReturn(
+				PROJECT_01);
 	}
 
 	@Test
 	public void testCreateProject() {
 		CreateProjectRequest request = new CreateProjectRequest();
 		request.setName("Test Project");
+		request.setLock(false);
 		service.createProject(request);
 
 		verify(projectService, times(1)).create(any(Project.class));
+
+		request.setSamples(ImmutableList.of(1L, 2L));
+		service.createProject(request);
+		verify(projectService, times(1)).createProjectWithSamples(any(Project.class), anyList(), anyBoolean());
 	}
 }
