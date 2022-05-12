9a4e0b11ab92ef818a47f666f5dad1e48926d04d
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-05-12 11:37:39 -0500

    Finished code for saving template

--- src/main/webapp/resources/js/pages/projects/linelist/components/TableControlPanel/TemplatesPanel/SaveTemplateModal.jsx
+++ src/main/webapp/resources/js/pages/projects/linelist/components/TableControlPanel/TemplatesPanel/SaveTemplateModal.jsx
@@ -1,249 +1,18 @@
 import React from "react";
-
-import isEqual from "lodash/isEqual";
-import { AutoComplete, Button, Checkbox, Form, Modal, Select } from "antd";
-
-const { Item } = Form;
-const { Option } = Select;
+import { AutoComplete, Checkbox, Form, Modal } from "antd";
 
 /**
- * Utility method to sort array of strings case insensitively.
- * @param {string} a
- * @param {string} b
- * @returns {number}
+ * Ant Design component to allow the user to save a metadata template.  Allows the
+ * user to either give a new name or overwrite the existing name.
+ *
+ * @param {function} onClose - function to close the modal
+ * @param {function} saveTemplate - function to save the template
+ * @param {array} templates - list of metadata template on the current project
+ * @param {boolean} visible - whether the modal is currently visible
+ * @param {object} template - the current state of the template to save
+ * @returns {JSX.Element}
+ * @constructor
  */
-const sortNames = (a, b) => a.toLowerCase().localeCompare(b.toLowerCase());
-
-/**
- * Custom footer for the SaveTemplateModal allowing the save button to
- * be disabled if the Template name is not valid.
- */
-function Footer(props) {
-  return (
-    <div>
-      <Button onClick={props.onCancel}>{i18n("form.btn.cancel")}</Button>
-      <Button
-        className="t-modal-save-template-btn"
-        type="primary"
-        disabled={props.disabled}
-        onClick={props.onClick}
-      >
-        {i18n("form.btn.save")}
-      </Button>
-    </div>
-  );
-}
-
-/**
- * Component to render a modal for the user to save the current state of the
- * linelist as a new MetadataTemplate.
- */
-class SaveTemplateModal__ extends React.Component {
-  validations = [
-    {
-      type: "required",
-      fn: (name) => name.length === 0,
-      state: {
-        existingTemplate: false,
-        status: "error",
-        message: i18n("linelist.templates.saveModal.required"),
-        valid: false,
-        overwriteTemplate: false,
-      },
-    },
-    {
-      type: "length",
-      fn: (name) => name.length < 5,
-      state: {
-        status: "error",
-        message: i18n("linelist.templates.saveModal.length"),
-        valid: false,
-        existingTemplate: false,
-        overwriteTemplate: false,
-      },
-    },
-    {
-      type: "nameExists",
-      fn: (name) => this._options.findIndex((o) => o === name) > -1,
-      state: {
-        status: "error",
-        message: i18n("linelist.templates.saveModal.nameExists"),
-        valid: false,
-        existingTemplate: true,
-        overwriteTemplate: false,
-      },
-    },
-    {
-      type: "valid",
-      fn: () => true,
-      state: {
-        status: "success",
-        message: "",
-        valid: true,
-        existingTemplate: false,
-      },
-    },
-  ];
-
-  constructor(props) {
-    super(props);
-    console.log({ props });
-    this._options = this.props.templates.map((t) => t.name).sort(sortNames);
-
-    this.state = {
-      options: this._options,
-      disabledLabel: this._options[0], // Name of the "all fields" option. Cannot save by that name.
-    };
-  }
-
-  /**
-   * React function immediately after updating occurs.  Used to check if an update
-   * to the UI is required.
-   * @param {object} prevProps
-   */
-  componentDidUpdate(prevProps) {
-    const template = this.props.template;
-
-    /*
-    Check to see if there is a new template being used, the new template
-    will require a refresh to the UI.
-     */
-    if (!isEqual(prevProps.template, template)) {
-      const existingTemplate = template.id > -1;
-      const value = existingTemplate ? template.name : undefined;
-      this.setState({
-        value,
-        existingTemplate,
-        overwriteTemplate: !existingTemplate,
-        valid: !existingTemplate,
-      });
-    }
-  }
-
-  /**
-   * What to do when searching the templates.
-   * @param {string} value - Value user searched in the select
-   */
-  onSearch = (value) => {
-    let options = Array.from(this._options);
-    if (value) {
-      // Determine if the name is in the current list of templates
-      if (!options.includes(value)) {
-        options.unshift(value);
-      }
-    }
-
-    // Validate the field to make sure that the template name is usable
-    for (const validation of this.validations) {
-      if (validation.fn(value)) {
-        this.setState({ value, options, ...validation.state });
-        return;
-      }
-    }
-
-    this.setState({ value });
-  };
-
-  /**
-   * How to filter each option in the select group
-   * @param {string} inputValue - Value entered by user
-   * @param {object} option - current select option to filter
-   * @returns {boolean}
-   */
-  filterOption = (inputValue, option) => {
-    return option.props.children
-      .toLowerCase()
-      .includes(inputValue.toLowerCase());
-  };
-
-  /**
-   * Save the current template using the entered name.
-   */
-  saveTemplate = () => {
-    const fields = this.props.template.modified.filter((t) => !t.hide);
-    const name = this.state.value;
-    const overwrite = this.state.overwriteTemplate;
-    let id = undefined;
-
-    if (overwrite) {
-      // Get the template to overwrite because we need its id.
-      const t = this.props.templates.find((t) => t.name === name);
-      id = t.id;
-    }
-
-    this.props.saveTemplate(name, fields, id);
-    this.props.onClose();
-  };
-
-  /**
-   * Event handler for when a user selects to overwrite and existing template.
-   * @param {object} e - Checkbox event
-   */
-  onExistingChange = (e) => {
-    this.setState({
-      valid: true,
-      overwriteTemplate: true,
-    });
-  };
-
-  render() {
-    const { existingTemplate, options, overwriteTemplate, value } = this.state;
-
-    return (
-      <Modal
-        closable={false}
-        title={i18n("linelist.templates.saveModal.title")}
-        visible={this.props.visible}
-        footer={
-          <Footer
-            disabled={!this.state.valid}
-            onCancel={this.props.onClose}
-            onClick={this.saveTemplate}
-          />
-        }
-      >
-        <Form>
-          <Item
-            label={i18n("linelist.templates.saveModal.name")}
-            hasFeedback
-            validateStatus={this.state.status}
-            help={this.state.message}
-          >
-            <Select
-              showSearch
-              className="t-template-name"
-              value={value}
-              style={{ width: "100%" }}
-              filterOption={this.filterOption}
-              onSearch={this.onSearch}
-              onChange={this.onSearch}
-            >
-              {options.map((template) => (
-                <Option
-                  disabled={template === this.state.disabledLabel}
-                  key={template}
-                >
-                  {template}
-                </Option>
-              ))}
-            </Select>
-          </Item>
-          {existingTemplate ? (
-            <Item>
-              <Checkbox
-                checked={overwriteTemplate}
-                onChange={this.onExistingChange}
-              >
-                Overwrite Existing Template
-              </Checkbox>
-            </Item>
-          ) : null}
-        </Form>
-      </Modal>
-    );
-  }
-}
-
 export function SaveTemplateModal({
   onClose,
   saveTemplate,
@@ -251,13 +20,21 @@ export function SaveTemplateModal({
   visible,
   template,
 }) {
-  console.log({ onClose, saveTemplate, templates, visible, template });
   const [form] = Form.useForm();
 
   const [options, setOptions] = React.useState(() =>
     templates.slice(1).map(({ name }) => ({ label: name, value: name }))
   );
+  const [existingName, setExistingName] = React.useState(false);
 
+  /**
+   * Handle user input.  This will filter the names available based on the current
+   * projects templates and provide a filtered list of those names.  Ant Design
+   * will allow the user to use the name they enter into the input field if not from
+   * one of the templates.
+   *
+   * @param {string} term - value user from input
+   */
   const onSearch = (term) => {
     const lowerTerm = term.toLowerCase();
     const newOptions = templates
@@ -267,21 +44,65 @@ export function SaveTemplateModal({
     setOptions(newOptions);
   };
 
+  /**
+   * When the user click ok, validate the name again and make sure all is good.
+   * When it is good submit to the server and then close the window.  Rest is
+   * handles by the parent components.
+   */
   const onOk = () => {
-    form.validateFields().then((values) => console.log({ values }));
+    form.validateFields().then((values) => {
+      const fields = template.modified.filter((t) => !t.hide);
+      let id = undefined;
+      if (values.overwrite) {
+        id = templates.find(({ name }) => name === values.name).id;
+      }
+      saveTemplate(values.name, fields, id);
+      onClose();
+    });
+  };
+
+  /**
+   * Validate the input template name against existing template names in this project
+   *
+   * @param {object} _ - Ant Design Form.Item object
+   * @param {String} value - Value from the input
+   * @returns {Promise<boolean>}
+   */
+  const existingValidator = async (_, value) => {
+    const overwrite = form.getFieldValue("overwrite");
+    const found = templates.findIndex(({ name }) => name === value) > -1;
+    setExistingName(found);
+    if (!overwrite && found) {
+      return Promise.reject();
+    }
+    return Promise.resolve(true);
   };
 
   return (
     <Modal
       title={i18n("linelist.templates.saveModal.title")}
       visible={visible}
+      onCancel={onClose}
       onOk={onOk}
     >
       <Form layout="vertical" form={form} initialValues={{ name: "" }}>
         <Form.Item
           label={i18n("linelist.templates.saveModal.name")}
           name="name"
-          rules={[{ required: true }]}
+          rules={[
+            {
+              required: true,
+              message: i18n("linelist.templates.saveModal.required"),
+            },
+            {
+              min: 5,
+              message: i18n("linelist.templates.saveModal.length"),
+            },
+            {
+              validator: existingValidator,
+              message: i18n("linelist.templates.saveModal.nameExists"),
+            },
+          ]}
         >
           <AutoComplete
             options={options}
@@ -291,6 +112,14 @@ export function SaveTemplateModal({
             notFoundContent={null}
             onSearch={onSearch}
           />
+        </Form.Item>
+        <Form.Item name="overwrite" valuePropName="checked">
+          <Checkbox
+            disabled={!existingName}
+            onChange={() => form.validateFields(["name"])}
+          >
+            {i18n("linelist.templates.saveModal.overwrite")}
+          </Checkbox>
         </Form.Item>
       </Form>
     </Modal>
