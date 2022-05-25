0365568a1109920977196a95609d6eff6db1f63e
Deep Sidhu <deepsidhu85@gmail.com>
2022-05-24 20:59:42 -0500

    Added controller tests

--- src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/PasswordResetAjaxControllerTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/web/controller/test/unit/PasswordResetAjaxControllerTest.java
@@ -1,6 +1,5 @@
 package ca.corefacility.bioinformatics.irida.web.controller.test.unit;
 
-import java.security.Principal;
 import java.util.Locale;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -8,9 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 
-import ca.corefacility.bioinformatics.irida.model.user.Role;
+import ca.corefacility.bioinformatics.irida.model.user.PasswordReset;
 import ca.corefacility.bioinformatics.irida.model.user.User;
+import ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.ajax.AjaxSuccessResponse;
 import ca.corefacility.bioinformatics.irida.ria.web.login.PasswordResetAjaxController;
 import ca.corefacility.bioinformatics.irida.ria.web.ajax.dto.ajax.AjaxResponse;
 import ca.corefacility.bioinformatics.irida.ria.web.services.UIPasswordResetService;
@@ -29,16 +33,64 @@ public class PasswordResetAjaxControllerTest {
 	private UIPasswordResetService uiPasswordResetService;
 	private PasswordResetAjaxController controller;
 
+	private Model model;
+	private User user = new User(1L, "tom", "tom@somewhere.com", null, null, null, null);
+	private PasswordReset passwordReset = new PasswordReset(user);
+
 	@BeforeEach
 	void setUp() {
 		userService = mock(UserService.class);
 		emailController = mock(EmailController.class);
 		messageSource = mock(MessageSource.class);
 		passwordResetService = mock(PasswordResetService.class);
+		model = mock(Model.class);
 		uiPasswordResetService = new UIPasswordResetService(userService, passwordResetService, emailController,
 				messageSource);
 		controller = new PasswordResetAjaxController(uiPasswordResetService);
+
 	}
 
+	@Test
+	public void testCreateAndSendNewPasswordResetEmail(){
+		String successMessage = "Check your email for password reset instructions";
+		when(userService.getUserByUsername(user.getUsername())).thenReturn(user);
+		when(messageSource.getMessage("server.ForgotPassword.checkEmail", null, Locale.ENGLISH)).thenReturn(successMessage);
+		ResponseEntity<AjaxResponse> response = controller.createAndSendNewPasswordResetEmail(user.getUsername(), Locale.ENGLISH);
+		assertEquals(response.getStatusCode(), HttpStatus.OK, "Receive an 200 OK response");
+		verify(userService, times(1)).getUserByUsername(user.getUsername());
+		AjaxSuccessResponse ajaxSuccessResponse = (AjaxSuccessResponse) response.getBody();
+		assertEquals(successMessage, ajaxSuccessResponse.getMessage(), "Messages should be equal");
+	}
+
+	@Test
+	public void testActivateAccount() {
+		when(passwordResetService.read(passwordReset.getId())).thenReturn(passwordReset);
+		ResponseEntity<AjaxResponse> response = controller.activateAccount(passwordReset.getId(), Locale.ENGLISH);
+		assertEquals(response.getStatusCode(), HttpStatus.OK, "Receive an 200 OK response");
+
+		AjaxSuccessResponse ajaxSuccessResponse = (AjaxSuccessResponse) response.getBody();
+		assertEquals(passwordReset.getId(), ajaxSuccessResponse.getMessage(), "The password reset identifier should be returned if it is valid");
+
+	}
+
+	@Test
+	public void testUpdatePassword() {
+		when(passwordResetService.read(passwordReset.getId())).thenReturn(passwordReset);
+		ResponseEntity<AjaxResponse> response = controller.updatePassword(passwordReset.getId(), "NewPassword1!", model, Locale.ENGLISH);
+		assertEquals(response.getStatusCode(), HttpStatus.OK, "Receive an 200 OK response");
+
+		User user2 = passwordReset.getUser();
+		assertEquals(user.getId(), user2.getId(), "The correct user should have the password reset set");
+
+		Authentication auth = new UsernamePasswordAuthenticationToken(user2, null);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+
+		verify(userService, times(1)).changePassword(user2.getId(), "NewPassword1!");
+		verify(passwordResetService, times(1)).delete(passwordReset.getId());
+		verify(userService, times(1)).loadUserByEmail(user2.getEmail());
+
+		AjaxSuccessResponse ajaxSuccessResponse = (AjaxSuccessResponse)response.getBody();
+		assertEquals("success", ajaxSuccessResponse.getMessage(), "Result should be success");
+	}
 
 }
