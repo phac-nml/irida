aa98f6d38671d6fe3ba92248a1eb8eb8db118be2
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-05-26 11:37:52 -0500

    Make sure only admins can get to it ;)

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/SPAController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/SPAController.java
@@ -1,13 +1,23 @@
 package ca.corefacility.bioinformatics.irida.ria.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller to handle entry points to new IRIDA SPAs
+ */
 @Controller
 @RequestMapping("/beta")
 public class SPAController {
 
+	/**
+	 * Get entry point for the administration panel
+	 *
+	 * @return Path to template
+	 */
 	@RequestMapping("/admin/**")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public String getAdminPage() {
 		return "beta/admin";
 	}
