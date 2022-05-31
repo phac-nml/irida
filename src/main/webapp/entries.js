e6d27deecd5330e6b4dae7a93487cf730c93ad02
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2022-05-31 09:46:52 -0500

    removing unused files

--- src/main/webapp/entries.js
+++ src/main/webapp/entries.js
@@ -31,7 +31,6 @@ module.exports = {
   "project-analyses": "./resources/js/pages/projects/project-analyses/",
   analyses: "./resources/js/pages/analyses/AnalysesPage.jsx",
   "users-list": "./resources/js/pages/user/components/UserListPage.jsx",
-  "users-password": "./resources/js/pages/users/users-password.js",
   "sample-edit": "./resources/js/pages/samples/sample-edit.js",
   "sample-files-concatenate":
     "./resources/js/pages/samples/sample-files-concatenate.js",
