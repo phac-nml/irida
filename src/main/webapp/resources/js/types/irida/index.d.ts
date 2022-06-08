93a80bc2a3ef9ec0e3af6e389ebfcfa06c7cbb97
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-06-07 21:50:34 -0500

    Added type for ExportUploadState

--- src/main/webapp/resources/js/types/irida/index.d.ts
+++ src/main/webapp/resources/js/types/irida/index.d.ts
@@ -2,71 +2,88 @@ export = IRIDA;
 export as namespace IRIDA;
 
 declare namespace IRIDA {
-    interface IridaBase {
-        id: number;
-        key: string;
-        name: string;
-        createdDate: Date;
-        modifiedDate: Date;
-    }
+  interface IridaBase {
+    id: number;
+    key: string;
+    name: string;
+    createdDate: Date;
+    modifiedDate: Date;
+  }
 
-    interface Announcement extends IridaBase {
-        title: string;
-        message: string;
-        priority: boolean;
-        createdBy: User;
-        users: User[];
-    }
+  interface Announcement extends IridaBase {
+    title: string;
+    message: string;
+    priority: boolean;
+    createdBy: User;
+    users: User[];
+  }
 
-    interface Project extends IridaBase {
-        description: string;
-        organism: string;
-        genomeSize: number;
-        minimumCoverage: number;
-        maximumCoverage: number;
-        remoteStatus: number;
-        syncFrequency: string; // TODO (Josh - 6/7/22): is this a string?
-        analysisPriority: string;
-        users: User[];
-        analysisTemplates: string[]; // TODO (Josh - 6/7/22): What should this be
-    }
+  type ExportUploadState =
+    | "NEW"
+    | "UPLOADING"
+    | "UPLOADED"
+    | "UPLOADED_ERROR"
+    | "created"
+    | "failed"
+    | "queued"
+    | "processing"
+    | "processed-ok"
+    | "processed-error"
+    | "waiting"
+    | "submitted"
+    | "Submission deleted"
+    | "retired"
+    | "unknown";
 
-    interface Sample extends IridaBase {
-        description: string;
-        organism: string;
-        isolate: string;
-        strain: string;
-        collectedBy: string;
-        collectionDate: Date;
-        geographicLocationName: string;
-        isolationSource: string;
-        latitude: string;
-        longitude: string;
-        projects: Project[];
-        sequenceFiles: any[]; // TODO (Josh - 6/7/22): FLush this out
-    }
+  interface Project extends IridaBase {
+    description: string;
+    organism: string;
+    genomeSize: number;
+    minimumCoverage: number;
+    maximumCoverage: number;
+    remoteStatus: number;
+    syncFrequency: string; // TODO (Josh - 6/7/22): is this a string?
+    analysisPriority: string;
+    users: User[];
+    analysisTemplates: string[]; // TODO (Josh - 6/7/22): What should this be
+  }
 
-    export type SystemRole =
-        "ROLE_ANONYMOUS"
-        | "ROLE_ADMIN"
-        | "ROLE_USER"
-        | "ROLE_MANAGER"
-        | "ROLE_SEQUENCER"
-        | "ROLE_TECHNICIAN";
+  interface Sample extends IridaBase {
+    description: string;
+    organism: string;
+    isolate: string;
+    strain: string;
+    collectedBy: string;
+    collectionDate: Date;
+    geographicLocationName: string;
+    isolationSource: string;
+    latitude: string;
+    longitude: string;
+    projects: Project[];
+    sequenceFiles: any[]; // TODO (Josh - 6/7/22): FLush this out
+  }
 
-    interface User extends IridaBase {
-        username: string;
-        email: string;
-        firstName: string;
-        lastName: string;
-        phoneNumber: string;
-        enabled: boolean;
-        systemRole: SystemRole;
-        lastLogin: Date;
-        locale: string;
-        projects: Project[];
-        tokens: string[]; // TODO (Josh - 6/7/22): Look into this one
-        announcements: Announcement[];
-        subscriptions: string[]; // TODO (Josh - 6/7/22): Look into this one as well
-    }
+  export type SystemRole =
+    | "ROLE_ANONYMOUS"
+    | "ROLE_ADMIN"
+    | "ROLE_USER"
+    | "ROLE_MANAGER"
+    | "ROLE_SEQUENCER"
+    | "ROLE_TECHNICIAN";
+
+  interface User extends IridaBase {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    enabled: boolean;
+    systemRole: SystemRole;
+    lastLogin: Date;
+    locale: string;
+    projects: Project[];
+    tokens: string[]; // TODO (Josh - 6/7/22): Look into this one
+    announcements: Announcement[];
+    subscriptions: string[]; // TODO (Josh - 6/7/22): Look into this one as well
+  }
 }
