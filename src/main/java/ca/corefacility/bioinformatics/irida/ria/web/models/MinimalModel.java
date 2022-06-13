83c0934cc21539ab5571794b36ed74acc6e3b457
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-06-13 12:53:57 -0500

    Updated JavaDoc

--- src/main/java/ca/corefacility/bioinformatics/irida/ria/web/models/MinimalModel.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/ria/web/models/MinimalModel.java
@@ -1,5 +1,9 @@
 package ca.corefacility.bioinformatics.irida.ria.web.models;
 
+/**
+ * Basic implementation for items that want to return a smaller model to the UI Usually should stay as just name and
+ * id.
+ */
 public abstract class MinimalModel {
 	private final Long id;
 	private final String name;
