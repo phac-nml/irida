a22892986bde40a9af2dc964d7ee45859f9da942
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-06-15 05:44:54 -0500

    Cleaned up ProjectSPA.tsx

--- src/main/webapp/resources/js/pages/projects/ProjectSPA.tsx
+++ src/main/webapp/resources/js/pages/projects/ProjectSPA.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { DataBrowserRouter, Outlet, Route } from "react-router-dom";
-import NcbiExportTable, {
-  loader as exportsLoader,
-} from "../../components/ncbi/export-table";
-import { setBaseUrl } from "../../utilities/url-utilities";
+import {DataBrowserRouter, Outlet, Route} from "react-router-dom";
+import NcbiExportTable, {loader as exportsLoader,} from "../../components/ncbi/export-table";
+import {setBaseUrl} from "../../utilities/url-utilities";
 import ProjectNCBILayout from "./ncbi";
-import NCBIExportDetails, { loader as detailsLoader } from "../../components/ncbi/details";
+import NCBIExportDetails, {loader as detailsLoader,} from "../../components/ncbi/details";
+import DefaultErrorBoundary from "../../components/DefaultErrorBoundary";
 
+/**
+ * Default layout for the Project Single Page Application
+ * @constructor
+ */
 function ProjectBase(): JSX.Element {
   return (
     <div>
-      {/* TODO: NAV AND STUFF HERE */}
+      {/* TODO: NAV AND OTHER TOP LEVEL ITEMS HERE */}
       <Outlet />
     </div>
   );
 }
 
+/**
+ * Base component for the Project SPA.
+ *  - Routes
+ *  - Any Redux store should be added here
+ * @constructor
+ */
 export default function ProjectSPA(): JSX.Element {
   return (
     <DataBrowserRouter>
@@ -28,12 +37,13 @@ export default function ProjectSPA(): JSX.Element {
             index
             element={<NcbiExportTable />}
             loader={exportsLoader}
-            errorElement={<div>FUCK ME, I BROKE!</div>}
+            errorElement={<DefaultErrorBoundary />}
           />
           <Route
             path=":id"
             element={<NCBIExportDetails />}
             loader={detailsLoader}
+            errorElement={<DefaultErrorBoundary />}
           />
         </Route>
       </Route>
