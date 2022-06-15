7b3a84c56fdd24225487870f46c33f47f9faf7d8
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-06-14 21:36:17 -0500

    Updated JSDoc

--- src/main/webapp/resources/js/apis/export/ncbi.ts
+++ src/main/webapp/resources/js/apis/export/ncbi.ts
@@ -1,54 +1,63 @@
 import axios from "axios";
-import {ExportUploadState, NcbiSubmission, User, UserMinimal} from "../../types/irida";
-import {setBaseUrl} from "../../utilities/url-utilities";
+import { NcbiSubmission, UserMinimal } from "../../types/irida";
+import { setBaseUrl } from "../../utilities/url-utilities";
+import { ExportUploadState } from "../../types/irida/ExportUpoadState";
 
 const BASE_URL = setBaseUrl(`/ajax/ncbi/project`);
 
 export interface NcbiExportSubmissionTableModel {
-    exportedSamples: number;
-    state: ExportUploadState;
-    submitter: UserMinimal;
-    bioProjectId: string;
+  exportedSamples: number;
+  state: ExportUploadState;
+  submitter: UserMinimal;
+  bioProjectId: string;
 }
 
 /**
- * Get a list of ncbi exports for the current project.
+ * Fetch a list of all NCBI SRA Submission for a given project
+ * @param projectId - Identifier for the current project
  */
-export async function getProjectNCBIExports(projectId: number): Promise<NcbiExportSubmissionTableModel[]> {
-    try {
-        const {data} = await axios.get(`/ajax/ncbi/project/${projectId}/list`);
-        return data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            if (error.response) {
-                return Promise.reject(error.response.data.error);
-            } else {
-                return Promise.reject(error.message);
-            }
-        } else {
-            return Promise.reject("An unexpected error occurred");
-        }
+export async function getProjectNCBIExports(
+  projectId: number
+): Promise<NcbiExportSubmissionTableModel[]> {
+  try {
+    const { data } = await axios.get(`/ajax/ncbi/project/${projectId}/list`);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        return Promise.reject(error.response.data.error);
+      } else {
+        return Promise.reject(error.message);
+      }
+    } else {
+      return Promise.reject("An unexpected error occurred");
     }
+  }
 }
 
+/**
+ * Get the details for an NCBI SRA Submission
+ * @param projectId - Identifier for the current project
+ * @param uploadId - Identifier for the submission
+ */
 export async function getNcbiSubmission(
-    projectId: number,
-    uploadId: number
+  projectId: number,
+  uploadId: number
 ): Promise<NcbiSubmission> {
-    try {
-        const {data} = await axios.get(
-            `${BASE_URL}/${projectId}/details/${uploadId}`
-        );
-        return data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            if (error.response) {
-                return Promise.reject(error.response.data.error);
-            } else {
-                return Promise.reject(error.message);
-            }
-        } else {
-            return Promise.reject("An unexpected error occurred");
-        }
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}/${projectId}/details/${uploadId}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response) {
+        return Promise.reject(error.response.data.error);
+      } else {
+        return Promise.reject(error.message);
+      }
+    } else {
+      return Promise.reject("An unexpected error occurred");
     }
+  }
 }
