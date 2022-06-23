8696a3859a4e44d9076d7aba9fa267220127e8cd
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2022-06-23 14:57:31 -0500

    cleanup + docs

--- src/main/java/ca/corefacility/bioinformatics/irida/exceptions/IridaLdapAuthenticationException.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/exceptions/IridaLdapAuthenticationException.java
@@ -2,9 +2,17 @@ package ca.corefacility.bioinformatics.irida.exceptions;
 
 import org.springframework.security.core.AuthenticationException;
 
+/**
+ * Thrown when an error occurs while trying to create a new user via LDAP/ADLDAP credentials
+ */
 public class IridaLdapAuthenticationException extends AuthenticationException {
 	private int errorCode;
 
+	/**
+	 * Creates a new {@link IridaLdapAuthenticationException} with error code
+	 * @param msg String
+	 * @param errorCode int alighed with the LoginPage.ldap_error.description_X properties in messages.properties
+	 */
 	public IridaLdapAuthenticationException(String msg, int errorCode) {
 		super(msg);
 		this.errorCode = errorCode;
--- src/test/java/ca/corefacility/bioinformatics/irida/service/analysis/workspace/galaxy/impl/integration/AnalysisWorkspaceServiceGalaxyIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/service/analysis/workspace/galaxy/impl/integration/AnalysisWorkspaceServiceGalaxyIT.java
@@ -29,7 +29,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
@@ -85,7 +84,6 @@ import ca.corefacility.bioinformatics.irida.service.SequencingObjectService;
 import ca.corefacility.bioinformatics.irida.service.analysis.workspace.galaxy.AnalysisWorkspaceServiceGalaxy;
 import ca.corefacility.bioinformatics.irida.service.sample.SampleService;
 import ca.corefacility.bioinformatics.irida.service.workflow.IridaWorkflowsService;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Tests out preparing a workspace for execution of workflows in Galaxy.
@@ -1058,8 +1056,6 @@ public class AnalysisWorkspaceServiceGalaxyIT {
 	 * @throws TimeoutException
 	 */
 	@Test
-//	@Transactional
-//	@DirtiesContext
 	@WithMockUser(username = "aaron", roles = "ADMIN")
 	public void testGetAnalysisResultsTestAnalysisPairedSingleSampleSuccess()
 			throws InterruptedException, ExecutionManagerException, IridaWorkflowNotFoundException, IOException,
