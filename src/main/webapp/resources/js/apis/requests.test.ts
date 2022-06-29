cc3ace067f627f19a596e134ba8c208e96ecd7c2
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-06-29 12:49:45 -0500

    ts-ignore required

--- src/main/webapp/resources/js/apis/requests.test.ts
+++ src/main/webapp/resources/js/apis/requests.test.ts
@@ -6,6 +6,8 @@ const mockedAxios = axios as jest.MockedFunction<typeof axios>;
 
 test(`Fetches successfully`, async () => {
   const data = { foo: `bar` };
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   mockedAxios.get.mockResolvedValueOnce({
     data,
     status: 200,
