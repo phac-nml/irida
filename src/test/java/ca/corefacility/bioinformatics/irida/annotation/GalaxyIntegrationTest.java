04a87ed800ffdf80bb4bc226895db417a7f39193
Eric Enns <eric.enns@phac-aspc.gc.ca>
2022-07-14 14:47:44 -0500

    Attempt fix galaxy integration tests.

--- src/test/java/ca/corefacility/bioinformatics/irida/annotation/GalaxyIntegrationTest.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/annotation/GalaxyIntegrationTest.java
@@ -8,19 +8,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.Tag;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import ca.corefacility.bioinformatics.irida.IridaApplication;
 import ca.corefacility.bioinformatics.irida.config.IridaApiGalaxyTestConfig;
 import ca.corefacility.bioinformatics.irida.config.data.IridaApiTestFilesystemConfig;
-import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConfig;
 
 /**
- * Annotation that is to be specified on Galaxy integration tests. Simplifies
- * the configuration of tests by automatically adding a number of necessary
- * annotations.
+ * Annotation that is to be specified on Galaxy integration tests. Simplifies the configuration of tests by
+ * automatically adding a number of necessary annotations.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
@@ -30,8 +27,8 @@ import ca.corefacility.bioinformatics.irida.config.services.IridaApiServicesConf
 @Tag("IntegrationTest")
 @Tag("Galaxy")
 @ActiveProfiles("test")
-@SpringBootTest(classes = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
-		IridaApiServicesConfig.class, IridaApiTestFilesystemConfig.class, IridaApiGalaxyTestConfig.class })
+@SpringBootTest(
+		classes = { IridaApplication.class, IridaApiTestFilesystemConfig.class, IridaApiGalaxyTestConfig.class })
 public @interface GalaxyIntegrationTest {
 
 }
