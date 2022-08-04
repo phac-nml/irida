091a6f7223200ad9aefc9689099695cdd5d9d706
Eric Enns <eric.enns@phac-aspc.gc.ca>
2022-08-04 14:46:30 -0500

    Remove old client_details tables that are no longer required.

--- src/main/java/ca/corefacility/bioinformatics/irida/model/IridaClientDetails.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/model/IridaClientDetails.java
@@ -1,6 +1,8 @@
 package ca.corefacility.bioinformatics.irida.model;
 
-import java.util.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -11,9 +13,7 @@ import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.GrantedAuthority;
 
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 
 /**
@@ -26,8 +26,6 @@ import com.google.common.collect.Sets;
 @Audited
 @EntityListeners(AuditingEntityListener.class)
 public class IridaClientDetails implements MutableIridaThing {
-	private static final long serialVersionUID = -1593194281520695701L;
-
 	public final static String CLIENT_ID_CONSTRAINT_NAME = "UK_CLIENT_DETAILS_CLIENT_ID";
 
 	// 12 hours
@@ -43,12 +41,6 @@ public class IridaClientDetails implements MutableIridaThing {
 	@NotNull
 	@Pattern(regexp = "[^\\s]+", message = "{remoteapi.details.nospace}")
 	private String clientId;
-
-	@NotNull
-	@ElementCollection(fetch = FetchType.EAGER)
-	@Column(name = "resource_id", nullable = false)
-	@CollectionTable(name = "client_details_resource_ids", joinColumns = @JoinColumn(name = "client_details_id"))
-	private Set<String> resourceIds;
 
 	@NotNull
 	private String clientSecret;
@@ -77,19 +69,6 @@ public class IridaClientDetails implements MutableIridaThing {
 	@Column(name = "refresh_validity")
 	private Integer refreshTokenValiditySeconds;
 
-	@ElementCollection(fetch = FetchType.EAGER)
-	@MapKeyColumn(name = "info_key")
-	@Column(name = "info_value")
-	@CollectionTable(name = "client_details_additional_information",
-			joinColumns = @JoinColumn(name = "client_details_id"))
-	private Map<String, String> additionalInformation;
-
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "client_details_authorities",
-			joinColumns = @JoinColumn(name = "client_details_id", nullable = false),
-			inverseJoinColumns = @JoinColumn(name = "authority_name", nullable = false))
-	Collection<ClientRole> authorities;
-
 	@LastModifiedDate
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date modifiedDate;
@@ -101,18 +80,15 @@ public class IridaClientDetails implements MutableIridaThing {
 	private Date createdDate;
 
 	/**
-	 * Default constructor with empty scopes, grant types, resource ids, redirect uris, and additional information
+	 * Default constructor with empty scopes, grant types, and redirect uris
 	 */
 	public IridaClientDetails() {
 		createdDate = new Date();
 		accessTokenValiditySeconds = DEFAULT_TOKEN_VALIDITY;
 		refreshTokenValiditySeconds = DEFAULT_REFRESH_TOKEN_VALIDITY;
 
-		resourceIds = new HashSet<>();
 		scope = new HashSet<>();
 		authorizedGrantTypes = new HashSet<>();
-		additionalInformation = new HashMap<>();
-		authorities = new HashSet<>();
 	}
 
 	/**
@@ -120,28 +96,20 @@ public class IridaClientDetails implements MutableIridaThing {
 	 * 
 	 * @param clientId             The ID of the client for this object
 	 * @param clientSecret         The Client Secret for this client
-	 * @param resourceIds          The resource IDs this client will access
 	 * @param scope                The scopes this client can access
 	 * @param authorizedGrantTypes The grant types allowed for this client
-	 * @param authorities          the collection of {@link ClientRole} that this client should have.
 	 */
-	public IridaClientDetails(String clientId, String clientSecret, Set<String> resourceIds, Set<String> scope,
-			Set<String> authorizedGrantTypes, Collection<ClientRole> authorities) {
+	public IridaClientDetails(String clientId, String clientSecret, Set<String> scope,
+			Set<String> authorizedGrantTypes) {
 		this();
 		this.clientId = clientId;
-		this.resourceIds = resourceIds;
 		this.clientSecret = clientSecret;
 		this.scope = scope;
 		this.authorizedGrantTypes = authorizedGrantTypes;
-		this.authorities = authorities;
 	}
 
 	public String getClientId() {
 		return clientId;
-	}
-
-	public Set<String> getResourceIds() {
-		return resourceIds;
 	}
 
 	public boolean isSecretRequired() {
@@ -176,20 +144,12 @@ public class IridaClientDetails implements MutableIridaThing {
 		return registeredRedirectUri;
 	}
 
-	public Collection<GrantedAuthority> getAuthorities() {
-		return new ArrayList<>(authorities);
-	}
-
 	public Integer getAccessTokenValiditySeconds() {
 		return accessTokenValiditySeconds;
 	}
 
 	public Integer getRefreshTokenValiditySeconds() {
 		return refreshTokenValiditySeconds;
-	}
-
-	public Map<String, Object> getAdditionalInformation() {
-		return Maps.newHashMap(additionalInformation);
 	}
 
 	public Long getId() {
@@ -202,13 +162,6 @@ public class IridaClientDetails implements MutableIridaThing {
 
 	public void setClientId(String clientId) {
 		this.clientId = clientId;
-	}
-
-	/**
-	 * @param resourceIds the resourceIds to set
-	 */
-	public void setResourceIds(Set<String> resourceIds) {
-		this.resourceIds = resourceIds;
 	}
 
 	/**
@@ -244,24 +197,6 @@ public class IridaClientDetails implements MutableIridaThing {
 	 */
 	public void setRefreshTokenValiditySeconds(Integer refreshTokenValiditySeconds) {
 		this.refreshTokenValiditySeconds = refreshTokenValiditySeconds;
-	}
-
-	/**
-	 * @param additionalInformation the additionalInformation to set
-	 */
-	public void setAdditionalInformation(Map<String, Object> additionalInformation) {
-		Map<String, String> newMap = new HashMap<>();
-		// create the new map by calling toString on all the values. I don't
-		// think we'll see non-string values here.
-		additionalInformation.entrySet().forEach(entry -> newMap.put(entry.getKey(), entry.getValue().toString()));
-		this.additionalInformation = newMap;
-	}
-
-	/**
-	 * @param authorities the authorities to set
-	 */
-	public void setAuthorities(Collection<ClientRole> authorities) {
-		this.authorities = authorities;
 	}
 
 	@Override
--- src/main/resources/ca/corefacility/bioinformatics/irida/sql/oauth-token.sql
+++ src/main/resources/ca/corefacility/bioinformatics/irida/sql/oauth-token.sql
@@ -1,10 +1,8 @@
 DROP TABLE IF EXISTS `oauth2_authorization`;
 DROP TABLE IF EXISTS `oauth2_authorization_consent`;
 
-DELETE FROM client_details_authorities;
 DELETE FROM client_details_scope;
 DELETE FROM client_details_grant_types;
-DELETE FROM client_details_resource_ids;
 DELETE FROM client_details;
 
 -- client details
@@ -13,14 +11,6 @@ insert into client_details (id, clientId, clientSecret, token_validity, createdD
 insert into client_details (id, clientId, clientSecret, token_validity, createdDate) values (3, "pythonLinker", "bySZBP5jNO9pSZTz3omFRtJs3XFAvshxGgvXIlZ2zjk", 43200,now());
 insert into client_details (id, clientId, clientSecret, token_validity, createdDate) values (4, "testClient", "testClientSecret", 43200,now());
 insert into client_details (id, clientId, clientSecret, redirect_uri, token_validity, createdDate) values (5, "webClient", "webClientSecret", "http://127.0.0.1:8080/api/oauth/authorization/token", 43200,now());
-
-insert into client_role (name, description) values ("ROLE_CLIENT","A basic OAuth2 client");
-
-insert into client_details_authorities (client_details_id,authority_name) values (1,"ROLE_CLIENT");
-insert into client_details_authorities (client_details_id,authority_name) values (2,"ROLE_CLIENT");
-insert into client_details_authorities (client_details_id,authority_name) values (3,"ROLE_CLIENT");
-insert into client_details_authorities (client_details_id,authority_name) values (4,"ROLE_CLIENT");
-insert into client_details_authorities (client_details_id,authority_name) values (5,"ROLE_CLIENT");
 
 insert into client_details_scope (client_details_id,scope) values (1,"read"), (1,"write");
 insert into client_details_scope (client_details_id,scope) values (2,"read");
@@ -33,12 +23,6 @@ insert into client_details_grant_types (client_details_id,grant_value) values (2
 insert into client_details_grant_types (client_details_id,grant_value) values (3,"password");
 insert into client_details_grant_types (client_details_id,grant_value) values (4,"password");
 insert into client_details_grant_types (client_details_id,grant_value) values (5,"authorization_code");
-
-insert into client_details_resource_ids (client_details_id,resource_id) values (1,"NmlIrida");
-insert into client_details_resource_ids (client_details_id,resource_id) values (2,"NmlIrida");
-insert into client_details_resource_ids (client_details_id,resource_id) values (3,"NmlIrida");
-insert into client_details_resource_ids (client_details_id,resource_id) values (4,"NmlIrida");
-insert into client_details_resource_ids (client_details_id,resource_id) values (5,"NmlIrida");
 
 CREATE TABLE `oauth2_authorization` ( `id` varchar(100) NOT NULL, `registered_client_id` varchar(100) NOT NULL, `principal_name` varchar(200) NOT NULL, `authorization_grant_type` varchar(100) NOT NULL, `attributes` varchar(4000) DEFAULT NULL, `state` varchar(500) DEFAULT NULL, `authorization_code_value` blob DEFAULT NULL, `authorization_code_issued_at` timestamp NULL DEFAULT NULL, `authorization_code_expires_at` timestamp NULL DEFAULT NULL, `authorization_code_metadata` varchar(2000) DEFAULT NULL, `access_token_value` blob DEFAULT NULL, `access_token_issued_at` timestamp NULL DEFAULT NULL, `access_token_expires_at` timestamp NULL DEFAULT NULL, `access_token_metadata` varchar(2000) DEFAULT NULL, `access_token_type` varchar(100) DEFAULT NULL, `access_token_scopes` varchar(1000) DEFAULT NULL, `oidc_id_token_value` blob DEFAULT NULL, `oidc_id_token_issued_at` timestamp NULL DEFAULT NULL, `oidc_id_token_expires_at` timestamp NULL DEFAULT NULL, `oidc_id_token_metadata` varchar(2000) DEFAULT NULL, `refresh_token_value` blob DEFAULT NULL, `refresh_token_issued_at` timestamp NULL DEFAULT NULL, `refresh_token_expires_at` timestamp NULL DEFAULT NULL, `refresh_token_metadata` varchar(2000) DEFAULT NULL, PRIMARY KEY (`id`));
 
