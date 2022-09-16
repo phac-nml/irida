049d8f9642eca9733abcfaed54f09bc1caecd086
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-09-16 13:16:12 -0500

    chore: Updated i18n for menu items

--- src/main/webapp/resources/js/pages/analysis/components/phylocanvas/PhylocanvasShapeDropDown.tsx
+++ src/main/webapp/resources/js/pages/analysis/components/phylocanvas/PhylocanvasShapeDropDown.tsx
@@ -1,10 +1,8 @@
 import { Button, Dropdown, Menu } from "antd";
 import React, { useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setBaseUrl } from "../../../../utilities/url-utilities";
 import { updateTreeType } from "../../redux/treeSlice";
 import { TreeTypes } from "@phylocanvas/phylocanvas.gl";
-import styled from "styled-components";
 import Icon from "@ant-design/icons";
 import { CustomIconComponentProps } from "@ant-design/icons/lib/components/Icon";
 
@@ -26,6 +24,18 @@ const DiagonalSVG = () => (
   </svg>
 );
 
+const CircularSVG = () => (
+  <svg viewBox="0 0 1024 1024" width="1em" height="1em" fill="currentColor">
+    <path d="M146.792,447.612l141.297,46.249c3.305,116.334 98.784,209.762 215.911,209.762c67.215,0 129.029,-31.103 169.226,-81.748l149.106,53.672c-20.556,38.143 -47.974,72.624 -81.279,101.531c-13.382,11.649 -15.647,31.591 -5.218,45.945c0.336,0.534 0.707,1.044 1.078,1.555c5.655,7.783 14.32,12.831 23.88,13.912c9.559,1.081 19.133,-1.905 26.383,-8.229c94.066,-81.641 148.824,-200.495 148.824,-326.261c0,-17.782 -1.098,-35.535 -3.283,-53.158c-1.199,-9.492 -6.32,-18.048 -14.118,-23.59c-7.799,-5.543 -17.563,-7.565 -26.922,-5.576c-0.668,0.063 -1.319,0.201 -1.97,0.339c-17.416,3.703 -29.098,20.125 -26.883,37.793c1.882,14.645 2.789,29.407 2.789,44.192c-0,36.011 -5.363,71.345 -15.576,104.997l-141.807,-51.045l0.002,-0.006l-0.456,-0.157l-43.863,-15.789l-0.219,0.61l-38.538,-13.27c-17.33,50.33 -63.89,84.555 -116.794,86.346c0,0 37.256,-108.083 56.468,-163.817c6.169,-17.897 -3.339,-37.407 -21.236,-43.576c-1.085,-0.374 -2.172,-0.749 -3.258,-1.123c-17.897,-6.169 -37.407,3.339 -43.576,21.236c-19.653,57.015 -58.354,169.291 -58.354,169.291c-37.449,-22.382 -62.543,-63.32 -62.543,-110.074c-0,-9.34 1.021,-18.639 3.038,-27.734l3.566,-10.895l-0.582,-0.191c0.082,-0.26 0.165,-0.518 0.249,-0.776l-83.562,-27.152l-0.192,0.596l-133.295,-43.63c34.229,-91.898 105.064,-167.554 197.868,-206.821c16.332,-6.957 24.648,-25.234 19.162,-42.118c-0.186,-0.599 -0.381,-1.197 -0.575,-1.795c-2.971,-9.144 -9.646,-16.62 -18.396,-20.604c-8.751,-3.984 -18.772,-4.11 -27.62,-0.347c-158.613,67.132 -263.524,223.223 -263.524,397.844c0,107.315 39.919,210.313 111.215,289.348c6.429,7.144 15.563,11.26 25.173,11.345c9.611,0.085 18.816,-3.869 25.371,-10.898c0.421,-0.449 0.84,-0.898 1.259,-1.347c12.117,-12.995 12.296,-33.093 0.414,-46.302c-59.649,-66.149 -93.045,-152.342 -93.045,-242.146c0,-19.113 1.502,-37.96 4.405,-56.388Z" />{" "}
+  </svg>
+);
+
+const HierarchicalSVG = () => (
+  <svg viewBox="0 0 1024 1024" width="1em" height="1em" fill="currentColor">
+    <path d="M872,476c0,-11.76 -5.651,-22.211 -14.384,-28.782c-6.021,-4.531 -13.507,-7.218 -21.616,-7.218l-144,-0l0,-252c0,-4.729 -0.914,-9.246 -2.573,-13.382l-0.007,-0.017c0,0 -0.029,-0.071 -0.029,-0.071c-5.339,-13.205 -18.285,-22.53 -33.391,-22.53l-216,-0l-0,-36c-0,-19.869 -16.131,-36 -36,-36c-19.869,-0 -36,16.131 -36,36l-0,36l-180,0c-12.41,0 -23.362,6.293 -29.836,15.859l-0.012,0.019c-0,-0 -0.038,0.056 -0.038,0.056c-3.86,5.734 -6.114,12.639 -6.114,20.066l-0,720c-0,19.869 16.131,36 36,36c19.869,0 36,-16.131 36,-36l-0,-684l396,-0l-0,216l-108,0c-8.109,0 -15.595,2.687 -21.616,7.218c-8.733,6.571 -14.384,17.022 -14.384,28.782l-0,432c-0,19.869 16.131,36 36,36c19.869,0 36,-16.131 36,-36l-0,-396l252,-0l-0,396c-0,19.869 16.131,36 36,36c19.869,0 36,-16.131 36,-36l-0,-432Z" />
+  </svg>
+);
+
 const RectangleIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={RectangleSVG} {...props} />
 );
@@ -38,15 +48,13 @@ const DiagonalIcon = (props: Partial<CustomIconComponentProps>) => (
   <Icon component={DiagonalSVG} {...props} />
 );
 
-const ClearMenu = styled(Menu)`
-  background-color: transparent;
-  box-shadow: none;
-`;
+const CircularIcon = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={CircularSVG} {...props} />
+);
 
-const ShadowButton = styled(Button)`
-  box-shadow: 0 3px 6px -4px rgb(0 0 0 / 12%), 0 6px 16px 0 rgb(0 0 0 / 8%),
-    0 9px 28px 8px rgb(0 0 0 / 5%);
-`;
+const HierarchicalIcon = (props: Partial<CustomIconComponentProps>) => (
+  <Icon component={HierarchicalSVG} {...props} />
+);
 
 export default function PhylocanvasShapeDropDown() {
   const dispatch = useDispatch();
@@ -55,49 +63,56 @@ export default function PhylocanvasShapeDropDown() {
     treeProps: { type },
   } = useSelector((state) => state.tree);
 
+  console.log({ type });
+
   const types = useMemo(
     () => ({
       [TreeTypes.Rectangular]: {
         icon: <RectangleIcon />,
-        title: "Rectangular",
+        title: i18n("PhylocanvasShapeDropDown.rectangular"),
       },
       [TreeTypes.Radial]: {
         icon: <RadialIcon />,
-        title: "Radial",
+        title: i18n("PhylocanvasShapeDropDown.radial"),
       },
       [TreeTypes.Diagonal]: {
         icon: <DiagonalIcon />,
-        title: "Diagonal",
+        title: i18n("PhylocanvasShapeDropDown.diagonal"),
       },
       [TreeTypes.Circular]: {
-        icon: setBaseUrl("/resources/img/phylocanvas/circular.svg"),
-        title: "Circular",
+        icon: <CircularIcon />,
+        title: i18n("PhylocanvasShapeDropDown.circular"),
+      },
+      [TreeTypes.Hierarchical]: {
+        icon: <HierarchicalIcon />,
+        title: i18n("PhylocanvasShapeDropDown.hierarchical"),
       },
     }),
     []
   );
 
   React.useEffect(() => {
-    console.log("IN EFFECT");
-    const current = Object.keys(types)
-      .filter((key) => key !== type)
-      .map((key) => (
-        <Menu.Item key={key} style={{ backgroundColor: "transparent" }}>
-          <ShadowButton
-            title={types[key].title}
-            onClick={() => dispatch(updateTreeType({ treeType: key }))}
-            icon={types[key].icon}
-            shape="circle"
-          />
-        </Menu.Item>
-      ));
+    const current = Object.keys(types).map((key) => (
+      <Menu.Item
+        key={key}
+        disabled={key === type}
+        style={{ backgroundColor: "transparent" }}
+        icon={types[key].icon}
+      >
+        {types[key].title}
+      </Menu.Item>
+    ));
     setOptions(current);
   }, [dispatch, type, types]);
 
-  const overlay = <ClearMenu>{options}</ClearMenu>;
+  const overlay = (
+    <Menu onClick={(item) => dispatch(updateTreeType({ treeType: item.key }))}>
+      {options}
+    </Menu>
+  );
 
   return (
-    <Dropdown overlay={overlay} placement="bottom" trigger="click">
+    <Dropdown overlay={overlay} trigger="click">
       <Button
         title={types[type].title}
         style={{ backgroundColor: `var(--grey-1)` }}
