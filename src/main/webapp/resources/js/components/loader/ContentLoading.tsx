9ec926b29bd28846e43a6f07ec29247d5d255290
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-09-29 10:53:01 -0500

    refactor: Updated i18n and ContentLoading component to use TS

--- src/main/webapp/resources/js/components/loader/ContentLoading.tsx
+++ src/main/webapp/resources/js/components/loader/ContentLoading.tsx
@@ -3,18 +3,21 @@
  * when data for a page is loading.
  */
 
-import React from "react";
-import { Spin } from "antd";
+import React, { ComponentProps, Props } from "react";
+import { Spin, SpinProps } from "antd";
 import { SPACE_SM } from "../../styles/spacing";
 
 /**
  * Stateless UI component for displaying a loading symbol with optional text
- * @param {string} message - Text to display next to loading symbol
- *
- * @returns {Element} - Returns a 'Spin' component from antd with optional text
  */
 
-export function ContentLoading({ message = "Loading", ...props }) {
+export function ContentLoading({
+  message = "Loading",
+  ...props
+}: {
+  message: string;
+  props: SpinProps;
+}): JSX.Element {
   return (
     <span>
       <Spin {...props} style={{ marginRight: SPACE_SM }} />
