bca3ba7a24d3ee197bc5c41fb9c7930bc91fc532
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-10-15 20:45:50 -0500

    chore: Updated to createRoot syntax for linelist

--- src/main/webapp/resources/js/pages/projects/linelist/index.js
+++ src/main/webapp/resources/js/pages/projects/linelist/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./linelist.css";
 import { setBaseUrl } from "../../../utilities/url-utilities";
 
 __webpack_public_path__ = setBaseUrl(`dist/`);
 
-// Render the application
-render(<App />, document.querySelector("#root"));
+// Render the app
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />);
