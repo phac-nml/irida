6331771c7fd8011c50517c58d5f9df717338bfb0
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-10-15 20:43:30 -0500

    chore: Updated to createRoot syntax for announcements page

--- src/main/webapp/resources/js/pages/announcement/index.js
+++ src/main/webapp/resources/js/pages/announcement/index.js
@@ -1,5 +1,6 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import React from "react";
 import { AnnouncementsPage } from "./components/AnnouncementsPage";
 
-render(<AnnouncementsPage />, document.querySelector("#announcements-root"));
+const root = createRoot(document.querySelector("#announcements-root"));
+root.render(<AnnouncementsPage />);
