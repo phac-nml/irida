884bbd36c1a6e953fed27f017c869b86042c3dac
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-10-15 20:34:42 -0500

    chore: Updated to createRoot syntax

--- src/main/webapp/resources/js/pages/projects/ProjectSPA.tsx
+++ src/main/webapp/resources/js/pages/projects/ProjectSPA.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { DataBrowserRouter, Outlet, Route } from "react-router-dom";
 import { loader as exportsLoader } from "../../components/ncbi/export-table";
 import { setBaseUrl } from "../../utilities/url-utilities";
@@ -78,4 +78,9 @@ export default function ProjectSPA(): JSX.Element {
   );
 }
 
-render(<ProjectSPA />, document.querySelector("#root"));
+const root = createRoot(document.querySelector("#root"));
+if (root) {
+  root.render(<ProjectSPA />);
+} else {
+  throw new Error("Cannot find dom node with id: #rrot");
+}
