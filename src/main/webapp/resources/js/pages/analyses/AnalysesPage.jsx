40a2f2643c42ae61f1ebc7e9d38b7283bf01dbdf
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-10-15 20:36:09 -0500

    chore: Updated to createRoot syntax

--- src/main/webapp/resources/js/pages/analyses/AnalysesPage.jsx
+++ src/main/webapp/resources/js/pages/analyses/AnalysesPage.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { AnalysesTable } from "../../components/AnalysesTable/AnalysesTable";
 import { PagedTableProvider } from "../../components/ant.design/PagedTable";
 import { PageWrapper } from "../../components/page/PageWrapper";
 import { AnalysesTableProvider } from "../../contexts/AnalysesTableContext";
 
-render(
+const root = createRoot(document.querySelector("#root"));
+root.render(
   <PageWrapper title={i18n("analyses.header")}>
     <PagedTableProvider url={window.PAGE.url}>
       <AnalysesTableProvider>
         <AnalysesTable canManage={window.PAGE.canManage} />
       </AnalysesTableProvider>
     </PagedTableProvider>
-  </PageWrapper>,
-  document.querySelector("#root")
+  </PageWrapper>
 );
