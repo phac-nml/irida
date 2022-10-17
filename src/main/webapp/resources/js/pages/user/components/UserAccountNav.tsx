5ea5e130ce40af43af229f35fc6021c1b525a266
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-10-17 16:11:35 -0500

    chore: Updated user account nav

---
+++ src/main/webapp/resources/js/pages/user/components/UserAccountNav.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Menu } from "antd";
+import type { MenuProps } from "antd";
+
+const menuItems: MenuProps["items"] = [
+  {
+    key: "account:details",
+    label: i18n("UserAccountNav.menu.details"),
+  },
+
+  {
+    key: "account:projects",
+    label: i18n("UserAccountNav.menu.projects"),
+  },
+  {
+    key: "account:security",
+    label: i18n("UserAccountNav.menu.security"),
+  },
+];
+
+/**
+ * React component to display the user account navigation.
+ * @returns {*}
+ * @constructor
+ */
+export default function UserAccountNav() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [selectedKeys, setSelectedKey] = React.useState<string[]>(() => {
+    const path = location.pathname.split("/").pop();
+    return [`account:${path}`];
+  });
+
+  const onClick: MenuProps["onClick"] = ({ key }) => {
+    const [, path] = key.split(":");
+    setSelectedKey([key]);
+    navigate(path);
+  };
+
+  return (
+    <Menu
+      items={menuItems}
+      mode="inline"
+      onClick={onClick}
+      selectedKeys={selectedKeys}
+      style={{ height: "100%" }}
+    />
+  );
+}
