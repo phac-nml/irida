d55590373ae23105408baae3e38fc3a339f720cc
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-10-17 14:28:50 -0500

    chore: Updated analysis settings nav

--- src/main/webapp/resources/js/pages/analysis/components/AnalysisSettings.tsx
+++ src/main/webapp/resources/js/pages/analysis/components/AnalysisSettings.tsx
@@ -9,9 +9,10 @@
  *required by the components encompassed within
  */
 
-import React, { Suspense, useContext } from "react";
+import React, { Suspense, useContext, useMemo } from "react";
+import type { MenuProps } from "antd";
 import { Layout, Menu } from "antd";
-import { Link, useLocation, Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 import { AnalysisContext } from "../../../contexts/AnalysisContext";
 import { AnalysisDetailsContext } from "../../../contexts/AnalysisDetailsContext";
@@ -19,8 +20,6 @@ import { AnalysisDetailsContext } from "../../../contexts/AnalysisDetailsContext
 import { SPACE_MD } from "../../../styles/spacing";
 import { ContentLoading } from "../../../components/loader/ContentLoading";
 import { grey1 } from "../../../styles/colors";
-import { ANALYSIS, SETTINGS } from "../routes";
-import { setBaseUrl } from "../../../utilities/url-utilities";
 import ScrollableSection from "./ScrollableSection";
 
 const { Content, Sider } = Layout;
@@ -30,27 +29,60 @@ const settingsPathRegx = /\/settings\/(?<path>\w+)/;
 // Regex for path where the url doesn't have settings in it
 const nonSettingsPathRegx = /\/\d+\/(?<path>\w+)/;
 
-export default function AnalysisSettings() {
+export default function AnalysisSettings(): JSX.Element {
   const location = useLocation();
-  const [current, setCurrent] = React.useState();
+  const navigate = useNavigate();
+  const [current, setCurrent] = React.useState<string>(() => {
+    const found =
+      location.pathname.match(settingsPathRegx) ||
+      location.pathname.match(nonSettingsPathRegx);
+
+    if (found && found.groups && found.groups.path !== "settings") {
+      return `analysis:settings:${found.groups.path}`;
+    } else {
+      return "analysis:settings:details";
+    }
+  });
+
   const { analysisDetailsContext } = useContext(AnalysisDetailsContext);
   const { analysisContext } = useContext(AnalysisContext);
 
   const analysisRunning =
     !analysisContext.isError && !analysisContext.isCompleted;
 
-  React.useEffect(() => {
-    const found =
-      location.pathname.match(settingsPathRegx) ||
-      location.pathname.match(nonSettingsPathRegx);
-    const path = found ? found.groups.path : SETTINGS.DETAILS;
+  const menuItems: MenuProps["items"] = useMemo(() => {
+    const items: MenuProps["items"] = [
+      {
+        key: "analysis:settings:details",
+        label: i18n("AnalysisDetails.details"),
+      },
+      {
+        key: "analysis:settings:samples",
+        label: i18n("AnalysisSamples.samples"),
+      },
+    ];
 
-    if (path === "settings") {
-      setCurrent(SETTINGS.DETAILS);
-    } else {
-      setCurrent(path);
+    if (analysisDetailsContext.updatePermission && !analysisContext.isError) {
+      items.push({
+        key: "analysis:settings:share",
+        label: i18n("AnalysisShare.manageResults"),
+      });
     }
-  }, [location.pathname]);
+
+    if (analysisDetailsContext.updatePermission) {
+      items.push({
+        key: "analysis:settings:delete",
+        label: i18n("AnalysisDelete.deleteAnalysis"),
+      });
+    }
+
+    return items;
+  }, [analysisContext.isError, analysisDetailsContext.updatePermission]);
+
+  const onClick: MenuProps["onClick"] = ({ key }) => {
+    setCurrent(key);
+    navigate(key.split(":")[2]);
+  };
 
   /*
    * The following renders the analysis details, and tabs
@@ -61,30 +93,12 @@ export default function AnalysisSettings() {
   return (
     <Layout style={{ height: `100%` }}>
       <Sider width={200} style={{ backgroundColor: grey1 }}>
-        <Menu mode="vertical" selectedKeys={[current]}>
-          <Menu.Item key="details">
-            <Link to={""}>{i18n("AnalysisDetails.details")}</Link>
-          </Menu.Item>
-          <Menu.Item key="samples">
-            <Link to={SETTINGS.SAMPLES}>{i18n("AnalysisSamples.samples")}</Link>
-          </Menu.Item>
-          {analysisDetailsContext.updatePermission
-            ? [
-                analysisContext.isError ? null : (
-                  <Menu.Item key="share">
-                    <Link to={SETTINGS.SHARE}>
-                      {i18n("AnalysisShare.manageResults")}
-                    </Link>
-                  </Menu.Item>
-                ),
-                <Menu.Item key="delete">
-                  <Link to={SETTINGS.DELETE}>
-                    {i18n("AnalysisDelete.deleteAnalysis")}
-                  </Link>
-                </Menu.Item>,
-              ]
-            : null}
-        </Menu>
+        <Menu
+          items={menuItems}
+          mode="vertical"
+          onClick={onClick}
+          selectedKeys={[current]}
+        />
       </Sider>
 
       <Layout style={{ paddingLeft: SPACE_MD, backgroundColor: grey1 }}>
