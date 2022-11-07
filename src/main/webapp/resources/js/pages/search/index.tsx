1492e512fd597bb7e54dfdeecad4db81caa5eb36
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-11-07 10:00:02 -0600

    chore:  Initial table setup with spacing

--- src/main/webapp/resources/js/pages/search/index.tsx
+++ src/main/webapp/resources/js/pages/search/index.tsx
@@ -1,5 +1,14 @@
-import { Checkbox, Input, Layout, PageHeader, Space } from "antd";
-import React, { useRef } from "react";
+import {
+  Checkbox,
+  Col,
+  Input,
+  Layout,
+  PageHeader,
+  Row,
+  Space,
+  Table,
+} from "antd";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import {
   createBrowserRouter,
@@ -26,19 +35,65 @@ function SearchLayout() {
   const user = useLoaderData();
   console.log(user);
   const [searchParams, setSearchParams] = useSearchParams();
-  const query = useRef<string>(searchParams.get("query"));
-  console.log(query);
+  const [query, setQuery] = useState<string>(searchParams.get("query") || "");
 
   return (
-    <PageHeader title={"SEARCH"}>
-      <Layout.Content style={{ backgroundColor: `var(--grey-1)`, padding: 12 }}>
-        <Space direction="vertical" style={{ width: `100%` }} size="small">
-          <Input.Search size={"large"} ref={query} />
-          <div>
-            <Checkbox>Project</Checkbox>
-            <Checkbox>Samples</Checkbox>
-            {user.admin && <Checkbox>Search Global</Checkbox>}
-          </div>
+    <PageHeader
+      title={"SEARCH"}
+      style={{
+        border: `5px solid green`,
+        flexGrow: 1,
+        display: `flex`,
+        flexDirection: "column",
+      }}
+    >
+      <Layout.Content
+        style={{
+          backgroundColor: `var(--grey-1)`,
+          padding: `var(--padding-md)`,
+          flexGrow: 1,
+        }}
+      >
+        <Space size="large" direction="vertical" style={{ width: `100%` }}>
+          <Space direction="vertical" size="small" style={{ width: `100%` }}>
+            <Input.Search
+              size={"large"}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <section>
+              <Checkbox>Project</Checkbox>
+              <Checkbox>Samples</Checkbox>
+              {user.admin && <Checkbox>Search Global</Checkbox>}
+            </section>
+          </Space>
+          <Table
+            dataSource={[
+              {
+                key: "1",
+                name: "Mike",
+                age: 32,
+                address: "10 Downing Street",
+              },
+            ]}
+            columns={[
+              {
+                title: "Name",
+                dataIndex: "name",
+                key: "name",
+              },
+              {
+                title: "Age",
+                dataIndex: "age",
+                key: "age",
+              },
+              {
+                title: "Address",
+                dataIndex: "address",
+                key: "address",
+              },
+            ]}
+          />
         </Space>
       </Layout.Content>
     </PageHeader>
@@ -47,7 +102,14 @@ function SearchLayout() {
 
 const element = document.querySelector("#root");
 render(
-  <Layout style={{ minHeight: `100%` }}>
+  <Layout
+    style={{
+      minHeight: `100%`,
+      border: `2px solid blue`,
+      display: `flex`,
+      flexDirection: "column",
+    }}
+  >
     <RouterProvider router={router} />
   </Layout>,
   element
