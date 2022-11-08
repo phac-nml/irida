effd2342279fb74d38a9aa4574b41a1c248b06d4
Deep Sidhu <deepsidhu85@gmail.com>
2022-11-08 12:34:23 -0600

    Added controller annotation

--- src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/RESTFilesystemController.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/web/controller/api/RESTFilesystemController.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,7 @@ import io.swagger.v3.oas.annotations.Operation;
 /**
  * REST controller to handle IRIDA file storage requests
  */
+@Controller
 public class RESTFilesystemController {
 	private static final Logger logger = LoggerFactory.getLogger(RESTFilesystemController.class);
 	private IridaFileStorageUtility iridaFileStorageUtility;
