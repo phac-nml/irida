effa040121501f9e0cfc471c6e47e59e5cd30167
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-11-10 08:14:58 -0600

    chore:  Updated JSDoc

--- src/main/webapp/resources/js/pages/search/SearchCount.tsx
+++ src/main/webapp/resources/js/pages/search/SearchCount.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Badge } from "antd";
 
+/**
+ * React component to display the total number of items found in the global search
+ * @param count
+ * @constructor
+ */
 export default function SearchCount({ count }: { count: number }) {
   return (
     <Badge
