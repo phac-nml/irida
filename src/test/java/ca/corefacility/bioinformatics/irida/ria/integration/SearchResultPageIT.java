5da6610bab118721b510f31b054d273103a45980
Josh Adam <josh.adam@phac-aspc.gc.ca>
2022-11-10 11:52:47 -0600

    test:  First test checking URL is updated correctly

--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/SearchResultPageIT.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/SearchResultPageIT.java
@@ -1,63 +1,68 @@
 package ca.corefacility.bioinformatics.irida.ria.integration;
 
-import org.junit.jupiter.api.Test;
-
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.LoginPage;
 import ca.corefacility.bioinformatics.irida.ria.integration.pages.SearchResultPage;
-
 import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Test class for global search
  */
 @DatabaseSetup("/ca/corefacility/bioinformatics/irida/ria/web/ProjectsPageIT.xml")
 public class SearchResultPageIT extends AbstractIridaUIITChromeDriver {
-	private SearchResultPage page;
 
 	@Test
-	public void testSampleSearch() {
-		LoginPage.loginAsUser(driver());
-		page = SearchResultPage.initPage(driver());
-
-		page.globalSearch("samp", false);
-		page = SearchResultPage.initPage(driver());
-		checkTranslations(page, ImmutableList.of("search"), null);
-
-		page.waitForSearchResults();
-		int sampleCount = page.getSampleCount();
-		int projectCount = page.getProjectCount();
-
-		assertEquals(2, sampleCount, "should be 2 samples");
-		assertEquals(0, projectCount, "should be no projects");
-
+	public void testGlobalSearchAsAdmin() {
+		LoginPage.loginAsAdmin(driver());
+		SearchResultPage searchResultPage = new SearchResultPage(driver());
+		final String query = "samp";
+		searchResultPage.enterSearchQueryInNavBar(query);
+		assertTrue(driver().getCurrentUrl().contains("search?query=" + query));
 	}
 
-	@Test
-	public void testProjectSearch() {
-		LoginPage.loginAsUser(driver());
-		page = SearchResultPage.initPage(driver());
-
-		page.globalSearch("project2", false);
-		page = SearchResultPage.initPage(driver());
-
-		page.waitForSearchResults();
-		int sampleCount = page.getSampleCount();
-		int projectCount = page.getProjectCount();
-
-		assertEquals(0, sampleCount, "should be no samples");
-		assertEquals(1, projectCount, "should be 1 project");
-
-		page.globalSearch("ABCD", false);
-		page = SearchResultPage.initPage(driver());
-
-		page.waitForSearchResults();
-		sampleCount = page.getSampleCount();
-		projectCount = page.getProjectCount();
-
-		assertEquals(0, sampleCount, "should be no samples");
-		assertEquals(0, projectCount, "should be no projects");
-	}
+//	@Test
+//	public void testSampleSearch() {
+//		LoginPage.loginAsUser(driver());
+//		page = SearchResultPage.initPage(driver());
+//
+//		page.globalSearch("samp", false);
+//		page = SearchResultPage.initPage(driver());
+//		checkTranslations(page, ImmutableList.of("search"), null);
+//
+//		page.waitForSearchResults();
+//		int sampleCount = page.getSampleCount();
+//		int projectCount = page.getProjectCount();
+//
+//		assertEquals(2, sampleCount, "should be 2 samples");
+//		assertEquals(0, projectCount, "should be no projects");
+//
+//	}
+//
+//	@Test
+//	public void testProjectSearch() {
+//		LoginPage.loginAsUser(driver());
+//		page = SearchResultPage.initPage(driver());
+//
+//		page.globalSearch("project2", false);
+//		page = SearchResultPage.initPage(driver());
+//
+//		page.waitForSearchResults();
+//		int sampleCount = page.getSampleCount();
+//		int projectCount = page.getProjectCount();
+//
+//		assertEquals(0, sampleCount, "should be no samples");
+//		assertEquals(1, projectCount, "should be 1 project");
+//
+//		page.globalSearch("ABCD", false);
+//		page = SearchResultPage.initPage(driver());
+//
+//		page.waitForSearchResults();
+//		sampleCount = page.getSampleCount();
+//		projectCount = page.getProjectCount();
+//
+//		assertEquals(0, sampleCount, "should be no samples");
+//		assertEquals(0, projectCount, "should be no projects");
+//	}
 }
--- src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/SearchResultPage.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/ria/integration/pages/SearchResultPage.java
@@ -1,57 +1,13 @@
 package ca.corefacility.bioinformatics.irida.ria.integration.pages;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 /**
  * Result page for global search
  */
 public class SearchResultPage extends AbstractPage {
 
-	@FindBy(id = "sample-count")
-	private WebElement sampleCount;
-
-	@FindBy(id = "project-count")
-	private WebElement projectCount;
-
 	public SearchResultPage(WebDriver driver) {
 		super(driver);
-	}
-
-	public static SearchResultPage initPage(WebDriver driver) {
-		return PageFactory.initElements(driver, SearchResultPage.class);
-	}
-
-	public void waitForSearchResults() {
-		int count = 0;
-		boolean found = false;
-		do {
-			waitForTime(100);
-
-			List<WebElement> spinners = driver.findElements(By.className("search-spinner"));
-
-			found = spinners.isEmpty();
-
-			if (count > 5) {
-				throw new RuntimeException("Search did not complete");
-			}
-
-			count++;
-
-		} while (!found);
-
-	}
-
-	public int getSampleCount() {
-		return Integer.parseInt(sampleCount.getText());
-	}
-
-	public int getProjectCount() {
-		return Integer.parseInt(projectCount.getText());
 	}
 }
