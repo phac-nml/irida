1f83cc6d5271201af05a38e2020e5fd64f8b5446
Eric Enns <eric.enns@phac-aspc.gc.ca>
2022-11-14 14:05:42 -0600

    chore: remove extra space

--- src/main/java/ca/corefacility/bioinformatics/irida/jackson2/mixin/SampleMixin.java
+++ src/main/java/ca/corefacility/bioinformatics/irida/jackson2/mixin/SampleMixin.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  * This mixin class is only used by the rest api for {@link Sample}.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
-@JsonIgnoreProperties(ignoreUnknown = true, value = { "defaultSequencingObject", "defaultGenomeAssembly " })
+@JsonIgnoreProperties(ignoreUnknown = true, value = { "defaultSequencingObject", "defaultGenomeAssembly" })
 public abstract class SampleMixin {
 
 }
