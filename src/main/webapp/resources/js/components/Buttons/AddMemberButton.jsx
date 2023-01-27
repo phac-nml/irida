4cf05f5154b16538c644bccc5e8775ef070cda1e
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-01-26 20:58:21 -0600

    removing empty function calls from AddMemberButton.jsx

--- src/main/webapp/resources/js/components/Buttons/AddMemberButton.jsx
+++ src/main/webapp/resources/js/components/Buttons/AddMemberButton.jsx
@@ -19,9 +19,9 @@ const { Text } = Typography;
 export function AddMemberButton({
   label,
   modalTitle,
-  addMemberFn = () => undefined,
-  getAvailableMembersFn = () => undefined,
-  addMemberSuccessFn = () => undefined,
+  addMemberFn,
+  getAvailableMembersFn,
+  addMemberSuccessFn,
 }) {
   /*
   Required a reference to the user select input so that focus can be set
