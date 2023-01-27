0eb00c18859b9254fe32d98744e94c446329c251
Katherine Thiessen <katherine.thiessen@phac-aspc.gc.ca>
2023-01-26 22:34:58 -0600

    converting index.js to index.tsx

---
+++ src/main/webapp/resources/js/pages/projects/samples-metadata-import/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { render } from "react-dom";
+import { SampleMetadataImportComplete } from "./components/SampleMetadataImportComplete";
+import { SampleMetadataImportMapColumns } from "./components/SampleMetadataImportMapColumns";
+import { SampleMetadataImportReview } from "./components/SampleMetadataImportReview";
+import { SampleMetadataImportSelectFile } from "./components/SampleMetadataImportSelectFile";
+import { setBaseUrl } from "../../../utilities/url-utilities";
+import store from "./redux/store";
+
+/**
+ * The Metadata Importer Page
+ */
+function SampleMetadataImport(): JSX.Element {
+  return (
+    <Provider store={store}>
+      <BrowserRouter basename={setBaseUrl("/projects")}>
+        <Routes>
+          <Route
+            path="/:projectId/sample-metadata/upload/file"
+            element={<SampleMetadataImportSelectFile />}
+          />
+          <Route
+            path="/:projectId/sample-metadata/upload/columns"
+            element={<SampleMetadataImportMapColumns />}
+          />
+          <Route
+            path="/:projectId/sample-metadata/upload/review"
+            element={<SampleMetadataImportReview />}
+          />
+          <Route
+            path="/:projectId/sample-metadata/upload/complete"
+            element={<SampleMetadataImportComplete />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+render(
+  <SampleMetadataImport />,
+  document.querySelector("#samples-metadata-import-root")
+);
