36745dea36174adbaed15ef14c25ccf553086fa4
Josh Adam <josh.adam@phac-aspc.gc.ca>
2023-02-06 05:15:48 -0600

    chore: Added recommended settings to remove `any`s
    See https://github.com/typescript-eslint/typescript-eslint/issues/6415

--- src/main/webapp/.eslintrc.js
+++ src/main/webapp/.eslintrc.js
@@ -31,5 +31,10 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/consistent-type-imports": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
   },
 };
