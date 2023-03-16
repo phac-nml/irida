97ccf665c235de8586c1d893f2324039a6a1d1ed
Jeffrey Thiessen <jeffreylthiessen@gmail.com>
2023-03-16 12:51:27 -0500

    fix unreachable chromedriver connections for tests

--- src/test/java/ca/corefacility/bioinformatics/irida/junit5/listeners/IntegrationUITestListener.java
+++ src/test/java/ca/corefacility/bioinformatics/irida/junit5/listeners/IntegrationUITestListener.java
@@ -94,6 +94,7 @@ public class IntegrationUITestListener implements TestExecutionListener {
 		}
 
 		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--remote-allow-origins=*");
 
 		// Set up default download directory
 		if (!DOWNLOAD_DIRECTORY.exists()) {
