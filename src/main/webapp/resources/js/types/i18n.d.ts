3b76cdda16048f3f39d20aa5d82f108e9750c277
Josh Adam <josh.adam@phac-aspc.gc.ca>
2024-11-06 11:18:24 -0600

    chore: Eslint autofixes

--- src/main/webapp/resources/js/types/i18n.d.ts
+++ src/main/webapp/resources/js/types/i18n.d.ts
@@ -1,2 +1,2 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 declare let i18n: (key: string, ...args: any[]) => string;
--- src/main/webapp/resources/js/types/irida.window.d.ts
+++ src/main/webapp/resources/js/types/irida.window.d.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 
 interface IridaWindow extends Window {
   project?: any;
